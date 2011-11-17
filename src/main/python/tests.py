0efff99275037ba60614da7ec0824b645cb73120
Carlos Sanchez <carlos.sanchez@nextthought.com>
2011-11-17 17:12:43 +0000

    remove unsed code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@3381 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/python/tests.py
+++ src/main/python/tests.py
@@ -1,7 +1,4 @@
 import os
-import sys
-import glob
-import argparse
 import unittest
 
 from unittest import defaultTestLoader
@@ -13,15 +10,13 @@ def load_suite(path, pattern, top_level_dir):
 		return suite 
 	except Exception, e:
 		print "Error importing tests from '%s'. %s" % (path, e)
-	except Error, e:
-		print "Error importing tests from '%s'. %s" % (path, e)
 		
 def collect_main_tests(path, pattern="*.py"):
 	test_list = []
 
-	def collector(dir):
-		for name in os.listdir(dir):
-			fd = os.path.join(dir, name)
+	def collector(dir_path):
+		for name in os.listdir(dir_path):
+			fd = os.path.join(dir_path, name)
 			if os.path.isdir(fd):
 				if name == "tests":
 					suite = load_suite(fd, pattern, path)
@@ -31,40 +26,18 @@ def collect_main_tests(path, pattern="*.py"):
 	collector(path)
 
 	return test_list
-		
-def collect_server_tests(path, pattern="test_*.py"):
-		 	
-	test_list = []
 	
-	def collector(dir):
-		suite = load_suite(dir, pattern, path)
-		if suite and suite.countTestCases() == 0:
-			for name in os.listdir(dir):
-				fd = os.path.join(dir, name)
-				if os.path.isdir(fd):
-					collector(fd)
-		elif suite:
-			test_list.extend(suite)
-			
-	collector(path)
-	
-	return test_list
-	
-def test_collector(include_integration=False):
+def test_collector():
 	"""
 	Collect all tests return a unittest.TestSuite
 	"""
 
 	test_list = []
 	src_dir = os.path.realpath(os.path.dirname(__file__))
-	main_dir = os.path.join( src_dir, "main/python/nti")
+	main_dir = os.path.join( src_dir, "nti")
 	
 	test_list.extend(collect_main_tests(main_dir))
 	
-	if include_integration:
-		tests_dir = os.path.join(src_dir, "test/python/servertests/integration/")
-		test_list.extend(collect_server_tests(tests_dir))
-
 	suite = unittest.TestSuite()
 	suite.addTests(test_list)
 		
@@ -73,6 +46,4 @@ def test_collector(include_integration=False):
 test_suite = test_collector
 
 if __name__ == '__main__':
-	args = sys.argv[1:]
-	include_integration = args[0] == '--include_integration' if args else False
-	test_collector(include_integration)
+	test_collector()
