91472141062e7ca695877c8523e3905ef286e26a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2011-11-17 17:41:52 +0000

    pick a better place .. for the time being
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@3385 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/buildout/setup.py
+++ src/main/buildout/setup.py
@@ -70,8 +70,8 @@ setup(
                         ],
     extras_require = {'test': ['z3c.coverage', 'zope.testing', 'zc.buildout']},
 
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
+    packages = find_packages('.'),
+    package_dir = {'': '.'},
     include_package_data = False,
     zip_safe = True,
     )
