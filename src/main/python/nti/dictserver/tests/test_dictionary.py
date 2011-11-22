8e6fe54ccca4d1ffb9b2f95bf952a0ff4e74f5af
Carlos Sanchez <carlos.sanchez@nextthought.com>
2011-11-22 00:25:33 +0000

    add __init__ and __main__ files
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@3480 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/python/nti/dictserver/tests/test_dictionary.py
+++ src/main/python/nti/dictserver/tests/test_dictionary.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python2.7
 
-import hamcrest
 from nti.appserver import tests
 from nti.dictserver import ChromeDictionary
 
 class TestDictionary(tests.ConfiguringTestBase): 
 
-	#This test makes sure that when a dict is constructed without a lookup path,
-	#or other bad path of some sort, it fails
+	# this test makes sure that when a dict is constructed without a lookup path,
+	# or other bad path of some sort, it fails
 	def test_badConstructorValues( self ):
 		self.assertRaises(LookupError, ChromeDictionary, '')
 		self.assertRaises(TypeError, ChromeDictionary)
