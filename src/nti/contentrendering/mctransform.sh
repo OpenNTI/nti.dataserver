9b4f7dc47101fb113cf6b25a3e7e44094d7ac59c
Jason Madden <jason.madden@nextthought.com>
2012-01-25 01:46:42 +0000

    Move the renderers to a resource location and fixup the paths.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@4611 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/mctransform.sh
+++ src/nti/contentrendering/mctransform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MYPATH=`dirname $0`
-export XHTMLTEMPLATES=$MYPATH/../renderers
+export XHTMLTEMPLATES=$MYPATH/zpts
 export PYTHONPATH=$MYPATH:$MYPATH/../plastex/
 
 ${PYTHON:-python2.7} $MYPATH/mctransform.py $*
