bbc00338581ec2ca7b29dad3dcf6d26bd8bd3c70
Jason Madden <jason.madden@nextthought.com>
2012-02-09 19:32:07 +0000

    Fix leaking greenlets during the indexing process.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@4993 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_indexagent.py
+++ src/nti/contentsearch/_indexagent.py
@@ -25,52 +25,68 @@ class IndexAgent(object):
 		self._queue = Queue()
 		self.indexmanager = indexmanager
 		def _worker():
+			# To guarantee that the greenlets run, we must
+			# keep a reference to them. We clear this when they finish,
+			# or we are asked to die. We only start them after we have
+			# a reference to them stored to avoid a race condition.
+			jobs = []
 			while True:
 				event = self._queue.get()
 				if event:
 					try:
-						self._handle_event(event)
+						job = self._handle_event(event)
+						jobs.append( job )
+						job.link( jobs.remove )
+						job.start()
 					except Exception:
 						logger.exception("When handling event %s" % event )
 				else:
 					logger.info("Exit signal for index agent worker received")
+					for job in jobs:
+						job.kill(block=True)
+					del jobs[:]
 					break
 
 		self._agent = gevent.spawn( _worker )
-		
+
 	def _handle_event(self, event):
 		"""
 		Handle user content indexing events.
+		:return: An unstarted greenlet.
 		"""
 
 		data = event.data
 
-		# TODO: These must be kept alive or they may never run!
 		if event.changeType in (Change.CREATED, Change.SHARED):
-			job = gevent.Greenlet.spawn(self.indexmanager.index_user_content,
-											externalValue=data,
-											username= event.creator,
-											typeName=event.dataType)
+			job = gevent.Greenlet(self.indexmanager.index_user_content,
+								  externalValue=data,
+								  username= event.creator,
+								  typeName=event.dataType)
 		elif event.changeType == Change.MODIFIED:
-			job = gevent.Greenlet.spawn(self.indexmanager.update_user_content,
-											externalValue=data,
-											username=event.creator,
-											typeName=event.dataType)
+			job = gevent.Greenlet(self.indexmanager.update_user_content,
+								  externalValue=data,
+								  username=event.creator,
+								  typeName=event.dataType)
 		elif event.changeType == Change.DELETED:
-			job = gevent.Greenlet.spawn(self.indexmanager.delete_user_content,
-											externalValue=data,
-											username=event.creator,
-											typeName=event.dataType)
-			
+			job = gevent.Greenlet(self.indexmanager.delete_user_content,
+								  externalValue=data,
+								  username=event.creator,
+								  typeName=event.dataType)
+
 		return job
-	
+
 	def _create_event(self, creator, changeType, dataType, data):
 		return _IndexEvent(creator, changeType, dataType, data)
-		
+
 	def add_event( self, creator, changeType, dataType, data):
 		event = self._create_event(creator, changeType, dataType, data)
 		logger.debug("Index event %s received", event)
 		self._queue.put_nowait( event )
 
 	def close( self ):
+		# Kill the background events and wait for them to
+		# die, to help ensure we're in a nice consistent state.
+		# If these things kept running after we're closed (and the indexmanager
+		# is closed) they will get exceptions
 		self._queue.put_nowait( None )
+		self._agent.join()
