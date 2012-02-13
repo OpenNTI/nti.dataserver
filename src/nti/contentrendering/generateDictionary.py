73c791a6694c6b6b8a84736d5579cc3c5f199803
Jason Madden <jason.madden@nextthought.com>
2012-02-13 15:58:27 +0000

    Fix bad import in generateDictionary. Could have sworn I did this once already.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@5055 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/generateDictionary.py
+++ src/nti/contentrendering/generateDictionary.py
@@ -1,8 +1,13 @@
-import struct, os, cPickle
+#!/usr/bin/env python
+import  os, cPickle
 import xml.sax
 import sys
-import zlib
 import gzip
+
+from xml.sax.handler import ContentHandler
+
+from pywikipedia.wiktionary import WiktionaryPage
+
 
 def main(args):
 	action = args.pop(0)
@@ -15,25 +20,24 @@ def main(args):
 
 def createDictionary(wikidumpname, destname):
 
-
 	#Keep an index of word to location in the file
-	index={}
+	index = {}
 
-	dictionary=gzip.open(destname, 'wb')
+	dictionary = gzip.open(destname, 'wb')
 
-	parser=xml.sax.make_parser()
+	parser = xml.sax.make_parser()
 	parser.setContentHandler(WiktionaryDumpHandler(index, dictionary))
-	dump=open(wikidumpname)
+	dump = open(wikidumpname)
 	parser.parse(dump)
 	dump.close()
 
 	dictionary.close()
 
-	path, ext=os.path.splitext(destname)
+	path, _ = os.path.splitext(destname)
 
-	indexLocation=path+'.index'
+	indexLocation = path + '.index'
 
-	indexFile=gzip.open(indexLocation, 'wb')
+	indexFile = gzip.open(indexLocation, 'wb')
 
 	cPickle.dump(index, indexFile)
 
@@ -43,93 +47,90 @@ import time
 
 def lookup(dicname, term):
 	start = time.time()
-	indexFile=gzip.open(dicname+'.index','rb')
-	index=cPickle.load(indexFile)
+	indexFile = gzip.open(dicname+'.index','rb')
+	index = cPickle.load(indexFile)
 	indexFile.close()
 
-	end=time.time()
+	end = time.time()
 
 	print 'Took %s seconds to load the index' % (end-start)
 
-	start=time.time()
+	start = time.time()
 	if term not in index:
 		return 'Not found'
 
-	print 'Checking at location %s'%index[term]
+	print 'Checking at location %s' % index[term]
 
-	dictionary=gzip.open(dicname+'.bin', 'rb')
+	dictionary = gzip.open(dicname+'.bin', 'rb')
 	dictionary.seek(index[term])
 
-	res= cPickle.load(dictionary)
+	res = cPickle.load(dictionary)
 	dictionary.close()
-	end=time.time()
+	end = time.time()
 
 	print 'Took %s seconds to lookup the entry' % (end-start)
 	return res
 
-from xml.sax.handler import ContentHandler
-
-from pywikipedia.wiktionary.wiktionarypage import *
-
 class WiktionaryDumpHandler(ContentHandler):
 
 	def __init__(self,index, dictionary):
-		self.index=index
-		self.dictionary=dictionary
+		ContentHandler.__init__( self )
+		self.index = index
+		self.dictionary = dictionary
 
-		self.name=u''
-		self.nameLabel='title'
-		self.insideName=False
+		self.name = u''
+		self.nameLabel = 'title'
+		self.insideName = False
 
-		self.markup=u''
-		self.markupLabel='text'
-		self.insideMarkup=False
+		self.markup = u''
+		self.markupLabel = 'text'
+		self.insideMarkup = False
 
-		self.pageLabel='page'
-		self.insidePage=False
+		self.pageLabel = 'page'
+		self.insidePage = False
 
-		self.page=0
+		self.page = 0
 
 	def startElement(self, localname, attrs):
 		if localname == self.pageLabel:
-			self.insidePage=True
-			self.page=self.page+1
-			print 'Found page %d'%self.page
+			self.insidePage = True
+			self.page = self.page+1
+			print 'Found page %d' % self.page
 		elif localname == self.nameLabel:
-			self.insideName=True
-			self.name=''
+			self.insideName = True
+			self.name = ''
 		elif localname == self.markupLabel:
-			self.insideMarkup=True
-			self.markup=''
+			self.insideMarkup = True
+			self.markup = ''
 
 	def endElement(self, localname):
 		if localname == self.pageLabel:
-			self.insidePage=False
+			self.insidePage = False
 		elif localname == self.nameLabel:
-			self.insideName=False
+			self.insideName = False
 			#In the event that its not a word (based on the title) set that we are out of the page so we don't collect wikimarkup
 			if self.name.find(':')>-1:
-				self.name=''
-				self.insidePage=False
+				self.name = ''
+				self.insidePage = False
 		elif self.insidePage and localname == self.markupLabel:
-			self.insideMarkup=False
+			self.insideMarkup = False
 			self.persistEntry(self.name, self.markup)
 
 	def persistEntry(self,title, text):
 		if text.lower().find('==english==') < 0:
 			return
-		loc=self.dictionary.tell()
+		loc = self.dictionary.tell()
 		#cPickle.dump(text, self.dictionary)
-		page=WiktionaryPage('en', title)
+		page = WiktionaryPage('en', title)
 		page.parseWikiPage(text)
 		cPickle.dump(page, self.dictionary)
-		self.index[title]=loc
+		self.index[title] = loc
 
 	def characters(self, data):
 		if self.insidePage and self.insideName:
-			self.name=self.name+data
+			self.name = self.name+data
 		elif self.insidePage and self.insideMarkup:
-			self.markup=self.markup+data
+			self.markup = self.markup + data
 
 
 
