0866b29a03ade72c1bded868833a239020471881
Jason Madden <jason.madden@nextthought.com>
2012-02-16 21:04:32 +0000

    Hook up the basics of a google-driven login. Still need to create account objects on our side, and store specific URLs for the future.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@5136 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/logon.py
+++ src/nti/appserver/logon.py
@@ -12,14 +12,16 @@ from nti.dataserver import interfaces as nti_interfaces
 from nti.dataserver.links import Link
 from nti.dataserver import mimetype
 
-from pyramid.view import view_config
+from pyramid.view import view_config, render_view_to_response
 from pyramid import security as sec
+import pyramid.request
 import pyramid.httpexceptions as hexc
 
 REL_HANDSHAKE = 'logon.handshake'
 REL_CONTINUE  = 'logon.continue'
 
 REL_LOGIN_NTI_PASSWORD = 'logon.nti.password'
+REL_LOGIN_GOOGLE = 'logon.google'
 
 def _links_for_authenticated_users( request ):
 	"""
@@ -78,6 +80,10 @@ def handshake(request):
 	links.append( Link( request.route_path( REL_LOGIN_NTI_PASSWORD ), rel=REL_LOGIN_NTI_PASSWORD ) )
 
 
+	links.append( Link( request.route_path( REL_LOGIN_GOOGLE ),
+						rel=REL_LOGIN_GOOGLE ) )
+
+
 	links.extend( _links_for_authenticated_users( request ) )
 
 	return _Handshake( links )
@@ -92,6 +98,29 @@ class _Handshake(dict):
 		dict.__init__( self )
 		self.links = lnks
 
+def _create_failure_response( request ):
+	response = None
+	if request.params.get( 'failure' ):
+		response = hexc.HTTPSeeOther( location=request.params.get( 'failure' ) )
+	else:
+		response = hexc.HTTPUnauthorized()
+	# Clear any cookies they sent that failed.
+	response.headers.extend( sec.forget(request) )
+
+	return response
+
+def _create_success_response( request, userid=None ):
+	# Incoming authentication worked. Remember the user, and
+	# either redirect or no-content
+	if request.params.get( 'success' ):
+		response = hexc.HTTPSeeOther( location=request.params.get( 'success' ) )
+	else:
+		response = hexc.HTTPNoContent()
+	if userid is None:
+		userid = sec.authenticated_userid( request )
+
+	response.headers.extend( sec.remember( request, userid ) )
+	return response
 
 
 @view_config(route_name=REL_LOGIN_NTI_PASSWORD, request_method='GET', renderer='rest')
@@ -99,20 +128,48 @@ def password_logon(request):
 	response = None
 
 	if not sec.authenticated_userid(request):
-		# Incoming authentication failed. Redirect or error
-		if request.params.get( 'failure' ):
-			response = hexc.HTTPSeeOther( location=request.params.get( 'failure' ) )
-		else:
-			response = hexc.HTTPUnauthorized()
-		# Clear any cookies they sent that failed.
-		response.headers.extend( sec.forget(request) )
+		response = _create_failure_response( request )
 	else:
-		# Incoming authentication worked. Remember the user, and
-		# either redirect or no-content
-		if request.params.get( 'success' ):
-			response = hexc.HTTPSeeOther( location=request.params.get( 'success' ) )
-		else:
-			response = hexc.HTTPNoContent()
-
-		response.headers.extend( sec.remember( request, sec.authenticated_userid( request ) ) )
+		response = _create_success_response( request )
 	return response
+
+import pyramid_openid.view
+
+@view_config(route_name=REL_LOGIN_GOOGLE, request_method="GET")
+def google_login(context, request):
+	nrequest = pyramid.request.Request.blank( request.route_url( 'logon.google.result', _query=request.params ),
+											  POST={'openid2': 'https://www.google.com/accounts/o8/id'} )
+	nrequest.registry = request.registry
+	# TODO: Why can't I use this API?
+#	return render_view_to_response(context, nrequest, name='verify_openid')
+	return pyramid_openid.view.verify_openid( context, nrequest )
+
+@view_config(route_name="logon.google.result", request_method='GET')
+def google_response(context, request):
+	"""
+	Process an OpenID response from google. This exists as a wrapper around
+	:func:`pyramid_openid.view.verify_openid` because that function
+	does nothing in failure, but we need to know about failure. (This is as-of
+	0.3.4; it is fixed in trunk.)
+	"""
+	response = None
+	openid_mode = request.params.get( 'openid.mode', None )
+	if openid_mode != 'id_res':
+		# Failure.
+		response = _create_failure_response( request )
+	else:
+		response = pyramid_openid.view.verify_openid( context, request )
+	return response
+
+def _openidcallback( context, request, success_dict ):
+	# Google only supports AX, sreg is ignored.
+	# Each of these comes back as a list, for some reason
+	fname = success_dict.get( 'ax', {} ).get('firstname', [''])[0]
+	lname = success_dict.get( 'ax', {} ).get('lastname', [''])[0]
+	email = success_dict.get( 'ax', {} ).get('email', [''])[0]
+	langu = success_dict.get( 'ax', {} ).get('language', [''])[0]
+	idurl = success_dict.get( 'identity_url' )
+	# TODO: Creating user object, etc
+	# The webapp actually needs its own cookie that we are not currently providing, so a simple
+	# redirect won't cut it. It's going to take some deeper cooperation.
+	return _create_success_response( request, userid=email )
