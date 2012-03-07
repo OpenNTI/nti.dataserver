8f2a276e505fa8d3e756c85d3380f4d77e174224
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-07 22:22:58 +0000

    for the time being
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@5609 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ old-python/zopyxtxng3corelogger.py
@@ -0,0 +1,13 @@
+import logging 
+logger = logging.getLogger('textindexng3')
+
+def PyLogger():
+	return logger
+
+def Z2Logger():
+	return logger
+
+def Logger():
+	return logger        
+
+LOG = logger
