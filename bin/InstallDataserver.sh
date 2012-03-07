a18ed0e797013c85e5c0de07aa2995a8a5cad540
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-07 21:21:16 +0000

    Install some nltk dep first through pip ( requires to find a fortran compier for scipy). Clean up the Dependencies file
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@5605 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- bin/InstallDataserver.sh
+++ bin/InstallDataserver.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [ -z "$WORKON_HOME" ]; then
 	echo "Must have a location to put virtualenv. Set WORKON_HOME."
 	exit 1
@@ -36,8 +35,15 @@ pip install --install-option="--zmq=/opt/local" pyzmq
 PROJECT_PARENT=${2:-~/Projects}
 echo "Checking project out to $PROJECT_PARENT/NextThoughtPlatform"
 
-mkdir -p $PROJECT_PARENT
-svn co https://svn.nextthought.com/repository/NextThoughtPlatform/trunk $PROJECT_PARENT/NextThoughtPlatform
+if [ -f "$PROJECT_PARENT/NextThoughtPlatform" ]; then
+	cd $PROJECT_PARENT/NextThoughtPlatform
+	svn up
+else
+	mkdir -p $PROJECT_PARENT
+	svn co https://svn.nextthought.com/repository/NextThoughtPlatform/trunk $PROJECT_PARENT/NextThoughtPlatform
+fi
+
+export INSTALL_EXTRAS="False"
 
 cd $PROJECT_PARENT/NextThoughtPlatform/nti.dataserver
 
@@ -45,6 +51,34 @@ cd $PROJECT_PARENT/NextThoughtPlatform/nti.dataserver
 # and probably add /usr/includ/libxml2/ to CFLAGS
 
 pip install -r requirements.txt
+
+if [ "$INSTALL_EXTRAS"=="True" ]; then
+
+	echo "Installing extras"
+
+	if [ -z "$UMFPACK" ]; then
+		export UMFPACK="None"
+	fi
+
+	TMPWK_DIR=`mktemp -d -t tmpwork`
+	chmod 777 $TMPWK_DIR
+	export PATH=$TMPWK_DIR:$PATH
+
+	if [ -f "/opt/local/bin/g95" ]; then
+		ln -s /opt/local/bin/g95 $TMPWK_DIR/g95
+	elif [ -f "/opt/local/bin/gfortran" ]; then
+		ln -s /opt/local/bin/gfortran $TMPWK_DIR/gfortran
+	elif [ -f "/opt/local/bin/gfortran-mp-4.4" ]; then
+		ln -s /opt/local/bin/gfortran-mp-4.4 $TMPWK_DIR/gfortran
+	fi
+	
+	pip install pyyaml
+	pip install numpy
+	pip install matplolib
+	pip install scipy
+	
+	rm -rf $TMPWK_DIR
+fi
 
 python setup.py develop
 
