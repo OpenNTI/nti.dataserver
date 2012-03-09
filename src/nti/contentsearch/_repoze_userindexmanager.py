cff14aee68b9c3da5226153d0289c1874acce8fa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-09 22:35:59 +0000

    check ongoing work before I really fall sleep and  I start checking things I have no clue about
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@5679 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_repoze_userindexmanager.py
+++ src/nti/contentsearch/_repoze_userindexmanager.py
@@ -4,23 +4,31 @@ logger = logging.getLogger( __name__ )
 #import time
 #from hashlib import md5
 
-#from contenttypes import empty_search_result
+
 #from contenttypes import empty_suggest_result
 #from contenttypes import merge_search_results
 #from contenttypes import merge_suggest_results
 #from contenttypes import empty_suggest_and_search_result
 #from contenttypes import merge_suggest_and_search_results
 
+from zope import component
 from zope import interface
-from . import interfaces
+
+from nti.dataserver import interfaces as nti_interfaces
+
+from nti.contentsearch import interfaces
+from nti.contentsearch._repoze_datastore import DataStore
+#from nti.contentsearch.contenttypes import empty_search_result
 
 class RepozeUserIndexManager(object):
 	interface.implements(interfaces.IUserIndexManager)
 
-	def __init__(self, username, datastore, use_md5=True):
-		self.use_md5 = use_md5
+	def __init__(self, username, repoze_store, dataserver=None):
 		self.username = username
-		self.datastore = datastore
+		self.datastore = repoze_store
+		self.ds = dataserver or component.queryUtility( nti_interfaces.IDataserver )
+		assert isinstance(repoze_store, DataStore), 'must specify a valid repoze store'
+		assert isinstance(dataserver, 'must specify a valid data server')
 
 	# -------------------
 	
@@ -34,14 +42,32 @@ class RepozeUserIndexManager(object):
 	
 	def get_username(self):
 		return self.username
+	
+	@property
+	def store(self):
+		return self.datastore
 
+	@property
+	def dataserver(self):
+		return self.ds
+	
 	# -------------------
 
+	def _get_catalog_names(self):
+		with self.datastore.dbTrans():
+			return self.get_catalog_names(self.username)
+			
 	def _adapt_search_on_types(self, search_on=None):
 		if search_on:
 			lm = lambda x: x[0:-1] if x.endswith('s') else x
-			search_on = [lm(x) for x in search_on]
+			search_on = [lm(x).lower() for x in search_on]
 		return search_on
 
 	def search(self, query, limit=None, search_on=None):
-		pass
+		search_on = self._adapt_search_on_types(search_on)
+		search_on = search_on if search_on else self._get_catalog_names()
+		#result = None
+		with self.store.dbTrans():
+			for _ in search_on:
+				pass #catalog = self.datastore.get_catalog(self.username, type_name)
+				
