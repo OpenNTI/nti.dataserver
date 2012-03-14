caeb403635906d81098e7a53a063046af4f12900
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-14 22:20:48 +0000

    work towards unifying index/search framework
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@5793 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_repoze_index.py
+++ src/nti/contentsearch/_repoze_index.py
@@ -5,24 +5,24 @@ from repoze.catalog.catalog import Catalog
 from repoze.catalog.indexes.field import CatalogFieldIndex
 from repoze.catalog.indexes.keyword import CatalogKeywordIndex
 
-from nti.dataserver.chat import MessageInfo
-from nti.dataserver.contenttypes import Note
-from nti.dataserver.contenttypes import Canvas
-from nti.dataserver.contenttypes import Highlight
-
 from nti.contentsearch.common import ngrams
-from nti.contentsearch.common import to_list
+from nti.contentsearch.common import get_attr
 from nti.contentsearch.common import epoch_time
 from nti.contentsearch.common import get_content
 from nti.contentsearch.common import get_collection
+from nti.contentsearch.common import get_multipart_content
 from nti.contentsearch.common import word_content_highlight
 from nti.contentsearch.common import ngram_content_highlight
 from nti.contentsearch.textindexng3 import CatalogTextIndexNG3
-from nti.contentsearch.common import (	OID, NTIID, CREATOR, LAST_MODIFIED, CONTAINER_ID, CLASS, TYPE, \
-										COLLECTION_ID, ITEMS, SNIPPET, ID)
 
-from nti.contentsearch.common import (	color_, ngrams_, channel_, content_, keywords_, references_, \
-										recipients_, sharedWith_ )
+from nti.contentsearch.common import (	oid_fields, ntiid_fields, creator_fields, container_id_fields,
+										last_modified_fields)
+
+from nti.contentsearch.common import (	OID, NTIID, CREATOR, LAST_MODIFIED, CONTAINER_ID, CLASS, TYPE,
+										COLLECTION_ID, SNIPPET, HIT, ID)
+
+from nti.contentsearch.common import (	color_, ngrams_, channel_, content_, keywords_, references_,
+										recipients_, sharedWith_, body_, startHighlightedFullText_)
 
 
 import logging
@@ -30,37 +30,14 @@ logger = logging.getLogger( __name__ )
 
 # -----------------------------------
 	
-def get_attr(obj, names, default=None):
-	if not obj: return default
-	
-	names = to_list(names)
-	if isinstance(obj, dict):
-		for name in names:
-			value = obj.get(name,None)
-			if value: return value
-	else:
-		for name in names:
-			try:
-				value = getattr(obj, name, None)
-			except:
-				value = None
-			if value: return value
-	return default
-
 def _attrs(names):
 	lexp = lambda x,y: get_attr(x, names,y)
 	return lexp
 
 # -----------------------------------
 
-_oid_fields = [OID, 'oid', 'id']
-_ntiid_fields = [NTIID, 'ntiid']
-_creator_fields = [CREATOR, 'creator']
-_container_id_fields = [CONTAINER_ID, 'ContainerId', 'containerId', 'container']
-_last_modified_fields =  [LAST_MODIFIED, 'lastModified', 'LastModified', 'last_modified']
-
 def _last_modified(obj, default):
-	value  = get_attr(obj, _last_modified_fields, default)
+	value  = get_attr(obj, last_modified_fields, default)
 	if value:
 		if isinstance(value, basestring):
 			value = float(value)
@@ -84,11 +61,11 @@ def _keywords(names):
 	return f
 	
 def _collectionId(obj, default):
-	containerId = get_attr(obj, _container_id_fields, default)
+	containerId = get_attr(obj, container_id_fields, default)
 	return get_collection(containerId)
 
 def get_objectId(data):
-	return data if isinstance(data, basestring) else get_attr(data, _oid_fields)
+	return data if isinstance(data, basestring) else get_attr(data, oid_fields)
 
 # -----------------------------------
 
@@ -98,43 +75,6 @@ def _content(names):
 		value = get_content(value)
 		return value
 	return f
-
-def get_multipart_content(source):
-	
-	gbls = globals()
-			
-	if isinstance(source, basestring):
-		return get_content(source)
-	elif isinstance(source, Iterable):
-		
-		def process_dict(d):
-			clazz = item.get(CLASS, None)
-			data = item.get(ITEMS, None)
-			if clazz and data:
-				name = "get_%s_content" % clazz.lower()
-				if name in gbls:
-					return gbls[name](d)
-			return u''
-		
-		items = []
-		if isinstance(source, dict):
-			items.append(process_dict(source))
-		else:
-			for item in source:
-				if isinstance(item, basestring) and item:
-					items.append(item)
-					continue
-				elif isinstance(item, dict):
-					items.append(process_dict(item))
-				else:
-					items.add(get_multipart_content(item))
-		return get_content(' '.join(items))
-	elif not source:
-		clazz = source.__class__.__name__
-		name = "get_%s_content" % clazz.lower()
-		if name in gbls:
-			return gbls[name](source)
-	return u''
 
 def _multipart_content(names):
 	def f(obj, default):
@@ -152,88 +92,41 @@ def _ngrams(names):
 
 # -----------------------------------
 
-_body = 'body'
-_startHighlightedFullText = 'startHighlightedFullText'
-
-def get_highlight_content(data):
-	if isinstance(data, dict):
-		return data.get(_startHighlightedFullText, u'')
-	elif isinstance(data, Highlight):
-		return getattr(data, _startHighlightedFullText, u'')
-	return u''
-
-def get_canvas_content(data):
-	result = []
-	if isinstance(data, dict):
-		shapes = data.get('shapeList', [])
-	elif isinstance(data, Canvas):
-		shapes = data.shapeList
-		
-	for s in shapes:
-		c = get_multipart_content(s)
-		if c: result.append(c)
-	return ' '.join(result)
-
-def get_note_content(data):
-	result = []
-	if isinstance(data, dict):
-		body = to_list(data.get(_body, u''))
-	elif isinstance(data, Note):
-		body = to_list(data.body)
-		
-	for item in body:
-		c = get_multipart_content(item)
-		if c: result.append(c)
-	return ' '.join(result)
-
-def get_messageinfo_content(data):
-	result = []
-	if isinstance(data, dict):
-		body = to_list(data.get(_body, u''))
-	elif isinstance(data, MessageInfo):
-		body = to_list(data.body)
-	for item in body:
-		c = get_multipart_content(item)
-		if c: result.append(c)
-	return ' '.join(result)
-
-# -----------------------------------
-
 def _create_text_index(field, discriminator):
 	return CatalogTextIndexNG3(field, discriminator)
 
 def _create_treadable_mixin_catalog():
 	catalog = Catalog()
 	catalog[LAST_MODIFIED] = CatalogFieldIndex(_last_modified)
-	catalog[OID] = CatalogFieldIndex(_attrs(_oid_fields))
-	catalog[CONTAINER_ID] = CatalogFieldIndex(_attrs(_container_id_fields))
+	catalog[OID] = CatalogFieldIndex(_attrs(oid_fields))
+	catalog[CONTAINER_ID] = CatalogFieldIndex(_attrs(container_id_fields))
 	catalog[COLLECTION_ID] = CatalogFieldIndex(_collectionId)
-	catalog[CREATOR] = CatalogFieldIndex(_attrs(_creator_fields))
-	catalog[NTIID] = CatalogFieldIndex(_attrs(_ntiid_fields))
+	catalog[CREATOR] = CatalogFieldIndex(_attrs(creator_fields))
+	catalog[NTIID] = CatalogFieldIndex(_attrs(ntiid_fields))
 	catalog[keywords_] = CatalogKeywordIndex(_keywords([keywords_]))
 	catalog[sharedWith_] = CatalogKeywordIndex(_keywords([sharedWith_]))
 	return catalog
 
 def create_notes_catalog():
 	catalog = _create_treadable_mixin_catalog()
-	catalog[ngrams_] = _create_text_index(ngrams_, _ngrams([_body]))
+	catalog[ngrams_] = _create_text_index(ngrams_, _ngrams([body_]))
 	catalog[references_] = CatalogKeywordIndex(_keywords([references_]))
-	catalog[content_] = _create_text_index(content_, _multipart_content([_body]))
+	catalog[content_] = _create_text_index(content_, _multipart_content([body_]))
 	return catalog
 	
 def create_highlight_catalog():
 	catalog = _create_treadable_mixin_catalog()
 	catalog[color_] = CatalogFieldIndex(_attrs([color_]))
-	catalog[ngrams_] = _create_text_index(ngrams_, _ngrams([_startHighlightedFullText]))
-	catalog[content_] = _create_text_index(content_, _content([_startHighlightedFullText]))
+	catalog[ngrams_] = _create_text_index(ngrams_, _ngrams([startHighlightedFullText_]))
+	catalog[content_] = _create_text_index(content_, _content([startHighlightedFullText_]))
 	return catalog
 
 def create_messageinfo_catalog():
 	catalog = _create_treadable_mixin_catalog()
 	catalog[ID] = CatalogFieldIndex(_attrs([ID]))
 	catalog[channel_] = CatalogFieldIndex(_attrs([channel_]))
-	catalog[ngrams_] = _create_text_index(ngrams_, _ngrams([_body]))
-	catalog[content_] = _create_text_index(content_, _multipart_content([_body]))
+	catalog[ngrams_] = _create_text_index(ngrams_, _ngrams([body_]))
+	catalog[content_] = _create_text_index(content_, _multipart_content([body_]))
 	catalog[references_] = CatalogKeywordIndex(_keywords([references_]))
 	catalog[recipients_] = CatalogKeywordIndex(_keywords([recipients_]))
 	return catalog
@@ -259,7 +152,7 @@ def get_type_name(obj):
 		return get_attr(obj, [CLASS])
 		
 def _get_last_modified(obj):
-	lm = get_attr(obj, _last_modified_fields )
+	lm = get_attr(obj, last_modified_fields )
 	return lm if lm else 0
 
 def _word_content_highlight(query=None, text=None,  *args, **kwargs):
@@ -278,29 +171,29 @@ def _highlight_content(query=None, text=None, use_word_highlight=True):
 	return content if content else text
 
 def _get_index_hit_from_object(obj):
-	result = {TYPE : get_type_name(obj)}		
-	result[OID] =  get_attr(obj, _oid_fields )
-	result[NTIID] =  get_attr(obj, _ntiid_fields )
-	result[CREATOR] =  get_attr(obj, _creator_fields )
-	result[CONTAINER_ID] = get_attr(obj, _container_id_fields )
+	result = {TYPE : get_type_name(obj), CLASS:HIT}		
+	result[OID] =  get_attr(obj, oid_fields )
+	result[NTIID] =  get_attr(obj, ntiid_fields )
+	result[CREATOR] =  get_attr(obj, creator_fields )
+	result[CONTAINER_ID] = get_attr(obj, container_id_fields )
 	result[COLLECTION_ID] = get_collection(result[CONTAINER_ID])
 	result[LAST_MODIFIED] = _get_last_modified(obj)
 	return result
 
 def get_index_hit_from_note(obj, query=None, use_word_highlight=True, *args, **kwargs):
-	text = get_attr(obj, [_body])
+	text = get_attr(obj, [body_])
 	result = _get_index_hit_from_object(obj)
 	result[SNIPPET] = _highlight_content(query, text, use_word_highlight)
 	return result
 
 def get_index_hit_from_hightlight(obj, query=None, use_word_highlight=True, *args, **kwargs):
 	result = _get_index_hit_from_object(obj)
-	text = get_attr(obj, [_startHighlightedFullText])
+	text = get_attr(obj, [startHighlightedFullText_])
 	result[SNIPPET] = _highlight_content(query, text, use_word_highlight)
 	return result
 
 def get_index_hit_from_messgeinfo(obj, query=None, use_word_highlight=True, *args, **kwargs):
-	text = get_attr(obj, [_body])
+	text = get_attr(obj, [body_])
 	result = _get_index_hit_from_object(obj)
 	result[SNIPPET] = _highlight_content(query, text, use_word_highlight)
 	return result
---
+++ src/nti/contentsearch/_whoosh_index.py
@@ -0,0 +1,499 @@
+import time
+import inspect
+import collections
+from datetime import datetime
+
+from whoosh import fields
+from whoosh import highlight
+from whoosh.searching import Hit
+from whoosh.qparser import QueryParser
+from whoosh.qparser import GtLtPlugin
+from whoosh.qparser.dateparse import DateParserPlugin
+
+from nti.contentsearch.common import echo
+from nti.contentsearch.common import get_attr
+from nti.contentsearch.common import epoch_time
+from nti.contentsearch.common import get_content
+from nti.contentsearch.common import get_collection
+from nti.contentsearch.common import empty_search_result
+from nti.contentsearch.common import empty_suggest_result
+from nti.contentsearch.common import word_content_highlight
+
+from nti.contentsearch.common import (	NTIID, CREATOR, LAST_MODIFIED, TYPE, CLASS, ID, 
+										COLLECTION_ID, ITEMS, SNIPPET, HIT, HIT_COUNT, SUGGESTIONS, 
+										CONTENT, CONTAINER_ID, TARGET_OID, BODY)
+
+from nti.contentsearch.common import (	color_, quick_, channel_, content_, keywords_, references_, body_,
+										id_, recipients_, sharedWith_, oid_ , ntiid_, title_, last_modified_,
+										creator_, startHighlightedFullText_, containerId_, collectionId_)
+	
+from nti.contentsearch.common import (	oid_fields, ntiid_fields, creator_fields, container_id_fields,
+										last_modified_fields)
+		
+import logging
+logger = logging.getLogger( __name__ )
+
+# ----------------------------------
+
+def get_datetime(x=None):
+	f = time.time()
+	if x:
+		f = float(x) if isinstance(x, basestring) else x
+	return datetime.fromtimestamp(f)
+
+def get_keywords(records):
+	result = ''
+	if records:
+		result = ','.join(records)
+	return unicode(result)
+
+def get_text_from_mutil_part_body(body):
+
+	if not body or isinstance(body, basestring):
+		return get_content(body)
+	elif isinstance(body, collections.Iterable):
+
+		gbls = globals()
+
+		def add_to_items(d, key):
+			data = d[key] if d and key in d else None
+			if data: items.append(str(data))
+
+		def add_from_dict(item):
+			name = item[CLASS] if CLASS in item else None
+			if 	name in gbls and inspect.isclass(gbls[name]) and \
+				hasattr(gbls[name], '__indexable__'):
+				try:
+					obj = gbls[name]()
+					d = obj.get_index_data(item)
+					add_to_items(d, content_)
+					add_to_items(d, 'text')
+				except:
+					pass
+
+		items = []
+		if isinstance(body, dict):
+			add_from_dict(body)
+		else:
+			for item in body:
+				if isinstance(item, basestring) and item:
+					items.append(item)
+				elif isinstance(item, dict):
+					add_from_dict(item)
+
+		return get_content(' '.join(items))
+	else:
+		return get_content(str(body))
+
+get_highlighted_content = word_content_highlight
+
+# ----------------------------------
+
+_default_search_limit = 10
+_default_word_max_dist = 15
+_default_suggest_limit = 15
+_default_search_plugins =  (GtLtPlugin, DateParserPlugin)
+
+class _SearchableContent(object):
+	
+	__indexable__ = False
+	
+	def get_schema(self):
+		return getattr(self, '_schema', None)
+	
+	def external_name(self, name):
+		"""
+		external name for the schema field name
+		"""
+		if name ==  ntiid_:
+			return NTIID
+		elif name ==  oid_:
+			return TARGET_OID
+		elif name == title_:
+			return name.capitalize()
+		elif name == last_modified_:
+			return LAST_MODIFIED
+		else:
+			return name
+	
+	# ---------------
+	
+	def _prepare_query(self, field, query, plugins=_default_search_plugins):
+		qp = QueryParser(field, schema=self.get_schema())
+		for pg in plugins or ():
+			qp.add_plugin(pg())
+		parsed_query = qp.parse(unicode(query))
+		return parsed_query
+	
+	def search(self, searcher, query, limit=_default_search_limit, *args, **kwargs):
+		parsed_query = self._prepare_query(content_, query)
+		return self.execute_query_and_externalize(searcher, content_, parsed_query, query, limit)
+		
+	def quick_search(self, searcher, query, limit=_default_search_limit, *args, **kwargs):
+		parsed_query = self._prepare_query(quick_, query)
+		return self.execute_query_and_externalize(searcher, quick_, parsed_query, query, limit)
+		
+	def suggest_and_search(self, searcher, query, limit=_default_search_limit, *args, **kwargs):
+		if ' ' in query:
+			suggestions = []
+			result = self.search(searcher, query, limit)
+		else:
+			prefix = kwargs.get('prefix', len(query))
+			maxdist = kwargs.get('maxdist', _default_word_max_dist)
+			suggest_limit = kwargs.get('suggest_limit', _default_suggest_limit)
+			suggestions = searcher.suggest(query, maxdist=maxdist, limit=suggest_limit, prefix=prefix)
+
+			if suggestions:
+				result = self.search(searcher, suggestions[0], limit, *args, **kwargs)
+			else:
+				result = self.search(searcher, query, limit,  *args, **kwargs)
+
+		result[SUGGESTIONS] = suggestions
+		return result
+
+	def suggest(self, searcher, word, limit=_default_suggest_limit, *args, **kwargs):
+		prefix = kwargs.get('prefix', len(word))
+		maxdist = kwargs.get('maxdist', _default_word_max_dist)
+		
+		result = empty_suggest_result(word)
+		records = searcher.suggest(word, maxdist=maxdist, limit=limit, prefix=prefix)
+		result[ITEMS] = records
+		result[HIT_COUNT] = len(records)
+		return result
+
+	def execute_query_and_externalize(self, searcher, search_field, parsed_query, query, limit, *args, **kwargs):
+
+		stored_field = search_field in self.get_schema().stored_names()
+		field_found_in_query = search_field in list(t[0] for t in parsed_query.iter_all_terms())
+		search_hits = searcher.search(parsed_query, limit=limit)
+	
+		surround = kwargs.get('surround', 20)
+		maxchars = kwargs.get('maxchars', 300)
+		search_hits.formatter = highlight.UppercaseFormatter()
+		search_hits.fragmenter = highlight.ContextFragmenter(maxchars=maxchars, surround=surround)
+		
+		result = empty_search_result(query)
+		items = result[ITEMS]
+		
+		lm = 0
+		for hit_count, hit in enumerate(search_hits):
+			d = {}
+			item_id = self.get_data_from_search_hit(hit, d)
+			lm = max (lm, d.get(LAST_MODIFIED, 0))
+	
+			snippet = None
+			if stored_field:
+				if field_found_in_query:
+					snippet = hit.highlights(search_field)
+				else:
+					snippet = get_highlighted_content(query, 
+													  hit.get(content_, u''),
+													  maxchars=maxchars, 
+													  surround=surround)
+			
+			if not snippet:
+				snippet = hit.get(content_, u'')
+
+			d[SNIPPET] = snippet
+			items[item_id or hit_count] = d
+
+		result[LAST_MODIFIED] = lm
+		result[HIT_COUNT] = len(items)
+		return result
+
+	def get_data_from_search_hit(self, hit, d):
+		"""
+		return a dictionary with that that represents a search hit
+		"""
+		d[CLASS] = HIT
+		d[LAST_MODIFIED] = epoch_time(hit[last_modified_]) if last_modified_ in hit else 0
+		return str(hit.docnum) if hit.__class__ == Hit else None
+	
+# ----------------------------------
+
+def create_book_schema():
+	"""
+	Book index schema
+
+	ntiid: Internal nextthought ID for the chapter/section
+	title: chapter/section title
+	last_modified: chapter/section last modification since the epoch
+	keywords: chapter/section key words
+	content: chapter/section text
+	quick: chapter/section text ngrams
+	related: ntiids of related sections
+	ref: chapter reference
+	"""
+	
+	schema = fields.Schema(	ntiid = fields.ID(stored=True, unique=True),
+							title = fields.TEXT(stored=True, spelling=True),
+				  			last_modified = fields.DATETIME(stored=True),
+				  			keywords = fields.KEYWORD(stored=True), 
+				 			quick = fields.NGRAM(maxsize=10),
+				 			related = fields.KEYWORD(stored=True),
+				 			section = fields.TEXT(),
+				 			content = fields.TEXT(stored=True, spelling=True))
+	return schema
+
+class Book(_SearchableContent):
+	"""
+	Base clase for any a book index.
+	"""
+	
+	_schema = create_book_schema()
+	
+	def get_data_from_search_hit(self, hit, d):
+		super(Book, self).get_data_from_search_hit(hit, d)
+		d[TYPE] = CONTENT
+		d[self.external_name(title_)] = hit[title_]
+		d[self.external_name(ntiid_)] = hit[ntiid_]
+		d[CONTAINER_ID] = hit[ntiid_]
+		return hit[ntiid_]
+
+# ----------------------------------
+
+class UserIndexableContent(_SearchableContent):
+
+	__indexable__ = False
+
+	def external_name(self, name):
+		if name ==  'creator':
+			return CREATOR
+		elif name ==  'containerId':
+			return CONTAINER_ID
+		elif name == collectionId_:
+			return COLLECTION_ID
+		else:
+			return super(UserIndexableContent, self).external_name(name)
+		
+	def get_index_data(self, data, *args, **kwargs):
+		"""
+		return a dictonary with the info to be stored in the index
+		"""
+		result = {}
+		if isinstance(data, basestring):
+			result[oid_] = data
+		else:
+			result[oid_] = echo(get_attr(data, oid_fields))
+			result[ntiid_] = echo(get_attr(data, ntiid_fields))
+			result[creator_] = echo(get_attr(data, creator_fields))
+			result[containerId_] = echo(get_attr(data, container_id_fields))
+			result[collectionId_] = echo(get_collection(result[containerId_]))
+			result[last_modified_] = get_datetime(get_attr(data, last_modified_fields))
+		return result
+
+	def get_data_from_search_hit(self, hit, d):
+		super(UserIndexableContent, self).get_data_from_search_hit(hit, d)
+		d[TYPE] = self.__class__.__name__
+		d[self.external_name(oid_)] = hit.get(oid_, '')
+		d[self.external_name(ntiid_)] = hit.get(ntiid_, '')
+		d[self.external_name(creator_)] = hit.get(creator_, '')
+		d[self.external_name(containerId_)] = hit.get(containerId_, '')
+		d[self.external_name(collectionId_)] = hit.get(collectionId_, '')
+		return hit.get(oid_, None)
+	
+	# ---------------
+	
+	def index_content(self, writer, data, auto_commit=True, **commit_args):
+		d = self.get_index_data(data)
+		if d.has_key(oid_) and d.has_key(containerId_):
+			writer.add_document(**d)
+			if auto_commit:
+				writer.commit(**commit_args)
+
+	def update_content(self, writer, data, auto_commit=True, **commit_args):
+		d = self.get_index_data(data)
+		if d.has_key(oid_) and d.has_key(containerId_):
+			writer.update_document(**d)
+			if auto_commit:
+				writer.commit(**commit_args)
+		
+	def delete_content(self, writer, data, auto_commit=True, **commit_args):
+		d = self.get_index_data(data)
+		if d.has_key(oid_) and d.has_key(containerId_):
+			writer.delete_by_term(oid_, unicode(d[oid_]))
+			if auto_commit:
+				writer.commit(**commit_args)
+				
+# ----------------------------------
+
+def create_highlight_schema():
+	"""
+	Highlight index schema
+
+	collectionId: Book/Library that the highlight belongs to
+	ntiid: Highlight nti id
+	oid: Highlight object id
+	creator: Highlight creator username
+	last_modified: Highlight last modification time
+	content: Highlight text
+	sharedWith: Highlight shared users
+	color: Highlight color
+	quick: Highlight text ngrams
+	keywords: Highlight key words
+	"""
+	
+	schema = fields.Schema(	collectionId = fields.ID(stored=True),
+							oid = fields.ID(stored=True, unique=True),
+							containerId = fields.ID(stored=True),
+							creator = fields.ID(stored=True),
+				  			last_modified = fields.DATETIME(stored=True),
+				  			content = fields.TEXT(stored=True, spelling=True),
+				  			sharedWith = fields.KEYWORD(stored=False), 
+				  			color = fields.TEXT(stored=False),
+				 			quick = fields.NGRAM(maxsize=10),
+				 			keywords = fields.KEYWORD(stored=True),
+				 			ntiid = fields.ID(stored=True))
+	return schema
+
+class Highlight(UserIndexableContent):
+	"""
+	Base clase for Highlight indexable content.
+	"""
+	
+	__indexable__ = True
+	_schema = create_highlight_schema()
+
+	def get_index_data(self, data, *args, **kwargs):
+		result = UserIndexableContent.get_index_data(self, data, *args, **kwargs)
+		result[color_] = echo(get_attr(data, color_))
+		result[keywords_] = get_keywords(get_attr(data, keywords_))
+		result[sharedWith_] = get_keywords(get_attr(data, sharedWith_))
+		result[content_] = get_content(get_attr(data, startHighlightedFullText_))
+		result[quick_] = result[content_]
+		return result
+	
+# ----------------------------------
+
+def create_note_schema():
+	"""
+	Note index schema
+
+	collectionId: Book/Library that the note belongs to
+	ntiid: Note nti id
+	oid: Note object id
+	creator: Note creator username
+	last_modified: Note last modification time
+	content: Note text content
+	references: Note references
+	sharedWith: Highlight shared users
+	id: Note id
+	quick: Note text ngrams
+	keywords: Note key words
+	"""
+	
+	schema = fields.Schema(	collectionId=fields.ID(stored=True),
+							oid = fields.ID(stored=True, unique=True),
+							containerId = fields.ID(stored=True),
+							creator = fields.ID(stored=True),
+				  			last_modified = fields.DATETIME(stored=True),
+				  			content = fields.TEXT(stored=True, spelling=True),
+				  			sharedWith = fields.KEYWORD(stored=False), 
+				  			references = fields.KEYWORD(stored=False), 
+				 			quick = fields.NGRAM(maxsize=10),
+				 			id = fields.NUMERIC(int, stored=False),
+				 			keywords = fields.KEYWORD(stored=True),
+				 			ntiid = fields.ID(stored=True))
+	return schema
+
+class Note(Highlight):
+	"""
+	Base clase for Note indexable content.
+	"""
+	
+	__indexable__ = True
+	_schema = create_note_schema()
+	
+	def get_index_data(self, data, *args, **kwargs):
+		result = UserIndexableContent.get_index_data(self, data, *args, **kwargs)
+		result[keywords_] = get_keywords(get_attr(data, keywords_))
+		result[references_] = get_keywords(get_attr(data, references_))
+		result[sharedWith_] = get_keywords(get_attr(data, sharedWith_))
+		result[content_] = get_text_from_mutil_part_body(get_attr(data, body_))
+		result[quick_] = result[content_]
+		return result
+
+# ----------------------------------
+
+def create_messageinfo_schema():
+	"""
+	MessageInfo/Chat index schema
+
+	collectionId: Book/Library that the chat/message belongs to
+	ntiid: Chat/message nti id
+	oid: Chat/message object id
+	creator: Chat/message creator username
+	last_modified: Chat/message last modification time
+	content: Chat/message text content
+	references: Chat/message references
+	sharedWith: Chat/message shared users
+	id: Chat/message id
+	quick: Chat/message text ngrams
+	keywords: Chat/message key words
+	"""
+	
+	schema = fields.Schema(	containerId = fields.ID(stored=True), 
+				  			content = fields.TEXT(stored=True, spelling=True),
+				  			creator = fields.ID(stored=True),
+				  			channel = fields.ID(stored=False),
+				  			references = fields.KEYWORD(stored=False), 
+				  			recipients = fields.KEYWORD(stored=False), 
+				  			sharedWith = fields.KEYWORD(stored=False), 
+				 			quick = fields.NGRAM(maxsize=10),
+				 			oid = fields.ID(stored=True, unique=True),
+				 			id = fields.ID(stored=True, unique=True),
+				 			last_modified = fields.DATETIME(stored=True),
+				 			keywords = fields.KEYWORD(stored=True),
+				 			ntiid = fields.ID(stored=True))
+	return schema
+
+class MessageInfo(Note):
+	"""
+	Base clase for any user chat message indices.
+	"""
+
+	__indexable__ = True
+	_schema = create_messageinfo_schema()
+
+	def get_index_data(self, data, *args, **kwargs):
+		result = UserIndexableContent.get_index_data(self, data, *args, **kwargs)
+		result[id_] = echo(get_attr(data, ID))
+		result[channel_] = echo(get_attr(data, channel_))
+		result[keywords_] = get_keywords(get_attr(data, keywords_))
+		result[references_] = get_keywords(get_attr(data, references_))
+		result[sharedWith_] = get_keywords(get_attr(data, sharedWith_))
+		result[recipients_] = get_keywords(get_attr(data, recipients_))
+		result[content_] = get_text_from_mutil_part_body(get_attr(data, BODY))
+		result[quick_] = result[content_]
+		return result
+
+# ----------------------------------
+
+class _Illustration(UserIndexableContent):
+	__indexable__ = False
+	
+	def get_index_data(self, data, *args, **kwargs):
+		return {}
+
+class Canvas(_Illustration):
+	
+	def get_index_data(self, data, *args, **kwargs):
+		items = []
+		shapeList = get_attr(data, 'shapeList', [])
+		for shape in shapeList:
+			txt = get_text_from_mutil_part_body(shape)
+			if txt: items.append(txt)
+		return {'text': ' '.join(items)} if items else {}
+
+class CanvasShape(_Illustration):
+	pass
+
+class CanvasCircleShape(CanvasShape):
+	pass
+
+class CanvasPolygonShape(CanvasShape):
+	pass
+
+class CanvasTextShape(CanvasShape):
+	pass
+
