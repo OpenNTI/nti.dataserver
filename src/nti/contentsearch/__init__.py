4b9e95a709db082008d411b89fb17b851122e5bd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-15 02:14:39 +0000

    add least frequen. used map
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@5807 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/__init__.py
+++ src/nti/contentsearch/__init__.py
@@ -1,6 +1,11 @@
+import UserDict
+from heapq import nsmallest
+from operator import itemgetter
+
 import logging
 logger = logging.getLogger( __name__ )
 
+# -----------------------------
 
 _instances = {}
 def singleton(cls):
@@ -35,6 +40,8 @@ class IndexTypeMixin(object):
 	def __repr__( self ):
 		return 'IndexTypeMixin(indexname=%s, type=%s)' %  (self.indexname, self.type_instance)
 
+# -----------------------------
+
 class NoOpCM(object):
 
 	singleton = None
@@ -49,3 +56,53 @@ class NoOpCM(object):
 
 	def __exit__(self,*args):
 		pass
+	
+# -----------------------------
+
+class _Counter(dict):
+	def __missing__(self, key):
+		return 0
+
+class LFUMap(UserDict.DictMixin):
+	
+	_kwd_mark = object()  
+	
+	def __init__(self, maxitems, maxremoval=1, callback=None):
+		self.cache = {}
+		self.use_count = _Counter()  
+		self.maxitems = maxitems
+		self.maxremoval = min(maxremoval, maxitems)
+	
+	def __len__(self):
+		return len(self.cache)
+
+	def __contains__(self, key):
+		return key in self.cache
+	
+	def __getitem__(self, key):
+		item = self.cache[key]
+		self.use_count[key] += 1
+		return item
+
+	def __setitem__(self, key, value):
+		self.cache[key] = value
+		self.use_count[key] = 0
+		if len(self.cache) > self.maxitems:
+			for key, _ in nsmallest(self.maxremoval, 
+									self.use_count.iteritems(),
+									key=itemgetter(1)):
+				del self.cache[key], self.use_count[key]
+
+	def __delitem__(self, key):
+		self.cache.pop(key)
+		if key in self.use_count:
+			self.use_count.pop(key)
+	
+	def __iter__(self):
+		return iter(self.cache)
+			
+	def iteritems(self):
+		return self.cache.iteritems()
+
+# -----------------------------
+
