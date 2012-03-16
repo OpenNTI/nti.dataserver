a3cc8fbde34bcb50986dbec4ad0524f0adfd8ed6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-16 19:04:23 +0000

    add a note test file
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@5890 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/tests/test_highlights.py
+++ src/nti/contentsearch/tests/test_highlights.py
@@ -32,24 +32,24 @@ class TestHighlight(unittest.TestCase):
 			d = cm.search(s, "divide")
 			self.assertEqual(d['Hit Count'], 1)
 			self.assertEqual(d['Query'], "divide")
-			self.assertEqual(d['Last Modified'], 1321584477.793211)
+			self.assertEqual(d['Last Modified'], 1331922120.967309)
 
 			items = d['Items']
 			self.assertEqual(len(items), 1)
-			item = items['0x62:5573657273']
+			item = items['tag:nextthought.com,2011-10:carlos.sanchez@nextthought.com-OID-0x085a:5573657273']
 			
-			self.assert_(item['NTIID'])
 			self.assertEqual('Hit', item['Class'])
 			self.assertEqual('Highlight', item['Type'])
 			self.assertEqual('prealgebra', item['CollectionID'])
-			self.assertEqual("0x62:5573657273", item["TargetOID"])
+			self.assertEqual("tag:nextthought.com,2011-10:carlos.sanchez@nextthought.com-OID-0x085a:5573657273", item["TargetOID"])
+			self.assertEqual("tag:nextthought.com,2011-10:carlos.sanchez@nextthought.com-OID-0x085a:5573657273", item["NTIID"])
 			self.assertEqual('multiply and DIVIDE In fact you may already', item['Snippet'])
-			self.assertEqual('tag:nextthought.com,2011-07-14:AOPS-HTML-prealgebra-0', item['ContainerId'])
+			self.assertEqual('tag:nextthought.com,2011-10:AOPS-HTML-prealgebra.0', item['ContainerId'])
 
 
 	def test_delete(self):
 		cm, idx = self._create_index()
-		cm.delete_content(idx.writer(), '0x62:5573657273')
+		cm.delete_content(idx.writer(), 'tag:nextthought.com,2011-10:carlos.sanchez@nextthought.com-OID-0x085a:5573657273')
 		with idx.searcher() as s:
 			self.assertEqual(s.doc_count(), 0)
 
