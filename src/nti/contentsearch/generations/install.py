9aee8e4398e8d7b999be373978aaa4e5012e3f57
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-26 20:46:35 +0000

    add search database (repoze.catalog) configuration
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@6109 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentsearch/generations/install.py
@@ -0,0 +1,34 @@
+from __future__ import print_function, unicode_literals
+
+__docformat__ = 'restructuredtext'
+
+generation = 1
+
+from zope.generations.generations import SchemaManager
+
+from nti.contentsearch import create_repoze_datastore
+from nti.contentsearch.interfaces import IRepozeDataStore
+
+class _ContentSearchSchemaManager(SchemaManager):
+	"A schema manager that we can register as a utility in ZCML."
+	def __init__( self ):
+		super( _ContentSearchSchemaManager, self ).__init__(generation=generation,
+														 	minimum_generation=generation,
+														 	package_name='nti.contentsearch.generations')
+
+def evolve( context ):
+	install_search( context )
+
+def install_search( context ):
+	conn = context.connection
+	root = conn.root()
+	container = root['nti.dataserver']
+	lsm = container.getSiteManager()
+	
+	search_conn = conn.get_connection( 'Search' )
+	search_root = search_conn.root()
+	repoze_datastore = create_repoze_datastore()
+	search_conn.add(repoze_datastore)
+	search_root['repoze_datastore'] = repoze_datastore
+	lsm.registerUtility( repoze_datastore, provided=IRepozeDataStore )
+	
