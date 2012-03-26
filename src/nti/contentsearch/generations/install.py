7a73cce188f8298e1ebfd30a94c72c8e5d063042
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-26 22:58:14 +0000

    a bit of clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@6129 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/generations/install.py
+++ src/nti/contentsearch/generations/install.py
@@ -11,6 +11,9 @@ from nti.dataserver import interfaces as nti_interfaces
 from nti.contentsearch import create_repoze_datastore
 from nti.contentsearch.interfaces import IRepozeDataStore
 
+import logging
+logger = logging.getLogger( __name__ )
+
 class _ContentSearchSchemaManager(SchemaManager):
 	"A schema manager that we can register as a utility in ZCML."
 	def __init__( self ):
@@ -24,20 +27,16 @@ def evolve( context ):
 def install_search( context ):
 	conn = context.connection
 	root = conn.root()
+	
 	container = root['nti.dataserver']
 	lsm = container.getSiteManager()
 	
-	
-	search_conn = conn.get_connection( 'Search' )
-	search_root = search_conn.root()
 	repoze_datastore = create_repoze_datastore()
+	search_conn = conn.get_connection( 'Search' )
 	search_conn.add(repoze_datastore)
-	search_root['repoze_datastore'] = repoze_datastore
+	search_conn.root()['repoze_datastore'] = repoze_datastore
 	lsm.registerUtility( repoze_datastore, provided=IRepozeDataStore )
 	
-	# FIXME: I don't understand why this is necessary. Why does
-	# the PersistentOidResolver sometimes not have a _p_jar?
 	rsv = lsm.getUtility( nti_interfaces.IOIDResolver )
 	if rsv._p_jar is None:
 		rsv._p_jar = conn
-	
