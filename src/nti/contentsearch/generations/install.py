05e2832966cece01cc5612dd753a4481e3f2f416
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-29 16:58:57 +0000

    Evolve generation 1 to generation 2 by creating a docmap for every user in the repoze datastore
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@6255 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/generations/install.py
+++ src/nti/contentsearch/generations/install.py
@@ -2,7 +2,7 @@ from __future__ import print_function, unicode_literals
 
 __docformat__ = 'restructuredtext'
 
-generation = 1
+generation = 2
 
 from zope.generations.generations import SchemaManager
 
