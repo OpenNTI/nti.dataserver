38dded535041997290ed9329129be9e3f171fe1e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-04-10 22:25:36 +0000

    expand QueryObject for unified search. Support whoosh first (it's easier)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@6540 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/__init__.py
+++ src/nti/contentsearch/__init__.py
@@ -1,4 +1,6 @@
+import six
 import UserDict
+
 from brownie.caching import LFUCache
 
 import logging
@@ -42,40 +44,16 @@ class LFUMap(LFUCache):
 
 class QueryObject(object, UserDict.DictMixin):
 	
-	__properties__ = ('term', 'query', 'books', 'indexname', 'username', 'limit')
-	
+	__int_properties__ 	= ('limit', 'maxdist', 'prefix', 'surround', 'maxchars')
+	__properties__ 		= ('term', 'query', 'books', 'indexname') + __int_properties__
+
 	def __init__(self, *args, **kwargs):
 		term = kwargs.get('term', None) or kwargs.get('query', None)
-		assert term, 'must specify a query term'
-		self._data = dict(kwargs)
-		self._data['term'] = unicode(term)
-		
-	@property
-	def books(self):
-		indexname = self._data.get('indexname', None)
-		if indexname:
-			return [indexname]
-		else:
-			books = self._data.get('books', None) or  ['prealgebra']
-			return books
-	
-	@property
-	def term(self):
-		return self._data['term']
-	query=term
-
-	@property
-	def username(self):
-		return self._data.get('username', None)
-	
-	@property
-	def limit(self):
-		return self._data.get('limit', None)
-	
-	def params(self):
-		for k in self.keys(): 
-			if k not in self.__properties__:
-				yield (k, self.get(k))
+		assert term is not None, 'must specify a query term'
+		self._data = {}
+		for k,v in kwargs.items():
+			if v is not None and k is not None:
+				self.__setitem__(k, v)
 				
 	def keys(self):
 		return self._data.keys()
@@ -85,7 +63,126 @@ class QueryObject(object, UserDict.DictMixin):
 
 	def __repr__( self ):
 		return 'QueryObject(%s)' % self._data
+	
+	def __getitem__(self, key):
+		if key.lower() in self.__properties__:
+			key = key.lower()
+		return self._data[key]
+			
+	def __setitem__(self, key, val):
+		if key.lower() in self.__properties__:
+			key = key.lower()
+			if key in ('query', 'term'):
+				self.set_term(val)
+			elif key in self.__int_properties__:
+				self._data[key] = int(val) if val is not None else val
+			else:
+				self._data[key] = unicode(val) if isinstance(val, six.string_types) else val
+		else:
+			self._data[key] = unicode(val) if isinstance(val, six.string_types) else val
+		
+	# -- search -- 
+	
+	@property
+	def books(self):
+		books = self._data.get('books', [])
+		if not books:
+			indexname = self._data.get('indexname', None)
+			if indexname:
+				books = [indexname]	
+		return books
+	
+	@property
+	def username(self):
+		return self._data.get('username', None)
+	
+	def get_term(self):
+		return self._data['term']
 
+	def set_term(self, term):
+		self._data['term'] = unicode(term) if term is not None else term
+	
+	term = property(get_term, set_term)
+	query = term
+		
+	def get_limit(self):
+		return self._data.get('limit', None)
+	
+	def set_limit(self, limit=None):
+		self.__setitem__('limit', limit)
+	
+	limit = property(get_limit, set_limit)
+	
+	def get_query_properties(self):
+		result = []
+		for k in self.keys(): 
+			if k not in self.__properties__:
+				result.append( (k, self._data.get(k)) )
+		return result
+	
+	# -- suggest -- 
+	
+	def get_maxdist(self):
+		return self._data.get('maxdist', None)
+
+	def set_maxdist(self, maxdist):
+		self.__setitem__('maxdist', maxdist)
+		
+	maxdist = property(get_maxdist, set_maxdist)
+	
+	def get_prefix(self):
+		return self._data.get('prefix', None)
+
+	def set_prefix(self, prefix):
+		self.__setitem__('prefix', prefix)
+		
+	prefix = property(get_prefix, set_prefix)
+	
+	# -- highlight -- 
+	
+	def get_surround(self):
+		return self._data.get('surround', 20)
+
+	def set_surround(self, surround=20):
+		self.__setitem__('surround', surround)
+		
+	surround = property(get_surround, set_surround)
+	
+	def get_maxchars(self):
+		return self._data.get('maxchars', 300)
+	
+	def set_maxchars(self, maxchars=300):
+		self.__setitem__('maxchars', maxchars)
+		
+	maxchars = property(get_maxchars, set_maxchars)
+	
+	# ---------------
+	
+	@classmethod
+	def parse_query_properties(cls, **kwargs):
+		result = {}
+		for k in kwargs.items(): 
+			if k in cls.__properties__:
+				result[k] = kwargs.get(k)
+		return result
+	
+	@classmethod
+	def create(cls, query, **kwargs):
+		
+		if isinstance(query, six.string_types):
+			queryobject = QueryObject(term=query, **kwargs)
+		else:
+			queryobject = query
+			
+		assert isinstance(queryobject, QueryObject) 
+
+		for k in cls.__properties__: 
+			v = kwargs.get(k, None)
+			if v is not None:
+				queryobject[k] = v
+				
+		return queryobject
+	
 # -----------------------------
 
 def create_repoze_datastore(users_key='users', docMap_key='docMap'):
