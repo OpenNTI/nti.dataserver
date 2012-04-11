5368186aff2e651c83559083c96c06fdb2441e4e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-04-11 20:37:38 +0000

    start work to parse queries for repoze catalog. Create internal mapping for param field names to schema/catalog-key names
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@6558 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_whoosh_query.py
+++ src/nti/contentsearch/_whoosh_query.py
@@ -4,8 +4,8 @@ from whoosh.qparser import QueryParser
 from whoosh.qparser.dateparse import DateParserPlugin
 from whoosh.qparser import (GtLtPlugin, PrefixPlugin, WildcardPlugin)
 
-from nti.contentsearch.common import (	sharedWith_, containerId_, collectionId_, last_modified_)
-from nti.contentsearch.common import (	last_modified_fields)
+from nti.contentsearch.common import (sharedWith_, containerId_, collectionId_, last_modified_)
+from nti.contentsearch.common import (last_modified_fields)
 		
 import logging
 logger = logging.getLogger( __name__ )
@@ -13,7 +13,6 @@ logger = logging.getLogger( __name__ )
 # ----------------------------------
 
 default_search_plugins =  (GtLtPlugin, DateParserPlugin, PrefixPlugin, WildcardPlugin)
-_lower_last_modified_fields = [n.lower() for n in last_modified_fields]
 
 # ----------------------------------
 
@@ -23,28 +22,21 @@ def create_query_parser(fieldname='foo', schema=None, plugins=default_search_plu
 		qparser.add_plugin(pg())
 	return qparser
 
-def map_to_schema_name(name, stored_names=()):
-	"""
-	return a field-name that matches the one schema
-	""" 
+# ----------------------------------
+
+_mappings = {sharedWith_.lower()   : sharedWith_, 
+			 containerId_.lower()  : containerId_,
+			 collectionId_.lower() : collectionId_}
+for n in last_modified_fields:
+	_mappings[n.lower()] = last_modified_
 	
+def map_to_schema_names(name, stored_names=()):
+	name = unicode(name) if name else u''
 	if not stored_names: return name
-	
-	name = name.lower() if name else u''
-	if name in _lower_last_modified_fields:
-		name = last_modified_
-	elif name == containerId_.lower():
-		name = containerId_
-	elif name == sharedWith_.lower():
-		name = sharedWith_
-	elif name == collectionId_.lower():
-		name = collectionId_
+	name = _mappings.get(name.lower(), name)
 	return name if name in stored_names else None
 	
-def get_subqueries(qo, stored_names=(), map_func=map_to_schema_name):
-	"""
-	return tuples (field-name/value) for the subqueries in the specified query object
-	"""
+def get_subqueries(qo, stored_names=(), map_func=map_to_schema_names):
 	result = []
 	for n, v in qo.get_subqueries():
 		n = map_func(n, stored_names)
@@ -64,7 +56,7 @@ def parse_or_term(name, value, qparser):
 		result = Term(name, value)
 	return result
 	
-def get_default_parsed_query(fieldname, qo, subqueries=None, stored_names=(), map_func=map_to_schema_name):
+def get_default_parsed_query(fieldname, qo, subqueries=None, stored_names=(), map_func=map_to_schema_names):
 	"""
 	return an 'And' expression for all the terms in the specified query object
 	"""
@@ -74,7 +66,7 @@ def get_default_parsed_query(fieldname, qo, subqueries=None, stored_names=(), ma
 	parsed_query = And(subqueries)
 	return parsed_query
 	
-def parse_query(fieldname, qo, schema_or_names, plugins=default_search_plugins, map_func=map_to_schema_name):
+def parse_query(fieldname, qo, schema_or_names, plugins=default_search_plugins, map_func=map_to_schema_names):
 	text = [qo.term]
 	schema = schema_or_names if isinstance(schema_or_names, fields.Schema) else None
 	stored_names = schema.stored_names() if schema else schema_or_names
