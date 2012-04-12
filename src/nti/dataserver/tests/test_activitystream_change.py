e833a7e02592aa0cc96524b28bb174ffedd1d29d
Jason Madden <jason.madden@nextthought.com>
2012-04-12 02:00:41 +0000

    Fix externalizing summaries of users, and make the change class more efficient to store.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@6566 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/dataserver/tests/test_activitystream_change.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2.7
+
+import unittest
+from hamcrest import assert_that, is_, not_none, has_entry
+from nti.dataserver.tests import has_attr, provides
+from nti.dataserver.tests.mock_dataserver import ConfiguringTestBase
+from nti.tests import verifiably_provides
+
+from zope.interface.verify import verifyObject
+from zope import component
+
+from nti.dataserver import datastructures
+from nti.dataserver.activitystream_change import Change
+from nti.dataserver.users import User
+
+class TestChange(ConfiguringTestBase):
+
+	def test_to_external(self):
+		user = User( 'jason.madden@nextthought.com' )
+
+		change = Change( Change.CIRCLED, user )
+		assert_that( change.object, is_( user ) )
+		assert_that( str(change), is_(repr(change)) )
+
+		# Non summary
+		ext_obj = datastructures.toExternalObject( change )
+		assert_that( ext_obj, has_entry( 'Item', not_none() ) )
+
+		# Summary
+		change.useSummaryExternalObject = True
+		ext_obj = datastructures.toExternalObject( change )
+		assert_that( ext_obj, has_entry( 'Item', not_none() ) )
