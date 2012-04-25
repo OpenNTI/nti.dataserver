263fce3e521ec5b3329768b3b8d946104f973461
Jason Madden <jason.madden@nextthought.com>
2012-04-25 15:27:12 +0000

    Same problem with _p_jar and IOIDResolver
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@6791 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/evolve10.py
+++ src/nti/dataserver/generations/evolve10.py
@@ -30,3 +30,9 @@ def evolve( context ):
 
 	lsm = conn.root()['nti.dataserver'].getSiteManager()
 	lsm.registerUtility( storage, provided=nti_interfaces.ISessionServiceStorage )
+
+	# FIXME: I don't understand why this is necessary. Why does
+	# the PersistentOidResolver sometimes not have a _p_jar?
+	rsv = lsm.getUtility( nti_interfaces.IOIDResolver )
+	if rsv._p_jar is None:
+		rsv._p_jar = conn
