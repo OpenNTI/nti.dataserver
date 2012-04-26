b1281a38800da08e198b67c5dec7f9cdab8a74eb
Jason Madden <jason.madden@nextthought.com>
2012-04-26 15:10:10 +0000

    The delayed imports are no longer necessary, that dependency is broken.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@6810 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/_Dataserver.py
+++ src/nti/dataserver/_Dataserver.py
@@ -40,6 +40,9 @@ from nti.dataserver import users
 from nti.dataserver import interfaces
 
 from nti.chatserver import interfaces as chat_interfaces
+from nti.chatserver.chatserver import Chatserver
+from . import meeting_container_storage
+
 from . import config
 
 
@@ -400,9 +403,6 @@ class Dataserver(MinimalDataserver):
 		return sessions.SessionService()
 
 	def _setup_chat( self, room_name ):
-		# Delayed imports due to cycles
-		from nti.chatserver.chatserver import Chatserver
-		import meeting_container_storage
 		return  Chatserver( self.session_manager,
 							 meeting_storage=_SessionDbMeetingStorage( room_name ),
 							 meeting_container_storage=meeting_container_storage.MeetingContainerStorage( ) )
