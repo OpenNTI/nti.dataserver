a2fe6c6e49aef4f4c54d19336a9e9f1365b7462d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-04-27 22:01:24 +0000

    add  bit more work for cloudsearch impl
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@6875 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_cloudsearch_index.py
+++ src/nti/contentsearch/_cloudsearch_index.py
@@ -3,9 +3,19 @@ import socket
 
 from nti.contentsearch.exfm.cloudsearch import connect_cloudsearch
 
+from nti.contentsearch.common import ngrams
+from nti.contentsearch.common import get_type_name
+from nti.contentsearch.common import get_note_content
+from nti.contentsearch.common import normalize_type_name
+from nti.contentsearch.common import get_highlight_content
+from nti.contentsearch.common import get_messageinfo_content
+
+from nti.contentsearch.common import (	CLASS, CREATOR, ID, OID, last_modified_fields, ntiid_fields, 
+										container_id_fields )
+
 from nti.contentsearch.common import (	ngrams_, channel_, content_, keywords_, references_, username_,
-										last_modified_, ntiid_, recipients_, sharedWith_, id_,
-										oid_, creator_, containerId_, )
+										last_modified_, ntiid_, recipients_, sharedWith_, id_, type_,
+										oid_, creator_, containerId_, note_, messageinfo, highlight_)
 
 import logging
 logger = logging.getLogger( __name__ )
@@ -15,23 +25,45 @@ logger = logging.getLogger( __name__ )
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', 'AKIAJ42UUP2EUMCMCZIQ')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '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')
 		
-def create_domain(domain_name, allow_ips=(), access_key=AWS_ACCESS_KEY_ID, secret_key=AWS_SECRET_ACCESS_KEY):
+connect = connect_cloudsearch # alias
+
+def create_domain(connection, domain_name, allow_ips=()):
 	
-	conn = connect_cloudsearch(access_key, secret_key)
-	domain = conn.create_domain(domain_name) 
+	domain = connection.create_domain(domain_name) 
 	if not allow_ips:
 		allow_ips = socket.gethostbyname_ex(socket.gethostname())[2]
 	for ip in allow_ips:
 		domain.allow_ip(ip)
 		
-	# liternal fields
+	# following should be storable fields
+	# type, creator, oid, last modified, ntiid, containerId, content, id
+	
+	domain.create_index_field(type_, 'literal', searchable=True, result=True,
+							  source_attributes=(type_, CLASS))
+	
+	domain.create_index_field(creator_, 'literal', searchable=True, result=True, 
+							  source_attributes=(creator_, CREATOR))
+	
+	domain.create_index_field(last_modified_, 'uint', searchable=True, result=True,
+							  source_attributes=last_modified_fields )
+	
+	domain.create_index_field(ntiid_, 'literal', searchable=True, result=True, 
+							  source_attributes=ntiid_fields)
+	
+	domain.create_index_field(containerId_, 'literal', searchable=True, result=True,
+							  source_attributes=container_id_fields)
+	
+	domain.create_index_field(id_, 'literal', searchable=True, result=True, 
+							  source_attributes=(id_, ID))
+	
+	domain.create_index_field(oid_, 'literal', searchable=True, result=True,
+							  source_attributes=(oid_, OID))
+	
+	domain.create_index_field(content_, 'text', searchable=True, result=True)
+		
+	# literal fields
 	domain.create_index_field(username_, 'literal', searchable=False, result=False)
-	domain.create_index_field(creator_, 'literal', searchable=False, result=False)
-	domain.create_index_field(ntiid_, 'literal', searchable=True, result=True)
-	domain.create_index_field(oid_, 'literal', searchable=True, result=True)
-	domain.create_index_field(id_, 'literal', searchable=True, result=False)
 	domain.create_index_field(channel_, 'literal', searchable=True, result=False)
-	domain.create_index_field(containerId_, 'literal', searchable=True, result=False)
 	
 	# faceted fields
 	domain.create_index_field(keywords_, 'text', searchable=True, result=False, facet=True)
@@ -39,10 +71,27 @@ def create_domain(domain_name, allow_ips=(), access_key=AWS_ACCESS_KEY_ID, secre
 	domain.create_index_field(recipients_, 'text', searchable=True, result=False, facet=True)
 	domain.create_index_field(references_, 'text', searchable=True, result=False, facet=True)
 	
-	# uint fields
-	domain.create_index_field(last_modified_, 'uint', searchable=True, result=True)
-	
 	# content fields
 	domain.create_index_field(ngrams_, 'text', searchable=True, result=False)
-	domain.create_index_field(content_, 'text', searchable=True, result=True)
-	
+
+	return domain
+
+# -----------------------------------
+
+def get_object_content(data, type_name=None):
+	type_name = normalize_type_name(type_name or get_type_name(data))
+	if type_name == note_:
+		result = get_note_content(data)
+	elif type_name == highlight_:
+		result = get_highlight_content(data)
+	elif type_name == messageinfo:
+		result = get_messageinfo_content(data)
+	else:
+		result = u''
+	return result.lower() if result else None
+
+def get_object_ngrams(data, type_name=None):
+	content = get_object_content(data, type_name)
+	result = ngrams(content)
+	return result
+
