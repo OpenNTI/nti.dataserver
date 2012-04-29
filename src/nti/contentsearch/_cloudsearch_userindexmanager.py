91f794cd150e633076e4ecd2a21d89d7d0688744
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-04-29 20:57:35 +0000

    clean code up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@6885 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_cloudsearch_userindexmanager.py
+++ src/nti/contentsearch/_cloudsearch_userindexmanager.py
@@ -1,30 +1,28 @@
-import contextlib
+#import contextlib
 
 from zope import component
 from zope import interface
 
 from nti.dataserver import interfaces as nti_interfaces
 from nti.externalization.oids import toExternalOID
-from nti.externalization.oids import fromExternalOID
+#from nti.externalization.oids import fromExternalOID
 from nti.externalization.externalization import toExternalObject
 
 from nti.contentsearch.exfm.cloudsearch import get_document_service
 
 from nti.contentsearch import interfaces
-from nti.contentsearch import QueryObject
-from nti.contentsearch import SearchCallWrapper
-from nti.contentsearch.interfaces import IUserIndexManagerFactory
+#from nti.contentsearch import QueryObject
+#from nti.contentsearch import SearchCallWrapper
+#from nti.contentsearch.interfaces import IUserIndexManagerFactory
 from nti.contentsearch.common import get_type_name
 from nti.contentsearch.common import normalize_type_name
-from nti.contentsearch.common import empty_search_result
-from nti.contentsearch.common import empty_suggest_result
-from nti.contentsearch._repoze_query import parse_query
-from nti.contentsearch._repoze_query import is_all_query
-from nti.contentsearch._repoze_index import create_catalog
-from nti.contentsearch._search_external import get_search_hit
-from nti.contentsearch.textindexng3 import CatalogTextIndexNG3
-from nti.contentsearch.common import (WORD_HIGHLIGHT, NGRAM_HIGHLIGHT)
-from nti.contentsearch.common import (NTIID, LAST_MODIFIED, ITEMS, HIT_COUNT, SUGGESTIONS, content_, ngrams_)
+#from nti.contentsearch.common import empty_search_result
+#from nti.contentsearch.common import empty_suggest_result
+#from nti.contentsearch._repoze_query import parse_query
+#from nti.contentsearch._repoze_query import is_all_query
+#from nti.contentsearch._search_external import get_search_hit
+#from nti.contentsearch.common import (WORD_HIGHLIGHT, NGRAM_HIGHLIGHT)
+#from nti.contentsearch.common import (NTIID, LAST_MODIFIED, ITEMS, HIT_COUNT, SUGGESTIONS, content_, ngrams_)
 
 import logging
 logger = logging.getLogger( __name__ )
@@ -64,55 +62,16 @@ class CloudSearchUserIndexManager(object):
 		if not data: return None
 		type_name = normalize_type_name(type_name or get_type_name(data))
 		data = toExternalObject(data)
-		
-		with repoze_context_manager():
-			catalog = self._get_create_catalog(data, type_name)
-			if catalog and _oid:
-				docid = self.store.get_or_create_docid_for_address(self.username, _oid)
-				catalog.index_doc(docid, data)
-		return docid
+		pass
 
 	def update_content(self, data, type_name=None, *args, **kwargs):
 		if not data: return None
-		_oid = self._get_id(data)
-		if not _oid: return None
-		with repoze_context_manager():
-			docid = self.store.docid_for_address(self.username, _oid)
-			if docid:
-				catalog = self._get_create_catalog(data, type_name)
-				catalog.reindex_doc(docid, data)
-			else:
-				docid = self.index_content(data, type_name)
-		return docid
+		pass
 
 	def delete_content(self, data, type_name=None, *args, **kwargs):
 		if not data: return None
-		_oid = self._get_id(data)
-		if not _oid: return None
-		with repoze_context_manager():
-			docid = self.store.docid_for_address(self.username, _oid)
-			if docid:
-				catalog = self._get_create_catalog(data, type_name, create=False)
-				if catalog: catalog.unindex_doc(docid)
-				self.store.remove_docid(self.username, docid)
-		return docid
 
 	def remove_index(self, type_name):
-		with repoze_context_manager():
-			result = self.store.remove_catalog(self.username, type_name)
-			return result
+		pass
 
-	def docid_for_address(self, address):
-		with repoze_context_manager():
-			docid = self.store.docid_for_address(self.username, address)
-			return docid
-		
-	def get_stored_indices(self):
-		names = get_stored_indices(self.username)
-		return names
-	get_catalog_names = get_stored_indices
-			
-	def has_stored_indices(self):
-		result = has_stored_indices(self.username)
-		return result
 	
