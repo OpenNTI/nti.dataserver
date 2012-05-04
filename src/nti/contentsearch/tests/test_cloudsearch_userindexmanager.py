c2d11f72a155f2cf74853b12e87021a799b99aea
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-05-04 22:21:45 +0000

    complete test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@7091 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/tests/test_cloudsearch_userindexmanager.py
+++ src/nti/contentsearch/tests/test_cloudsearch_userindexmanager.py
@@ -29,10 +29,12 @@ from hamcrest import (is_, is_not, has_key, has_entry, has_length, assert_that)
 
 cloudsearch_index.compute_ngrams = True
 
+@unittest.SkipTest
 class TestCloudSearchIndexManager(ConfiguringTestBase):
 	
 	__name__ = "TestCloudSearchIndexManager"
 	
+	aws_op_delay = 3
 	aws_access_key_id = 'AKIAJ42UUP2EUMCMCZIQ'
 	aws_secret_access_key = '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'
 
@@ -41,8 +43,10 @@ class TestCloudSearchIndexManager(ConfiguringTestBase):
 		self.user_id = unicode(str(uuid.uuid1()) + '@nti.com')
 		self.store = create_cloudsearch_store(self.aws_access_key_id, self.aws_secret_access_key)
 		component.provideUtility(self.store, provides=ICloudSearchStore)
+		self.cim = CloudSearchUserIndexManager(self.user_id)
 
 	def tearDown(self):
+		self._remove_all(self.cim)
 		super(TestCloudSearchIndexManager, self).tearDown()
 		resetHooks()
 		
@@ -75,56 +79,88 @@ class TestCloudSearchIndexManager(ConfiguringTestBase):
 	def index_notes(self, dataserver=None, usr=None, conn=None, do_assert=True):
 		result = []
 		notes, usr = self.add_notes(usr=usr, conn=conn)
-		cim = CloudSearchUserIndexManager (usr.username)
 		for note in notes:
-			resp = cim.index_content(note)
+			resp = self.cim.index_content(note)
 			if do_assert: 
 				assert_that(resp, is_not(None))
 			result.append(resp)
-		return cim, notes, result
+		return notes, result
 	
 	def add_user_index_notes(self, ds=None):
 		usr = User( self.user_id, 'temp' )
 		ds = ds or mock_dataserver.current_mock_ds
 		ds.root['users'][self.user_id] = usr
-		cim, notes, resps = self.index_notes(dataserver=ds, usr=usr, do_assert=False)
-		return usr, cim, notes, resps
+		notes, resps = self.index_notes(dataserver=ds, usr=usr, do_assert=False)
+		time.sleep(self.aws_op_delay)
+		return usr, notes, resps
 	
 	# ---------------------
 	
-	@unittest.skip
 	@WithMockDSTrans
 	def test_query_notes(self):
-		_, cim, _, _ = self.add_user_index_notes()
-		time.sleep(3)
-		try:
-			hits = cim.search("shield")
-			assert_that(hits, has_entry(HIT_COUNT, 1))
-			assert_that(hits, has_entry(QUERY, 'shield'))
-			assert_that(hits, has_key(ITEMS))
-	
-			items = hits[ITEMS]
-			assert_that(items, has_length(1))
-	
-			key = list(items.keys())[0]
-			hit = toExternalObject(items[key])
-			assert_that(hit, has_entry(CLASS, HIT))
-			assert_that(hit, has_entry(NTIID, is_not(None)))
-			assert_that(hit, has_entry(TARGET_OID, is_not(None)))
-			assert_that(key, is_(hit[NTIID]))
-			assert_that(hit, has_entry(CONTAINER_ID, 'tag:nextthought.com,2011-10:bleach-manga'))
-			assert_that(hit, has_entry(SNIPPET, 'all waves rise now and become my SHIELD lightning strike now and become my blade'))
-	
-			hits = cim.search("*")
-			assert_that(hits, has_entry(HIT_COUNT, len(zanpakuto_commands)))
-	
-			hits = cim.search("?")
-			assert_that(hits, has_entry(HIT_COUNT, len(zanpakuto_commands)))
-	
-			hits = cim.search("ra*")
-			assert_that(hits, has_entry(HIT_COUNT, 3))
-		finally:
-			self._remove_all(cim)
+		self.add_user_index_notes()
+
+		hits = self.cim.search("shield")
+		assert_that(hits, has_entry(HIT_COUNT, 1))
+		assert_that(hits, has_entry(QUERY, 'shield'))
+		assert_that(hits, has_key(ITEMS))
+
+		items = hits[ITEMS]
+		assert_that(items, has_length(1))
+
+		key = list(items.keys())[0]
+		hit = toExternalObject(items[key])
+		assert_that(hit, has_entry(CLASS, HIT))
+		assert_that(hit, has_entry(NTIID, is_not(None)))
+		assert_that(hit, has_entry(TARGET_OID, is_not(None)))
+		assert_that(key, is_(hit[NTIID]))
+		assert_that(hit, has_entry(CONTAINER_ID, 'tag:nextthought.com,2011-10:bleach-manga'))
+		assert_that(hit, has_entry(SNIPPET, 'all waves rise now and become my SHIELD lightning strike now and become my blade'))
+
+		hits = self.cim.search("*")
+		assert_that(hits, has_entry(HIT_COUNT, len(zanpakuto_commands)))
+
+		hits = self.cim.search("?")
+		assert_that(hits, has_entry(HIT_COUNT, len(zanpakuto_commands)))
+
+		hits = self.cim.search("ra*")
+		assert_that(hits, has_entry(HIT_COUNT, 3))
+
+	@WithMockDSTrans
+	def test_update_note(self):
+		_, notes, _ = self.add_user_index_notes()
+		note = notes[5]
+		note.body = [u'Blow It Away']
+		self.cim.update_content(note)
+		
+		hits = self.cim.search("shield")
+		assert_that(hits, has_entry(HIT_COUNT, 0))
+		assert_that(hits, has_entry(QUERY, 'shield'))
+
+		hits = self.cim.search("blow")
+		assert_that(hits, has_entry(HIT_COUNT, 1))
+		assert_that(hits, has_entry(QUERY, 'blow'))
+
+	@WithMockDSTrans
+	def test_delete_note(self):
+		_, notes, _ = self.add_user_index_notes()
+		note = notes[5]
+		self.cim.delete_content(note)
+		
+		time.sleep(self.aws_op_delay) #time to delete
+		
+		hits = self.cim.search("shield")
+		assert_that(hits, has_entry(HIT_COUNT, 0))
+		assert_that(hits, has_entry(QUERY, 'shield'))
+
+	@WithMockDSTrans
+	def test_ngram_search(self):
+		self.add_user_index_notes()
+		hits = self.cim.ngram_search("sea")
+		assert_that(hits, has_entry(HIT_COUNT, 1))
+		assert_that(hits, has_entry(QUERY, 'sea'))
+		assert_that(hits, has_key(ITEMS))
+		assert_that(hits[ITEMS], has_length(1))
 		
 if __name__ == '__main__':
 	unittest.main()
