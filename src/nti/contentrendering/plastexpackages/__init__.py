4eb3deef619c4233c114b6065e681abddfcf1aba
Jason Madden <jason.madden@nextthought.com>
2012-05-09 20:37:56 +0000

    A step towards sanitizing and unifying the way content packages are handled. No longer need to replicate empty .sty files around.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@7212 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Package to hold modules corresponding to packages that can
+be used in latex documents.
+
+A best practice is to make these modules importable directly (as if this
+directory was on the PYTHONPATH). This allows the use of \usepackage{foo}
+rather than \usepackage{nti.contentrendering.plastexpackages.foo}.
+"""
