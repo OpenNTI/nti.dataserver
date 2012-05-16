5f38b64b2b0be6a24ae3b4370e6fda99fe097c0f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-05-16 20:32:28 +0000

    boto supports cloud search
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@7397 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_cloudsearch_store.py
+++ src/nti/contentsearch/_cloudsearch_store.py
@@ -1,9 +1,15 @@
+import six
 from zope import interface
 
 from persistent import Persistent
-from nti.contentsearch.interfaces import ICloudSearchStore
 
-from nti.contentsearch.exfm.cloudsearch import connect_cloudsearch
+from boto.cloudsearch import regions
+from boto.cloudsearch import connect_to_region
+from boto.cloudsearch.domain import Domain 
+from boto.cloudsearch.search import SearchConnection
+from boto.cloudsearch.document import DocumentServiceConnection
+
+from nti.contentsearch.interfaces import ICloudSearchStore
 
 import logging
 logger = logging.getLogger( __name__ )
@@ -20,24 +26,45 @@ AWS_CS_PARAMS = {'aws_access_key_id': (str, None),
 				 'proxy_user': (str, None),
 				 'proxy_pass': (str, None) }
 
+def find_aws_region(region):
+	if region and isinstance(region, six.string_types):
+		for r in regions():
+			if r.name == region:
+				region = r
+				break
+	# if no region grab first available
+	if not region:
+		region = regions()[0]
+		
+	return region
+	
+def get_search_service(domain=None, endpoint=None):
+	return SearchConnection(domain=domain, endpoint=endpoint)
+
+def get_document_service(domain=None, endpoint=None):
+	return DocumentServiceConnection(domain=domain, endpoint=endpoint)
+
 class CloudSearchStore(Persistent):
 	interface.implements(ICloudSearchStore)
 	
 	def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
 				 is_secure=True, host='cloudsearch.us-east-1.amazonaws.com', port=None, 
 				 proxy=None, proxy_port=None, proxy_user=None, proxy_pass=None,
-				 debug=0, https_connection_factory=None, path='/'):
+				 debug=0, https_connection_factory=None, path='/', region=None,
+				 api_version=None, security_token=None):
 		
 		super(CloudSearchStore, self).__init__()
 		kwargs = dict(locals())
 		kwargs.pop('self' , None)
-		self._v_connection = connect_cloudsearch(**kwargs)
-		self._set_aws_domains()
+		kwargs.pop('region' , None)
+		region = find_aws_region(region)
+		self._v_connection = connect_to_region(region.name, **kwargs)
+		self._set_aws_domains(self._v_connection)
 
-	def _set_aws_domains(self):
+	def _set_aws_domains(self, connection):
 		self._v_domains = {}
 		for d in self.get_aws_domains():
-			self._v_domains[d.domain_name] = d
+			self._v_domains[d['domain_name']] = Domain(connection, d)
 					
 	@property
 	def connection(self):
@@ -51,7 +78,7 @@ class CloudSearchStore(Persistent):
 		return self._v_connection
 		
 	def get_aws_domains(self):
-		domains = self.connection.get_domains()
+		domains = self.connection.describe_domains()
 		return domains
 		
 	def get_domain(self, domain_name):
