f8a144338d4e2b5d943e0007bbcb6975f20cea1e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-05-24 22:09:24 +0000

    add a script to export user objects to a json file. Rename script to delete user objects to nti_delete_user_objects
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@7578 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_delete_user_objects.py
+++ src/nti/dataserver/utils/nti_delete_user_objects.py
@@ -11,38 +11,24 @@ from collections import defaultdict
 from zope.generations.utility import findObjectsProviding
 
 from nti.dataserver import users
-from nti.dataserver.mimetype import MIME_BASE
 from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver import interfaces as nti_interfaces
 from nti.externalization.externalization import toExternalObject
-from nti.externalization.interfaces import StandardExternalFields
 from nti.contentsearch.utils.nti_remove_user_content import remove_user_content
 
 type_exceptions = (u'friendslist')
 
-def normalize_type_name(x, encode=True):
+def _normalize_type_name(x, encode=True):
 	result = ''
 	if x:
 		result =x[0:-1].lower() if x.endswith('s') else x.lower()
 	return unicode(result) if encode else result
 	
-def get_object_type(obj):
-	if not isinstance(obj, dict):
-		result = obj.__class__.__name__
-	elif StandardExternalFields.CLASS in obj:
-		result = obj[StandardExternalFields.CLASS]
-	elif StandardExternalFields.MIMETYPE in obj:
-		result = obj[StandardExternalFields.MIMETYPE]
-		if result and result.startswith(MIME_BASE):
-			idx = len(MIME_BASE) + 1
-			result = result[idx:]
-	else:
-		result = None
-	return normalize_type_name(result) if result else u''
-
+def _get_object_type(obj):
+	result = obj.__class__.__name__
+	return _normalize_type_name(result) if result else u''
 
 def remove_user_objects( username, object_types=(), export_dir=None ):
-	
 	user = users.User.get_user( username )
 	if not user:
 		print( "User '%s' does not exists" % username, file=sys.stderr )
@@ -54,33 +40,31 @@ def remove_user_objects( username, object_types=(), export_dir=None ):
 		if not os.path.exists(export_dir):
 			os.makedirs(export_dir)
 		
-	object_types = set(map(lambda x: normalize_type_name(x), object_types))
-	
+	object_types = set(map(lambda x: _normalize_type_name(x), object_types))
 	for obj in findObjectsProviding( user, nti_interfaces.IModeledContent):
-		external = toExternalObject(obj)
-		type_name = get_object_type(external)
-		if 	type_name and type_name not in type_exceptions and \
-			(not object_types or type_name in object_types):			
-
+		type_name = _get_object_type(obj)
+		if type_name not in type_exceptions and (not object_types or type_name in object_types):			
+			external = toExternalObject(obj)	
 			with user.updates():
 				_id = getattr(obj, 'id', None )		
 				containerId = getattr(obj, 'containerId', None )
 				if containerId and _id and user.deleteContainedObject( containerId, _id ):
-						captured_types[type_name].append(external)
+					captured_types[type_name].append(external)
 
 	if captured_types:
 		remove_user_content(username, captured_types.keys())
+		counter = 0
 		for name, objs in captured_types.items():
-			print("%s %s object(s) was/were removed for user '%s'" % (len(objs), name, username), file=sys.stderr )
+			counter = counter + len(objs)
 			if export_dir:
 				utc_datetime = datetime.datetime.utcnow()
 				s = utc_datetime.strftime("%Y-%m-%d-%H%M%SZ")
 				name = "%s-%s-%s.json" % (username, name, s)
 				outname = os.path.join(export_dir, name)
 				with open(outname, "w") as fp:
-					for obj in objs:
-						json.dump(obj, fp, indent=4)
-						fp.write("\n")
+					json.dump(objs, fp, indent=4)
+					
+		print("%s object(s) were removed from user '%s'" % (counter, username), file=sys.stderr)
 	else:
 		print( "No objects were removed for user '%s'" % username, file=sys.stderr)
 
