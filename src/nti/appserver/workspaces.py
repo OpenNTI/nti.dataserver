b584bb65d32bf8b2c86d15df7b2b23d517fbf222
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-06-13 01:39:59 +0000

    use  to_standard_external_dictionary
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@8096 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/workspaces.py
+++ src/nti/appserver/workspaces.py
@@ -12,7 +12,8 @@ from nti.dataserver import datastructures
 from nti.dataserver import interfaces as model_interfaces
 from nti.contentlibrary import interfaces as content_interfaces
 
-from nti.externalization.externalization import toExternalObject, toExternalDictionary, isSyntheticKey
+from nti.externalization.externalization import to_standard_external_dictionary
+from nti.externalization.externalization import toExternalObject, isSyntheticKey
 from nti.externalization.datastructures import LocatedExternalDict
 from nti.externalization.interfaces import StandardExternalFields
 import nti.externalization.interfaces as ext_interfaces
@@ -274,7 +275,7 @@ class ContainerCollectionDetailExternalizer(object):
 		# that as well
 		summary_collection = toExternalObject( collection, name='summary' )
 		# Copy the basic attributes
-		ext_collection = toExternalDictionary( collection )
+		ext_collection = to_standard_external_dictionary( collection )
 		# Then add the summary info as top-level...
 		ext_collection.update( summary_collection )
 		# ... and nested
