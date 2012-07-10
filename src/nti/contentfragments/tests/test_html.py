d53d659f98499555916b51c8cdae766802e69452
Jason Madden <jason.madden@nextthought.com>
2012-07-10 15:46:53 +0000

    Specialized adapters to speed up conversion of HTML to plain text for contentsearch. ZCA rocks.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@8648 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentfragments/tests/test_html.py
+++ src/nti/contentfragments/tests/test_html.py
@@ -18,11 +18,12 @@ tearDownModule = nti.tests.module_teardown
 from nti.contentfragments import interfaces as frg_interfaces
 
 
-def _check_sanitized( inp, expect ):
+def _check_sanitized( inp, expect, expect_iface=frg_interfaces.IUnicodeContentFragment ):
 	was = frg_interfaces.IUnicodeContentFragment( inp )
 	__traceback_info__ = inp, type(inp), was, type(was)
 	assert_that( was, is_( expect.strip() ) )
-	assert_that( was, verifiably_provides( frg_interfaces.IUnicodeContentFragment ) )
+	assert_that( was, verifiably_provides( expect_iface ) )
+	return was
 
 def test_sanitize_html():
 	strings = plistlib.readPlist( os.path.join( os.path.dirname(__file__), 'contenttypes-notes-tosanitize.plist' ) )
@@ -31,11 +32,23 @@ def test_sanitize_html():
 		yield _check_sanitized, s[0], s[1]
 
 def test_sanitize_data_uri():
-	_check_sanitized( "<audio src='data:foobar' controls />",
-					  u'<html><body><audio controls="" src="data:foobar"></audio></body></html>')
+	_ = _check_sanitized( "<audio src='data:foobar' controls />",
+							 u'<html><body><audio controls="" src="data:foobar"></audio></body></html>')
 
 
 def test_normalize_html_text_to_par():
 	html = u'<html><body><p style=" text-align: left;"><span style="font-family: \'Helvetica\';  font-size: 12pt; color: black;">The pad replies to my note.</span></p>The server edits it.</body></html>'
 	exp =  u'<html><body><p style="text-align: left;"><span>The pad replies to my note.</span></p><p style="text-align: left;">The server edits it.</p></body></html>'
-	_check_sanitized( html, exp )
+	sanitized = _check_sanitized( html, exp, frg_interfaces.ISanitizedHTMLContentFragment )
+
+	plain_text = frg_interfaces.IPlainTextContentFragment( sanitized )
+	assert_that( plain_text, verifiably_provides( frg_interfaces.IPlainTextContentFragment ) )
+	assert_that( plain_text, is_( "The pad replies to my note.The server edits it." ) )
+
+
+def test_html_to_text():
+	exp =  frg_interfaces.HTMLContentFragment( '<html><body><p style="text-align: left;"><span>The pad replies to my note.</span></p><p style="text-align: left;">The server edits it.</p></body></html>' )
+
+	plain_text = frg_interfaces.IPlainTextContentFragment( exp )
+	assert_that( plain_text, verifiably_provides( frg_interfaces.IPlainTextContentFragment ) )
+	assert_that( plain_text, is_( "The pad replies to my note.The server edits it." ) )
