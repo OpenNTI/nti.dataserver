d5354f9530d82bad64689ee181313eca0c30832e
Sean Jones <sean.jones@nextthought.com>
2012-07-11 20:10:09 +0000

    Handle cases where \chapterauthor is a cousin of \chapterquote instead of a sibling.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@8710 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/transforms/transchapters.py
+++ src/nti/contentrendering/transforms/transchapters.py
@@ -10,8 +10,12 @@ def transform(document):
 	# Chapterauthor and chapterquote need to move down a level, inside
 	# their respective chapters.
 	for chapterquote in document.getElementsByTagName( 'chapterquote'):
-		chapterauthor = chapterquote.parentNode.getElementsByTagName( 'chapterauthor' )[0]
-		parent = chapterquote.parentNode
+		if len(chapterquote.parentNode.getElementsByTagName( 'chapterauthor' )):
+			chapterauthor = chapterquote.parentNode.getElementsByTagName( 'chapterauthor' )[0]
+			parent = chapterquote.parentNode
+		else:
+			chapterauthor = chapterquote.parentNode.parentNode.getElementsByTagName( 'chapterauthor' )[0]
+			parent = chapterquote.parentNode.parentNode
 		# move it up out of containing par and raggedbottom, etc
 		while parent:
 			if parent.nextSibling and parent.nextSibling.nodeName == 'chapter':
@@ -20,7 +24,7 @@ def transform(document):
 
 		if parent and parent.nextSibling:
 			logger.info( "Moving chaterquote/author (%s/%s) to %s", chapterquote, chapterauthor, parent )
-			chapterquote.parentNode.removeChild( chapterauthor )
+			chapterauthor.parentNode.removeChild( chapterauthor )
 			chapterquote.parentNode.removeChild( chapterquote )
 			parent.nextSibling.insert( 0, chapterquote )
 			parent.nextSibling.insert( 1, chapterauthor )
