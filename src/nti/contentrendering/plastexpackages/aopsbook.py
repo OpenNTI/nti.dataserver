fc8970a77917e47b117d871fbb682f73b34de3c0
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2012-07-16 22:31:31 +0000

    fix the renegade comma issues in the prealgebra content
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@8870 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/aopsbook.py
+++ src/nti/contentrendering/plastexpackages/aopsbook.py
@@ -686,7 +686,39 @@ class revprob(Base.subsection):
 		self.attributes['probnum'] = str(self.ownerDocument.context.counters['chapter'].value) + '.' + str(self.ownerDocument.context.counters['probnum'].value)
 		return res
 
-class chall(Base.subsection):
+class challProb(Base.subsection):
+	args = ''
+	
+	def invoke(self, tex ):
+		return super(challProb,self).invoke( tex )
+	
+	def digest(self, tokens):
+		super(challProb,self).digest( tokens )
+		""" Where we have two consecutive hints, we would like to remove the separating ',' (Fix renegade commas issue)
+			We have observed two patterns of this issue: <hint>','<hint> and <hint>',''~'<hint>
+			and in both cases, we want to omit the comma. 
+		"""
+		for child in self.childNodes:
+			hintsList = child.getElementsByTagName('hint')	
+			if hintsList != None and len(hintsList) > 1:		
+				for aHint in hintsList:					
+					try:
+						hindex = child.childNodes.index(aHint)
+						if hindex!= None and len(child.childNodes) > hindex + 2 :
+							# check for this pattern: <hint>,<hint>
+							if isinstance( child.childNodes[hindex+2], aHint.__class__ ) and child.childNodes[hindex+1] == ', ':
+								child.childNodes.pop(hindex+1)
+							# check for this pattern: <hint>,~ <hint>
+							elif len(child.childNodes) > hindex + 3 and \
+								isinstance( child.childNodes[hindex+3], aHint.__class__ ) and  \
+								child.childNodes[hindex+1] == ',' and \
+								child.childNodes[hindex+2].source == '~ ':
+								child.childNodes.pop(hindex+1)
+														
+					except ValueError:
+						pass 
+
+class chall(challProb):
 	args = ''
 	counter = 'probnum'
 	title = 'chall'
@@ -696,7 +728,7 @@ class chall(Base.subsection):
 		self.attributes['probnum'] = str(self.ownerDocument.context.counters['chapter'].value) + '.' + str(self.ownerDocument.context.counters['probnum'].value)
 		return res
 
-class challhard(Base.subsection):
+class challhard(challProb):
 	args = ''
 	counter = 'probnum'
 	title = 'challhard'
