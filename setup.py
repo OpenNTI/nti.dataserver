dd85d655dd9f11671451e8389879b92077fc0297
Jason Madden <jason.madden@nextthought.com>
2012-07-17 19:50:05 +0000

    A script to create classes and sections.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@8932 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ entry_points = {
 		"nti_pubsub_device = nti.dataserver._PubSubDevice:main",
 		"nti_cache_avatars = nti.dataserver.utils.nti_cache_avatars:main",
 		"nti_create_user = nti.dataserver.utils.nti_create_user:main",
+		"nti_create_class = nti.dataserver.utils.nti_create_class:main",
 		"nti_remove_user = nti.dataserver.utils.nti_remove_user:main",
 		"nti_delete_user_objects = nti.dataserver.utils.nti_delete_user_objects:main",
 		"nti_export_user_objects = nti.dataserver.utils.nti_export_user_objects:main",
---
+++ src/nti/dataserver/utils/nti_create_class.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+$Id$
+"""
+from __future__ import print_function, unicode_literals
+
+import sys
+
+from nti.dataserver import users
+from nti.dataserver import classes
+from nti.dataserver import providers
+from . import run_with_dataserver
+
+import argparse
+
+_type_map = { 'user': users.User.create_user,
+			  'provider': providers.Provider.create_provider }
+
+def main():
+	arg_parser = argparse.ArgumentParser( description="Create a class with a section" )
+	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
+	arg_parser.add_argument( 'provider', help="The provider of the class" )
+	arg_parser.add_argument( 'id', help="The id of the class" )
+	arg_parser.add_argument( '--name', dest='name', help="The name of the class" )
+	arg_parser.add_argument( '--section-id',
+							 required=True,
+							 help="The ID of the section",
+							 dest="section_id" )
+	arg_parser.add_argument( '-i', '--instructor',
+							 dest='instructors',
+							 nargs="+",
+							 required='true',
+							 help="The usernames of the instructors" )
+	arg_parser.add_argument( '-e', '--enrolled',
+							 dest='enrolled',
+							 nargs="+",
+							 help="The usernames of the students enrolled" )
+	args = arg_parser.parse_args()
+
+	env_dir = args.env_dir
+
+
+	run_with_dataserver( environment_dir=env_dir, function=lambda: _create_class(args) )
+
+
+def _create_class( args ):
+	provider = providers.Provider.get_entity( args.provider )
+	if not provider:
+		print( "No provider found", args, file=sys.stderr )
+		sys.exit( 2 )
+
+	_do_create_class( provider,
+					  args.id, args.name,
+					  args.section_id,
+					  args.instructors, args.enrolled )
+
+
+def _do_create_class(
+					  provider,
+					  class_id,
+					  class_name=None,
+					  section_id=None,
+					  section_instructors=(),
+					  usernames_to_enroll=()):
+	klass = provider.maybeCreateContainedObjectWithType(  'Classes', None )
+	klass.containerId = 'Classes'
+	klass.ID = class_id
+	klass.Description = class_name or class_id
+
+	if section_id:
+		section = classes.SectionInfo()
+		section.ID = section_id
+		section.creator = provider
+		klass.add_section( section )
+		section.InstructorInfo = classes.InstructorInfo()
+		for user in usernames_to_enroll:
+			section.enroll( user )
+		for ins in section_instructors:
+			section.InstructorInfo.Instructors.append( ins )
+
+		section.Provider = provider.username
+
+	provider.addContainedObject( klass )
+
+
+	return klass
