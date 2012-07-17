8f3c64f9d3acf00a200b8823f481efed4023c5d4
Jason Madden <jason.madden@nextthought.com>
2012-07-17 20:48:16 +0000

    Hack in a workaround to let ++fields++friends work for editing the friends of a friends list; the web app does it all this way or the other way right now instead of a mix. NOTE: There may be other fields they were depending on that are now broken as well.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@8935 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/appserver/_hacks.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+$Id$
+"""
+from __future__ import print_function, unicode_literals
+
+from .dataserver_pyramid_views import _UGDPutView as UGDPutView
+
+from pyramid.view import view_config
+
+
+from nti.dataserver import interfaces as nti_interfaces
+
+from nti.dataserver import authorization as nauth
+
+
+@view_config( route_name='objects.generic.traversal',
+			  renderer='rest',
+			  context=nti_interfaces.IFriendsList,
+			  name='++fields++friends',
+			  permission=nauth.ACT_UPDATE, request_method='PUT' )
+class _FriendsListsFriendsFieldUpdateView(UGDPutView):
+	"""
+	This is a temporary fast hack to enable updating friends list objects
+	with just friends using the new ++fields++ syntax until the unification
+	is complete.
+
+	This is done by specifically naming a view for the remainder of the path
+	after a friends list.
+	"""
+
+	def _transformInput( self, value ):
+		return value
+
+	def _get_object_to_update( self ):
+		return self.request.context
+
+	def _do_update_from_external_object( self, unit_prefs, externalValue, notify=True ):
+		externalValue = {"friends": externalValue}
+		return super(_FriendsListsFriendsFieldUpdateView,self)._do_update_from_external_object( unit_prefs, externalValue, notify=notify )
