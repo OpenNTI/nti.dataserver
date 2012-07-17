8be99524fb8b45f209ff907db0912f394c9b9f54
Sean Jones <sean.jones@nextthought.com>
2012-07-17 21:18:18 +0000

    Support files for CMU content.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@8941 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/cmumacros.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+from __future__ import print_function, unicode_literals
+
+import re
+
+from zope import interface
+
+from nti.contentrendering import plastexids
+from nti.contentrendering.resources import interfaces as res_interfaces
+
+from plasTeX import Base
+#from plasTex import Command
+#from plasTex import DimenCommand
+#from plasTex import Environment
+
+class lecture(Base.chapter):
+	args = '* [shorttitle] title { label }'
+
+# Parse the frame environment and \frametitle as if it was a \paragraph element 
+
+class frametitle(Base.paragraph):
+	pass
+
+# Parses the \frame environment and produces no output.  The childred of
+# this environment print as if they were not inside the frame environment.
+class frame(Base.Environment):
+        args = ' {title:str} [unknown:str]'
+
+        def invoke( self, tex ):
+                self.parse(tex)
+                return []
+
+# Parses the \columns environment and produces no output.  The childred of
+# this environment print as if they were not inside the solumns environment.
+class columns(Base.Environment):
+        args = '[ pos:str ]'
+
+        def invoke( self, tex ):
+                self.parse(tex)
+                return []
+
+class column(Base.Command):
+        args = '[ pos:str ] width:int'
+
+        def invoke( self, tex ):
+                self.parse(tex)
+                return []
+
+class mtiny(Base.Command):
+        pass
+
+class contentwidth(Base.DimenCommand):
+	value = Base.DimenCommand.new('600pt')
+
+class alt(Base.Command):
+	args = '< overlay > default alternative'
+
+#	def invoke( self, tex ):		
+#		_t = self.parse(tex)
+#		print("Default is: %s" % self.attributes['default'])
+#		print("Alternative is: %s" % self.attributes['alternative'])
+#		return []
