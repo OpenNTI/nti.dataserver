3a442ab9aaa862cf55ddabfcb1caf6ed8a44efb1
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2012-07-25 02:59:08 +0000

    code organisation. add a trasrightpics transform
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@9279 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/transforms/transchapters.py
+++ src/nti/contentrendering/transforms/transchapters.py
@@ -3,14 +3,14 @@ logger = logging.getLogger( __name__ )
 
 from zope import interface
 from . import interfaces
-interface.moduleProvides(interfaces.IDocumentTransformer)
+interface.moduleProvides( interfaces.IDocumentTransformer )
 
 
-def transform(document):
+def transform( document ):
 	# Chapterauthor and chapterquote need to move down a level, inside
 	# their respective chapters.
-	for chapterquote in document.getElementsByTagName( 'chapterquote'):
-		if len(chapterquote.parentNode.getElementsByTagName( 'chapterauthor' )):
+	for chapterquote in document.getElementsByTagName( 'chapterquote' ):
+		if len( chapterquote.parentNode.getElementsByTagName( 'chapterauthor' ) ):
 			chapterauthor = chapterquote.parentNode.getElementsByTagName( 'chapterauthor' )[0]
 			parent = chapterquote.parentNode
 		else:
@@ -28,17 +28,3 @@ def transform(document):
 			chapterquote.parentNode.removeChild( chapterquote )
 			parent.nextSibling.insert( 0, chapterquote )
 			parent.nextSibling.insert( 1, chapterauthor )
-			
-	for rightpic in document.getElementsByTagName( 'rightpic' ):
-		if rightpic.parentNode.parentNode.nodeName == 'exer' or rightpic.parentNode.parentNode.nodeName == 'revprob' or rightpic.parentNode.parentNode.nodeName == 'chall' or rightpic.parentNode.parentNode.nodeName == 'challhard':
-			rightpicContainer = rightpic.parentNode.parentNode
-			parentType = rightpic.parentNode.parentNode.nodeName
-			
-			#make sure that it's indeed the rightpic that we should move
-			if rightpicContainer.lastChild.firstChild == rightpic and rightpicContainer.nextSibling != None and rightpicContainer.nextSibling.nodeName == parentType :
-				#move it down a level
-				logger.info( "Moving rightpic %s of %s %s down a level to %s %s", rightpic, parentType, rightpicContainer, parentType, rightpicContainer.nextSibling )
-				#step 1: rm
-				rightpicContainer.lastChild.removeChild( rightpic )
-				#step2: add it to the next sibling as the first child
-				rightpicContainer.nextSibling.firstChild.insert( 0, rightpic )
