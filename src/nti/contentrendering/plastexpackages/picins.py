a7c507ed2f240cb2eef9fb2fb88f86bd573e286d
Sean Jones <sean.jones@nextthought.com>
2012-07-26 14:54:21 +0000

    Clean-up of the picins package.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@9340 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/picins.py
+++ src/nti/contentrendering/plastexpackages/picins.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python
 from __future__ import print_function, unicode_literals
 
-import re, sys
-
-from zope import interface
-
-from nti.contentrendering import plastexids
-from nti.contentrendering.resources import interfaces as res_interfaces
-
-from plasTeX import Base
-from plasTeX.Base.LaTeX import Index
-from plasTeX.Packages import graphicx
-from plasTeX.Base import Crossref
-from plasTeX.Base import Node
-
-###
-# FIXME: star imports!
-# If we don't, then we wind up with some rendering problems (at least the following):
-# - unable to traverse to 'url' on an includegraphics
-# - "No resource types for align* using default renderer <type 'unicode'>"
-###
-from plasTeX.Packages.fancybox import *
-from plasTeX.Packages.graphicx import *
-from plasTeX.Packages.amsmath import *
+from plasTeX import Command
 
 # SAJ: Partial support for the picins package.
 
-class picskip(Base.Command):
+class picskip(Command):
 	args = ' {text:int} '
 
 	def invoke( self, tex ):
@@ -36,10 +15,9 @@ class picskip(Base.Command):
 		return []
 
 	def digest( self, tokens ):
-		sys.stderr.write("Digesting: " + tokens + "\n" )
 		return super(picskip,self).digest( tokens )
 
-class parpic(Base.Command):
+class parpic(Command):
 	args = '( size:dimen ) ( offset:dimen ) [Options:str] [Position] {Picture}'
 
 	def invoke(self, tex):
