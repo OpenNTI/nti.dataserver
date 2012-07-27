b892aa2d3a2bb79f0696fcbeeecd74e4b9906ddd
Jason Madden <jason.madden@nextthought.com>
2012-07-27 01:38:47 +0000

    Update tests to the latest assessment_items from Milady. Test the multiple choice items grade correctly. Allow sending indexes of choices as either ints (preferred strongly) or the string version of the int; might reverse that later, it does expose an odd corner case.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@9393 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/test_application.py
+++ src/nti/appserver/tests/test_application.py
@@ -1170,7 +1170,7 @@ class TestApplicationAssessment(ApplicationTestBase):
 		assert_that( qmap,
 					 has_key( self.child_ntiid ) )
 		assert_that( qmap.by_file,
-					 has_key( os.path.join( os.path.dirname(__file__), 'ExLibrary', 'WithAssessment', 'tag_nextthought.com,2011-10_mathcounts-HTML-MN.2012.0.html' ) ) )
+					 has_key( os.path.join( os.path.dirname(__file__), 'ExLibrary', 'WithAssessment', 'tag_nextthought_com_2011-10_mathcounts-HTML-MN_2012_0.html' ) ) )
 
 
 
@@ -1192,7 +1192,7 @@ class TestApplicationAssessment(ApplicationTestBase):
 		testapp = TestApp( self.app )
 
 		for accept_type in ('application/json','application/vnd.nextthought.pageinfo','application/vnd.nextthought.pageinfo+json'):
-
+			__traceback_info__ = accept_type
 			res = testapp.get( '/dataserver2/NTIIDs/tag:nextthought.com,2011-10:mathcounts-HTML-MN.2012.0',
 							   headers={"Accept": accept_type},
 							   extra_environ=self._make_extra_environ() )
@@ -1242,3 +1242,33 @@ class TestApplicationAssessment(ApplicationTestBase):
 		data = json.serialize( ext_obj )
 		res = testapp.post( '/dataserver2/users/sjohnson@nextthought.com', data, extra_environ=self._make_extra_environ() )
 		self._check_submission( res )
+
+	def test_posting_multiple_choice(self):
+
+		with mock_dataserver.mock_db_trans(self.ds):
+			self._create_user()
+		testapp = TestApp( self.app )
+
+		# The correct answer is at index 1, and has the value 'Steam distillation". We should be able to submit all
+		# three forms
+		for submittedResponse in ( 1, "1", "Steam distillation", ):
+			sub = asm_submission.QuestionSubmission( questionId='tag:nextthought.com,2011-10:MN-NAQ-MiladyCosmetology.naq.21', parts=(submittedResponse,) )
+			ext_obj = toExternalObject( sub )
+
+			ext_obj['ContainerId'] = 'tag:nextthought.com,2011-10:mathcounts-HTML-MN.2012.0'
+			data = json.serialize( ext_obj )
+			res = testapp.post( '/dataserver2/users/sjohnson@nextthought.com', data, extra_environ=self._make_extra_environ() )
+			self._check_submission( res )
+			assert_that( res.json_body, has_entry( 'parts', has_item( has_entries( 'assessedValue', 1.0, 'submittedResponse', submittedResponse ) ) ) )
+
+		# The correct answer is at index 3, and has the value '1000 BC". We should be able to submit all
+		# three forms
+		for submittedResponse in ( 3, "3", "1000 BC", ):
+			sub = asm_submission.QuestionSubmission( questionId='tag:nextthought.com,2011-10:MN-NAQ-MiladyCosmetology.naq.10', parts=(submittedResponse,) )
+			ext_obj = toExternalObject( sub )
+
+			ext_obj['ContainerId'] = 'tag:nextthought.com,2011-10:mathcounts-HTML-MN.2012.0'
+			data = json.serialize( ext_obj )
+			res = testapp.post( '/dataserver2/users/sjohnson@nextthought.com', data, extra_environ=self._make_extra_environ() )
+			self._check_submission( res )
+			assert_that( res.json_body, has_entry( 'parts', has_item( has_entries( 'assessedValue', 1.0, 'submittedResponse', submittedResponse ) ) ) )
