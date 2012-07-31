d182f04845e78a1db5abdcfd96fa712d56118698
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-07-31 19:12:26 +0000

    use constants
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@9523 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/tests/test_search_results.py
+++ src/nti/contentsearch/tests/test_search_results.py
@@ -5,6 +5,8 @@ import collections
 
 from nti.contentsearch.tests import ConfiguringTestBase
 
+from nti.contentsearch.common import LAST_MODIFIED, QUERY, ITEMS, HIT_COUNT, SUGGESTIONS
+
 from nti.contentsearch._search_results import merge_search_results
 from nti.contentsearch._search_results import merge_suggest_results
 from nti.contentsearch._search_results import empty_search_result
@@ -23,51 +25,51 @@ class TestCommon(ConfiguringTestBase):
 		
 	def _check_empty(self, d, query):
 		assert_that(isinstance(d, collections.Mapping), is_(True))
-		assert_that(d, has_entry('Query', query))
-		assert_that(d, has_entry('Hit Count', 0))
-		assert_that(d, has_entry('Last Modified', 0))
+		assert_that(d, has_entry(QUERY, query))
+		assert_that(d, has_entry(HIT_COUNT, 0))
+		assert_that(d, has_entry(LAST_MODIFIED, 0))
 
 	def test_empty_search_result(self):
 		d = empty_search_result('myQuery')
 		self._check_empty(d, 'myQuery')
-		assert_that(d, has_entry('Items',{}))
+		assert_that(d, has_entry(ITEMS,{}))
 
 	def test_empty_suggest_result(self):
 		d = empty_suggest_result('myQuery')
 		self._check_empty(d, 'myQuery')
-		assert_that( d['Items'], has_length(0) )
+		assert_that( d[ITEMS], has_length(0) )
 
 	def test_empty_suggest_and_search_result(self):
 		d = empty_suggest_and_search_result('myQuery')
 		self._check_empty(d, 'myQuery')
-		assert_that(d, has_entry('Items',{}))
-		assert_that(d, has_entry('Suggestions', []))
+		assert_that(d, has_entry(ITEMS,{}))
+		assert_that(d, has_entry(SUGGESTIONS, []))
 
 	def test_merge_search_results(self):
-		a = {'Last Modified': 1, 'Items':{'a':1}}
-		b = {'Last Modified': 2, 'Items':{'b':2}}
+		a = {LAST_MODIFIED: 1, ITEMS:{'a':1}}
+		b = {LAST_MODIFIED: 2, ITEMS:{'b':2}}
 		m = merge_search_results(a, b)
 		
-		assert_that(m, has_entry('Last Modified', 2))
-		assert_that(m, has_entry('Items', {'a':1, 'b':2}))
-		assert_that(m, has_entry('Hit Count', 2))
+		assert_that(m, has_entry(LAST_MODIFIED, 2))
+		assert_that(m, has_entry(ITEMS, {'a':1, 'b':2}))
+		assert_that(m, has_entry(HIT_COUNT, 2))
 	
-		a = {'Last Modified': 1, 'Items':{'a':1}}
-		b = {'Items':{'b':2}}
+		a = {LAST_MODIFIED: 1, ITEMS:{'a':1}}
+		b = {ITEMS:{'b':2}}
 		m = merge_search_results(a, b)
-		assert_that(m, has_entry('Last Modified', 1))
+		assert_that(m, has_entry(LAST_MODIFIED, 1))
 
-		a = {'Items':{'a':1}}
-		b = {'Last Modified': 3, 'Items':{'b':2}}
+		a = {ITEMS:{'a':1}}
+		b = {LAST_MODIFIED: 3, ITEMS:{'b':2}}
 		m = merge_search_results(a, b)
-		assert_that(m, has_entry('Last Modified', 3))
+		assert_that(m, has_entry(LAST_MODIFIED, 3))
 
 		a = None
-		b = {'Last Modified': 3, 'Items':{'b':2}}
+		b = {LAST_MODIFIED: 3, ITEMS:{'b':2}}
 		m = merge_search_results(a, b)
 		assert_that(m, is_(b))
 
-		a = {'Last Modified': 3, 'Items':{'b':2}}
+		a = {LAST_MODIFIED: 3, ITEMS:{'b':2}}
 		b = None
 		m = merge_search_results(a, b)
 		assert_that(m, is_(a))
@@ -76,31 +78,31 @@ class TestCommon(ConfiguringTestBase):
 		assert_that(m, is_(None))
 
 	def test_merge_suggest_results(self):
-		a = {'Last Modified': 4, 'Items':['a']}
-		b = {'Last Modified': 2, 'Items':['b','c']}
+		a = {LAST_MODIFIED: 4, ITEMS:['a']}
+		b = {LAST_MODIFIED: 2, ITEMS:['b','c']}
 		m = merge_suggest_results(a, b)
 		
-		assert_that(m, has_entry('Last Modified', 4))
-		assert_that(m, has_entry('Items', ['a','c', 'b']))
-		assert_that(m, has_entry('Hit Count', 3))
+		assert_that(m, has_entry(LAST_MODIFIED, 4))
+		assert_that(m, has_entry(ITEMS, ['a','c', 'b']))
+		assert_that(m, has_entry(HIT_COUNT, 3))
 
-		a = {'Last Modified': 1, 'Items':['a']}
-		b = {'Items':['b']}
+		a = {LAST_MODIFIED: 1, ITEMS:['a']}
+		b = {ITEMS:['b']}
 		m = merge_suggest_results(a, b)
-		assert_that(m, has_entry('Last Modified', 1))
-		assert_that(m, has_entry('Items', ['a','b']))
+		assert_that(m, has_entry(LAST_MODIFIED, 1))
+		assert_that(m, has_entry(ITEMS, ['a','b']))
 
-		a = {'Items':['a']}
-		b = {'Last Modified': 3, 'Items':['b']}
+		a = {ITEMS:['a']}
+		b = {LAST_MODIFIED: 3, ITEMS:['b']}
 		m = merge_suggest_results(a, b)
-		self.assertEqual(3, m['Last Modified'])
+		self.assertEqual(3, m[LAST_MODIFIED])
 
 		a = None
-		b = {'Last Modified': 3, 'Items':['b']}
+		b = {LAST_MODIFIED: 3, ITEMS:['b']}
 		m = merge_suggest_results(a, b)
 		assert_that(m, is_(b))
 
-		a = {'Last Modified': 3, 'Items':['c']}
+		a = {LAST_MODIFIED: 3, ITEMS:['c']}
 		b = None
 		m = merge_suggest_results(a, b)
 		assert_that(m, is_(a))
