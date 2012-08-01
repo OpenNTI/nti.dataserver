0c57d0516867d29c5b510f4475aa3917785a1d05
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-01 23:01:49 +0000

    fix tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/branches/csadapter@9582 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/tests/test_whoosh_bookindexmanager.py
+++ src/nti/contentsearch/tests/test_whoosh_bookindexmanager.py
@@ -6,6 +6,9 @@ from datetime import datetime
 
 from nti.ntiids.ntiids import make_ntiid
 
+from nti.externalization.externalization import toExternalObject
+
+from nti.contentsearch import _whoosh_index
 from nti.contentsearch._whoosh_index import create_book_schema
 from nti.contentsearch._whoosh_indexstorage import create_directory_index
 from nti.contentsearch._whoosh_bookindexmanager import WhooshBookIndexManager
@@ -13,10 +16,13 @@ from nti.contentsearch._whoosh_bookindexmanager import WhooshBookIndexManager
 from nti.contentsearch.common import (HIT, CLASS, CONTAINER_ID, HIT_COUNT, QUERY, ITEMS, SNIPPET, NTIID)
 
 from nti.contentsearch.tests import zanpakuto_commands
+from nti.contentsearch.tests import ConfiguringTestBase
 
 from hamcrest import (assert_that, is_, has_key, has_entry, has_length, is_not, has_item)
 
-class TestWhooshBookIndexManager(unittest.TestCase):
+_whoosh_index.compute_ngrams = True
+
+class TestWhooshBookIndexManager(ConfiguringTestBase):
 			
 	@classmethod
 	def setUpClass(cls):
@@ -27,8 +33,8 @@ class TestWhooshBookIndexManager(unittest.TestCase):
 		
 		idx = cls.bim.bookidx
 		writer = idx.writer()
-		for x in zanpakuto_commands:
-			writer.add_document(ntiid = unicode(make_ntiid(nttype='bleach', specific='manga')),
+		for k, x in enumerate(zanpakuto_commands):
+			writer.add_document(ntiid = unicode(make_ntiid(provider=str(k), nttype='bleach', specific='manga')),
 								title = unicode(x),
 								content = unicode(x),
 								quick = unicode(x),
@@ -52,15 +58,18 @@ class TestWhooshBookIndexManager(unittest.TestCase):
 		assert_that(items, has_length(1))
 		
 		key = list(items.keys())[0]
-		assert_that(items[key], has_entry(CLASS, HIT))
-		assert_that(items[key], has_entry(NTIID, is_not(None)))
-		assert_that(key, is_(items[key][NTIID]))
-		assert_that(items[key], has_entry(CONTAINER_ID, 'tag:nextthought.com,2011-10:bleach-manga'))
-		assert_that(items[key], has_entry(SNIPPET, 'now and Become my SHIELD, Lightning, Strike'))
+		item = items[key]
+		assert_that(key, is_(item[NTIID]))
+		assert_that(item, has_entry(CLASS, HIT))
+		assert_that(item, has_entry(NTIID, is_not(None)))
+		assert_that(item, has_entry(CONTAINER_ID,  is_not(None)))
+		
+		item = toExternalObject(item)
+		assert_that(item, has_entry(SNIPPET, 'All Waves, Rise now and Become my SHIELD, Lightning, Strike now and Become my Blade'))
 		
 	def test_search_start(self):
 		hits = self.bim.search("ra*", limit=None)
-		assert_that(hits, has_entry(HIT_COUNT, 1))
+		assert_that(hits, has_entry(HIT_COUNT, 3))
 		assert_that(hits, has_entry(QUERY, 'ra*'))
 		assert_that(hits, has_key(ITEMS))
 		
