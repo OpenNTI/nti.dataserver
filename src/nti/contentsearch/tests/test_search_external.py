5ed418907f1e5980257138b7da5ea8836b8d6af0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-01 02:27:48 +0000

    add tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/branches/csadapter@9547 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/tests/test_search_external.py
+++ src/nti/contentsearch/tests/test_search_external.py
@@ -2,6 +2,14 @@ import os
 import json
 import unittest
 
+from nti.dataserver.users import User
+from nti.dataserver.contenttypes import Note
+from nti.dataserver.contenttypes import Highlight
+from nti.dataserver.contenttypes import Redaction
+
+from nti.ntiids.ntiids import make_ntiid
+
+from nti.externalization.oids import to_external_ntiid_oid
 from nti.externalization.externalization import toExternalObject
 
 from nti.contentsearch.common import get_type_name
@@ -12,8 +20,11 @@ from nti.contentsearch._search_external import _RedactionSearchHit
 from nti.contentsearch._search_external import _MessageInfoSearchHit
 from nti.contentsearch._search_external import _provide_highlight_snippet
 
+import nti.dataserver.tests.mock_dataserver as mock_dataserver
+from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
+
 from nti.contentsearch.tests import ConfiguringTestBase
-from nti.contentsearch.common import (NTIID, CREATOR, CONTAINER_ID, CLASS, HIT, SNIPPET, TARGET_OID)
+from nti.contentsearch.common import (NTIID, CREATOR, CONTAINER_ID, CLASS, TYPE, HIT, SNIPPET, TARGET_OID)
 
 from hamcrest import (assert_that, is_, is_not, has_entry)
 
@@ -29,10 +40,6 @@ class TestRepozeIndex(ConfiguringTestBase):
 		with open(path, "r") as f:
 			cls.note = json.load(f)
 			
-		path = os.path.join(os.path.dirname(__file__), 'note2.json')
-		with open(path, "r") as f:
-			cls.note2 = json.load(f)
-			
 		path = os.path.join(os.path.dirname(__file__), 'message_info.json')
 		with open(path, "r") as f:
 			cls.messageinfo = json.load(f)
@@ -44,6 +51,11 @@ class TestRepozeIndex(ConfiguringTestBase):
 	def _externalize(self, clazz, data, query):
 		d = _provide_highlight_snippet(clazz(data), query)
 		return toExternalObject(d)
+			
+	def test_get_type_name(self):
+		assert_that(get_type_name(self.note), is_('note'))
+		assert_that(get_type_name(self.hightlight), is_('highlight'))
+		assert_that(get_type_name(self.messageinfo), is_('messageinfo'))
 		
 	def test_get_search_hit(self):
 		hit = get_search_hit({})
@@ -86,10 +98,62 @@ class TestRepozeIndex(ConfiguringTestBase):
 		assert_that(d, has_entry(TARGET_OID, u'tag:nextthought.com,2011-10:zope.security.management.system_user-OID-0x8a:53657373696f6e73'))
 		assert_that(d, has_entry(SNIPPET, u'Zanpakuto and Zangetsu'))
 		
-	def test_get_type_name(self):
-		assert_that(get_type_name(self.note), is_('note'))
-		assert_that(get_type_name(self.hightlight), is_('highlight'))
-		assert_that(get_type_name(self.messageinfo), is_('messageinfo'))
+	@WithMockDSTrans
+	def test_search_hit_note_ds(self):
+		usr = User.create_user( mock_dataserver.current_mock_ds, username='nt@nti.com', password='temp' )
+		note = Note()
+		note.body = [u'It is not enough to mean well, we actually have to do well']
+		note.creator =  usr.username
+		note.containerId = make_ntiid(nttype='bleach', specific='manga')
+		mock_dataserver.current_transaction.add(note)
+		note = usr.addContainedObject( note ) 
+		oidstr = to_external_ntiid_oid(note)
+		d = self._externalize(_NoteSearchHit, note, 'well')
+		assert_that(d, has_entry(CLASS, HIT))
+		assert_that(d, has_entry(TYPE, 'Note'))
+		assert_that(d, has_entry(CONTAINER_ID, u'tag:nextthought.com,2011-10:bleach-manga'))
+		assert_that(d, has_entry(CREATOR, u'nt@nti.com'))
+		assert_that(d, has_entry(NTIID, oidstr))
+		assert_that(d, has_entry(TARGET_OID, oidstr))
+		assert_that(d, has_entry(SNIPPET, u'It is not enough to mean WELL we actually have to do WELL'))
+		
+	@WithMockDSTrans
+	def test_search_hit_hightlight_ds(self):
+		usr = User.create_user( mock_dataserver.current_mock_ds, username='nt@nti.com', password='temp' )
+		highlight = Highlight()
+		highlight.selectedText = u'Kon saw it! The Secret of a Beautiful Office Lady'
+		highlight.creator = usr.username
+		highlight.containerId =  make_ntiid(nttype='bleach', specific='manga')
+		highlight = usr.addContainedObject( highlight )
+		oidstr = to_external_ntiid_oid(highlight)
+		d = self._externalize(_HighlightSearchHit, highlight, 'secret')
+		assert_that(d, has_entry(CLASS, HIT))
+		assert_that(d, has_entry(TYPE, 'Highlight'))
+		assert_that(d, has_entry(CONTAINER_ID, u'tag:nextthought.com,2011-10:bleach-manga'))
+		assert_that(d, has_entry(CREATOR, u'nt@nti.com'))
+		assert_that(d, has_entry(NTIID, oidstr))
+		assert_that(d, has_entry(TARGET_OID, oidstr))
+		assert_that(d, has_entry(SNIPPET, u'Kon saw it The SECRET of a Beautiful Office Lady'))
+		
+	@WithMockDSTrans
+	def test_search_hit_redaction_ds(self):
+		usr = User.create_user( mock_dataserver.current_mock_ds, username='nt@nti.com', password='temp' )
+		redaction = Redaction()
+		redaction.selectedText = u'Fear'
+		redaction.replacementContent = 'redaction'
+		redaction.redactionExplanation = 'Have overcome it everytime I have been on the verge of death'
+		redaction.creator = usr.username
+		redaction.containerId =  make_ntiid(nttype='bleach', specific='manga')
+		redaction = usr.addContainedObject( redaction )
+		oidstr = to_external_ntiid_oid(redaction)
+		d = self._externalize(_RedactionSearchHit, redaction, 'death')
+		assert_that(d, has_entry(CLASS, HIT))
+		assert_that(d, has_entry(TYPE, 'Redaction'))
+		assert_that(d, has_entry(CONTAINER_ID, u'tag:nextthought.com,2011-10:bleach-manga'))
+		assert_that(d, has_entry(CREATOR, u'nt@nti.com'))
+		assert_that(d, has_entry(NTIID, oidstr))
+		assert_that(d, has_entry(TARGET_OID, oidstr))
+		assert_that(d, has_entry(SNIPPET, u'overcome it everytime I have been on the verge of DEATH Fear'))
 
 if __name__ == '__main__':
 	unittest.main()
