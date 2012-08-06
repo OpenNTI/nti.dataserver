20d33a6e25d61cc70ccdac88f11f19895d7ee236
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-06 23:23:06 +0000

    start adding user [repoz] index manager annotation adapter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@9780 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentsearch/_search_indexmanager.py
@@ -0,0 +1,59 @@
+from __future__ import print_function, unicode_literals
+
+import zope.intid
+from zope import component
+from zope import interface
+
+from persistent.mapping import PersistentMapping
+
+from nti.dataserver import interfaces as nti_interfaces
+
+from nti.contentsearch import interfaces
+
+import logging
+logger = logging.getLogger( __name__ )
+
+class _SearchUserIndexManager(PersistentMapping):
+	interface.implements(interfaces.IUserIndexManager)
+	
+	def get_uid(self, obj):
+		_ds_intid = component.getUtility( zope.intid.IIntIds )
+		return _ds_intid.getId(obj)
+	
+	def get_object(self, uid):
+		_ds_intid = component.getUtility( zope.intid.IIntIds )
+		return _ds_intid.getObject(uid)
+		
+	@property
+	def dataserver(self):
+		return component.getUtility( nti_interfaces.IDataserver )
+
+	def search(self, query, *args, **kwargs):
+		raise NotImplementedError()
+
+	def ngram_search(self, query, *args, **kwargs):
+		raise NotImplementedError()
+	quick_search = ngram_search
+
+	def suggest(self, query, *args, **kwargs):
+		raise NotImplementedError()
+
+	def suggest_and_search(self, query, limit=None, *args, **kwargs):
+		raise NotImplementedError()
+	
+	def index_content(self, data, type_name=None, **kwargs):
+		raise NotImplementedError()
+
+	def update_content(self, data, type_name=None, *args, **kwargs):
+		raise NotImplementedError()
+
+	def delete_content(self, data, type_name=None, *args, **kwargs):
+		raise NotImplementedError()
+
+	def remove_index(self, type_name):
+		raise NotImplementedError()
+		
+	def get_stored_indices(self):
+		raise NotImplementedError()
+			
+	
