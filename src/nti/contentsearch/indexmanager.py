e614f51f541714315b92cd456beedcfdbf8c15be
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-14 19:00:50 +0000

    remove unneeded code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@10065 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/indexmanager.py
+++ src/nti/contentsearch/indexmanager.py
@@ -1,23 +1,19 @@
 from __future__ import print_function, unicode_literals
 
 from nti.contentsearch._indexmanager import IndexManager
-from nti.contentsearch.interfaces import IRepozeEntityIndexManager
+from nti.contentsearch import interfaces  as search_interfaces
 from nti.contentsearch._whoosh_bookindexmanager import wbm_factory
-from nti.contentsearch._whoosh_userindexmanager import wuim_factory
-from nti.contentsearch._whoosh_indexstorage import UserDirectoryStorage
 
 import logging
 logger = logging.getLogger( __name__ )
 
 def create_index_manager_with_whoosh(indexdir=None, use_md5=True, max_users=100):
 	book_idx_manager = wbm_factory()
-	index_storage = UserDirectoryStorage(indexdir)
-	user_idx_manager = wuim_factory(index_storage, max_users=max_users, use_md5=use_md5)
-	return IndexManager(book_idx_manager, user_idx_manager)
+	return IndexManager(book_idx_manager, search_interfaces.IWhooshEntityIndexManager)
 
 def create_index_manager_with_repoze():
 	book_idx_manager = wbm_factory()
-	return IndexManager(book_idx_manager, IRepozeEntityIndexManager)
+	return IndexManager(book_idx_manager, search_interfaces.IRepozeEntityIndexManager)
 
 _DefaultIndexManager = create_index_manager_with_repoze()
 
