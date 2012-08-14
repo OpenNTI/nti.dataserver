0e64040aa4bd5b549e1bd6e2caa85e7a88c858f9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-14 13:53:16 +0000

    don't register twice the index manager.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@10050 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/indexmanager.py
+++ src/nti/contentsearch/indexmanager.py
@@ -1,7 +1,5 @@
 from __future__ import print_function, unicode_literals
 
-import os
-
 from nti.contentsearch._indexmanager import IndexManager
 from nti.contentsearch.interfaces import IRepozeEntityIndexManager
 from nti.contentsearch._whoosh_bookindexmanager import wbm_factory
@@ -23,22 +21,3 @@ def create_index_manager_with_repoze():
 
 _DefaultIndexManager = create_index_manager_with_repoze()
 
-def create_directory_index_manager(user_index_dir="/tmp",  use_md5=True, dataserver=None, *args, **kwargs):
-	"""
-	Create a directory based index manager"
-	
-	:param user_index_dir: location of user indices
-	:param use_md5: flag to md5 has the indices names
-	"""
-	
-	logger.info("Creating a directory based index manager '%s'", user_index_dir)
-	
-	if user_index_dir == '/tmp' and 'DATASERVER_DIR' in os.environ:
-		user_index_dir = os.environ['DATASERVER_DIR']
-		
-	storage = UserDirectoryStorage(user_index_dir)
-	im = create_index_manager_with_whoosh(storage, use_md5=use_md5, dataserver=dataserver)
-	return im
-
-create_index_manager = create_directory_index_manager
-create_repoze_index_manager = create_index_manager_with_repoze
