2db1c8b2fcd8edabb7adc1cbfae3096488b040a8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-15 01:32:31 +0000

    use factory instead of component
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@10091 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/indexmanager.py
+++ src/nti/contentsearch/indexmanager.py
@@ -11,9 +11,12 @@ def create_index_manager_with_whoosh(indexdir=None, use_md5=True, max_users=100)
 	book_idx_manager = wbm_factory()
 	return IndexManager(book_idx_manager, search_interfaces.IWhooshEntityIndexManager)
 
+def create_index_manager_with_cloudsearch():
+	book_idx_manager = wbm_factory()
+	return IndexManager(book_idx_manager, search_interfaces.ICloudSearchEntityIndexManager)
+
 def create_index_manager_with_repoze():
 	book_idx_manager = wbm_factory()
 	return IndexManager(book_idx_manager, search_interfaces.IRepozeEntityIndexManager)
 
-_DefaultIndexManager = create_index_manager_with_repoze()
 
