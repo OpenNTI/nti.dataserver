2e0c94ee43bcba5da2245681a4bb831707029903
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-15 02:47:08 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@10096 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/tests/test_cloudsearch_adapter.py
+++ src/nti/contentsearch/tests/test_cloudsearch_adapter.py
@@ -1,9 +1,7 @@
+import os
 import time
 import uuid
 import unittest
-
-from zope import component
-from zope.component.hooks import resetHooks
 
 from nti.dataserver.users import User
 from nti.dataserver.contenttypes import Note
@@ -11,9 +9,7 @@ from nti.externalization.externalization import toExternalObject
 
 from nti.ntiids.ntiids import make_ntiid
 
-from nti.contentsearch.interfaces import ICloudSearchStore
-from nti.contentsearch import _cloudsearch_index as cloudsearch_index
-#from nti.contentsearch._cloudsearch_userindexmanager import CloudSearchUserIndexManager
+from nti.contentsearch import interfaces as search_interfaces
 
 import nti.dataserver.tests.mock_dataserver as mock_dataserver
 from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
@@ -26,8 +22,6 @@ from nti.contentsearch.tests import ConfiguringTestBase
 
 from hamcrest import (is_, is_not, has_key, has_entry, has_length, assert_that)
 
-cloudsearch_index.compute_ngrams = True
-
 @unittest.SkipTest
 class TestCloudSearchIndexManager(ConfiguringTestBase):
 	
@@ -37,24 +31,10 @@ class TestCloudSearchIndexManager(ConfiguringTestBase):
 	aws_access_key_id = 'AKIAJ42UUP2EUMCMCZIQ'
 	aws_secret_access_key = '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'
 
-	def setUp(self):
-		super(TestCloudSearchIndexManager, self).setUp()
-		self.user_id = unicode(str(uuid.uuid1()) + '@nti.com')
-		#self.store = create_cloudsearch_store(self.aws_access_key_id, self.aws_secret_access_key)
-		component.provideUtility(self.store, provides=ICloudSearchStore)
-		#self.cim = CloudSearchUserIndexManager(self.user_id)
-
-	def tearDown(self):
-		self._remove_all(self.cim)
-		super(TestCloudSearchIndexManager, self).tearDown()
-		resetHooks()
-		
-	def _remove_all(self, cim):
-		try:
-			cim.remove_index()
-		except Exception, e:
-			print repr(e)
-			pass
+	@classmethod
+	def setUpClass(cls):
+		os.environ['aws_access_key_id']= cls.aws_access_key_id
+		os.environ['aws_secret_access_key']= cls.aws_secret_access_key
 		
 	# ---------------------
 	
@@ -65,31 +45,31 @@ class TestCloudSearchIndexManager(ConfiguringTestBase):
 		note.containerId = containerId or make_ntiid(nttype='bleach', specific='manga')
 		return note
 
-	def add_notes(self, usr=None, conn=None, messages=zanpakuto_commands):
+	def add_notes(self, usr, messages=zanpakuto_commands):
 		notes = []
-		conn = conn or mock_dataserver.current_transaction
-		usr = usr or User( self.user_id, 'temp' )
+		conn = mock_dataserver.current_transaction
 		for x in zanpakuto_commands:
 			note = self.create_note(x, usr.username)
 			if conn: conn.add(note)
-			notes.append(usr.addContainedObject( note ))
-		return notes, usr
+			note = usr.addContainedObject( note )
+			notes.append(note)
+		return notes
 
-	def index_notes(self, dataserver=None, usr=None, conn=None, do_assert=True):
+	def index_notes(self, usr, do_assert=True):
 		result = []
-		notes, usr = self.add_notes(usr=usr, conn=conn)
+		notes = self.add_notes(usr=usr)
+		cim = search_interfaces.ICloudSearchEntityIndexManager(usr)
 		for note in notes:
-			resp = self.cim.index_content(note)
+			resp = cim.index_content(note)
 			if do_assert: 
 				assert_that(resp, is_not(None))
 			result.append(resp)
 		return notes, result
 	
 	def add_user_index_notes(self, ds=None):
-		usr = User( self.user_id, 'temp' )
-		ds = ds or mock_dataserver.current_mock_ds
-		ds.root['users'][self.user_id] = usr
-		notes, resps = self.index_notes(dataserver=ds, usr=usr, do_assert=False)
+		username = unicode(str(uuid.uuid1())) + '@nti.com' 
+		usr = User.create_user(mock_dataserver.current_mock_ds, username=username, password='temp' )
+		notes, resps = self.index_notes(usr=usr, do_assert=False)
 		time.sleep(self.aws_op_delay)
 		return usr, notes, resps
 	
@@ -97,9 +77,10 @@ class TestCloudSearchIndexManager(ConfiguringTestBase):
 	
 	@WithMockDSTrans
 	def test_query_notes(self):
-		self.add_user_index_notes()
-
-		hits = self.cim.search("shield")
+		usr, _, _ = self.add_user_index_notes()
+		cim = search_interfaces.ICloudSearchEntityIndexManager(usr)
+		
+		hits = cim.search("shield")
 		assert_that(hits, has_entry(HIT_COUNT, 1))
 		assert_that(hits, has_entry(QUERY, 'shield'))
 		assert_that(hits, has_key(ITEMS))
@@ -116,48 +97,49 @@ class TestCloudSearchIndexManager(ConfiguringTestBase):
 		assert_that(hit, has_entry(CONTAINER_ID, 'tag:nextthought.com,2011-10:bleach-manga'))
 		assert_that(hit, has_entry(SNIPPET, 'all waves rise now and become my SHIELD lightning strike now and become my blade'))
 
-		hits = self.cim.search("*")
+		hits = cim.search("*")
 		assert_that(hits, has_entry(HIT_COUNT, len(zanpakuto_commands)))
 
-		hits = self.cim.search("?")
+		hits = cim.search("?")
 		assert_that(hits, has_entry(HIT_COUNT, len(zanpakuto_commands)))
 
-		hits = self.cim.search("ra*")
+		hits = cim.search("ra*")
 		assert_that(hits, has_entry(HIT_COUNT, 3))
 
 	@WithMockDSTrans
 	def test_update_note(self):
-		_, notes, _ = self.add_user_index_notes()
+		usr, notes, _ = self.add_user_index_notes()
+		cim = search_interfaces.ICloudSearchEntityIndexManager(usr)
+		
 		note = notes[5]
 		note.body = [u'Blow It Away']
-		self.cim.update_content(note)
-		
+		cim.update_content(note)
 		time.sleep(self.aws_op_delay)
 		
-		hits = self.cim.search("shield")
+		hits = cim.search("shield")
 		assert_that(hits, has_entry(HIT_COUNT, 0))
 		assert_that(hits, has_entry(QUERY, 'shield'))
 
-		hits = self.cim.search("blow")
+		hits = cim.search("blow")
 		assert_that(hits, has_entry(HIT_COUNT, 1))
 		assert_that(hits, has_entry(QUERY, 'blow'))
 
 	@WithMockDSTrans
 	def test_delete_note(self):
-		_, notes, _ = self.add_user_index_notes()
+		usr, notes, _ = self.add_user_index_notes()
+		cim = search_interfaces.ICloudSearchEntityIndexManager(usr)
 		note = notes[5]
-		self.cim.delete_content(note)
-		
+		cim.delete_content(note)
 		time.sleep(self.aws_op_delay)
-		
 		hits = self.cim.search("shield")
 		assert_that(hits, has_entry(HIT_COUNT, 0))
 		assert_that(hits, has_entry(QUERY, 'shield'))
 
 	@WithMockDSTrans
 	def test_ngram_search(self):
-		self.add_user_index_notes()
-		hits = self.cim.ngram_search("sea")
+		usr, _, _ = self.add_user_index_notes()
+		cim = search_interfaces.ICloudSearchEntityIndexManager(usr)
+		hits = cim.ngram_search("sea")
 		assert_that(hits, has_entry(HIT_COUNT, 1))
 		assert_that(hits, has_entry(QUERY, 'sea'))
 		assert_that(hits, has_key(ITEMS))
