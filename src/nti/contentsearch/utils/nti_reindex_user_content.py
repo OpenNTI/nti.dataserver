6f1c0ca32e112134e74d28865879f14d72d8588a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-16 21:04:58 +0000

    very much untested code to reindex the content of a user using a script
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@10190 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/utils/nti_reindex_user_content.py
+++ src/nti/contentsearch/utils/nti_reindex_user_content.py
@@ -5,6 +5,7 @@ from __future__ import print_function, unicode_literals
 import os
 import sys
 
+import zope.intid
 from zope import component
 from ZODB.POSException import POSKeyError
 from zope.generations.utility import findObjectsMatching
@@ -14,14 +15,80 @@ from nti.dataserver import users
 from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver import interfaces as nti_interfaces
 
-from nti.externalization.oids import toExternalOID
+from nti.dataserver.chat_transcripts import _MeetingTranscriptStorage as MTS
 
 import nti.contentsearch
 from nti.contentsearch.common import get_type_name
-from nti.contentsearch.interfaces import IRepozeDataStore
-from nti.contentsearch.common import indexable_type_names
-from nti.contentsearch._repoze_index import create_catalog
-from nti.dataserver.chat_transcripts import _MeetingTranscriptStorage as MTS
+from nti.contentsearch import get_indexable_types
+from nti.contentsearch import interfaces as search_interfaces
+
+def _get_uid(obj):
+	_ds_intid = component.getUtility( zope.intid.IIntIds )
+	result = _ds_intid.getId(obj)
+	return result
+
+def _get_sharedWith(obj):
+	# from IPython.core.debugger import Tracer;  Tracer()() ## DEBUG ##
+	adapted = component.getAdapter(obj, search_interfaces.IContentResolver)
+	if adapted and hasattr(adapted, "get_sharedWith"):
+		result = adapted.get_sharedWith()
+	else:
+		result = ()
+	return result
+
+def _get_indeaxable_objects(user, users, process_shared=True):
+	username = user.username
+	
+	from IPython.core.debugger import Tracer;  Tracer()() ## DEBUG ##
+	rim = search_interfaces.IRepozeEntityIndexManager(user, None)
+	for obj in findObjectsProviding( user, nti_interfaces.IModeledContent):
+		
+		# ignore friends lists
+		if nti_interfaces.IFriendsList.providedBy(obj):
+			continue
+		
+		yield (rim, obj)
+		
+		for uname in _get_sharedWith(obj):
+			sharing_user = users.get(uname, None)
+			if sharing_user and uname != username: 
+				srim = search_interfaces.IRepozeEntityIndexManager(sharing_user, None)
+				if srim is not None:
+					yield (srim, obj)
+		
+	for mts in findObjectsMatching( user, lambda x: isinstance(x,MTS) ):
+		for obj in mts.itervalues():
+			type_name = get_type_name(obj)
+			if type_name in get_indexable_types():
+				yield (rim, obj)			
+	
+def _index(rim, obj):
+	catalog = rim.get_create_catalog(obj)
+	if catalog:
+		docid = _get_uid(obj)
+		catalog.index_doc(docid, obj)
+		return True
+	return False
+			
+def _reindex_user_content( username ):
+	user = users.User.get_user( username )
+	if not user:
+		print( "user '%s' does not exists" % username, file=sys.stderr )
+		sys.exit( 2 )
+
+	# remove all catalogs
+	rim = search_interfaces.IRepozeEntityIndexManager(user)
+	for key in list(rim.keys()):
+		rim.pop(key, None)
+		
+	counter = 0
+	for rim, obj in _get_indeaxable_objects(user):
+		try:
+			_index(rim, obj)
+			counter = counter + 1
+		except POSKeyError:
+			# broken reference for object
+			pass
 
 def main():
 	if len(sys.argv) < 3:
@@ -33,51 +100,6 @@ def main():
 	run_with_dataserver( environment_dir=env_dir,
 						 xmlconfig_packages=(nti.contentsearch,),
 						 function=lambda: _reindex_user_content(username) )
-
-def indexable_objects(user, indexable_types=indexable_type_names):
-	for obj in findObjectsProviding( user, nti_interfaces.IModeledContent):
-		type_name = get_type_name(obj)
-		if type_name and type_name in indexable_types:
-			yield type_name, obj
-	
-	for mts in findObjectsMatching( user, lambda x: isinstance(x,MTS) ):
-		for obj in mts.itervalues():
-			type_name = get_type_name(obj)
-			if type_name and type_name in indexable_types:
-				yield type_name, obj
-			
-def _reindex_user_content( username ):
-	user = users.User.get_user( username )
-	if not user:
-		print( "user '%s' does not exists" % username, file=sys.stderr )
-		sys.exit( 2 )
-
-	# get and register rds
-	lsm = component.getSiteManager()
-	conn = getattr( lsm, '_p_jar', None )
-	search_conn = conn.get_connection( 'Search' )
-	rds = search_conn.root()['repoze_datastore']
-	lsm.registerUtility( rds, provided=IRepozeDataStore )
-
-	# remove user catalogs
-	rds.remove_user(username)
-
-	# recreate catalogs
-	for type_name in indexable_type_names:
-		catalog = create_catalog(type_name)
-		rds.add_catalog(username, catalog, type_name)
-
-	counter = 0
-	for type_name, obj in indexable_objects(user):
-		catalog = rds.get_catalog(username, type_name)
-		try:
-			address = toExternalOID(obj)
-			docid = rds.get_or_create_docid_for_address(username, address)
-			catalog.index_doc(docid, obj)
-			counter = counter + 1
-		except POSKeyError:
-			# broken reference for object
-			pass
 
 if __name__ == '__main__':
 	main()
