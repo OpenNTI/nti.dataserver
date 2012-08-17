1e41b8a091777d93623c71048938ed72cef24a07
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-17 04:16:20 +0000

    add back interface
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@10238 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/interfaces.py
+++ src/nti/contentsearch/interfaces.py
@@ -2,11 +2,22 @@ from __future__ import print_function, unicode_literals
 
 from zope import schema
 from zope import interface
+from zope.deprecation import deprecated
+from zope.interface.common.mapping import IFullMapping
 
 from dolmen.builtins import IDict
 
 from nti.externalization import interfaces as ext_interfaces
 
+@deprecated()
+class IRepozeDataStore(IFullMapping):
+	def has_user(username):
+		"""
+		return if the store has catalogs for the specified user
+		
+		:param user: username
+		"""
+		
 # searcher
 	
 class ISearcher(interface.Interface):
