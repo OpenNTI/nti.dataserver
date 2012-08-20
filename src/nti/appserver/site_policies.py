c5e64b7d344d50d32e4744f5331984640b0a032f
Jason Madden <jason.madden@nextthought.com>
2012-08-20 20:21:55 +0000

    The start of site-specific policies for user interfaces and communities. One for mathcounts. Plus a long monograph on how we might handle this
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@10369 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/site_policies.py
+++ src/nti/appserver/site_policies.py
@@ -45,10 +45,13 @@ def get_possible_site_names():
 		# TODO: The port splitting breaks on IPv6
 		# Origin comes in as a complete URL, host and potentially port
 		host = request.headers['origin'].split( '//' )[1].split( ":" )[0]
-		result.append( host )
+		result.append( host.lower() )
 	if request.host:
 		# Host is a plain name/IP address, and potentially port
-		result.append( request.host.split(':')[0] )
+		result.append( request.host.split(':')[0].lower() )
+
+	if 'localhost' in result:
+		result.remove( 'localhost' )
 	return result
 
 @component.adapter(lib_interfaces.IS3Key)
@@ -110,3 +113,87 @@ class RequestAwareUserPlacer(nti_shards.AbstractShardPlacer):
 
 		if not placed:
 			component.getUtility( nti_interfaces.INewUserPlacer, name='default' ).placeNewUser( user, users_directory, shards )
+
+####
+## Handling events within particular sites
+#
+# TODO: It's not clear what the best way is to handle this. We have a
+# few options.
+#
+# The simplest is that each site-aware handler re-delegates to a named
+# utility based on the site names. This works because it can be
+# statically configured in files that either ship with the code or
+# live on the site machine(s). However, it is limited in what it can
+# do since it's not really re-firing an event.
+#
+# A second option is to look for a ISiteManager matching the active
+# sites and really re-dispatch the event into those sites (either all
+# events, by adding to zope.event, or just object events by adding an
+# IObjectEvent subscriber; the former would require re-registering the
+# component object event dispatcher in the subsites?). Each shard we
+# have is an IPossibleSite and could have an ISiteManager added to it,
+# for example, when shards match site names. That preserves
+# flexibility, but has the disadvantage of requiring code to do
+# configuration changes. (In some ways, the ZMI would be very nice to
+# have...)
+#
+# A third, but similar option, is to have the shard or site objects more fully
+# configured, and to re-dispatch events based on the current site. Then
+# listeners for each event would be registered for (object, event_type, site_type)
+# and could examine the site object for details, such as additional interfaces
+# to apply or communities to join. This still requires those objects exist, and possibly be
+# registered somewhere and saved in the database, but avoids the confusing complexity of
+# working with multiple ISiteManagers that are not in a hierarchy.
+#
+# Initially, we are taking the simplest approach, and even going so far
+# as to put the actual policies in code (so a config change is a code release).
+####
+
+class ISitePolicyUserEventListener(interface.Interface):
+	"""
+	Register instances of these as utilities by the name of the site
+	they should apply to.
+	"""
+
+	def user_created( user, event ):
+		"""
+		Called when a user is created.
+		"""
+
+from zope.lifecycleevent.interfaces import IObjectCreatedEvent
+@component.adapter(nti_interfaces.IUser,IObjectCreatedEvent)
+def dispatch_user_created_to_site_policy( user, event ):
+	for site_name in get_possible_site_names():
+		utility = component.queryUtility( ISitePolicyUserEventListener, name=site_name )
+		if utility:
+			utility.user_created( user, event )
+			break
+
+from nti.dataserver import users
+
+@interface.implementer(ISitePolicyUserEventListener)
+class MathcountsSitePolicyEventListener(object):
+	"""
+	Implements the policy for the mathcounts site.
+	"""
+
+	def user_created( self, user, event ):
+		"""
+		This policy places newly created users in the ``MathCounts`` community
+		(creating it if it doesn't exist).
+
+		It also applies the :class:`nti.dataserver.interfaces.ICoppaUserWithoutAgreement` interface
+		to the object.
+
+		"""
+
+		community = users.Entity.get_entity( 'MathCounts' )
+		if community is None:
+			community = users.Community.create_community( username='MathCounts' )
+
+
+		user.join_community( community )
+		user.follow( community )
+
+		interface.alsoProvides( user, nti_interfaces.ICoppaUser )
+		interface.alsoProvides( user, nti_interfaces.ICoppaUserWithoutAgreement )
