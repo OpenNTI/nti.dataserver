7ece3c771e7e14229f5f6267cbc1e720d412dca3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-20 20:58:51 +0000

    first pass at a combined 'trivial' and word censor matching scanner
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@10373 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentfragments/tests/test_censor.py
+++ src/nti/contentfragments/tests/test_censor.py
@@ -15,10 +15,14 @@ from zope import component
 from zope import interface
 from zope.schema.fieldproperty import FieldPropertyStoredThroughField
 
+from pkg_resources import resource_filename
+
 import nti.contentfragments
 import nti.contentfragments.schema
 import nti.contentfragments.interfaces
 from nti.contentfragments.censor import WordMatchScanner
+from nti.contentfragments.censor import WordPlusTrivialMatchScanner
+from nti.contentfragments.censor import SimpleReplacementCensoredContentStrategy
 
 import nti.tests
 from hamcrest import is_
@@ -35,7 +39,6 @@ def test_defaults():
 	assert_that( strat.censor_ranges( bad_val, scanner.scan( bad_val ) ),
 				 is_( 'This is ******* stupid, you ************ *******' ) )
 
-
 def test_mord_match_scanner():
 	wm = WordMatchScanner(['lost','like'])
 	bad_val = """So I feel a little like, a child who's lost, a little like, (everything's changed) a
@@ -44,16 +47,26 @@ def test_mord_match_scanner():
 	ranges = list(wm.scan(bad_val))
 	assert_that(ranges, is_([(19, 23), (54, 58), (104, 108), (39, 43)]))
 	
+def test_trivial_and_word_match_scanner():
+	
+	profanity_file = resource_filename( __name__, '../profanity_list.txt' )
+	profanity_list = [x.encode('rot13').strip() for x in open(profanity_file, 'rU').readlines()]
+
+	strat = SimpleReplacementCensoredContentStrategy()	
+	scanner = WordPlusTrivialMatchScanner(('stupid',), profanity_list)
+
+	bad_val = 'Guvf vf shpxvat fghcvq, lbh ZbgureShpxre onfgneq'.encode( 'rot13' )
+	assert_that( strat.censor_ranges( bad_val, scanner.scan( bad_val ) ),
+				 is_( 'This is ******* ******, you ************ *******' ) )
+	
 def test_schema_event_censoring():
 
 	class ICensored(interface.Interface):
-
 		body = nti.contentfragments.schema.TextUnicodeContentFragment( title="Body" )
 
 	@interface.implementer(ICensored)
 	class Censored(object):
 		body = FieldPropertyStoredThroughField(ICensored['body'])
-
 
 	component.provideAdapter( nti.contentfragments.censor.DefaultCensoredContentPolicy,
 							  adapts=(unicode,ICensored),
@@ -62,7 +75,8 @@ def test_schema_event_censoring():
 
 	censored = Censored()
 
-	bad_val = nti.contentfragments.interfaces.UnicodeContentFragment( 'Guvf vf shpxvat fghcvq, lbh ZbgureShpxre onfgneq'.encode( 'rot13' ) )
+	bad_val = 'Guvf vf shpxvat fghcvq, lbh ZbgureShpxre onfgneq'.encode( 'rot13' )
+	bad_val = nti.contentfragments.interfaces.UnicodeContentFragment( bad_val )
 
 	censored.body = bad_val
 
