71f17fbbb98fd50caccc2433dbaa0e257465d90b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-22 21:07:36 +0000

    really fix test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@10492 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/tests/test_indexer.py
+++ src/nti/contentrendering/tests/test_indexer.py
@@ -23,7 +23,7 @@ class TestIndexer(unittest.TestCase):
 	def test_index_content(self):
 		toc = os.path.join(os.path.dirname(__file__), 'eclipse-toc-complete.xml')
 		index_content(toc, indexdir=self.idxdir, optimize=False, indexname='prealgebra')
-		idx = get_or_create_index(indexdir=self.idxdir, recreate=False)
+		idx = get_or_create_index(indexdir=self.idxdir, indexname='prealgebra', recreate=False)
 
 		q = Term("keywords", u"mathcounts")
 		with idx.searcher() as s:
