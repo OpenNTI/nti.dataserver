fb9d51a61786e0343d347075c92c5eceef13882b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-09-06 05:43:58 +0000

    for the time being disallow search on 'Everyone' community
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@11630 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_indexmanager.py
+++ src/nti/contentsearch/_indexmanager.py
@@ -91,7 +91,9 @@ class IndexManager(object):
 
 	def get_user_communities(self, username):
 		user = self.get_entity(username)
-		result = list(user.communities) if user and hasattr(user, 'communities') else ()
+		result = set(user.communities) if user and hasattr(user, 'communities') else ()
+		if result and 'Everyone' in result:
+			result.remove('Everyone')
 		return result
 
 	# -------------------
