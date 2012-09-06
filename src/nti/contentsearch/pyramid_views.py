1ef8b363d8c5262392559f76ce2c74f5bbd49808
Jason Madden <jason.madden@nextthought.com>
2012-09-06 00:54:25 +0000

    Apply the same techniques to content search results that are applied to usersearch results to avoid MIME and caching problems.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@11588 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/pyramid_views.py
+++ src/nti/contentsearch/pyramid_views.py
@@ -1,7 +1,22 @@
-from __future__ import print_function, unicode_literals
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+
+$Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
 
 from zope import component
+from zope import interface
+from zope.mimetype import interfaces as zmime_interfaces
+
 from pyramid.security import authenticated_userid
+
 
 from nti.ntiids.ntiids import is_valid_ntiid_string
 from nti.contentlibrary.interfaces import IContentPackageLibrary
@@ -15,7 +30,6 @@ import logging
 logger = logging.getLogger( __name__ )
 
 def get_collection(ntiid, default=None, registry=component):
-	# from IPython.core.debugger import Tracer;  Tracer()() ## DEBUG ##
 	result = default
 	if ntiid and is_valid_ntiid_string(ntiid):
 		_library = registry.queryUtility( IContentPackageLibrary )
@@ -25,9 +39,21 @@ def get_collection(ntiid, default=None, registry=component):
 	return unicode(result.lower()) if result else default
 
 def _locate(obj, parent, name=None):
+	# TODO: We really need to have a specific model object to represent results
+	# with proper IExternalObject and IContentTypeAware support. As it is,
+	# we wind up guessing MimeType and modification info (leading to the hack below)
+	# (Instead of modification info, we should be using etags here, anyway).
+	# cf nti.apserver.usersearch_views._format_result
 	obj = LocatedExternalDict( obj )
 	obj.__parent__ = parent
 	obj.__name__ = name
+	# Sadly, these are not properly cachable
+	from nti.appserver import interfaces as app_interfaces # Avoid circular imports
+	from nti.dataserver import mimetype as nti_mimetype
+
+	interface.alsoProvides( obj, app_interfaces.IUncacheableInResponse )
+	interface.alsoProvides( obj, zmime_interfaces.IContentTypeAware )
+	obj.mime_type = nti_mimetype.nti_mimetype_with_class( None )
 	return obj
 
 class Search(object):
@@ -62,7 +88,7 @@ class UserSearch(object):
 		indexmanager = self.request.registry.getUtility( IIndexManager )
 		return _locate( indexmanager.user_data_search( query=query, username=query.username ),
 						self.request.root, 'UserSearch' )
-		
+
 def clean_search_query(query):
 	if query in ('*', '?'):
 		return None
