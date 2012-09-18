453f492a76e2e4efbc6b7676c23df38246fa4145
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-09-18 01:05:14 +0000

    With the help of TS, we could save time by not externalizing the search found UGD object when returning search hits
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@12037 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_search_external.py
+++ src/nti/contentsearch/_search_external.py
@@ -5,12 +5,10 @@ import UserDict
 import zope.intid
 from zope import component
 from zope import interface
-from persistent.interfaces import IPersistent
 
 from nti.dataserver import interfaces as nti_interfaces
 from nti.chatserver import interfaces as chat_interfaces
 from nti.externalization import interfaces as ext_interfaces
-from nti.externalization.externalization import toExternalObject
 
 from nti.contentsearch import interfaces as search_interfaces
 
@@ -20,8 +18,8 @@ from nti.contentsearch._search_highlights import (word_content_highlight, ngram_
 from nti.contentsearch._search_highlights import (WORD_HIGHLIGHT, NGRAM_HIGHLIGHT, WHOOSH_HIGHLIGHT)
 
 from nti.contentsearch.common import (	NTIID, CREATOR, LAST_MODIFIED, CONTAINER_ID, CLASS, TYPE,
-										SNIPPET, HIT, ID, TARGET_OID, OID, CONTENT, INTID, QUERY,
-										HIT_COUNT, ITEMS)
+										SNIPPET, HIT, ID, TARGET_OID, CONTENT, INTID, QUERY,
+										HIT_COUNT, ITEMS, SUGGESTIONS)
 
 from nti.contentsearch.common import ( last_modified_, content_, title_, ntiid_)
 
@@ -40,24 +38,31 @@ def _ngram_content_highlight(query=None, text=None, default=None):
 	content = ngram_content_highlight(query, text) if query and text else u''
 	return unicode(content) if content else default
 
+@interface.implementer(ext_interfaces.IExternalObjectDecorator)
+class _BaseHighlightDecorator(object):
+	
+	@classmethod
+	def get_query_term(cls, original):
+		query = getattr(original, 'query', None)
+		if query and search_interfaces.ISearchQuery.providedBy(query):
+			query = query.term
+		return query
+	
+	@classmethod
+	def decorateExternalObject(cls, original, external):
+		pass
+	
+@component.adapter(search_interfaces.INoSnippetHighlight)
+class NoSnippetHighlightDecorator(_BaseHighlightDecorator):
+	pass
+	
 def NoSnippetHighlightDecoratorFactory(*args):
 	return NoSnippetHighlightDecorator()
-
-@interface.implementer(ext_interfaces.IExternalObjectDecorator)
-@component.adapter(search_interfaces.INoSnippetHighlight)
-class NoSnippetHighlightDecorator(object):
-
-	def decorateExternalObject(self, original, external):
-		pass
-					
-def NgramSnippetHighlightDecoratorFactory(*args):
-	return NgramSnippetHighlightDecorator()
-
-@interface.implementer(ext_interfaces.IExternalObjectDecorator)
-class _BaseNgramSnippetHighlightDecorator(object):
+				
+class _BaseNgramSnippetHighlightDecorator(_BaseHighlightDecorator):
 	
 	def decorateExternalObject(self, original, external):
-		query = getattr(original, 'query', None)
+		query = self.get_query_term(original)
 		if query:
 			text = external.get(SNIPPET, None)
 			text = _ngram_content_highlight(query, text.lower(), text)
@@ -66,18 +71,15 @@ class _BaseNgramSnippetHighlightDecorator(object):
 @component.adapter(search_interfaces.INgramSnippetHighlight)
 class NgramSnippetHighlightDecorator(_BaseNgramSnippetHighlightDecorator):
 	pass
-		
-def WhooshHighlightDecoratorFactory(*args):
-	return WhooshHighlightDecorator()
 	
-def WordSnippetHighlightDecoratorFactory(*args):
-	return WordSnippetHighlightDecorator()
-	
+def NgramSnippetHighlightDecoratorFactory(*args):
+	return NgramSnippetHighlightDecorator()
+
 @interface.implementer(ext_interfaces.IExternalObjectDecorator)
-class _BaseWordSnippetHighlightDecorator(object):
+class _BaseWordSnippetHighlightDecorator(_BaseHighlightDecorator):
 
 	def decorateExternalObject(self, original, external):
-		query = getattr(original, 'query', None)
+		query = self.get_query_term(original)
 		if query:
 			text = external.get(SNIPPET, None)
 			text = _word_content_highlight(query, text, text)
@@ -87,6 +89,9 @@ class _BaseWordSnippetHighlightDecorator(object):
 class WordSnippetHighlightDecorator(_BaseWordSnippetHighlightDecorator):
 	pass
 	
+def WordSnippetHighlightDecoratorFactory(*args):
+	return WordSnippetHighlightDecorator()
+
 @component.adapter(search_interfaces.IWhooshSnippetHighlight)
 class WhooshHighlightDecorator(_BaseWordSnippetHighlightDecorator):
 
@@ -97,6 +102,9 @@ class WhooshHighlightDecorator(_BaseWordSnippetHighlightDecorator):
 		#else:
 		super(WhooshHighlightDecorator, self).decorateExternalObject(original, external)
 
+def WhooshHighlightDecoratorFactory(*args):
+	return WhooshHighlightDecorator()
+
 # search results
 
 @interface.implementer(ext_interfaces.IExternalObject)
@@ -106,43 +114,53 @@ class _BaseSearchResultsExternalizer(object):
 		
 	@property
 	def query(self):
-		self.results.query
-		
+		return self.results.query
+	
+	@property
+	def highlight_type(self):
+		return getattr(self.results, 'highlight_type', None)
+			
 	def toExternalObject(self):
-		return {}
+		eo = {QUERY: self.query.term}
+		eo[HIT_COUNT] = len(self.results)
+		return eo
 	
 @component.adapter(search_interfaces.ISearchResults)
 class _SearchResultsExternalizer(_BaseSearchResultsExternalizer):
-	def toExternalObject(self):
-		eo  = {QUERY: self.query.term}
-		return eo
 	
-#result = {}
-#	result[QUERY] = query
-#	result[HIT_COUNT] = 0
-#	result[ITEMS] = [] if is_suggest else {}
-#	result[LAST_MODIFIED] = 0
-#	return result
+	@property
+	def hits(self):
+		return self.results.hits
+	
+	def toExternalObject(self):
+		eo = super(_SearchResultsExternalizer, self).toExternalObject()
+		return eo
 
 @component.adapter(search_interfaces.ISuggestResults)
 class _SuggestResultsExternalizer(_BaseSearchResultsExternalizer):
-	pass
 	
-@component.adapter(search_interfaces.ISuggestResults)
-class _SuggestAndSearchResultsExternalizer(_BaseSearchResultsExternalizer):
-	pass
-
+	@property
+	def suggestions(self):
+		return sorted(self.results.suggestions, key=lambda x: len(x), reverse=True)
+	
+	def toExternalObject(self):
+		eo = super(_SuggestResultsExternalizer, self).toExternalObject()
+		eo[ITEMS] = self.suggestions
+		eo[SUGGESTIONS] = eo[ITEMS]
+		eo[LAST_MODIFIED] = 0 
+		return eo
+	
+@component.adapter(search_interfaces.ISuggestAndSearchResults)
+class _SuggestAndSearchResultsExternalizer(_SearchResultsExternalizer, _SuggestResultsExternalizer):
+	
+	def toExternalObject(self):
+		eo = _SearchResultsExternalizer.toExternalObject(self)
+		eo[SUGGESTIONS] = self.get_suggestions()
+		return eo
 # search hits
 
 hit_search_external_fields  = (	CLASS, CREATOR, TARGET_OID, TYPE, LAST_MODIFIED, NTIID, \
 								CONTAINER_ID, SNIPPET, ID, INTID)
-	
-default_search_hit_mappings =  ((CLASS, TYPE), (OID, TARGET_OID), \
-								(last_modified_, LAST_MODIFIED), (content_, SNIPPET) )
-def get_content(obj):
-	adapted = component.queryAdapter(obj, search_interfaces.IContentResolver)
-	result = adapted.get_content() if adapted else u''
-	return result
 
 def get_uid(obj):
 	_ds_intid = component.getUtility( zope.intid.IIntIds )
@@ -150,50 +168,10 @@ def get_uid(obj):
 	return uid
 
 @interface.implementer(search_interfaces.ISearchHit)
-class _SearchHit(object, UserDict.DictMixin):
-	
-	def __init__( self, original ):
-		if IPersistent.providedBy(original):
-			self._data = toExternalObject(original)
-		else:
-			self._data = dict(original) if original else {}
-			
+class _BaseSearchHit(object, UserDict.DictMixin):
+	def __init__( self ):
+		self._data = {}
 		self._query = None
-		self._supplement(original, self._data)
-		self._reduce(self._data)
-		
-	@classmethod
-	def _supplement(cls, original, external, intid=None):
-		for k, r in default_search_hit_mappings:
-			if k in external:
-				external[r] = external[k]
-				
-		if SNIPPET not in external:
-			external[SNIPPET] = get_content(original)
-		
-		if intid is not None:
-			external[INTID] = intid
-			
-		external[CLASS] = HIT
-		external[NTIID] = external.get(NTIID, None) or external.get(TARGET_OID, None)
-		
-	@classmethod
-	def _reduce(cls, data):
-		for key in list(data.keys()):
-			if not key in hit_search_external_fields:
-				data.pop(key)
-	
-	def get_query(self):
-		return self._query if search_interfaces.ISearchQuery.providedBy(self._query) else self._query
-	
-	def set_query(self, query):
-		self._query = query
-		
-	query = property(get_query, set_query)
-	
-	@property
-	def last_modified(self):
-		return self._data.get(LAST_MODIFIED, 0)
 		
 	def toExternalObject(self):
 		return self._data
@@ -212,6 +190,31 @@ class _SearchHit(object, UserDict.DictMixin):
 		
 	def __repr__(self):
 		return "<%s %r>" % (self.__class__.__name__, self._data)
+	
+class _SearchHit(_BaseSearchHit):
+	def __init__( self, original ):
+		super(_SearchHit, self).__init__()
+		adapted = component.queryAdapter(original, search_interfaces.IContentResolver)
+		self._data[CLASS] = HIT
+		self._data[TYPE] = original.__class__.__name__
+		self._data[CREATOR] = adapted.get_creator() if adapted else u''
+		self._data[NTIID] = adapted.get_ntiid() if adapted else u''
+		self._data[SNIPPET] = adapted.get_content() if adapted else u''
+		self._data[TARGET_OID] = adapted.get_external_oid() if adapted else u''
+		self._data[CONTAINER_ID] = adapted.get_containerId() if adapted else u''
+		self._data[LAST_MODIFIED] = adapted.get_last_modified() if adapted else 0
+		
+	def get_query(self):
+		return self._query if search_interfaces.ISearchQuery.providedBy(self._query) else self._query
+	
+	def set_query(self, query):
+		self._query = query
+		
+	query = property(get_query, set_query)
+	
+	@property
+	def last_modified(self):
+		return self._data.get(LAST_MODIFIED, 0)
 		
 @component.adapter(nti_interfaces.IHighlight)
 class _HighlightSearchHit(_SearchHit):
@@ -227,28 +230,26 @@ class _NoteSearchHit(_SearchHit):
 	
 @component.adapter(chat_interfaces.IMessageInfo)
 class _MessageInfoSearchHit(_SearchHit):
-	pass
+	def __init__( self, original ):
+		super(_MessageInfoSearchHit, self).__init__(original)
+		adapted = component.queryAdapter(original, search_interfaces.IContentResolver)
+		self._data[ID] = adapted.get_id() if adapted else u''
 		
 @component.adapter(search_interfaces.IWhooshBookContent)
-class _WhooshBookSearchHit(_SearchHit):
+class _WhooshBookSearchHit(_BaseSearchHit):
 	
 	def __init__( self, hit ):
-		self._data = {}	
-		self.query = None
+		super(_WhooshBookSearchHit, self).__init__()
+		self._data[CLASS] = HIT	
+		self._data[TYPE] = CONTENT
+		self._data[NTIID] = hit[ntiid_]
+		self._data[SNIPPET] = hit[content_]
+		self._data[CONTAINER_ID] = hit[ntiid_]
+		self._data[title_.capitalize()] = hit[title_]
+		self._data[LAST_MODIFIED] = epoch_time(hit[last_modified_])
+		
+		# set whoosh highlight
 		self.whoosh_highlight = None
-		self._supplement(hit, self._data)
-	
-	def _supplement(self, hit, external, *args, **kwargs):
-		external[CLASS] = HIT	
-		external[TYPE] = CONTENT
-		external[NTIID] = hit[ntiid_]
-		external[SNIPPET] = hit[content_]
-		external[CONTAINER_ID] = hit[ntiid_]
-		external[title_.capitalize()] = hit[title_]
-		external[LAST_MODIFIED] = epoch_time(hit[last_modified_])
-		self.set_whoosh_highlight(hit, external)
-
-	def set_whoosh_highlight(self, hit, external):
 		search_field = getattr(hit, 'search_field', None)
 		if search_field:
 			try:
@@ -269,7 +270,7 @@ def _provide_highlight_snippet(hit, query=None, highlight_type=WORD_HIGHLIGHT):
 			interface.alsoProvides( hit, search_interfaces.INoSnippetHighlight )
 	return hit
 
-def get_search_hit(obj, query=None, highlight_type=WORD_HIGHLIGHT, *args, **kwargs):
+def get_search_hit(obj, query=None, highlight_type=WORD_HIGHLIGHT):
 	hit = component.queryAdapter( obj, ext_interfaces.IExternalObject, default=None, name='search-hit')
 	hit = hit or _SearchHit(obj)
 	hit = _provide_highlight_snippet(hit, query, highlight_type)
