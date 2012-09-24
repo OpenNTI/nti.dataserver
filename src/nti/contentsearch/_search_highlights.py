4c226c0e18dadcc49f41cc0c797c06d4cdb0b04f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-09-24 02:01:26 +0000

    change analyzer for word highlights
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@12353 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_search_highlights.py
+++ src/nti/contentsearch/_search_highlights.py
@@ -1,16 +1,37 @@
 from __future__ import print_function, unicode_literals
 
+import re
+
+from zope import component
+
 from whoosh import analysis
 from whoosh import highlight
 
 from nti.contentsearch._ngrams_utils import ngram_tokens
+from nti.contentsearch import interfaces as search_interfaces
 
 import logging
 logger = logging.getLogger( __name__ )
 
+default_analyzer = None
+default_pattern = "\w+(\.?\w+)*"
+
 WORD_HIGHLIGHT  = "WordHighlight"
 NGRAM_HIGHLIGHT = "NGRAMHighlight"
 WHOOSH_HIGHLIGHT = "WhooshHighlight"
+
+def get_terms(query, pattern=default_pattern):
+	pos = 0
+	terms = []
+	query = re.sub('[*?]','', query)
+	p = re.compile(default_pattern, re.I)
+	m = p.search(query, pos)
+	while m is not None:
+		pos = m.end()
+		term = query[m.start():pos]
+		terms.append(term)
+		m = p.search(query, pos)
+	return frozenset(terms)
 
 def _set_matched_filter(tokens, termset, text, multiple_match=True):
 	index = {} if multiple_match else None
@@ -45,7 +66,7 @@ def ngram_content_highlight(query, text, maxchars=300, surround=50, order=highli
 	text_lower = unicode(text.lower())
 	
 	query = unicode(query.lower())
-	termset = frozenset([query])
+	termset = get_terms(query)
 		
 	scorer = highlight.BasicFragmentScorer()
 	tokens = ngram_tokens(text_lower, unique=not multiple_match, minsize=minsize)
@@ -59,14 +80,23 @@ def ngram_content_highlight(query, text, maxchars=300, surround=50, order=highli
 	result = formatter(text, fragments)
 	return result
 
-def word_content_highlight(query, text, analyzer=None, maxchars=300, surround=50, *args, **kwargs):
+def get_default_analyzer():
+	global default_analyzer
+	if default_analyzer is None:
+		sw_util = component.queryUtility(search_interfaces.IStopWords) 
+		stoplist = sw_util.stopwords() if sw_util else analysis.STOP_WORDS
+		tokenizer = analysis.RegexTokenizer(expression=default_pattern, gaps=False)
+		lc_filter = analysis.LowercaseFilter()
+		stopword_filter = analysis.StopFilter(stoplist=stoplist)
+		default_analyzer = analysis.CompositeAnalyzer(tokenizer, lc_filter, stopword_filter)
+	return default_analyzer
+
+def word_content_highlight(query, text, analyzer=None, top=3, maxchars=300, surround=50, *args, **kwargs):
 	"""
 	whoosh highlight based on words
 	"""
-	terms = frozenset([query])
-	analyzer = analyzer or analysis.SimpleAnalyzer()
+	terms = get_terms(query)
+	analyzer = analyzer or get_default_analyzer()
 	fragmenter = highlight.ContextFragmenter(maxchars=maxchars, surround=surround)
 	formatter = highlight.NullFormatter() # highlight.UppercaseFormatter()
-	return highlight.highlight(text, terms, analyzer, fragmenter, formatter)
-
-
+	return highlight.highlight(text, terms, analyzer, fragmenter, formatter, top=top)
