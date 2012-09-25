6646a57c5c0e3d39b6c2e1161274b1fbe16a4d74
Jason Madden <jason.madden@nextthought.com>
2012-09-25 17:31:11 +0000

    If we are in an HTTP request that is supposed to be free of side effects, a GET request, just abort the transaction rather than try to commit it, avoiding any locking. All unit, gp and int tests pass; I can't think of any place we are creating and relying on transactional side effects in a GET request right now. This is probably not long term (we may ultimately want to do transactional bookeeping in GET) but for now it should eliminate many (not all) observable side effects of long commit lock times.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@12460 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/zope_site_tween.py
+++ src/nti/appserver/zope_site_tween.py
@@ -95,7 +95,16 @@ class site_tween(object):
 			# Confirmed and filed against 0.4. Probably still the case with 0.5, but our tests
 			# pass with or without these next two lines. There's no real harm leaving them in, other than
 			# that transaction.commit shows up as being called twice in profiles
-			if not transaction.isDoomed() and not pyramid_tm.default_commit_veto( request, response ):
+			if request.method == 'GET' and 'socket.io' not in request.url:
+				# GET requests must NEVER have side effects. (Unfortunately, socket.io polling does)
+				# So these transactions can safely be aborted and ignored, reducing contention on commit locks
+				# TODO: It would be cool to open them readonly.
+				# TODO: I don't really know if this is kosher.
+				now = time.time()
+				transaction.abort()
+				done = time.time() # TODO: replace all this with statsd
+				logger.debug( "Aborted side-effect free transaction for %s in %ss", request.url, done - now )
+			elif not transaction.isDoomed() and not pyramid_tm.default_commit_veto( request, response ):
 				exc_info = sys.exc_info()
 				try:
 					now = time.time()
