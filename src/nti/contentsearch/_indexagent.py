b5011c82ea600d03f4e2ef4c14cafda8c9e89747
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-09-26 00:52:16 +0000

    add code to only delete index entries by owner. Don't enable it yet
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@12502 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_indexagent.py
+++ src/nti/contentsearch/_indexagent.py
@@ -2,12 +2,16 @@
 
 from __future__ import print_function, unicode_literals
 
+import six
+
+from zope import component
 from ZODB import loglevels
 
 from nti.dataserver.activitystream_change import Change
 
 from nti.contentsearch import get_indexable_types
 from nti.contentsearch.common import normalize_type_name
+from nti.contentsearch import interfaces as search_interfaces
 
 import logging
 logger = logging.getLogger(__name__)
@@ -17,10 +21,24 @@ _event_types = { Change.CREATED: 'index_user_content',
 				 Change.MODIFIED:'update_user_content',
 				 Change.DELETED: 'delete_user_content' }
 
-def _process_event(indexmanager, target, change_type, data_type, data):
-	if 	change_type in _event_types and normalize_type_name(data_type) in get_indexable_types():
+_only_delete_by_owner = False
 
-		logger.log( loglevels.TRACE, 'Index event ("%s", "%s", "%s") received', target, change_type, data_type)
+def get_creator(obj):
+	adapted = component.getAdapter(obj, search_interfaces.IContentResolver)
+	return adapted.get_creator() if adapted else None
+							
+def _check_event(target, change_type, data_type, data):
+	result = change_type in _event_types and normalize_type_name(data_type) in get_indexable_types()
+	if result and _only_delete_by_owner and change_type==Change.DELETED:
+		username = target if isinstance( target, six.string_types ) else target.username
+		creator = get_creator(data)
+		result = username == creator
+	return result
+
+def _process_event(indexmanager, target, change_type, data_type, data):
+	if 	_check_event(target, change_type, data_type, data):
+
+		logger.log( loglevels.BLATHER, 'Index event ("%s", "%s", "%s") received', target, change_type, data_type)
 
 		func_name = _event_types.get( change_type )
 		func = getattr( indexmanager, func_name, None ) if func_name else None
