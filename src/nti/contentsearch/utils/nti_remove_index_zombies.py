5630f5e6c16e22ac6d5daf546650074ca24e5d90
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-09-26 04:41:38 +0000

    add verbose mode
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@12532 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/utils/nti_remove_index_zombies.py
+++ src/nti/contentsearch/utils/nti_remove_index_zombies.py
@@ -17,6 +17,7 @@ from nti.contentsearch import interfaces as search_interfaces
 def main():
 	arg_parser = argparse.ArgumentParser( description="Remove index zombies" )
 	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
+	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
 	arg_parser.add_argument( 'usernames',
 							 nargs="*",
 							 help="The username(s) to process" )
@@ -24,10 +25,10 @@ def main():
 
 	env_dir = args.env_dir
 	usernames = args.usernames
-	
+	verbose = args.verbose
 	run_with_dataserver( environment_dir=env_dir,
 						 xmlconfig_packages=(nti.contentsearch,),
-						 function=lambda: remove_zombies(usernames) )
+						 function=lambda: remove_zombies(usernames, verbose) )
 		
 
 def _get_object(uid):
@@ -37,7 +38,7 @@ def _get_object(uid):
 
 def _get_docids_from_catalog_field(catfield):
 	m = getattr(catfield, "_indexed", None)
-	result = catfield._indexed() if m is not None else ()
+	result = list(catfield._indexed()) if m is not None else ()
 	return result	
 		
 def _get_docids_from_rim(rim):
@@ -46,14 +47,14 @@ def _get_docids_from_rim(rim):
 			yield catalog, _get_docids_from_catalog_field(catfield)
 			break
 			
-def remove_zombies( usernames ):
+def remove_zombies( usernames, verbose=False):
 	
 	if not usernames:
 		dataserver = component.getUtility( nti_interfaces.IDataserver)
 		_users = nti_interfaces.IShardLayout( dataserver ).users_folder
 		usernames = _users.iterkeys()
 	
-	counter = 0
+
 	for username in usernames:
 		entity = users.Entity.get_entity( username )
 		if entity is None or nti_interfaces.IFriendsList.providedBy(entity):
@@ -61,12 +62,13 @@ def remove_zombies( usernames ):
 		
 		rim = search_interfaces.IRepozeEntityIndexManager(entity, None)
 		if rim is not None:
+			counter = 0
 			for catalog, docids in _get_docids_from_rim(rim):
 				for docid in docids:
 					if _get_object(docid) is None:
 						catalog.unindex_doc(docid)
 						counter = counter + 1
-	return counter
+			if verbose: print('%s object(s) unindexed for %s' % (counter, username))
 
 if __name__ == '__main__':
 	main()
