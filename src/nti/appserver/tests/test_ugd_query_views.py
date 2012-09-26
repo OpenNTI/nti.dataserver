98e5a7cb72e68d5b6a0fc39f8cd3e710760c76bc
Jason Madden <jason.madden@nextthought.com>
2012-09-26 17:47:08 +0000

    Implement, test, and document returning only certain types of objects from the UGD views.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@12553 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/test_ugd_query_views.py
+++ src/nti/appserver/tests/test_ugd_query_views.py
@@ -9,6 +9,7 @@ from hamcrest import none
 from hamcrest import has_entry
 from hamcrest import has_length
 from hamcrest import has_key
+from hamcrest import contains
 
 
 from nti.appserver.ugd_query_views import lists_and_dicts_to_ext_collection
@@ -278,3 +279,141 @@ def test_lists_and_dicts_to_collection():
 	o = O()
 	col1.append( o )
 	yield _check_items, (col1,col2), [o], 42
+
+from .test_application import ApplicationTestBase
+from nti.contentrange import contentrange
+from nti.dataserver import contenttypes
+from nti.dataserver import liking
+from nti.externalization.oids import to_external_ntiid_oid
+from webtest import TestApp
+
+class TestApplicationUGDQueryViews(ApplicationTestBase):
+
+	def test_sort_filter_page(self):
+		with mock_dataserver.mock_db_trans(self.ds):
+			user = self._create_user( )
+
+			top_n = contenttypes.Note()
+			top_n.applicableRange = contentrange.ContentRangeDescription()
+			top_n.containerId = 'tag:nti:foo'
+			top_n.body = ("Top",)
+			liking.like_object( top_n, 'foo@bar' )
+			user.addContainedObject( top_n )
+			top_n_id = top_n.id
+			top_n.lastModified = 1
+
+			reply_n = contenttypes.Note()
+			reply_n.applicableRange = contentrange.ContentRangeDescription()
+			reply_n.containerId = 'tag:nti:foo'
+			reply_n.body = ('Reply',)
+			reply_n.inReplyTo = top_n
+			reply_n.addReference(top_n)
+			user.addContainedObject( reply_n )
+			reply_n_id = reply_n.id
+			reply_n.lastModified = 2
+
+			top_n_ext_id = to_external_ntiid_oid( top_n )
+
+		testapp = TestApp( self.app )
+		path = '/dataserver2/users/sjohnson@nextthought.com/Pages(' + top_n.containerId + ')/UserGeneratedData'
+
+		res = testapp.get( path, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 2 ) ) )
+
+		res = testapp.get( path, params={'filter': 'TopLevel,MeOnly'}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 1 ) ) )
+		assert_that( res.json_body, has_entry( 'Items', contains( has_entry( 'ID', top_n_id ) ) ) )
+		assert_that( res.json_body,
+					 has_entry( 'Items',
+								contains(
+									has_entry( 'ReferencedByCount', 1 ) ) ) )
+
+		res = testapp.get( path, params={'sortOn': 'LikeCount'}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 2 ) ) )
+		# Descending by default
+		assert_that( res.json_body,
+					 has_entry( 'Items',
+								contains(
+									has_entry( 'ID', top_n_id ),
+									has_entry( 'ID', reply_n_id ) ) ) )
+
+		res = testapp.get( path, params={'sortOn': 'LikeCount', 'sortOrder': 'ascending'}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 2 ) ) )
+		assert_that( res.json_body,
+					 has_entry( 'Items',
+								contains(
+									has_entry( 'ID', reply_n_id ),
+									has_entry( 'ID', top_n_id ) ) ) )
+
+		res = testapp.get( path, params={'sortOn': 'ReferencedByCount'}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 2 ) ) )
+		# Descending by default
+		assert_that( res.json_body,
+					 has_entry( 'Items',
+								contains(
+									has_entry( 'ID', top_n_id ),
+									has_entry( 'ID', reply_n_id ) ) ) )
+		# And, if we asked for this info, we get data back about it
+		assert_that( res.json_body,
+					 has_entry( 'Items',
+								contains(
+									has_entry( 'ReferencedByCount', 1 ),
+									has_entry( 'ReferencedByCount', 0 ) ) ) )
+
+		res = testapp.get( path, params={'sortOn': 'lastModified', 'sortOrder': 'descending'}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 2 ) ) )
+		assert_that( res.json_body,
+					 has_entry( 'Items',
+								contains(
+									has_entry( 'ID', reply_n_id ),
+									has_entry( 'ID', top_n_id ) ) ) )
+
+		res = testapp.get( path, params={'batchSize': '1', 'batchStart': '0', 'sortOn': 'lastModified', 'sortOrder': 'ascending'}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 1 ) ) )
+		assert_that( res.json_body, has_entry( 'Items', contains( has_entry( 'ID', top_n_id ) ) ) )
+
+		res = testapp.get( path, params={'batchSize': '1', 'batchStart': '1', 'sortOn': 'lastModified', 'sortOrder': 'ascending'}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 1 ) ) )
+		assert_that( res.json_body, has_entry( 'Items', contains( has_entry( 'ID', reply_n_id ) ) ) )
+
+		res = testapp.get( path, params={'batchSize': '1', 'batchStart': '2'}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 0 ) ) )
+
+		# Top-level filtering is only useful if we can get replies on demand.
+		path = '/dataserver2/users/sjohnson@nextthought.com/Objects/' + top_n_ext_id  + '/@@replies'
+		res = testapp.get( path, extra_environ=self._make_extra_environ() )
+		assert_that( res.json_body, has_entry( 'Items', has_length( 1 ) ) )
+		assert_that( res.json_body, has_entry( 'Items', contains( has_entry( 'ID', reply_n_id ) ) ) )
+
+
+		# Now add a highlight and test type filtering
+		with mock_dataserver.mock_db_trans(self.ds):
+			user = users.User.get_user( user.username )
+			hl = contenttypes.Highlight()
+			hl.applicableRange = contentrange.ContentRangeDescription()
+			hl.containerId = 'tag:nti:foo'
+			user.addContainedObject( hl )
+			hl.lastModified = 3
+			hl_id = to_external_ntiid_oid( hl )
+
+		# Top level now include the hl
+		path = '/dataserver2/users/sjohnson@nextthought.com/Pages(' + top_n.containerId + ')/UserGeneratedData'
+		res = testapp.get( path, params={'filter': 'TopLevel'}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 2 ) ) )
+		assert_that( res.json_body, has_entry( 'Items', contains( has_entry( 'ID', hl_id ), has_entry( 'ID', top_n_id ) ) ) )
+
+		res = testapp.get( path, params={'filter': 'TopLevel', 'accept': contenttypes.Highlight.mime_type}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 1 ) ) )
+		assert_that( res.json_body, has_entry( 'Items', contains( has_entry( 'ID', hl_id ) ) ) )
+
+		res = testapp.get( path, params={'filter': 'TopLevel', 'accept': contenttypes.Note.mime_type}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 1 ) ) )
+		assert_that( res.json_body, has_entry( 'Items', contains( has_entry( 'ID', top_n_id ) ) ) )
+
+		res = testapp.get( path, params={'accept': contenttypes.Note.mime_type + ',' + contenttypes.Highlight.mime_type}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 3 ) ) )
+
+
+		res = testapp.get( path, params={'filter': 'TopLevel', 'accept': contenttypes.Note.mime_type + ',' + contenttypes.Highlight.mime_type}, extra_environ=self._make_extra_environ())
+		assert_that( res.json_body, has_entry( 'Items', has_length( 2 ) ) )
+		assert_that( res.json_body, has_entry( 'Items', contains( has_entry( 'ID', hl_id ), has_entry( 'ID', top_n_id ) ) ) )
