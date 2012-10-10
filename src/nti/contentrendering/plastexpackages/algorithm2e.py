cc128d7f30576cddf55a02bc88c375da204d4753
Sean Jones <sean.jones@nextthought.com>
2012-10-10 18:48:53 +0000

    Add partial support for the algorithm2e package.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13079 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/algorithm2e.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+from __future__ import print_function, unicode_literals
+
+import re
+
+from zope import interface
+
+from nti.contentrendering import plastexids
+from nti.contentrendering.resources import interfaces as res_interfaces
+
+from plasTeX import Base
+
+# SAJ: This is a partial implementation of the algorithm package.  Full support is planned.
+
+class _Ignored(Base.Command):
+    unicode = ''
+    def invoke( self, tex ):
+        return []
+
+class DontPrintSemicolon(_Ignored):
+    pass
+
+class SetNoLine(_Ignored):
+    pass
+
+class SetAlgoNoLine(_Ignored):
+    pass
+
+class KwData(Base.Command):
+    blockType = True
+    args = 'self'
+
+class KwResult(Base.Command):
+    blockType = True
+    args = 'self'
+
+class function(Base.Environment):
+    args = '[placement]'
+    blockType = True
+
+    class TitleOfAlgo(Base.Command):
+        blockType = True
+        args = 'self'
+
+    class semicolon(Base.Command):
+        macroName = ';'
+
+# Commands for different types of If / If-Then / If-Then-Else blocks
+
+class If(Base.Command):
+    blockType = True
+    args = '(comment) self then'
+
+class uIf(If):
+    pass
+
+class ElseIf(If):
+    pass
+
+class uElseIf(If):
+    pass
+
+class Else(Base.Command):
+    blockType = True
+    args = '(comment) self'
+
+class uElse(Else):
+    pass
+
+class eIf(Base.Command):
+    blockType = True
+    args = '(then_comment) self then (else_comment) else'
+
