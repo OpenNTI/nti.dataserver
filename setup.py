4a2c7105d70ad3ece9e664c0f275311e6dbcef01
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-10-11 19:25:30 +0000

    add entry point for script nti_set_user_attribute
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13132 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ entry_points = {
 		"nti_follow_entity = nti.dataserver.utils.nti_follow_entity:main",
 		"nti_remove_user = nti.dataserver.utils.nti_remove_user:main",
 		"nti_export_entities = nti.dataserver.utils.nti_export_entities:main",
+		"nti_set_user_attribute = nti.dataserver.utils.nti_set_user_attribute:main",
 		"nti_delete_user_objects = nti.dataserver.utils.nti_delete_user_objects:main",
 		"nti_export_user_objects = nti.dataserver.utils.nti_export_user_objects:main",
 		"nti_sharing_listener = nti.appserver.application:sharing_listener_main",
