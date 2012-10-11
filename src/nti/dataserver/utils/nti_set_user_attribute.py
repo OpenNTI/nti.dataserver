5bd14233934cfbc3d0fbfa15f3da88dd78d4ce6d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-10-11 21:34:43 +0000

    Based on JM's input get the appropriate schema fields for a user and use update_from_external_object to set attributes
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13143 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_set_user_attribute.py
+++ src/nti/dataserver/utils/nti_set_user_attribute.py
@@ -6,71 +6,85 @@ import sys
 import argparse
 from pprint import pprint
 
+from zope import interface
 from zope.schema import getFields
 
 from nti.dataserver import users
+from nti.dataserver.utils import run_with_dataserver
+from nti.utils.schema import find_most_derived_interface
 from nti.dataserver.users import interfaces as user_interfaces
 from nti.externalization.externalization import to_external_object
+from nti.externalization.internalization import update_from_external_object
 
-from nti.dataserver.utils import run_with_dataserver
-
-def change_attributes( username, data, verbose):
-	user = users.User.get_user( username )
+def _change_attributes(args):
+	user = users.User.get_user( args.username )
 	if not user:
-		print( "No user found", username, file=sys.stderr )
+		print( "No user found", args.username, file=sys.stderr )
 		sys.exit( 2 )
 
-	profile = user_interfaces.ICompleteUserProfile( user )
-	for k,v in data.items():
-		setattr(profile, k, v)
-		
-	if verbose:
+	external = {}
+	fields = _find_allowed_fields(user)
+	for name, sch_def in fields.items():
+		value = getattr(args, name, None)
+		if value is not None:
+			value = None if not value else unicode(value)
+			external[name] = sch_def.fromUnicode(value) if value else None
+
+	update_from_external_object(user, external)
+
+	if args.verbose:
 		pprint( to_external_object( user ) )
 
-def parse_schema(iface, attributes):
-	names = iface.names()
-	fields = getFields(iface) or {}
-	for name in names or ():
-		sch_def = fields.get(name, None)
-		if sch_def and name not in attributes:
-			attributes[name] = sch_def
-	
-	for base in iface.getBases() or ():
-		parse_schema(base, attributes)
+def _find_allowed_fields(user):
+	profile_iface = user_interfaces.IUserProfileSchemaProvider( user ).getSchema()
+	profile = profile_iface( user )
+	profile_schema = find_most_derived_interface( profile, profile_iface, possibilities=interface.providedBy(profile) )
+
+	result = {}
+	for k, v in profile_schema.namesAndDescriptions(all=True):
+		if interface.interfaces.IMethod.providedBy( v ):
+			continue
 		
-def main():
-	arg_parser = argparse.ArgumentParser( description="Opt email communication." )
+		if v.queryTaggedValue( user_interfaces.TAG_HIDDEN_IN_UI ):
+			continue
+
+		result[k] = v
+		
+	return result
+
+def _create_args_parser():
+	arg_parser = argparse.ArgumentParser( description="Set user attributes." )
 	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
 	arg_parser.add_argument( 'username', help="The username to edit" )
 	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
 	
 	attributes = {}
-	parse_schema(user_interfaces.ICompleteUserProfile, attributes)
+	def get_schema_fields(iface, attributes):
+		names = iface.names()
+		fields = getFields(iface) or {}
+		for name in names or ():
+			sch_def = fields.get(name, None)
+			if sch_def and name not in attributes:
+				attributes[name] = sch_def
+		
+		for base in iface.getBases() or ():
+			get_schema_fields(base, attributes)
+		
+	get_schema_fields(user_interfaces.ICompleteUserProfile, attributes)
 	for name, sch in attributes.items():
 		if not sch.readonly:
 			help_ = sch.getDoc() or sch.title
 			help_ = help_.replace('\n', '.')
 			opt = '--%s' % name
 			arg_parser.add_argument( opt, help=help_, dest=name, required=False)
-		
+			
+	return arg_parser
+			
+def main():
+	arg_parser = _create_args_parser()
 	args = arg_parser.parse_args()
-	
-	data = {}
-	for name, sch_def in attributes.items():
-		value = getattr(args, name, None)
-		if value is not None:
-			value = None if not value else unicode(value)
-			data[name] = sch_def.fromUnicode(value) if value else None
-	
-	if not data:
-		print( "Nothing to set", args, file=sys.stderr )
-		sys.exit( 2 )
-		
-	env_dir = args.env_dir	
-	username = args.username	
-	verbose = args.verbose
-	run_with_dataserver( environment_dir=env_dir, 
-						 function=lambda: change_attributes(username, data, verbose) )
+	run_with_dataserver( environment_dir=args.env_dir, 
+						 function=lambda: _change_attributes(args) )
 
 if __name__ == '__main__':
 	main()
