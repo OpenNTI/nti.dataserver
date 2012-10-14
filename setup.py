c845039c7b243ae92ad74a70813c481d3ac24a9e
Jason Madden <jason.madden@nextthought.com>
2012-10-14 16:33:16 +0000

    Bump lxml.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13189 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ setup(
 		 # fewer dependencies, but less helpful output and doesn't work with multiple workers (?)
 		#'linesman >= 0.2.2', # Conflicts with Pillow. Modify requires.txt as part of the patch
 		'logilab-common >= 0.58.1',
-		'lxml >= 3.0', # Powerful and Pythonic XML processing library combining libxml2/libxslt with the ElementTree API.
+		'lxml >= 3.0.1', # Powerful and Pythonic XML processing library combining libxml2/libxslt with the ElementTree API.
 		'nameparser >= 0.2.2', # Human name parsing
 		'nltk >= 2.0.3',
 		# numpy is req'd by nltk, but not depended on. sigh.
