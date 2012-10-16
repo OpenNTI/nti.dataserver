1a938c29318c226a48f3ca4c002846a619ba821b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-10-16 22:42:34 +0000

    For phrase search make sure we only return fragments where the actual phrase (word sequence) occurs.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13229 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/tests/test_search_highlights.py
+++ src/nti/contentsearch/tests/test_search_highlights.py
@@ -11,7 +11,7 @@ from hamcrest import (assert_that, is_, has_length)
 
 class TestSearchHighlight(ConfiguringTestBase):
 	
-	def test_word_content_highlight(self):
+	def test_word_content(self):
 		text = unicode(get_content("""
 			An orange-haired high school student, Ichigo becomes a "substitute Shinigami (Soul Reaper)"
 			after unintentionally absorbing most of Rukia Kuchiki's powers"""))
@@ -29,8 +29,7 @@ class TestSearchHighlight(ConfiguringTestBase):
 		assert_that(word_content_highlight('"high school"', text), 
 					is_('An orange-haired high school student Ichigo becomes a substitute Shinigami Soul'))
 		
-
-	def test_word_fragments_highlight(self):
+	def test_word_fragments(self):
 		text = unicode(get_content('All Waves, Rise now and Become my Shield, Lightning, Strike now and Become my Blade'))
 		
 		snippet, fragments = word_fragments_highlight('strike', text)
@@ -45,11 +44,6 @@ class TestSearchHighlight(ConfiguringTestBase):
 		assert_that(fragments[0].text, is_(_text))
 		assert_that(fragments[0].matches, is_([(23, 29), (65, 71)]))
 		
-		_, fragments = word_fragments_highlight('"become strike"', text)
-		assert_that(fragments, has_length(1))
-		assert_that(fragments[0].text, is_(_text))
-		assert_that(fragments[0].matches, is_([(23, 29), (50, 56), (65, 71)]))
-		
 		text = '. '.join(zanpakuto_commands)
 		_text = u'my Shield, Lightning, Strike now and Become my Blade. Cry, Raise Your Head, Rain Without end. Sting All Enemies To'
 		snippet, fragments = word_fragments_highlight('rain blade', text,  maxchars=50)
@@ -58,7 +52,7 @@ class TestSearchHighlight(ConfiguringTestBase):
 		assert_that(fragments[0].text, is_(_text))
 		assert_that(fragments[0].matches, is_([(47, 52), (76, 80)]))
 		
-	def test_word_fragments_highlight_multiple(self):
+	def test_word_fragments_multiple(self):
 		text = "Carlos is going on vacation from Mexico to London with a brief stop in New York He forgot to exchange his pesos for " +\
 		"British pounds and must do so in New York He would like to have 2000 British pounds for his trip 12.1 Mexican pesos can be " +\
 		"exchanged for 1 dollar and 1 dollar can be exchanged for 0.62 pounds To the nearest peso how many pesos will Carlos have to "+\
@@ -66,14 +60,14 @@ class TestSearchHighlight(ConfiguringTestBase):
 		snippet, fragments = word_fragments_highlight('carlos', text)
 		
 		f1 = "Carlos is going on vacation from Mexico to London with a brief stop in New York He forgot to exchange his pesos for British"
-		f2 = "62 pounds To the nearest peso how many pesos will Carlos have to exchange in order to get 2000 British pounds"
+		f2 = "pounds To the nearest peso how many pesos will Carlos have to exchange in order to get 2000 British pounds"
 		s = "...".join([f1,f2])
 		assert_that(fragments, has_length(2))
 		assert_that(snippet, is_(s))
 		assert_that(fragments[0].text, is_(f1))
 		assert_that(fragments[0].matches, is_([(0, 6)]))
 		assert_that(fragments[1].text, is_(f2))
-		assert_that(fragments[1].matches, is_([(50, 56)]))
+		assert_that(fragments[1].matches, is_([(47, 53)]))
 		
 	def test_word_fragments_prealgebra(self):
 		text = u"get more complicated like trying to compute the trajectory or trying to analyze a " +\
@@ -83,7 +77,7 @@ class TestSearchHighlight(ConfiguringTestBase):
 		assert_that(snippet, is_(text))
 		assert_that(fragments[0].matches, is_([(26, 32), (33, 35), (48, 58), (62, 68), (72, 79), (102, 108), (69, 71)]))
 		
-	def test_word_cohen(self):
+	def test_word_fragments_cohen(self):
 		text = u"Critical to the Court's judgment is the undisputed fact that [Fields] was told that he was free to end the questioning " +\
 				"and to return to his cell. Ante, at 1194. Never mind the facts suggesting that Fields's submission to the overnight " +\
 				"interview was anything but voluntary. Was Fields held for interrogation?"
@@ -95,5 +89,18 @@ class TestSearchHighlight(ConfiguringTestBase):
 		assert_that(fragments[0].text, is_(_text))
 		assert_that(fragments[0].matches, is_([(16, 23)]))
 		
+	def test_word_fragments_phrase(self):
+		text = u'Black and white net. Twenty two bridges in the land of gulf, sixty six crowns and belts. Footprints, distant thunder, ' +\
+				'sharp peak, engulfing land, hidden in the night, sea of clouds, blue line. Form a circle and fly though the heavens and' +\
+				'form the land of creation'
+				
+		snippet, fragments = word_fragments_highlight('"engulfing land"', text)
+		_text = u"sharp peak, engulfing land, hidden in the night, sea of clouds, blue line. Form a circle"
+		assert_that(fragments, has_length(1))
+		assert_that(snippet, is_(_text))
+		assert_that(fragments[0].text, is_(_text))
+		assert_that(fragments[0].matches, is_([(12,21), (22, 26)]))
+
+			
 if __name__ == '__main__':
 	unittest.main()
