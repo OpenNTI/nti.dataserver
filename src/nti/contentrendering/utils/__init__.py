aad1542f00761b661d55411b9c3ca35df20ee54c
Sean Jones <sean.jones@nextthought.com>
2012-10-18 23:02:21 +0000

    Add a new RenderedBook subclass that does not use concurrency or phantomJS. This side-steps a intermitant deadlock in the ProcessPoolExecuter code on Linux.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13294 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/utils/__init__.py
+++ src/nti/contentrendering/utils/__init__.py
@@ -21,3 +21,19 @@ class NoPhantomRenderedBook(RenderedBook):
     def _get_phantom_function(self):
         return _phantom_function
 
+class NoConcurrentPhantomRenderedBook(RenderedBook):
+
+    # Replaces RenderedBook.runPhandomOnPages with a version that does not require phantomJS and does not
+    # call the sometimes problematic concurrency module. Useful for the creation of RenderedBook objects
+    # where we are only going to alter basic things such as the index or sharewith attribute.
+    def runPhantomOnPages(self, script, *args):
+        eclipseTOC = self.toc
+        nodesForPages = eclipseTOC.getPageNodes()
+
+        results = {}
+        for node in nodesForPages:
+            key = (node.getAttribute(b'ntiid'),node.getAttribute(b'href'),node.getAttribute(b'label'))
+            result = {'ntiid': key[0]}
+            results[key] = result
+
+    return results
