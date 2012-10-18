21175a21f5fef85c97b1b9968265b8d95967ecea
Jason Madden <jason.madden@nextthought.com>
2012-10-18 18:07:37 +0000

    Much better job rendering content for feeds.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13273 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/ugd_feed_views.py
+++ src/nti/appserver/ugd_feed_views.py
@@ -20,17 +20,39 @@ import datetime
 
 from pyramid.view import view_config
 
+from zope import interface
+from zope import component
+
 from zope.dublincore import interfaces as dc_interfaces
 from nti.dataserver import interfaces as nti_interfaces
 from nti.dataserver.users import interfaces as user_interfaces
+from nti.contentfragments import interfaces as frg_interfaces
 
 from nti.appserver import httpexceptions as hexc
-from nti.appserver._table_utils import NoteBodyColumn
 from nti.appserver.pyramid_renderers import compress_body
 from nti.appserver.ugd_query_views import _RecursiveUGDStreamView
 
 from nti.dataserver import authorization as nauth
+
+from nti.externalization.externalization import to_external_representation
 from nti.externalization.oids import to_external_ntiid_oid
+
+import html5lib
+from html5lib import treebuilders
+#from html5lib.filters._base import Filter as FilterBase
+
+import lxml.etree
+
+class _BetterDateAtom1Feed(feedgenerator.Atom1Feed):
+	"""
+	Provides the ``published`` element for atom feeds; if this
+	is missing and only ``updated`` is present (the default in the super class)
+	some readers will fail to present a valid date.
+	"""
+	def add_item_elements(self, handler, item):
+		super(_BetterDateAtom1Feed,self).add_item_elements( handler, item )
+		if item.get('published') is not None:
+			handler.addQuickElement(u"published", feedgenerator.rfc3339_date(item['published']).decode('utf-8'))
 
 
 @view_config( route_name='user.pages.odata.traversal.feeds',
@@ -44,6 +66,13 @@ from nti.externalization.oids import to_external_ntiid_oid
 			  permission=nauth.ACT_READ, request_method='GET',
 			  http_cache=datetime.timedelta(hours=1))
 def feed_view( request ):
+	"""
+	Primary view for producing Atom and RSS feeds. Accepts the same filtering
+	parameters as :class:`nti.appserver.ugd_query_views._RecursiveUGDStreamView`
+	so you can control what types of actions you see in the feed as well as the
+	length of the feed.
+
+	"""
 	response = request.response
 
 	stream_view = _RecursiveUGDStreamView(request)
@@ -62,7 +91,7 @@ def feed_view( request ):
 			raise not_mod
 
 
-	feed_factory = feedgenerator.Rss201rev2Feed if request.view_name == 'feed.rss' else feedgenerator.Atom1Feed
+	feed_factory = feedgenerator.Rss201rev2Feed if request.view_name == 'feed.rss' else _BetterDateAtom1Feed
 
 	feed = feed_factory(
 		title=request.context.ntiid, # TODO: Better title
@@ -71,15 +100,9 @@ def feed_view( request ):
 		description='',
 		language='en' )
 
-	# TODO: adapters
-	note_description_maker = NoteBodyColumn(None,None,None).renderCell
-
 	for change in ext_dict['Items']:
-		descr = ''
-		if nti_interfaces.INote.providedBy( change.object ):
-			descr = note_description_maker( change.object )
-		elif nti_interfaces.IHighlight.providedBy( change.object ):
-			descr = change.object.selectedText
+		renderer = IFeedRenderer( change.object, None )
+		descr = renderer.render() if renderer else ''
 
 		creator_profile = user_interfaces.IUserProfile( change.creator )
 		feed.add_item(
@@ -87,13 +110,92 @@ def feed_view( request ):
 			link=request.application_url, # TODO: Not right
 			description=descr,
 			author_email=getattr( creator_profile, 'email', None ),
-			author_name=getattr( creator_profile, 'realname', None ), # TODO: Alias or something
+			author_name=creator_profile.realname or creator_profile.alias or unicode(change.creator),
 			pubdate=dc_interfaces.IDCTimes( change ).created,
 			unique_id=to_external_ntiid_oid(change.object),
-			categories=(change.type,) )
+			categories=(change.type,),
+			# extras. If we don't provide a 'published' date
+			updated=dc_interfaces.IDCTimes( change ).modified,
+			published=dc_interfaces.IDCTimes( change ).created,
+			)
 
 
 	response.content_type = feed.mime_type
 	response.body = compress_body( request, response, feed.writeString('utf-8'), False )
 
 	return response
+
+class IFeedRenderer(interface.Interface):
+
+	def render():
+		"""
+		Render to an HTML string the context object.
+		"""
+
+@interface.implementer(IFeedRenderer)
+@component.adapter(nti_interfaces.INote)
+class NoteFeedRenderer(object):
+	"""
+	Renderers notes in HTML for feeds. Does what it can with canvas objects,
+	which is to include their URL.
+
+	.. note:: This is similar to :class:`nti.appserver._table_utils.NoteBodyColumn`.
+
+	"""
+	def __init__( self, context ):
+		self.context = context
+
+	def render(self):
+		parts = []
+		for part in self.context.body:
+			if nti_interfaces.ICanvas.providedBy( part ):
+				for shape in part.shapeList:
+					url = getattr( shape, 'url', None )
+					if url:
+						ext_url = shape.toExternalObject()['url']
+						if nti_interfaces.ILink.providedBy( ext_url ):
+							ext_url = to_external_representation( ext_url, 'json' )
+						part = "<img src=%s />" % ext_url
+					else:
+						part = None
+			elif frg_interfaces.IHTMLContentFragment.providedBy( part ):
+				parser = html5lib.HTMLParser( tree=treebuilders.getTreeBuilder("lxml"), namespaceHTMLElements=False )
+				doc = parser.parse( part )
+				body = doc.find('body')
+				if body:
+					part = lxml.etree.tostring( body, method='html', encoding=unicode )
+					part = part[6:-7] # strip the enclosing body tag
+				else:
+					part = None
+			else:
+				part = frg_interfaces.IPlainTextContentFragment( part, None ) or unicode(part)
+
+			if part:
+				parts.append( part )
+		return '<br />'.join( parts )
+
+@interface.implementer(IFeedRenderer)
+@component.adapter(nti_interfaces.ISelectedRange)
+class SelectedRangeFeedRenderer(object):
+	"""
+	For highlights and the like.
+	"""
+
+	def __init__( self, context ):
+		self.context = context
+
+	def render(self):
+		return self.context.selectedText
+
+@interface.implementer(IFeedRenderer)
+@component.adapter(nti_interfaces.IEntity)
+class EntityFeedRenderer(object):
+	"""
+	For circled users.
+	"""
+
+	def __init__( self, context ):
+		self.context = context
+
+	def render(self):
+		return self.context.username
