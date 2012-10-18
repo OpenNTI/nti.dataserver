9bef73188297bd64a12f5154ca072847bea11936
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-10-18 21:36:09 +0000

    Rollback per JM's input
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13289 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/generations/evolve12.py
+++ src/nti/contentsearch/generations/evolve12.py
@@ -2,11 +2,6 @@ from __future__ import print_function, unicode_literals
 
 generation = 12
 
-import math
-import multiprocessing
-
-from concurrent.futures import ProcessPoolExecutor
-
 import zope.intid
 from zope import component
 from ZODB.POSException import POSKeyError
@@ -36,11 +31,7 @@ def _index(rim, obj, ds_intid):
 		return True
 	return False
 
-def reindex_ugd(user, all_users_map, ds_intid, ignore_errors=False):
-	
-	if user is None:
-		return None
-	
+def reindex_ugd(user, users, ds_intid, ignore_errors=False):
 	username = user.username
 	logger.debug('Reindexing object(s) for user %s' % username)
 	
@@ -57,7 +48,7 @@ def reindex_ugd(user, all_users_map, ds_intid, ignore_errors=False):
 			if _index(rim, obj, ds_intid):
 				counter = counter + 1
 				for uname in get_sharedWith(obj):
-					sharing_user = all_users_map.get(uname, None)
+					sharing_user = users.get(uname, None)
 					if sharing_user and uname != username: 
 						srim = search_interfaces.IRepozeEntityIndexManager(sharing_user, None)
 						if srim is not None:
@@ -71,22 +62,6 @@ def reindex_ugd(user, all_users_map, ds_intid, ignore_errors=False):
 
 	logger.debug('%s object(s) for user %s were reindexed' % (counter, username))
 	
-	return counter
-	
-def _chunk(_input, input_size=None, chunks=multiprocessing.cpu_count()):
-	input_size = input_size or len(_input)
-	size = math.ceil(input_size/chunks)
-	size = int(max(1, size))
-	return map(None, *([iter(_input)] * size))
-
-def _reindex_user_list(user_list, all_users_map, ds_intid):
-	counter = 0
-	for user in user_list:
-		cnt = reindex_ugd(user, all_users_map, ds_intid)
-		if cnt is not None:
-			counter += 1
-	return counter
-		
 def do_evolve(context):
 	conn = context.connection
 	root = conn.root()
@@ -98,21 +73,9 @@ def do_evolve(context):
 	
 	# make sure we register/provide the intid so it can be found
 	component.provideUtility(ds_intid, zope.intid.IIntIds )
-	
-	no_users = len(users)
-	chunks = _chunk(users.values(), no_users)
-	
-	logger.debug('Evolving data for %s user(s)' % no_users)
-	
-	counter = 0
-	with ProcessPoolExecutor() as executor:
-		for cnt in executor.map(_reindex_user_list, 
-								chunks,
-								[users] * len(chunks),
-								[ds_intid] * len(chunks)):
-			counter += cnt
-			logger.debug('%s out of %s user(s) have been processed' % (counter, no_users))
-			
+	for user in users.values():
+		reindex_ugd(user, users, ds_intid)
+		
 	logger.debug('Evolution done!!!')
 		
 def evolve(context):
