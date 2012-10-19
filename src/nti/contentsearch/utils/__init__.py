313b9272ee7e78df640f79ba39c6f68a5c85499a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-10-19 19:09:27 +0000

    group common index utilities.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13345 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/utils/__init__.py
+++ src/nti/contentsearch/utils/__init__.py
@@ -1,44 +1,66 @@
 from __future__ import print_function, unicode_literals
 
+import zope.intid
 from zope import component
 from zope.generations.utility import findObjectsProviding
 
+from nti.dataserver import users
 from nti.dataserver.users import friends_lists
 from nti.dataserver import interfaces as nti_interfaces
 
+from nti.contentsearch import get_indexable_types
+from nti.contentsearch.common import get_type_name
 from nti.contentsearch import interfaces as search_interfaces
 
-def find_user_dfls(user):
-    result = {}
-    for obj in findObjectsProviding( user, nti_interfaces.IFriendsList):
-        if isinstance(obj, friends_lists.DynamicFriendsList):
-            result[obj.username] = obj
-    return result
+def get_uid(obj, intids=None):
+	intids = intids or component.getUtility( zope.intid.IIntIds )
+	result = intids.getId(obj)
+	return result
 
-def remove_rim_catalogs(rim):
-    if rim is not None:
-        for key in list(rim.keys()):
-            rim.pop(key, None)
-        return True
-    return False
-            
-def remove_entity_catalogs(entity):
-    rim = search_interfaces.IRepozeEntityIndexManager(entity, None)
-    return remove_rim_catalogs(rim)
+def find_user_dfls(user):
+	"""return a generator with dfl objects for the specfied user"""
+	
+	for obj in findObjectsProviding( user, nti_interfaces.IFriendsList):
+		if isinstance(obj, friends_lists.DynamicFriendsList):
+			yield obj
 
 def get_sharedWith(obj):
-    # from IPython.core.debugger import Tracer;  Tracer()() ## DEBUG ##
-    adapted = component.getAdapter(obj, search_interfaces.IContentResolver)
-    result = getattr(adapted, 'get_sharedWith', None) if adapted is not None else None
-    result = result() if result else ()
-    return result
-         
-def get_catalog_and_docids(entity):
-    rim = search_interfaces.IRepozeEntityIndexManager(entity, None)
-    if rim is not None:
-        for catalog in rim.values():
-            catfield = list(catalog.values())[0] if catalog else None
-            if hasattr(catfield, "_indexed"):
-                yield catalog, list(catfield._indexed())
+	"""return the usernames the specified obejct is shared with"""
+	# from IPython.core.debugger import Tracer;  Tracer()() ## DEBUG ##
+	adapted = component.queryAdapter(obj, search_interfaces.IContentResolver)
+	result = getattr(adapted, 'get_sharedWith', None) if adapted is not None else None
+	result = result() if result else ()
+	return result
 
-        
+def find_all_indexable_pairs(user, user_get=users.Entity.get_entity, include_dfls=True):
+	"""
+	return a generator with all the objects that need to be indexed. 
+	The genertor yield pairs (entity, obj) indicating that the object has to be indexed
+	for the particuluar entity
+	"""
+
+	dfls = []
+	username = user.username
+	
+	#intids = intids or component.getUtility( zope.intid.IIntIds )
+	
+	indexable_types = get_indexable_types()
+	for obj in findObjectsProviding( user, nti_interfaces.IModeledContent):
+		
+		if isinstance(obj, friends_lists.DynamicFriendsList) and include_dfls:
+			dfls.append(obj)
+		elif get_type_name(obj) in indexable_types:
+			
+			yield (user, obj)
+			
+			# check if object is shared 
+			for uname in get_sharedWith(obj):
+				sharing_user = user_get(uname)
+				if sharing_user and uname != username: 
+					yield (sharing_user, obj)
+
+	for dfl in dfls:
+		for container in dfl.containersOfShared.containers.values():
+			for obj in container:
+				if obj is not None and get_type_name(obj) in indexable_types:
+					yield (dfl, obj)
--- src/nti/contentsearch/utils/nti_reindex_user_content.py
+++ src/nti/contentsearch/utils/nti_reindex_user_content.py
@@ -4,78 +4,47 @@ from __future__ import print_function, unicode_literals
 
 import os
 import sys
+import argparse
 
-import zope.intid
-from zope import component
 from ZODB.POSException import POSKeyError
-from zope.generations.utility import findObjectsProviding
 
 from nti.dataserver import users
+from nti.dataserver.users import friends_lists
 from nti.dataserver.utils import run_with_dataserver
-from nti.dataserver import interfaces as nti_interfaces
 
 import nti.contentsearch
-from nti.contentsearch.common import get_type_name
-from nti.contentsearch import get_indexable_types
+from nti.contentsearch.utils import get_uid
 from nti.contentsearch import interfaces as search_interfaces
-
-def _get_uid(obj):
-	_ds_intid = component.getUtility( zope.intid.IIntIds )
-	result = _ds_intid.getId(obj)
-	return result
-
-def _get_sharedWith(obj):
-	adapted = component.getAdapter(obj, search_interfaces.IContentResolver)
-	if adapted and hasattr(adapted, "get_sharedWith"):
-		result = adapted.get_sharedWith()
-	else:
-		result = ()
-	return result
-
-def _get_indeaxable_objects(user, rim=None):
-	username = user.username
-
-	indexable_types = get_indexable_types()
-	rim = rim or search_interfaces.IRepozeEntityIndexManager(user, None)
-	for obj in findObjectsProviding( user, nti_interfaces.IModeledContent):
-		
-		if get_type_name(obj) not in indexable_types:
-			continue
-		
-		yield (rim, obj)
-		
-		for uname in _get_sharedWith(obj):
-			sharing_user = users.Entity.get_entity(uname)
-			if sharing_user and uname != username: 
-				srim = search_interfaces.IRepozeEntityIndexManager(sharing_user, None)
-				if srim is not None:
-					yield (srim, obj)
-		
-def _index(rim, obj):
-	catalog = rim.get_create_catalog(obj)
-	if catalog:
-		docid = _get_uid(obj)
-		catalog.index_doc(docid, obj)
-		return True
-	return False
+from nti.contentsearch.utils import find_all_indexable_pairs
+from nti.contentsearch.utils.repoze_utils import remove_entity_catalogs
 			
-def _reindex_entity_content( username ):
+def reindex_entity_content( username, include_dfls):
 	entity = users.Entity.get_entity( username )
 	if not entity:
 		print( "user/entity '%s' does not exists" % username, file=sys.stderr )
 		sys.exit( 2 )
 
-	counter = 0
-	rim = search_interfaces.IRepozeEntityIndexManager(entity, None)
-	if rim is not None:
-		# remove all catalogs
-		for key in list(rim.keys()):
-			rim.pop(key, None)
+	counter = 0	
+	dfl_names = set()
+	
+	# remove catalogs for main entity
+	remove_entity_catalogs(entity)
+	
+	# loop through all user indexable objects
+	for e, obj in find_all_indexable_pairs(entity, include_dfls=include_dfls):
 		
-		for eim, obj in _get_indeaxable_objects(entity, rim):
+		# if we find a DFL clear its catalogs
+		if isinstance(e, friends_lists.DynamicFriendsList) and e.username not in dfl_names:
+			remove_entity_catalogs(e)
+			dfl_names.add(e.username)
+		
+		rim = search_interfaces.IRepozeEntityIndexManager(e, None)
+		catalog = rim.get_create_catalog(obj) if rim is not None else None
+		if catalog is not None:
 			try:
-				if _index(eim, obj):
-					counter = counter + 1
+				docid = get_uid(obj)
+				catalog.index_doc(docid, obj)
+				counter = counter + 1
 			except POSKeyError:
 				# broken reference for object
 				pass
@@ -83,15 +52,19 @@ def _reindex_entity_content( username ):
 	return counter
 
 def main():
-	if len(sys.argv) < 3:
-		print( "Usage %s env_dir username" % sys.argv[0] )
-		sys.exit( 1 )
+	arg_parser = argparse.ArgumentParser( description="Reindex user content" )
+	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
+	arg_parser.add_argument( 'username', help="The username" )
+	arg_parser.add_argument( '--include_dfls', help="Unindex content in user's dfls", action='store_true', dest='include_dfls')
+	args = arg_parser.parse_args()
 
-	env_dir = os.path.expanduser(sys.argv[1])
-	username = sys.argv[2]
+	username = args.username
+	include_dfls = args.include_dfls
+	env_dir = os.path.expanduser(args.env_dir)
+	
 	run_with_dataserver( environment_dir=env_dir,
 						 xmlconfig_packages=(nti.contentsearch,),
-						 function=lambda: _reindex_entity_content(username) )
+						 function=lambda: reindex_entity_content(username, include_dfls) )
 
 if __name__ == '__main__':
 	main()
