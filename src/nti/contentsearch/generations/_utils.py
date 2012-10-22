6566d2e8c1a24e48426439434890a183b7339c50
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-10-22 15:06:11 +0000

    do only one evolution (mostly for prod)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13360 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentsearch/generations/_utils.py
@@ -0,0 +1,63 @@
+from __future__ import print_function, unicode_literals
+
+import zope.intid
+from zope import component
+from ZODB.POSException import POSKeyError
+
+from nti.dataserver.users import friends_lists
+
+from nti.contentsearch.utils import get_uid
+from nti.contentsearch.utils import find_all_indexable_pairs
+from nti.contentsearch import interfaces as search_interfaces 
+from nti.contentsearch.utils.repoze_utils import remove_entity_catalogs
+						
+import logging
+logger = logging.getLogger( __name__ )
+
+def reindex_ugd(user, users_get, ds_intid):
+	username = user.username
+	logger.debug('Reindexing object(s) for %s' % username)
+	
+	remove_entity_catalogs(user)
+	
+	counter = 0
+	dfl_names = set()
+	
+	for e, obj in find_all_indexable_pairs(user, user_get=users_get, include_dfls=True):
+		
+		# if we find a DFL clear its catalogs
+		if isinstance(e, friends_lists.DynamicFriendsList) and e.username not in dfl_names:
+			remove_entity_catalogs(e)
+			dfl_names.add(e.username)
+		
+		rim = search_interfaces.IRepozeEntityIndexManager(e, None)
+		catalog = rim.get_create_catalog(obj) if rim is not None else None
+		if catalog is not None:
+			try:
+				docid = get_uid(obj, ds_intid)
+				catalog.index_doc(docid, obj)
+				counter = counter + 1
+			except POSKeyError:
+				# broken reference for object
+				pass
+	
+	logger.debug('%s object(s) for user %s were reindexed' % (counter, username))
+	
+	return counter
+	
+def reindex_all(context):
+	conn = context.connection
+	root = conn.root()
+	container = root['nti.dataserver']
+	lsm = container.getSiteManager()
+	users = context.connection.root()['nti.dataserver']['users']
+	
+	ds_intid = lsm.getUtility( provided=zope.intid.IIntIds )
+	component.provideUtility(ds_intid, zope.intid.IIntIds )
+	
+	for user in users.values():
+		reindex_ugd(user, users.get, ds_intid)
+		
+	logger.debug('Evolution done!!!')
+
+		
--- src/nti/contentsearch/generations/evolve12.py
+++ src/nti/contentsearch/generations/evolve12.py
@@ -2,88 +2,11 @@ from __future__ import print_function, unicode_literals
 
 generation = 12
 
-import zope.intid
-from zope import component
-from ZODB.POSException import POSKeyError
-from zope.generations.utility import findObjectsProviding
-
-from nti.dataserver import interfaces as nti_interfaces
-
-from nti.contentsearch import interfaces as search_interfaces 
-
-import logging
-logger = logging.getLogger( __name__ )
-
-def get_sharedWith(obj):
-	# from IPython.core.debugger import Tracer;  Tracer()() ## DEBUG ##
-	adapted = component.getAdapter(obj, search_interfaces.IContentResolver)
-	if adapted and hasattr(adapted, "get_sharedWith"):
-		result = adapted.get_sharedWith()
-	else:
-		result = ()
-	return result
-
-def _index(rim, obj, ds_intid):
-	catalog = rim.get_create_catalog(obj)
-	if catalog:
-		docid = ds_intid.getId(obj)
-		catalog.index_doc(docid, obj)
-		return True
-	return False
-
-def reindex_ugd(user, users, ds_intid, ignore_errors=False):
-	username = user.username
-	logger.debug('Reindexing object(s) for user %s' % username)
-	
-	counter = 0
-	rim = search_interfaces.IRepozeEntityIndexManager(user, None)
-	for obj in findObjectsProviding( user, nti_interfaces.IModeledContent):
-		
-		# ignore friends lists
-		if nti_interfaces.IFriendsList.providedBy(obj):
-			continue
-		
-		# index object
-		try:
-			if _index(rim, obj, ds_intid):
-				counter = counter + 1
-				for uname in get_sharedWith(obj):
-					sharing_user = users.get(uname, None)
-					if sharing_user and uname != username: 
-						srim = search_interfaces.IRepozeEntityIndexManager(sharing_user, None)
-						if srim is not None:
-							_index(srim, obj, ds_intid)
-		except POSKeyError:
-			pass # broken reference for object
-		except Exception:
-			if not ignore_errors:
-				raise
-			pass
-
-	logger.debug('%s object(s) for user %s were reindexed' % (counter, username))
-	
-def do_evolve(context):
-	conn = context.connection
-	root = conn.root()
-	container = root['nti.dataserver']
-	lsm = container.getSiteManager()
-	users = context.connection.root()['nti.dataserver']['users']
-	
-	ds_intid = lsm.getUtility( provided=zope.intid.IIntIds )
-	
-	# make sure we register/provide the intid so it can be found
-	component.provideUtility(ds_intid, zope.intid.IIntIds )
-	for user in users.values():
-		reindex_ugd(user, users, ds_intid)
-		
-	logger.debug('Evolution done!!!')
-		
+from nti.contentsearch.generations._utils import reindex_all
+						
 def evolve(context):
 	"""
 	Evolve generation 11 to generation 12 by reindexing in the user space
 	"""
-	conn = context.connection
-	search_conn = conn.get_connection( 'Search' )
-	search_conn.root().pop('repoze_datastore', None)
-	do_evolve(context)
+	reindex_all(context)
 		
