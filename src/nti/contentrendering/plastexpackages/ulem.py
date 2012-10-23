e6524f1c521a5afcfb97cdcd4727e1222982e56c
Sean Jones <sean.jones@nextthought.com>
2012-10-23 17:46:56 +0000

    Add support for the ulem package.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13400 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/ulem.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from __future__ import print_function, unicode_literals
+
+from plasTeX.Base import TextCommand
+
+class uline(TextCommand):
+	pass
+
+class uuline(TextCommand):
+	pass
+
+class uwave(TextCommand):
+	pass
+
+class sout(TextCommand):
+	pass
+
+class xout(TextCommand):
+	pass
+
+class dashuline(TextCommand):
+	pass
+
+class dotuline(TextCommand):
+	pass
+
