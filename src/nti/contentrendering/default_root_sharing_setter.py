3cabc56b455395bdc6ca1b424add61353865c67e
Sean Jones <sean.jones@nextthought.com>
2012-10-25 23:12:35 +0000

    Add the option to specify the sharedWith parameter from the command line instead of from a file.  Updated the test suite and fixed several broken tests.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13482 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/default_root_sharing_setter.py
+++ src/nti/contentrendering/default_root_sharing_setter.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function, unicode_literals
 
+import argparse
 import io
 import os
-import sys
 
 import logging
 logger = logging.getLogger(__name__)
@@ -15,36 +15,43 @@ from zope.configuration import xmlconfig
 
 import nti.contentrendering
 from nti.contentrendering import interfaces
-from nti.contentrendering import RenderedBook
 
 interface.moduleProvides( interfaces.IRenderedBookTransformer )
 
 DEFAULT_SHARING_GROUP_FILENAME = 'nti-default-root-sharing-group.txt'
 
+def _parse_args():
+	arg_parser = argparse.ArgumentParser( description="Content default sharing setter" )
+        arg_parser.add_argument( 'contentpath', help="Content book location" )
+	arg_parser.add_argument( "-g", "--groupname", dest='groupname', help="Name of the default sharing group", default=None)
+	return arg_parser.parse_args()
+
 def main():
 	""" Main program routine """
+	from nti.contentrendering.utils import NoConcurrentPhantomRenderedBook, EmptyMockDocument
 
-	contentLocation = sys.argv[1]
+	args = _parse_args()
+	contentpath = os.path.expanduser(args.contentpath)
 
 	xmlconfig.file( 'configure.zcml', package=nti.contentrendering )
-	zope_conf_name = os.path.join( contentLocation, '..', 'configure.zcml' )
+	zope_conf_name = os.path.join( contentpath, '..', 'configure.zcml' )
 	if os.path.exists( zope_conf_name ):
 		xmlconfig.file( os.path.abspath( zope_conf_name ), package=nti.contentrendering )
 
-	context = interfaces.JobComponents( os.path.split( os.path.abspath( contentLocation ) )[-1] )
+	context = interfaces.JobComponents( os.path.split( os.path.abspath( contentpath ) )[-1] )
 
-	book = RenderedBook.RenderedBook( None, contentLocation )
-	transform( book, context=context )
+	book = NoConcurrentPhantomRenderedBook( EmptyMockDocument(), contentpath )
+	transform( book, context=context, group_name=args.group_name )
 
-def transform( book, save_toc=True, context=None ):
+def transform( book, save_toc=True, context=None, group_name=None ):
 	"""
 	Modifies the TOC dom by adding the "shareWith" attribute to the root node.
 	"""
 	dom = book.toc.dom
 	toc = dom.getElementsByTagName("toc")
 
-	if toc and os.path.exists( os.path.join( book.contentLocation, '..', DEFAULT_SHARING_GROUP_FILENAME ) ):
-		modified = _handle_toc(toc[0], book)
+	if toc and ( group_name or os.path.exists( os.path.join( book.contentLocation, '..', DEFAULT_SHARING_GROUP_FILENAME ) ) ):
+		modified = _handle_toc(toc[0], book, group_name=group_name)
 		if save_toc:
 			if modified:
 				book.toc.save()
@@ -53,17 +60,21 @@ def transform( book, save_toc=True, context=None ):
 
 	raise Exception( "Failed to add default sharing group to  %s. Either the RenderedBook is malformed or the default sharing group data file is missing." % (book) )
 
-def _handle_toc(toc, book):
+def _handle_toc(toc, book, group_name=None):
 	contentLocation = book.contentLocation
 	modified = True
 
 	if contentLocation:
 		sharedWith = ''
-		with io.open( os.path.join(contentLocation, '..', DEFAULT_SHARING_GROUP_FILENAME), 'rb') as file:
-			for line in file.readlines():
-				if line[0] is not '#':
-					# Otherwise the line is a comment
-					sharedWith = ' '.join([sharedWith, line.strip()])
+		if group_name:
+			sharedWith = group_name
+		else:
+			sharedWith_file = os.path.join(contentLocation, '..', DEFAULT_SHARING_GROUP_FILENAME)
+			with io.open( sharedWith_file, 'rb') as file:
+				for line in file.readlines():
+					if line[0] is not '#':
+						# Otherwise the line is a comment
+						sharedWith = ' '.join([sharedWith, line.strip()])
 
 		index = book.toc.root_topic
 		modified = index.set_default_sharing_group( sharedWith.strip() )
