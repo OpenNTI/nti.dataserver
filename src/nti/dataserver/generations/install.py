378676950ef12ebe26ec75b5fe35655209c04a89
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-10-26 01:26:08 +0000

    add a field called viewportRatio (default to 1.0) to all canvas objects.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13487 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/install.py
+++ src/nti/dataserver/generations/install.py
@@ -6,7 +6,7 @@ from __future__ import print_function, unicode_literals
 
 __docformat__ = 'restructuredtext'
 
-generation = 31
+generation = 32
 
 from zope.generations.generations import SchemaManager
 
