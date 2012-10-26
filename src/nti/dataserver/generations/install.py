c005cc84a9ce80fdbfabbccea5bc61b51ff46da6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-10-26 02:45:10 +0000

    perfor a scale and rotation on the transform of existing squares.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13489 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/install.py
+++ src/nti/dataserver/generations/install.py
@@ -6,7 +6,7 @@ from __future__ import print_function, unicode_literals
 
 __docformat__ = 'restructuredtext'
 
-generation = 32
+generation = 33
 
 from zope.generations.generations import SchemaManager
 
