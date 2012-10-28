ae091c1bcbe1a365a547313080c8b5a91ec170b4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-10-28 18:36:42 +0000

    minor change
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13540 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/contentchecks/checkpagewidth.py
+++ src/nti/contentrendering/contentchecks/checkpagewidth.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2.7
+
 import logging
 logger = logging.getLogger( __name__ )
 
--- src/nti/contentrendering/contentchecks/tests/test_contentchecks.py
+++ src/nti/contentrendering/contentchecks/tests/test_contentchecks.py
@@ -1,10 +1,8 @@
 import os
 
-from nti.contentrendering.utils import EmptyMockDocument, NoPhantomRenderedBook
-
-from nti.contentrendering.contentchecks import performChecks
-
 from nti.contentrendering.contentchecks import mathjaxerror
+from nti.contentrendering.contentchecks import performChecks
+from nti.contentrendering.utils import EmptyMockDocument, NoPhantomRenderedBook
 
 from nti.contentrendering.tests import ConfiguringTestBase
 
@@ -12,18 +10,24 @@ from hamcrest import assert_that, has_length, greater_than_or_equal_to, is_
 
 class TestContentChecks(ConfiguringTestBase):
 
+	def get_path(self, p):
+		return os.path.join( os.path.dirname( __file__ ), *p)
+	
 	def test_checks(self):
-		book = NoPhantomRenderedBook( EmptyMockDocument(), os.path.join( os.path.dirname( __file__ ), '..', '..', 'tests', 'intro-biology-rendered-book' ) )
+		path = self.get_path(('..', '..', 'tests', 'intro-biology-rendered-book' ))
+		book = NoPhantomRenderedBook( EmptyMockDocument(), path)
 		res = performChecks( book )
 		assert_that( res, has_length( greater_than_or_equal_to( 3 ) ) )
 
 	def test_check_mathjax(self):
-		book = NoPhantomRenderedBook( EmptyMockDocument(), os.path.join( os.path.dirname( __file__ ), '..', '..', 'tests', 'intro-biology-rendered-book' ) )
+		path = self.get_path(('..', '..', 'tests', 'intro-biology-rendered-book'))
+		book = NoPhantomRenderedBook( EmptyMockDocument(), path )
 		res = mathjaxerror.check( book )
 		assert_that( res, is_( 3 ) )
 
 	def test_check_mathjax2(self):
-		book = NoPhantomRenderedBook( EmptyMockDocument(), os.path.join( os.path.dirname( __file__ ), 'book-with-mathjaxerror' ) )
+		path = self.get_path(('book-with-mathjaxerror', ))
+		book = NoPhantomRenderedBook( EmptyMockDocument(), path )
 		res = mathjaxerror.check( book )
 		assert_that( res, is_( 24 ) )
 
