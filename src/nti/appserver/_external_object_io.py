801381e4dd002be3ef6ea58e5e19a78b0a41ba22
Jason Madden <jason.madden@nextthought.com>
2012-10-31 13:39:43 +0000

    After review, merge first round of refactoring into trunk
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13607 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/_external_object_io.py
+++ src/nti/appserver/_external_object_io.py
@@ -27,8 +27,41 @@ from nti.appserver import httpexceptions as hexc
 from nti.appserver._util import raise_json_error
 
 from nti.dataserver import interfaces as nti_interfaces
+from nti.dataserver.mimetype import nti_mimetype_class
 import nti.externalization.internalization
 
+
+def create_modeled_content_object( dataserver, owner, datatype, externalValue, creator ):
+	"""
+	:param owner: The entity which will contain the object.
+	:param creator: The user attempting to create the object. Possibly separate from the
+		owner. Permissions will be checked for the creator
+	"""
+	# The datatype can legit be null if we are MimeType-only
+	if externalValue is None:
+		return None
+
+	result = None
+	if datatype is not None and owner is not None:
+		result = owner.maybeCreateContainedObjectWithType( datatype, externalValue )
+
+	if result is None:
+		result = nti.externalization.internalization.find_factory_for( externalValue )
+		if result:
+			result = result()
+
+	return result
+
+
+
+def class_name_from_content_type( request ):
+	"""
+	:return: The class name portion of one of our content-types, or None
+		if the content-type doesn't conform. Note that this will be lowercase.
+	"""
+	content_type = request.content_type if hasattr( request, 'content_type' ) else request
+	content_type = content_type or ''
+	return nti_mimetype_class( content_type )
 
 
 def read_body_as_external_object( request, input_data=None, expected_type=collections.Mapping ):
