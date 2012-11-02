b3fc2a07f8360f692b83c97089e7805c97d96916
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-02 01:41:06 +0000

    start using zcml for settings
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13665 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/spambayes/__init__.py
+++ src/nti/contentsearch/spambayes/__init__.py
@@ -2,20 +2,14 @@ from __future__ import print_function, unicode_literals
 
 import math
 
+from zope import component
+
+from nti.contentsearch.spambayes import interfaces as sps_interfaces
+
 LN2 = math.log(2)
 
 default_hist_nbuckets = 200
 default_hist_percentiles = (5, 25, 75, 95)
-
-default_ham_cutoff = 0.20
-default_spam_cutoff = 0.90
-
-default_use_bigrams = False
-default_unknown_word_prob = 0.5
-default_arc_discriminators = 50
-default_max_discriminators = 150
-default_unknown_word_strength = 0.45
-default_minimum_prob_strength = 0.10
 
 PERSISTENT_HAM_INT = 1
 PERSISTENT_SPAM_INT = 2
@@ -30,12 +24,5 @@ def is_ham(disposition):
 def is_unsure(disposition=None):
 	return disposition is None or disposition == PERSISTENT_UNSURE_INT
 
-def classify(prob, ham_cutoff=default_ham_cutoff, spam_cutoff=default_spam_cutoff):
-	if prob < ham_cutoff:
-		disposition = PERSISTENT_HAM_INT
-	elif prob > spam_cutoff:
-		disposition = PERSISTENT_SPAM_INT
-	else:
-		disposition = PERSISTENT_UNSURE_INT
-	return disposition
-		
+def classify(prob):
+	return component.getUtility(sps_interfaces.IProbabilityClassifier)(prob)
