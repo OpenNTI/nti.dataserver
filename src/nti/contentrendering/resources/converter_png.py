OpenNTI/nti.dataserver

commit 9a0cbf48e6a0f01f706dafc8690ef60ba4a8673f

some versions of pngcrush will not produce an output file. This gets arround a 0 byte file png files I have seen when rendering content
git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13531 3dfb626b-079f-4ab1-a9f6-4654e5377508

src/nti/contentrendering/resources/converter_png.py
@@ -47,12 +47,15 @@ def _scale(input, output, scale, defaultScale):
 	#return os.system( 'convert %s -resize %f%% PNG32:%s' % (input, 100*(scale/defaultScale) , output) )
 	command = [ 'convert', input, '-resize', '%f%%' % (100*(scale/defaultScale)), 'PNG32:%s' % output ]
 	retval1 = subprocess.call( command )
+	
 	# Use pngcrush to clean the resized PNG.
-	tmpfile = tempfile.mkstemp( suffix = '.png' )
-	os.close(tmpfile[0])
-	command = [ 'pngcrush', '-q', output, tmpfile[1] ]
+	prefix = hex(int(time.time()))
+	tmpfile = tempfile.mktemp(suffix='.png', prefix=prefix)
+	command = [ 'pngcrush', '-q', output, tmpfile ]
 	retval2 = subprocess.call( command )
-	shutil.move( tmpfile[1], output )
+	if os.path.exists(tmpfile):
+		shutil.move( tmpfile, output )
+		
 	return retval1 if (retval1 < retval2) else retval2
 
 class _GSPDFPNG2(plasTeX.Imagers.gspdfpng.GSPDFPNG):

commit 498e9439b9f09a7ee5c2ea6eb703db2f26962181

Fix the image roulette bug created by temp file collisions introduced in r13531.  This was mostly done by shifting to the race-free tempfile.mkstemp() instead of tempfile.mktemp().
git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13722 3dfb626b-079f-4ab1-a9f6-4654e5377508

src/nti/contentrendering/resources/converter_png.py
@@ -50,8 +50,8 @@ def _scale(input, output, scale, defaultScale):
 	retval1 = subprocess.call( command )
 
 	# Use pngcrush to clean the resized PNG.
-	prefix = hex(int(time.time()))
-	tmpfile = tempfile.mktemp(suffix='.png', prefix=prefix)
+	prefix = hex(int(time.time()*1000))
+	tmpfile = tempfile.mkstemp(suffix='.png', prefix=prefix)[1]
 	command = [ 'pngcrush', '-q', output, tmpfile ]
 	__traceback_info__ = command
 	retval2 = subprocess.call( command )