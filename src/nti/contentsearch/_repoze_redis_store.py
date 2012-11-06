5fc2494b551a6e981074be748a6a8654afbbf035
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-06 22:17:55 +0000

    individual operation in transaction runner. This is for testing.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@13786 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_repoze_redis_store.py
+++ src/nti/contentsearch/_repoze_redis_store.py
@@ -4,6 +4,8 @@ import gevent
 import random
 import collections
 
+from transaction.interfaces import TransientError
+
 from zope import component
 from zope import interface
 from ZODB import loglevels
@@ -42,57 +44,38 @@ class _RepozeRedisStorageService(_RedisStorageService):
 				self._push_back_msgs(msg_list, encode=True)		
 				logger.exception("Error while processing index message for %s" % username)
 			
-	def _process_message(self, username, msg):
+	def _process_message(self, msg):
 		trxrunner = component.getUtility(nti_interfaces.IDataserverTransactionRunner)
 		def f():
+			op, oid, username = msg
 			entity = Entity.get_entity(username)
 			im = search_interfaces.IRepozeEntityIndexManager(entity, None)
 			if im is None:
 				logger.log(self.logging_level, "Cannot adapt to repoze index manager for entity %s", username)
-				logger.debug("Cannot adapt to repoze index manager for entity %s" % username)
 				return	
+			intids = component.getUtility( zc_intid.IIntIds )
+			
+			data = intids.queryObject(int(oid), None)
+			if op in ('add', 'update'):
+				if data is not None:
+					if op == 'add':
+						im.index_content(data)
+						notify(search_interfaces.IndexEvent(entity, data, search_interfaces.IE_INDEXED))
+					else:
+						im.update_content(data)
+						notify(search_interfaces.IndexEvent(entity, data, search_interfaces.IE_REINDEXED))
+				else:
+					s = 'Could not find object %s' % oid
+					raise TransientError(s)
+			elif op == 'delete':
+				im.unindex_doc(oid)
+				notify(search_interfaces.IndexEvent(entity, data or oid, search_interfaces.IE_UNINDEXED))
+		try:	
+			trxrunner(f, retries=5, sleep=0.1)
+		except TransientError:
+			pass
 			
 	def _process_user_messages(self, username, msg_list):
+		for m in msg_list:
+			self._process_message(m)
 		
-		trxrunner = component.getUtility(nti_interfaces.IDataserverTransactionRunner)
-		def f():
-			entity = Entity.get_entity(username)
-			im = search_interfaces.IRepozeEntityIndexManager(entity, None)
-			if im is None:
-				logger.debug("Cannot adapt to repoze index manager for entity %s" % username)
-				return
-			
-			idx = 0
-			retries = 0
-			intids = component.getUtility( zc_intid.IIntIds )
-			while idx < len(msg_list):
-				advance = True
-				op, oid, _ = msg_list[idx]
-				data = intids.queryObject(int(oid), None)
-				if op in ('add', 'update'):
-					if data is not None:
-						if op == 'add':
-							im.index_content(data)
-							notify(search_interfaces.IndexEvent(entity, data, search_interfaces.IE_INDEXED))
-						else:
-							im.update_content(data)
-							notify(search_interfaces.IndexEvent(entity, data, search_interfaces.IE_REINDEXED))
-					else:
-						retries += 1  
-						if retries <= 5:
-							# sometimes we need to wait to make sure db commit has happened
-							# this should go away when we handle index events as zope events
-							advance = False
-							logger.log(self.logging_level, 'Could not find object %s. Retry %s', oid, retries)
-							gevent.sleep(0.5) 
-						else:
-							logger.log(self.logging_level, 'Cannot find object with id %s', oid)
-				elif op == 'delete':
-					im.unindex_doc(oid)
-					notify(search_interfaces.IndexEvent(entity, data or oid, search_interfaces.IE_UNINDEXED))
-					
-				if advance:
-					idx += 1
-					retries = 0
-					
-		trxrunner(f, retries=5, sleep=0.1)
