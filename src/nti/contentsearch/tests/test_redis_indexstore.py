4b99501a8212f33fb9b7aec690c6d2b416fdb01e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-07 05:50:56 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13793 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentsearch/tests/test_redis_indexstore.py
@@ -0,0 +1,27 @@
+import unittest
+
+from nti.contentsearch._redis_indexstore import sort_messages
+from nti.contentsearch._redis_indexstore import (ADD_OPERATION, UPDATE_OPERATION, DELETE_OPERATION)
+
+from hamcrest import (assert_that, is_)
+
+class TestRedisIndexStore(unittest.TestCase):
+
+	def test_sort_messages(self):
+		msgs = [(DELETE_OPERATION, 1, 'a'), (ADD_OPERATION, 1, 'a'), (UPDATE_OPERATION, 1, 'a')]
+		sorted_list = sort_messages(msgs)
+		exp = [(ADD_OPERATION, 1, 'a'), (UPDATE_OPERATION, 1, 'a'), (DELETE_OPERATION, 1, 'a')]
+		assert_that(sorted_list, is_(exp))
+		
+		msgs = [(DELETE_OPERATION, 1, 'z'), (ADD_OPERATION, 1, 'x'), (UPDATE_OPERATION, 1, 'y')]
+		sorted_list = sort_messages(msgs)
+		exp = [(ADD_OPERATION, 1, 'x'), (UPDATE_OPERATION, 1, 'y'), (DELETE_OPERATION, 1, 'z')]
+		assert_that(sorted_list, is_(exp))
+		
+		msgs = [(DELETE_OPERATION, 5, 'a'), (ADD_OPERATION, 2, 'a'), (UPDATE_OPERATION, 7, 'a')]
+		sorted_list = sort_messages(msgs)
+		exp = [(ADD_OPERATION, 2, 'a'), (DELETE_OPERATION, 5, 'a'), (UPDATE_OPERATION, 7, 'a')]
+		assert_that(sorted_list, is_(exp))
+	
+if __name__ == '__main__':
+	unittest.main()
