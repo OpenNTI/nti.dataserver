1e571a66ecd518f0a520c7f950cc37097cb82132
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-12 22:19:12 +0000

    create a script to print the email info for users that have the opt_in_communations flag
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@13925 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_opt_in_communication.py
+++ src/nti/dataserver/utils/nti_opt_in_communication.py
@@ -4,54 +4,46 @@ from __future__ import print_function, unicode_literals
 
 import sys
 import argparse
-from datetime import datetime
 
 from zope import component
 
 from nti.dataserver import users
 from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver import interfaces as nti_interfaces
+from nti.dataserver.users import interfaces as user_interfaces
 
 def main():
-	arg_parser = argparse.ArgumentParser( description="Get the current active sessions" )
+	arg_parser = argparse.ArgumentParser( description="Return the users that have the opt_in_email_communication set" )
 	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
 	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
 	args = arg_parser.parse_args()
 
 	env_dir = args.env_dir
 	verbose = args.verbose
-	run_with_dataserver( environment_dir=env_dir, function=lambda: _get_active_sessions(verbose) )
+	run_with_dataserver( environment_dir=env_dir, function=lambda: _get_user_info(verbose) )
 	sys.exit( 0 )
 
-
-def _format_time(ts):
-	result = datetime.fromtimestamp(ts)
-	result = result.strftime("%Y-%m-%d %H:%M:%S")
-	return result
-		
-def _get_active_sessions(verbose=False):
+def _get_user_info(verbose=False):
 	
 	dataserver = component.getUtility( nti_interfaces.IDataserver)
 	_users = nti_interfaces.IShardLayout( dataserver ).users_folder
 	usernames = _users.iterkeys()
-		
-	sss = component.getUtility( nti_interfaces.ISessionServiceStorage)
+
 	for username in usernames:
 		user = users.User.get_user( username )
 		if not user:
 			continue
 		
-		sessions = list(sss.get_sessions_by_owner(user)) 
-		if sessions:
+		profile = user_interfaces.ICompleteUserProfile(user, None)
+		if profile is None and profile.opt_in_email_communication:
+			lst = None
 			if not verbose:
-				print(user, len(sessions))
+				lst = [username, profile.email]
 			else:
-				for s in sessions:
-					ct = _format_time(s.creation_time)
-					lht = _format_time(s.last_heartbeat_time)
-					print('%s\t%s\t%s\t%s\t%s' % (user.username, s.id, s.state, ct, lht))
-				
+				lst = [username, profile.email, profile.home_page, profile.description]
 			
+			print('\t'.join(lst))
+				
 if __name__ == '__main__':
 	main()
 	
