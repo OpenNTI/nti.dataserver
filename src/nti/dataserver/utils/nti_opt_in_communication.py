6ea7bb81164da069f2631882955e78a16ba1f621
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-14 21:47:12 +0000

    add a debug option for greg
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@14014 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_opt_in_communication.py
+++ src/nti/dataserver/utils/nti_opt_in_communication.py
@@ -10,22 +10,35 @@ from zope.catalog.interfaces import ICatalog
 
 from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver.users import index as user_index
+from nti.dataserver import interfaces as nti_interfaces
 from nti.dataserver.users import interfaces as user_interfaces
 
 def main():
 	arg_parser = argparse.ArgumentParser( description="Return the users that have the opt_in_email_communication set" )
 	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
+	arg_parser.add_argument( '-d', '--debug', help="Don't use entity index", action='store_true', dest='debug')
+	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
 	args = arg_parser.parse_args()
 
 	env_dir = args.env_dir
-	run_with_dataserver( environment_dir=env_dir, function=lambda: _get_user_info() )
+	verbose = args.verbose
+	useindex = not args.debug
+	run_with_dataserver( environment_dir=env_dir, function=lambda: _get_user_info(useindex, verbose) )
 	sys.exit( 0 )
 
-def _get_user_info():
+def _get_user_info(useindex=False, verbose=False):
 	ent_catalog = component.getUtility(ICatalog, name=user_index.CATALOG_NAME)
-	for user in list(ent_catalog.searchResults( topics='opt_in_email_communication')):
+	
+	if not useindex:
+		dataserver = component.getUtility( nti_interfaces.IDataserver)
+		_users = nti_interfaces.IShardLayout( dataserver ).users_folder
+		users = _users.values()
+	else:
+		users = ent_catalog.searchResults( topics='opt_in_email_communication')
+		
+	for user in users:
 		profile = user_interfaces.ICompleteUserProfile(user, None)
-		if profile is not None:
+		if profile is not None and (useindex or profile.opt_in_email_communication):
 			print('\t'.join((user.username, profile.email or u'')))
 		
 if __name__ == '__main__':
