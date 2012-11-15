995dfff30c6dbc68d96b1dd3691a8104ad00987b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-15 22:08:10 +0000

    final change to index chats
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/branches/meeting_catalog@14030 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/utils/nti_index_chats.py
+++ src/nti/contentsearch/utils/nti_index_chats.py
@@ -13,29 +13,20 @@ from nti.chatserver import interfaces as chat_interfaces
 from nti.dataserver.users import Entity
 from nti.dataserver.utils import run_with_dataserver
 
+import nti.contentsearch
 from nti.contentsearch import interfaces as search_interfaces
-
-def main():
-	arg_parser = argparse.ArgumentParser( description="Return the users that have the opt_in_email_communication set" )
-	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
-	arg_parser.add_argument( 'created', help="Meeting creation date in ISO format (e.g. 2012-11-15)" )
-	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
-	args = arg_parser.parse_args()
-
-	env_dir = args.env_dir
-	verbose = args.verbose
-	created = args.created
-	run_with_dataserver( environment_dir=env_dir, function=lambda: _index_meetings(created, verbose) )
-	sys.exit( 0 )
 
 def _index_meeting(user, roomid, verbose=False):
 	count = 0
 	users = set()
+	messages = []
+	
 	storage = chat_interfaces.IUserTranscriptStorage(user, None)
+	rim = search_interfaces.IRepozeEntityIndexManager(user, None)
 	transcript = storage.transcript_for_meeting(roomid) if storage is not None else None
-	if transcript is not None:
+	if transcript is not None and rim is not None:
 		for m in transcript.Messages:
-			rim = search_interfaces.IRepozeEntityIndexManager(user, None)
+			messages.append(m)
 			if rim is not None:
 				rim.index_content(m)
 				count += 1 
@@ -45,15 +36,15 @@ def _index_meeting(user, roomid, verbose=False):
 		users.remove(user.username)
 	
 	for username in users:
-		user = Entity.get_entity(username)
-		if user is not None:
-			rim = search_interfaces.IRepozeEntityIndexManager(user, None)
-			if rim is not None:
+		e = Entity.get_entity(username)
+		rim = search_interfaces.IRepozeEntityIndexManager(e, None) if e is not None else None
+		if rim is not None:
+			for m in messages:
 				rim.index_content(m)
 				count += 1 
 			
 	if verbose:
-		print('\t\t%s records were indexed' % count)
+		print('\t%s records were indexed' % count)
 		
 	return users
 
@@ -71,7 +62,22 @@ def _index_meetings(created, verbose=False):
 		user = Entity.get_entity(m.creator)
 		if user is not None:
 			_index_meeting(user, m.RoomId, verbose)
-				
+			
+def main():
+	arg_parser = argparse.ArgumentParser( description="Return the users that have the opt_in_email_communication set" )
+	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
+	arg_parser.add_argument( 'created', help="Meeting creation date in ISO format (e.g. 2012-11-15)" )
+	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
+	args = arg_parser.parse_args()
+
+	env_dir = args.env_dir
+	verbose = args.verbose
+	created = args.created
+	run_with_dataserver( environment_dir=env_dir,
+						 xmlconfig_packages=(nti.contentsearch,),
+						 function=lambda: _index_meetings(created, verbose) )
+	sys.exit( 0 )
+	
 if __name__ == '__main__':
 	main()
 	
