50ef78cd75f0f396851efd9e3731c6fbe40b4fcd
Jason Madden <jason.madden@nextthought.com>
2012-11-16 17:58:36 +0000

    Drop the now-uneeded monkey patch.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@14087 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/nti_pserve.py
+++ src/nti/appserver/nti_pserve.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 A hack to help us ensure that we are loading and monkey-patching
-the entire system before pyramid loads: loading pyramid's pserve
-loads many pyramid modules, including pyramid.traversal, which
-in turn loads repoze.lru and allocates real, non-recursive
+the entire system before Pyramid loads: loading Pyramid's ``pserve``
+loads many Pyramid modules, including :mod:`pyramid.traversal`, which
+in turn loads :mod:`repoze.lru` and allocates real, non-recursive
 thread locks. These are not compatible with gevent and eventually
 lead to a hang if we re-enter a greenlet that wants to acquire one
 of these locks while a previous greenlet already has it.
@@ -18,13 +18,16 @@ __docformat__ = "restructuredtext en"
 from nti.monkey import gevent_patch_on_import
 gevent_patch_on_import.patch()
 
-from nti.monkey import pyramid_excview_tween_iface_patch
-pyramid_excview_tween_iface_patch.patch()
-
 import sys
-from pkg_resources import load_entry_point
+from pkg_resources import load_entry_point, get_distribution
 
 def main():
-    sys.exit(
-        load_entry_point('pyramid', 'console_scripts', 'pserve')()
-    )
+	# We used to monkey patch some things in 1.3. We no longer
+	# do now that we expect to be on 1.4. Check this.
+	pyramid_dist = get_distribution( 'pyramid' )
+	if pyramid_dist and pyramid_dist.has_version():
+		assert pyramid_dist.version.startswith( '1.4' )
+
+	sys.exit(
+		load_entry_point('pyramid', 'console_scripts', 'pserve')()
+	)
