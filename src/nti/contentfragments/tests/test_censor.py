0ab36de22974c2a203d3b2a1e454caa3ddccbe1d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-16 22:11:25 +0000

    add untested regexp content scanner
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@14103 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentfragments/tests/test_censor.py
+++ src/nti/contentfragments/tests/test_censor.py
@@ -18,12 +18,9 @@ from zope.schema.fieldproperty import FieldPropertyStoredThroughField
 from pkg_resources import resource_filename
 
 import nti.contentfragments
-import nti.contentfragments.schema
-import nti.contentfragments.interfaces
-from nti.contentfragments.censor import WordMatchScanner
-from nti.contentfragments.censor import WordPlusTrivialMatchScanner
-from nti.contentfragments.censor import DefaultCensoredContentPolicy
-from nti.contentfragments.censor import SimpleReplacementCensoredContentStrategy
+from nti.contentfragments import censor as frag_censor
+from nti.contentfragments import schema as frag_schema
+from nti.contentfragments import interfaces as frag_interfaces
 
 import nti.tests
 from hamcrest import is_
@@ -32,16 +29,16 @@ from hamcrest import assert_that
 setUpModule = lambda: nti.tests.module_setup( set_up_packages=(nti.contentfragments,) )
 
 def test_defaults():
-	scanner = component.getUtility( nti.contentfragments.interfaces.ICensoredContentScanner )
-	strat = component.getUtility( nti.contentfragments.interfaces.ICensoredContentStrategy )
+	scanner = component.getUtility( frag_interfaces.ICensoredContentScanner )
+	strat = component.getUtility( frag_interfaces.ICensoredContentStrategy )
 
 	bad_val = 'Guvf vf shpxvat fghcvq, lbh ZbgureShpxre onfgneq'.encode( 'rot13' )
 	assert_that( strat.censor_ranges( bad_val, scanner.scan( bad_val ) ),
 				 is_( 'This is ******* stupid, you ************ *******' ) )
 	
 def test_mike_words():
-	scanner = component.getUtility( nti.contentfragments.interfaces.ICensoredContentScanner )
-	strat = component.getUtility( nti.contentfragments.interfaces.ICensoredContentStrategy )
+	scanner = component.getUtility( frag_interfaces.ICensoredContentScanner )
+	strat = component.getUtility( frag_interfaces.ICensoredContentStrategy )
 
 	bad_val = 'ubefrfuvg'.encode( 'rot13' )
 	assert_that( strat.censor_ranges( bad_val, scanner.scan( bad_val ) ),
@@ -55,32 +52,31 @@ def test_mike_words():
 	assert_that( strat.censor_ranges( bad_val, scanner.scan( bad_val ) ),
 				 is_( 'associated press' ) )
 	
-	
 def test_kaley_words():
-	scanner = component.getUtility( nti.contentfragments.interfaces.ICensoredContentScanner )
-	strat = component.getUtility( nti.contentfragments.interfaces.ICensoredContentStrategy )
+	scanner = component.getUtility( frag_interfaces.ICensoredContentScanner )
+	strat = component.getUtility( frag_interfaces.ICensoredContentStrategy )
 	vals = "Manuscript, proclamation, grasses, endorsement, passes, hoarse  safeguard, farther, seashore shore ashore, glasses"
 	assert_that( strat.censor_ranges( vals, scanner.scan( vals ) ),
 				 is_( vals ) )
 	
 def test_word_match_scanner():
-	wm = WordMatchScanner((), ['lost','like'])
+	wm = frag_censor.WordMatchScanner((), ['lost','like'])
 	bad_val = """So I feel a little like, a child who's lost, a little like, (everything's changed) a
 			  lot, I didn't like all of the pain"""
 
 	ranges = list(wm.scan(bad_val))
 	assert_that(ranges, is_([(39, 43), (19, 23), (54, 58), (104, 108)]))
 	
-	wm = WordMatchScanner((), ['thought'])
+	wm = frag_censor.WordMatchScanner((), ['thought'])
 	ranges = list(wm.scan(bad_val))
 	assert_that(ranges, is_([]))
 	
-	wm = WordMatchScanner(('lost',), ['lost','like'])
+	wm = frag_censor.WordMatchScanner(('lost',), ['lost','like'])
 	ranges = list(wm.scan(bad_val))
 	assert_that(ranges, is_([(19, 23), (54, 58), (104, 108)]))
 	
 	bad_val = "So I am a rock on!!! forever"
-	wm = WordMatchScanner((), ['rock on','apple'])
+	wm = frag_censor.WordMatchScanner((), ['rock on','apple'])
 	ranges = list(wm.scan(bad_val))
 	assert_that(ranges, is_([(10,17)]))
 	bad_val = "So I am a rock one and that is it"
@@ -88,12 +84,12 @@ def test_word_match_scanner():
 	assert_that(ranges, is_([]))
 	
 	bad_val = "buck bill gates"
-	wm = WordMatchScanner((), ['buck'])
+	wm = frag_censor.WordMatchScanner((), ['buck'])
 	ranges = list(wm.scan(bad_val))
 	assert_that(ranges, is_([(0,4)]))
 	
 	bad_val = "buck bill gates"
-	wm = WordMatchScanner((), ['gates'])
+	wm = frag_censor.WordMatchScanner((), ['gates'])
 	ranges = list(wm.scan(bad_val))
 	assert_that(ranges, is_([(10,15)]))
 	
@@ -102,8 +98,8 @@ def test_trivial_and_word_match_scanner():
 	profanity_file = resource_filename( __name__, '../profanity_list.txt' )
 	profanity_list = [x.encode('rot13').strip() for x in open(profanity_file, 'rU').readlines()]
 
-	strat = SimpleReplacementCensoredContentStrategy()	
-	scanner = WordPlusTrivialMatchScanner((), ('stupid',), profanity_list)
+	strat = frag_censor.SimpleReplacementCensoredContentStrategy()	
+	scanner = frag_censor.WordPlusTrivialMatchScanner((), ('stupid',), profanity_list)
 
 	bad_val = 'Guvf vf shpxvat fghcvq, lbh ZbgureShpxre onfgneq'.encode( 'rot13' )
 	assert_that( strat.censor_ranges( bad_val, scanner.scan( bad_val ) ),
@@ -113,33 +109,37 @@ def test_trivial_and_word_match_scanner():
 	assert_that( strat.censor_ranges( bad_val, scanner.scan( bad_val ) ),
 				 is_( 'butter constitution computation' ) )
 	
+#def test_regexp_match_scanner():
+#	profanity_file = resource_filename( __name__, '../profanity_2_regexp.txt' )
+#	strat = frag_censor.RegExpMatchScannerExternalFile(profanity_file)	
+#	
 def test_html_and_default_policy():
-	policy = DefaultCensoredContentPolicy()
+	policy = frag_censor.DefaultCensoredContentPolicy()
 	template = '<html><head/><body><b>%s</b></body></html>'
 	for w in ('shpx', 'penc'):
 		bad_val = template % w.encode( 'rot13' )
-		bad_val =  nti.contentfragments.interfaces.HTMLContentFragment(bad_val)
+		bad_val =  frag_interfaces.HTMLContentFragment(bad_val)
 		assert_that(policy.censor(bad_val, None),
 				 	is_( '<html><head/><body><b>****</b></body></html>' ) )
 	
 def test_schema_event_censoring():
 
 	class ICensored(interface.Interface):
-		body = nti.contentfragments.schema.TextUnicodeContentFragment( title="Body" )
+		body = frag_schema.TextUnicodeContentFragment( title="Body" )
 
 	@interface.implementer(ICensored)
 	class Censored(object):
 		body = FieldPropertyStoredThroughField(ICensored['body'])
 
-	component.provideAdapter( nti.contentfragments.censor.DefaultCensoredContentPolicy,
+	component.provideAdapter( frag_censor.DefaultCensoredContentPolicy,
 							  adapts=(unicode,ICensored),
-							  provides=nti.contentfragments.interfaces.ICensoredContentPolicy,
+							  provides=frag_interfaces.ICensoredContentPolicy,
 							  name=Censored.body.field.__name__ )
 
 	censored = Censored()
 
 	bad_val = 'Guvf vf shpxvat fghcvq, lbh ZbgureShpxre onfgneq'.encode( 'rot13' )
-	bad_val = nti.contentfragments.interfaces.UnicodeContentFragment( bad_val )
+	bad_val = frag_interfaces.UnicodeContentFragment( bad_val )
 
 	censored.body = bad_val
 
@@ -148,4 +148,4 @@ def test_schema_event_censoring():
 	
 if __name__ == '__main__':
 	nti.tests.module_setup( set_up_packages=(nti.contentfragments,) )
-	test_html_and_default_policy()
+	test_regexp_match_scanner()
