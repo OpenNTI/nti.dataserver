837c9191db1f764d4704e65d3ddcd122ae59b693
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-17 00:16:17 +0000

    we would like to derive the fields to  update from a schema
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@14104 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/dataserver/utils/nti_update_object.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, unicode_literals
+
+import sys
+import argparse
+from pprint import pprint
+
+from zope import schema
+from zope import interface
+
+from nti.chatserver import interfaces as chat_interfaces
+
+from nti.contentfragments import schema as frg_schema
+
+from nti.dataserver import users
+from nti.dataserver.utils import run_with_dataserver
+from nti.dataserver import interfaces as nti_interfaces
+from nti.utils.schema import find_most_derived_interface
+from nti.dataserver.users import interfaces as user_interfaces
+
+from nti.externalization import interfaces as ext_interfaces
+from nti.externalization.externalization import to_external_object
+from nti.externalization.internalization import update_from_external_object
+
+known_imodeled = (nti_interfaces.INote, nti_interfaces.IHighlight, nti_interfaces.IRedaction, nti_interfaces.ICanvas, 
+				  chat_interfaces.IMessageInfo)
+
+forbidden_fields = set([v for k,v in ext_interfaces.StandardExternalFields.__dict__.iteritems() if not k.startswith( '_' ) and k !='ALL'])
+forbidden_int_fields = set([v for k,v in ext_interfaces.StandardInternalFields.__dict__.iteritems() if not k.startswith( '_' )])
+forbidden_fields = tuple(forbidden_fields.union(forbidden_int_fields)) + ('flattenedSharingTargetNames', 'sharedWith')
+
+threadable_fields = {'inReplyTo' : frg_schema.TextUnicodeContentFragment(title="The object to which this object is directly a reply"),
+					 'references': schema.List( value_type=frg_schema.TextUnicodeContentFragment(title="a reference id"),
+												title="A sequence of ids this object transiently references.")}
+
+def get_schema_fields(iface, attributes):		
+	names = iface.names()
+	fields = schema.getFields(iface) or {}
+	for name in names or ():
+		sch_def = fields.get(name, None)
+		if sch_def and name not in attributes and name not in forbidden_fields and \
+		   not name.startswith( '_' ) and not name[0].isupper():
+			attributes[name] = sch_def
+	
+	for base in iface.getBases() or ():
+		get_schema_fields(base, attributes)
+			
+def get_imodeled_schemas():
+	result = {}
+	for iface in known_imodeled:
+		attributes = {}
+		if nti_interfaces.IThreadable in iface.getBases():
+			attributes.update(threadable_fields)
+		get_schema_fields(iface, attributes)
+		result[iface] = attributes
+	return result
+
+def _create_args_parser():
+	imodeled_schemas = get_imodeled_schemas()
+	pprint(imodeled_schemas)
+	arg_parser = argparse.ArgumentParser( description="Set object attributes." )
+	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
+	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
+	return arg_parser
+	
+def main():
+	arg_parser = _create_args_parser()
+	args = arg_parser.parse_args()
+	#run_with_dataserver( environment_dir=args.env_dir, 
+	#					 function=lambda: _change_attributes(args) )
+
+if __name__ == '__main__':
+	main()
