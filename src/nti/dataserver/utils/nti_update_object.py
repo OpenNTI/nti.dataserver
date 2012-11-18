eddebe39c11f80b4d2878e594427d09ffc899f75
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-18 22:07:29 +0000

    more work on update script.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@14120 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_update_object.py
+++ src/nti/dataserver/utils/nti_update_object.py
@@ -2,92 +2,109 @@
 
 from __future__ import print_function, unicode_literals
 
-import sys
+import os
+import six
 import argparse
+import anyjson as json
 from pprint import pprint
-
-from zope import schema
-from zope import interface
-
-from nti.chatserver import interfaces as chat_interfaces
-
-from nti.contentfragments import schema as frg_schema
 
 from nti.dataserver import users
 from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver import interfaces as nti_interfaces
-from nti.utils.schema import find_most_derived_interface
-from nti.dataserver.users import interfaces as user_interfaces
 
 from nti.externalization import interfaces as ext_interfaces
 from nti.externalization.externalization import to_external_object
 from nti.externalization.internalization import update_from_external_object
 
-known_imodeled = (nti_interfaces.INote, nti_interfaces.IHighlight, nti_interfaces.IRedaction, nti_interfaces.ICanvas, 
-				  chat_interfaces.IMessageInfo)
+from nti.ntiids import ntiids
+
+import logging
+logger = logging.getLogger( __name__ )
 
 forbidden_fields = set([v for k,v in ext_interfaces.StandardExternalFields.__dict__.iteritems() if not k.startswith( '_' ) and k !='ALL'])
 forbidden_int_fields = set([v for k,v in ext_interfaces.StandardInternalFields.__dict__.iteritems() if not k.startswith( '_' )])
-forbidden_fields = tuple(forbidden_fields.union(forbidden_int_fields)) + ('flattenedSharingTargetNames', 'sharedWith')
-
-threadable_fields = {'inReplyTo' : frg_schema.TextUnicodeContentFragment(title="The object to which this object is directly a reply"),
-					 'references': schema.List( value_type=frg_schema.TextUnicodeContentFragment(title="a reference id"),
-												title="A sequence of ids this object transiently references.")}
-
-def get_schema_fields(iface, attributes):		
-	names = iface.names()
-	fields = schema.getFields(iface) or {}
-	for name in names or ():
-		sch_def = fields.get(name, None)
-		if sch_def and name not in attributes and name not in forbidden_fields and \
-		   not name.startswith( '_' ) and not name[0].isupper():
-			attributes[name] = sch_def
-	
-	for base in iface.getBases() or ():
-		get_schema_fields(base, attributes)
-			
-def get_imodeled_schemas():
-	result = {}
-	all_fields = {}
-	for iface in known_imodeled:
-		attributes = {}
-		if nti_interfaces.IThreadable in iface.getBases():
-			attributes.update(threadable_fields)
-		get_schema_fields(iface, attributes)
-		all_fields.update(attributes)
-		result[iface] = attributes
-	return result, all_fields
+forbidden_fields = tuple(forbidden_fields.union(forbidden_int_fields)) + ('flattenedSharingTargetNames', 'sharedWith', 'body')
 
 def _create_args_parser():
-	
-	# parse model content schemas
-	imodeled_schemas, all_fields = get_imodeled_schemas()
-		
 	arg_parser = argparse.ArgumentParser( description="Set object attributes." )
 	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
-	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
-	arg_parser.add_argument( '--cascade', help="Cascade operation on threadable objects", action='store_true', dest='cascade')
+	arg_parser.add_argument( 'id', help="Object's OID or NTIID" )
+	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')		
+	arg_parser.add_argument( '-j', '--json', dest='json', help="JSON expression" )
+	arg_parser.add_argument( '-i', '--input', dest='input', help="JSON input file" )
+	arg_parser.add_argument( '-f', '--fields', dest='fields', nargs="*", help="Key:value pairs" )
+	arg_parser.add_argument( '--cascade', help="Cascade operation on threadable objects", action='store_true', dest='cascade')	
+	return arg_parser
 	
-	# add params
-	group = arg_parser.add_mutually_exclusive_group()
-	group.add_argument('-nid', '--ntiid', dest='ntiid', help="Object NTIID" )
-	group.add_argument('-oid', '--oid', dest='oid', help="Object ID (hex)")
+def _get_ntiid(arg):
+	try:
+		oid = int( arg, 0 )
+		ntiid = ntiids.make_ntiid( type=ntiids.TYPE_OID, specific=oid )
+	except ValueError:
+		ntiid = arg
+		ntiids.validate_ntiid_string( ntiid )
+	return ntiid
 
-	for name, sch in all_fields.items():
-		help_ = sch.getDoc() or sch.title
-		help_ = help_.replace('\n', '.')
-		opt = '--%s' % name
-		arg_parser.add_argument( opt, help=help_, dest=name, required=False)
-			
-	arg_parser.add_argument( '--fields', dest='fields', nargs="*", help="Key:value pairs" )
+def _find_object(ntiid):
+	obj = ntiid.find_object_with_ntiid(ntiid)
+	if obj is None:
+		raise Exception("Cannot find object with NTIID '%s'" % ntiid)
+	elif nti_interfaces.IModeledContent.providedBy(obj):
+		raise Exception("Object referenced by '%s' does not implement IModeledContent interface" % ntiid)
+	return obj
+
+def _get_external_object(args):
+	result = {}
 	
-	return arg_parser, imodeled_schemas
+	# process ant json in args
+	if args.json:
+		result.update(json.dumps(args.json))
 	
+	# process an json input file
+	if args.input:
+		path = os.path.expanduser(args.input)
+		with open(path, "r") as f:
+			src = f.read()
+		result.update(json.dumps(src))
+
+	# process any key/value pairs
+	for f in args.fields or ():
+		p = f.split('=')
+		if p and len(p) >=2:
+			result[p[0]] = p[1]
+	
+	for k in result.keys():
+		if k in forbidden_fields:
+			raise Exception('Cannot set prohibited key %s' % k)
+	return result
+
+def _get_creator(obj):
+	result = getattr(obj, ext_interfaces.StandardInternalFields.CREATOR)
+	if isinstance(result, six.string_types):
+		result = users.Entity.get_entity(result)
+	return result
+	
+def _update_object(creator, obj, ext_object, verbose=False):
+	objId = obj.id
+	containerId = obj.containerId
+	with creator.updates():
+		obj = creator.getContainedObject( containerId, objId )
+		obj = update_from_external_object(obj, ext_object)
+		if verbose:
+			pprint(to_external_object(obj))
+
+def _process(args):
+	ntiid = _get_ntiid(args.id)
+	modeled_obj = _find_object(ntiid)
+	ext_obj = _get_external_object(args)
+	creator = _get_creator(modeled_obj)
+	modeled_obj = _update_object(creator, modeled_obj, ext_obj)
+	return modeled_obj
+
 def main():
 	arg_parser = _create_args_parser()
 	args = arg_parser.parse_args()
-	#run_with_dataserver( environment_dir=args.env_dir, 
-	#					 function=lambda: _change_attributes(args) )
+	run_with_dataserver( environment_dir=args.env_dir, function=lambda: _process(args) )
 
 if __name__ == '__main__':
 	main()
