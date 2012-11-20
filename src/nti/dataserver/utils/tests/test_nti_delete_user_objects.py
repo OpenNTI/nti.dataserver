52c1b0921349fc6e6eba2696ae90b781ac29fc04
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-20 16:41:11 +0000

    add a test script for deleteing user objects by type. Also, make sure we use getattr to get inReplyTo and references fields from an object
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@14166 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/dataserver/utils/tests/test_nti_delete_user_objects.py
@@ -0,0 +1,51 @@
+import unittest
+
+import nti.dataserver
+from nti.dataserver.users import User
+from nti.dataserver.contenttypes import Note
+from nti.dataserver.utils import nti_delete_user_objects as nti_delete
+
+from nti.ntiids.ntiids import make_ntiid
+
+import nti.dataserver.tests.mock_dataserver as mock_dataserver
+from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
+
+import nti.contentsearch
+
+from nti.tests import ConfiguringTestBase
+
+from hamcrest import (assert_that, is_, has_entry)
+
+class TestNTIDeleteUserObjects(ConfiguringTestBase):
+	
+	set_up_packages = (nti.dataserver,nti.contentsearch)
+			
+	def _create_user(self, username='nt@nti.com', password='temp001'):
+		ds = mock_dataserver.current_mock_ds
+		usr = User.create_user( ds, username=username, password=password)
+		return usr
+		
+	def create_note(self, text, user, containerId=None, inReplyTo=None, references=()):
+		note = Note()
+		note.body = [text]
+		note.creator = user
+		note.setInReplyTo(inReplyTo)
+		for r in references or ():
+			note.addReference(r)
+		note.containerId = containerId or make_ntiid(nttype='bleach', specific='manga')
+		mock_dataserver.current_transaction.add(note)
+		note = user.addContainedObject( note ) 	
+		return note
+
+	@WithMockDSTrans
+	def test_simple_delete(self):
+		usr = self._create_user()
+		self.create_note('my note', usr)
+		cmap, _ = nti_delete.delete_entity_objects(usr)
+		assert_that(cmap, has_entry('note', is_(1)))
+
+		
+if __name__ == '__main__':
+	unittest.main()
+	
+		
