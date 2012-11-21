67a4024f258fa4a8152b44d07bdf88410b2055f7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-21 03:47:45 +0000

    add test for join community
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@14187 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_join_community.py
+++ src/nti/dataserver/utils/nti_join_community.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-$Id$
-"""
+
 from __future__ import print_function, unicode_literals
 
 import sys
+import argparse
 
 from nti.dataserver import users
+from nti.dataserver.utils import run_with_dataserver
 from nti.externalization.externalization import to_external_object
 
-from . import run_with_dataserver
-import argparse
+def join_communities(user, communities=(), follow=False, exitOnError=False):
+	not_found = set()
+	for com_name in communities:
+		comm = users.Community.get_entity( com_name )
+		if not comm:
+			not_found.add(com_name)
+			if exitOnError: break
+		user.record_dynamic_membership( comm )
+		if follow:
+			user.follow( comm )
+		
+	return tuple(not_found)
+
+def _process_args( args ):
+	user = users.User.get_user( args.username )
+	if not user:
+		print( "No user found", args, file=sys.stderr )
+		sys.exit( 2 )
+
+	not_found = join_communities(user, args.communities, args.follow, True)
+	if not_found:
+		print( "No community found", args, file=sys.stderr )
+		sys.exit( 3 )
+	
+	if args.verbose:
+		print( to_external_object( user ) )
 
 def main():
 	arg_parser = argparse.ArgumentParser( description="Join one or more existing communities" )
@@ -26,25 +49,5 @@ def main():
 	args = arg_parser.parse_args()
 
 	env_dir = args.env_dir
+	run_with_dataserver( environment_dir=env_dir, function=lambda: _process_args(args) )
 
-	run_with_dataserver( environment_dir=env_dir, function=lambda: _join_community(args) )
-
-
-def _join_community( args ):
-	user = users.User.get_user( args.username )
-	if not user:
-		print( "No user found", args, file=sys.stderr )
-		sys.exit( 2 )
-
-	for com_name in args.communities:
-		comm = users.Community.get_entity( com_name )
-		if not comm:
-			print( "No community found", args, file=sys.stderr )
-			sys.exit( 3 )
-		user.join_community( comm )
-		if args.follow:
-			user.follow( comm )
-
-	if args.verbose:
-		print( to_external_object( user ) )
-	return user
