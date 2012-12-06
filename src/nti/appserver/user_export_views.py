f748b9e2b183a8abdf393c35245bbaf7e0fbe30f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-12-06 04:17:48 +0000

    I tried to use app_iter, but I needed for the db connection to stay open while the request is processed. So in the mean time use respose.body_file. Wonder if I can use IDataserverTransactionRunner
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@14457 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/user_export_views.py
+++ src/nti/appserver/user_export_views.py
@@ -1,5 +1,7 @@
 from __future__ import print_function, unicode_literals
 
+from cStringIO import StringIO
+
 from pyramid.view import view_config
 
 import zope.intid
@@ -24,31 +26,37 @@ def _get_field_info(userid, ent_catalog, indexname):
 	result = rev_index.get(userid, u'')
 	return result
 
+def _get_user_info_extract():
+	_ds_intid = component.getUtility( zope.intid.IIntIds )
+	ent_catalog = component.getUtility(ICatalog, name=user_index.CATALOG_NAME)
+	userids = _get_userids(ent_catalog)
+	
+	header = ['username', 'realname', 'alias', 'email']
+	yield ','.join(header).encode('utf-8')
+	
+	for iid in userids:
+		u = _ds_intid.queryObject(iid, None)
+		if u is not None:
+			alias = _get_field_info(iid, ent_catalog, 'alias')
+			email = _get_field_info(iid, ent_catalog, 'email')
+			realname = _get_field_info(iid, ent_catalog, 'realname')
+			yield ','.join([u.username, realname, alias, email]).encode('utf-8')
+				
 @view_config(route_name='objects.generic.traversal',
 			 name='user_info_extract',
 			 request_method='GET',
 			 permission=nauth.ACT_MODERATE)
 def user_info_extract(request):
 	
-	_ds_intid = component.getUtility( zope.intid.IIntIds )
-	ent_catalog = component.getUtility(ICatalog, name=user_index.CATALOG_NAME)
-	userids = _get_userids(ent_catalog)
-
-	header = ['username', 'realname', 'alias', 'email']
-	def f():
-		yield ','.join(header).encode('utf-8')
-		for iid in userids:
-			u = _ds_intid.queryObject(iid, None)
-			if u is not None:
-				alias = _get_field_info(iid, ent_catalog, 'alias')
-				email = _get_field_info(iid, ent_catalog, 'email')
-				realname = _get_field_info(iid, ent_catalog, 'realname')
-				yield ','.join([u.username, realname, alias, email]).encode('utf-8')
-		
+	# write to buffer
+	sio = StringIO()
+	for line in _get_user_info_extract():
+		sio.write(line)
+		sio.write("\n")
+	sio.seek(0)
+	
 	response = request.response
 	response.content_type = b'text/csv; charset=UTF-8'
 	response.content_disposition = b'attachment; filename="report.csv"'
-	response.app_iter = f()
-	
+	response.body_file = sio
 	return response
-
