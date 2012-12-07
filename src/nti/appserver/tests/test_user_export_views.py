8c5c5824d57f676b01fca75f71ef294bccf62a0d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-12-07 15:38:26 +0000

    set a new view to get the users that have opted in email communications
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@14505 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/test_user_export_views.py
+++ src/nti/appserver/tests/test_user_export_views.py
@@ -2,6 +2,10 @@ from __future__ import print_function
 
 import unittest
 
+from zope import interface
+
+from nti.dataserver import interfaces as nti_interfaces
+
 from webtest import TestApp
 
 from nti.dataserver.tests import mock_dataserver
@@ -34,6 +38,36 @@ class TestApplicationUserExporViews(SharedApplicationTestBase):
 		assert_that(app_iter, has_length(4) )
 		for t in app_iter:
 			assert_that(t.split(','), has_length(4))
+			
+	@WithSharedApplicationMockDS
+	def test_opt_in_comm(self):
+		with mock_dataserver.mock_db_trans( self.ds ):
+			u = self._create_user(	external_value={u'email':u"nti@nt.com", u'opt_in_email_communication':True})
+			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithAgreementUpgraded)
+			
+			u = self._create_user(	username='rukia@nt.com', 
+									external_value={u'email':u'rukia@nt.com', u'opt_in_email_communication':True})
+			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithAgreementUpgraded)
+			
+			u = self._create_user(	username='ichigo@nt.com', 
+									external_value={u'email':u'ichigo@nt.com', u'opt_in_email_communication':True})
+			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithAgreementUpgraded)
+			
+		testapp = TestApp( self.app )
+
+		path = '/dataserver2/@@user_opt_in_comm'
+		environ = self._make_extra_environ()
+		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
+		
+		res = testapp.get( path, extra_environ=environ )
+		assert_that( res.status_int, is_( 200 ) )
+		app_iter = res.app_iter[0].split('\n')[:-1]
+		assert_that(app_iter, has_length(4) )
+		for idx, t in enumerate(app_iter):
+			split = t.split(',')
+			assert_that(split, has_length(6))
+			if idx > 0:
+				assert_that(split[-1], is_('True'))
 
 if __name__ == '__main__':
 	unittest.main()
