0cd00e0a2b46aef3d7b8d81bc54f480395f9315b
Sean Jones <sean.jones@nextthought.com>
2012-12-07 21:47:34 +0000

    Add module to support the concept of "slides." Support for multiple slide decks in a presentation via ntislidedeck and indexable slide content via ntislidetext is incomplete.  The test suite covers the ntislide and ntislidevideo environments.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@14528 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/tests/test_ntislidedeck.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" """
+from __future__ import print_function, unicode_literals
+
+from hamcrest import assert_that, is_, has_length
+from hamcrest import has_property
+from hamcrest import instance_of
+from hamcrest import equal_to
+import unittest
+
+import plasTeX
+from plasTeX.Base.LaTeX.Crossref import label
+
+from nti.contentrendering.tests import buildDomFromString as _buildDomFromString
+from nti.contentrendering.tests import simpleLatexDocumentText
+
+import nti.contentrendering
+from nti.contentrendering.plastexpackages.ntilatexmacros import ntiincludevideo
+from nti.contentrendering.plastexpackages.ntislidedeck import ntislide, ntislidevideo
+
+def _simpleLatexDocument(maths):
+    return simpleLatexDocumentText( preludes=(br'\usepackage{nti.contentrendering.plastexpackages.ntilatexmacros}',
+                                              br'\usepackage{nti.contentrendering.plastexpackages.ntislidedeck}'),
+                                    bodies=maths )
+
+def test_ntislidevideo():
+    example = br"""
+\begin{ntislidevideo}
+\label{video1}
+\ntiincludevideo{//www.youtube.com/embed/goKHhz9RfGo?html5=1&rel=0}
+\end{ntislidevideo}
+"""
+    dom = _buildDomFromString( _simpleLatexDocument( (example,) ) )
+
+    # Check that the DOM has the expected structure
+    assert_that( dom.getElementsByTagName('ntislidevideo'), has_length( 1 ) )
+    assert_that( dom.getElementsByTagName('ntislidevideo')[0], is_( ntislidevideo ) )
+
+    # Check that the ntislidevideo object has the expected children
+    elem = dom.getElementsByTagName('ntislidevideo')[0]
+    assert_that( elem.childNodes, has_length( 5 ) )
+    assert_that( elem.childNodes[1], instance_of( label ) )
+    assert_that( elem.childNodes[3], instance_of( ntiincludevideo ) )
+
+    # Check that the ntislidevido object has the expected attributes
+    assert_that( elem, has_property( 'id' ) )
+    assert_that( elem, has_property( 'type' ) )
+    assert_that( elem, has_property( 'provider_id' ) )
+    assert_that( elem, has_property( 'thumbnail' ) )
+    assert_that( elem, has_property( 'video_url' ) )
+
+    # Check that the attributes have the expected values
+    assert_that( elem.id, equal_to( elem.childNodes[3].id ) )
+    assert_that( elem.type, equal_to( elem.childNodes[3].attributes['service'] ) )
+    assert_that( elem.provider_id, equal_to( elem.childNodes[3].attributes['video_id'] ) )
+    assert_that( elem.thumbnail, equal_to( elem.childNodes[3].attributes['thumbnail'] ) )
+    assert_that( elem.video_url, equal_to( elem.childNodes[3].attributes['video_url'] ) )
+
+def test_ntislide():
+    example = br"""
+\begin{ntislidevideo}
+\label{video1}
+\ntiincludevideo{//www.youtube.com/embed/goKHhz9RfGo?html5=1&rel=0}
+\end{ntislidevideo}
+
+\begin{ntislide}
+\label{slide1}
+\ntislidetitle{Test Slide}
+\ntislideimage[width=640px]{test}
+\ntislidevideoref[start=0:00,end=1:25]{video1}
+\begin{ntislidetext}
+Title Slide
+\end{ntislidetext}
+\end{ntislide}
+"""
+    dom = _buildDomFromString( _simpleLatexDocument( (example,) ) )
+
+    # Check that the DOM has the expected structure
+    assert_that( dom.getElementsByTagName('ntislidevideo'), has_length( 1 ) )
+    assert_that( dom.getElementsByTagName('ntislidevideo')[0], is_( ntislidevideo ) )
+    assert_that( dom.getElementsByTagName('ntislide'), has_length( 1 ) )
+    assert_that( dom.getElementsByTagName('ntislide')[0], is_( ntislide ) )
+
+    # Check that the ntislidevideo object has the expected children
+    elem = dom.getElementsByTagName('ntislide')[0]
+    assert_that( elem.childNodes, has_length( 11 ) )
+    assert_that( elem.childNodes[1], instance_of( label ) )
+    assert_that( elem.childNodes[3], instance_of( ntislide.ntislidetitle ) )
+    assert_that( elem.childNodes[5], instance_of( ntislide.ntislideimage ) )
+    assert_that( elem.childNodes[7], instance_of( ntislide.ntislidevideoref ) )
+    assert_that( elem.childNodes[9], instance_of( ntislide.ntislidetext ) )
+
+    # Check that the ntislidevido object has the expected attributes
+    assert_that( elem, has_property( 'id' ) )
+    assert_that( elem, has_property( 'title' ) )
+    assert_that( elem, has_property( 'slidenumber' ) )
+    assert_that( elem, has_property( 'slideimage' ) )
+    assert_that( elem, has_property( 'slidevideo' ) )
+    assert_that( elem, has_property( 'slidevideostart' ) )
+    assert_that( elem, has_property( 'slidevideoend' ) )
+
+    # Check that the attributes have the expected values
+    assert_that( elem.title, equal_to( elem.childNodes[3].attributes['title'] ) )
+    assert_that( elem.slidenumber, equal_to( elem.ownerDocument.context.counters[elem.counter].value ) )
+    assert_that( elem.slideimage, equal_to( elem.childNodes[5] ) )
+    assert_that( elem.slidevideo, equal_to( elem.childNodes[7].idref['label'] ) )
+    assert_that( elem.slidevideostart, equal_to( elem.childNodes[7].attributes['options']['start'] ) )
+    assert_that( elem.slidevideoend, equal_to( elem.childNodes[7].attributes['options']['end'] ) )
+
