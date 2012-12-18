d32945ce9fdb00b82eba3ca328c275acbafdafa0
Jason Madden <jason.madden@nextthought.com>
2012-12-18 15:03:20 +0000

    Migration to clean up all extant broken dynamic relationships.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@14717 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/install.py
+++ src/nti/dataserver/generations/install.py
@@ -6,7 +6,7 @@ from __future__ import print_function, unicode_literals
 
 __docformat__ = 'restructuredtext'
 
-generation = 35
+generation = 36
 
 from zope.generations.generations import SchemaManager
 
