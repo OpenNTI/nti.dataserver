dd2f806d1be924b42c6e3e465cf0db0e5d12a47a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-12-31 18:14:17 +0000

    add unit test (skipped)  for alchemy keyword extract
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@14844 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/keyword/tests/test_termextract.py
+++ src/nti/contentprocessing/keyword/tests/test_termextract.py
@@ -2,26 +2,39 @@ import os
 import unittest
 			
 from nti.contentprocessing.keyword import extract_key_words
-		
+from nti.contentprocessing.keyword._alchemy import _AlchemyAPIKeyWorExtractor
+
 from nti.contentprocessing.tests import ConfiguringTestBase
 		
-from hamcrest import assert_that, is_
+from hamcrest import (assert_that, is_, has_length)
 
-class TestTermExtract(ConfiguringTestBase):
+class TestKeyWordExtract(ConfiguringTestBase):
 
-	def test_index_content(self):
+	@classmethod
+	def setUpClass(cls):		
 		name = os.path.join(os.path.dirname(__file__), 'sample.txt')
 		with open(name, "r") as f:
-			content = f.read()
+			cls.sample_content = f.read()
 			
-		terms = extract_key_words(content)
-		terms = [(r.norm, r.occur, r.strength) for r in terms]
+	def test_term_extract(self):
+		terms = extract_key_words(self.sample_content)
+		terms = [(r.token, r.frequency, r.strength) for r in terms]
 		assert_that(terms, is_([('blood', 4, 1),
 								('virus', 3, 1),
 								('blood vessel', 1, 2), 
 								('blood cells', 1, 2),
 								('body works', 1, 2),
 								('blood cells viruses', 1, 3)]))
+		
+	@unittest.SkipTest
+	def test_alchemy_extract(self):
+		terms = _AlchemyAPIKeyWorExtractor()(self.sample_content)
+		terms = [(r.token, r.relevance) for r in terms]
+		assert_that(terms, has_length(15))
+		assert_that(terms[0], is_((u'blood cells', 0.998273)))
+		assert_that(terms[1], is_((u'knobby green objects', 0.80723)))
+		assert_that(terms[2], is_((u'viruses', 0.7604)))
+		assert_that(terms[3], is_((u'red blood cells', 0.732536)))
 
 if __name__ == '__main__':
 	unittest.main()
