f66af6fb826d56c48e477b25c259d7efe23479c5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-12-31 22:47:47 +0000

    there is a payload limit.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@14848 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/keyword/_alchemy.py
+++ src/nti/contentprocessing/keyword/_alchemy.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals
 
+import sys
 import requests
 
 from zope import interface
@@ -16,18 +17,24 @@ class _AlchemyAPIKeyWorExtractor():
 	#TODO: Get NT API Key
 	apikey = u'afe98c5b8fb8586e930d1b2128386d40c136e6d3'
 	url = u'http://access.alchemyapi.com/calls/text/TextGetRankedKeywords'
+	limit_kb = 150
 	
 	def __call__(self, content, **kwargs):
-		headers = {u'content-type': u'application/x-www-form-urlencoded'}
-		params = {u'text':unicode(content), u'apikey':self.apikey, u'outputMode':u'json'}
-		params.update(kwargs)
-		r = requests.post(self.url, params=params, headers=headers)
-		data = r.json
-		
-		if r.status_code ==200 and data.get('status','ERROR') == 'OK':
-			keywords = data.get('keywords', ())
-			result = [ContentKeyWord(d['text'], float(d.get('relevance', 0))) for d in keywords]
-		else:
-			result = ()
+		result = ()	
+		content = content or u''
+		size_kb = sys.getsizeof(content)/1024.0
+		if size_kb <= self.limit_kb:
+			headers = {u'content-type': u'application/x-www-form-urlencoded'}
+			params = {u'text':unicode(content), u'apikey':self.apikey, u'outputMode':u'json'}
+			params.update(kwargs)
+			try:
+				r = requests.post(self.url, params=params, headers=headers)
+				data = r.json
 				
+				if r.status_code ==200 and data.get('status','ERROR') == 'OK':
+					keywords = data.get('keywords', ())
+					result = [ContentKeyWord(d['text'], float(d.get('relevance', 0))) for d in keywords]
+			except:
+				logging.exception('Error while getting keywords from Alchemy')
+							
 		return result
