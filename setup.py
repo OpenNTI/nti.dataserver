bb23891402456ef4ecc46743837f9dfcf7881efc
Jason Madden <jason.madden@nextthought.com>
2013-01-03 15:22:15 +0000

    Bump pytz
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@14860 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -208,7 +208,7 @@ setup(
 		 # statsd server implementation, pure python. probably easier than setting up node. Might want to get it from https://github.com/sivy/py-statsd
 		 # Consider also https://github.com/phensley/gstatsd
 		'pystatsd >= 0.1.6',
-		'pytz >= 2012h',
+		'pytz >= 2012j',
 		'rdflib >= 3.2.3',
 		# Redis python client. Note that Amazon deployed servers are still in the 2.6 (2.4?) series
 		'redis >= 2.7.2',
