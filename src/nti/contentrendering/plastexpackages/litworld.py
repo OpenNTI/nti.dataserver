0a4f303c30dcbd0ef69f30220e5c5bb4a44438d3
Sean Jones <sean.jones@nextthought.com>
2013-01-11 17:20:27 +0000

    Add support package and templates for LitWorld content
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@15010 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/litworld.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2.7
+#pylint: disable=R0904
+
+from plasTeX import Base
+
+class litclubsession(Base.chapter):
+    pass
+
+class litclubsessionsection(Base.section):
+    pass
+
+class litclubwelcomesection(Base.subsection):
+    pass
+
+class litclubhellosongsubsection(Base.subsubsection):
+    pass
+
+class litclubcheckinsubsection(Base.subsubsection):
+    pass
+
+class litclubcommunitysection(Base.subsection):
+    pass
+
+class litclubreadaloudsection(Base.subsection):
+    pass
+
+class litclubdiscussionsubsection(Base.subsubsection):
+    pass
+
+class litclubcoresection(Base.subsection):
+    pass
+
+class litclubindereadingsection(Base.subsection):
+    pass
+
+class litclubwrapupsection(Base.subsection):
+    pass
+
+class litclubpraisesubsection(Base.subsubsection):
+    pass
+
+class litclubgoodbyesongsubsection(Base.subsubsection):
+    pass
+
