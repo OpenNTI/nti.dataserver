8ed02a72911810680f676983f4c9d75da3c472e9
Jason Madden <jason.madden@nextthought.com>
2013-01-15 14:57:11 +0000

    Correct encoding problem that could lead to spurious test failures.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@15068 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/tests/test_default_root_sharing_setter.py
+++ src/nti/contentrendering/tests/test_default_root_sharing_setter.py
@@ -1,19 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# $Id$
+
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+#disable: accessing protected members, too many methods
+#pylint: disable=W0212,R0904
+
+
+from hamcrest import assert_that
+from hamcrest import is_
+from hamcrest import has_property
+from hamcrest import not_none
+
 from . import ConfiguringTestBase
 from nti.contentrendering import RenderedBook
 from nti.contentrendering import default_root_sharing_setter
-from nti.tests import provides
+from nti.tests import verifiably_provides
 from nti.contentrendering import interfaces
-from nti.contentrendering.utils import EmptyMockDocument, NoConcurrentPhantomRenderedBook
+from nti.contentrendering.utils import NoConcurrentPhantomRenderedBook
 
 import io
 import os
-import shutil
-from hamcrest import assert_that, has_entry, has_item, has_property, has_length, greater_than_or_equal_to, is_, is_not, none
+
+
 
 TEST_CONTENT = 'NextThoughtGenericTutorial-rendered-book'
 
+def _resource( name ):
+	return os.path.join( os.path.dirname( __file__ ), name )
+
 def test_module_provides():
-	assert_that( default_root_sharing_setter, provides(interfaces.IRenderedBookTransformer ) )
+	assert_that( default_root_sharing_setter, verifiably_provides(interfaces.IRenderedBookTransformer ) )
 
 class TestTransforms(ConfiguringTestBase):
 
@@ -24,22 +45,19 @@ class TestTransforms(ConfiguringTestBase):
 		"""
 
 		# Read the reference data
-		refData = ''
-		with io.open(os.path.join( os.path.dirname( __file__ ), 'nti-default-root-sharing-group.txt' ) ) as file:
-			for line in file.readlines():
-                                refData = ' '.join([refData, line.strip()])
-			refData = refData.strip()
+		lines = io.open( _resource( 'nti-default-root-sharing-group.txt' ), encoding='utf-8' ).readlines()
+		refData = ' '.join( (line.strip() for line in lines) ).strip()
 
 		# Open the copy of the rendered book
-		book = NoConcurrentPhantomRenderedBook( None, os.path.join( os.path.dirname( __file__ ), TEST_CONTENT ) )
+		book = NoConcurrentPhantomRenderedBook( None, _resource( TEST_CONTENT ) )
 
 		# Assert ToC is present
-		assert_that( book, has_property( 'toc', is_not( none() ) ) )
+		assert_that( book, has_property( 'toc', not_none() ) )
 		# Assert that the root topic is present
-		assert_that( book.toc, has_property( 'root_topic', is_not( none() ) ) )
-		
+		assert_that( book.toc, has_property( 'root_topic', not_none() ) )
+
 		default_root_sharing_setter.transform( book, save_toc=False )
-			
+
 		# Assert that the shareWith property of the ToC root element is set correctly
 		assert_that( book.toc.root_topic.get_default_sharing_group(), is_( refData ) )
 
@@ -53,14 +71,14 @@ class TestTransforms(ConfiguringTestBase):
 		refData = 'entity0 entity1 entity2 entity3'
 
 		# Open the copy of the rendered book
-		book = RenderedBook.RenderedBook( None, os.path.join( os.path.dirname( __file__ ), TEST_CONTENT ) )
+		book = RenderedBook.RenderedBook( None, _resource( TEST_CONTENT ) )
 
 		# Assert ToC is present
-		assert_that( book, has_property( 'toc', is_not( none() ) ) )
+		assert_that( book, has_property( 'toc', not_none() ) )
 		# Assert that the root topic is present
-		assert_that( book.toc, has_property( 'root_topic', is_not( none() ) ) )
-		
+		assert_that( book.toc, has_property( 'root_topic', not_none() ) )
+
 		default_root_sharing_setter.transform( book, save_toc=False, group_name=refData )
-			
+
 		# Assert that the shareWith property of the ToC root element is set correctly
 		assert_that( book.toc.root_topic.get_default_sharing_group(), is_( refData ) )
