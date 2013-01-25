a7617835d29403d79c0074e889f921086c080ae5
Sean Jones <sean.jones@nextthought.com>
2013-01-25 19:33:23 +0000

    Add a basic in content 'glossary'
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@15353 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/ntiglossary.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from __future__ import print_function, unicode_literals
+
+from plasTeX import Command, Environment, DimenCommand
+from plasTeX.Logging import getLogger
+
+
+# SAJ: The logic of the glossary entry is derived from that of the footnote.
+class ntiglossaryentry(Command):
+    args = 'entryname self'
+    mark = None
+
+    def invoke(self, tex):
+        # Add the glossary entry to the document
+        output = Command.invoke(self, tex)
+        userdata = self.ownerDocument.userdata
+        if 'glossary' not in userdata:
+            userdata['glossary'] = []
+        userdata['glossary'].append(self)
+        self.mark = self
+        return output
+
