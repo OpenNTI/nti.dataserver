a7ed42cf366d92ffe312bdb7e1c153ed5d55b1ea
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-01-26 06:40:47 +0000

    fix compilation errors
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@15371 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/qti/response/interfaces.py
+++ src/nti/assessment/qti/response/interfaces.py
@@ -4,6 +4,7 @@ from zope import schema
 from zope import interface
 from zope.interface.common.sequence import IFiniteSequence
 
+from nti.assessment.qti.basic import interfaces as basic_interfaces
 from nti.assessment.qti.outcome import interfaces as out_interfaces
 from nti.assessment.qti.expression import interfaces as exp_interfaces
 from nti.assessment.qti.attributes import interfaces as attr_interfaces
@@ -13,39 +14,32 @@ from nti.assessment.qti.attributes import interfaces as attr_interfaces
 class IresponseRule(interface.Interface):
 	pass
 	
-class IresponseProcessing(attr_interfaces.IresponseProcessingAttrGroup, IFiniteSequence):
-	__display_name__ = "responseProcessing"
-	value = schema.List(IresponseRule, title="An ordered list of values", min_length=1)
+class IresponseProcessing(attr_interfaces.IresponseProcessingAttrGroup, IFiniteSequence, basic_interfaces.IConcrete):
+	value = schema.List(schema.Object(IresponseRule), title="An ordered list of values", min_length=1)
 
-class IresponseIf(IresponseRule):
-	__display_name__ = "responseIf"
+class IresponseIf(IresponseRule, basic_interfaces.IConcrete):
 	expression = schema.Object(exp_interfaces.Iexpression, title='The expression', required=True)
-	responseRule = schema.List(IresponseRule, title='The ordered response rules', required=False, min_length=0)
+	responseRule = schema.List(schema.Object(IresponseRule), title='The ordered response rules', required=False, min_length=0)
 	
-class IresponseElseIf(IresponseRule):
-	__display_name__ = "responseElseIf"
+class IresponseElseIf(IresponseRule, basic_interfaces.IConcrete):
 	expression = schema.Object(exp_interfaces.Iexpression, title='The expression', required=True)
-	responseRule = schema.List(IresponseRule, title='The ordered response rules', required=False, min_length=0)
+	responseRule = schema.List(schema.Object(IresponseRule), title='The ordered response rules', required=False, min_length=0)
 	
-class IresponseElse(IresponseRule):
-	__display_name__ = "responseElse"
-	responseRule = schema.List(IresponseRule, title='The ordered response rules', required=False, min_length=0)
+class IresponseElse(IresponseRule, basic_interfaces.IConcrete):
+	responseRule = schema.List(schema.Object(IresponseRule), title='The ordered response rules', required=False, min_length=0)
 	
-class IresponseCondition(IresponseRule):
-	__display_name__ = "responseCondition"
+class IresponseCondition(IresponseRule, basic_interfaces.IConcrete):
 	responseIf = schema.Object(IresponseIf, title='response if', required=True)
-	responseElseIf = schema.List(IresponseElseIf, title='response if list', required=False, min_length=0)
+	responseElseIf = schema.List(schema.Object(IresponseElseIf), title='response if list', required=False, min_length=0)
 	responseElse = schema.Object(IresponseElse, title='response else', required=False)
 
-class IsetOutcomeValue(out_interfaces.IoutcomeRule, IresponseRule, attr_interfaces.IsetOutcomeValueAttrGroup):
-	__display_name__ = "setOutcomeValue"
+class IsetOutcomeValue(out_interfaces.IoutcomeRule, IresponseRule, attr_interfaces.IsetOutcomeValueAttrGroup, basic_interfaces.IConcrete):
 	expression = schema.Object(exp_interfaces.Iexpression, title='The expression', required=True)
 
-class IlookupOutcomeValue(out_interfaces.IoutcomeRule, IresponseRule, attr_interfaces.IlookupOutcomeValueAttrGroup):
-	__display_name__ = "lookupOutcomeValue"
+class IlookupOutcomeValue(out_interfaces.IoutcomeRule, IresponseRule, attr_interfaces.IlookupOutcomeValueAttrGroup, basic_interfaces.IConcrete):
 	expression = schema.Object(exp_interfaces.Iexpression, title='A single cardinality expression', required=True)
 	
-class IexitResponse(IresponseRule):
-	__display_name__ = "exitResponse"
+class IexitResponse(IresponseRule, basic_interfaces.IConcrete):
+	pass
 
 	
