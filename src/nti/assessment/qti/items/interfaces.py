d5a01822e4746609cb35131ea7e32c0d673054ef
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-01-28 22:07:35 +0000

    add qti fragments
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@15410 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/qti/items/interfaces.py
+++ src/nti/assessment/qti/items/interfaces.py
@@ -1,9 +1,24 @@
 from __future__ import unicode_literals, print_function
 
+from zope import schema
 
 from nti.assessment.qti.basic import interfaces as basic_interfaces
+from nti.assessment.qti.content import interfaces as cnt_interfaces
+from nti.assessment.qti.response import interfaces as rsp_interfaces
+from nti.assessment.qti.template import interfaces as tmp_interfaces
+from nti.assessment.qti.feedback import interfaces as feed_interfaces
+from nti.assessment.qti.variables import interfaces as var_interfaces
 from nti.assessment.qti.attributes import interfaces as attr_interfaces
 
 class IitemSessionControl(attr_interfaces.IitemSessionControlAttrGroup, basic_interfaces.IConcrete):
 	pass
 
+class IassessmentItem(attr_interfaces.IassessmentItemAttrGroup, basic_interfaces.IConcrete):
+	responseDeclaration = schema.List(schema.Object(var_interfaces.IresponseDeclaration), min_lenght=0, required=True)
+	outcomeDeclaration = schema.List(schema.Object(var_interfaces.IoutcomeDeclaration), min_lenght=0, required=True)
+	templateDeclaration = schema.List(schema.Object(tmp_interfaces.ItemplateDeclaration), min_lenght=0, required=True)
+	templateProcessing = schema.Object(tmp_interfaces.ItemplateProcessing, required=False)
+	stylesheet = schema.List(schema.Object(cnt_interfaces.Istylesheet), min_lenght=0, title='Ordered list', required=False)
+	itemBody = schema.Object(cnt_interfaces.IitemBody, required=False)
+	responseProcessing  = schema.Object(rsp_interfaces.IresponseProcessing, required=False)
+	modalFeedback = schema.List(schema.Object(feed_interfaces.ImodalFeedback), min_lenght=0, title='Ordered list', required=False)
