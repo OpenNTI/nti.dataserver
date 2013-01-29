d0261dc76b23e65aec8c6fdef6ad790704e871b7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-01-29 23:46:53 +0000

    no need to use load_module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@15451 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/qti/__init__.py
+++ src/nti/assessment/qti/__init__.py
@@ -2,8 +2,6 @@ from __future__ import print_function, unicode_literals, absolute_import
 
 import os
 import sys
-import types
-from imp import find_module, load_module, acquire_lock, release_lock
 
 from zope import interface
 	
@@ -18,22 +16,14 @@ def find_concrete_interfaces():
 	from nti.assessment.qti import interfaces as qti_interfaces
 	
 	src_path = os.path.split(qti_interfaces.__file__)[0]
-	path_length = len(src_path)-len(qti_interfaces.__package__)
+	package = getattr(qti_interfaces, '__package__')
+	path_length = len(src_path)-len(package)
 	def _load_module(path, name):
 		part = path[path_length:]
 		part = part.replace(os.sep, '.') + '.' + name
 		if part in sys.modules:
 			return sys.modules[part]
-		
-		fh = None
-		try:
-			acquire_lock()
-			fh, filename, desc = find_module(name, [path])
-			result = load_module(part, fh, filename, desc)
-			return result
-		finally:
-			if fh: fh.close()
-			release_lock()
+		return __import__(part)
 			
 	def _get_concrete_ifaces(m):
 		for name in dir(m):
@@ -58,5 +48,6 @@ def find_concrete_interfaces():
 	return result
 
 if __name__ == '__main__':
-	find_concrete_interfaces()
+	import pprint
+	pprint.pprint(find_concrete_interfaces())
 
