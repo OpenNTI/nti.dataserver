81c7213180def447648d8c99e86ac1bfbc3851c9
Jason Madden <jason.madden@nextthought.com>
2013-01-30 18:11:48 +0000

    The one-line form of getting a logger is preferred because it doesn't pollute the samespace. Also restore missing absolute_import imports
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@15469 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentfragments/urlmatcher.py
+++ src/nti/contentfragments/urlmatcher.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
 
-from __future__ import print_function, unicode_literals
+
+$Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
 
 import re
 import six
@@ -12,9 +19,6 @@ from html5lib import treebuilders
 
 from zope import interface
 from nti.contentfragments import interfaces
-
-import logging
-logger = logging.getLogger(__name__)
 
 @interface.implementer(interfaces.IHyperlinkFormatter)
 class GrubberHyperlinkFormatter(object):
