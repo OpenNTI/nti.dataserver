dd7c6cd1bd59b49b3d7f74c6bd1fb32e26189a56
Jason Madden <jason.madden@nextthought.com>
2013-02-01 22:21:55 +0000

    Working on the forking issues. Refactor the gevent and relstorage patches to make them more reusable.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@15549 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/nti_pserve.py
+++ src/nti/appserver/nti_pserve.py
@@ -15,6 +15,7 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
+# Note that we must not import *anything* before the patch
 from nti.monkey import gevent_patch_on_import
 gevent_patch_on_import.patch()
 
