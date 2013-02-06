2c4c63e49e75bdc54c97db24cd251ab5134343af
Jason Madden <jason.madden@nextthought.com>
2013-02-06 16:31:48 +0000

    Add a nose plugin for getting timing information on individual tests with --with-timer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@15637 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary/tests/test_filesystem.py
+++ src/nti/contentlibrary/tests/test_filesystem.py
@@ -10,9 +10,9 @@ from hamcrest import is_
 from hamcrest import none
 from hamcrest import has_property
 from hamcrest import has_entry
-from nti.tests import verifiably_provides, ConfiguringTestBase
+from nti.tests import verifiably_provides, SharedConfiguringTestBase
 
-from nti.contentlibrary import interfaces, eclipse, filesystem
+from nti.contentlibrary import interfaces, filesystem
 import nti.contentlibrary
 import nti.externalization
 import nti.externalization.externalization
@@ -20,9 +20,9 @@ import nti.externalization.externalization
 import anyjson as json
 import os.path
 
-class TestFilesystemContentUnit(ConfiguringTestBase):
+class TestFilesystemContentUnit(SharedConfiguringTestBase):
 
-	set_up_packages = (nti.contentlibrary, nti.externalization)
+	set_up_packages = (nti.contentlibrary, nti.externalization, 'nti.appserver')
 
 	def test_filesystem_content_interfaces(self):
 
@@ -37,10 +37,7 @@ class TestFilesystemContentUnit(ConfiguringTestBase):
 
 	def test_adapter_prefs(self):
 		import zope.dottedname.resolve as dottedname
-		appserver = dottedname.resolve( 'nti.appserver' )
 		IPrefs = dottedname.resolve( 'nti.appserver.interfaces.IContentUnitPreferences' )
-
-		self.configure_packages( set_up_packages=(appserver,) )
 
 		unit = filesystem.FilesystemContentPackage(
 			filename='prealgebra/index.html',
@@ -59,7 +56,7 @@ class TestFilesystemContentUnit(ConfiguringTestBase):
 		package = filesystem._package_factory( os.path.join( os.path.dirname( __file__ ), 'TestFilesystem' ) )
 		assert_that( package.creators, is_( ('Jason',) ) )
 
-		self.configure_packages( set_up_packages=(nti.externalization,nti.contentlibrary) )
+
 		ext_package = nti.externalization.externalization.toExternalObject( package )
 		assert_that( ext_package, has_entry( 'DCCreator', ('Jason',) ) )
 		assert_that( ext_package, has_entry( 'Creator', 'Jason') )
