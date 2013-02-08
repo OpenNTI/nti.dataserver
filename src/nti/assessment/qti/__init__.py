d6b4e21fcd39b16ba1578eb67c2c177fcff3dff7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-02-08 23:21:12 +0000

    correct file $Id:
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@15745 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/qti/__init__.py
+++ src/nti/assessment/qti/__init__.py
@@ -2,7 +2,7 @@
 """
 QTI entry module
 
-$Id: pyramid_views.py 15718 2013-02-08 03:30:41Z carlos.sanchez $
+$Id: __init__.py 15718 2013-02-08 03:30:41Z carlos.sanchez $
 """
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
