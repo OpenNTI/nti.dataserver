d6a613b432bb3e80ddf992a75af9db4e7d7393d5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-02-08 03:30:41 +0000

    put back logger
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@15718 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/pyramid_views.py
+++ src/nti/contentsearch/pyramid_views.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
+"""
+Search pyramid views.
 
+$Id$
+"""
 from __future__ import print_function, unicode_literals, absolute_import
-__docformat__ = "restructuredtext en"
+
+logger = __import__( 'logging' ).getLogger( __name__ )
 
 import re
 
