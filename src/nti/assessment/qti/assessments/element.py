184b3d0eec2581b529b4d183387fce255fa0a0df
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-02-19 18:19:27 +0000

    start adding concrete classes
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@16026 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/assessment/qti/assessments/element.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Defines a QTI assesments element
+
+$Id$
+"""
+from __future__ import unicode_literals, print_function, absolute_import
+__docformat__ = "restructuredtext en"
+
+from zope import interface
+
+from ..basic.element import QTIElement
+from ..basic.element import qti_creator
+from . import interfaces as ast_interfaces
+
+@qti_creator
+@interface.implementer(ast_interfaces.Iselection)
+class Selection(QTIElement):
+	pass
+	
+@qti_creator
+@interface.implementer(ast_interfaces.Iordering)
+class Ordering(QTIElement):
+	pass
+
+@qti_creator
+@interface.implementer(ast_interfaces.ItimeLimits)
+class TimeLimits(QTIElement):
+	pass
+
+@qti_creator
+@interface.implementer(ast_interfaces.IvariableMapping)
+class VariableMapping(QTIElement):
+	pass
+
+@qti_creator
+@interface.implementer(ast_interfaces.ItemplateDefault)
+class TemplateDefault(QTIElement):
+	pass
+
+@qti_creator
+@interface.implementer(ast_interfaces.Iweight)
+class Weight(QTIElement):
+	pass
+
+@qti_creator
+@interface.implementer(ast_interfaces.IassessmentSection)
+class AssessmentSection(QTIElement):
+	pass
+
+@qti_creator
+@interface.implementer(ast_interfaces.IassessmentSectionRef)
+class AssessmentSectionRef(QTIElement):
+	pass
+
+@qti_creator
+@interface.implementer(ast_interfaces.IassessmentItemRef)
+class AssessmentItemRef(QTIElement):
+	pass
+	
+@qti_creator
+@interface.implementer(ast_interfaces.ItestPart)
+class TestPart(QTIElement):
+	pass
+
+@qti_creator
+@interface.implementer(ast_interfaces.IassessmentTest)
+class AssessmentTest(QTIElement):
+	pass
