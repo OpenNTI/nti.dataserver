ac26ae09bc4de8e77415a07bf9f8d80df17dd78a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-02-19 03:18:27 +0000

    clean up a bit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@16003 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/stemmers/_nltk.py
+++ src/nti/contentprocessing/stemmers/_nltk.py
@@ -1,19 +1,15 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-
+NLTK based stemmers
 
 $Id$
 """
-
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
+import nltk
 
 from zope import interface
-
-import nltk
 
 from nti.contentprocessing.stemmers import interfaces as stemmer_interfaces
 
--- src/nti/contentprocessing/stemmers/interfaces.py
+++ src/nti/contentprocessing/stemmers/interfaces.py
@@ -1,4 +1,11 @@
-from __future__ import unicode_literals, print_function
+# -*- coding: utf-8 -*-
+"""
+Stemmer interfaces
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
 
 from zope import interface
 
