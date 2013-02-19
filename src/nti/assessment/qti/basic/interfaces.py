a8df9ad48499d7cdbb270d19b9e765801e5df98e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-02-19 16:03:17 +0000

    add some warning arround unhandled field types
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@16012 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/qti/basic/interfaces.py
+++ src/nti/assessment/qti/basic/interfaces.py
@@ -7,13 +7,13 @@ $Id$
 from __future__ import unicode_literals, print_function, absolute_import
 __docformat__ = "restructuredtext en"
 
-from nti.assessment.qti import interfaces as qti_interfaces
-from nti.assessment.qti.attributes import interfaces as attr_interfaces
+from .. import interfaces as qti_interfaces
+from ..attributes import interfaces as attr_interfaces
 
 class IbodyElement(attr_interfaces.IbodyElementAttrGroup, qti_interfaces.IQTIElement):
 	"""
 	Marker interface for common attribute for elements
 	"""
-		
+
 class ITextOrVariable(qti_interfaces.IQTIElement):
 	pass
