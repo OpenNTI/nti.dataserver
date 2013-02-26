252bb6955c20b2b8c363f98e49217f75b4d9c599
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-02-26 02:03:31 +0000

    correct .zcml file
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@16282 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/qti/tests/__init__.py
+++ src/nti/assessment/qti/tests/__init__.py
@@ -5,8 +5,9 @@ from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
 import nti.dataserver as dataserver
+import nti.assessment.qti as qti
 
 from nti.dataserver.tests.mock_dataserver import SharedConfiguringTestBase as DSSharedConfiguringTestBase
 
 class ConfiguringTestBase(DSSharedConfiguringTestBase):
-	set_up_packages = (dataserver,)
+	set_up_packages = (dataserver, qti)
