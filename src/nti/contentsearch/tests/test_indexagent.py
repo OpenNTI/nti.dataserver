e8f9b502828d1b1de973a8b1e3d12d5cb2637032
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-02-27 17:00:57 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@16350 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/tests/test_indexagent.py
+++ src/nti/contentsearch/tests/test_indexagent.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-
-
-$Id$
-"""
 
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
-
-import unittest
+#disable: accessing protected members, too many methods
+#pylint: disable=W0212,R0904
 
 from nti.dataserver.contenttypes import Note
 from nti.dataserver.activitystream_change import Change
