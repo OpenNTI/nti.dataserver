46e1a04d4bfbf00a51b524ad51145c896713263e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-01 19:10:54 +0000

    checkpoint .. creating book schemas should be based on lang/book names
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@16520 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/indexing/_termextract.py
+++ src/nti/contentrendering/indexing/_termextract.py
@@ -1,4 +1,11 @@
-from __future__ import print_function, unicode_literals
+# -*- coding: utf-8 -*-
+"""
+TermExtract key word extractor.
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
 
 from zope import interface
 
