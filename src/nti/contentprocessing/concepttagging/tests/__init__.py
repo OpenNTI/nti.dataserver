e971d9663f224cb62cbaa45815a478321c0ef49c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-03 01:16:22 +0000

    minor changes
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@16579 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/concepttagging/tests/__init__.py
+++ src/nti/contentprocessing/concepttagging/tests/__init__.py
@@ -10,7 +10,7 @@ __docformat__ = "restructuredtext en"
 import nti.dataserver as dataserver
 import nti.contentprocessing as contentprocessing
 
-from nti.dataserver.tests.mock_dataserver import ConfiguringTestBase as DSConfiguringTestBase
+from nti.dataserver.tests.mock_dataserver import SharedConfiguringTestBase as DSConfiguringTestBase
 
 class ConfiguringTestBase(DSConfiguringTestBase):
 	set_up_packages = (dataserver, contentprocessing)
