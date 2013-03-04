b93c54b804dd58c29fc947b2d9b420998237e9cd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-04 22:39:05 +0000

    ongoing work
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@16634 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/store_views.py
+++ src/nti/appserver/store_views.py
@@ -13,6 +13,12 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.dataserver import authorization as nauth
+
+from ..store import pyramid_views 
 from ..store import get_purchase_attempt
 from ..store import interfaces as store_interfaces
 
@@ -21,3 +27,10 @@ def _purchase_attempt_successful( event ):
 	get_purchase_attempt(event.purchase_id, event.username)
 	# TODO: send email
 
+@view_config( context=store_interfaces.IPurchaseAttempt )
+@view_defaults( route_name='objects.generic.traversal',
+				renderer='rest',
+				permission=nauth.ACT_READ,
+				request_method='GET' )
+class GetPurchaseAttemptView(pyramid_views.GetPurchaseAttempt):
+	""" Support for simply returning the blog item """
