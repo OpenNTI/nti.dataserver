d981031cfe5d52689e1f96f52e8caaeaae07e4e2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-12 03:59:39 +0000

    add to more punk chars
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@16965 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/__init__.py
+++ src/nti/contentprocessing/__init__.py
@@ -18,7 +18,7 @@ default_ngram_minsize = 2
 default_ngram_maxsize = 20 # average word size in English in 5.10 
 default_word_tokenizer_expression = r"(?x)([A-Z]\.)+ | \$?\d+(\.\d+)?%? | \w+([-']\w+)*"
 
-default_punk_char_expression = r'[\?|!|(|)|"|\'|`|{|}|\[|\]|:|;|,|\.|\^|%|&|#|\*|@|$|&|+|\-|<|>|=|_|\~|\\|\s]'
+default_punk_char_expression = r'[\?|!|(|)|"|\'|`|{|}|\[|\]|:|;|,|\.|\^|%|&|#|\*|@|$|&|+|\-|<|>|=|_|\~|\\|/|\||\s]'
 
 default_word_tokenizer_pattern = re.compile(default_word_tokenizer_expression, re.I | re.MULTILINE | re.DOTALL | re.UNICODE)
 
