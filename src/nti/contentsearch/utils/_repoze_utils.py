6b500b7ab7ed18831311370078b39cffbaef3400
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-14 19:26:46 +0000

    one more test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@17138 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/utils/_repoze_utils.py
+++ src/nti/contentsearch/utils/_repoze_utils.py
@@ -15,7 +15,7 @@ from .. import interfaces as search_interfaces
 def remove_rim_catalogs(rim, content_types=()):
 	"""remove all the repoze catalogs from the specified entity manager"""
 	count = 0
-	content_types = content_types or get_indexable_types()
+	content_types = content_types if content_types else get_indexable_types()
 	for key in list(rim.keys()):
 		if key in content_types:
 			rim.pop(key, None)
@@ -25,7 +25,7 @@ def remove_rim_catalogs(rim, content_types=()):
 def remove_entity_catalogs(entity, content_types=()):
 	"""remove all the repoze catalogs from the specified entity"""
 	result = 0
-	content_types = content_types or get_indexable_types()
+	content_types = content_types if content_types else get_indexable_types()
 	try:
 		rim = search_interfaces.IRepozeEntityIndexManager(entity)
 		result = remove_rim_catalogs(rim, content_types)
