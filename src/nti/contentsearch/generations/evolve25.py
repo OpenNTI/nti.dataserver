10e87d8cf1c6d222008d4f0783727be3e50073dd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-14 16:26:33 +0000

    another migration b/c alpha seems in a funny state. Simply check that the redaction new fields are in the catalogs. Create them in not, reindex
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@17126 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentsearch/generations/evolve25.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Content search generation 25.
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 25
+
+import zope.intid
+from zope import component
+from zc import intid as zc_intid
+from zope.component.hooks import site, setHooks
+
+from .. import _repoze_index
+from .. import interfaces as search_interfaces
+from ..constants import (redaction_, replacementContent_, redactionExplanation_)
+
+from .evolve23 import reindex_redactions
+
+def do_evolve(context):
+	"""
+	Reindex redactions if required.
+	"""
+	setHooks()
+	conn = context.connection
+	root = conn.root()
+	ds_folder = root['nti.dataserver']
+	lsm = ds_folder.getSiteManager()
+
+	ds_intid = lsm.getUtility(provided=zope.intid.IIntIds)
+	component.provideUtility(ds_intid, zope.intid.IIntIds)
+	component.provideUtility(ds_intid, zc_intid.IIntIds)
+
+	with site(ds_folder):
+		assert component.getSiteManager() == ds_folder.getSiteManager(), "Hooks not installed?"
+
+		users = ds_folder['users']
+
+		# remove all post catalogs first
+		for user in users.values():
+			rim = search_interfaces.IRepozeEntityIndexManager(user)
+			catalog = rim.get(redaction_, None)
+			if catalog is None:
+				continue
+
+			reindex = False
+			if replacementContent_ not in catalog:
+				reindex = True
+				_repoze_index._zopytext_field_creator(catalog, replacementContent_, search_interfaces.IRedactionRepozeCatalogFieldCreator)
+			if redactionExplanation_ not in catalog:
+				reindex = True
+				_repoze_index._zopytext_field_creator(catalog, redactionExplanation_, search_interfaces.IRedactionRepozeCatalogFieldCreator)
+
+			if reindex:
+				logger.debug('Reindexing redactions for %s !!!' % user)
+				reindex_redactions(user, users.get, ds_intid)
+
+	logger.debug('Evolution done!!!')
+
+def evolve(context):
+	"""
+	Evolve generation 24 to 25 by reindexing redactions if required
+	"""
+	do_evolve(context)
---
+++ src/nti/contentsearch/generations/tests/test_evolve25.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+import fudge
+
+import nti.dataserver
+import nti.contentsearch
+
+from nti.dataserver import users
+from nti.dataserver.contenttypes import Redaction
+from nti.dataserver.utils.example_database_initializer import ExampleDatabaseInitializer
+
+from nti.ntiids.ntiids import make_ntiid
+
+from nti.contentsearch import interfaces as search_interfaces
+from nti.contentsearch.constants import (redaction_, content_, replacementContent_, redactionExplanation_)
+
+from nti.contentsearch.generations.evolve25 import evolve
+
+from nti.dataserver.tests import mock_dataserver
+from nti.dataserver.tests.mock_dataserver import  mock_db_trans, WithMockDS
+
+from nti.deprecated import hides_warnings
+
+from hamcrest import (assert_that, has_length, has_key)
+
+class TestEvolve25(mock_dataserver.ConfiguringTestBase):
+	set_up_packages = (nti.dataserver, nti.contentsearch)
+
+	@hides_warnings
+	@WithMockDS
+	def test_evolve25(self):
+		with mock_db_trans() as conn:
+			context = fudge.Fake().has_attr(connection=conn)
+			ExampleDatabaseInitializer(max_test_users=0, skip_passwords=True).install(context)
+
+			jason = users.User.get_user(dataserver=mock_dataserver.current_mock_ds, username='jason.madden@nextthought.com')
+
+			redaction = Redaction()
+			redaction.selectedText = u'Fear'
+			redaction.replacementContent = 'Ichigo'
+			redaction.redactionExplanation = 'Have overcome it everytime I have been on the verge of death'
+			redaction.creator = jason.username
+			redaction.containerId = make_ntiid(nttype='bleach', specific='manga')
+			redaction = jason.addContainedObject(redaction)
+
+			rim = search_interfaces.IRepozeEntityIndexManager(jason)
+			rim.index_content(redaction)
+
+			catalog = rim.get(redaction_)
+			catalog.pop(replacementContent_, None)
+			catalog.pop(redactionExplanation_, None)
+
+		with mock_db_trans() as conn:
+			context = fudge.Fake().has_attr(connection=conn)
+			evolve(context)
+
+		with mock_db_trans() as conn:
+			ds_folder = context.connection.root()['nti.dataserver']
+			jason = ds_folder['users']['jason.madden@nextthought.com']
+			rim = search_interfaces.IRepozeEntityIndexManager(jason)
+
+			assert_that(rim, has_key(redaction_))
+			catalog = rim[redaction_]
+			assert_that(catalog, has_key(content_))
+			assert_that(catalog, has_key(replacementContent_))
+			assert_that(catalog, has_key(redactionExplanation_))
+
+			hits = rim.search("fear")
+			assert_that(hits, has_length(1))
+
+			hits = rim.search("death")
+			assert_that(hits, has_length(1))
+
+			hits = rim.search("ichigo")
+			assert_that(hits, has_length(1))
