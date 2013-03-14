39103a489761df84b52b8aae7a9e4d8595ca9211
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-14 16:26:33 +0000

    another migration b/c alpha seems in a funny state. Simply check that the redaction new fields are in the catalogs. Create them in not, reindex
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@17126 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/generations/install.py
+++ src/nti/contentsearch/generations/install.py
@@ -7,7 +7,7 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-generation = 24
+generation = 25
 
 from zope.generations.generations import SchemaManager
 
