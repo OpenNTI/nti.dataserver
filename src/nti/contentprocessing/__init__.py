bae77f6f174d3ff3a935a3532e11b3bce5124dc9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-15 15:39:04 +0000

    add a method to convert to normalized unicode, remove non-alpha chars and compress runs of spaces
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@17179 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/__init__.py
+++ src/nti/contentprocessing/__init__.py
@@ -8,6 +8,8 @@ from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
 import re
+import sys
+import unicodedata
 
 from zope import interface
 
@@ -26,9 +28,23 @@ default_word_tokenizer_pattern = re.compile(default_word_tokenizer_expression, r
 default_punk_char_pattern = re.compile(default_punk_char_expression, re.I | re.MULTILINE | re.DOTALL | re.UNICODE)
 default_punk_char_pattern_plus = re.compile(default_punk_char_expression_plus, re.I | re.MULTILINE | re.DOTALL | re.UNICODE)
 
+space_pattern = re.compile('\s+', re.UNICODE)
+
+def _makeNonAlphaRe():
+	nonAlpha = [u'[^']
+	for i in range(sys.maxunicode):
+		c = unichr(i)
+		if c.isalpha(): nonAlpha.append(c)
+		nonAlpha.append(u']')
+	nonAlpha = u"".join(nonAlpha)
+	return re.compile(nonAlpha)
+
+non_alpha_pattern = _makeNonAlphaRe()
+del _makeNonAlphaRe
+
 # export common functions
+from ._content_utils import normalize
 from ._content_utils import rank_words
 from ._ngrams_utils import compute_ngrams
 from ._content_utils import split_content
 from ._content_utils import get_content_translation_table
-
