d280d0b4437fc62651da1c911d191a3a5e5edbb0
Jason Madden <jason.madden@nextthought.com>
2013-03-20 16:06:40 +0000

    The HTTP spec is slightly unclear on whether a Content-Type header is required on a POST; repoze.who 2.0 let it go, 2.1 requires it. Exceptions were being made for test cases, turns out the browser app is also not sending it so expand those exceptions to everyone.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@17336 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/test_application_usersearch.py
+++ src/nti/appserver/tests/test_application_usersearch.py
@@ -122,7 +122,7 @@ class TestApplicationUserSearch(SharedApplicationTestBase):
 		res = testapp.get( path, extra_environ=self._make_extra_environ())
 
 		assert_that( res.content_type, is_( 'application/vnd.nextthought+json' ) )
-		assert_that( res.cache_control, has_property( 'no_store', True ) )
+		assert_that( res.cache_control, has_property( 'no_cache', '*' ) )
 
 		assert_that( res.body, contains_string( str('sjohnson@nextthought.com') ) )
 
