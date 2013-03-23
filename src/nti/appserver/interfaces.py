b1911d88cd09fe1ce315e837826ef0ecf4f5ef5f
Jason Madden <jason.madden@nextthought.com>
2013-03-23 14:46:10 +0000

    Items deleted through moderation consistently use a different string than those deleted by the creator/admin. Expose this fact as a property externally too.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@17472 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/interfaces.py
+++ src/nti/appserver/interfaces.py
@@ -286,6 +286,13 @@ class IUncacheableUnModifiedInResponse(IUncacheableInResponse):
 	"""
 
 
+class IModeratorDealtWithFlag(interface.Interface):
+	"""
+	Marker interface denoting that an object that is IFlaggable
+	(and cannot lose that interface) has been dealt with by the moderator
+	and shouldn't be subject to further flagging or mutation.
+	"""
+
 from nti.dataserver.interfaces import IDeletedObjectPlaceholder # BWC
 
 ###
