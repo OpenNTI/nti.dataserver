6a936653cb501343e07edde5947d5d6cd58b3d25
Jason Madden <jason.madden@nextthought.com>
2013-03-29 16:09:46 +0000

    Bite the bullet and make un/flagging an object updates its modification time. This is consistent with liking an object, which already did that. It is necessary for caching at the HTTP level to work reliably without including the flag state of each object in the etag. It lets us remove one workaround we had in place for that very problem on chat transcripts.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@17748 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/tests/test_flagging.py
+++ src/nti/dataserver/tests/test_flagging.py
@@ -13,12 +13,16 @@ from hamcrest import assert_that
 from hamcrest import is_
 from hamcrest import none
 from hamcrest import contains
+from hamcrest import has_length
 
 from zope import component
 from zope.intid import interfaces as intid_interfaces
+from zope.component import eventtesting
+import time
 
 from nti.tests import validly_provides as verifiably_provides
 from nti.tests import is_true, is_false
+from nti.tests import time_monotonically_increases
 
 from nti.dataserver import interfaces as nti_interfaces
 from nti.dataserver import flagging
@@ -35,17 +39,27 @@ def Note():
 class TestFlagging(SharedConfiguringTestBase):
 
 	@WithMockDSTrans
+	@time_monotonically_increases
 	def test_flagging(self):
 		"Notes can be flagged and unflagged"
 		n = Note()
 		component.getUtility( intid_interfaces.IIntIds ).register( n )
 
 		assert_that( component.getAdapter( n, nti_interfaces.IGlobalFlagStorage ), verifiably_provides( nti_interfaces.IGlobalFlagStorage ) )
+		eventtesting.clearEvents()
 
 		# first time does something
+		n.lastModified = 0
+		now = time.time()
 		assert_that( flagging.flag_object( n, 'foo@bar' ), is_( none() ) )
 		# second time no-op
 		assert_that( flagging.flag_object( n, 'foo@bar' ), is_( none() ) )
+
+		# Fired one event
+		assert_that( eventtesting.getEvents(nti_interfaces.IObjectFlaggedEvent), has_length(1) )
+		# Updated time once
+		assert_that( n.lastModified, is_( now + 1 ) )
+		n.lastModified = 0
 
 		assert_that( flagging.flags_object( n, 'foo@bar' ), is_true() )
 		assert_that( list(component.getAdapter( n, nti_interfaces.IGlobalFlagStorage ).iterflagged()), contains( n ) )
@@ -54,7 +68,10 @@ class TestFlagging(SharedConfiguringTestBase):
 		assert_that( flagging.unflag_object( n, 'foo@bar' ), is_( none() ) )
 		# second time no-op
 		assert_that( flagging.unflag_object( n, 'foo@bar' ), is_( none() ) )
-
+		# Fired one event
+		assert_that( eventtesting.getEvents(nti_interfaces.IObjectUnflaggedEvent), has_length(1) )
+		# updated time once
+		assert_that( n.lastModified, is_( now + 2 ) )
 		assert_that( flagging.flags_object( n, 'foo@bar' ), is_false() )
 
 		# If we unregister while flagged, the flagging status changes
