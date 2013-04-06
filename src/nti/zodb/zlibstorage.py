9eb8dc15e3e5543202ddc53de4b87989ec27b495
Jason Madden <jason.madden@nextthought.com>
2013-04-06 16:05:58 +0000

    Configure ZEO to use persistent disk caches. This makes it robust to the ZEO server not being available for read-only transactions and has some nice speed benefits too on subsequent restarts (loading from the ClientStorage cache is faster than loading from ZEO)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@17971 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/zodb/zlibstorage.py
+++ src/nti/zodb/zlibstorage.py
@@ -11,7 +11,8 @@ except ImportError: # pypy?
 	class resolvers(object):
 		ClientStorageURIResolver = object
 		RESOLVERS = {}
-
+from nti.utils import make_cache_dir
+import os
 
 class ZlibStorageClientStorageURIResolver(resolvers.ClientStorageURIResolver):
 	"""
@@ -29,7 +30,7 @@ class ZlibStorageClientStorageURIResolver(resolvers.ClientStorageURIResolver):
 
 		# It expect to find 'zeo' so make that happen
 		uri = uri.replace( b'zlibzeo://', b'zeo://' )
-		key, args, kw, _ = super(ZlibStorageClientStorageURIResolver,self).__call__(uri)
+		key, args, storage_kw, _ = super(ZlibStorageClientStorageURIResolver,self).__call__(uri)
 		# key = (args, tuple(kw items), tuple(dbkw items))
 		dbkw = dict(key[2])
 		orig_kw = dict(key[1])
@@ -37,15 +38,26 @@ class ZlibStorageClientStorageURIResolver(resolvers.ClientStorageURIResolver):
 		def zlibfactory():
 			# Wraps uri in :class:`zc.slibstorage.ZlibStorage` and returns a :class:`ZODB.DB`
 
+			# Delay setting the client name until the very end so whatever is going to
+			# set environment variables will have done so.
+			if 'var' not in storage_kw:
+				storage_kw['var'] = make_cache_dir( 'zeo' )
+			if 'client' not in storage_kw:
+				name = os.environ.get( "DATASERVER_ZEO_CLIENT_NAME" )
+				if name:
+					storage_kw['client'] = name # storage name is automatically part of it
+			if 'cache_size' not in storage_kw:
+				storage_kw['cache_size'] = 200 * 1024 * 1024 # ClientCache docs say 200MB is good
+
 			# Client storage is very picky: a Unix path must be bytes, not unicode
-			client = ClientStorage( args, **kw )
+			client = ClientStorage( *args, **storage_kw )
 			if 'demostorage' in orig_kw: # pragma: no cover
 				client = DemoStorage( base=client )
 
 			zlib = ZlibStorage( client )
 			return DB( zlib, **dbkw )
 
-		return key, args, kw, zlibfactory
+		return key, args, storage_kw, zlibfactory
 
 def install_zlib_client_resolver():
 	"""
