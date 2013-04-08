387363acf4a04d26494787208e82061774d8dcd7
Jason Madden <jason.madden@nextthought.com>
2013-04-08 08:55:55 +0000

    dep bumps
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@17983 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup(
 		# 'nose >= 1.2.1',
 		# But it's also listed in extras/test, and it's very hard to upgrade
 		# when that changes
-		# 'distribute >= 0.6.35', # Can't seem to include that anywhere
+		# 'distribute >= 0.6.36', # Can't seem to include that anywhere
 		# In theory this should make it possible to get
 		# the svn revision number from svn 1.7. Doesn't seem
 		# to work (with distribute?)
@@ -201,7 +201,7 @@ setup(
 		'numpy >= 1.7.0' if not IS_PYPY else '',
 		'paste >= 1.7.5.1',
 		'perfmetrics >= 1.0',  # easy statsd metrics.
-		'plone.scale >= 1.3',  # image scaling/storage based on PIL
+		'plone.scale >= 1.3.1',  # image scaling/storage based on PIL
 		'plone.namedfile >= 2.0.1',  # much like zope.file, but some image-specific goodness.
 		'pyparsing >= 1.5.6, < 2.0.0',  # used by matplotlib, experimental in zopyx.txng.core
 		# Pure python PDF reading library. Not complex. Has newer fork pyPDF2, not yet on PyPI?
@@ -241,7 +241,7 @@ setup(
 		# 'scss >= 0.8.72', # we no longer use
 		'setproctitle >= 1.1.7',  # used by gunicorn
 		'setuptools >= 0.6c11',
-		'simplejson >= 3.1.2',
+		'simplejson >= 3.1.3',
 		'six >= 1.3.0',
 		'sympy == 0.7.2',  # sympy-docs-html-0.7.1 is currently greater
 		'stripe >= 1.7.10',  # stripe payments
@@ -370,7 +370,7 @@ setup(
 			'dblatex >= 0.3.4',  # content rendering, convert docbook to tex
 			'epydoc >= 3.0.1',  # auto-api docs
 			'httpie >= 0.4.1',  # 0.4.0 explicitly requires requests > 1.0.4, 0.3.1 explicitly requires requests < 1.0
-			'ipython >= 0.13.1',  # the extra notebook is web based, pulls in tornado
+			'ipython >= 0.13.2',  # the extra notebook is web based, pulls in tornado
 			'logilab_astng >= 0.24.1',
 			'pip >= 1.3.1',
 			'pip-tools >= 0.3.1',  # command pip-review, pip-dump
@@ -385,7 +385,7 @@ setup(
 			'sphinxcontrib-programoutput >= 0.8',
 			'sphinxtheme.readability >= 0.0.6',
 			'virtualenv >= 1.9.1',
-			'virtualenvwrapper >= 3.7',
+			'virtualenvwrapper >= 3.7.1',
 			'zc.buildout >= 2.1.0',
 			'z3c.dependencychecker >= 1.10',  # unused/used imports; see also tl.eggdeps
 			'zodbbrowser >= 0.10.4',
