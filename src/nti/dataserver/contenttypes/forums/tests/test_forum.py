6572bdb0f48be18000c2d2a6a667d73ed3e05071
Jason Madden <jason.madden@nextthought.com>
2013-04-10 21:40:07 +0000

    Move the ever-churning forum NewestDescendant data into redis.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@18090 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/contenttypes/forums/tests/test_forum.py
+++ src/nti/dataserver/contenttypes/forums/tests/test_forum.py
@@ -70,6 +70,11 @@ def setUpModule():
 
 	component.provideAdapter(CommentKeyRef)
 	component.provideAdapter(CommentKeyRef, adapts=(IPersonalBlogEntry,) )
+
+	from nti.dataserver.tests import mock_redis
+	client = mock_redis.InMemoryMockRedis()
+	component.provideUtility( client )
+
 tearDownModule = nti.tests.module_teardown
 
 
