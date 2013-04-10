fb8d14f615c4f29cedb8b478723e260394aefc91
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-04-10 16:08:57 +0000

    extend _TrivialDefaultInvitations to allow store invitations
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@18068 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/invitation_views.py
+++ src/nti/appserver/invitation_views.py
@@ -14,72 +14,73 @@ logger = __import__('logging').getLogger(__name__)
 from zope import interface
 from zope import component
 
-from nti.externalization import interfaces as ext_interfaces
-from nti.dataserver import interfaces as nti_interfaces
-from nti.appserver import interfaces as app_interfaces
-from nti.appserver.invitations import interfaces as invite_interfaces
-from pyramid import interfaces as pyramid_interfaces
 from zc import intid as zc_intid
 
 from pyramid.view import view_config
+from pyramid import interfaces as pyramid_interfaces
 
 from nti.dataserver.links import Link
 from nti.dataserver import authorization as nauth
+from nti.dataserver import interfaces as nti_interfaces
+
 from nti.externalization import integer_strings
+from nti.externalization import interfaces as ext_interfaces
+
+from nti.store import interfaces as store_interfaces
 
 from . import httpexceptions as hexc
 from ._util import link_belongs_to_user
 from . import _external_object_io as obj_io
 
-
-from nti.appserver.invitations.utility import accept_invitations, ZcmlInvitations
-from nti.appserver.invitations.invitation import JoinEntitiesInvitation
-
 from nti.appserver import _util
+from nti.appserver import interfaces as app_interfaces
 from nti.appserver.pyramid_authorization import is_writable
+from nti.appserver.invitations import interfaces as invite_interfaces
+from nti.appserver.invitations.invitation import JoinEntitiesInvitation
+from nti.appserver.invitations.utility import accept_invitations, ZcmlInvitations
 
-#: The link relationship type to which an authenticated
-#: user can ``POST`` data to accept outstanding invitations. Also the name of a
-#: view to handle this feedback: :func:`accept_invitations_view`
-#: The data should be an dictionary containing the key ``invitation_codes``
-#: whose value is an array of strings naming codes.
-#: See also :func:`nti.appserver.account_creation_views.account_create_view`
+# : The link relationship type to which an authenticated
+# : user can ``POST`` data to accept outstanding invitations. Also the name of a
+# : view to handle this feedback: :func:`accept_invitations_view`
+# : The data should be an dictionary containing the key ``invitation_codes``
+# : whose value is an array of strings naming codes.
+# : See also :func:`nti.appserver.account_creation_views.account_create_view`
 REL_ACCEPT_INVITATIONS = 'accept-invitations'
 
-#: The link relationship type that will be exposed to the creator of a
-#: :class:`nti.dataserver.users.friends_lists.DynamicFriendsList`. A ``GET``
-#: to this link will return the invitation code corresponding to the default invitation
-#: to join that group, in the form of a dictionary: ``{invitation_code: "thecode"}``
-#: If the invitation does not exist, one will be created; at most one such code can exist at a time.
-#: There is no way to disable the code at this time (in the future that could be done with a
-#: ``DELETE`` to this link type). See also :func:`get_default_trivial_invitation_code`
+# : The link relationship type that will be exposed to the creator of a
+# : :class:`nti.dataserver.users.friends_lists.DynamicFriendsList`. A ``GET``
+# : to this link will return the invitation code corresponding to the default invitation
+# : to join that group, in the form of a dictionary: ``{invitation_code: "thecode"}``
+# : If the invitation does not exist, one will be created; at most one such code can exist at a time.
+# : There is no way to disable the code at this time (in the future that could be done with a
+# : ``DELETE`` to this link type). See also :func:`get_default_trivial_invitation_code`
 REL_TRIVIAL_DEFAULT_INVITATION_CODE = 'default-trivial-invitation-code'
 
-@view_config( route_name='objects.generic.traversal',
-			  renderer='rest',
-			  context=nti_interfaces.IUser,
-			  permission=nauth.ACT_UPDATE,
-			  request_method='POST',
-			  name=REL_ACCEPT_INVITATIONS )
-def accept_invitations_view( request ):
+@view_config(route_name='objects.generic.traversal',
+			 renderer='rest',
+			 context=nti_interfaces.IUser,
+			 permission=nauth.ACT_UPDATE,
+			 request_method='POST',
+			 name=REL_ACCEPT_INVITATIONS)
+def accept_invitations_view(request):
 	"""
 	Implementation of :const:`REL_ACCEPT_INVITATIONS`.
 	"""
 
-	json_body = obj_io.read_body_as_external_object( request )
+	json_body = obj_io.read_body_as_external_object(request)
 	if 'invitation_codes' not in json_body:
 		raise hexc.HTTPBadRequest()
 
 	try:
 		invite_codes = json_body['invitation_codes']
 		if invite_codes:
-			accept_invitations( request.context, invite_codes )
+			accept_invitations(request.context, invite_codes)
 
 	except invite_interfaces.InvitationValidationError as e:
 		e.field = 'invitation_codes'
-		obj_io.handle_validation_error( request, e )
-	except Exception as e: #pragma: no cover
-		obj_io.handle_possible_validation_error( request, e )
+		obj_io.handle_validation_error(request, e)
+	except Exception as e:  # pragma: no cover
+		obj_io.handle_possible_validation_error(request, e)
 
 	return hexc.HTTPNoContent()
 
@@ -100,32 +101,50 @@ class _DefaultJoinEntityInvitation(JoinEntitiesInvitation):
 class _TrivialDefaultInvitations(ZcmlInvitations):
 
 	def _getDefaultInvitationCode(self, dfl):
-		iid = component.getUtility( zc_intid.IIntIds ).getId( dfl )
-		return integer_strings.to_external_string( iid )
+		iid = component.getUtility(zc_intid.IIntIds).getId(dfl)
+		return integer_strings.to_external_string(iid)
 
-	def getInvitationByCode( self, code ):
+	def _getObjectByCode(self, code):
+		iid = integer_strings.from_external_string(code)
+		result = component.getUtility(zc_intid.IIntIds).getObject(iid)
+		return result
+
+	def getInvitationByCode(self, code):
 		__traceback_info__ = code,
-		invite = super(_TrivialDefaultInvitations,self).getInvitationByCode( code )
+		invite = super(_TrivialDefaultInvitations, self).getInvitationByCode(code)
 		if invite is None:
 			try:
-				iid = integer_strings.from_external_string( code )
-				dfl = component.getUtility( zc_intid.IIntIds ).getObject( iid )
-				if nti_interfaces.IDynamicSharingTargetFriendsList.providedBy( dfl ):
-					invite = _DefaultJoinEntityInvitation( code, dfl )
+				dfl = self._getObjectByCode(code)
+				if nti_interfaces.IDynamicSharingTargetFriendsList.providedBy(dfl):
+					invite = _DefaultJoinEntityInvitation(code, dfl)
 					invite.creator = dfl.creator
-			except (KeyError,ValueError,AttributeError):
+			except (KeyError, ValueError, AttributeError):
 				return None
 		return invite
 
+class _TrivialAndStoreInvitations(_TrivialDefaultInvitations):
 
-@view_config( route_name='objects.generic.traversal',
-			  renderer='rest',
-			  context=nti_interfaces.IDynamicSharingTargetFriendsList,
-			  permission=nauth.ACT_UPDATE, # The creator only, not members who have read access
-			  request_method='GET',
-			  name=REL_TRIVIAL_DEFAULT_INVITATION_CODE)
+	def getInvitationByCode(self, code):
+		__traceback_info__ = code,
+		invite = super(_TrivialAndStoreInvitations, self).getInvitationByCode(code)
+		if invite is None:
+			from nti.store import invitations
+			try:
+				purchase = self._getObjectByCode(code)
+				if store_interfaces.IPurchaseAttempt(purchase):
+					invite = invitations.create_store_invitation(purchase, code)
+			except (KeyError, ValueError, AttributeError):
+				return None
+		return invite
+
+@view_config(route_name='objects.generic.traversal',
+			 renderer='rest',
+			 context=nti_interfaces.IDynamicSharingTargetFriendsList,
+			 permission=nauth.ACT_UPDATE,  # The creator only, not members who have read access
+			 request_method='GET',
+			 name=REL_TRIVIAL_DEFAULT_INVITATION_CODE)
 def get_default_trivial_invitation_code(request):
-	code = component.getUtility(invite_interfaces.IInvitations)._getDefaultInvitationCode( request.context )
+	code = component.getUtility(invite_interfaces.IInvitations)._getDefaultInvitationCode(request.context)
 	return {'invitation_code': code}
 
 @interface.implementer(ext_interfaces.IExternalMappingDecorator)
@@ -134,5 +153,5 @@ class DFLGetInvitationLinkProvider(_util.AbstractTwoStateViewLinkDecorator):
 
 	true_view = REL_TRIVIAL_DEFAULT_INVITATION_CODE
 
-	def predicate( self, context, username ):
-		return is_writable( context )
+	def predicate(self, context, username):
+		return is_writable(context)
