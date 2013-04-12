4b385402afad14f6363458b9b09fa5fb93f3f835
Jason Madden <jason.madden@nextthought.com>
2013-04-12 13:09:26 +0000

    Remove some deprecated and wrong uses of request.registry when traversing. Make the /dataserver2 path automatically also be a ++adapter traverser, looking for named adapters for subpaths. Per CS earlier question, use this to place store views in their own subtree, configurable by site; update test case to account for this.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@18190 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/store_views.py
+++ src/nti/appserver/store_views.py
@@ -11,72 +11,73 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from zope import component
+from zope import interface
+from zope.traversing.interfaces import IPathAdapter
+from zope.location.interfaces import IContained
 
 from pyramid.view import view_config
 
 from nti.dataserver import authorization as nauth
 
-from ..store import pyramid_views
-from ..store import get_purchase_attempt
-from ..store import interfaces as store_interfaces
+from nti.store import pyramid_views
+from nti.store import get_purchase_attempt
+from nti.store import interfaces as store_interfaces
+
+@interface.implementer(IPathAdapter,IContained)
+class StorePathAdapter(object):
+	"""
+	Exists to provide a namespace in which to place all of these views,
+	and perhaps to traverse further on.
+	"""
+
+	__parent__ = None
+	__name__ = None
+
+	def __init__( self, context, request ):
+		self.context = context
+		self.request = request
 
 @component.adapter(store_interfaces.IPurchaseAttemptSuccessful)
 def _purchase_attempt_successful(event):
 	get_purchase_attempt(event.purchase_id, event.username)
 	# TODO: send email
 
-@view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 permission=nauth.ACT_READ,
-			 request_method='GET',
-			 name="get_purchase_attempt")
+
+_view_defaults = dict(route_name='objects.generic.traversal',
+					  renderer='rest',
+					  permission=nauth.ACT_READ,
+					  context=StorePathAdapter,
+					  request_method='GET' )
+_post_view_defaults = _view_defaults.copy()
+_post_view_defaults['request_method'] = 'POST'
+
+@view_config(name="get_purchase_attempt", **_view_defaults)
 class GetPurchaseAttemptView(pyramid_views.GetPurchaseAttemptView):
 	""" Returning a purchase attempt """
 
-@view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 permission=nauth.ACT_READ,
-			 request_method='GET',
-			 name="get_pending_purchases")
+@view_config(name="get_pending_purchases", **_view_defaults)
 class GetPendingPurchasesView(pyramid_views.GetPendingPurchasesView):
 	""" Return all pending purchases items """
 
-@view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 permission=nauth.ACT_READ,
-			 request_method='GET',
-			 name="get_purchase_history")
+@view_config(name="get_purchase_history", **_view_defaults)
 class GetPurchaseHistoryView(pyramid_views.GetPurchaseHistoryView):
 	""" Return purchase history """
 
-@view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 permission=nauth.ACT_READ,
-			 request_method='GET',
-			 name="get_purchasables")
+@view_config(name="get_purchasables", **_view_defaults)
 class GetPurchasablesView(pyramid_views.GetPurchasablesView):
 	""" Return all purchasables items """
 
-@view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 permission=nauth.ACT_READ,
-			 request_method='GET',
-			 name="get_stripe_connect_key")
+@view_config(name="get_stripe_connect_key", **_view_defaults)
 class GetStripeConnectKeyView(pyramid_views.GetStripeConnectKeyView):
 	""" Return the stripe connect key """
 
-@view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 permission=nauth.ACT_READ,
-			 request_method='POST',
-			 name="post_stripe_payment")
+@view_config(name="post_stripe_payment", **_post_view_defaults)
 class ProcessPaymentWithStripeView(pyramid_views.StripePaymentView):
 	""" Process a payment using stripe """
 
-@view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 permission=nauth.ACT_READ,
-			 request_method='POST',
-			 name="validate_stripe_coupon")
+@view_config(name="validate_stripe_coupon", **_post_view_defaults)
 class ValidateStripeCouponView(pyramid_views.ValidateStripeCouponView):
 	""" Validate a stripe token """
+
+del _view_defaults
+del _post_view_defaults
