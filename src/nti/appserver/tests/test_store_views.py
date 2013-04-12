fa33e4239d71ff611043dba037c60411253f0423
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-04-12 03:31:06 +0000

    start adding app tests for the store
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@18177 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/appserver/tests/test_store_views.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from nti.appserver.tests.test_application import SharedApplicationTestBase, WithSharedApplicationMockDS
+
+from hamcrest import (assert_that, is_, has_length)
+
+class TestApplicationUserExporViews(SharedApplicationTestBase):
+
+	set_up_packages = SharedApplicationTestBase.set_up_packages + (('purchasables.zcml', 'nti.appserver.tests'),)
+
+	@WithSharedApplicationMockDS(users=True, testapp=True)
+	def test_get_purchasables(self):
+
+		url = '/dataserver2/@@get_purchasables'
+		res = self.testapp.get(url, status=200)
+		json_body = res.json_body
+		assert_that(json_body, has_length(1))
+		assert_that(json_body[0]['NTIID'],
+					is_("tag:nextthought.com,2011-10:CMU-HTML-04630_main.04_630:_computer_science_for_practicing_engineers"))
