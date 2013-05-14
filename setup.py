fed1989450e1e5eecfc36ffcabbfc5be603142da
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-14 13:52:12 +0000

    bump redis, distribute
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@19126 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup(
 		# 'nose >= 1.2.1',
 		# But it's also listed in extras/test, and it's very hard to upgrade
 		# when that changes
-		# 'distribute >= 0.6.39', # Can't seem to include that anywhere
+		# 'distribute >= 0.6.40', # Can't seem to include that anywhere
 		# In theory this should make it possible to get
 		# the svn revision number from svn 1.7. Doesn't seem
 		# to work (with distribute?)
@@ -228,7 +228,7 @@ setup(
 		'pytz >= 2013b',
 		'rdflib >= 3.4.0',
 		# Redis python client. Note that Amazon deployed servers are still in the 2.6 (2.4?) series
-		'redis >= 2.7.4',
+		'redis >= 2.7.5',
 		# There is a nice complete mock for it at fakeredis, installed for tests
 		'repoze.catalog >= 0.8.2',
 		'repoze.lru >= 0.6',  # LRU caching. Dep of Pyramid
