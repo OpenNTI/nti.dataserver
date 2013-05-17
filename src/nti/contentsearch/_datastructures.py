f4b0f0f59c0b4aae513249edac454c30b97c02a6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-17 02:16:47 +0000

    no need to register
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@19264 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_datastructures.py
+++ src/nti/contentsearch/_datastructures.py
@@ -8,7 +8,6 @@ from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
 import itertools
-import collections
 
 from brownie.caching import LFUCache
 
@@ -35,27 +34,25 @@ class CaseInsensitiveDict(dict):
 
     def __getitem__(self, key):
         return super(CaseInsensitiveDict, self).__getitem__(key.lower())
-    
+
     def __delitem__(self, key):
         return super(CaseInsensitiveDict, self).__delitem__(key.lower())
-
-collections.Mapping.register(CaseInsensitiveDict)
 
 class IterableWrapper(object):
 
     def __init__(self, it, size=0):
         self.it = it
         self.size = size
-    
+
     def __len__(self):
         return self.size
-    
+
     def __iter__(self):
         for elt in self.it:
             yield elt
-    
+
     def __getitem__(self, index):
         if type(index) is slice:
             return list(itertools.islice(self.it, index.start, index.stop, index.step))
         else:
-            return next(itertools.islice(self.it, index, index+1))
+            return next(itertools.islice(self.it, index, index + 1))
