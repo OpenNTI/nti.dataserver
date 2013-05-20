bba4b48fed4f567ec9720466dd937e6df255c9e0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-20 20:01:38 +0000

    fix content elements
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@19374 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/assessment/qti/items/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Defines QTI item elements
+
+$Id$
+"""
+from __future__ import unicode_literals, print_function, absolute_import
+__docformat__ = "restructuredtext en"
+
+from .elements import *
--- src/nti/assessment/qti/tests/test_parser.py
+++ src/nti/assessment/qti/tests/test_parser.py
@@ -11,8 +11,9 @@ import os
 from .. import parser
 
 from . import ConfiguringTestBase
+from ..items import AssessmentItem
 
-from hamcrest import (assert_that, is_not, none)
+from hamcrest import (assert_that, is_, is_not, instance_of, has_length, none)
 
 class TestParser(ConfiguringTestBase):
 
@@ -20,5 +21,32 @@ class TestParser(ConfiguringTestBase):
 		path = os.path.join(os.path.dirname(__file__), 'choice.xml')
 		with open(path, "r") as f:
 			qti = parser.parser(f)
-			assert_that(qti, is_not(none()))
+
+		assert_that(qti, is_not(none()))
+		assert_that(qti, instance_of(AssessmentItem))
+		assert_that(qti.identifier, is_('choice'))
+		assert_that(qti.adaptive, is_(False))
+		assert_that(qti.timeDependent, is_(False))
+		assert_that(qti.title, is_('Unattended Luggage'))
+		assert_that(qti.responseDeclaration, has_length(1))
+		assert_that(qti.outcomeDeclaration, has_length(1))
+		assert_that(qti.itemBody, is_not(none()))
+
+		rd = qti.responseDeclaration[0]
+		assert_that(rd.identifier, is_("RESPONSE"))
+		assert_that(rd.cardinality, is_("single"))
+		assert_that(rd.baseType, is_('identifier'))
+		assert_that(rd.correctResponse, is_not(none()))
+		assert_that(rd.correctResponse, has_length(1))
+
+		od = qti.outcomeDeclaration[0]
+		assert_that(od.identifier, is_("SCORE"))
+		assert_that(od.cardinality, is_("single"))
+		assert_that(od.baseType, is_("integer"))
+
+		ib = qti.itemBody
+		assert_that(ib, has_length(3))
+		assert_that(ib.blocks, has_length(3))
+
+
 
