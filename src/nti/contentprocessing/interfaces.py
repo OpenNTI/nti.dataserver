8fd061ced74d270d5b8b91e005d0f11535924dc4
Jason Madden <jason.madden@nextthought.com>
2013-05-20 17:27:49 +0000

    Checkpoint the generalized metadata extractors for online external links; not finished or tested.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@19364 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/interfaces.py
+++ src/nti/contentprocessing/interfaces.py
@@ -55,3 +55,66 @@ class IPunctuationCharPatternPlus(interface.Interface):
 class IAlchemyAPIKey(interface.Interface):
 	alias = interface.Attribute("Key name or alias")
 	value = interface.Attribute("The actual key value")
+
+
+####
+# Metadata extraction
+####
+
+from nti.contentfragments import schema as frg_schema
+from zope.mimetype.interfaces import mimeTypeConstraint
+
+class IContentMetadata(interface.Interface):
+	"""
+	Metadata extracted from existing content.
+	Each of the attributes is filled in based on the best
+	possible extraction and each may be missing or empty.
+	"""
+
+	title = frg_schema.PlainTextLine(title="The title of the content",
+									 required=False,
+									 default='')
+	description = frg_schema.PlainText(title="A short description of the content",
+									   required=False,
+									   default='' ) # TODO: Size limits?
+	creator = frg_schema.PlainTextLine(title="A description of the creator",
+									   description="Possibly one or more names or even an organization.",
+									   required=False,
+									   default='' )
+	mimeType = schema.ASCIILine(
+		title="The Mime Type of the content",
+		constraint=mimeTypeConstraint,
+		required=False )
+
+	sourceLocation = schema.TextLine( title="The location of the content",
+									  description=("The unprocessed, original location of the content"
+									  				" used to find the metadata. May be a local file"
+													" path or a URL."),
+									  required=False )
+	sourcePath = schema.TextLine( title="A local file path to the content",
+								  description=("If the content was a local file, or"
+											   " had to be downloaded to a temporary file"
+											   " that was preserved following metadata processing,"
+											   " this will be the path to that file."),
+								  required=False)
+
+class IContentMetadataExtractorArgs(interface.Interface):
+	"""
+	Arguments for extracting content metadata.
+	"""
+
+	stream = interface.Attribute( "A file-like object for reading the content" )
+	bidirectionalstream = interface.Attribute( "A file-like object for reading the content, supports seeking" )
+	bytes = interface.Attribute("Raw bytes of the content.")
+	text = interface.Attribute("Decoded text of the content.")
+
+class IContentMetadataExtractor(interface.Interface):
+	"""
+	Intended to be registered as named utilities with the name of the mimetype
+	they handle.
+	"""
+
+	def __call__( args ):
+		"""
+		Called with an :class:`IContentMetadataExtractorArgs`.
+		"""
