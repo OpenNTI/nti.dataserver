639eb30ab1aec29469b9257152dbf794e7a2e977
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-22 13:09:38 +0000

    bump pyramid
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@19431 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -233,7 +233,7 @@ setup(
 		# jquery-like traversing of python datastructures. lxml, cssselect
 		# optional dependency on 'restkit' for interactive WSGI stuff (used to be Paste)
 		'pyquery >= 1.2.4',
-		'pyramid >= 1.4.1' ,
+		'pyramid >= 1.4.2' ,
 		'pyramid_mailer >= 0.11',  # Which uses repoze.sendmail
 		'pyramid_who >= 0.3',
 		'pyramid_zcml >= 1.0.0',
