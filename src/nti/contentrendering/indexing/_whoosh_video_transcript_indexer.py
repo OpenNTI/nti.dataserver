8e6f08fa1f023a2160926a05c0574c5936f39497
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-22 18:31:50 +0000

    be more flexible looking for video nodes
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@19453 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/indexing/_whoosh_video_transcript_indexer.py
+++ src/nti/contentrendering/indexing/_whoosh_video_transcript_indexer.py
@@ -20,7 +20,7 @@ from nti.contentsearch import vtrans_prefix
 from nti.contentsearch import videotimestamp_to_datetime
 from nti.contentsearch import interfaces as search_interfaces
 
-from . import _node_utils as nu
+from . import _node_utils as node_utils
 from . import _content_utils as content_utils
 from . import interfaces as cridxr_interfaces
 from ._common_indexer import _BasicWhooshIndexer
@@ -43,43 +43,61 @@ class _WhooshVideoTranscriptIndexer(_BasicWhooshIndexer):
 		result = None
 		for obj in node.iterchildren():
 			if obj.tag != 'object': continue
-			type_ = nu.get_attribute(obj, 'type')
-			itemprop = nu.get_attribute(obj, 'itemprop')
+			type_ = node_utils.get_attribute(obj, 'type')
+			itemprop = node_utils.get_attribute(obj, 'itemprop')
 			if itemprop == 'nti-slide-video' and type_ == 'application/vnd.nextthought.slidevideo':
 				result = {}
 				for p in obj.iterchildren():
 					if p.tag != 'param': continue
-					name = nu.get_attribute(p, 'name')
-					value = nu.get_attribute(p, 'value')
+					name = node_utils.get_attribute(p, 'name')
+					value = node_utils.get_attribute(p, 'value')
 					if name and value:
 						result[name] = value
 				break
 		return result
 
+	def _find_video_transcript(self, base_location, bases, parser_names):
+		for location in ('.', '../Transcripts', './Transcripts'):
+			path = os.path.join(base_location, location)
+			if not os.path.exists(path):
+				continue
+			for pn in parser_names:
+				ext = '.' + pn
+				for basename in bases:
+					fname = basename + ext
+					video_path = os.path.join(path, fname)
+					if os.path.exists(video_path):
+						return pn, video_path
+		return None
+
 	def _get_externalvideo_info(self, topic, node):
-		info = self._parse_externalvideo_node(node)
-		if not info:
-			return
+		type_ = node_utils.get_attribute(node, 'type')
+		if type_ == u'application/vnd.nextthought.slidevideo':
+			result = {}
+			for p in node.iterchildren():
+				if p.tag != 'param': continue
+				name = node_utils.get_attribute(p, 'name')
+				value = node_utils.get_attribute(p, 'value')
+				if name and value:
+					result[name] = value
 
-		video_id = info.get('id')
-		video_type = info.get('type')
-		video_ntiid = info.get('ntiid')
-		parser_names = self._get_video_transcript_parser_names()
-		content_path = os.path.dirname(topic.location)
+			video_id = result.get('id')
+			video_type = result.get('type')
+			video_ntiid = result.get('ntiid')
+			parser_names = self._get_video_transcript_parser_names()
+			content_path = os.path.dirname(topic.location)
 
-		bases = [video_ntiid]
-		if video_type == 'youtube':
-			bases.append(video_id)
+			# collect video-base names
+			bases = {video_ntiid}
+			if video_type == 'youtube':
+				bases.add(video_id)
+			bases.discard(None)
+			
+			vid_tupl = self._find_video_transcript(content_path, bases, parser_names)
+			if vid_tupl:
+				parser_name, video_path = vid_tupl
+				return (parser_name, video_ntiid, video_path)
 
-		# search for filenames
-		for pn in parser_names:
-			ext = '.' + pn
-			for basename in bases:
-				if not basename: continue
-				fname = basename + ext
-				vpath = os.path.join(content_path, fname)
-				if os.path.exists(vpath):
-					return (pn, video_ntiid, vpath)
 		return None
 
 	def index_transcript_entry(self, writer, containerId, video_id, entry, language=u'en'):
@@ -107,14 +125,14 @@ class _WhooshVideoTranscriptIndexer(_BasicWhooshIndexer):
 	def process_topic(self, idxspec, topic, writer, language='en'):
 		count = 0
 		containerId = unicode(topic.ntiid)
-		for n in topic.dom(b'div').filter(b'.externalvideo'):
+		for n in topic.dom(b'object'):
 			info = self._get_externalvideo_info(topic, n)
 			if not info:
 				continue
 
-			pname, video_ntiid, vpath = info
+			pname, video_ntiid, video_path = info
 			parser = component.getUtility(media_interfaces.IVideoTranscriptParser, name=pname)
-			with open(vpath, "r") as source:
+			with open(video_path, "r") as source:
 				transcript = parser.parse(source)
 
 			video_ntiid = unicode(video_ntiid)
