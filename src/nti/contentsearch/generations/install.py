5f4a3eb15c60f56d656569e7dceb1fb58785707c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-24 22:05:27 +0000

    migration -> add title column to note catalogs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@19551 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/generations/install.py
+++ src/nti/contentsearch/generations/install.py
@@ -7,7 +7,7 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-generation = 25
+generation = 26
 
 from zope.generations.generations import SchemaManager
 
