aad34c052dba975828ea0dc21c8d72b280ecab3e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-28 19:07:29 +0000

    include dfls
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@19584 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/utils/nti_add_title_2_note_catalog.py
+++ src/nti/contentsearch/utils/nti_add_title_2_note_catalog.py
@@ -18,9 +18,20 @@ from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver import interfaces as nti_interfaces
 
 import nti.contentsearch
+from nti.contentsearch.utils import find_user_dfls
 from nti.contentsearch import _repoze_index
 from nti.contentsearch.constants import (note_, title_)
 from nti.contentsearch import interfaces as search_interfaces
+
+def _update_rim(entity):
+	rim = search_interfaces.IRepozeEntityIndexManager(entity, None)
+	catalog = rim.get(note_, None)  if rim is not None else None
+	if catalog is None:
+		continue
+
+	if title_ not in catalog:
+		_repoze_index._title_field_creator(catalog, title_, search_interfaces.INoteRepozeCatalogFieldCreator)
+		print("Title column added to note catalog for '%s'" % entity)
 
 def _update():
 
@@ -29,14 +40,9 @@ def _update():
 
 	for user in _users.values():
 		try:
-			rim = search_interfaces.IRepozeEntityIndexManager(user, None)
-			catalog = rim.get(note_, None)  if rim is not None else None
-			if catalog is None:
-				continue
-
-			if title_ not in catalog:
-				_repoze_index._title_field_creator(catalog, title_, search_interfaces.INoteRepozeCatalogFieldCreator)
-				print("Title column added to note catalog for user '%s'" % user)
+			_update_rim(user)
+			for dfl in find_user_dfls(user):
+				_update_rim(dfl)
 
 		except POSKeyError:
 			# broken reference for user
