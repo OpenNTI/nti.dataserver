1fc69658e926813634dc7dc71ee40aad0c6952de
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-29 22:25:58 +0000

    add a new transcript sample webvtt file
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@19642 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/media/tests/test_video_transcripts_parsers.py
+++ src/nti/contentrendering/media/tests/test_video_transcripts_parsers.py
@@ -22,7 +22,7 @@ from hamcrest import (assert_that, has_length, is_not, none, is_)
 class TestVideoTranscriptParser(ConfiguringTestBase):
 
 	def test_srt_parser(self):
-		path = os.path.join(os.path.dirname(__file__), 'automatic_captions_systemic_risk_drivers.srt')
+		path = os.path.join(os.path.dirname(__file__), 'transcripts/automatic_captions_systemic_risk_drivers.srt')
 		parser = component.getUtility(media_interfaces.IVideoTranscriptParser, name="srt")
 		with open(path, "rt") as source:
 			transcript = parser.parse(source)
@@ -35,7 +35,7 @@ class TestVideoTranscriptParser(ConfiguringTestBase):
 			assert_that(e.transcript, is_not(none()))
 
 	def test_sbv_parser(self):
-		path = os.path.join(os.path.dirname(__file__), 'nextthought_captions_002_000.sbv')
+		path = os.path.join(os.path.dirname(__file__), 'transcripts/nextthought_captions_002_000.sbv')
 		parser = component.getUtility(media_interfaces.IVideoTranscriptParser, name="sbv")
 		with open(path, "rt") as source:
 			transcript = parser.parse(source)
@@ -45,8 +45,8 @@ class TestVideoTranscriptParser(ConfiguringTestBase):
 			assert_that(e, is_not(none()))
 			assert_that(e.transcript, is_not(none()))
 
-	def test_webvtt_parser(self):
-		path = os.path.join(os.path.dirname(__file__), 'sample_web.vtt')
+	def test_webvtt_parser_1(self):
+		path = os.path.join(os.path.dirname(__file__), 'transcripts/sample_web.vtt')
 		parser = component.getUtility(media_interfaces.IVideoTranscriptParser, name="vtt")
 		with open(path, "rt") as source:
 			transcript = parser.parse(source)
@@ -56,3 +56,11 @@ class TestVideoTranscriptParser(ConfiguringTestBase):
 			assert_that(e, is_not(none()))
 			assert_that(e.transcript, is_not(none()))
 		assert_that(e.transcript, is_('Peter Griffin'))
+
+	def test_webvtt_parser_2(self):
+		path = os.path.join(os.path.dirname(__file__), 'transcripts/abcdef.vtt')
+		parser = component.getUtility(media_interfaces.IVideoTranscriptParser, name="vtt")
+		with open(path, "rt") as source:
+			transcript = parser.parse(source)
+		assert_that(transcript, is_not(none()))
+		assert_that(transcript, has_length(10))
