46a61a41f56f936fc5e2460e24c225370481c45d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-30 23:13:01 +0000

    remove unicode_literals for the time being
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@19694 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/_latexplastexconverter.py
+++ src/nti/assessment/_latexplastexconverter.py
@@ -4,7 +4,7 @@ Convenient functions for creating simple math doms from latex expressions.
 
 $Id$
 """
-from __future__ import unicode_literals, print_function, absolute_import
+from __future__ import print_function, absolute_import
 __docformat__ = "restructuredtext en"
 
 import sys
