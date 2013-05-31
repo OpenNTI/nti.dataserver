3dbe9d0b7909c64350d02ab0ff94dfb3d12b2c80
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-31 03:43:12 +0000

    # -*- coding: utf-8 -*-
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@19713 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/_latexplastexdomcompare.py
+++ src/nti/assessment/_latexplastexdomcompare.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Support functions for comparing latex Math DOMs using PlasTeX
 
 $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
 logger = __import__('logging').getLogger(__name__)
 
 from sympy.parsing import sympy_parser
