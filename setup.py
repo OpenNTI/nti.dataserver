a7e34b09dd336745c4ec93dbd23ab3510a5dc4a3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-06-02 18:38:03 +0000

    bump feedgenerator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@19769 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -187,8 +187,8 @@ setup(
 		'dolmen.builtins >= 0.3.1',  # interfaces for common python types
 		'filechunkio >= 1.5',  # Req'd for multi-put in boto == 2.5.2
 		# A very simple (one module, no deps) RSS and Atom feed generator.
-		# 1.5 is a modern rewrite with much better unicode and Py3k support
-		'feedgenerator == 1.5',
+		# 1.6 is a modern rewrite with much better unicode and Py3k support
+		'feedgenerator == 1.6',
 		'futures >= 2.1.3',
 		# 'gevent == 1.0rc1', Coming from requirements.txt right now
 		'greenlet >= 0.4.0',
