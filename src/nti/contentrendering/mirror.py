eb8f5f425c96c1ff858bb828df32472f6eef8822
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-06-06 17:01:18 +0000

    clean up and formatting
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@19954 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/mirror.py
+++ src/nti/contentrendering/mirror.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Creates an archive for a book assets.
+
+$Id$
+"""
+from __future__ import print_function, absolute_import
+__docformat__ = "restructuredtext en"
+
 import os
 import six
 import sys
@@ -6,18 +16,43 @@ import shutil
 import socket
 import thread
 import tempfile
-import html5lib
 import subprocess
-
+import SocketServer
+import SimpleHTTPServer
 from urlparse import urljoin
 from urlparse import urlparse
 
 from pyquery import PyQuery as pq
 
-import logging
-logger = logging.getLogger( __name__ )
+logger = __import__('logging').getLogger(__name__)
 
-WGET_CMD = '/opt/local/bin/wget -q'
+WGET_CMD = 'wget -q'
+
+def _execute_cmd(args):
+	cmd = ' '.join(args)
+	retcode = subprocess.call(cmd, shell=True)
+	if retcode != 0:
+		raise Exception("Fail to execute '%s'" % cmd)
+	return True
+
+def _remove_file(target):
+	try:
+		os.remove(target)
+	except OSError:
+		pass
+
+def _create_path(path):
+	path = os.path.expanduser(path)
+	if not os.path.exists(path):
+		os.makedirs(path)
+	return path
+
+def _is_valid_url(url_or_path):
+	try:
+		pr = urlparse(url_or_path)
+		return pr.scheme in ('http', 'https') and pr.netloc
+	except:
+		return False
 
 def get_open_port():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,87 +63,36 @@ def get_open_port():
 	finally:
 		s.close()
 
-def main(url_or_path,
-		 out_dir="/tmp/mirror",
-		 zip_archive=True,
-		 zip_root_dir=None,
-		 process_links=True,
-		 archive_index=False, # The Whoosh search index files, not 'index.html'
-		 port=None):
-
-	httpd = None
-	port = port or get_open_port()
-	try:
-		result = False
-
-		if not _is_valid_url(url_or_path):
-			httpd = _launch_server(url_or_path, port)
-			url = "http://localhost:%s" % port
-		else:
-			httpd = None
-			url = url_or_path
-			url = url[:-1] if url[-1] =='/' else url
-
-		out_dir = _create_path(out_dir)
-
-		if zip_archive:
-			archive_dir = tmp_dir = _create_path(tempfile.mkdtemp())
-		else:
-			tmp_dir = None
-			archive_dir = out_dir
-
-
-		result = _get_url_content(url, archive_dir) and _get_toc_file(url, archive_dir)
-		if result:
-			toc_file = os.path.join(archive_dir, 'eclipse-toc.xml')
-			_process_toc_file(url, archive_dir, process_links, toc_file)
-			if archive_index:
-				_get_index_dir(url, archive_dir)
-
-		if zip_archive:
-			zip_name = zip_archive if isinstance(zip_archive, six.string_types) else 'archive.zip'
-			_zip_archive(archive_dir, out_dir, zip_name, zip_root_dir)
-
-		return result
-	finally:
-		if httpd:
-			httpd.shutdown()
-			httpd.server_close()
-
-		if result:
-			if tmp_dir:
-				shutil.rmtree(tmp_dir, ignore_errors=True)
-
-
 def _zip_archive(source_path, out_dir, zip_name="archive.zip", zip_root_dir=None):
 
-	logger.info( "Archiving '%s' to '%s'", source_path, zip_name )
+	logger.info("Archiving '%s' to '%s'", source_path, zip_name)
 
-	zip_cmd = "zip -q -9 -r %s %s/*" % (zip_name, source_path)
 	new_dir = None
+	zip_cmd = "zip -q -9 -r %s %s/*" % (zip_name, source_path)
 	if zip_root_dir and os.path.split( source_path )[-1] != zip_root_dir:
 		# If they ask for a zip name (and we do not already match it)
 		# then rename the archive dir to match (in the same directory)
-		new_dir = os.path.join( os.path.split( source_path )[0], zip_root_dir )
+		new_dir = os.path.join(os.path.split(source_path)[0], zip_root_dir)
 		__traceback_info__ = locals()
 		try:
-			os.rename( source_path, new_dir )
+			os.rename(source_path, new_dir)
 		except OSError:
 			# TODO: This entire approach is very fragile. We're relying
 			# on a directory one level up from the
 			logger.exception( "Failed to rename %s to %s", source_path, new_dir )
 			raise
-		zip_cmd = "cd %s && zip -q -9 -r %s %s && mv %s %s" % (os.path.split( source_path )[0], zip_name, zip_root_dir, zip_name, os.path.realpath( os.curdir ))
 
+		zip_cmd = "cd %s && zip -q -9 -r %s %s && mv %s %s" % \
+				  (os.path.split(source_path)[0], zip_name, zip_root_dir, zip_name, os.path.realpath(os.curdir))
+
+	# execute zip cmd
 	os.system( zip_cmd )
 
+	# move it back
 	if new_dir:
-		# move it back
-		os.rename( new_dir, source_path )
-
+		os.rename(new_dir, source_path)
 
 def _process_toc_file(url, out_dir, process_links, toc_file='eclipse-toc.xml'):
-
 	e = pq(filename=toc_file)
 	# Mirror the main index and each referenced page (topic)
 	# Then mirror things only referenced in related items: they may have their
@@ -117,10 +101,9 @@ def _process_toc_file(url, out_dir, process_links, toc_file='eclipse-toc.xml'):
 	# make sure not to fetch dups
 	accum = set()
 	e('toc,topic,page,video').map(lambda i,e: _process_node(e, url, out_dir, process_links, accum))
-
 	return True
 
-def _process_node(node, url, out_dir, process_links,set_of_hrefs=None):
+def _process_node(node, url, out_dir, process_links, set_of_hrefs=None):
 	# note things we've fetched (minus fragments) and don't fetch again
 	if set_of_hrefs is None: set_of_hrefs = set()
 	result = True
@@ -139,7 +122,9 @@ def _process_node(node, url, out_dir, process_links,set_of_hrefs=None):
 		# strip the fragment, if any
 		try:
 			value = value[0:value.index('#')]
-		except (ValueError,AttributeError):	pass
+		except (ValueError, AttributeError):
+			pass
+
 		if value and value not in set_of_hrefs:
 			# TODO: We should only be handling relative links here. When
 			# we get absolute links, we'll be in trouble
@@ -158,30 +143,10 @@ def _handle_attribute(target, url, out_dir, force_html=False):
 def _get_toc_file(url, out_dir, toc_file='eclipse-toc.xml'):
 	return _get_file(url, out_dir, toc_file, True)
 
-def _get_index_dir(url, out_dir):
-
-	tmp = tempfile.mktemp(".html", "index", out_dir)
-	try:
-		url2 = urljoin(url + '/', 'indexdir')
-		args = [WGET_CMD, '-O %s' % tmp, url2]
-		_execute_cmd(args)
-
-		with open(tmp, "r") as source:
-			doc = html5lib.parse(source, encoding="utf-8")
-			for node in doc:
-				if node.type == 5 and node.name == 'a':
-					attributes = node.attributes
-					href = attributes.get('href', None)
-					if href and (href.startswith('_') or href.endswith('_WRITELOCK')):
-						target = "indexdir/" + href
-						_get_file(url, os.path.join(out_dir,"indexdir"), target, False)
-	finally:
-		_remove_file(tmp)
-
-import SimpleHTTPServer
-import SocketServer
-
 def _launch_server(data_path, port=None):
+	""" 
+	Launch in a separatred thread a local http server to serve files from the specified path
+	"""
 
 	if not os.path.exists(data_path):
 		raise Exception("'%s' does not exists" % data_path)
@@ -203,10 +168,14 @@ def _launch_server(data_path, port=None):
 	thread.start_new_thread(worker, ())
 	return httpd
 
+def _get_cut_dirs(url):
+	r = urlparse(url)
+	s = r.path.split('/')
+	return len(s) - 1
+
 def _get_url_content(url, out_dir="/tmp"):
 	"""
-	Get as much as possible content from the
-	specified URL
+	Get as much as possible content from the specified URL
 	"""
 
 	# -m   --mirror
@@ -222,7 +191,6 @@ def _get_url_content(url, out_dir="/tmp"):
 		args.append('--cut-dirs=%s' % cut_dirs)
 	args.append('-P %s' % out_dir)
 	args.append(url)
-
 	_execute_cmd(args)
 	return glob.glob(out_dir + "*.html") > 0
 
@@ -253,37 +221,43 @@ def _get_file(url, out_dir, target, force_html=False):
 	target = os.path.split(target)[1]
 	return os.path.exists(os.path.join(out_dir, target))
 
-def _get_cut_dirs(url):
-	r = urlparse(url)
-	s = r.path.split('/')
-	return len(s) - 1
-
-def _execute_cmd(args):
-	cmd = ' '.join(args)
-	retcode = subprocess.call(cmd, shell=True)
-	if retcode != 0:
-		raise Exception("Fail to execute '%s'" % cmd)
-
-	return True
-
-def _remove_file(target):
+def main(url_or_path, out_dir=None, zip_archive=True, zip_root_dir=None, process_links=True, port=None):
+	result = False
+	httpd = tmp_dir = None
+	port = port or get_open_port()
+	out_dir = out_dir or "/tmp/mirror"
 	try:
-		os.remove(target)
-	except OSError:
-		pass
+		if not _is_valid_url(url_or_path):
+			httpd = _launch_server(url_or_path, port)
+			url = "http://localhost:%s" % port
+		else:
+			url = url_or_path
+			url = url[:-1] if url[-1] == '/' else url
 
-def _create_path(path):
-	path = os.path.expanduser(path)
-	if not os.path.exists(path):
-		os.makedirs(path)
-	return path
+		out_dir = _create_path(out_dir)
 
-def _is_valid_url(url_or_path):
-	try:
-		pr = urlparse(url_or_path)
-		return pr.scheme == 'http' and pr.netloc
-	except:
-		return False
+		if zip_archive:
+			archive_dir = tmp_dir = _create_path(tempfile.mkdtemp())
+		else:
+			archive_dir = out_dir
+
+		result = _get_url_content(url, archive_dir) and _get_toc_file(url, archive_dir)
+		if result:
+			toc_file = os.path.join(archive_dir, 'eclipse-toc.xml')
+			_process_toc_file(url, archive_dir, process_links, toc_file)
+
+		if zip_archive:
+			zip_name = zip_archive if isinstance(zip_archive, six.string_types) else 'archive.zip'
+			_zip_archive(archive_dir, out_dir, zip_name, zip_root_dir)
+
+		return result
+	finally:
+		if httpd:
+			httpd.shutdown()
+			httpd.server_close()
+
+		if result and tmp_dir:
+			shutil.rmtree(tmp_dir, ignore_errors=True)
 
 def _run_main():
 	args = sys.argv[1:]
