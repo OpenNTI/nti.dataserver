6be7aadb4c0bb91bfda664afe59033171600d7db
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-06-13 23:56:16 +0000

    subscriber w/o code for questions
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@20182 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/salesforce/subscribers.py
+++ src/nti/salesforce/subscribers.py
@@ -9,3 +9,11 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import component
+from zope.lifecycleevent.interfaces import IObjectCreatedEvent
+
+from nti.assessment import interfaces as as_interfaces
+
+@component.adapter(as_interfaces.IQAssessedQuestion, IObjectCreatedEvent)
+def question_assessed(question, event):
+	pass
