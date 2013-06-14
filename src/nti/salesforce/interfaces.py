a756c4ec8229e7dfa4fa3a82e1339d8bf4d83a06
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-06-14 05:37:40 +0000

    execute w/ valid section
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@20228 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/salesforce/interfaces.py
+++ src/nti/salesforce/interfaces.py
@@ -12,7 +12,7 @@ from zope import interface
 from nti.utils import schema as nti_schema
 
 class ISalesforceUser(interface.Interface):
-    userId = nti_schema.ValidTextLine(title='Salesforce userID', required=True)
+    UserID = nti_schema.ValidTextLine(title='Salesforce UserID', required=True)
 
 class ISalesforceApplication(interface.Interface):
     ClientID = nti_schema.ValidTextLine(title='Client id', required=True)
