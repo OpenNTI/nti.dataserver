4d9e294ef7937b8db4a157777d6001de393d000a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-06-18 18:29:45 +0000

    start preparing post content
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@20321 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/salesforce/subscribers.py
+++ src/nti/salesforce/subscribers.py
@@ -12,8 +12,26 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope.lifecycleevent.interfaces import IObjectCreatedEvent
 
+from pyramid.security import authenticated_userid
+from pyramid.threadlocal import get_current_request
+
 from nti.assessment import interfaces as as_interfaces
 
+from nti.ntiids.ntiids import find_object_with_ntiid
+
+from .chatter import Chatter
+
 @component.adapter(as_interfaces.IQAssessedQuestion, IObjectCreatedEvent)
-def question_assessed(question, event):
-	pass
+def question_assessed(assessed_question, event):
+	question = find_object_with_ntiid(assessed_question.questionId)
+	content = getattr(question, 'content', None)
+	if not content:
+		text = []
+		for q_part in question.parts:
+			text.append(getattr(q_part, 'content', ''))
+		content = ''.join(text)
+
+	# TODO: get [access/refresh] token
+	username = authenticated_userid(get_current_request())
+	chatter = Chatter(username)
+	chatter.post_text_news_feed_item(content)
