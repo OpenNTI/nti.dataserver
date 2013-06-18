cdd9247fac1686e709492874d75d0d57f53acd5a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-06-18 14:05:22 +0000

    create a custom SalesforceException
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@20314 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/salesforce/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Salesforce module
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+class SalesforceException(Exception):
+    pass
