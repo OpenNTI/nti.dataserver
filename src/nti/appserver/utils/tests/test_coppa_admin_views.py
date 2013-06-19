41cf307b49627802488ad5efeb5388e8dd790327
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-06-19 20:14:37 +0000

    start adding a view to upgrade a coppa user. Need to get details
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@20353 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/tests/test_coppa_admin_views.py
+++ src/nti/appserver/utils/tests/test_coppa_admin_views.py
@@ -13,6 +13,8 @@ from nti.appserver.link_providers import flag_link_provider
 from nti.dataserver import users
 from nti.dataserver import interfaces as nti_interfaces
 
+from nti.externalization.externalization import to_json_representation
+
 from nti.appserver.tests.test_application import TestApp
 
 from nti.dataserver.tests import mock_dataserver
@@ -23,7 +25,7 @@ from hamcrest import (assert_that, is_, has_length, has_entry)
 class TestCoppaAdminViews(SharedApplicationTestBase):
 
 	@WithSharedApplicationMockDS
-	def test_rollback(self):
+	def xtest_rollback(self):
 		with mock_dataserver.mock_db_trans(self.ds):
 			self._create_user()
 			u = self._create_user(username='aizen@nt.com', external_value={u'email':u"nti@nt.com", u'opt_in_email_communication':True})
@@ -54,5 +56,22 @@ class TestCoppaAdminViews(SharedApplicationTestBase):
 		with mock_dataserver.mock_db_trans(self.ds):
 			u = users.User.get_user('aizen@nt.com')
 			assert_that(flag_link_provider.has_link(u, 'coppa.upgraded.rollbacked'), is_(True))
+			
+	@WithSharedApplicationMockDS
+	def test_upgrade_preflight_coppa_user(self):
+		with mock_dataserver.mock_db_trans(self.ds):
+			u = self._create_user()
+			interface.alsoProvides(u, nti_interfaces.ICoppaUser)
 
-	
+		testapp = TestApp(self.app)
+
+		path = '/dataserver2/users/sjohnson@nextthought.com/@@upgrade_preflight_coppa_user'
+		environ = self._make_extra_environ()
+		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
+		
+		data = to_json_representation( {'Username': 'aizen@nt.com',
+										'birthdate': '2007-11-30',
+										'realname': 'Aizen',
+										'email': 'aizen@bleach.com',} )
+		
+		testapp.post(path, data, extra_environ=environ)
