d6d0426f561ab880469de483ff27464595d5208b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-06-19 16:25:34 +0000

    add unit test for rollback_coppa_users
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@20344 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/coppa_admin_views.py
+++ src/nti/appserver/utils/coppa_admin_views.py
@@ -10,10 +10,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import simplejson
-
 from pyramid.view import view_config
-from pyramid import httpexceptions as hexc
 
 from zope import component
 from zope import interface
@@ -22,7 +19,6 @@ from nti.dataserver import users
 from nti.dataserver import authorization as nauth
 from nti.dataserver import interfaces as nti_interfaces
 
-from nti.appserver.utils import is_true
 from nti.appserver.utils import _JsonBodyView
 from nti.appserver.link_providers import flag_link_provider
 
@@ -36,27 +32,23 @@ _view_admin_defaults['permission'] = nauth.ACT_COPPA_ADMIN
 _post_view_defaults = _view_defaults.copy()
 _post_view_defaults['request_method'] = 'POST'
 
-_admin_view_defaults = _post_view_defaults.copy()
-_admin_view_defaults['permission'] = nauth.ACT_COPPA_ADMIN
-
-@view_config(name="rollback_coppa_upgraded_users", **_admin_view_defaults)
-class RollbackCoppaUpgradedUsers(_JsonBodyView):
+@view_config(name="rollback_coppa_users", **_post_view_defaults)
+class RollbackCoppaUsers(_JsonBodyView):
 
 	def __call__(self):
 		values = self.readInput()
 		usernames = values.get('usernames', '')
-		all_coppa = values.get('all', 'F')
 		
-		if is_true(all_coppa):
+		if usernames:
+			usernames = usernames.split(',')
+		else:
 			dataserver = component.getUtility( nti_interfaces.IDataserver)
 			usernames = nti_interfaces.IShardLayout(dataserver).users_folder.keys()
-		else:
-			usernames = usernames.split(',')
 
 		items = []
 		for username in usernames:
 			user = users.User.get_user(username)
-			if not user or not nti_interfaces.ICoppaUserWithAgreementUpgraded.providedBy(user):
+			if user is None or not nti_interfaces.ICoppaUser.providedBy(user):
 				continue
 
 			items.append(username)
@@ -69,12 +61,8 @@ class RollbackCoppaUpgradedUsers(_JsonBodyView):
 			# add link
 			flag_link_provider.add_link(user, 'coppa.upgraded.rollbacked')
 			
-		response = self.request.response
-		response.content_type = b'application/json; charset=UTF-8'
-		response.body = simplejson.dumps({'Count':len(items), 'Items':items})
-		return hexc.HTTPNoContent()
+		return {'Count':len(items), 'Items':items}
 
 del _view_defaults
 del _post_view_defaults
-del _admin_view_defaults
 del _view_admin_defaults
---
+++ src/nti/appserver/utils/tests/test_coppa_admin_views.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+import simplejson
+
+from zope import interface
+
+from nti.appserver.link_providers import flag_link_provider
+
+from nti.dataserver import users
+from nti.dataserver import interfaces as nti_interfaces
+
+from nti.appserver.tests.test_application import TestApp
+
+from nti.dataserver.tests import mock_dataserver
+from nti.appserver.tests.test_application import SharedApplicationTestBase, WithSharedApplicationMockDS
+
+from hamcrest import (assert_that, is_, has_length, has_entry)
+
+class TestCoppaAdminViews(SharedApplicationTestBase):
+
+	@WithSharedApplicationMockDS
+	def test_rollback(self):
+		with mock_dataserver.mock_db_trans(self.ds):
+			self._create_user()
+			u = self._create_user(username='aizen@nt.com', external_value={u'email':u"nti@nt.com", u'opt_in_email_communication':True})
+			interface.alsoProvides(u, nti_interfaces.ICoppaUser)
+			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithAgreementUpgraded)
+
+			u = self._create_user(username='rukia@nt.com',
+								  external_value={u'email':u'rukia@nt.com', u'opt_in_email_communication':True})
+			interface.alsoProvides(u, nti_interfaces.ICoppaUser)
+			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithoutAgreement)
+
+			u = self._create_user(username='ichigo@nt.com',
+								  external_value={u'email':u'ichigo@nt.com', u'opt_in_email_communication':True})
+			interface.alsoProvides(u, nti_interfaces.ICoppaUser)
+
+		testapp = TestApp(self.app)
+
+		path = '/dataserver2/@@rollback_coppa_users'
+		environ = self._make_extra_environ()
+		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
+		
+		res = testapp.post(path,  extra_environ=environ)
+		assert_that(res.status_int, is_(200))
+		body = simplejson.loads(res.body)
+		assert_that(body, has_entry('Count', 3))
+		assert_that(body, has_entry('Items', has_length(3)))
+		
+		with mock_dataserver.mock_db_trans(self.ds):
+			u = users.User.get_user('aizen@nt.com')
+			assert_that(flag_link_provider.has_link(u, 'coppa.upgraded.rollbacked'), is_(True))
+
+	
