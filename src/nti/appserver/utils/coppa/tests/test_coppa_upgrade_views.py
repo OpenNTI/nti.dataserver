0d614fa148a659c485061e45242e42f54fb9b873
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-06-20 17:00:52 +0000

    add uni tests for view to upgrade coppa user
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@20367 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/coppa/tests/test_coppa_upgrade_views.py
+++ src/nti/appserver/utils/coppa/tests/test_coppa_upgrade_views.py
@@ -7,7 +7,9 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+
 import simplejson
+from datetime import date
 
 from zope import interface
 
@@ -15,6 +17,7 @@ from nti.appserver.link_providers import flag_link_provider
 
 from nti.dataserver import users
 from nti.dataserver import interfaces as nti_interfaces
+from nti.dataserver.users import interfaces as users_interfaces
 
 from nti.externalization.externalization import to_json_representation
 
@@ -23,7 +26,7 @@ from nti.appserver.tests.test_application import TestApp
 from nti.dataserver.tests import mock_dataserver
 from nti.appserver.tests.test_application import SharedApplicationTestBase, WithSharedApplicationMockDS
 
-from hamcrest import (assert_that, is_, has_length, has_entry)
+from hamcrest import (assert_that, is_, has_length, has_entry, has_key)
 
 class TestCoppaUpgradeViews(SharedApplicationTestBase):
 
@@ -61,7 +64,31 @@ class TestCoppaUpgradeViews(SharedApplicationTestBase):
 			assert_that(flag_link_provider.has_link(u, 'coppa.upgraded.rollbacked'), is_(True))
 			
 	@WithSharedApplicationMockDS
-	def test_upgrade_preflight_coppa_user(self):
+	def test_upgrade_preflight_coppa_user_under_13(self):
+		with mock_dataserver.mock_db_trans(self.ds):
+			u = self._create_user()
+			interface.alsoProvides(u, nti_interfaces.ICoppaUser)
+
+		testapp = TestApp(self.app)
+
+		path = '/dataserver2/users/sjohnson@nextthought.com/@@upgrade_preflight_coppa_user'
+		environ = self._make_extra_environ()
+		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
+
+		data = to_json_representation({'Username': 'sjohnson@nextthought.com',
+									   'birthdate': '2007-11-30',
+									   'realname': 'Aizen',
+									   'contact_email': 'aizen@bleach.com', })
+
+		res = testapp.post(path, data, extra_environ=environ)
+		assert_that(res.status_int, is_(200))
+		body = simplejson.loads(res.body)
+		assert_that(body, has_entry('Username', 'sjohnson@nextthought.com'))
+		assert_that(body, has_entry('ProfileSchema', has_key('birthdate')))
+		assert_that(body, has_entry('ProfileSchema', has_key('contact_email')))
+
+	@WithSharedApplicationMockDS
+	def test_upgrade_preflight_coppa_user_over_13(self):
 		with mock_dataserver.mock_db_trans(self.ds):
 			u = self._create_user()
 			interface.alsoProvides(u, nti_interfaces.ICoppaUser)
@@ -72,9 +99,76 @@ class TestCoppaUpgradeViews(SharedApplicationTestBase):
 		environ = self._make_extra_environ()
 		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
 		
-		data = to_json_representation( {'Username': 'aizen@nt.com',
-										'birthdate': '2007-11-30',
-										'realname': 'Aizen',
-										'email': 'aizen@bleach.com',} )
+		data = to_json_representation({'Username': 'sjohnson@nextthought.com',
+									   'birthdate': '1973-11-30',
+									   'realname': 'Aizen Sosuke',
+									   'email': 'aizen@bleach.com' })
 		
-		testapp.post(path, data, extra_environ=environ)
+		res = testapp.post(path, data, extra_environ=environ)
+		assert_that(res.status_int, is_(200))
+		body = simplejson.loads(res.body)
+		assert_that(body, has_entry('Username', 'sjohnson@nextthought.com'))
+		assert_that(body, has_entry('ProfileSchema', has_key('birthdate')))
+		assert_that(body, has_entry('ProfileSchema', has_key('email')))
+		assert_that(body, has_entry('ProfileSchema', has_key('realname')))
+
+	@WithSharedApplicationMockDS
+	def test_upgrade_coppa_user_over_13(self):
+		with mock_dataserver.mock_db_trans(self.ds):
+			u = self._create_user()
+			interface.alsoProvides(u, nti_interfaces.ICoppaUser)
+			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithoutAgreement)
+			flag_link_provider.add_link(u, 'coppa.upgraded.rollbacked')
+
+		testapp = TestApp(self.app)
+
+		path = '/dataserver2/users/sjohnson@nextthought.com/@@upgrade_coppa_user'
+		environ = self._make_extra_environ()
+		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
+
+		data = to_json_representation({'Username': 'sjohnson@nextthought.com',
+									   'birthdate': '1973-11-30',
+									   'realname': 'Aizen Sosuke',
+									   'email': 'aizen@bleach.com', })
+
+		res = testapp.post(path, data, extra_environ=environ)
+		assert_that(res.status_int, is_(204))
+
+		with mock_dataserver.mock_db_trans(self.ds):
+			u = users.User.get_user('sjohnson@nextthought.com')
+			assert_that(nti_interfaces.ICoppaUser.providedBy(u), is_(False))
+			assert_that(nti_interfaces.ICoppaUserWithoutAgreement.providedBy(u), is_(False))
+			profile = users_interfaces.IUserProfile(u)
+			assert_that(profile.email, is_('aizen@bleach.com'))
+			assert_that(flag_link_provider.has_link(u, 'coppa.upgraded.rollbacked'), is_(False))
+
+	@WithSharedApplicationMockDS
+	def test_upgrade_coppa_user_under_13(self):
+		with mock_dataserver.mock_db_trans(self.ds):
+			u = self._create_user()
+			interface.alsoProvides(u, nti_interfaces.ICoppaUser)
+			flag_link_provider.add_link(u, 'coppa.upgraded.rollbacked')
+
+		testapp = TestApp(self.app)
+
+		path = '/dataserver2/users/sjohnson@nextthought.com/@@upgrade_coppa_user'
+		environ = self._make_extra_environ()
+		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
+
+		today = date.today()
+		d = date(today.year - 5, today.month, today.day)
+
+		data = to_json_representation({'Username': 'sjohnson@nextthought.com',
+									   'birthdate': d.isoformat(),
+									   'contact_email': 'aizen@bleach.com', })
+
+		res = testapp.post(path, data, extra_environ=environ)
+		assert_that(res.status_int, is_(204))
+
+		with mock_dataserver.mock_db_trans(self.ds):
+			u = users.User.get_user('sjohnson@nextthought.com')
+			assert_that(nti_interfaces.ICoppaUser.providedBy(u), is_(True))
+			assert_that(nti_interfaces.ICoppaUserWithoutAgreement.providedBy(u), is_(True))
+			assert_that(flag_link_provider.has_link(u, 'coppa.upgraded.rollbacked'), is_(False))
+
+
