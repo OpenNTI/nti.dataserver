880e340ad50247781a5aea4f65d0916732f545a6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-07-01 23:51:12 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@20661 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/test_user_policies.py
+++ src/nti/appserver/tests/test_user_policies.py
@@ -8,7 +8,6 @@ __docformat__ = "restructuredtext en"
 # pylint: disable=W0212,R0904
 
 import pyPdf
-from pyPdf import PdfFileReader
 
 from nti.appserver import user_policies
 
@@ -21,14 +20,11 @@ class TestUserPolicies(SharedApplicationTestBase):
 	def test__alter_pdf(self):
 		stream = user_policies._alter_pdf('coppa_consent_request_email_attachment.pdf', 
 										  'supportmathcoppa69', 'Greg', 'greg.higgins@nextthought.com')
-		pdf_reader=PdfFileReader(stream)
+		pdf_reader = pyPdf.PdfFileReader(stream)
 		assert_that(pdf_reader.numPages, is_(1))
-		pdf_page = pdf_reader.getPage( 0 )
-		page_content = pyPdf.pdf.ContentStream( pdf_page['/Contents'].getObject(), pdf_page.pdf )
+		pdf_page = pdf_reader.getPage(0)
+		page_content = pyPdf.pdf.ContentStream(pdf_page['/Contents'].getObject(), pdf_page.pdf)
 		assert_that(page_content.operations[937][0], is_(['supportmathcoppa69']))
 		assert_that(page_content.operations[970][0], is_(['Greg']))
 		assert_that(page_content.operations[1021][0], is_(['greg.higgins@nextthought.com']))
-		
-		#stream.seek(0)
-		#with open("/tmp/p.pdf", "wb") as f:
-		#	f.write(stream.read())
+
