4b4b336a6ce23c781cf6f1a40b959aebf00d15f6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-07-21 13:10:15 +0000

    plane is late
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@21221 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/contenttypes/redaction.py
+++ src/nti/dataserver/contenttypes/redaction.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Definition of the redaction object.
-"""
 
-from __future__ import print_function, unicode_literals
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
 
 from nti.dataserver import interfaces as nti_interfaces
 
 from zope import interface
 from zope import component
-from nti.utils.schema import createDirectFieldProperties
 
 from .selectedrange import SelectedRange
+from .selectedrange import SelectedRangeInternalObjectIO
+
+from nti.utils.schema import createDirectFieldProperties
 
 @interface.implementer(nti_interfaces.IRedaction)
 class Redaction(SelectedRange):
-
-	createDirectFieldProperties(nti_interfaces.IRedaction) # replacementContent, redactionExplanation
-
-
-from .selectedrange import SelectedRangeInternalObjectIO
+	createDirectFieldProperties(nti_interfaces.IRedaction)  # replacementContent, redactionExplanation
 
 @component.adapter(nti_interfaces.IRedaction)
 class RedactionInternalObjectIO(SelectedRangeInternalObjectIO):
 	_ext_iface_upper_bound = nti_interfaces.IRedaction
 	validate_after_update = True
-	#_schema_fields_to_validate_ = SelectedRangeInternalObjectIO._schema_fields_to_validate_ + ('replacementContent','redactionExplanation')
-	#_schema_to_validate_ = nti_interfaces.IRedaction
