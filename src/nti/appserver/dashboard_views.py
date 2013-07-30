e12c05bbbb0ccafa865488c841a897fec04b9a5b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-07-30 01:58:11 +0000

    make sure we scan the dashboard views and process unique Circle events
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@21490 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/dashboard_views.py
+++ src/nti/appserver/dashboard_views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Views for user dashvoard
+Views for user dashboard
 
 $Id: ugd_query_views.py 20912 2013-07-13 16:15:43Z carlos.sanchez $
 """
@@ -16,6 +16,7 @@ from nti.appserver import _view_utils
 
 from nti.dataserver import interfaces as nti_interfaces
 
+from nti.externalization.oids import to_external_oid
 from nti.externalization.interfaces import LocatedExternalDict
 
 from .ugd_query_views import _UGDAndRecursiveStreamView
@@ -29,23 +30,38 @@ class _TopUserSummaryView(_view_utils.AbstractAuthenticatedView):
 			self.ntiid = the_ntiid or self.request.context.ntiid
 
 	def __call__( self ):
+		# query objects
+		view = _UGDAndRecursiveStreamView(self.request)
+		view._DEFAULT_BATCH_SIZE = view._DEFAULT_BATCH_START = None
+		all_objects = view.getObjectsForId(self.user, self.ntiid)
+
+		# loop and collect
+		seen = set()
 		by_type = collections.defaultdict(int)
 		result = LocatedExternalDict()
-		all_objects = _UGDAndRecursiveStreamView(self.request).getObjectsForId(self.user, self.ntiid)
 		for iterable in all_objects:
 			try:
 				to_iter = iterable.itervalues()
 			except (AttributeError, TypeError):
 				to_iter = iterable
 			
-			for o in to_iter:
+			for o in to_iter or ():
 
-				if not nti_interfaces.IModeledContent.providedBy(o) or nti_interfaces.IStreamChangeEvent.providedBy(o):
+				if nti_interfaces.IStreamChangeEvent.providedBy(o) and o.type in (nti_interfaces.SC_CREATED, nti_interfaces.SC_MODIFIED):
+					obj = o.object
+				elif not nti_interfaces.IModeledContent.providedBy(o) or nti_interfaces.IStreamChangeEvent.providedBy(o):
+					obj = o
+				else:
+					continue
+				
+				oid = to_external_oid(obj)
+				if oid in seen:
 					continue
 
-				creator = getattr(o, 'creator', None)
+				seen.add(oid)
+				creator = getattr(obj, 'creator', None)
 				creator = getattr(creator, 'username', creator)
-				mime_type = getattr(o, "mimeType", getattr(o, "mime_type", None))
+				mime_type = getattr(obj, "mimeType", getattr(obj, "mime_type", None))
 				
 				if creator and mime_type:
 					counter = result.get(creator)
