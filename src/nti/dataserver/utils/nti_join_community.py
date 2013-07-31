23a9a856f66d912a4e71c947ae92ec7b50c8247d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-07-31 00:51:30 +0000

    minor change
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@21584 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_join_community.py
+++ src/nti/dataserver/utils/nti_join_community.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Join community utility
 
-from __future__ import print_function, unicode_literals
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
 
 import sys
+import pprint
 import argparse
 
 from nti.dataserver import users
@@ -12,40 +19,40 @@ from nti.externalization.externalization import to_external_object
 def join_communities(user, communities=(), follow=False, exitOnError=False):
 	not_found = set()
 	for com_name in communities:
-		comm = users.Community.get_entity( com_name )
+		comm = users.Community.get_entity(com_name)
 		if not comm:
 			not_found.add(com_name)
 			if exitOnError: break
-		user.record_dynamic_membership( comm )
+		user.record_dynamic_membership(comm)
 		if follow:
 			user.follow( comm )
 
 	return tuple(not_found)
 
 def _process_args( args ):
-	user = users.User.get_user( args.username )
+	user = users.User.get_user(args.username)
 	if not user:
-		print( "No user found", args, file=sys.stderr )
+		print("No user found", args, file=sys.stderr)
 		sys.exit( 2 )
 
 	not_found = join_communities(user, args.communities, args.follow, True)
 	if not_found:
-		print( "No community found", args, file=sys.stderr )
+		print("No community found", args, file=sys.stderr)
 		sys.exit( 3 )
 
 	if args.verbose:
-		print( to_external_object( user ) )
+		pprint.pprint(to_external_object(user))
 
 def main():
 	arg_parser = argparse.ArgumentParser( description="Join one or more existing communities" )
-	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
-	arg_parser.add_argument( 'username', help="The username that should join communities" )
-	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
-	arg_parser.add_argument( '-f', '--follow', help="Also follow the communities", action='store_true', dest='follow')
-	arg_parser.add_argument( '-c', '--communities',
-							 dest='communities',
-							 nargs="+",
-							 help="The usernames of the communities to join" )
+	arg_parser.add_argument('env_dir', help="Dataserver environment root directory")
+	arg_parser.add_argument('username', help="The username that should join communities")
+	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
+	arg_parser.add_argument('-f', '--follow', help="Also follow the communities", action='store_true', dest='follow')
+	arg_parser.add_argument('-c', '--communities',
+							dest='communities',
+							nargs="+",
+							help="The usernames of the communities to join")
 	args = arg_parser.parse_args()
 
 	env_dir = args.env_dir
