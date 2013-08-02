1a4fcc958636a644ca90ee658e2a2efb190aa8c3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-02 03:39:28 +0000

    gather data from the quizzes containers when getting summary data
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@21768 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/dashboard_views.py
+++ src/nti/appserver/dashboard_views.py
@@ -12,15 +12,21 @@ logger = __import__('logging').getLogger(__name__)
 
 import collections
 
+from zope import component
+
+import pyramid.httpexceptions as _hexc
+
 from nti.appserver import _view_utils
+
+from nti.contentlibrary import interfaces as lib_interfaces
 
 from nti.dataserver import interfaces as nti_interfaces
 
 from nti.externalization.oids import to_external_oid
 from nti.externalization.interfaces import LocatedExternalDict
 
-from .ugd_query_views import check_container
-from .ugd_query_views import _UGDAndRecursiveStreamView
+from . import interfaces as app_interfaces
+from . import ugd_query_views as query_views
 
 class _TopUserSummaryView(_view_utils.AbstractAuthenticatedView):
 
@@ -30,13 +36,15 @@ class _TopUserSummaryView(_view_utils.AbstractAuthenticatedView):
 			self.user = the_user or self.request.context.user
 			self.ntiid = the_ntiid or self.request.context.ntiid
 
-	def __call__( self ):
-		check_container(self.ntiid, self.user, self.request.registry)
+	def _get_summary_items(self, ntiid, recurse=True):
 
 		# query objects
-		view = _UGDAndRecursiveStreamView(self.request)
+		view = query_views._UGDAndRecursiveStreamView(self.request) if recurse else query_views._UGDView(self.request)
 		view._DEFAULT_BATCH_SIZE = view._DEFAULT_BATCH_START = None
-		all_objects = view.getObjectsForId(self.user, self.ntiid)
+		try:
+			all_objects = view.getObjectsForId(self.user, ntiid)
+		except _hexc.HTTPNotFound:
+			return ({}, {})
 
 		# loop and collect
 		seen = set()
@@ -47,16 +55,17 @@ class _TopUserSummaryView(_view_utils.AbstractAuthenticatedView):
 				to_iter = iterable.itervalues()
 			except (AttributeError, TypeError):
 				to_iter = iterable
-			
+
 			for o in to_iter or ():
 				# check for model content and Change events
-				if nti_interfaces.IStreamChangeEvent.providedBy(o) and o.type in (nti_interfaces.SC_CREATED, nti_interfaces.SC_MODIFIED):
+				if 	nti_interfaces.IStreamChangeEvent.providedBy(o) and \
+					o.type in (nti_interfaces.SC_CREATED, nti_interfaces.SC_MODIFIED):
 					obj = o.object
 				elif nti_interfaces.IModeledContent.providedBy(o):
 					obj = o
 				else:
 					continue
-				
+
 				oid = to_external_oid(obj)
 				if oid in seen:
 					continue
@@ -65,7 +74,7 @@ class _TopUserSummaryView(_view_utils.AbstractAuthenticatedView):
 				creator = getattr(obj, 'creator', None)
 				creator = getattr(creator, 'username', creator)
 				mime_type = getattr(obj, "mimeType", getattr(obj, "mime_type", None))
-				
+
 				if creator and mime_type:
 					counter = result.get(creator)
 					if counter is None:
@@ -75,8 +84,60 @@ class _TopUserSummaryView(_view_utils.AbstractAuthenticatedView):
 					else:
 						counter[mime_type] = counter[mime_type] + 1
 					by_type[mime_type] = by_type[mime_type] + 1
+
+		return result, by_type
+
+	def _merge_map(self, total, m):
+		# merge by mime_type
+		for k, v in m.items():
+			if k not in total:
+				total[k] = v
+			else:
+				total[k] = total[k] + v
+				
+	def _merge_maps(self, total_user_map, total_by_type, usr_map, by_type):
+		# merge by user
+		for username, m in usr_map.items():
+			counter = total_user_map.get(username, None)
+			if counter is None:
+				total_user_map[username] = m
+			else:
+				self._merge_map(counter, m)
+
+		# merge by mime_type
+		self._merge_map(total_by_type, by_type)
+
+	def _scan_quizzes(self, total_user_map, total_by_type):
+		# check there are questions
+		question_map = component.getUtility(app_interfaces.IFileQuestionMap)
+		if not question_map:
+			return
+
+		# gather all paths for the request ntiid
+		library = self.request.registry.getUtility(lib_interfaces.IContentPackageLibrary)
+		paths = library.pathToNTIID(self.ntiid) if library else None
+		children = library.childrenOfNTIID(self.ntiid) if library else None
+
+		if not paths:
+			return
+
+		# gather all ugd in the question containers
+		all_paths = [paths[-1]]
+		all_paths.extend(children or ())
+		for unit in all_paths:
+			questions = question_map.by_file.get(getattr(unit, 'key', None))
+			for question in questions or ():
+				ntiid = getattr(question, 'containerId', None) or getattr(question, 'ntiid', None)
+				usr_map, by_type = self._get_summary_items(ntiid, False) if ntiid else ({}, {})
+				self._merge_maps(total_user_map, total_by_type, usr_map, by_type)
+
+	def __call__( self ):
+		# gather data
+		total_ugd, total_by_type = self._get_summary_items(self.ntiid)
+		self._scan_quizzes(total_ugd, total_by_type)
+
 		# sort
-		items_sorted = sorted(result.items(), key=lambda e: sum(e[1].values()), reverse=True)
+		items_sorted = sorted(total_ugd.items(), key=lambda e: sum(e[1].values()), reverse=True)
 
 		# compute
 		total = 0
@@ -88,5 +149,5 @@ class _TopUserSummaryView(_view_utils.AbstractAuthenticatedView):
 			items.append(entry)
 		result['Items'] = items
 		result['Total'] = total
-		result['Summary'] = LocatedExternalDict(by_type)
+		result['Summary'] = LocatedExternalDict(total_by_type)
 		return result
