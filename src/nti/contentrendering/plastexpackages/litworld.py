2a42d39b098f615c78a531a21575c2dc902c7163
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-05 03:15:24 +0000

    add Id
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@21912 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/litworld.py
+++ src/nti/contentrendering/plastexpackages/litworld.py
@@ -1,5 +1,10 @@
-#!/usr/bin/env python2.7
-#pylint: disable=R0904
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
 
 from plasTeX import Base
 
