b5974a1b745369bc68beff2428efd29247fe9ac2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-06 02:54:09 +0000

    use one script (nti_content_indexer) to index video/nticard/book content
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@21991 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/content_indexer.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Content indexer.
+
+$Id: video_transcript_indexer.py 20547 2013-06-27 19:34:03Z carlos.sanchez $
+"""
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+import logging
+logger = logging.getLogger(__name__)
+
+import os
+import argparse
+
+from zope import interface
+from zope import component
+
+from nti.contentrendering import interfaces as cr_interfaces
+
+interface.moduleProvides(cr_interfaces.IRenderedBookIndexer)
+
+_type_map = { 'book': cr_interfaces.IBookIndexer,
+			  'video': cr_interfaces.IVideoTranscriptIndexer,
+			  'nticard': cr_interfaces.INTICardIndexer }
+
+def transform(book, iface=cr_interfaces.IBookIndexer, name=''):
+	name = name or u''
+	indexer = component.queryUtility(iface, name=name)
+	if indexer is None:
+		indexer = component.getUtility(iface)
+	indexer.index(book)
+
+def main():
+	from nti.contentrendering.utils import NoConcurrentPhantomRenderedBook, EmptyMockDocument
+
+	def register():
+		from zope.configuration import xmlconfig
+		from zope.configuration.config import ConfigurationMachine
+		from zope.configuration.xmlconfig import registerCommonDirectives
+		context = ConfigurationMachine()
+		registerCommonDirectives(context)
+
+		import nti.contentrendering as contentrendering
+		xmlconfig.file("configure.zcml", contentrendering, context=context)
+
+	# parse arguments
+	arg_parser = argparse.ArgumentParser(description="Content Transcript indexer")
+	arg_parser.add_argument('contentpath', help="Content book location")
+	arg_parser.add_argument('-n' '--name', help="Indexer name", dest='name')
+	arg_parser.add_argument('-t', '--type',
+							dest='type',
+							choices=_type_map,
+							default='book',
+							help="The content type to index")
+	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
+	args = arg_parser.parse_args()
+
+	# process arguments
+	contentpath = os.path.expanduser(args.contentpath)
+	jobname = os.path.basename(contentpath)
+
+	verbose = args.verbose
+	name = args.name or jobname
+	indexer = _type_map[args.type]
+	contentpath = contentpath[:-1] if contentpath.endswith(os.path.sep) else contentpath
+	if os.path.exists(contentpath) or not os.path.isdir(contentpath):
+		raise IOError("Invalid content directory")
+
+	if verbose:
+		logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(name)-5s %(levelname)-8s %(message)s')
+
+	# register zcml
+	register()
+
+	# do indexing
+	document = EmptyMockDocument()
+	document.userdata['jobname'] = jobname
+	book = NoConcurrentPhantomRenderedBook(document, contentpath)
+	transform(book, iface=indexer, name=name)
+
+if __name__ == '__main__':
+	main()
