5afe2f8344506c50a942a4be0fc3904f404f758e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-06 16:06:54 +0000

    use queryUtility
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@22024 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/taggers/__init__.py
+++ src/nti/contentprocessing/taggers/__init__.py
@@ -18,6 +18,6 @@ def tag_word(word, language=u'en'):
     return tag_tokens([word], language)
 
 def tag_tokens(tokens, language=u'en'):
-    tagger = component.getUtility(tagger_interfaces.ITagger, name=language)
-    result = tagger.tag(tokens) if tokens else ()
+    tagger = component.queryUtility(tagger_interfaces.ITagger, name=language)
+    result = tagger.tag(tokens) if tokens and tagger else ()
     return result
