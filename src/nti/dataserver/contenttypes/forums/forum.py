f0131a9a3a0052ac445d5dc7b23c934959ac2b3f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-10 18:55:18 +0000

    set __external_class_name__
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@22330 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/contenttypes/forums/forum.py
+++ src/nti/dataserver/contenttypes/forums/forum.py
@@ -273,5 +273,6 @@ def GeneralForumCommunityAdapter(community):
 @interface.implementer(frm_interfaces.IACLCommunityForum)
 class ACLCommunityForum(CommunityForum):
 	__external_can_create__ = True
+	__external_class_name__ = "CommunityForum"
 	ACL = ()
 
