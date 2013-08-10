63aee6d4ba43ce2b569a9250a4f47e930f6ec0ab
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-10 03:31:26 +0000

    move to proper place
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@22313 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/contenttypes/forums/acl.py
+++ src/nti/dataserver/contenttypes/forums/acl.py
@@ -22,7 +22,6 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from zope import component
-from zope import interface
 
 from nti.dataserver.users import users
 from nti.dataserver import authorization as nauth
@@ -30,9 +29,6 @@ from nti.dataserver.traversal import find_interface
 from nti.dataserver import interfaces as nti_interfaces
 from nti.dataserver.authorization_acl import ace_allowing
 from nti.dataserver.authorization_acl import AbstractCreatedAndSharedACLProvider
-
-from nti.utils.schema import SchemaConfigured
-from nti.utils.schema import createDirectFieldProperties
 
 from . import interfaces as frm_interfaces
 
@@ -126,32 +122,12 @@ class _PostACLProvider(AbstractCreatedAndSharedACLProvider):
 		if topic_creator:
 			acl.append( ace_allowing( topic_creator, nauth.ACT_DELETE, self ) )
 			acl.append( ace_allowing( topic_creator, nauth.ACT_READ, self ) )
-	
-@interface.implementer(frm_interfaces.IForumACE)
-class ForumACE(SchemaConfigured):
-	__external_class_name__ = "ACE"
-	createDirectFieldProperties(frm_interfaces.IForumACE)
 
-	def __str__(self):
-		return "%s(%s,%s,%s)" % (self.__class__, self.Action, self.Permission, self.Entities)
 
-	__repr__ = __str__
+class _ACLCommunityForumACLProvider(_CommunityForumACLProvider):
+	pass
 
-	def __eq__(self, other):
-		try:
-			return self is other or (self.Action == other.Action
-									 and self.Permission == other.Permission
-									 and set(self.Entities) == set(other.Entities))
-		except AttributeError:
-			return NotImplemented
-
-	def __hash__(self):
-		xhash = 47
-		xhash ^= hash(self.Action)
-		xhash ^= hash(self.Entities)
-		xhash ^= hash(self.Permission)
-		return xhash
-	
+# FIXME: this has to be removed
 class _ClassForumACLProvider(_CommunityForumACLProvider):
 	"""
 	Only the shared users can create new topics within it. Everyone else can read
