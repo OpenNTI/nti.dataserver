7b5649ee0e3a0e3516ad190fc6f000d21c347da2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-10 02:42:48 +0000

    start adding Forum ACE
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@22312 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/contenttypes/forums/externalization.py
+++ src/nti/dataserver/contenttypes/forums/externalization.py
@@ -5,23 +5,25 @@ Externalization for forum objects.
 
 $Id$
 """
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
 
-import logging
-logger = logging.getLogger( __name__ )
+logger = __import__('logging').getLogger(__name__)
 
-
+from zope import component
 from zope import interface
 
-
 from nti.externalization import interfaces as ext_interfaces
+from nti.externalization.datastructures import InterfaceObjectIO
 from nti.externalization.autopackage import AutoPackageSearchingScopedInterfaceObjectIO
+
+from . import interfaces as frm_interfaces
 from ..base import UserContentRootInternalObjectIOMixin
 
 @interface.implementer(ext_interfaces.IInternalObjectIO)
 class _ForumInternalObjectIO(UserContentRootInternalObjectIOMixin,AutoPackageSearchingScopedInterfaceObjectIO):
 
-#	_excluded_out_ivars_ = UserContentRootInternalObjectIO._excluded_out_ivars_
+	# _excluded_out_ivars_ = UserContentRootInternalObjectIO._excluded_out_ivars_
 
 	@classmethod
 	def _ap_enumerate_externalizable_root_interfaces( cls, frm_interfaces ):
@@ -32,3 +34,8 @@ class _ForumInternalObjectIO(UserContentRootInternalObjectIOMixin,AutoPackageSea
 		return ('board', 'forum', 'post', 'topic')
 
 _ForumInternalObjectIO.__class_init__()
+
+@interface.implementer(ext_interfaces.IInternalObjectIO)
+@component.adapter(frm_interfaces.IForumACE)
+class _ForumACEObjectIO(InterfaceObjectIO):
+	_ext_iface_upper_bound = frm_interfaces.IForumACE
