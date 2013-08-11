2fd6483c25cf1f6a961c347c7be366ea26b5fc3c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-11 20:46:35 +0000

    start moving store func to its own appserver module. This is to better support link addition
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@22434 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/store/workspaces.py
+++ src/nti/appserver/store/workspaces.py
@@ -3,7 +3,7 @@
 """
 Workspaces / Collections related NTI store
 
-$Id: store_views.py 21508 2013-07-30 03:37:46Z carlos.sanchez $
+$Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
@@ -12,29 +12,66 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 from zope import interface
+from zope.location import interfaces as loc_interfaces
 
 from nti.appserver import interfaces as app_interfaces
 
+from nti.dataserver import links
+
+from nti.store.course import Course
+from nti.store.purchasable import Purchasable
+
+@interface.implementer(app_interfaces.IWorkspace)
+@component.adapter(app_interfaces.IUserService)
+def _store_workspace(user_service):
+    store_ws = StoreWorkspace(parent=user_service.__parent__)
+    return store_ws
+
+@interface.implementer(app_interfaces.IWorkspace)
+class StoreWorkspace(object):
+
+    links = ()
+    __parent__ = None
+
+    def __init__(self, parent=None):
+        super(StoreWorkspace, self).__init__()
+        if parent:
+            self.__parent__ = parent
+
+    @property
+    def name(self): return 'store'
+    __name__ = name
+
+    @property
+    def collections(self):
+        return (_StoreCollection(self),)
+
 @interface.implementer(app_interfaces.IContainerCollection)
 @component.adapter(app_interfaces.IUserWorkspace)
-class _UserStoreCollection(object):
+class _StoreCollection(object):
 
     name = 'Store'
-    __name__ = name
+    __name__ = u''
     __parent__ = None
 
     def __init__(self, user_workspace):
         self.__parent__ = user_workspace
 
     @property
+    def links(self):
+        result = []
+        for l in ('UserSearch', 'ResolveUser'):
+            link = links.Link(l, rel=l)
+            link.__name__ = link.target
+            link.__parent__ = self.__parent__
+            interface.alsoProvides(link, loc_interfaces.ILocation)
+            result.append(link)
+        return result
+
+    @property
     def container(self):
-        return  ()
+        return ()
 
     @property
     def accepts(self):
-        return ()
-
-@interface.implementer(app_interfaces.IContainerCollection)
-@component.adapter(app_interfaces.IUserWorkspace)
-def _UserStoreCollectionFactory(workspace):
-    return _UserStoreCollection(workspace)
+        return (Course.mimeType, Purchasable.mimeType)
