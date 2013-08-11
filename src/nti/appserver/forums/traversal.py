cd9a8477b08b44162cffab65589ac09177275518
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-11 04:13:33 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@22400 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/forums/traversal.py
+++ src/nti/appserver/forums/traversal.py
@@ -5,27 +5,26 @@ Related to traversing into forum objects.
 
 $Id$
 """
-
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 from zope import component
-from zope.container.traversal import ContainerTraversable
 from zope.traversing.interfaces import TraversalError
+from zope.container.traversal import ContainerTraversable
+
+from nti.appserver._adapters import GenericModeledContentExternalFieldTraverser
 
 from nti.dataserver.contenttypes.forums import interfaces as frm_interfaces
 from nti.dataserver.contenttypes.forums.forum import CommunityForum
-_FORUM_NAME = CommunityForum.__default_name__
 
-from nti.appserver._adapters import GenericModeledContentExternalFieldTraverser
+_FORUM_NAME = CommunityForum.__default_name__
 
 @component.adapter(frm_interfaces.IPost)
 class _PostFieldTraverser(GenericModeledContentExternalFieldTraverser):
 	"Disallow updates to the sharedWith field of blog posts/comments"
 	_allowed_fields = tuple( set(GenericModeledContentExternalFieldTraverser._allowed_fields) - set( ('sharedWith',)) )
-
 
 @component.adapter(frm_interfaces.ICommunityBoard)
 class _CommunityBoardTraversable(ContainerTraversable):
