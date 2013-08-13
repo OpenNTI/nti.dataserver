65995c0ac2a58b28efe443e584982f942df85d27
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-13 19:40:07 +0000

    bump plone.i18n
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@22655 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -219,7 +219,7 @@ setup(
 		'numpy' if not IS_PYPY else '',
 		'paste >= 1.7.5.1',
 		'perfmetrics >= 1.0',  # easy statsd metrics.
-		'plone.i18n >= 2.0.8',  # provides ISO3166 country/codes and flag images
+		'plone.i18n >= 2.0.9',  # provides ISO3166 country/codes and flag images
 		'plone.scale >= 1.3.2',  # image scaling/storage based on PIL
 		'plone.namedfile >= 2.0.2',  # much like zope.file, but some image-specific goodness.
 		'pyparsing >= 1.5.6, <=1.5.7',  # used by matplotlib, rdflib, experimental zopyx.txng3.core
