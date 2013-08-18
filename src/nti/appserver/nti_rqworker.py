4da76fcd42c3c7a15d3ba6f53a245887bc6bab1b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-18 07:52:34 +0000

    add an entry point for an rq worker
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@23110 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/appserver/nti_rqworker.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+A entry point to help us ensure that we are loading rqworkers
+along w/ the dataserver code
+
+$Id: nti_pserve.py 15952 2013-02-16 22:36:48Z jason.madden $
+"""
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+import sys
+
+from rq.scripts import rqworker
+
+from nti.dataserver.utils import run_with_dataserver
+
+def _run_worker():
+	rqworker.main()
+
+def main():
+	args = sys.argv[1:]
+	if not args:
+		raise Exception('Specify a dataserver environment root directory')
+
+	env_dir = args[0]
+	sys.argv = args[1:]
+	run_with_dataserver(environment_dir=env_dir,
+						function=lambda: _run_worker(args))
+	sys.exit(0)
