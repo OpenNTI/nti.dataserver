c6325edd95e8d04142ca3dc3db4988a812d8174b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-19 11:51:35 +0000

    change queue name
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@23283 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_redis_indexstore.py
+++ src/nti/contentsearch/_redis_indexstore.py
@@ -22,7 +22,7 @@ from nti.dataserver import interfaces as nti_interfaces
 
 SLEEP_WAIT_TIME = 10
 EXPIRATION_TIME_IN_SECS = 60
-DEFAULT_QUEUE_NAME = u'nti/cloudsearch'
+DEFAULT_QUEUE_NAME = u'nti/indexing/ops'
 
 ADD_OPERATION = 'add'
 UPDATE_OPERATION = 'update'
@@ -106,7 +106,7 @@ class _RedisStorageService(object):
 		return result
 	
 	# redis message manipulation
-		
+
 	def encode_message(self, op, docid, username):
 		msg = repr((op, docid, username))
 		return msg
