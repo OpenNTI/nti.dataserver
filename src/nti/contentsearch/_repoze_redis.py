f19a3897acb8bb0ed238031b851b4c0befb47580
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-19 12:16:14 +0000

    change props
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@23285 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_repoze_redis.py
+++ src/nti/contentsearch/_repoze_redis.py
@@ -2,7 +2,7 @@
 """
 Index operations using redis and repoze
 
-$Id: _cloudsearch_store.py 17789 2013-03-31 04:18:10Z carlos.sanchez $
+$Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
@@ -22,7 +22,7 @@ from ._redis_indexstore import _RedisStorageService
 class _RepozeStorageService(_RedisStorageService):
 
 	def index_content(self, docid, username):
-		obj = _discriminators.query_object(docid)
+		obj = _discriminators.query_object(int(docid))
 		entity = users.Entity.get_entity(username)
 		um = search_interfaces.IRepozeEntityIndexManager(entity, None)
 		if obj is not None and um is not None:
@@ -30,7 +30,7 @@ class _RepozeStorageService(_RedisStorageService):
 		return False
 
 	def update_content(self, docid, username):
-		obj = _discriminators.query_object(docid)
+		obj = _discriminators.query_object(int(docid))
 		entity = users.Entity.get_entity(username)
 		um = search_interfaces.IRepozeEntityIndexManager(entity, None)
 		if obj is not None and um is not None:
@@ -41,11 +41,12 @@ class _RepozeStorageService(_RedisStorageService):
 		entity = users.Entity.get_entity(username)
 		um = search_interfaces.IRepozeEntityIndexManager(entity, None)
 		if um is not None:
-			return um.unindex(docid)
+			return um.unindex(int(docid))
 		return False
 
 	def process_messages(self, msgs):
 		for m in sort_messages(msgs):
+			__traceback_info__ = m
 			try:
 				op, docid, username = m
 				if op == 'add':
@@ -55,4 +56,4 @@ class _RepozeStorageService(_RedisStorageService):
 				elif op == 'delete':
 					self.unindex(docid, username)
 			except:
-				logger.exception("Failed to run index operation %s" % m)
+				logger.exception("Failed to run index operation")
