4d331a01f82ffd2521bf66f648aeef08c9a95630
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-19 17:07:54 +0000

    for the time being have rqworker be a pass through. Sadly if we run a rqworker under run_with_dataserver something happen to the ZODB connection when a fork for the target function occurs whereby objects cannot load their state. Since RQ does a fork state is not kept. Need to find out if it's possible to create a new connection w/o having to load the entire environment again
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@23316 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/nti_rqworker.py
+++ src/nti/appserver/nti_rqworker.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 A entry point to help us ensure that we are loading rqworkers
-along w/ the dataserver code
 
 $Id$
 """
@@ -13,22 +12,11 @@ import sys
 
 from rq.scripts import rqworker
 
-from nti.dataserver.utils import run_with_dataserver
-
 def _run_worker():
 	rqworker.main()
 
 def main():
-	exe = sys.argv[0]
-	args = sys.argv[1:]
-	if not args:
-		raise Exception('Specify a dataserver environment root directory')
-
-	env_dir = args[0]
-	sys.argv = [exe] + args[1:]
-	run_with_dataserver(environment_dir=env_dir,
-						xmlconfig_packages= ('nti.appserver',),
-						function=lambda: _run_worker())
+	_run_worker()
 	sys.exit(0)
 
 if __name__ == '__main__':
