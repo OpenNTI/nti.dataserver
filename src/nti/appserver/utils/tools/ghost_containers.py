5935557dfa58fdedb89d019bd60607e4d95a2393
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-21 16:32:49 +0000

    check point .. eclipse is being a pain
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@23411 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/tools/ghost_containers.py
+++ src/nti/appserver/utils/tools/ghost_containers.py
@@ -3,7 +3,7 @@
 """
 Store admin views
 
-$Id: store_admin_views.py 22434 2013-08-11 20:46:35Z carlos.sanchez $
+$Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
@@ -16,39 +16,55 @@ import argparse
 from zope import interface
 from zope import component
 
-from nti.externalization.externalization import to_external_object
+from nti.contentmanagement import get_collection_root
 
 from nti.dataserver import users
-from nti.dataserver import providers
-from nti.dataserver import shards as nti_shards
 from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver import interfaces as nti_interfaces
 
+exclude_containers = (u'Devices', u'FriendsLists', u'')
 
+def _check_users(usernames=(), args=(), options=None):
+	__traceback_info__ = locals().items()
 
-
-def _check_users(usernames, args=(), options=None):
-    __traceback_info__ = locals().items()
-
+	if usernames:
+		_users = {users.User.get_entity(x) for x in usernames}
+		_users.discard(None)
+	else:
+		dataserver = component.getUtility(nti_interfaces.IDataserver)
+		_users = nti_interfaces.IShardLayout(dataserver).users_folder
+		_users = _users.values()
+	
+	result = {}
+	for user in _users:
+		method = getattr(user, 'getAllContainers', lambda : ())
+		usermap = {}
+		for name in method():
+			if get_collection_root(name) is None:
+				container = user.getContainer(name)
+				usermap[name] = len(container) if container is not None else 0
+			print(type(container))
+	
+	return result
 
 def main(args=None):
-    arg_parser = argparse.ArgumentParser(description="Create a user-type object")
-    arg_parser.add_argument('env_dir', help="Dataserver environment root directory")
-    arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
-    arg_parser.add_argument('-u', '--users',
-                             dest='users',
-                             nargs="+",
-                             default=(),
-                             help="The names of users to check")
+	arg_parser = argparse.ArgumentParser(description="Create a user-type object")
+	arg_parser.add_argument('env_dir', help="Dataserver environment root directory")
+	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
+	arg_parser.add_argument('-u', '--users',
+							 dest='users',
+							 nargs="+",
+							 default=(),
+							 help="The names of users to check")
 
-    args = arg_parser.parse_args(args=args)
+	args = arg_parser.parse_args(args=args)
 
-    env_dir = args.env_dir
-    usernames = args.usernames
+	env_dir = args.env_dir
+	usernames = args.usernames
 
-    conf_packages = ('nti.appserver',)
-    run_with_dataserver(environment_dir=env_dir,
-                        xmlconfig_packages=conf_packages,
-                        verbose=args.verbose,
-                        function=lambda: _check_users(usernames, args))
-    sys.exit(0)
+	conf_packages = ('nti.appserver',)
+	run_with_dataserver(environment_dir=env_dir,
+						xmlconfig_packages=conf_packages,
+						verbose=args.verbose,
+						function=lambda: _check_users(usernames, args))
+	sys.exit(0)
