4c7ddc1b596e956622bca3e846265c12dd4befdf
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-25 17:34:52 +0000

    add some debugging
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@23563 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/wsgi_ping.py
+++ src/nti/appserver/wsgi_ping.py
@@ -3,10 +3,8 @@
 """
 Simple ping paste filter that does nothing but return a 200.
 
-
 $Id$
 """
-
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
@@ -16,19 +14,22 @@ class PingHandler(object):
 	"""
 	Handles the ``/_ops/ping`` url exactly.
 	"""
-
-	def __init__( self, app ):
+	def __init__(self, app):
 		self.captured = app
 
-	def __call__( self, environ, start_response ):
-		__traceback_info__ = environ
+	def __call__(self, environ, start_response):
+		__traceback_info__ = environ, start_response
 		if environ['PATH_INFO'] == b'/_ops/ping':
 			start_response( b'200 OK', [(b'Content-Type', b'text/plain')] )
 			result = (b"",)
 		else:
-			result = self.captured( environ, start_response )
+			try:
+				result = self.captured(environ, start_response)
+			except:
+				logger.exception("Error during ping. Handler (%s)" % getattr(self.captured, "handle_request", None))
+				raise
 		return result
 
-def ping_handler_factory( app, global_conf=None ):
+def ping_handler_factory(app, global_conf=None):
 	"Paste factory for :class:`PingHandler`"
-	return PingHandler( app )
+	return PingHandler(app)
