b1b48981ab91f984928261b12a88dd1647ebdf4b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-27 19:55:46 +0000

    specify a class to add rqworker as supervisord entry
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@23643 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_init_env.py
+++ src/nti/dataserver/utils/nti_init_env.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+Utility to initialize an environment
+
 $Id$
 """
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
 
 import sys
 import os.path
@@ -11,7 +16,6 @@ import argparse
 
 from zope import component
 import z3c.password.password
-import z3c.password.interfaces
 
 from nti.dataserver import config
 from nti.dataserver.utils import run
@@ -19,12 +23,13 @@ import nti.dataserver.utils.example_database_initializer
 
 def main():
 	arg_parser = argparse.ArgumentParser( description="Initialize a base dataserver environment" )
-	arg_parser.add_argument( 'env_dir', help="Dataserver environment root directory" )
-	arg_parser.add_argument( 'pserve_ini_file', help="The path to the .ini file for pserve" )
-	arg_parser.add_argument( '-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
-	arg_parser.add_argument( '--with-example', help="Populate the example data", action='store_true', dest='with_example')
-	arg_parser.add_argument( '--update-existing', help="Update an existing env directory", action='store_true', dest='update_existing')
-	arg_parser.add_argument( '--write-supervisord', help="Write supervisord config file", action='store_true', dest='write_supervisord')
+	arg_parser.add_argument('env_dir', help="Dataserver environment root directory")
+	arg_parser.add_argument('pserve_ini_file', help="The path to the .ini file for pserve")
+	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
+	arg_parser.add_argument('--with-example', help="Populate the example data", action='store_true', dest='with_example')
+	arg_parser.add_argument('--update-existing', help="Update an existing env directory", action='store_true', dest='update_existing')
+	arg_parser.add_argument('--write-supervisord', help="Write supervisord config file", action='store_true', dest='write_supervisord')
+	arg_parser.add_argument('--write-rqworker', help="Write rqworker as supervisord entry", action='store_true', dest='write_rqworker')
 	args = arg_parser.parse_args()
 
 	run( lambda: init_env( args ) )
@@ -48,8 +53,12 @@ def init_env( args ):
 
 	update_existing = args.update_existing
 	write_supervisord = args.write_supervisord
+	write_rqworker = args.write_rqworker
 	
-	config.write_configs(root_dir, pserve_ini, update_existing=update_existing, write_supervisord=write_supervisord)
+	config.write_configs(root_dir, pserve_ini,
+						 update_existing=update_existing, 
+						 write_supervisord=write_supervisord,
+						 write_rqworker=write_rqworker)
 
 if __name__ == '__main__':
 	main()
