548b8fec081885112c465bc0e964088e4a97b6df
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-29 20:53:39 +0000

    add unit tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@23779 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/users/tests/test_preferences_views.py
+++ src/nti/appserver/utils/users/tests/test_preferences_views.py
@@ -9,14 +9,6 @@ __docformat__ = "restructuredtext en"
 
 import simplejson
 
-from io import BytesIO
-from zope import interface
-
-from nti.dataserver.contenttypes import Note
-from nti.dataserver import interfaces as nti_interfaces
-
-from nti.externalization import externalization
-
 from nti.externalization.externalization import to_json_representation
 
 from nti.appserver.tests.test_application import TestApp
@@ -24,7 +16,7 @@ from nti.appserver.tests.test_application import TestApp
 from nti.dataserver.tests import mock_dataserver
 from nti.appserver.tests.test_application import SharedApplicationTestBase, WithSharedApplicationMockDS
 
-from hamcrest import (assert_that, is_, has_length, has_entry, greater_than)
+from hamcrest import (assert_that, is_, has_length, has_entry)
 
 class TestApplicationUserExporViews(SharedApplicationTestBase):
 
@@ -46,8 +38,6 @@ class TestApplicationUserExporViews(SharedApplicationTestBase):
 		assert_that(res.status_int, is_(200))
 		d = simplejson.loads(res.body)
 		assert_that(d, has_entry(u'Items', has_length(3)))
-		assert_that(d, has_entry(u'Items', has_entry('power', 560)))
-		assert_that(d, has_entry(u'Items', has_entry('shikai', 'ryujin jakka')))
 		
 		path = '/dataserver2/users/sjohnson@nextthought.com/@@get_preferences'
 		res = testapp.get(path, extra_environ=environ)
@@ -56,3 +46,47 @@ class TestApplicationUserExporViews(SharedApplicationTestBase):
 		assert_that(d, has_entry(u'Items', has_length(3)))
 		assert_that(d, has_entry(u'Items', has_entry('power', 560)))
 		assert_that(d, has_entry(u'Items', has_entry('shikai', 'ryujin jakka')))
+		assert_that(d, has_entry(u'Items', has_entry('bankai', 'zanka no tachi')))
+
+	@WithSharedApplicationMockDS
+	def test_delete_preferences(self):
+		with mock_dataserver.mock_db_trans(self.ds):
+			self._create_user()
+
+		testapp = TestApp(self.app)
+
+		path = '/dataserver2/users/sjohnson@nextthought.com/@@set_preferences'
+		environ = self._make_extra_environ()
+
+		data = to_json_representation({'shikai': 'ryujin jakka',
+									   'bankai': 'zanka no tachi',
+									   'power': 560 })
+
+		res = testapp.post(path, data, extra_environ=environ)
+		assert_that(res.status_int, is_(200))
+		d = simplejson.loads(res.body)
+		assert_that(d, has_entry(u'Items', has_length(3)))
+
+		path = '/dataserver2/users/sjohnson@nextthought.com/@@delete_preferences'
+		data = to_json_representation({'keys': 'power'})
+		
+		res = testapp.delete(path, data, extra_environ=environ)
+		assert_that(res.status_int, is_(200))
+		d = simplejson.loads(res.body)
+		assert_that(d, has_entry(u'Items', has_length(2)))
+		assert_that(d, has_entry(u'Items', has_entry('shikai', 'ryujin jakka')))
+		assert_that(d, has_entry(u'Items', has_entry('bankai', 'zanka no tachi')))
+
+		data = to_json_representation({'shikai': 'ryujin jakka'})
+		res = testapp.delete(path, data, extra_environ=environ)
+		assert_that(res.status_int, is_(200))
+		d = simplejson.loads(res.body)
+		assert_that(d, has_entry(u'Items', has_length(1)))
+		assert_that(d, has_entry(u'Items', has_entry('bankai', 'zanka no tachi')))
+
+		data = to_json_representation({})
+		res = testapp.delete(path, data, extra_environ=environ)
+		assert_that(res.status_int, is_(200))
+		d = simplejson.loads(res.body)
+		assert_that(d, has_entry(u'Items', has_length(0)))
+
