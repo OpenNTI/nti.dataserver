47a141cd9a8a08f13d6c56600178eff82a5fbcd3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-30 05:54:27 +0000

    add test for decorator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@23850 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/users/tests/test_preferences.py
+++ src/nti/dataserver/users/tests/test_preferences.py
@@ -6,19 +6,32 @@ __docformat__ = "restructuredtext en"
 from hamcrest import is_
 from hamcrest import none
 from hamcrest import is_not
+from hamcrest import has_entry
 from hamcrest import assert_that
 from hamcrest import has_property
 
+from nti.externalization.externalization import to_external_object
+
 import nti.tests
 
-setUpModule = lambda: nti.tests.module_setup(set_up_packages=('nti.dataserver',))
+from nti.dataserver.tests import mock_dataserver
+
+setUpModule = lambda: nti.tests.module_setup(set_up_packages=('nti.dataserver', 'nti.dataserver.users',))
 tearDownModule = nti.tests.module_teardown
 
 from nti.dataserver.users import User
 from nti.dataserver.users import interfaces as user_interfaces
 
 def test_adapter():
-	user = User( 'foo@bar' )
+	user = User('foo@bar')
 	pref = user_interfaces.IEntityPreferences(user, None)
 	assert_that(pref, is_not(none()))
 	assert_that(pref, has_property('username', is_('foo@bar')))
+
+@mock_dataserver.WithMockDSTrans
+def test_decorator():
+	user = User.create_user(username="foo@bar")
+	pref = user_interfaces.IEntityPreferences(user)
+	pref['foo'] = 'bar'
+	ext_user = to_external_object(user)
+	assert_that(ext_user, has_entry('Preferences', has_entry('foo', 'bar')))
