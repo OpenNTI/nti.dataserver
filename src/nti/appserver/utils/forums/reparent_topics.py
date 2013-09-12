f4fc2f9c3b15209630a138064a791a507c112bb1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-09-12 14:52:54 +0000

    allow passing a forum name to reparent
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@24235 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/forums/reparent_topics.py
+++ src/nti/appserver/utils/forums/reparent_topics.py
@@ -19,7 +19,7 @@ from nti.dataserver import interfaces as nti_interfaces
 from nti.dataserver.contenttypes.forums.forum import CommunityForum
 from nti.dataserver.contenttypes.forums import interfaces as frm_interfaces
 
-def reparent_topics(community, list_forums=False, verbose=False):
+def reparent_topics(community, forum_name, list_forums=False, verbose=False):
 	community = users.Community.get_community(community)
 	if not community or not nti_interfaces.ICommunity.providedBy(community):
 		print('Community not found')
@@ -34,12 +34,17 @@ def reparent_topics(community, list_forums=False, verbose=False):
 		for forum in board.keys():
 			print(forum)
 		return
-	forum = frm_interfaces.ICommunityForum(community, None)
+
+	forum = board.get(forum_name)
+	if forum is None:
+		print('Forum not found')
+		sys.exit(3)
+
 	if forum.__parent__ != board:
 		forum.__parent__ = board
-		forum.__name__ = CommunityForum.__default_name__
+		forum.__name__ = forum_name
 		if verbose:
-			print('Setting parent for forum %s' % CommunityForum.__default_name__)
+			print('Setting parent for forum %s' % forum_name)
 
 	count = 0
 	for name, topic in forum.items():
@@ -57,13 +62,15 @@ def main():
 	arg_parser.add_argument('community', help="The name of the community")
 	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
 	arg_parser.add_argument('-l', '--listforums', help="List forums", action='store_true', dest='list_forums')
+	arg_parser.add_argument('-f', '--forum', help="Forum name", default=CommunityForum.__default_name__, dest='forum')
 	args = arg_parser.parse_args()
 
+	fourm = args.forum
 	env_dir = args.env_dir
-	community = args.community
 	verbose = args.verbose
+	community = args.community
 	list_forums = args.list_forums
-	run_with_dataserver(environment_dir=env_dir, function=lambda: reparent_topics(community, list_forums, verbose))
+	run_with_dataserver(environment_dir=env_dir, function=lambda: reparent_topics(community, fourm, list_forums, verbose))
 	sys.exit(0)
 
 if __name__ == '__main__':
