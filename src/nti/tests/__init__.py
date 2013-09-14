d5721d0dd2b059896b1c07d3f7a4aeeab998cdf0
Jason Madden <jason.madden@nextthought.com>
2013-09-14 14:15:34 +0000

    Cut over to the more full-featured exception formatter.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@24312 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/tests/__init__.py
+++ src/nti/tests/__init__.py
@@ -579,39 +579,15 @@ def module_teardown():
 # transferred to github as https://github.com/zopefoundation/zope.component/pull/1
 import zope.site.site
 
-
-import nose.plugins
-class ZopeExceptionLogPatch(nose.plugins.Plugin):
+# BWC for the old name of the plugin
+from nti.nose_traceback_info import NoseTracebackInfoPlugin
+class ZopeExceptionLogPatch(NoseTracebackInfoPlugin):
 	name = 'zopeexceptionlogpatch'
-	score = 1000
-	enabled = True # Enabled by default
-	def configure(self, options, conf ):
-		# Force the logcapture plugin, enabled by default,
-		# to use the zope exception formatter.
-		import zope.exceptions.log
-		import logging
-		logging.Formatter = zope.exceptions.log.Formatter
+	enabled = False # The superclass is enabled by default
 
-	# Also present failure cases formatted the same way
-	def formatError(self, test, exc_info):
-		t, v, tb = exc_info
-		from zope.exceptions.exceptionformatter import format_exception
-		# Despite what the docs say, you do not return the test.
-		# see logcapture and failuredetail.
-		# Omitting filenames makes things shorter
-		# and generally more readable, but when the last part of the traceback
-		# is in initializing a module, then the filename is the only discriminator
+	def options(self, parser, env ):
+		return # No options
 
-		# Note that we are joining with a byte string, not a unicode string. Under
-		# python2, tracebacks are byte strings and mixing unicode at this level may
-		# result in UnicodeDecodeError
-		formatted_tb = b''.join(format_exception(t, v, tb, with_filenames=False))
-		if b'Module None' in formatted_tb:
-			formatted_tb = b''.join(format_exception(t, v, tb, with_filenames=True))
-		return (t, formatted_tb, None)
-
-	def formatFailure(self, test, exc_info):
-		return self.formatError( test, exc_info)
 
 # Zope.mimetype registers hundreds and thousands of objects
 # doing that for each test makes them take SO much longer
