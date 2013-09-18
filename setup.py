884babee6dbcc148af0b9318023bc0b85720a1e8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-09-18 19:47:36 +0000

    add util to setup.py
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@24442 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ entry_points = {
 		"nti_set_password = nti.dataserver.utils.nti_set_password:main",
 		"nti_follow_entity = nti.dataserver.utils.nti_follow_entity:main",
 		"nti_remove_user = nti.dataserver.utils.nti_remove_user:main",
+		"nti_export_entities = nti.dataserver.utils.nti_export_entities:main",
 		"nti_set_user_attribute = nti.dataserver.utils.nti_set_user_attribute:main",
 		"nti_sharing_listener = nti.appserver.application:sharing_listener_main",
 		"nti_index_listener = nti.appserver.application:index_listener_main",
