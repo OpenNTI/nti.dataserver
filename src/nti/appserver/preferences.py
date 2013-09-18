f50231ee6225191fa18ee124efc0bd73e27c6789
Jason Madden <jason.madden@nextthought.com>
2013-09-18 19:25:30 +0000

    Finish modeling the chat presence pref tree, and test reading the defaults and updating them. Along with updating the app, the next step is a migration from the old stuff if we want to preserve them; I think this may be a good module to break out into its own package so it can more or less be deployed independently if desired.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@24440 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/preferences.py
+++ src/nti/appserver/preferences.py
@@ -24,7 +24,9 @@ Things to remember:
   schema? Because children can be fetched and edited independently,
   settings that are frequently updated together (or not updated when
   other settings are) are a good candidate for a group. When parent
-  data is fetched, all (recursive) children are also fetched.
+  data is fetched, all (recursive) children are also fetched. A sub-group
+  is the easiest way to introduce modeled preferences that go together; otherwise,
+  you are limited to what you can define for the Dict or Mapping types.
 * If you add a group, you must add it to the ZCML file.
 * Preferences can have defaults specified in the schema. They can also
   have defaults specified on a site-by-site basis, and even at particular
@@ -59,7 +61,64 @@ class IWebAppUserSettings(Interface):
 		default=False)
 
 
+from nti.chatserver.interfaces import IUnattachedPresenceInfo
+from nti.contentfragments.interfaces import PlainTextContentFragment
+
 class IChatPresenceSettings(Interface):
 	"""
 	A child of the root, specifying chat presence defaults.
+
+	This interface itself is blank; instead, its values
+	are defined as child groups. Each group corresponds to
+	a "preset" that the user can choose from. The "Active"
+	preset is the one to use when the user enters the application;
+	the other presets define what happens when the user selects the
+	corresponding preset.
+
+	When the user picks a preset, the Active group should be updated
+	by copying the preset. If the user customizes their state, those
+	customizations can be recorded in the Active group.
+
+	Subgroups may include: DND, Available, Away (but see the ZCML
+	file for definitions).
+
 	"""
+
+# Below we're going out of our way to reuse the existing
+# presence info class and fields so that we're sure
+# they match the constraints actually enforced by the
+# chatserver. Most new preferences won't need to do this
+
+class IAvailableChatPresenceSettings(IUnattachedPresenceInfo):
+	"""Provide the defaults for the Available state"""
+
+	show = IUnattachedPresenceInfo['show'].bind(None)
+	status = IUnattachedPresenceInfo['status'].bind(None)
+
+IAvailableChatPresenceSettings['status'].default = PlainTextContentFragment('Available')
+
+class IAwayChatPresenceSettings(IUnattachedPresenceInfo):
+	"""Provide the defaults for the Away state"""
+
+	show = IUnattachedPresenceInfo['show'].bind(None)
+	status = IUnattachedPresenceInfo['status'].bind(None)
+
+IAwayChatPresenceSettings['status'].default = PlainTextContentFragment('Away')
+IAwayChatPresenceSettings['show'].default = 'away'
+
+
+class IDNDChatPresenceSettings(IUnattachedPresenceInfo):
+	"""Provide the defaults for the DND state"""
+
+	show = IUnattachedPresenceInfo['show'].bind(None)
+	status = IUnattachedPresenceInfo['status'].bind(None)
+
+IDNDChatPresenceSettings['status'].default = PlainTextContentFragment('Do Not Disturb')
+IDNDChatPresenceSettings['show'].default = 'dnd'
+
+for iface in IAvailableChatPresenceSettings, IAwayChatPresenceSettings, IDNDChatPresenceSettings:
+	for name in 'show', 'status':
+		iface[name].interface = iface
+		if iface[name].__dict__['default'] != iface[name].default:
+			del iface[name].defaultFactory
+	iface.changed(iface)
