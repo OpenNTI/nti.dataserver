1d9b12f278f884a41a8d5749ec3dccc276ab9d7a
Jason Madden <jason.madden@nextthought.com>
2013-09-26 12:13:28 +0000

    move some of the testing code to its new homes. more to do
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@24767 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/__init__.py
+++ src/nti/appserver/tests/__init__.py
@@ -1,12 +1,36 @@
 import nti.appserver
+
+import zope.deferredimport
+zope.deferredimport.initialize()
+
 from hamcrest import assert_that
 from hamcrest import is_
 from hamcrest import is_not
 from hamcrest import none
 from pyramid.testing import setUp as psetUp
 from pyramid.testing import tearDown as ptearDown
-#from pyramid.testing import DummyRequest
-from nti.tests import ByteHeadersDummyRequest as _ByteHeadersDummyRequest
+
+from nti.testing.base import ConfiguringTestBase as _ConfiguringTestBase
+from nti.testing.base import SharedConfiguringTestBase as _SharedConfiguringTestBase
+
+zope.deferredimport.deprecatedFrom(
+	"Import from nti.app.testing.request_response instead.",
+	"nti.app.testing.request_response",
+	"ByteHeadersDummyRequest",
+	)
+
+zope.deferredimport.deprecatedFrom(
+	"Import from nti.app.testing.testing instead.", # Subject to change
+	"nti.app.testing.testing",
+	"unicode_check_headers",
+	"monkey_patch_check_headers",
+	)
+import nti.deprecated # Increase warning verbosity
+assert nti.deprecated
+from nti.app.testing.request_response import DummyRequest
+from nti.app.testing.testing import TestMailDelivery
+from nti.app.testing.testing import ITestMailDelivery
+
 
 _old_pw_manager = None
 def setUpPackage():
@@ -16,7 +40,8 @@ def setUpPackage():
 	# bcrypt default, we speed application-level tests
 	# up (due to faster principal creation and faster password authentication)
 	# The forum tests go from 55s to 15s
-	# This is a nose1 feature and will have to be moved for nose2
+	# This is a nose1 feature and will have to be moved for nose2,
+	# probably to layers (which is a good thing in general)
 	_old_pw_manager = Principal.password_manager_name
 	Principal.password_manager_name = 'Plain Text'
 
@@ -24,14 +49,6 @@ def tearDownPackage():
 	if _old_pw_manager:
 		from nti.dataserver.users import Principal
 		Principal.password_manager_name = _old_pw_manager
-
-
-class DummyRequest(_ByteHeadersDummyRequest):
-	possible_site_names = ()
-	def _on_set_header( self, key, val ):
-		from nti.appserver.tweens.zope_site_tween import _get_possible_site_names
-		site_names = _get_possible_site_names( self )
-		self.possible_site_names = tuple(site_names)
 
 import pyramid.interfaces
 
@@ -42,58 +59,13 @@ from nti.dataserver.tests import mock_dataserver
 import nti.tests
 
 
-from pyramid_mailer.interfaces import IMailer
-from pyramid_mailer.mailer import DummyMailer as _DummyMailer
-from repoze.sendmail.interfaces import IMailDelivery
-
-class ITestMailDelivery(IMailer, IMailDelivery):
-	pass
 
 from nti.appserver import z3c_zpt
 
-import webtest.lint
-from webtest.lint import check_headers as _orig_check_headers
-def unicode_check_headers(headers):
-	"""
-	Up through at least WebTest 1.4.0, the middleware in webtest.lint
-	doesn't ensure that header names and values are bytestrings, but
-	this is required (if not in the spec, then in the implementation
-	provided in gevent 1.0rc1). This check causes that to happen.
-	"""
-	_orig_check_headers( headers )
-	for k, v in headers:
-		assert_that( k, is_( str ), 'Header names must be byte strings' )
-		assert_that( v, is_( str ), 'Header values must be byte strings' )
 
-def monkey_patch_check_headers():
-	"""
-	Patches webtest.lint to use :func:`unicode_check_headers`. This module
-	does this on import.
-	"""
-	if webtest.lint.check_headers.__module__ == 'webtest.lint':
-		webtest.lint.check_headers = unicode_check_headers
-monkey_patch_check_headers()
 
-import simplejson
-import webtest.compat
 import webtest.app
 import webtest.utils
-def monkey_patch_webtest_json_to_simplejson():
-	"""
-	Make webtest use the faster simplejson dump/load functions.
-	"""
-	webtest.compat.loads = simplejson.loads
-	webtest.compat.dumps = simplejson.dumps
-	webtest.app.dumps = simplejson.dumps
-	webtest.app.loads = simplejson.loads
-	webtest.utils.dumps = simplejson.dumps
-	webtest.utils.loads = simplejson.loads
-monkey_patch_webtest_json_to_simplejson()
-
-def monkey_patch_webtest_form20_to_not_be_stupid():
-	import webtest.forms
-	webtest.forms.Field.value = None # Idiot thing is broken in 2.0
-monkey_patch_webtest_form20_to_not_be_stupid()
 
 def _create_request( self, request_factory, request_args ):
 	self.request = request_factory( *request_args )
@@ -105,6 +77,9 @@ def _create_request( self, request_factory, request_args ):
 		if 'REQUEST_METHOD' not in self.request.environ:
 			# req'd by repoze.who 2.1
 			self.request.environ['REQUEST_METHOD'] = 'UNKNOWN'
+
+from nti.app.testing.matchers import has_permission as _has_permission
+from nti.app.testing.matchers import doesnt_have_permission as _doesnt_have_permission
 
 class _TestBaseMixin(object):
 	set_up_packages = (nti.appserver,)
@@ -126,10 +101,10 @@ class _TestBaseMixin(object):
 	ds = property( get_ds, set_ds )
 
 	def has_permission( self, permission ):
-		return HasPermission( True, permission, self.request )
+		return _has_permission( permission, self.request )
 
 	def doesnt_have_permission( self, permission ):
-		return HasPermission( False, permission, self.request )
+		return _doesnt_have_permission( permission, self.request )
 
 
 	def link_with_rel( self, ext_obj, rel ):
@@ -155,7 +130,7 @@ class _TestBaseMixin(object):
 
 
 TestBaseMixin = _TestBaseMixin
-class ConfiguringTestBase(_TestBaseMixin,nti.tests.ConfiguringTestBase):
+class ConfiguringTestBase(_TestBaseMixin,_ConfiguringTestBase):
 	"""
 	Attributes:
 	self.config A pyramid configurator. Note that it does not have a
@@ -194,7 +169,7 @@ class ConfiguringTestBase(_TestBaseMixin,nti.tests.ConfiguringTestBase):
 		ptearDown()
 		super(ConfiguringTestBase,self).tearDown()
 from nti.appserver import pyramid_authorization
-class SharedConfiguringTestBase(_TestBaseMixin,nti.tests.SharedConfiguringTestBase):
+class SharedConfiguringTestBase(_TestBaseMixin,_SharedConfiguringTestBase):
 
 	HANDLE_GC = True # Must do GCs for ZCA cleanup. See superclass
 
@@ -256,28 +231,3 @@ class NewRequestSharedConfiguringTestBase(SharedConfiguringTestBase):
 		result = super(NewRequestSharedConfiguringTestBase,self).setUp()
 		self.beginRequest()
 		return result
-
-from nti.app.testing.matchers import HasPermission
-
-from pyramid.security import has_permission
-
-
-
-class TestMailDelivery(_DummyMailer):
-
-	default_sender = 'no-reply@nextthought.com'
-
-	def __init__( self ):
-		super(TestMailDelivery,self).__init__()
-
-
-	def send( self, fromaddr, toaddr, message ):
-		#from IPython.core.debugger import Tracer; Tracer()() ## DEBUG ##
-
-		self.queue.append( message )
-		# compat with pyramid_mailer messages
-		message.subject = message.get( 'Subject' )
-		payload = message.get_payload()
-		message.body = payload[0].get_payload()
-		if len(payload) > 1:
-			message.html = payload[1].get_payload()
