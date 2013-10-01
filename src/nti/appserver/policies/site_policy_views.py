74f823e16c1eea7cf26810ea64e9ea02e5021bb0
Jason Madden <jason.madden@nextthought.com>
2013-10-01 22:38:36 +0000

    Bring the resources for the mathcounts site into the mathcounts package itself (currently using svn:externals to avoid moving them because our current proxying rules are too strict and won't let us serve the images so they have to appear twice; these probably need rethought---should add a buildout template). Document how to do this for site packages and add a test for strings as well.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@25078 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/policies/site_policy_views.py
+++ src/nti/appserver/policies/site_policy_views.py
@@ -60,20 +60,61 @@ class LegacyResourceView(object):
 
 
 
-def response_for_site_resource_with_marker( marker_interface, request, resource, mime_type ):
+def _response_for_site_resource_with_marker( marker_interface, request, resource, mime_type ):
 	"""
-	Either redirects to a configured resource or returns an empty response with the give mimetype, based
-	on the active sites. We should be registered as a view on a path to a CSS file, and
-	we will return responses within the directory enclosing that css file.
+	Searches for an :class:`.IView` utility having the given resource name
+	and if found, hands control over to it.
 
-	We look for a simple named utility based on the site and determine what to
-	do based on its presence. We could probably simplify this to just redirecting
-	unconditionally, but we might wind up getting lots of 404 responses which is ugly.
+	Currently, this is invoked within a sub-space of some other
+	URL tree. Specific holes are punched in proxy forwarding rules
+	to send exact URL matches (down to the file) through to the routes
+	that call this function. Therefore, the view that is called cannot
+	assume that it can return more than one file (e.g., no siblings
+	can be referenced without also mapping them somehow into the other
+	URL space). This is probably a FIXME: area (to map entire directories
+	through to avoid needing to change multiple URL trees).
+
+	Static Views
+	============
+
+	To use pyramid's default static views, a site package should set
+	up a directory that contains the exact paths to serve (e.g.,
+	``/login/resources/css/site.css``). For example, ``views.py``
+	could live beside the directory ``assets`` containing the file
+	``resources/css/site.css``; then a call to
+	:class:`pyramid.static.static_view` like ``static_view("assets")``
+	would create a view that could be registered under the name
+	``site.css``.
+
+	Redirects and CDNs
+	------------------
+
+	Generally these resources are extremely cacheable and will not place a load
+	on the dataserver. If it is desired to serve them from a CDN sometimes, however,
+	a view callable can use a "static URL" request and redirect to that. In the above
+	example, the view callable would return a redirect to
+	``request.static_url("my.package:assets/resources/css/site.css")``, and
+	the environment would have configured a static view with this path pointing
+	to the CDN: ``config.add_static_view( path="my.package:assets", name="//cdn.com/assets/")``
+	(usually this would be done in a pyramid ZCML file)
+
+	Legacy Documentation
+	====================
+	Either redirects to a configured resource or returns an empty
+	response with the give mimetype, based on the active sites. We
+	should be registered as a view on a path to a CSS file, and we
+	will return responses within the directory enclosing that css
+	file.
+
+	We look for a simple named utility based on the site and determine
+	what to do based on its presence. We could probably simplify this
+	to just redirecting unconditionally, but we might wind up getting
+	lots of 404 responses which is ugly.
 	"""
 
 	view = component.queryUtility( IView, name=resource )
 	if view:
-		return view( None, request )
+		return view( request.context, request )
 
 	# Extra legacy support...these markers are DEPRECATED
 	marker, site_name = site_policies.queryUtilityInSite( marker_interface, request=request, return_site_name=True )
@@ -112,7 +153,18 @@ def webapp_strings_view(request):
 	Redirects to a site specific strings file based on the current site policy.
 	"""
 
-	return response_for_site_resource_with_marker( ISiteStringsMarker, request, 'strings.js', b'application/json')
+	# XXX: FIXME: The final part of the name of the path that matches this route
+	# is 'site.js', but the resource we want to look for is actually 'strings.js',
+	# This makes doing simple static redirections much harder than necessary and places
+	# a lot of burden on specific implementations for sites.
+	# Our hacky fix here is to adjust the PATH_INFO to reflect the desired name
+	assert request.environ['PATH_INFO'].endswith( 'site.js' )
+	request.environ['PATH_INFO'] = request.environ['PATH_INFO'][:-7] + 'strings.js'
+
+	return response_for_site_resource_with_marker( ISiteStringsMarker,
+												   request,
+												   'strings.js',
+												   b'application/javascript')
 
 
 _SITE_LANDING_COOKIE_NAME = b'nti.landing_site_name'
