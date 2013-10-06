274a119bd66a96f2e7ba3bce7239aa1e1c0a5304
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-06 21:12:06 +0000

    change props
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@25287 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/policies/interfaces.py
+++ src/nti/appserver/policies/interfaces.py
@@ -2,59 +2,58 @@
 """
 Site policies interfaces
 
-$Id: interfaces.py 19763 2013-05-31 22:02:07Z carlos.sanchez $
+$Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 from zope import interface
 
-
 class ISitePolicyUserEventListener(interface.Interface):
-    """
-    Register instances of these as utilities by the name of the site
-    they should apply to.
-    """
+	"""
+	Register instances of these as utilities by the name of the site
+	they should apply to.
+	"""
 
-    def map_validation_exception(incoming_data, exception):
-        """
-        Gives a site policy a chance to change an exception being returned
-        during validation.
-        """
+	def map_validation_exception(incoming_data, exception):
+		"""
+		Gives a site policy a chance to change an exception being returned
+		during validation.
+		"""
 
-    def user_will_update_new(user, event):
-        """
-        Handler for the IWillUpdateNewEntityEvent, called
-        before creation is complete or the user is updated.
-        """
+	def user_will_update_new(user, event):
+		"""
+		Handler for the IWillUpdateNewEntityEvent, called
+		before creation is complete or the user is updated.
+		"""
 
-    def user_created(user, event):
-        """
-        Called when a user is created.
-        """
+	def user_created(user, event):
+		"""
+		Called when a user is created.
+		"""
 
 	def user_did_logon(user, event):
 		"""
 		Called when a user logs on in response to a IUserLogonEvent.
 		"""
 
-    def user_created_with_request(user, event):
-        """
-        Called when a user is created in the scope of an interactive
-        request (after user_created).
-        """
+	def user_created_with_request(user, event):
+		"""
+		Called when a user is created in the scope of an interactive
+		request (after user_created).
+		"""
 
-    def user_will_create(user, event):
-        """
-        Called just before a user is created. Do most validation here.
-        """
+	def user_will_create(user, event):
+		"""
+		Called just before a user is created. Do most validation here.
+		"""
 
-    # TODO : I'm not entirely sure this belongs here. Might want to rethink this a lot
-    def upgrade_user(user):
-        """
-        Transition a user from a limited form to the next lest limited forrm.
-        Specifically intended to deal with providing coppa consent.
-        """
+	# TODO : I'm not entirely sure this belongs here. Might want to rethink this a lot
+	def upgrade_user(user):
+		"""
+		Transition a user from a limited form to the next lest limited forrm.
+		Specifically intended to deal with providing coppa consent.
+		"""
 
 import zope.deferredimport
 zope.deferredimport.initialize()
