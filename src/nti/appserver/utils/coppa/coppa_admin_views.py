db5f550618efbed687292aeb2560056324931404
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-07 22:27:26 +0000

    removed used tools
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@25322 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/coppa/coppa_admin_views.py
+++ src/nti/appserver/utils/coppa/coppa_admin_views.py
@@ -5,7 +5,7 @@ Views relating to coppa administration.
 
 $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 
 logger = __import__('logging').getLogger(__name__)
 
