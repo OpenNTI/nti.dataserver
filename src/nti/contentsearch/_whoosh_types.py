cca1b9a7555b01b99fab2822e42483af3de8ac53
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-09 14:56:37 +0000

    change props
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@25399 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_whoosh_types.py
+++ src/nti/contentsearch/_whoosh_types.py
@@ -2,7 +2,7 @@
 """
 Whoosh content types.
 
-$Id: _whoosh_index.py 24097 2013-09-06 21:09:52Z carlos.sanchez $
+$Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
