2dc3df93ee1ad5736909062366364f7b63fc24e5
Jason Madden <jason.madden@nextthought.com>
2013-10-12 20:15:00 +0000

    Move the handling of the password manager from package level down to layer/fixture level. this is cleaner and nose2 doesn't support package fixtures anyway
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@25524 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/forums/tests/__init__.py
+++ src/nti/appserver/forums/tests/__init__.py
@@ -1,4 +1,1 @@
-import nti.appserver.tests
-# Run this setup once at this level
-setUpPackage = nti.appserver.tests.setUpPackage
-tearDownPackage = nti.appserver.tests.tearDownPackage
+
--- src/nti/appserver/tests/__init__.py
+++ src/nti/appserver/tests/__init__.py
@@ -3,49 +3,12 @@ import nti.appserver
 import zope.deferredimport
 zope.deferredimport.initialize()
 
-from pyramid.testing import setUp as psetUp
-from pyramid.testing import tearDown as ptearDown
-
-from nti.testing.base import ConfiguringTestBase as _ConfiguringTestBase
-from nti.testing.base import SharedConfiguringTestBase as _SharedConfiguringTestBase
 
 import nti.deprecated # Increase warning verbosity
 assert nti.deprecated
 from nti.app.testing.request_response import DummyRequest
 from nti.app.testing.testing import TestMailDelivery
 from nti.app.testing.testing import ITestMailDelivery
-
-
-_old_pw_manager = None
-def setUpPackage():
-	from nti.dataserver.users import Principal
-	global _old_pw_manager
-	# By switching from the very secure and very expensive
-	# bcrypt default, we speed application-level tests
-	# up (due to faster principal creation and faster password authentication)
-	# The forum tests go from 55s to 15s
-	# This is a nose1 feature and will have to be moved for nose2,
-	# probably to layers (which is a good thing in general)
-	_old_pw_manager = Principal.password_manager_name
-	Principal.password_manager_name = 'Plain Text'
-
-def tearDownPackage():
-	if _old_pw_manager:
-		from nti.dataserver.users import Principal
-		Principal.password_manager_name = _old_pw_manager
-
-import pyramid.interfaces
-
-
-import zope.component as component
-
-from nti.dataserver.tests import mock_dataserver
-import nti.testing.base
-
-
-
-import webtest.app
-import webtest.utils
 
 
 from nti.app.testing.matchers import has_permission as _has_permission
