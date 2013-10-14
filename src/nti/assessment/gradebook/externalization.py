4605047156a78aa95a941a88e9aba9ccf8c2d138
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-14 16:19:39 +0000

    add updateFromExternalObject for Grades object
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@25556 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/gradebook/externalization.py
+++ src/nti/assessment/gradebook/externalization.py
@@ -8,12 +8,15 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
+logger = __import__('logging').getLogger(__name__)
+
 from zope import interface
 from zope import component
 
 from nti.externalization import externalization
 from nti.externalization import interfaces as ext_interfaces
 from nti.externalization.datastructures import InterfaceObjectIO
+from nti.externalization.datastructures import LocatedExternalDict
 from nti.externalization.autopackage import AutoPackageSearchingScopedInterfaceObjectIO
 
 from . import interfaces as grades_interfaces
@@ -36,7 +39,7 @@ class GradesExternalizer(object):
 		self.grades = grades
 
 	def toExternalObject(self):
-		result = {CLASS:'Grades', MIMETYPE:self.grades.mimeType}
+		result = LocatedExternalDict({CLASS:'Grades', MIMETYPE:self.grades.mimeType})
 		items = result['Items'] = {}
 		for username, grades in self.grades:
 			lst = items[username] = []
@@ -44,7 +47,6 @@ class GradesExternalizer(object):
 				ext = externalization.to_external_object(g)
 				lst.append(ext)
 		return result
-
 
 @interface.implementer(ext_interfaces.IInternalObjectIO)
 class _GradesObjectIO(AutoPackageSearchingScopedInterfaceObjectIO):
