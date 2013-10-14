cad685ab787eb6c62f6800b169e5ac41672de843
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-14 16:33:21 +0000

    add clear methods
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@25558 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/gradebook/gradebook.py
+++ src/nti/assessment/gradebook/gradebook.py
@@ -8,6 +8,8 @@ $Id$
 from __future__ import unicode_literals, print_function, absolute_import
 __docformat__ = "restructuredtext en"
 
+logger = __import__('logging').getLogger(__name__)
+
 from zope import interface
 from zope.container import contained as zcontained
 from zope.annotation import interfaces as an_interfaces
