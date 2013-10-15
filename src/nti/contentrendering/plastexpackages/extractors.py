16eeb8198700e561dc1814e4c16333c861ed6919
Jason Madden <jason.madden@nextthought.com>
2013-10-15 19:12:37 +0000

    The nti.assessment package now houses (almost) everything necessary to render and extract itself. Having this in closer proximity to where the question types are defined will make it easier to define new types, e.g., file submissions.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@25620 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/extractors.py
+++ src/nti/contentrendering/plastexpackages/extractors.py
@@ -31,126 +31,6 @@ from nti.contentrendering import interfaces as crd_interfaces
 from nti.externalization import internalization
 from nti.externalization.externalization import toExternalObject
 
-@interface.implementer(crd_interfaces.IAssessmentExtractor)
-@component.adapter(crd_interfaces.IRenderedBook)
-class _AssessmentExtractor(object):
-	"""
-	"Transforms" a rendered book by extracting assessment information into a separate file
-	called ``assessment_index.json.`` This file describes a dictionary with the following
-	structure::
-
-		Items => { # Keyed by NTIID of the file
-			NTIID => string # This level is about an NTIID section
-			filename => string # The relative containing file for this NTIID
-			AssessmentItems => { # Keyed by NTIID of the question
-				NTIID => string # The NTIID of the question
-				... # All the rest of the keys of the question object
-			}
-			Items => { # Keyed by NTIIDs of child sections; recurses
-				# As containing, except 'filename' will be null/None
-			}
-		}
-
-	"""
-
-	def __init__(self, book=None):
-		# Usable as either a utility factory or an adapter
-		pass
-
-	def transform(self, book):
-		index = {'Items': {}}
-		self._build_index(book.document.getElementsByTagName('document')[0], index['Items'])
-		# index['filename'] = index.get( 'filename', 'index.html' ) # This leads to duplicate data
-		index['href'] = index.get('href', 'index.html')
-		with codecs.open(os.path.join(book.contentLocation, 'assessment_index.json'), 'w', encoding='utf-8') as fp:
-			# sort_keys for repeatability. Do force ensure_ascii because even though we're using codes to
-			# encode automatically, the reader might not decode
-			json.dump(index, fp, indent='\t', sort_keys=True, ensure_ascii=True)
-		return index
-
-	def _build_index(self, element, index):
-		"""
-		Recurse through the element adding assessment objects to the index,
-		keyed off of NTIIDs.
-
-		:param dict index: The containing index node. Typically, this will be
-			an ``Items`` dictionary in a containing index.
-		"""
-		if self._is_uninteresting(element):
-			# It's important to identify uninteresting nodes because
-			# some uninteresting nodes that would never make it into the TOC or otherwise be noticed
-			# actually can present with hard-coded duplicate NTIIDs, which would
-			# cause us to fail.
-			return
-
-		ntiid = getattr(element, 'ntiid', None)
-		if not ntiid:
-			# If we hit something without an ntiid, it's not a section-level
-			# element, it's a paragraph or something like it. Thus we collapse into
-			# the parent. Obviously, we'll only look for AssessmentObjects inside of here
-			element_index = index
-		else:
-			assert ntiid not in index, ("NTIIDs must be unique", ntiid, index.keys())
-			element_index = index[ntiid] = {}
-
-			element_index['NTIID'] = ntiid
-			element_index['filename'] = getattr(element, 'filename', None)
-			if not element_index['filename'] and getattr(element, 'filenameoverride', None):
-				# FIXME: XXX: We are assuming the filename extension. Why aren't we finding
-				# these at filename? See EclipseHelp.zpts for comparison
-				element_index['filename'] = getattr(element, 'filenameoverride') + '.html'
-			element_index['href'] = getattr(element, 'url', element_index['filename'])
-
-		assessment_objects = element_index.setdefault('AssessmentItems', {})
-
-		for child in element.childNodes:
-			ass_obj = getattr(child, 'assessment_object', None)
-			if callable(ass_obj):
-				int_obj = ass_obj()
-				self._ensure_roundtrips(int_obj, provenance=child)  # Verify that we can round-trip this object
-				assessment_objects[child.ntiid] = toExternalObject(int_obj)
-				# assessment_objects are leafs, never have children to worry about
-			elif child.hasChildNodes():  # Recurse for children if needed
-				if getattr(child, 'ntiid', None):
-					containing_index = element_index.setdefault('Items', {})  # we have a child with an NTIID; make sure we have a container for it
-				else:
-					containing_index = element_index  # an unnamed thing; wrap it up with us; should only have AssessmentItems
-				self._build_index(child, containing_index)
-
-	def _ensure_roundtrips(self, assm_obj, provenance=None):
-		ext_obj = toExternalObject(assm_obj)  # No need to go into its children, like parts.
-		__traceback_info__ = provenance, assm_obj, ext_obj
-		raw_int_obj = type(assm_obj)()  # Use the class of the object returned as a factory.
-		internalization.update_from_external_object(raw_int_obj, ext_obj, require_updater=True)
-		factory = internalization.find_factory_for(toExternalObject(assm_obj))  # Also be sure factories can be found
-		assert factory is not None
-		# The ext_obj was mutated by the internalization process, so we need to externalize
-		# again. Or run a deep copy (?)
-
-	def _is_uninteresting(self, element):
-		"""
-		Uninteresting elements do not get an entry in the index. These are
-		elements that have no children and no assessment items of their own.
-		"""
-
-		cache_attr = '@assessment_extractor_uninteresting'
-		if getattr(element, cache_attr, None) is not None:
-			return getattr(element, cache_attr)
-
-		boring = False
-		if callable(getattr(element, 'assessment_object', None)):
-			boring = False
-		elif not element.hasChildNodes():
-			boring = True
-		elif all((self._is_uninteresting(x) for x in element.childNodes)):
-			boring = True
-
-		try:
-			setattr(element, cache_attr, boring)
-		except AttributeError: pass
-
-		return boring
-
 @interface.implementer(crd_interfaces.ICourseExtractor)
 @component.adapter(crd_interfaces.IRenderedBook)
 class _CourseExtractor(object):
@@ -376,7 +256,7 @@ class _RelatedWorkExtractor(object):
 			}
 			result.append((content, dom.childNodes[0]))
 		return result
-	
+
 	def _save_related_content(self, outpath, dom, content_items):
 		items = {}
 		filename = 'related_content_index.json'
@@ -520,7 +400,7 @@ class _NTIVideoExtractor(object):
 				if ntiid and topic_ntiid:
 					result[ntiid].add(topic_ntiid)
 		return result
-					
+
 	def _process_video(self, dom, video, topic_map):
 		entry = {'sources':[], 'transcripts':[]}
 
@@ -619,7 +499,7 @@ class _NTIVideoExtractor(object):
 
 					# add to parent
 					parent.childNodes.append(obj_el)
-				
+
 		# apply overrides
 		# remove from all existing. TOC always win
 		for vid_ntiid in overrides.keys():
@@ -629,7 +509,7 @@ class _NTIVideoExtractor(object):
 		for vid_ntiid, cnt_ids in overrides.items():
 			for container in cnt_ids:
 				containers[container].add(vid_ntiid)
-			
+
 		# remove any empty elements
 		for ntiid, vid_ids in list(containers.items()):
 			if not vid_ids:
@@ -689,56 +569,3 @@ class _NTIVideoExtractor(object):
 					if lesson_el is not None:
 						lesson_el.appendChild(toc_el)
 						lesson_el.appendChild(dom.createTextNode(u'\n'))
-
-@interface.implementer(crd_interfaces.ILessonQuestionSetExtractor)
-@component.adapter(crd_interfaces.IRenderedBook)
-class _LessonQuestionSetExtractor(object):
-
-	def __init__(self, book=None):
-		pass
-
-	def transform( self, book ):
-		lesson_els = book.document.getElementsByTagName( 'courselesson' )
-		dom = book.toc.dom
-		if lesson_els:
-			topic_map = self._get_topic_map(dom)
-			self._process_lessons(dom, lesson_els, topic_map)
-			book.toc.save()
-
-	def _get_topic_map(self, dom):
-		result = {}
-		for topic_el in dom.getElementsByTagName('topic'):
-			ntiid = topic_el.getAttribute('ntiid')
-			if ntiid:
-				result[ntiid] = topic_el
-		return result
-
-	def _process_lessons(self, dom, els, topic_map):
-		for el in els:
-			questionset_els = el.getElementsByTagName('naquestionset')
-			if questionset_els:
-				for questionset_el in questionset_els:
-					lesson_el = topic_map.get(el.ntiid)
-
-					# SAJ: Hack to give question sets a title
-					title_el = questionset_el.parentNode
-					while (not hasattr(title_el, 'title')):
-						title_el = title_el.parentNode
-					label = render_children( title_el.renderer, title_el.title)[0]
-
-					# If the title_el is a topic in the ToC, suppress it.
-					if title_el.ntiid in topic_map.keys():
-						topic_map[title_el.ntiid].setAttribute('suppressed', 'true')
-
-					# Count how many questions are in a question set
-					count = unicode(len(questionset_el.getElementsByTagName('naquestionref')))
-
-					toc_el = dom.createElement('object')
-					toc_el.setAttribute('target-ntiid', questionset_el.ntiid)
-					toc_el.setAttribute('mimeType', questionset_el.mimeType)
-					toc_el.setAttribute('label', label)
-					toc_el.setAttribute('question-count', count)
-					if lesson_el:
-						lesson_el.appendChild(toc_el)
-						lesson_el.appendChild(dom.createTextNode(u'\n'))
-
