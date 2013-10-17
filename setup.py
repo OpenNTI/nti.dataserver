6cf6eff345ebe421e3e7cc9852ae2cbaab460f2a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-17 15:31:52 +0000

    remove rq dep
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@25694 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -304,7 +304,6 @@ setup(
 		 # give a version) (?). grequests 0.1.0 is not compatible with this.
 		 # If something used hooks, a change from 1.1 to 1.2 might break it; no initial issues seen
 		'requests >= 2.0.0',
-		'rq >= 0.3.11',	 # queues for python
 		'setproctitle >= 1.1.7',  # used by gunicorn
 		'setuptools >= 0.6c11',
 		'simplejson >= 3.3.1',
