8ae06706a8afd2f21a26b731ce19248c4217b946
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-21 05:55:23 +0000

    test relatedcontent map subscriber methods
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@25979 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/appserver/contentlibrary/tests/test_relatedcontent_map.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+import os
+
+from zope import component
+
+from nti.contentlibrary import interfaces as lib_interfaces
+from nti.contentlibrary.filesystem import DynamicFilesystemLibrary as FileLibrary
+
+from nti.appserver import interfaces as app_interfaces
+from nti.appserver.contentlibrary import _related_content_map as rcm_module
+
+from nti.testing.base import SharedConfiguringTestBase
+
+from hamcrest import (assert_that, is_, has_entry, has_length, has_property)
+
+class TestRelatedContentIndexMap(SharedConfiguringTestBase):
+
+	def setUp(self):
+		library = FileLibrary(os.path.join(os.path.dirname(__file__), 'library'))
+		component.provideUtility(library, lib_interfaces.IFilesystemContentPackageLibrary)
+		rc_map = rcm_module.RelatedContentIndexMap()
+		component.provideUtility(rc_map, app_interfaces.IRelatedContentIndexMap)
+
+	def test_check_rc_map(self):
+		library = component.getUtility(lib_interfaces.IFilesystemContentPackageLibrary)
+		content_package = library.contentPackages[0]
+		rc_map = component.getUtility(app_interfaces.IRelatedContentIndexMap)
+
+		# add
+		rcm_module.add_related_content_items_from_new_content(content_package, None)
+		assert_that(rc_map, has_length(2))
+		assert_that(rc_map,
+					has_entry('tag:nextthought.com,2011-10:NTI-RelatedWorkRef-CourseTestContent.relatedworkref.0',
+							  has_length(10)))
+		assert_that(rc_map,
+					has_entry('tag:nextthought.com,2011-10:NTI-RelatedWork-CourseTestContent.relatedwork.source1',
+							  has_length(9)))
+
+		assert_that(rc_map, has_property('by_container', has_length(2)))
+
+		assert_that(rc_map.by_container,
+					has_entry('tag:nextthought.com,2011-10:NTI-HTML-CourseTestContent.lesson1',
+							 is_([u'tag:nextthought.com,2011-10:NTI-RelatedWorkRef-CourseTestContent.relatedworkref.0'])))
+
+		assert_that(rc_map.by_container,
+					has_entry('tag:nextthought.com,2011-10:NTI-HTML-CourseTestContent.section_1',
+							 is_([u'tag:nextthought.com,2011-10:NTI-RelatedWork-CourseTestContent.relatedwork.source1'])))
+
+
+		# remove
+		rcm_module.remove_related_content_items_from_old_content(content_package, None)
+		assert_that(rc_map, has_length(0))
+		assert_that(rc_map, has_property('by_container', has_length(0)))
+
+
+if __name__ == '__main__':
+	import unittest
+	unittest.main()
+"""
+
+{
+    "Items": {
+        "tag:nextthought.com,2011-10:NTI-RelatedWorkRef-CourseTestContent.relatedworkref.0": {
+            "ntiid": "tag:nextthought.com,2011-10:NTI-RelatedWorkRef-CourseTestContent.relatedworkref.0",
+            "href": "tag:nextthought.com,2011-10:USSC-HTML-SeleniumTestContent.unified_video",
+            "target-ntiid": "tag:nextthought.com,2011-10:USSC-HTML-SeleniumTestContent.unified_video",
+            "creator": "NextThought, LLC",
+            "desc": "Unified Video 1",
+            "icon": "",
+            "type": "application/vnd.nextthought.content",
+            "section": "required",
+            "visibility": "everyone",
+            "label": "Selenium Tests"
+        },
+        "tag:nextthought.com,2011-10:NTI-RelatedWork-CourseTestContent.relatedwork.source1": {
+            "ntiid": "tag:nextthought.com,2011-10:NTI-RelatedWork-CourseTestContent.relatedwork.source1",
+            "href": "tag:nextthought.com,2011-10:USSC-HTML-SeleniumTestContent.selenium_tests",
+            "target-ntiid": "tag:nextthought.com,2011-10:USSC-HTML-SeleniumTestContent.selenium_tests",
+            "creator": "NextThought, LLC",
+            "desc": "Non-course test content.",
+            "icon": "../SeleniumTestContent/icons/chapters/generic_book.png",
+            "type": "application/vnd.nextthought.content",
+            "visibility": "everyone",
+            "label": "Selenium Tests"
+        }
+    },
+    "Containers": {
+        "tag:nextthought.com,2011-10:NTI-HTML-CourseTestContent.lesson1": [
+            "tag:nextthought.com,2011-10:NTI-RelatedWorkRef-CourseTestContent.relatedworkref.0"
+        ],
+        "tag:nextthought.com,2011-10:NTI-HTML-CourseTestContent.course_test_content": [
+            "tag:nextthought.com,2011-10:NTI-RelatedWork-CourseTestContent.relatedwork.source1"
+        ]
+    }
+}
+"""
