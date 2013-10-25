53d2f20f2f0e6e347506fb6de2b2d88d6b354681
Jason Madden <jason.madden@nextthought.com>
2013-10-25 03:05:14 +0000

    Add a repoze.who plugin we can use for doing token-based auth that is sensitive to the user's password, pretty much like what Github does for RSS feeds...though our token is a bit longer. Still rough and to be integrated.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@26238 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/pyramid_auth.py
+++ src/nti/appserver/pyramid_auth.py
@@ -134,6 +134,128 @@ class _NTIUsersAuthenticatorPlugin(object):
 		except KeyError: # pragma: no cover
 			return None
 
+class _KnownUrlTokenBasedAuthenticator(object):
+	"""
+	A :mod:`repoze.who` plugin that acts in the role of identifier
+	(determining who the remote user is claiming to be)
+	as well as authenticator (matching the claimed remote credentials
+	to the real credentials). This information is not sent in the
+	headers (Authenticate or Cookie) but instead, for the use of
+	copy-and-paste, retrieved directly from query parameters in the
+	URL (specifically, the 'token' parameter).
+
+	This is similar in principal to the AuthTkt cookie we use
+	in HTTP headers, but there is one crucial difference:
+	the authtkt cookie is generally time limited (to mitigate damage due to a
+	publicized cookie) but does not have any relationship to the
+	user's password (the password can change, and existing tkts stay
+	valid).
+
+	In this case, we want the value to stay valid as long as possible.
+	The user is being directly given this token and told to safeguard it,
+	and to plug it into something they won't look at often (an RSS reader).
+	So rather than using a timestamp value to mitigate damage due to a
+	public token, we instead tie it to the password. If the user fears the
+	password has been lost, he is advised to change the password.
+
+	Because it is part of the URL, this information is visible in the
+	logs for the entire HTTP pipeline. To limit the overuse of this, we
+	only want to allow it for particular URLs, as based on the path.
+	"""
+
+	# We actually piggyback off the authtkt implementation, using
+	# a version of the user's password as the 'user data'
+
+	from repoze.who.plugins.auth_tkt import auth_tkt
+	from paste.request import parse_dict_querystring
+	parse_dict_querystring = staticmethod(parse_dict_querystring)
+	from hashlib import sha256
+
+	def __init__( self, secret, allowed_views=() ):
+		"""
+		Creates a combo :class:`.IIdentifier` and :class:`.IAuthenticator`
+		using an auth-tkt like token.
+
+		:param string secret: The encryption secret. May be the same as the
+			auth_tkt secret.
+		:param sequence allowed_views: A set of view names (final path sequences)
+			that will be allowed to be authenticated by this plugin.
+		"""
+		self.secret = secret
+		self.allowed_views = allowed_views
+
+	def identify( self, environ ):
+		# Obviously if there is no token we can't identify
+		if b'QUERY_STRING' not in environ or b'token' not in environ[b'QUERY_STRING']:
+			return
+		if b'PATH_INFO' not in environ:
+			return
+		if not any((environ['PATH_INFO'].endswith(view) for view in self.allowed_views)):
+			return
+
+		query_dict = self.parse_dict_querystring( environ )
+		token = query_dict['token']
+
+		try:
+			_, userid, tokens, user_data = self.auth_tkt.parse_ticket( self.secret,
+																  token,
+																  '0.0.0.0' )
+		except self.auth_tkt.BadTicket: # pragma: no cover
+			return None
+
+		identity = {}
+		identity['nti.pyramid_auth.token.userid'] = userid
+		identity['nti.pyramid_auth.token.userdata'] = user_data
+		return identity
+
+	def forget(self, environ, identity): # pragma: no cover
+		return []
+	def remember(self, environ, identity): # pragma: no cover
+		return []
+
+	def authenticate(self, environ, identity):
+		if 'nti.pyramid_auth.token.userid' not in identity:
+			return None
+
+		userid = identity['nti.pyramid_auth.token.userid']
+		# This part is where the password tie-in is implemented.
+		userdata = identity['nti.pyramid_auth.token.userdata']
+		environ[b'AUTH_TYPE'] = b'token'
+		# We would be storing hashed and salted password data for the
+		# user, currently in bcrypt. Exposing the bcrypt hash and salt
+		# is not a security problem, as the raw password cannot be obtained
+		# from the bcrypt value (not in a feasible amount of time anyway),
+		# and there is no entry point to the system that will accept the
+		# raw bcrypt value and directly compare it to produce an authentication
+		# result---the user must give the real password.
+		#
+		# That said, I'm not comfortable exposing it directly. Therefore, we
+		# take *another* hash on top of that. This needs to be fast, unlike bcrypt,
+		# because we produce and write out these tokens frequently.
+		user_passwd = _NTIUsers.user_password( userid )
+		if user_passwd:
+			raw_data = user_passwd.getPassword()
+			hexdigest = self.sha256( raw_data ).hexdigest()
+			return  userid if hexdigest == userdata else None
+
+		return None # No user or password has changed.
+
+	def tokenForUserid( self, userid ):
+		"""
+		Given a logon for a user, return a token that can be
+		used to identify the user in the future. If the user
+		does not exist or cannot get a token, return None.
+		"""
+
+		user_pw = _NTIUsers.user_password( userid )
+		if user_pw is None:
+			return None
+
+		raw_data = user_pw.getPassword()
+		hexdigest = self.sha256( raw_data ).hexdigest()
+		tkt = self.auth_tkt.AuthTicket(self.secret, userid, '0.0.0.0', user_data=hexdigest)
+		return tkt.cookie_value()
+
 @interface.implementer(IAuthenticator,IIdentifier)
 class _FixedUserAuthenticatorPlugin(object): # pragma: no cover # For use with redbot testing
 
