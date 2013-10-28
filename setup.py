1ba71017c810946867ce28cbae8b6b62e7778821
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-28 13:54:40 +0000

    bump setuptools
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@26305 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -304,7 +304,7 @@ setup(
 		 # If something used hooks, a change from 1.1 to 1.2 might break it; no initial issues seen
 		'requests >= 2.0.1',
 		'setproctitle >= 1.1.8',  # used by gunicorn
-		'setuptools >= 0.6c11',
+		'setuptools >= 1.1.7',
 		'simplejson >= 3.3.1',
 		'six >= 1.4.1',
 		'sympy >= 0.7.3',
