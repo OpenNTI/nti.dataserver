fbe8be3e45abffc8f0cfb7f9f600c4334a0204c2
Jason Madden <jason.madden@nextthought.com>
2013-10-28 20:53:44 +0000

    Switch to pure etag-based cache control, not max-age, for page infos. This needs at least r26323 of the app with info caching on to be effective.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@26327 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/pyramid_renderers.py
+++ src/nti/appserver/pyramid_renderers.py
@@ -535,20 +535,22 @@ class _ContentPackageLibraryCacheController(_AbstractReliableLastModifiedCacheCo
 class _ContentUnitInfoCacheController(object):
 	# rendering this doesn't take long, and we need the rendering
 	# process to decorate us with any sharing preferences that may change
-	# and update our modification stamp.
-	# We exist solely to change the cache age, which speeds up navigation in the app
+	# and update our modification stamp (this is why we can't be a
+	# subclass of _AbstractReliableLastModifiedCacheController)
 
-	max_age = 300 # XXX arbitrary; we can probably go even longer?
+	# We used to set a cache-contral max-age header of five minutes
+	# because that sped up navigation in the app noticebly. But it
+	# also led to testing problems if the tester switched accounts.
+	# The fix is to have the app cache these objects itself,
+	# and we go back to ETag/LastModified caching based on
+	# the rendered form.
+	# See Trell #2962 https://trello.com/c/1TGen4z1
 
 	def __init__( self, context ):
 		pass
 
 	def __call__( self, context, system ):
-		request = system['request']
-		response = request.response
-		if not response.cache_control.max_age:
-			response.cache_control.max_age = self.max_age
-		return request.response
+		return system['request'].response
 
 class REST(object):
 
