e7c79ff8f722ac2326824048da8fd1cb2ec80d11
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-30 16:43:30 +0000

    remove cloud search func
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@26442 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/indexmanager.py
+++ src/nti/contentsearch/indexmanager.py
@@ -4,29 +4,36 @@ Index manager creation methods.
 
 $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-from ._indexmanager import IndexManager
-from . import interfaces  as search_interfaces
-from ._whoosh_content_searcher import wbm_factory
-from ._redis_indexmanager import _RedisIndexManager
-from . import _cloudsearch_interfaces as cloudsearch_interfaces
+logger = __import__('logging').getLogger(__name__)
 
-def create_index_manager_with_whoosh(indexdir=None, use_md5=True, max_users=100, parallel_search=True):
-	content_idx_manager = wbm_factory()
-	return IndexManager(content_idx_manager, search_interfaces.IWhooshEntityIndexManager, parallel_search)
+from . import _indexmanager as search_manager
+from . import interfaces  as search_interfaces
+from . import _redis_indexmanager as redis_search_manager
+from . import _whoosh_content_searcher as content_searcher
+
+def create_index_manager_with_whoosh(indexdir=None,
+									use_md5=True,
+									max_users=100,
+									parallel_search=True):
+	content_idx_manager = content_searcher.wbm_factory()
+	return search_manager.IndexManager(content_idx_manager,
+								  	   search_interfaces.IWhooshEntityIndexManager,
+								  	   parallel_search)
 
 def create_index_manager_with_repoze(parallel_search=True):
-	content_idx_manager = wbm_factory()
-	return IndexManager(content_idx_manager, search_interfaces.IRepozeEntityIndexManager, parallel_search)
-
-def create_index_manager_with_cloudsearch(parallel_search=True):
-	content_idx_manager = wbm_factory()
-	return _RedisIndexManager(content_idx_manager, cloudsearch_interfaces.ICloudSearchEntityIndexManager, parallel_search)
+	content_idx_manager = content_searcher.wbm_factory()
+	return search_manager.IndexManager(content_idx_manager,
+									   search_interfaces.IRepozeEntityIndexManager,
+									   parallel_search)
 
 def create_index_manager_with_repoze_redis(parallel_search=True):
-	content_idx_manager = wbm_factory()
-	return _RedisIndexManager(content_idx_manager, search_interfaces.IRepozeEntityIndexManager, parallel_search)
+	content_idx_manager = content_searcher.wbm_factory()
+	return redis_search_manager._RedisIndexManager(
+										content_idx_manager,
+										search_interfaces.IRepozeEntityIndexManager,
+										parallel_search)
 
 create_index_manager = create_index_manager_with_repoze
