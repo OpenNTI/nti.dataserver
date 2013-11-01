95c12d3279d81c5c3779a86e792e3afb47385c65
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-11-01 22:50:45 +0000

    use zope.deprecation for renamed modules
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@26547 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentsearch/_content_utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+import zope.deprecation
+zope.deprecation.moved('nti.contentsearch.discriminators')
