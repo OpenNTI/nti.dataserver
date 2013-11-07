0236b3fb11c51f941db1be542e1f308efbbc57f0
Jason Madden <jason.madden@nextthought.com>
2013-11-07 19:15:12 +0000

    Jonathan's crash reporter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@26755 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/test_application_feedback.py
+++ src/nti/appserver/tests/test_application_feedback.py
@@ -32,29 +32,35 @@ class TestApplicationFeedback(SharedApplicationTestBase):
 
 	extra_environ_default_user = 'ossmkitty'
 
-	@WithSharedApplicationMockDS(testapp=True, users=True)
-	def test_post_feedback_sends_email(self):
+	def _do_test_email(self, url, subject, message_key):
 		with mock_dataserver.mock_db_trans( self.ds ):
 			coppa_user = users.User.get_user( self.extra_environ_default_user )
-			interface.alsoProvides( coppa_user, site_policies.IMathcountsCoppaUserWithoutAgreement )
 			user_interfaces.IFriendlyNamed( coppa_user ).realname = u'Jason'
 
 		testapp = self.testapp
 		mailer = component.getUtility( ITestMailDelivery )
-		href = self.require_link_href_with_rel( self.resolve_user(), REL_SEND_FEEDBACK )
-		assert_that( href, is_('/dataserver2/users/ossmkitty/@@send-feedback') )
 
-		data = {'body': 'Hi there. I love it. This is a string that is long enough to make the wrapping kick in.'
+		data = {message_key: 'Hi there. I love it. This is a string that is long enough to make the wrapping kick in.'
 				' It must be more than 60 or seventy characters.'}
 
-		res = testapp.post_json( href, data )
+		res = testapp.post_json( url, data )
 		assert_that( res, has_property( 'status_int', 204 ) )
 
 		mailer = component.getUtility( ITestMailDelivery )
 		assert_that( mailer, has_property( 'queue', has_length( 1 ) ) )
-		assert_that( mailer, has_property( 'queue', contains( has_property( 'subject', "Feedback from ossmkitty" ) ) ) )
+		assert_that( mailer, has_property( 'queue', contains( has_property( 'subject', subject ) ) ) )
 
 		assert_that( decodestring(mailer.queue[0].as_string()), contains_string( "Hi there. I love it." ) )
+
+
+	@WithSharedApplicationMockDS(testapp=True, users=True)
+	def test_post_feedback_sends_email(self):
+		url = '/dataserver2/users/ossmkitty/@@send-feedback'
+		self._do_test_email(url, 'Feedback From ossmkitty', 'body')
+
+		href = self.require_link_href_with_rel( self.resolve_user(), REL_SEND_FEEDBACK )
+		assert_that( href, is_(url) )
+
 
 	@WithSharedApplicationMockDS(testapp=True,users=True)
 	def test_post_bad_feedback(self):
@@ -75,3 +81,8 @@ class TestApplicationFeedback(SharedApplicationTestBase):
 							   status=400 )
 			mailer = component.getUtility( ITestMailDelivery )
 			assert_that( mailer, has_property( 'queue', is_empty() ) )
+
+	@WithSharedApplicationMockDS(testapp=True, users=True)
+	def test_post_crash_sends_email(self):
+		url = '/dataserver2/@@send-crash-report'
+		self._do_test_email(url, 'Crash Report From ossmkitty', 'message')
