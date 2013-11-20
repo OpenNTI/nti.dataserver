dff2f12e1f8f0c40e9dca2fdac4e748e85989478
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-11-20 03:46:52 +0000

    add test for rating views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@27250 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/rating_views.py
+++ src/nti/appserver/rating_views.py
@@ -14,22 +14,16 @@ import simplejson
 
 from zope import interface
 from zope import component
-from zope.location.interfaces import ILocation
 
 from pyramid.view import view_config
 from pyramid import httpexceptions as hexc
 from pyramid.security import authenticated_userid
-from pyramid.threadlocal import get_current_request
 
 from nti.appserver import _util
 
-from nti.externalization import oids as ext_oids
-from nti.externalization.singleton import SingletonDecorator
 from nti.externalization import interfaces as ext_interfaces
-from nti.externalization.interfaces import StandardExternalFields
 
-from nti.dataserver import links
-from nti.dataserver import ratings
+from nti.dataserver import rating
 from nti.dataserver import authorization as nauth
 from nti.dataserver import interfaces as nti_interfaces
 
@@ -37,42 +31,22 @@ from nti.utils.maps import CaseInsensitiveDict
 
 @interface.implementer(ext_interfaces.IExternalMappingDecorator)
 @component.adapter(nti_interfaces.IRatable)
-class RatingLinkDecorator(object):
-
-	__metaclass__ = SingletonDecorator
-
-	def decorateExternalMapping(self, context, mapping, extra_elements=()):
-		request = get_current_request()
-		current_username = authenticated_userid(request) if request else None
-		if not current_username or not context.__parent__:
-			return
-
-		target_ntiid = ext_oids.to_external_ntiid_oid(context)
-		if target_ntiid is None:
-			logger.warn("Failed to get ntiid; not adding rating links for %s", context)
-			return
-
-		data = (("rate", "POST"), ('unrate', 'DELETE'))
-		_links = mapping.setdefault(StandardExternalFields.LINKS, [])
-		for rel, method in data:
-			link = links.Link(target_ntiid, rel=rel, method=method,
-							  elements=('@@' + rel,) + extra_elements)
-			interface.alsoProvides(link, ILocation)
-			link.__name__ = ''
-			link.__parent__ = context
-			_links.append(link)
+class RatingLinkDecorator(_util.AbstractTwoStateViewLinkDecorator):
+	false_view = 'rate'
+	true_view = 'unrate'
+	predicate = staticmethod(rating.rates_object)
 
 @view_config( route_name='objects.generic.traversal',
 			  renderer='rest',
 			  context=nti_interfaces.IRatable,
 			  permission=nauth.ACT_READ, # anyone that can see the object
 			  request_method='POST',
-			  name='like')
+			  name='rate')
 def _RateView(request):
 	data = unicode(request.body, request.charset)
 	values = simplejson.loads(data) if request.body else {}
 	values = CaseInsensitiveDict(**values)
-	rate = values.get('rating', values.get('rate', None))
+	rate = values.get('rating', values.get('ranking', values.get('rate', None)))
 	if rate is None:
 		raise hexc.HTTPUnprocessableEntity(detail='rating not specified')
 
@@ -81,7 +55,7 @@ def _RateView(request):
 	except ValueError:
 		raise hexc.HTTPUnprocessableEntity(detail='invaing rating')
 
-	ratings.rate_object(request.context, authenticated_userid(request), rate)
+	rating.rate_object(request.context, authenticated_userid(request), rate)
 	return _util.uncached_in_response( request.context )
 
 @view_config( route_name='objects.generic.traversal',
@@ -89,7 +63,7 @@ def _RateView(request):
 			  context=nti_interfaces.IRatable,
 			  permission=nauth.ACT_READ,
 			  request_method='DELETE',
-			  name='unlike')
+			  name='unrate')
 def _RemoveRatingView(request):
-	ratings.unrate_object(request.context, authenticated_userid(request))
+	rating.unrate_object(request.context, authenticated_userid(request))
 	return _util.uncached_in_response(request.context)
