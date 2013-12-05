301ba31c028134875418bf45b1beb4e75cfb6312
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-12-05 03:30:51 +0000

    add legacy
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@27723 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentsearch/_whoosh_schemas.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+$Id: _whoosh_schemas.py 26609 2013-11-04 22:32:31Z carlos.sanchez $
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+import zope.deprecation
+zope.deprecation.moved('nti.contentsearch.whoosh_schemas')
