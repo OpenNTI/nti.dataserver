e2b15db860fa480e14a3b12774fbfd0f71d4b834
Jason Madden <jason.madden@nextthought.com>
2013-12-23 18:13:18 +0000

    Allow authoring multi-level courses: the courseunit and top-level courselesson structure remain as is for BWC, but courselesson can now have sections, subsections and subsubsections. These are defined inline with the lesson as children...coursesections should use the subsection macro for children that are not coursesubsections for example. For the time being (until I hear otherwise), dates remain defined at the lesson level. The CourseOutline is fully recursively parsed and returned. Test all this.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@28469 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/nticourse.py
+++ src/nti/contentrendering/plastexpackages/nticourse.py
@@ -16,7 +16,11 @@ from datetime import timedelta
 from plasTeX.Base import Command
 from plasTeX.Base import Crossref
 from plasTeX.Base import Environment
-from plasTeX.Base import StartSection
+from plasTeX.Base.LaTeX.Sectioning import part
+from plasTeX.Base.LaTeX.Sectioning import chapter
+from plasTeX.Base.LaTeX.Sectioning import section
+from plasTeX.Base.LaTeX.Sectioning import subsection
+from plasTeX.Base.LaTeX.Sectioning import subsubsection
 
 from pytz import timezone
 from pytz import all_timezones
@@ -26,148 +30,166 @@ from nti.contentrendering import plastexids
 DEFAULT_TZ = 'US/Central'
 
 class coursename(Command):
-    pass
+	pass
 
 class course(Environment, plastexids.NTIIDMixin):
-    args = '[ options:dict ] title number'
+	args = '[ options:dict ] title number'
 
-    counter = 'course'
-    blockType = True
-    forcePars = False
-    _ntiid_cache_map_name = '_course_ntiid_map'
-    _ntiid_allow_missing_title = False
-    _ntiid_suffix = 'course.'
-    _ntiid_title_attr_name = 'ref'
-    _ntiid_type = 'NTICourse'
+	counter = 'course'
+	blockType = True
+	forcePars = False
+	_ntiid_cache_map_name = '_course_ntiid_map'
+	_ntiid_allow_missing_title = False
+	_ntiid_suffix = 'course.'
+	_ntiid_title_attr_name = 'ref'
+	_ntiid_type = 'NTICourse'
 
-    def invoke(self, tex):
-        res = super(course,self).invoke(tex)
-        if self.macroMode == self.MODE_BEGIN:
-            self.title = self.attributes['title']
-            self.number = self.attributes['number']
-        return res
+	def invoke(self, tex):
+		res = super(course,self).invoke(tex)
+		if self.macroMode == self.MODE_BEGIN:
+			self.title = self.attributes['title']
+			self.number = self.attributes['number']
+		return res
 
-    def digest(self, tokens):
-        tok = super(course,self).digest(tokens)
-        if self.macroMode == self.MODE_BEGIN:
-            if not getattr(self, 'title', ''):
-                raise ValueError("Must specify a title using \\caption")
+	def digest(self, tokens):
+		tok = super(course,self).digest(tokens)
+		if self.macroMode == self.MODE_BEGIN:
+			if not getattr(self, 'title', ''):
+				raise ValueError("Must specify a title using \\caption")
 
-            options = self.attributes.get( 'options', {} ) or {}
-            if 'tz' in options and options['tz'] in all_timezones:
-                self.tz = timezone(options['tz'])
-            else:
-                logger.warn('No valid timezone specified')
-                self.tz = timezone( DEFAULT_TZ )
+			options = self.attributes.get( 'options', {} ) or {}
+			if 'tz' in options and options['tz'] in all_timezones:
+				self.tz = timezone(options['tz'])
+			else:
+				logger.warn('No valid timezone specified')
+				self.tz = timezone( DEFAULT_TZ )
 
-            __traceback_info__ = options, self.attributes
+			__traceback_info__ = options, self.attributes
 
-        return tok
+		return tok
 
-    class courseboard(Command):
-        args = '[ options:dict ] ntiid:str'
+	class courseboard(Command):
+		args = '[ options:dict ] ntiid:str'
 
-        def digest(self, tokens):
-            super(course.courseboard, self).digest(tokens)
-            self.parentNode.discussion_board = self.attributes.get('ntiid')
+		def digest(self, tokens):
+			super(course.courseboard, self).digest(tokens)
+			self.parentNode.discussion_board = self.attributes.get('ntiid')
 
-    class courseannouncementboard(Command):
-        args = '[ options:dict ] ntiid:str'
+	class courseannouncementboard(Command):
+		args = '[ options:dict ] ntiid:str'
 
-        def digest(self, tokens):
-            super(course.courseannouncementboard, self).digest(tokens)
-            self.parentNode.announcement_board = self.attributes.get('ntiid')
+		def digest(self, tokens):
+			super(course.courseannouncementboard, self).digest(tokens)
+			self.parentNode.announcement_board = self.attributes.get('ntiid')
 
-    class coursecommunity(Command):
-        args = '[ options:dict ] ntiid:str'
+	class coursecommunity(Command):
+		args = '[ options:dict ] ntiid:str'
 
-        def digest(self, tokens):
-            tok = super(course.coursecommunity,self).digest(tokens)
+		def digest(self, tokens):
+			tok = super(course.coursecommunity,self).digest(tokens)
 
-            options = self.attributes.get( 'options', {} ) or {}
-            if 'scope' in options:
-                self.scope = options['scope']
-            else:
-                self.scope = u'public'
+			options = self.attributes.get( 'options', {} ) or {}
+			if 'scope' in options:
+				self.scope = options['scope']
+			else:
+				self.scope = u'public'
 
-            __traceback_info__ = options, self.attributes
+			__traceback_info__ = options, self.attributes
 
-            return tok
+			return tok
 
 
 class courseunitname(Command):
-    pass
+	pass
 
 class courseunit(Environment, plastexids.NTIIDMixin):
-    args = '[ options:dict ] title:str'
+	args = '[ options:dict ] title:str'
 
-    counter = "courseunit"
-    blockType = True
-    forcePars = False
-    _ntiid_cache_map_name = '_courseunit_ntiid_map'
-    _ntiid_allow_missing_title = False
-    _ntiid_suffix = 'course.unit.'
-    _ntiid_title_attr_name = 'ref'
-    _ntiid_type = 'NTICourseUnit'
+	counter = "courseunit"
+	blockType = True
+	forcePars = False
+	_ntiid_cache_map_name = '_courseunit_ntiid_map'
+	_ntiid_allow_missing_title = False
+	_ntiid_suffix = 'course.unit.'
+	_ntiid_title_attr_name = 'ref'
+	_ntiid_type = 'NTICourseUnit'
 
-    def invoke(self, tex):
-        res = super(courseunit,self).invoke(tex)
-        if self.macroMode == self.MODE_BEGIN:
-            self.title = self.attributes['title']
-        return res
+	def invoke(self, tex):
+		res = super(courseunit,self).invoke(tex)
+		if self.macroMode == self.MODE_BEGIN:
+			self.title = self.attributes['title']
+		return res
 
-    def digest(self, tokens):
-        res = super(courseunit,self).digest(tokens)
-        if self.macroMode == self.MODE_BEGIN:
-            if not getattr(self, 'title', ''):
-                raise ValueError("Must specify a title using \\caption")
+	def digest(self, tokens):
+		res = super(courseunit,self).digest(tokens)
+		if self.macroMode == self.MODE_BEGIN:
+			if not getattr(self, 'title', ''):
+				raise ValueError("Must specify a title using \\caption")
 
-            options = self.attributes.get( 'options', {} ) or {}
-            __traceback_info__ = options, self.attributes
+			options = self.attributes.get( 'options', {} ) or {}
+			__traceback_info__ = options, self.attributes
 
-        return res
+		return res
 
 class courselessonname(Command):
-    pass
+	pass
 
 class courselessonref(Crossref.ref):
-    args = 'label:idref date:str'
+	args = 'label:idref {date:str}'
 
-    def invoke( self, tex ):
-        res = super(courselessonref, self).invoke( tex )
-        dates = self.attributes['date'].split(',')
-        self.date = []
-        for date in dates:
-            if date:
-                date = date.split('/')
-                self.date.append(datetime(int(date[2]), int(date[0]), int(date[1])))
-            else:
-                self.date.append(datetime.today())
-        self.date[-1] = self.date[-1] + timedelta(days=1) - timedelta(microseconds=1)
-        return res
+	date = ()
 
-class courselesson(StartSection):
-    args = '* [ toc ] title label:id'
-    blockType = True
-    counter = 'courselesson'
-    forcePars = False
-    level = Command.CHAPTER_LEVEL
+	def invoke( self, tex ):
+		res = super(courselessonref, self).invoke( tex )
+		if self.attributes.get('date'):
+			__traceback_info__ = self.attributes['date']
+			dates = self.attributes['date'].split(',')
+			self.date = []
+			for date in dates:
+				if date:
+					date = date.split('/')
+					# FIXME: Non-standard date representation
+					self.date.append(datetime(int(date[2]), int(date[0]), int(date[1])))
+				else:
+					self.date.append(datetime.today())
+			self.date[-1] = self.date[-1] + timedelta(days=1) - timedelta(microseconds=1)
+		return res
+
+class coursepart(part):
+	counter = 'course' + part.counter
+
+class courselesson(chapter):
+	args = '* [ toc ] title label:id'
+	blockType = True
+	counter = 'courselesson'
+	forcePars = False
+
+class courselessonsection(section):
+	counter = 'course' + section.counter
+
+class courselessonsubsection(subsection):
+	counter = 'course' + subsection.counter
+
+class courselessonsubsubsection(subsubsection):
+	counter = 'course' + subsubsection.counter
 
 class courseinfoname(Command):
-    pass
+	pass
 
-class courseinfo(StartSection):
-    args = '* [ toc ] title'
-    blockType = True
-    counter = 'courseinfo'
-    forcePars = False
-    level = Command.SECTION_LEVEL
+class courseinfo(section):
+	args = '* [ toc ] title'
+	blockType = True
+	counter = 'courseinfo'
+	forcePars = False
 
 def ProcessOptions( options, document ):
-    document.context.newcounter('course')
-    document.context.newcounter('courseinfo')
-    document.context.newcounter('courselesson')
-    document.context.newcounter('courseunit')
+	document.context.newcounter('course')
+	document.context.newcounter('courseinfo')
+	document.context.newcounter('courseunit')
+
+	for counter_cls in courseunit, coursepart, courselesson, courselessonsection, courselessonsubsection, courselessonsubsubsection:
+		document.context.newcounter(counter_cls.counter)
+
 
 from plasTeX.interfaces import IOptionAwarePythonPackage
 from zope import interface
