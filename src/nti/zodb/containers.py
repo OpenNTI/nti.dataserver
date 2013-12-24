dd8f53fa630a15b274759bb6ddf3ff2531294fb7
Jason Madden <jason.madden@nextthought.com>
2013-12-24 18:47:17 +0000

    For insructors, provide a CourseActivity link that contains, from most recent to oldest, a pageable list of things that happened in the course. Currently this means assignment submissions and feedback items added. That's more or less the dataset for slide 3.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@28485 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/zodb/containers.py
+++ src/nti/zodb/containers.py
@@ -11,24 +11,36 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-
-
 import struct
+
+# ! means network byte order, in case we cross architectures
+# anywhere (doesn't matter), but also causes the sizes to be
+# standard, which may matter between 32 and 64 bit machines
+# Q is 64-bit unsigned int, d is 64-bit double
+
+_float_to_double_bits = struct.Struct(b'!d').pack
+_double_bits_to_long = struct.Struct(b'!Q').unpack
+
+_long_to_double_bits = struct.Struct(b'!Q').pack
+_double_bits_to_float = struct.Struct(b'!d').unpack
+
 def time_to_64bit_int( value ):
 	"""
-	Given a Python floating point object (usually a time value), return a 64-bit unsigned int that represents it.
-	Useful for storing in a OI tree, when you really want a float (since BTrees
-	does not provide OF).
+	Given a Python floating point object (usually a time value),
+	return a 64-bit unsigned int that represents it. Useful for
+	storing as the value in a OL tree, when you really want a float
+	(since BTrees does not provide OF), or as a key in a Lx tree.
 	"""
 	if value is None: # pragma: no cover
 		raise ValueError("For consistency, you must supply the lastModified value" )
-	# ! means network byte order, in case we cross architectures
-	# anywhere (doesn't matter), but also causes the sizes to be
-	# standard, which may matter between 32 and 64 bit machines
-	# Q is 64-bit unsigned int, d is 64-bit double
-	return struct.unpack( b'!Q', struct.pack( b'!d', value ) )[0]
+
+	return _double_bits_to_long( _float_to_double_bits( value ) )[0]
+
 
 ZERO_64BIT_INT = time_to_64bit_int( 0.0 )
 
 def bit64_int_to_time( value ):
-	return struct.unpack( b'!d', struct.pack( b'!Q', value ) )[0]
+	return _double_bits_to_float( _long_to_double_bits(value) )[0]
+
+
+assert bit64_int_to_time(ZERO_64BIT_INT) == 0.0
