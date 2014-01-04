7ce16e5ff599d8aec1dad9c09709b2464c9188e8
Jason Madden <jason.madden@nextthought.com>
2014-01-04 15:23:15 +0000

    As the last environments go to buildout, remove the temporary direct dependencies on separate packages from nti.dataserver. Some environments (e.g., ou-test) were in an inbetween state with old develop-eggs so they still 'worked' even though they had no sources entry anymore.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@28853 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -133,20 +133,6 @@ setup(
 		'nti.plasTeX',
 		'nti.geventwebsocket',
 		'pywikipedia',
-		# NOTE: These are temporary!
-
-		'nti.app.sites.mathcounts',
-		'nti.app.sites.alpha',
-		'nti.app.sites.demo',
-		'nti.app.sites.litworld',
-		'nti.app.sites.prmia',
-		'nti.app.sites.law',
-		'nti.app.sites.rwanda',
-		'nti.app.sites.columbia',
-		'nti.wsgi.cors',
-		'nti.openmath',
-		'nti.assessment',
-
 		# Zope Acquisition; used by contentratings implicitly
 		# cool concept. Pulls in ExtensionClass (which should only be used for acquisition)
 		'Acquisition >= 4.0' if not IS_PYPY else '',  # Extensions don't build on pypy
