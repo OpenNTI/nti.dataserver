dfc9747950bb267b283ee421cb996b1ee4145398
Jason Madden <jason.madden@nextthought.com>
2014-01-20 19:36:06 +0000

    Support adding '/@@avatar' to any user URL to get a re-direct to the current avatarURL
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@30990 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/appserver/user_views.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Generic views for any user (or sometimes, entities).
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import six
+import time
+
+from pyramid.view import view_config
+
+from nti.dataserver.users.users_external import _avatar_url
+from nti.dataserver.links_external import render_link
+
+from .httpexceptions import HTTPFound
+
+@view_config( route_name='objects.generic.traversal',
+			  renderer='rest',
+			  context='nti.dataserver.interfaces.IUser',
+			  request_method='GET',
+			  name='avatar')
+def avatar_view(context, request):
+	"""
+	Redirects to the location of the actual avatar.
+	"""
+
+	# Use a 302 response to tell clients where to go,
+	# and let them cache it for awhile (a 303 is completely
+	# uncachable). We expect that this method will not be
+	# hit by the actual user himself, only his friends, so
+	# he won't notice a stale response.
+
+	# We use a private method to do this because we rely
+	# on implementation details about the user and his data.
+
+	url_or_link = _avatar_url(context)
+	if not isinstance(url_or_link, six.string_types):
+		# In this case, we have a file we're hosting.
+		# What happens when the user changes or removes that file?
+		# we're sending direct OID links, does it still work? Or will it 404?
+		url_or_link = render_link(url_or_link)
+
+	result = HTTPFound(url_or_link)
+	# Let it be cached for a bit. gravatar uses 5 minutes
+	result.cache_control.max_age = 300
+	result.expires = time.time() + 300
+	return result
