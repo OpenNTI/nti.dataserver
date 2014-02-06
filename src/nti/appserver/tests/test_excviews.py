41fb384cddbd14d9442d99cc8f6253e622e439e9
Jason Madden <jason.madden@nextthought.com>
2014-02-06 17:04:27 +0000

    A way to both email an error report in a rate-limited fashion, and also respond with a custom error response. Use this for storage errors and errors communicating with LDAP during handshake.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@32392 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/appserver/tests/test_excviews.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+#disable: accessing protected members, too many methods
+#pylint: disable=W0212,R0904
+
+
+from hamcrest import assert_that
+from hamcrest import is_
+from hamcrest import contains_string
+from hamcrest import has_property
+
+import sys
+
+from nti.testing.time import time_monotonically_increases
+
+from ..excviews import AbstractRateLimitedExceptionView
+from ..excviews import EmailReportingExceptionView
+
+
+@time_monotonically_increases
+def test_rate_limiting_and_exception_catching():
+
+	class TestView(AbstractRateLimitedExceptionView):
+		# This is dependent on the log capture taking time
+		# measures, so it could vary
+		aux_period = 5
+		aux_called = False
+
+		def _do_create_response(self):
+			raise Exception()
+
+		def _do_aux_action(self):
+			self.aux_called = True
+			raise Exception()
+
+	view = TestView(None,None)
+
+	view()
+	# Initially
+	assert_that( view, has_property('aux_called', True) )
+	assert_that( view, has_property( '_last_aux_time', 2 ))
+	view.aux_called = False
+
+	# Now we have to step it two more times
+	view()
+	assert_that( view, has_property('aux_called', False) )
+
+	view()
+	assert_that( view, has_property( '_last_aux_time', 2 ))
+	assert_that( view, has_property('aux_called', False) )
+
+	view()
+	assert_that( view, has_property('aux_called', True) )
+	assert_that( view, has_property( '_last_aux_time', 9 ))
+
+from cStringIO import StringIO
+
+def test_find_paste():
+
+	from paste.exceptions.errormiddleware import ErrorMiddleware
+
+	class Exp(Exception):
+		pass
+
+	def app(*args):
+		raise Exp()
+
+	wrapped = ErrorMiddleware(app)
+
+	environ = {'paste.throw_errors': True}
+
+	try:
+		wrapped(environ, None)
+		assert False
+	except Exp as e:
+		info = sys.exc_info()
+
+	class Request(object):
+		exc_info = None
+		environ = None
+
+	request = Request()
+	request.exc_info = info
+	request.environ = environ
+	# The default configuration will report to
+	# wsgi.errors, we can test that
+	environ['wsgi.errors'] = StringIO()
+	view = EmailReportingExceptionView(e,request)
+
+	assert_that( view._find_exception_handler(),
+				 is_(wrapped.exception_handler))
+
+	view._do_aux_action()
+	assert_that( environ['wsgi.errors'].getvalue(),
+				 contains_string('raise Exp'))
