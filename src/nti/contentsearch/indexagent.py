87766c8de796e8353c5a94f0f4cb1cddb764e31f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-02-10 17:49:18 +0000

    remove unused
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@32601 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/indexagent.py
+++ src/nti/contentsearch/indexagent.py
@@ -12,17 +12,10 @@ logger = __import__('logging').getLogger(__name__)
 
 import six
 
-from zope import component
-
-from ZODB import loglevels
-
 from nti.dataserver.users import Entity
 from nti.dataserver import interfaces as nti_interfaces
 
-from nti.ntiids import ntiids
-
 from . import common
-from . import discriminators
 from . import get_indexable_types
 from . import interfaces as search_interfaces
 
@@ -38,7 +31,8 @@ def get_creator(obj):
 	return adapted.get_creator()
 
 def _check_event(target, change_type, data_type, data):
-	result = change_type in _event_types and common.normalize_type_name(data_type) in get_indexable_types()
+	result = change_type in _event_types and \
+			 common.normalize_type_name(data_type) in get_indexable_types()
 	if result and _only_delete_by_owner and change_type == nti_interfaces.SC_DELETED:
 		username = target if isinstance(target, six.string_types) else target.username
 		creator = get_creator(data)
@@ -52,22 +46,25 @@ def _process_event(indexmanager, target, change_type, data_type, data):
 		return func(target, data=data, type_name=data_type) or True if func else False
 	return False
 
+_changeType_events = (nti_interfaces.SC_CREATED,
+					  nti_interfaces.SC_SHARED,
+					  nti_interfaces.SC_MODIFIED)
+
 def _handle_event(indexmanager, entity, changeType, change_object, broadcast=None):
 	# make sure we have a valid entity
-	entity = Entity.get_entity(entity) if isinstance(entity, six.string_types) else entity
+	entity = Entity.get_entity(entity) \
+			 if isinstance(entity, six.string_types) else entity
 
 	should_process = True
 	if not broadcast:  # only check if we're not a global broadcast
-		if changeType in (nti_interfaces.SC_CREATED, nti_interfaces.SC_SHARED, nti_interfaces.SC_MODIFIED):
+		if changeType in _changeType_events:
 			should_process = change_object.isSharedDirectlyWith(entity)
 
 	result = False
 	if should_process:
 		data_type = common.get_type_name(change_object)
-		result = _process_event(indexmanager, entity, changeType, data_type, change_object)
-		logger.log(loglevels.TRACE, 'Index event target="%s", change=%s, data_type=%s, broadcast=%s handled=%s',
-				   entity, changeType, data_type, broadcast, result)
-
+		result = _process_event(indexmanager, entity, changeType, data_type,
+								change_object)
 	return result
 
 def handle_index_event(indexmanager, target, change, broadcast=None):
@@ -85,8 +82,9 @@ def handle_index_event(indexmanager, target, change, broadcast=None):
 
 	:param broadcast: This is set to true when events are being sent for the sole
 		purpose of indexing to the target. In that case, we bypass any sharing/ownership
-		restrictions and do process the event. NOTE: This is unfortunately tightly coupled with
-		users.py and chat_transcripts.py and needs rethought. chat_transcripts.py sends all
+		restrictions and do process the event. NOTE: This is unfortunately tightly
+		coupled with users.py and chat_transcripts.py and needs rethought.
+		chat_transcripts.py sends all
 		events as broadcasts
 
 	"""
@@ -94,28 +92,4 @@ def handle_index_event(indexmanager, target, change, broadcast=None):
 		return False
 
 	result = _handle_event(indexmanager, target, change.type, change.object, broadcast)
-	return result
-
-def handle_external(entity, changeType, oid, uid, broadcast=None):
-	"""
-	entity: Entity to which the event is directed
-	changeType: Change type
-	oid: Object unicode id
-	uid: Object int id
-	broadcast: Broadcast flag
-	"""
-	indexmanager = component.queryUtility(search_interfaces.IIndexManager)
-	if indexmanager is None:
-		return
-
-	if changeType == nti_interfaces.SC_DELETED:
-		result = indexmanager.unindex(entity, uid)
-	else:
-		change_object = discriminators.query_object(uid)
-		try:
-			change_object = ntiids.find_object_with_ntiid(oid) if change_object is None else change_object
-		except KeyError:
-			return
-
-		result = _handle_event(indexmanager, entity, changeType, change_object, broadcast)
 	return result
