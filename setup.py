bf0420b0edeb05aa42a061f1ec23cc0ac922e99c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-02-11 20:37:19 +0000

    bump logilab-common
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@32715 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -232,7 +232,7 @@ setup(
 		'html5lib',
 		'isodate >= 0.4.9',	 # ISO8601 date/time/duration parser and formatter
 		'joblib >= 0.7.1',	# Python functions as pipeline jobs.
-		'logilab-common >= 0.60.0',
+		'logilab-common',
 		'lxml', # Powerful and Pythonic HTML/XML processing library combining libxml2/libxslt with the ElementTree API. Also a pull parser.
 		'nameparser >= 0.2.8', # Human name parsing
 		'nltk >= 2.0.4',
