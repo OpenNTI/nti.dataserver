c78c446d0fdae802e665f08f48e2673c3cdfcc47
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-02-12 04:46:15 +0000

    compute a digest
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@32786 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/tests/test_search_query.py
+++ src/nti/contentsearch/tests/test_search_query.py
@@ -20,5 +20,12 @@ class TestSearchQuery(unittest.TestCase):
 		qo = QueryObject(term=u'term')
 		assert_that(qo.term, is_(u'term'))
 		assert_that(qo.query, is_(u'term'))
-		
-
+		assert_that(qo.digest(), is_('3c602f4dbf6d3edddf108dda5316ab97'))
+		qo.searchOn = ('note',)
+		assert_that(qo.digest(), is_('4d1c2abdc06d3a5a79bdabe95d3decd1'))
+		qo.searchOn = ('note', 'redaction')
+		assert_that(qo.digest(), is_('1e7fe7209ca6cc50c0d0d6a22d117943'))
+		qo.searchOn = ('redaction', 'note')
+		assert_that(qo.digest(), is_('1e7fe7209ca6cc50c0d0d6a22d117943'))
+		qo.indexid = 'xyz'
+		assert_that(qo.digest(), is_('23c695bbef1e918025da86f2d281383b'))
