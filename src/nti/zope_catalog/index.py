be70bffc23282feffb1f6b835138f536c22d2dfc
Jason Madden <jason.madden@nextthought.com>
2014-02-24 21:19:20 +0000

    storage checkpoint two.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@33545 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/zope_catalog/index.py
+++ src/nti/zope_catalog/index.py
@@ -23,10 +23,29 @@ import zope.index.topic
 import zope.container.contained
 
 import zc.catalog.catalogindex
+import zc.catalog.index
 
 import BTrees
 
 from nti.utils.property import alias
+
+class _ZCApplyMixin(object):
+	"""
+	Convert zope.index style two-tuple query to new style.
+	"""
+
+
+	def apply(self, query):
+		# Convert zope.index style two-tuple (min/max)
+		# query to new-style
+		if isinstance(query, tuple) and len(query) == 2:
+			if query[0] == query[1]:
+				# common case of exact match
+				query = {'any_of': (query[0],)}
+			else:
+				query = {'between': query}
+		return super(_ZCApplyMixin,self).apply(query)
+
 
 class _ZCAbstractIndexMixin(object):
 	"""
@@ -38,17 +57,6 @@ class _ZCAbstractIndexMixin(object):
 	_fwd_index = alias('values_to_documents')
 	_rev_index = alias('documents_to_values')
 	_num_docs = alias('documentCount')
-
-	def apply(self, query):
-		# Convert zope.index style two-tuple (min/max)
-		# query to new-style
-		if isinstance(query, tuple) and len(query) == 2:
-			if query[0] == query[1]:
-				# common case of exact match
-				query = {'any_of': query[0]}
-			else:
-				query = {'between': query}
-		return super(_ZCAbstractIndexMixin,self).apply(query)
 
 
 @interface.implementer(IFieldIndex)
@@ -84,15 +92,57 @@ class CaseInsensitiveAttributeFieldIndex(AttributeIndex,
 			value = value.lower()
 		return value
 
+# Normalizing and wrappers:
+# The normalizing code needs to get the actual values. Because AttributeIndex
+# gets the attribute value in index_doc and then calls the same method on super
+# with that returned value, the NormalizationWrapper has to extend AttributeIndex
+# to get the right value to pass to the normamlizer. That means it cannot be used
+# to wrap another AttributeIndex, only a plain ValueIndex or SetIndex. Note
+# that it is somewhat painful to construct
 
-class IntegerAttributeIndex(_ZCAbstractIndexMixin,
+class IntegerValueIndex(_ZCApplyMixin,
+						_ZCAbstractIndexMixin,
+						zc.catalog.index.ValueIndex):
+	"""
+	A \"raw\" index that is optimized for, and only supports,
+	storing integer values. To normalize, use a :class:`zc.catalog.index.NormalizationWrapper`;
+	to store in a catalog and normalize, use a  :class:`zc.catalog.catalogindex.NormalizationWrapper`
+	(which is an attribute index).
+	"""
+	def clear(self):
+		super(IntegerValueIndex, self).clear()
+		self.documents_to_values = self.family.II.BTree()
+		self.values_to_documents = self.family.IO.BTree()
+
+class IntegerAttributeIndex(IntegerValueIndex,
 							zc.catalog.catalogindex.ValueIndex):
 	"""
 	An attribute index that is optimized for, and only supports,
-	storing integer values. To normalize, use a :class:`zc.catalog.catalogindex.NormalizationWrapper`
+	storing integer values. To normalize, use a :class:`zc.catalog.index.NormalizationWrapper`;
+	note that because :class:`zc.catalog.catalogindex.NormalizationWrapper` is
+	also an attribute index it cannot be used to wrap this class, and your normalizer
+	will have to return an object that has the right attribute.
 	"""
 
-	def clear(self):
-		super(IntegerAttributeIndex, self).clear()
-		self.documents_to_values = self.family.II.BTree()
-		self.values_to_documents = self.family.IO.BTree()
+
+class NormalizationWrapper(_ZCApplyMixin,
+						   zc.catalog.catalogindex.NormalizationWrapper):
+	"""
+	An attribute index that wraps a raw index and normalizes values.
+
+	This class exists mainly to sort out the difficulty constructing
+	instances by only accepting keyword arguments.
+	"""
+
+	def __init__( self, field_name=None, interface=None, field_callable=False,
+				  index=None, normalizer=None, is_collection=False):
+		"""
+		You should only call this constructor with keyword arguments;
+		due to inheritance, mixing and matching keyword and non-keyword is a bad idea.
+		The first three arguments that are not keyword are taken as `field_name`,
+		`interface` and `field_callable`.
+		"""
+		# sadly we can't reuse any of the defaults from the super classes, and we
+		# must rely on the order of parameters
+		super(NormalizationWrapper,self).__init__(field_name, interface, field_callable,
+												  index, normalizer, is_collection)
--- src/nti/zope_catalog/tests/test_datetime.py
+++ src/nti/zope_catalog/tests/test_datetime.py
@@ -17,8 +17,8 @@ logger = __import__('logging').getLogger(__name__)
 import unittest
 from hamcrest import assert_that
 from hamcrest import is_
-from hamcrest import has_key
-from hamcrest import has_entry
+from hamcrest import contains
+from hamcrest import contains_inanyorder
 
 from nti.testing import base
 from nti.testing import matchers
@@ -26,13 +26,15 @@ from nti.testing import matchers
 from ..datetime import TimestampTo64BitIntNormalizer
 from ..datetime import TimestampNormalizer
 from ..datetime import TimestampToNormalized64BitIntNormalizer
+from ..index import IntegerValueIndex
+from ..index import  NormalizationWrapper
 import datetime
 import time
 
 from nti.zodb.containers import time_to_64bit_int
 
 class TestNormalizers(unittest.TestCase):
-
+	field = 1
 	def test_int_normalizer(self):
 		assert_that( TimestampTo64BitIntNormalizer().value(123456.78),
 					 is_(4683220298531686318) )
@@ -77,3 +79,46 @@ class TestNormalizers(unittest.TestCase):
 		# change resolution
 		normalizer.resolution = TimestampNormalizer.RES_HOUR
 		assert_that( normalizer.value(orig), is_(hour_normalized) )
+
+	def test_combined_normalizer_query(self):
+		orig = datetime.datetime(2014, 2, 24, 12, 30, 7, 650261)
+		orig = time.mktime(orig.timetuple())
+
+		minute_normalized = datetime.datetime(2014, 2, 24, 12, 30)
+		minute_normalized_time = time.mktime(minute_normalized.timetuple())
+		minute_normalized = time_to_64bit_int(minute_normalized_time)
+
+		hour_normalized = datetime.datetime(2014, 2, 24, 12)
+		hour_normalized_time = time.mktime(hour_normalized.timetuple())
+		hour_normalized = time_to_64bit_int(hour_normalized_time)
+
+		normalizer = TimestampToNormalized64BitIntNormalizer()
+
+		index = NormalizationWrapper('field', index=IntegerValueIndex(),
+									 normalizer=normalizer)
+
+		self.field = orig
+
+		index.index_doc(1, self)
+
+		# Exact-match query for the original value
+		assert_that( index.apply((orig, orig)),
+					 contains(1))
+
+		# exact-match query for the normalized value
+		assert_that( index.apply((minute_normalized_time,minute_normalized_time)),
+					 contains(1))
+
+		# range query, beginning time only
+		assert_that( index.apply({'between': (hour_normalized_time,)}),
+					 contains(1))
+
+
+		self.field = 1234
+		index.index_doc(2, self)
+
+		assert_that( index.apply( {'any': None} ),
+					 contains_inanyorder(1, 2))
+
+		assert_that( list(index.sort( (2,1) )),
+					 contains(2, 1))
