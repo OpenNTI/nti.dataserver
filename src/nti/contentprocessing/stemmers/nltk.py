bcd9201d6ad601dd6812aec72480542edc8a840e
Jason Madden <jason.madden@nextthought.com>
2014-03-06 18:02:16 +0000

    The stemmer object we were using was not concurrency safe which could cause rendering failures or incorrect index results.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@34305 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/stemmers/nltk.py
+++ src/nti/contentprocessing/stemmers/nltk.py
@@ -19,12 +19,15 @@ from . import interfaces as stemmer_interfaces
 @interface.implementer(stemmer_interfaces.IStemmer)
 class _PorterStemmer(object):
 
-    __slots__ = ('stemmer',)
+	__slots__ = ()
 
-    def __init__(self):
-        self.stemmer = nltk.PorterStemmer()
+	def __init__(self):
+		pass
 
-    def stem(self, token):
-        token = unicode(token)
-        result = self.stemmer.stem(token)
-        return result if result else token
+	def stem(self, token):
+		token = unicode(token)
+		# The underlying stemmer object is NOT thread safe,
+		# it must not be used concurrently
+		stemmer = nltk.PorterStemmer()
+		result = stemmer.stem(token)
+		return result if result else token
