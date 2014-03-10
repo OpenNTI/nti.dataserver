660296c367280581041ff605bf1329a81c75c8f3
Jason Madden <jason.madden@nextthought.com>
2014-03-10 17:19:15 +0000

    Fix another place we were leaking a filehandle. Simplify the logic by only opening the main PDF once.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@34512 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/resources/converter_svg.py
+++ src/nti/contentrendering/resources/converter_svg.py
@@ -11,6 +11,7 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 import os
+import platform
 import math
 import subprocess
 
@@ -28,9 +29,8 @@ def _do_convert(page, input_filename='images.pdf'):
 
 	FIXME: We assume the existence of ``images.pdf`` in the current directory.
 
-	:param int page: The page of the document to convert, in the current directory.
-	:return: A tuple giving the relative path of the SVG filename, plus the width and
-		height in points, or None of the conversion failed.
+	:param int page: The page of the document to convert, in the current directory. 1-based, not 0-based.
+	:return: A string giving the relative path of the SVG filename,  or None of the conversion failed.
 	"""
 	# convert to svg
 	# must be toplevel function so it can be pickled
@@ -45,20 +45,19 @@ def _do_convert(page, input_filename='images.pdf'):
 			logger.warn( 'Failed to convert %s', filename )
 			return None
 
-	#Then remove the width and height boxes since this interacts badly with
-	#HTML embedding
-	exitcode = os.system( "sed -i -e 's/width=.*pt\"//' %s" % filename )
-	if exitcode:
-		logger.warn('Failed to convert %s', filename)
-		return None
+	# Then remove the width and height boxes since this interacts badly with
+	# HTML embedding.
+	# NOTE: The behaviour of -i is different between GNU sed and BSD sed;
+	# On OS X we assume BSD sed, otherwise we assume GNU sed
+	pattern = 's/width=.*pt\"//'
 
-	# Get the width and height from the media box since we're not cropping it
-	# in Python code
-	with open(input_filename, 'rb') as f:
-		mediaBox = PdfFileReader(f).getPage(page).mediaBox
-		width_in_pt, height_in_pt = mediaBox.getWidth(), mediaBox.getHeight()
+	if platform.system() == 'Darwin':
+		sed_args = ('sed', '-i', '', '-e', pattern, filename)
+	else:
+		sed_args = ('sed', '-i', '-e', pattern, filename)
+	subprocess.check_call(sed_args)
 
-	return (filename, width_in_pt, height_in_pt)
+	return filename
 
 class PDF2SVG(plasTeX.Imagers.VectorImager):
 	"""
@@ -72,34 +71,38 @@ class PDF2SVG(plasTeX.Imagers.VectorImager):
 	compiler = 'pdflatex'
 
 	def executeConverter(self, output):
-		open('images.pdf', 'w').write(output.read())
+		with open('images.pdf', 'wb') as f:
+			f.write(output.read())
 		# Crop all the pages of the PDF to the exact size
 		# os.system( "pdfcrop --hires --margin 0 images.pdf images.pdf" )
 		with open('/dev/null', 'w') as dev_null:
 			subprocess.check_call(
 					 ('pdfcrop', '--hires', '--margin', '0', 'images.pdf', 'images.pdf'),
 					  stdout=dev_null, stderr=dev_null)
-		# We must mark these as cropped
-		for img in self.images.values():
-			img._cropped = True
+		_images = list(self.images.values())
 
-		# Find out how many pages to expect
-		# TODO: Use of shell is deprecated.
-		cmd = "pdfinfo images.pdf | grep Pages | awk '{print $2}'"
-		result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()
-		logger.info("pdf info max pages output %s", result)
-		maxpages = int(result[0].strip())
+		# Find out how many pages to expect and record file sizes
+		with open('images.pdf', 'rb') as f:
+			pdf = PdfFileReader(f)
+			maxpages = pdf.getNumPages()
+
+			for i in range(maxpages):
+				mediaBox = pdf.getPage(i).mediaBox
+				width_in_pt, height_in_pt = mediaBox.getWidth(), mediaBox.getHeight()
+
+				# We must mark these as cropped and give them a size
+				# Note that self.images is an ordereddict, so they match up by index
+				image = _images[i]
+				image._cropped = True
+				image.width = math.ceil(width_in_pt) * 1.3
+				image.height = math.ceil(height_in_pt) * 1.3
+				# FIXME: The depth (height above baseline) is not correct
+				image.depth = -3
 
 		filenames = []
 		with ProcessPoolExecutor(max_workers=16) as executor:
-			_images = self.images.values()
-			for the_tuple in zip(executor.map(_do_convert, xrange(1, maxpages + 1)), _images):
-				filenames.append(the_tuple[0][0])
-
-				the_tuple[1].width = math.ceil(float(the_tuple[0][1])) * 1.3
-				the_tuple[1].height = math.ceil(float(the_tuple[0][2])) * 1.3
-				# FIXME: The depth (height above baseline) is not correct
-				the_tuple[1].depth = -3
+			for filename in executor.map(_do_convert, xrange(1, maxpages + 1)):
+				filenames.append(filename)
 
 		return 0, filenames
 
