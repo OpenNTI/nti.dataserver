46761bade982ee0fc48188c22865d638b3852c4b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-12 02:49:11 +0000

    add 'division' and clean up a bit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@34689 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/dashboard/decorators.py
+++ src/nti/appserver/dashboard/decorators.py
@@ -5,7 +5,7 @@ External decorators to provide access to the things exposed through this package
 
 $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -23,9 +23,9 @@ from nti.externalization import interfaces as ext_interfaces
 from nti.utils._compat import aq_base
 
 from .views import TOP_TOPICS_VIEW
+
 # These imports are broken out explicitly for speed (avoid runtime attribute lookup)
 LINKS = ext_interfaces.StandardExternalFields.LINKS
-
 
 @interface.implementer(ext_interfaces.IExternalMappingDecorator)
 class ForumObjectTopTopicsLinkProvider(AbstractAuthenticatedRequestAwareDecorator):
@@ -45,5 +45,7 @@ class ForumObjectTopTopicsLinkProvider(AbstractAuthenticatedRequestAwareDecorato
 
 	def _do_decorate_external(self, context, mapping):
 		context = aq_base(context)
-		elements = (TOP_TOPICS_VIEW, md5_etag(context.lastModified, self.authenticated_userid).replace('/', '_'))
+		userid = self.authenticated_userid
+		elements = (TOP_TOPICS_VIEW, md5_etag(context.lastModified, userid).replace('/', '_'))
 		self.add_link(TOP_TOPICS_VIEW, context, mapping, self.request, elements)
+
