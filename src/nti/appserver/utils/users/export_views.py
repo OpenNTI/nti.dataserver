08af9784b4ee03647094fcb74c2f01491f2fcd36
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-21 16:54:44 +0000

    fix and test export object user views. Remove view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@35311 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/users/export_views.py
+++ src/nti/appserver/utils/users/export_views.py
@@ -8,32 +8,25 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import six
 import gzip
 from io import BytesIO
 from cStringIO import StringIO
-from collections import defaultdict
 
 from pyramid.view import view_config
-from pyramid import httpexceptions as hexc
 
-import ZODB
+import zope.intid
 from zope import component
-from zope.generations.utility import findObjectsMatching
 
-from nti.appserver.utils import is_true, _JsonBodyView
-
-from nti.chatserver import interfaces as chat_interfaces
+from ZODB.interfaces import IBroken
+from ZODB.POSException import POSKeyError
 
 from nti.contentlibrary import interfaces as lib_interfaces
 
 from nti.dataserver import users
 from nti.dataserver import authorization as nauth
 from nti.dataserver import interfaces as nti_interfaces
-from nti.dataserver.contenttypes.forums import interfaces as for_interfaces
 from nti.dataserver.chat_transcripts import _DocidMeetingTranscriptStorage as DMTS
 
-from nti.externalization.oids import to_external_ntiid_oid
 from nti.externalization.externalization import toExternalObject
 from nti.externalization.datastructures import LocatedExternalDict
 from nti.externalization.externalization import to_json_representation_externalized
@@ -51,12 +44,6 @@ def _write_generator(generator, stream=None, seek0=True, separator="\n"):
 	return stream
 
 _transcript_mime_type = u'application/vnd.nextthought.transcript'
-_forum_comment_mime_type = u'application/vnd.nextthought.forums.generalforumcomment'
-_headline_community_topic_mime_type = u'application/vnd.nextthought.forums.communityheadlinetopic'
-
-def _get_mime_type(x):
-	mt = getattr(x, "mimeType", getattr(x, 'mime_type', None))
-	return mt
 
 def _parse_mime_types(value):
 	mime_types = set(value.split(','))
@@ -67,49 +54,47 @@ def _parse_mime_types(value):
 		mime_types.discard(u'')
 	return mime_types
 
-def _get_user_objects(user, mime_types=(), broken=False):
+def _make_min_max_btree_range(search_term):
+	min_inclusive = search_term  # start here
+	max_exclusive = search_term[0:-1] + unichr(ord(search_term[-1]) + 1)
+	return min_inclusive, max_exclusive
 
-	mime_types = set(map(lambda x: x.lower(), mime_types or ()))
-	def condition(x):
-		return 	isinstance(x, DMTS) or \
-				(ZODB.interfaces.IBroken.providedBy(x) and broken) or \
-				nti_interfaces.ITitledDescribedContent.providedBy(x) or \
-				(nti_interfaces.IModeledContent.providedBy(x) and not chat_interfaces.IMessageInfo.providedBy(x))
+def username_search(search_term):
+	min_inclusive, max_exclusive = _make_min_max_btree_range(search_term)
+	dataserver = component.getUtility(nti_interfaces.IDataserver)
+	_users = nti_interfaces.IShardLayout(dataserver).users_folder
+	usernames = list(_users.iterkeys(min_inclusive, max_exclusive, excludemax=True))
+	return usernames
 
-	seen = set()
-	for obj in findObjectsMatching(user, condition):
-		if ZODB.interfaces.IBroken.providedBy(obj):
-			yield obj, obj
-		else:
-			mime_type = _get_mime_type(obj)
+def all_objects_iids(users=(), mime_types=()):
+	intids = component.getUtility(zope.intid.IIntIds)
+	usernames = {getattr(user, 'username', user).lower() for user in users or ()}
+	for uid, obj in intids.items():
+		try:
+			if IBroken.providedBy(obj):
+				continue
+
+			creator = getattr(obj, 'creator', None)
+			creator = getattr(creator, 'username', creator)
+			if not creator:
+				continue
+
+			if usernames and creator.lower() not in usernames:
+				continue
+
+			mime_type = getattr(obj, "mimeType", getattr(obj, 'mime_type', None))
 			if mime_types and mime_type not in mime_types:
 				continue
 
-			oid = to_external_ntiid_oid(obj)
-			if oid in seen:
-				continue
-			seen.add(oid)
-
 			if isinstance(obj, DMTS):
 				if not mime_types or _transcript_mime_type in mime_types:
-					adapted = component.getAdapter(obj, nti_interfaces.ITranscript)
-					yield adapted, obj
-			else:
-				yield obj, obj
+					obj = component.getAdapter(obj, nti_interfaces.ITranscript)
+				else:
+					continue
 
-	if not mime_types or _forum_comment_mime_type in mime_types or _headline_community_topic_mime_type in mime_types:
-		for community in getattr(user, 'dynamic_memberships', ()):
-			if not nti_interfaces.ICommunity.providedBy(community):
-				continue
-			board = for_interfaces.IBoard(community, {})
-			for forum in board.values():
-				for topic in forum.values():
-					if getattr(topic, 'creator') == user:
-						yield topic, topic
-
-					for comment in topic.values():
-						if getattr(comment, 'creator') == user:
-							yield comment, comment
+			yield uid, obj
+		except (TypeError, POSKeyError) as e:
+			logger.error("Error processing object %s(%s); %s", type(obj), uid, e)
 
 @view_config(route_name='objects.generic.traversal',
 			 name='export_user_objects',
@@ -117,12 +102,16 @@ def _get_user_objects(user, mime_types=(), broken=False):
 			 permission=nauth.ACT_MODERATE)
 def user_export_objects(request):
 	values = request.params
-	username = values.get('username') or request.authenticated_userid
-	user = users.Entity.get_entity(username)
-	if not user:
-		raise hexc.HTTPNotFound(detail='User %s not found' % username)
+	term = values.get('term', values.get('search', None))
+	usernames = values.get('usernames', values.get('username', None))
+	if term:
+		usernames = username_search(term)
+	elif usernames:
+		usernames = usernames.split(",")
+	else:
+		usernames = ()
 
-	mime_types = values.get('mime_types', u'')
+	mime_types = values.get('mime_types', values.get('mimeTypes', u''))
 	mime_types = _parse_mime_types(mime_types)
 
 	stream = BytesIO()
@@ -131,12 +120,13 @@ def user_export_objects(request):
 	response.content_encoding = b'gzip'
 	response.content_type = b'application/json; charset=UTF-8'
 	response.content_disposition = b'attachment; filename="objects.txt.gz"'
-	def _generator():
-		for obj, _ in _get_user_objects(user, mime_types):
-			external = to_json_representation_externalized(obj)
-			yield external
 
-	_write_generator(_generator, gzstream, seek0=False)
+	for _, obj in all_objects_iids(usernames, mime_types):
+		external = to_json_representation_externalized(obj)
+		gzstream.write(external)
+		gzstream.write(b"\n")
+
+	gzstream.flush()
 	gzstream.close()
 	stream.seek(0)
 	response.body_file = stream
@@ -148,76 +138,51 @@ def user_export_objects(request):
 			 request_method='GET',
 			 permission=nauth.ACT_MODERATE)
 def object_resolver(request):
+	keys = set()
 	values = request.params
-	keys = values.get('id', values.get('key', values.get('ids', values.get('keys'))))
-	if isinstance(keys, six.string_types):
-		keys = keys.split()
+	for name in ('id', 'key', 'oid', 'ntiid'):
+		oids = values.get(name, None) or u''
+		keys.update(oids.split())
+		oids = values.get(name + 's', None) or u''
+		keys.update(oids.split())
 
 	result = LocatedExternalDict()
 	items = result['Items'] = []
-	for key in set(keys):
+	unresolved = result['Unresolved'] = []
+	for key in keys:
 		obj = ntiids.find_object_with_ntiid(key)
 		if obj is not None:
 			items.append(obj)
+		else:
+			unresolved.append(key)
 	return result
 
 @view_config(route_name='objects.generic.traversal',
-			 name='delete_user_objects',
+			 name='export_users',
 			 renderer='rest',
-			 request_method='POST',
+			 request_method='GET',
 			 permission=nauth.ACT_MODERATE)
-class DeleteObjectObjects(_JsonBodyView):
+def export_users(request):
+	values = request.params
+	term = values.get('term', values.get('search', None))
+	usernames = values.get('usernames', values.get('username', None))
+	if term:
+		usernames = username_search(term)
+	elif usernames:
+		usernames = usernames.split(',')
+	else:
+		dataserver = component.getUtility(nti_interfaces.IDataserver)
+		_users = nti_interfaces.IShardLayout(dataserver).users_folder
+		usernames = _users.keys()
 
-	def __call__(self):
-		values = self.readInput()
-		username = values.get('username') or self.request.authenticated_userid
+	result = LocatedExternalDict()
+	items = result['Items'] = {}
+	for username in usernames:
 		user = users.User.get_user(username)
-		if not user:
-			raise hexc.HTTPNotFound(detail='User not found')
-
-		broken = is_true(values.get('broken', 'F'))
-		mime_types = _parse_mime_types(values.get('mime_types', u''))
-
-		broken_objects = set()
-		counter_map = defaultdict(int)
-		for _, obj in list(_get_user_objects(user, mime_types, broken)):
-			if ZODB.interfaces.IBroken.providedBy(obj):
-				oid = getattr(obj, 'oid', None)
-				pid = getattr(obj, '_p_oid', None)
-				if pid:
-					broken_objects.add(pid)
-				if oid:
-					broken_objects.add(oid)
-			else:
-				mime_type = _get_mime_type(obj)
-				with user.updates():
-					objId = obj.id
-					containerId = obj.containerId
-					obj = user.getContainedObject(containerId, objId)
-					if obj is not None and user.deleteContainedObject(containerId, objId):
-						counter_map[mime_type] = counter_map[mime_type] + 1
-
-
-		if broken_objects:
-			for container in list(user.containers.values()):
-				for _, obj in list(container.items()):
-
-					broken = getattr(obj, 'oid', None) in broken_objects or \
-					  		 getattr(obj, '_p_oid', None) in broken_objects
-
-					if not broken:
-						strong = obj if not callable(obj) else obj()
-						broken = strong is not None and \
-								 getattr(strong, 'oid', None) in broken_objects and \
-								 getattr(strong, '_p_oid', None) in broken_objects
-						if broken:
-							obj = strong
-
-					if broken:
-						counter_map['broken'] = counter_map['broken'] + 1
-						user.containers._v_removeFromContainer(container, obj)
-
-		return counter_map
+		if user and nti_interfaces.IUser.providedBy(user):
+			items[user.username] = toExternalObject(user, name='summary')
+	result['Total'] = len(items)
+	return result
 
 exclude_containers = (u'Devices', u'FriendsLists', u'', u'Blog')
 
@@ -227,17 +192,13 @@ def get_collection_root(ntiid, library=None, registry=component):
 	return paths[0] if paths else None
 
 def _check_users_containers(usernames=()):
-	if usernames:
-		_users = {users.User.get_entity(x) for x in usernames}
-		_users.discard(None)
-	else:
-		dataserver = component.getUtility(nti_interfaces.IDataserver)
-		_users = nti_interfaces.IShardLayout(dataserver).users_folder
-		_users = _users.values()
+	for name in usernames:
+		user = users.User.get_entity(name)
+		if not user:
+			continue
 
-	for user in _users:
-		method = getattr(user, 'getAllContainers', lambda : ())
 		usermap = {}
+		method = getattr(user, 'getAllContainers', lambda : ())
 		for name in method():
 			if name in exclude_containers:
 				continue
@@ -245,6 +206,7 @@ def _check_users_containers(usernames=()):
 			if get_collection_root(name) is None:
 				container = user.getContainer(name)
 				usermap[name] = len(container) if container is not None else 0
+
 		if usermap:
 			yield user.username, usermap
 
@@ -255,38 +217,19 @@ def _check_users_containers(usernames=()):
 			 permission=nauth.ACT_MODERATE)
 def user_ghost_containers(request):
 	values = request.params
-	usernames = values.get('usernames')
-	usernames = set(usernames.split(',')) if usernames else ()
+	term = values.get('term', values.get('search', None))
+	usernames = values.get('usernames', values.get('username', None))
+	if term:
+		usernames = username_search(term)
+	elif usernames:
+		usernames = usernames.split(',')
+	else:
+		dataserver = component.getUtility(nti_interfaces.IDataserver)
+		_users = nti_interfaces.IShardLayout(dataserver).users_folder
+		usernames = _users.keys()
 
 	result = LocatedExternalDict()
 	items = result['Items'] = {}
 	for username, rmap in _check_users_containers(usernames):
 		items[username] = rmap
-	return result
-
-@view_config(route_name='objects.generic.traversal',
-			 name='export_users',
-			 renderer='rest',
-			 request_method='GET',
-			 permission=nauth.ACT_MODERATE)
-def export_users(request):
-	values = request.params
-	usernames = values.get('usernames')
-	if usernames:
-		if isinstance(usernames, six.string_types):
-			usernames = usernames.split(',')
-		_users = {users.User.get_user(x) for x in usernames}
-		_users.discard(None)
-	else:
-		dataserver = component.getUtility(nti_interfaces.IDataserver)
-		_users = nti_interfaces.IShardLayout(dataserver).users_folder
-		_users = _users.values()
-
-	result = LocatedExternalDict()
-	items = result['Items'] = {}
-	for user in _users:
-		if not nti_interfaces.IUser.providedBy(user):
-			continue
-		items[user.username] = toExternalObject(user, name='summary')
-	result['Total'] = len(items)
 	return result
--- src/nti/appserver/utils/users/tests/test_export_views.py
+++ src/nti/appserver/utils/users/tests/test_export_views.py
@@ -1,119 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-
-from hamcrest import assert_that
 from hamcrest import is_
-from hamcrest import has_length
 from hamcrest import has_entry
+from hamcrest import has_length
+from hamcrest import assert_that
 from hamcrest import greater_than
 
 import simplejson
-
 from io import BytesIO
-from zope import interface
 
 from nti.dataserver.contenttypes import Note
-from nti.dataserver import interfaces as nti_interfaces
 
 from nti.externalization import externalization
 
 from nti.dataserver.tests import mock_dataserver
 
-
-from nti.app.testing.application_webtest import ApplicationLayerTest
-from nti.app.testing.decorators import WithSharedApplicationMockDS
 from nti.app.testing.webtest import TestApp
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+from nti.app.testing.application_webtest import ApplicationLayerTest
 
 class TestApplicationUserExporViews(ApplicationLayerTest):
 
 	@WithSharedApplicationMockDS
-	def test_user_info_extract(self):
-		with mock_dataserver.mock_db_trans(self.ds):
-			self._create_user(external_value={u'email':u"nti@nt.com",
-											  u'realname':u'steve johnson',
-											  u'alias':u'citadel'})
-			self._create_user(username='rukia@nt.com',
-							  external_value={u'email':u'rukia@nt.com',
-											  u'realname':u'rukia foo',
-											  u'alias':u'sode no shirayuki'})
-			self._create_user(username='ichigo@nt.com',
-							  external_value={u'email':u'ichigo@nt.com',
-											  u'realname':u'ichigo bar',
-											  u'alias':u'zangetsu'})
-		testapp = TestApp(self.app)
-
-		path = '/dataserver2/@@user_info_extract'
-		environ = self._make_extra_environ()
-		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
-
-		res = testapp.get(path, extra_environ=environ)
-		assert_that(res.status_int, is_(200))
-		app_iter = res.app_iter[0].split('\n')[:-1]
-		assert_that(app_iter, has_length(4))
-		for t in app_iter:
-			assert_that(t.split(','), has_length(4))
-
-	@WithSharedApplicationMockDS
-	def test_opt_in_comm(self):
-		with mock_dataserver.mock_db_trans(self.ds):
-			u = self._create_user(external_value={u'email':u"nti@nt.com", u'opt_in_email_communication':True})
-			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithAgreementUpgraded)
-
-			u = self._create_user(username='rukia@nt.com',
-								  external_value={u'email':u'rukia@nt.com', u'opt_in_email_communication':True})
-			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithAgreementUpgraded)
-
-			u = self._create_user(username='ichigo@nt.com',
-								  external_value={u'email':u'ichigo@nt.com', u'opt_in_email_communication':True})
-			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithAgreementUpgraded)
-
-		testapp = TestApp(self.app)
-
-		path = '/dataserver2/@@user_opt_in_comm'
-		environ = self._make_extra_environ()
-		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
-
-		res = testapp.get(path, extra_environ=environ)
-		assert_that(res.status_int, is_(200))
-		app_iter = res.app_iter[0].split('\n')[:-1]
-		assert_that(app_iter, has_length(4))
-		for idx, t in enumerate(app_iter):
-			split = t.split(',')
-			assert_that(split, has_length(6))
-			if idx > 0:
-				assert_that(split[-1], is_('True'))
-
-	@WithSharedApplicationMockDS
-	def test_profile_info(self):
-		with mock_dataserver.mock_db_trans(self.ds):
-			u = self._create_user(external_value={u'email':u"nti@nt.com", u'opt_in_email_communication':True})
-			interface.alsoProvides(u, nti_interfaces.ICoppaUserWithAgreementUpgraded)
-
-			u = self._create_user(username='ichigo@nt.com',
-								  external_value={u'email':u'ichigo@nt.com', u'opt_in_email_communication':True})
-
-		testapp = TestApp(self.app)
-
-		path = '/dataserver2/@@user_profile_info'
-		environ = self._make_extra_environ()
-		environ[b'HTTP_ORIGIN'] = b'http://mathcounts.nextthought.com'
-
-		res = testapp.get(path, extra_environ=environ)
-		assert_that(res.status_int, is_(200))
-		app_iter = res.app_iter[0].split('\n')[:-1]
-		assert_that(app_iter, has_length(3))
-
-	@WithSharedApplicationMockDS
 	def test_export_user_objects(self):
 		with mock_dataserver.mock_db_trans(self.ds):
-			user = self._create_user(external_value={u'email':u"nti@nt.com", u'opt_in_email_communication':True})
+			user = self._create_user(external_value={u'email':u"nti@nt.com",
+													 u'opt_in_email_communication':True})
 			note = Note()
 			note.body = [u'bankai']
 			note.creator = user
@@ -123,36 +42,19 @@ class TestApplicationUserExporViews(ApplicationLayerTest):
 		testapp = TestApp(self.app)
 		environ = self._make_extra_environ()
 		path = '/dataserver2/@@export_user_objects'
-		res = testapp.get(path, extra_environ=environ)
+		params = {"usernames":"sjohnson@nextthought.com", "mimeTypes":'application/vnd.nextthought.note'}
+		res = testapp.get(path, params=params, extra_environ=environ)
 		assert_that(res.status_int, is_(200))
 		assert_that(res.headers, has_entry('Content-Length', greater_than(500)))
 		stream = BytesIO(res.body)
 		stream.seek(0)
-		d = simplejson.load(stream)
-		assert_that(d, has_entry(u'body', [u'bankai']))
-
-	@WithSharedApplicationMockDS
-	def test_delete_user_objects(self):
-		with mock_dataserver.mock_db_trans(self.ds):
-			user = self._create_user(external_value={u'email':u"nti@nt.com", u'opt_in_email_communication':True})
-			note = Note()
-			note.body = [u'bankai']
-			note.creator = user
-			note.containerId = u'mycontainer'
-			note = user.addContainedObject(note)
-
-		testapp = TestApp(self.app)
-		environ = self._make_extra_environ()
-		path = '/dataserver2/@@delete_user_objects'
-		res = testapp.post(path, extra_environ=environ)
-		assert_that(res.status_int, is_(200))
-		d = simplejson.loads(res.body)
-		assert_that(d, has_entry(u"application/vnd.nextthought.note", 1))
+		assert_that(stream.readlines(), has_length(1))
 
 	@WithSharedApplicationMockDS
 	def test_ghost_containers(self):
 		with mock_dataserver.mock_db_trans(self.ds):
-			user = self._create_user(external_value={u'email':u"nti@nt.com", u'opt_in_email_communication':True})
+			user = self._create_user(external_value={u'email':u"nti@nt.com",
+													 u'opt_in_email_communication':True})
 			username = user.username
 			note = Note()
 			note.body = [u'bankai']
@@ -210,7 +112,8 @@ class TestApplicationUserExporViews(ApplicationLayerTest):
 	@WithSharedApplicationMockDS
 	def test_object_resolver(self):
 		with mock_dataserver.mock_db_trans(self.ds):
-			user = self._create_user(external_value={u'email':u"nti@nt.com", u'opt_in_email_communication':True})
+			user = self._create_user(external_value={u'email':u"nti@nt.com",
+													 u'opt_in_email_communication':True})
 			note = Note()
 			note.body = [u'bankai']
 			note.creator = user
