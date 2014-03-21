f749d93710ece9fb99ab2fe60891587b2c929435
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-21 03:32:48 +0000

    remove deprecations
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@35278 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/forums/traversal.py
+++ src/nti/appserver/forums/traversal.py
@@ -5,7 +5,7 @@ Related to traversing into forum objects.
 
 $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
