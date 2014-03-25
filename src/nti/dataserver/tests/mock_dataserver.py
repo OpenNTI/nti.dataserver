6721dc71b155f0d6ca9ba7f0a3e9a739caf9857a
Jason Madden <jason.madden@nextthought.com>
2014-03-25 15:08:48 +0000

    Markup the new user email with the attributes required for i18n as an example and update the catalogs. If we need to do this we'll also need to come up with a solution for the plain-text content.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@35488 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/tests/mock_dataserver.py
+++ src/nti/dataserver/tests/mock_dataserver.py
@@ -1,5 +1,8 @@
 from __future__ import print_function, unicode_literals, absolute_import
 
+logger = __import__('logging').getLogger(__name__)
+
+
 import ZODB
 
 from ZODB.DemoStorage import DemoStorage
