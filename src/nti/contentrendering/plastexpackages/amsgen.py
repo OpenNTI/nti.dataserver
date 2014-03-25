204af59a9c6f27ba438beff694e9e4e6af43dc2a
Sean Jones <sean.jones@nextthought.com>
2014-03-25 17:37:58 +0000

    Initial commit of the amsgen package. The package is blank to prevent plasTeX from reading amsgen.sty. This package is an amsmath internal package so the commands that we should support is open to discussion.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@35520 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/amsgen.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+This is a blank placeholder to prevent amsgen.sty from being read by plasTeX. 
+Need to determine if it makes sense to define any of the commands since they are 
+supposed to be internal to the amsmath package.
+
+$Id:$
+"""
+
