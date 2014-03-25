53b60856e05f4ecd668df8eb6b1c841b2de58ae2
Sean Jones <sean.jones@nextthought.com>
2014-03-25 17:40:14 +0000

    Initial commit of the xspace package. This package does some complicated whitespace manipulation that we will need to examine if it makes sense to duplicate that behavior for the web.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@35522 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/xspace.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Partial support for the xspace package. This is just enough 
+to support parsing, however this command does semi-complicated 
+whitespace manipulation that will require careful work on the render templates 
+to do correctly.
+
+$Id:$
+"""
+
+from __future__ import print_function, unicode_literals
+
+from plasTeX import Command
+
+class xspace(Command):
+	pass
+
