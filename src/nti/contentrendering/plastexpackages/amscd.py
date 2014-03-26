f8fc8bd0f18fec8be647fbfca6b981ff8c0e4427
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-26 04:04:10 +0000

    set properties
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@35607 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/amscd.py
+++ src/nti/contentrendering/plastexpackages/amscd.py
@@ -4,7 +4,7 @@ Partial support for the amscd package. This allows for parsing the environment
 and assumes that the actual processing will be handed off to either pdfLaTeX 
 or MathJax.
 
-$Id:$
+$Id$
 """
 
 from plasTeX.Base.LaTeX.Arrays import Array
