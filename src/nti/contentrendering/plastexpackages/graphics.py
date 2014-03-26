27426d93883d343e49409b1666811035aba49084
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-26 04:16:56 +0000

    defer imports
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@35608 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/graphics.py
+++ src/nti/contentrendering/plastexpackages/graphics.py
@@ -3,19 +3,25 @@
 """
 $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
 
 from plasTeX import Command
 
-# for export
-from plasTeX.Packages.graphics import graphicspath
-from plasTeX.Packages.graphics import includegraphics
-from plasTeX.Packages.graphics import _locate_image_file
-from plasTeX.Packages.graphics import DeclareGraphicsExtensions
-
-# SAJ: Adds a stub version of the \DeclareGraphicsRule command
-
+# Adds a stub version of the \DeclareGraphicsRule command
 class DeclareGraphicsRule(Command):
 	packageName = 'graphics'
 	args = '{extension:str}{type:str}{readfile:str}{command:str}'
+
+import zope.deferredimport
+zope.deferredimport.initialize()
+
+zope.deferredimport.deprecatedFrom(
+    "Moved to plasTeX.Packages.graphics",
+    "plasTeX.Packages.graphics",
+    "graphicspath",
+    "includegraphics",
+    "DeclareGraphicsExtensions",
+    "_locate_image_file")
