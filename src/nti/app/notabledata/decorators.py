e8504021e2f5756a5010c7c1354491591e99846d
Jason Madden <jason.madden@nextthought.com>
2014-03-27 18:55:32 +0000

    For things that go across the socket and are notable to the destination user, give them a field that matches the URL, set to true. This is also true of items in the stream URL. I think this turns out to be relatively inexpensive, at least in my testing.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@35765 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/notabledata/decorators.py
+++ src/nti/app/notabledata/decorators.py
@@ -11,4 +11,41 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-# TODO: Add a decorator for stream change events to say when they are notable
+from zope import component
+from zope import interface
+
+from nti.dataserver.interfaces import IStreamChangeEvent
+from pyramid.interfaces import IRequest
+from nti.externalization.interfaces import IExternalObjectDecorator
+from .interfaces import IUserNotableData
+
+from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
+
+@interface.implementer(IExternalObjectDecorator) # Because the stream externalizer doesn't call Mapping Decorator
+@component.adapter(IStreamChangeEvent,IRequest)
+class _StreamChangeNotableDecorator(AbstractAuthenticatedRequestAwareDecorator):
+	# We expect to be used at externalization time, after everything has
+	# calmed down so its safe to cache intids. however, we do need
+	# to watch out for the authenticated userid from changing due to
+	# delegation (when we send on a socket, for example)
+
+	def _is_notable(self, context):
+		request = self.request
+		# This is really what's zope's request annotations are for
+		cache = getattr(request, '_StreamChangeNotableDecorator', None)
+		if cache is None:
+			cache = dict()
+			setattr(request, '_StreamChangeNotableDecorator', cache)
+		nd = cache.get(self.authenticated_userid)
+		if nd is None:
+			nd = component.getMultiAdapter( (self.remoteUser, request), IUserNotableData)
+			cache[self.authenticated_userid] = nd
+
+		return nd.is_object_notable(context) or nd.is_object_notable(context.object)
+
+
+	def _predicate(self, context, result):
+		return self._is_authenticated and self._is_notable(context)
+
+	def _do_decorate_external(self, context, result):
+		result['RUGDByOthersThatIMightBeInterestedIn'] = True
