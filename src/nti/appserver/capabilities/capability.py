ab5035a72cb6caade337a1278aaf75a624659536
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-04-01 15:44:01 +0000

    clean up unused, add 'division'
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@36163 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/capabilities/capability.py
+++ src/nti/appserver/capabilities/capability.py
@@ -3,10 +3,10 @@
 """
 Implementations of capability objects.
 
-$Id$
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -23,5 +23,5 @@ class Capability(permission.Permission):
 	Basic implementation of a :class:`.ICapability`.
 	"""
 
-	def __init__( self, cap_id, title='', description='' ):
+	def __init__(self, cap_id, title='', description=''):
 		super(Capability, self).__init__( cap_id, title=title, description=description )
--- src/nti/appserver/capabilities/vocabulary.py
+++ src/nti/appserver/capabilities/vocabulary.py
@@ -3,35 +3,36 @@
 """
 Zope vocabularies relating to capabilities.
 
-$Id$
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 from zope import interface
 
-from . import interfaces
-
 from zope.schema import interfaces as sch_interfaces
 from zope.componentvocabulary.vocabulary import UtilityNames
 from zope.componentvocabulary.vocabulary import UtilityVocabulary
 
+from . import interfaces
+
 # Make pylint not complain about "badly implemented container"
-#pylint: disable=R0924
+# pylint: disable=R0924
 
 @interface.provider(sch_interfaces.IVocabularyFactory)
-class CapabilityNameTokenVocabulary(object,UtilityNames):
+class CapabilityNameTokenVocabulary(object, UtilityNames):
+
 	# This one is 'live"
-	def __init__( self, context=None ):
+	def __init__(self, context=None):
 		# The context argument is ignored. It is to support the VocabularyFactory interface
-		UtilityNames.__init__( self, interfaces.ICapability )
+		UtilityNames.__init__(self, interfaces.ICapability)
 
 class CapabilityUtilityVocabulary(UtilityVocabulary):
-	interface = interfaces.ICapability
 	# This one enumerates at instance creation time
+	interface = interfaces.ICapability
 
 class CapabilityNameVocabulary(CapabilityUtilityVocabulary):
 	nameOnly = True
