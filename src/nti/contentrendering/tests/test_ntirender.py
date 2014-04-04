757e755fc513838d2865b401b2ddc123d74b0637
Jason Madden <jason.madden@nextthought.com>
2014-04-04 14:22:47 +0000

    Testing non-BMP unicode characters.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@36490 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/tests/test_ntirender.py
+++ src/nti/contentrendering/tests/test_ntirender.py
@@ -3,7 +3,7 @@
 """
 $Id$
 """
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, absolute_import, unicode_literals, division
 
 
 #disable: accessing protected members, too many methods
@@ -12,6 +12,8 @@ from __future__ import print_function, unicode_literals
 
 from hamcrest import assert_that
 from hamcrest import has_entry
+from hamcrest import has_length
+from hamcrest import contains_string
 
 import os.path
 
@@ -20,6 +22,10 @@ from . import ContentrenderingLayerTest
 from nti.contentrendering.tests import RenderContext
 
 from nti.contentrendering import nti_render
+from nti.contentrendering import transforms
+
+from nti.contentrendering.resources.ResourceDB import ResourceDB
+
 from zope.dublincore import xmlmetadata
 
 class TestNTIRender(ContentrenderingLayerTest):
@@ -40,3 +46,52 @@ class TestNTIRender(ContentrenderingLayerTest):
 
 			assert_that( metadata, has_entry( 'Creator', ('Jason','Steve') ) )
 			assert_that( metadata, has_entry( 'Title', ('Staggering Work',) ) )
+
+
+	def test_dont_destroy_non_bmp_chars(self):
+		# This is 'mathematical italic small tau', a character
+		# that lives outside the BMP. It requires four utf-8 bytes
+		# to encode, or two in utf-16 and cannot be encoded in ascii.
+		# if we are on a "narrow" python build, it will require two
+		# chars of storage, whereas a "wide" python build will use one char
+		# (Python 3 will always use one char)
+		tau = u"\U0001D70F"
+
+		# right now, we expect to be on narrow builds
+		assert_that( tau, has_length(2) )
+
+		body = "\\includegraphics{Glossary.png} This is some text $z^2$ \\[t^3\\]" + tau
+
+
+		text = simpleLatexDocumentText(bodies=(body,),
+									   preludes=(br'\usepackage{graphicx}',))
+
+		# Our default rendering process with default output encodings
+		with RenderContext(text, input_encoding='utf-8',
+						   files=(os.path.join(os.path.dirname(__file__), 'Glossary.png'),)) as ctx:
+			transforms.performTransforms(ctx.dom)
+
+			rdb = ResourceDB( ctx.dom )
+			rdb.overrides['$z^2$'] = ('png',)
+			rdb.overrides[r'\[t^3\]'] = ('svg',)
+
+			rdb.generateResourceSets()
+
+			nti_render.render(ctx.dom, 'XHTML', rdb)
+			fname = os.path.join(ctx.docdir, 'tag_nextthought_com_2011-10_testing-HTML-temp_0.html')
+			with open(fname, 'r') as f:
+				contents = f.read()
+
+			assert_that( contents,
+						 contains_string('This is some text') )
+			assert_that( contents, contains_string('&#120591'))
+
+			# Now the post processing
+			nti_render.postRender(ctx.dom)
+
+			with open(fname, 'r') as f:
+				contents = f.read()
+
+			assert_that( contents,
+						 contains_string('This is some text') )
+			assert_that( contents, contains_string('&#120591'))
