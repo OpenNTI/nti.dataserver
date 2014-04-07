12133f9c30120b00ea05d8c3fbd364ffabc492e1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-04-07 19:14:12 +0000

    no need to monkey patch. After 36623 the dbs can be gced
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@36629 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/__init__.py
+++ src/nti/contentsearch/__init__.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -
 """
 .. $Id$
 """
@@ -9,10 +10,6 @@ logger = __import__('logging').getLogger(__name__)
 
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
-
-# monkey patch
-from  . import monkey
-monkey.patch()
 
 import zope.deferredimport
 zope.deferredimport.initialize()
