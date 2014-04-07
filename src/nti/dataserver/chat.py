a106768f256c0976d9b52c5e81c294225f7212c8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-04-07 18:35:32 +0000

    clean up a bit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@36621 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/chat.py
+++ src/nti/dataserver/chat.py
@@ -3,29 +3,49 @@
 """
 Chatserver functionality. This is all deprecated; prefer the nti.chatserver package.
 
-$Id$
+.. $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import zope.deferredimport
+zope.deferredimport.initialize()
+
+zope.deferredimport.deprecatedFrom(
+    "Moved to nti.chatserver.interfaces",
+    "nti.chatserver.interfaces",
+    "CHANNELS",
+    "CHANNEL_POLL",
+    "CHANNEL_META",
+    "CHANNEL_DEFAULT",
+    "CHANNEL_WHISPER",
+    "CHANNEL_CONTENT")
+
+zope.deferredimport.deprecatedFrom(
+    "Moved to nti.chatserver._handler",
+    "nti.chatserver._handler",
+    "_ChatHandler",
+    "ChatHandlerFactory")
+
+zope.deferredimport.deprecatedFrom(
+    "Moved to nti.chatserver.messageinfo",
+    "nti.chatserver.messageinfo",
+    "MessageInfo")
+
+zope.deferredimport.deprecatedFrom(
+    "Moved to nti.chatserver.chatserver",
+    "nti.chatserver.chatserver",
+    "Chatserver")
+
 import zope.deprecation
 from zope.deprecation import deprecated
-
-from nti.chatserver.interfaces import CHANNELS
-from nti.chatserver.interfaces import CHANNEL_POLL
-from nti.chatserver.interfaces import CHANNEL_META
-from nti.chatserver.interfaces import CHANNEL_DEFAULT
-from nti.chatserver.interfaces import CHANNEL_WHISPER
-from nti.chatserver.interfaces import CHANNEL_CONTENT
 
 # Many of these class names need to stick around to avoid broken objects
 # in old datbases
 zope.deprecation.__show__.off()
 
-from nti.chatserver._handler import _ChatHandler
-from nti.chatserver.messageinfo import MessageInfo
 from nti.chatserver.meeting import _Meeting, _ModeratedMeeting
 
 _ChatRoom = _Meeting
@@ -33,9 +53,6 @@ deprecated('_ChatRoom', 'Prefer _Meeting' )
 
 _ModeratedChatRoom = _ModeratedMeeting
 deprecated('_ModeratedChatRoom', 'Prefer _ModeratedMeeting' )
-
-from nti.chatserver.chatserver import Chatserver
-from nti.chatserver._handler import ChatHandlerFactory
 
 deprecated('Chatserver', 'Prefer nti.chatserver')
 deprecated('MessageInfo', 'Prefer nti.chatserver')
