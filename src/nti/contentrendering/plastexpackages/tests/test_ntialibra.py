6b076b8285751bcf5464583d839fc9634d942b70
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-04-25 04:23:57 +0000

    initial nti-alibra bridge
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@37960 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/tests/test_ntialibra.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+import unittest
+
+from hamcrest import is_
+from hamcrest import has_length
+from hamcrest import assert_that
+
+# from nti.contentrendering.plastexpackages.graphicx import includegraphics
+from nti.contentrendering.plastexpackages.ntialibra import ntiheader, ntisequenceitem, ntisequence
+
+from nti.contentrendering.tests import simpleLatexDocumentText
+from nti.contentrendering.tests import buildDomFromString as _buildDomFromString
+
+def _simpleLatexDocument(maths):
+	return simpleLatexDocumentText(
+					preludes=(br'\usepackage{nti.contentrendering.plastexpackages.ntilatexmacros}',
+						 	  br'\usepackage{nti.contentrendering.plastexpackages.ntialibra}'),
+					bodies=maths)
+
+class TestNTIAlibra(unittest.TestCase):
+
+	def test_ntintisequence(self):
+		example = br"""
+			\begin{ntisequence}[creator=NTI]
+				\begin{ntiheader}
+					\includegraphics[width=106px,height=60px]{test}
+				\end{ntiheader}
+				\ntisequenceitem \includegraphics[width=106px,height=60px]{test2}
+				\ntisequenceitem \includegraphics[width=106px,height=60px]{test3}
+			\end{ntisequence}
+		"""
+		dom = _buildDomFromString( _simpleLatexDocument( (example,) ) )
+
+		# Check that the DOM has the expected structure
+		assert_that(dom.getElementsByTagName('ntisequence'), has_length(1))
+		assert_that(dom.getElementsByTagName('ntisequence')[0], is_(ntisequence))
+
+		# Check that the ntisequence object has the expected children
+		elem = dom.getElementsByTagName('ntisequence')[0]
+		assert_that(elem.childNodes[0], is_(ntiheader))
+		assert_that(elem.childNodes[2], is_(ntisequenceitem))
+		assert_that(elem.childNodes[3], is_(ntisequenceitem))
