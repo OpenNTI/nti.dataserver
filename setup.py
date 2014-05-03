067a43cdd05dc179d7de7360e7be33ee69f789f6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-03 21:42:52 +0000

    no need for zopyx.txng3.core
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@38425 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -406,9 +406,7 @@ setup(
 		# Plug to make zope.schema's vocabulary registry ZCA
 		# based and thus actually useful
 		'zope.vocabularyregistry >= 1.0.0',
-		# textindexng3
-		'zopyx.txng3.core >= 3.6.1.1' if not IS_PYPY else '',  # extensions don't build
-		'zopyx.txng3.ext >= 3.3.4' if not IS_PYPY else '',
+		'zopyx.txng3.ext >= 3.3.4' if not IS_PYPY else '',  # extensions don't build
 		# Data analysis
 		# pandas,
 		# scikit-learn,
