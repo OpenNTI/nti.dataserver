3e66f257bc8fae01bc36afb4255e609438ae2603
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-07 20:15:07 +0000

    helper script to join a dfl
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@38612 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_join_dfl.py
+++ src/nti/dataserver/utils/nti_join_dfl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Join community utility
+Join DFL utility
 
 .. $Id$
 """
@@ -15,28 +15,11 @@ logger = __import__('logging').getLogger(__name__)
 
 import os
 import sys
-import pprint
 import argparse
 
 from nti.dataserver import users
 from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver import interfaces as nti_interfaces
-from nti.externalization.externalization import to_external_object
-
-def join_communities(user, communities=(), follow=False, exitOnError=False):
-	not_found = set()
-	for com_name in communities:
-		comm = users.Entity.get_entity(com_name)
-		if not comm or not nti_interfaces.ICommunity.providedBy(comm):
-			not_found.add(com_name)
-			if exitOnError:
-				break
-		else:
-			user.record_dynamic_membership(comm)
-			if follow:
-				user.follow(comm)
-
-	return tuple(not_found)
 
 def _process_args(args):
 	user = users.User.get_user(args.username)
@@ -44,24 +27,21 @@ def _process_args(args):
 		print("No user found", args, file=sys.stderr)
 		sys.exit(2)
 
-	not_found = join_communities(user, args.communities, args.follow, True)
-	if not_found:
-		print("No community found", args, file=sys.stderr)
-		sys.exit(3)
+	dfl = users.Entity.get_entity(args.dfl)
+	if not dfl or not nti_interfaces.IDynamicSharingTargetFriendsList.providedBy(user):
+		print("No dfl found", args, file=sys.stderr)
+		sys.exit(2)
 
-	if args.verbose:
-		pprint.pprint(to_external_object(user))
+	if not dfl.addFriend(user):
+		print('User not added to DFL')
+		sys.exit(3)
 
 def main():
 	arg_parser = argparse.ArgumentParser( description="Join one or more existing communities" )
 	arg_parser.add_argument('username', help="The username that should join communities")
-	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
-	arg_parser.add_argument('-f', '--follow', help="Also follow the communities", action='store_true', dest='follow')
-	arg_parser.add_argument('-c', '--communities',
-							dest='communities',
-							nargs="+",
-							help="The usernames of the communities to join")
+	arg_parser.add_argument('dfl', help="The DFL identifier")
 	arg_parser.add_argument('--env_dir', help="Dataserver environment root directory")
+	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true', dest='verbose')
 	args = arg_parser.parse_args()
 	
 	env_dir = args.env_dir
@@ -70,7 +50,8 @@ def main():
 	if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
 		raise ValueError("Invalid dataserver environment root directory", env_dir)
 	
-	run_with_dataserver(environment_dir=env_dir, function=lambda: _process_args(args))
+	run_with_dataserver(environment_dir=env_dir, function=lambda: _process_args(args),
+						verbose=args.verbose)
 
 if __name__ == '__main__':
 	main()
