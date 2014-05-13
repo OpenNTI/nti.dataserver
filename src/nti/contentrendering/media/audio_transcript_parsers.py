ee5a2818ac3fd9daced94769445f79e5d027a289
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-13 19:07:14 +0000

    register audio transcript parsers
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@38928 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/media/audio_transcript_parsers.py
+++ src/nti/contentrendering/media/audio_transcript_parsers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-video transcript parsers.
+audio transcript parsers.
 
-.. $Id$
+.. $Id: video_transcript_parsers.py 38907 2014-05-13 16:48:46Z carlos.sanchez $
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
@@ -12,22 +12,22 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import interface
 
-from . import VideoTranscript
-from . import VideoTranscriptEntry
+from . import AudioTranscript
+from . import AudioTranscriptEntry
 from . import media_transcript_parsers
 from . import interfaces as media_interfaces
 
-@interface.implementer(media_interfaces.IVideoTranscriptParser)
+@interface.implementer(media_interfaces.IAudioTranscriptParser)
 class _SRTTranscriptParser(media_transcript_parsers._SRTTranscriptParser):
-	entry_cls = VideoTranscriptEntry
-	transcript_cls = VideoTranscript
+	entry_cls = AudioTranscriptEntry
+	transcript_cls = AudioTranscript
 
-@interface.implementer(media_interfaces.IVideoTranscriptParser)
+@interface.implementer(media_interfaces.IAudioTranscriptParser)
 class _SBVTranscriptParser(media_transcript_parsers._SBVTranscriptParser):
-	entry_cls = VideoTranscriptEntry
-	transcript_cls = VideoTranscript
+	entry_cls = AudioTranscriptEntry
+	transcript_cls = AudioTranscript
 
-@interface.implementer(media_interfaces.IVideoTranscriptParser)
+@interface.implementer(media_interfaces.IAudioTranscriptParser)
 class _WebVttTranscriptParser(media_transcript_parsers._WebVttTranscriptParser):
-	entry_cls = VideoTranscriptEntry
-	transcript_cls = VideoTranscript
+	entry_cls = AudioTranscriptEntry
+	transcript_cls = AudioTranscript
