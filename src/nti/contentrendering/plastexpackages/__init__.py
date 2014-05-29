96fd83bcab4966f476764ab387191f3f3f0f0cb5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-29 20:23:17 +0000

    remove
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@40071 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/__init__.py
+++ src/nti/contentrendering/plastexpackages/__init__.py
@@ -10,7 +10,4 @@ rather than \usepackage{nti.contentrendering.plastexpackages.foo}.
 
 .. $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
