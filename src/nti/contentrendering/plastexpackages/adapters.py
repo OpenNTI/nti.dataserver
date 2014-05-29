c4ea8c1964a7599474184d7d907d86c2ac478dda
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-29 20:14:37 +0000

    set props
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@40068 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/adapters.py
+++ src/nti/contentrendering/plastexpackages/adapters.py
@@ -2,7 +2,7 @@
 """
 adapters for externalizing plasTeX objects
 
-.. $Id: $
+.. $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
