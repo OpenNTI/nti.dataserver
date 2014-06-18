25f968b6690450cc20c83376ec8309a9a20107d8
Jason Madden <jason.madden@nextthought.com>
2014-06-18 12:53:13 +0000

    No, do not evolve, not ready yet.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@41628 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/install.py
+++ src/nti/dataserver/generations/install.py
@@ -9,7 +9,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 47
+generation = 46
 
 # Allow going forward/backward for testing
 import os
