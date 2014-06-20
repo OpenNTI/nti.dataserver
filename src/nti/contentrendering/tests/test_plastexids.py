901ff7e0b1f49d3bcd3e511480903c18044d3545
Jason Madden <jason.madden@nextthought.com>
2014-06-20 16:28:21 +0000

    cross-document referencing for common embedded objects like equations, figures and tables. note that the latter two MUST have captions.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@41841 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/tests/test_plastexids.py
+++ src/nti/contentrendering/tests/test_plastexids.py
@@ -14,6 +14,7 @@ import unittest
 
 from hamcrest import assert_that
 from hamcrest import is_
+from hamcrest import is_not as does_not
 from hamcrest import has_entry
 from hamcrest import has_property
 from hamcrest import contains_string
@@ -105,29 +106,57 @@ class TestPlastexIdsRender(ContentrenderingLayerTest):
 		\label{A}
 		Some text
 
+		\subsection{SubsectionA}
+		\label{SubsectionA}
+		This is in the same HTML page as the chapter.
+
 		\chapter{B}
 		\label{B}
 		Some other text \ref{A}.
 
 		\section{SectionB}
 		\label{SectionB}
-		Some text in subsection.
+		Some text in subsection that refs \ref{SubsectionA}.
+
+		\begin{equation}
+		\label{equation}
+			\forall x \in X, \quad \exists y \leq \epsilon
+		\end{equation}
+
+		We would like to ref the equation in \ref{equation}.
+
+		\begin{figure}
+		\caption{A figure}
+		\label{figure} % recall that captions are labeled
+		Stuff in figure
+		\end{figure}
+
+		We would like to ref the equation in \ref{figure}.
 		'''
 
 		ref_str = r'''
 		\chapter{One}
 		\label{One}
-		Back ref to \ntiidref{A}.
+		Back ref to chapter \ntiidref{A}.
+
 
 		\section{Two}
 		Section refs \ntiidref{SectionB}.
+
+		Section also refs \ntiidref{equation}.
+
+		Section also refs \ntiidref{figure}.
+
+		Back ref to subsection \ntiidref{SubsectionA}.
+
 		'''
 
 		source_a_ntiid = "tag:nextthought.com,2011-10:testing-HTML-temp.A"
 		source_b_ntiid = "tag:nextthought.com,2011-10:testing-HTML-temp.SectionB"
 
 
-		with RenderContext(simpleLatexDocumentText( preludes=(br'\usepackage{nti.contentrendering.plastexpackages.ntilatexmacros}',),
+		with RenderContext(simpleLatexDocumentText( preludes=(br'\usepackage{nti.contentrendering.plastexpackages.ntilatexmacros}',
+															  br'\usepackage{amsmath}'),
 													bodies=(source_str,) )) as source_context:
 
 			source_context.render()
@@ -136,6 +165,28 @@ class TestPlastexIdsRender(ContentrenderingLayerTest):
 			chapter_b = source_context.read_rendered_file('tag_nextthought_com_2011-10_testing-HTML-temp_B.html')
 			assert_that( chapter_b,
 						 contains_string('href="tag_nextthought_com_2011-10_testing-HTML-temp_A.html"'))
+
+			sec_b = source_context.read_rendered_file('tag_nextthought_com_2011-10_testing-HTML-temp_SectionB.html')
+			# check the back-ref to the subsection
+			assert_that( sec_b,
+						 contains_string('tag_nextthought_com_2011-10_testing-HTML-temp_A.html#SubsectionA'))
+
+			# check the original equation
+			assert_that( sec_b,
+						 contains_string('id="equation"'))
+			# check its reference.
+			assert_that( sec_b,
+						 contains_string('href="tag_nextthought_com_2011-10_testing-HTML-temp_SectionB.html#equation"'))
+
+			# The ntiid for the equation did not make it into the ToC
+			eqn_ntiid = source_context.dom.getElementsByTagName('equation')[0].ntiid
+			fig_ntiid = source_context.dom.getElementsByTagName('figure')[0].getElementsByTagName('caption')[0].ntiid
+			assert_that( eqn_ntiid, is_('tag:nextthought.com,2011-10:testing-HTMLequation-temp.equation.equation'))
+			assert_that( fig_ntiid, is_('tag:nextthought.com,2011-10:testing-HTMLfigure-temp.figure.figure'))
+			toc = source_context.read_rendered_file('eclipse-toc.xml')
+			for i in eqn_ntiid, fig_ntiid:
+				assert_that( toc,
+							 does_not( contains_string(i)))
 
 			# Capture the value of the paux file for use later, so
 			# we can let this render be completely cleaned up
@@ -159,5 +210,9 @@ class TestPlastexIdsRender(ContentrenderingLayerTest):
 
 			assert_that( chapter_one,
 						 contains_string('href="%s"' % source_a_ntiid))
-			assert_that( section_two,
-						 contains_string('href="%s"' % source_b_ntiid))
+
+			# Note that the embedded things are referenced
+			# by DOM ID relative to the containing ntiid
+			for ntiid in source_b_ntiid, source_b_ntiid + '#equation', source_b_ntiid + '#figure', source_a_ntiid + '#SubsectionA':
+				assert_that( section_two,
+							 contains_string('href="%s"' % ntiid))
