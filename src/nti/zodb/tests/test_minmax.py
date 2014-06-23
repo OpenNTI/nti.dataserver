32c55714366bdbd901885700db5ce58143381ada
Jason Madden <jason.madden@nextthought.com>
2014-06-23 13:42:39 +0000

    Fix an issue that manifested as a problem indexing negative timestamps (ValueError: long integer out of range). Following is a description of the issue and an explanation of why the change is safe.
    That's actually very interesting, because -1 produces a 64-bit long
    value as expected:
    
      >>> from nti.zodb.containers import time_to_64bit_int
      >>> x = time_to_64bit_int( -1 )
      >>> x
      13830554455654793216L
      >>> x.bit_length()
      64
      >>> type(x)
      <type 'long'>
    
    
    That should be able to go in a 64bit BTree, but as you noted, it fails:
    
      >>> from BTrees import family64
      >>> family64.IO.using64bits
      >>> bt = family64.IO.BTree()
      >>> bt[x] = 'str'
      Traceback (most recent call last):
        File "<console>", line 1, in <module>
      ValueError: long integer out of range
    
    The C code is a bit hard to debug here as its all based on macros. Lets
    see what the Python code does:
    
    
      >> bt = family64.IO.LOBTreePy()
      >>> bt[x] = 'str'
      Traceback (most recent call last):
      File "<console>", line 1, in <module>
      File = "/BTrees/_base.py", line 744, in __setitem__
        self._set(self._to_key(key), self._to_value(value))
      File = "/BTrees/_base.py", line 1431, in to_long
        raise ValueError("Value out of range", v)
      ValueError: ('Value out of range', 13830554455654793216L)
    
    Same thing, but it tells us where to look:
    
        try:
            # XXX Python 2.6 doesn't truncate, it spews a warning.
            if not unpack("q", pack("q", v))[0] == v: #pragma NO COVER
                if isinstance(v, int_types):
                    raise ValueError("Value out of range", v)
                raise TypeError('64-bit integer expected')
        except (struct_error,
                OverflowError, #PyPy
               ):
            if isinstance(v, int_types):
                raise ValueError("Value out of range", v)
            raise TypeError('64-bit integer expected')
    
    And there's the problem. Our time_to_64bit_int code does this:
    
      _long_to_double_bits = struct.Struct(b'!Q').pack
      def time_to_64bit_int( value ):
    	"""
    	Given a Python floating point object (usually a time value),
    	return a 64-bit unsigned int that represents it. Useful for
    	storing as the value in a OL tree, when you really want a float
    	(since BTrees does not provide OF), or as a key in a Lx tree.
    	"""
    
    That is, we use "Q" and they use "q". We're producing unsigned values,
    they're expecting signed values.
    
    Luckily, I think we can easily switch to using 'q' because these two
    properties hold:
    
    First, all negative things would be out of range, and greater than their
    positive counterparts:
    
       >>> : for i in range(-1, -100000, -1):
       ....:     try:
       ....:         bt[time_to_64bit_int(i)] =3D 'str'
       ....:         raise Exception('Should not get here')
       ....:     except ValueError:
       ....:         continue
       ....:
    
       >>> : for i in range(-1, -1000000, -1):
       ....:     assert time_to_64bit_int(i) > time_to_64bit_int(-i)
       ....:
    
    Second, all positive values pack the same either way, so existing data
    stays valid:
    
       >>> : for i in xrange(1, sys.maxint):
       ....:     assert struct.pack('q', i) == struct.pack('Q', i)
       ....:
    
    
    
    
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@41893 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/zodb/tests/test_minmax.py
+++ src/nti/zodb/tests/test_minmax.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals, print_function, absolute_import
 
+#disable: accessing protected members, too many methods
+#pylint: disable=W0212,R0904
+
+
+import unittest
 from hamcrest import assert_that, is_, less_than, greater_than, less_than_or_equal_to
 from hamcrest import same_instance
 from hamcrest import has_property
 from hamcrest import has_key
+from hamcrest import calling
+from hamcrest import raises
 
 from nti.testing.matchers import validly_provides
 
@@ -12,94 +19,95 @@ from nti.zodb import interfaces
 from nti.zodb.minmax import MergingCounter, NumericMinimum, NumericMaximum, ConstantZeroValue, NumericPropertyDefaultingToZero
 from nti.zodb.minmax import Minimum, Maximum
 
-from nose.tools import assert_raises
+
 import cPickle as pickle
 
-def test_zope_imports_have_set():
-	for t in Minimum, Maximum:
-		v = t( 0 )
-		v.set( 1 )
-		assert_that( v.value, is_( 1 ) )
+class TestMinMax(unittest.TestCase):
 
-def test_comparisons():
-	mc1 = MergingCounter()
-	mc2 = MergingCounter()
+	def test_zope_imports_have_set(self):
+		for t in Minimum, Maximum:
+			v = t( 0 )
+			v.set( 1 )
+			assert_that( v.value, is_( 1 ) )
 
-	assert_that( mc1, is_( mc2 ) )
-	assert_that( mc1, validly_provides( interfaces.INumericCounter ) )
+	def test_comparisons(self):
+		mc1 = MergingCounter()
+		mc2 = MergingCounter()
 
-	mc2.increment()
+		assert_that( mc1, is_( mc2 ) )
+		assert_that( mc1, validly_provides( interfaces.INumericCounter ) )
 
-	assert_that( mc1, is_( less_than( mc2 ) ) )
-	assert_that( mc2, is_( greater_than( mc1 ) ) )
+		mc2.increment()
 
-	mc1.increment()
-	assert_that( mc1, is_( less_than_or_equal_to( mc2 ) ) )
+		assert_that( mc1, is_( less_than( mc2 ) ) )
+		assert_that( mc2, is_( greater_than( mc1 ) ) )
 
-	assert_that( hash(mc1), is_( mc1.value ) )
+		mc1.increment()
+		assert_that( mc1, is_( less_than_or_equal_to( mc2 ) ) )
 
-def test_add():
-	mc1 = MergingCounter(1)
-	mc2 = MergingCounter(2)
+		assert_that( hash(mc1), is_( mc1.value ) )
 
-	assert_that( mc1 + mc2, is_( MergingCounter( 3 ) ) )
+	def test_add(self):
+		mc1 = MergingCounter(1)
+		mc2 = MergingCounter(2)
 
-	assert_that( mc1 + 2, is_( 3 ) )
+		assert_that( mc1 + mc2, is_( MergingCounter( 3 ) ) )
 
-	mc1 += 2
-	assert_that( mc1, is_( MergingCounter( 3 ) ) )
+		assert_that( mc1 + 2, is_( 3 ) )
 
-def test_merge_resolve():
+		mc1 += 2
+		assert_that( mc1, is_( MergingCounter( 3 ) ) )
 
-	assert_that( MergingCounter()._p_resolveConflict( 0, 0, 1 ), is_( 1 ) )
-	# simultaneous increment adds
-	assert_that( MergingCounter()._p_resolveConflict( 0, 1, 1 ), is_( 2 ) )
+	def test_merge_resolve(self):
 
-def test_min_resolve():
+		assert_that( MergingCounter()._p_resolveConflict( 0, 0, 1 ), is_( 1 ) )
+		# simultaneous increment adds
+		assert_that( MergingCounter()._p_resolveConflict( 0, 1, 1 ), is_( 2 ) )
 
-	assert_that( NumericMinimum()._p_resolveConflict( 0, 0, 1 ), is_( 0 ) )
-	# simultaneous increment adds
-	assert_that( NumericMinimum()._p_resolveConflict( 3, 4, 2 ), is_( 2 ) )
+	def test_min_resolve(self):
+
+		assert_that( NumericMinimum()._p_resolveConflict( 0, 0, 1 ), is_( 0 ) )
+		# simultaneous increment adds
+		assert_that( NumericMinimum()._p_resolveConflict( 3, 4, 2 ), is_( 2 ) )
 
 
-def test_str():
+	def test_str(self):
 
-	mc = MergingCounter()
-	assert_that( str(mc), is_( "0" ) )
-	assert_that( repr(mc), is_( "MergingCounter(0)" ) )
+		mc = MergingCounter()
+		assert_that( str(mc), is_( "0" ) )
+		assert_that( repr(mc), is_( "MergingCounter(0)" ) )
 
-	mc.set( 1 )
-	assert_that( str(mc), is_( "1" ) )
-	assert_that( repr(mc), is_( "MergingCounter(1)" ) )
+		mc.set( 1 )
+		assert_that( str(mc), is_( "1" ) )
+		assert_that( repr(mc), is_( "MergingCounter(1)" ) )
 
-def test_zero():
-	czv = ConstantZeroValue()
-	assert_that( czv, is_( same_instance( ConstantZeroValue() ) ) )
-	assert_that( czv, has_property( 'value', 0 ) )
+	def test_zero(self):
+		czv = ConstantZeroValue()
+		assert_that( czv, is_( same_instance( ConstantZeroValue() ) ) )
+		assert_that( czv, has_property( 'value', 0 ) )
 
-	# equality
-	assert_that( czv, is_( czv ) )
-	v = NumericMaximum()
-	assert_that( czv, is_( v ) )
-	assert_that( v, is_( czv ) )
+		# equality
+		assert_that( czv, is_( czv ) )
+		v = NumericMaximum()
+		assert_that( czv, is_( v ) )
+		assert_that( v, is_( czv ) )
 
-	v.value = -1
-	assert_that( v, is_( less_than( czv ) ) )
+		v.value = -1
+		assert_that( v, is_( less_than( czv ) ) )
 
-	v.value = 1
-	assert_that( v, is_( greater_than( czv ) ) )
+		v.value = 1
+		assert_that( v, is_( greater_than( czv ) ) )
 
-	czv.value = 1
-	assert_that( czv, has_property( 'value', 0 ) )
+		czv.value = 1
+		assert_that( czv, has_property( 'value', 0 ) )
 
-	czv.set( 2 )
-	assert_that( czv, has_property( 'value', 0 ) )
+		czv.set( 2 )
+		assert_that( czv, has_property( 'value', 0 ) )
 
-	with assert_raises( TypeError ):
-		pickle.dumps( czv )
-
-	with assert_raises( NotImplementedError ):
-		czv._p_resolveConflict( None, None, None )
+		assert_that( calling( pickle.dumps ).with_args(czv),
+					 raises(TypeError))
+		assert_that( calling(czv._p_resolveConflict).with_args(None,None,None),
+					 raises(NotImplementedError))
 
 from nti.zodb.persistentproperty import PersistentPropertyHolder
 from ZODB import DB
@@ -108,80 +116,82 @@ class WithProperty(PersistentPropertyHolder):
 	a = NumericPropertyDefaultingToZero( str('a'), NumericMaximum, as_number=True )
 	b = NumericPropertyDefaultingToZero( str('b'), MergingCounter )
 
-def test_zero_property_increment():
-	db = DB(None)
-	conn = db.open()
+class TestProperty(unittest.TestCase):
 
-	obj = WithProperty()
-	conn.add( obj )
+	def test_zero_property_increment(self):
+		db = DB(None)
+		conn = db.open()
 
-	assert_that( obj._p_status, is_( 'saved' ) )
+		obj = WithProperty()
+		conn.add( obj )
 
-	# Just accessing them doesn't change the saved status
-	assert_that( obj.a, is_( 0 ) )
-	assert_that( obj._p_status, is_( 'saved' ) )
+		assert_that( obj._p_status, is_( 'saved' ) )
 
-	assert_that( obj.b.value, is_( 0 ) )
-	assert_that( obj._p_status, is_( 'saved' ) )
+		# Just accessing them doesn't change the saved status
+		assert_that( obj.a, is_( 0 ) )
+		assert_that( obj._p_status, is_( 'saved' ) )
 
-	assert_that( obj.__getstate__(), is_( {} ) )
+		assert_that( obj.b.value, is_( 0 ) )
+		assert_that( obj._p_status, is_( 'saved' ) )
 
-	# Only when we do something does the status change
-	obj.b.increment()
-	assert_that( obj._p_status, is_( 'changed' ) )
-	assert_that( obj.b, is_( same_instance( obj.b ) ) )
-	assert_that( obj.__getstate__(), has_key( 'b' ) )
-	assert_that( obj.b, is_( MergingCounter ) )
+		assert_that( obj.__getstate__(), is_( {} ) )
 
-
-def test_zero_property_set():
-	db = DB(None)
-	conn = db.open()
-
-	obj = WithProperty()
-	conn.add( obj )
-
-	assert_that( obj._p_status, is_( 'saved' ) )
-
-	# Just accessing them doesn't change the saved status
-	assert_that( obj.a, is_( 0 ) )
-	assert_that( obj._p_status, is_( 'saved' ) )
-
-	assert_that( obj.b.value, is_( 0 ) )
-	assert_that( obj._p_status, is_( 'saved' ) )
-
-	assert_that( obj.__getstate__(), is_( {} ) )
-
-	# Only when we do something does the status change
-	obj.b.set(3)
-	assert_that( obj._p_status, is_( 'changed' ) )
-	assert_that( obj.b, is_( same_instance( obj.b ) ) )
-	assert_that( obj.__getstate__(), has_key( 'b' ) )
-	assert_that( obj.b.value, is_( 3 ) )
+		# Only when we do something does the status change
+		obj.b.increment()
+		assert_that( obj._p_status, is_( 'changed' ) )
+		assert_that( obj.b, is_( same_instance( obj.b ) ) )
+		assert_that( obj.__getstate__(), has_key( 'b' ) )
+		assert_that( obj.b, is_( MergingCounter ) )
 
 
+	def test_zero_property_set(self):
+		db = DB(None)
+		conn = db.open()
 
-def test_zero_property_value():
-	db = DB(None)
-	conn = db.open()
+		obj = WithProperty()
+		conn.add( obj )
 
-	obj = WithProperty()
-	conn.add( obj )
+		assert_that( obj._p_status, is_( 'saved' ) )
 
-	assert_that( obj._p_status, is_( 'saved' ) )
+		# Just accessing them doesn't change the saved status
+		assert_that( obj.a, is_( 0 ) )
+		assert_that( obj._p_status, is_( 'saved' ) )
 
-	# Just accessing them doesn't change the saved status
-	assert_that( obj.a, is_( 0 ) )
-	assert_that( obj._p_status, is_( 'saved' ) )
+		assert_that( obj.b.value, is_( 0 ) )
+		assert_that( obj._p_status, is_( 'saved' ) )
 
-	assert_that( obj.b.value, is_( 0 ) )
-	assert_that( obj._p_status, is_( 'saved' ) )
+		assert_that( obj.__getstate__(), is_( {} ) )
 
-	assert_that( obj.__getstate__(), is_( {} ) )
+		# Only when we do something does the status change
+		obj.b.set(3)
+		assert_that( obj._p_status, is_( 'changed' ) )
+		assert_that( obj.b, is_( same_instance( obj.b ) ) )
+		assert_that( obj.__getstate__(), has_key( 'b' ) )
+		assert_that( obj.b.value, is_( 3 ) )
 
-	# Only when we do something does the status change
-	obj.a = 3
-	assert_that( obj._p_status, is_( 'changed' ) )
-	assert_that( obj.a, is_( same_instance( obj.a ) ) )
-	assert_that( obj.__getstate__(), has_key( 'a' ) )
-	assert_that( obj.a, is_( 3 ) )
+
+
+	def test_zero_property_value(self):
+		db = DB(None)
+		conn = db.open()
+
+		obj = WithProperty()
+		conn.add( obj )
+
+		assert_that( obj._p_status, is_( 'saved' ) )
+
+		# Just accessing them doesn't change the saved status
+		assert_that( obj.a, is_( 0 ) )
+		assert_that( obj._p_status, is_( 'saved' ) )
+
+		assert_that( obj.b.value, is_( 0 ) )
+		assert_that( obj._p_status, is_( 'saved' ) )
+
+		assert_that( obj.__getstate__(), is_( {} ) )
+
+		# Only when we do something does the status change
+		obj.a = 3
+		assert_that( obj._p_status, is_( 'changed' ) )
+		assert_that( obj.a, is_( same_instance( obj.a ) ) )
+		assert_that( obj.__getstate__(), has_key( 'a' ) )
+		assert_that( obj.a, is_( 3 ) )
