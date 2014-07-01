ff56f97817a8e590ff9558ec252056417cffbacc
Jason Madden <jason.madden@nextthought.com>
2014-07-01 01:12:24 +0000

    Use a different template style to avoid some extra space at the end of rendered inline math. NOTE this only handles one of two possible cases, but there are no test failures because I think that other case doesn't exist anymore...if it does that render job will fail with a clear AttributeError and we'll have to revisit a bit. You need updated nti.plasTeX
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@42368 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/resources/tests/test_resourcerenderer.py
+++ src/nti/contentrendering/resources/tests/test_resourcerenderer.py
@@ -14,10 +14,9 @@ logger = __import__('logging').getLogger(__name__)
 #disable: accessing protected members, too many methods
 #pylint: disable=W0212,R0904
 
-
-
 from hamcrest import assert_that
 from hamcrest import has_property
+from hamcrest import contains_string
 
 from nti.contentrendering.tests import simpleLatexDocumentText
 from nti.contentrendering.tests import ContentrenderingLayerTest
@@ -55,3 +54,14 @@ class TestResourceRenderer(ContentrenderingLayerTest):
 			# Its renderers should now claim to be enabled
 			assert_that( renderer, has_property( 'vectorImager', has_property( 'enabled', True ) ) )
 			assert_that( renderer, has_property( 'imager', has_property( 'enabled', True ) ) )
+
+	def test_extra_mathjax_spaces(self):
+		# make sure that no extra space is inserted after inline math
+		body = "A line of text $a + b$, and a comma."
+
+		text = simpleLatexDocumentText( bodies=(body, ) )
+
+		with RenderContext( text ) as ctx:
+			ctx.render(images=True)
+			val = ctx.read_rendered_file('tag_nextthought_com_2011-10_testing-HTML-temp_0.html')
+			assert_that( val, contains_string('</span>, and a comma.'))
