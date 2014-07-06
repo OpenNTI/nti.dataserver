c75b24e6a091e9f4f5b0ec97ecbbfd50575f89cd
Jason Madden <jason.madden@nextthought.com>
2014-07-06 15:22:40 +0000

    Enable updating content indexes automatically when library contents change.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@42676 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/indexmanager.py
+++ src/nti/contentsearch/indexmanager.py
@@ -23,17 +23,22 @@ from nti.dataserver import interfaces as nti_interfaces
 from . import search_query
 from . import search_utils
 from . import search_results
-from . import interfaces as search_interfaces
+
+from .interfaces import IIndexManager
+from .interfaces import IContentSearcherFactory
+from .interfaces import IContentSearcher
+from .interfaces import ISearchQuery
+from .interfaces import IEntityIndexController
 
 from nti.externalization.persistence import NoPickle
 
-@interface.implementer(search_interfaces.IIndexManager)
+@interface.implementer(IIndexManager)
 @NoPickle
 class IndexManager(object):
 
 	@classmethod
 	def get_shared_indexmanager(cls):
-		return component.getGlobalSiteManager().getUtility(search_interfaces.IIndexManager)
+		return component.getGlobalSiteManager().getUtility(IIndexManager)
 
 	parallel_search = False
 
@@ -49,7 +54,7 @@ class IndexManager(object):
 
 	@classmethod
 	def create_content_searcher(cls, *args, **kwargs):
-		factory = component.getUtility(search_interfaces.IContentSearcherFactory)
+		factory = component.getUtility(IContentSearcherFactory)
 		result = factory(*args, **kwargs)
 		return result
 
@@ -100,8 +105,9 @@ class IndexManager(object):
 			return False
 		ntiid = ntiid.lower()
 
-		searcher = component.queryUtility(search_interfaces.IContentSearcher,
-										  name=ntiid)
+		sm = component.getSiteManager()
+		searcher = sm.queryUtility(IContentSearcher,
+								   name=ntiid)
 		if searcher is not None:
 			return searcher
 
@@ -111,22 +117,41 @@ class IndexManager(object):
 		searcher = self.create_content_searcher(ntiid=ntiid,
 												*args, **kwargs)
 		if searcher is not None:
-			component.provideUtility(searcher, search_interfaces.IContentSearcher,
-									 name=ntiid)
+			sm.registerUtility(searcher,
+							   provided=IContentSearcher,
+							   name=ntiid)
 			logger.info("Content '%s' has been added to index manager", ntiid)
 		else:
 			logger.error("Content '%s' could not be added to index manager", ntiid)
 		return searcher
 
-	add_book = register_content
+	def unregister_content(self, ntiid):
+		# Be careful to only unregister something found in the
+		# current sitemanager. If nothing is registered at all,
+		# return None. Otherwise, return True if we removed something
+		# from this site manager, and False if we had nothing to remove
+		# (meaning it was registered at a higher site)
+		ntiid = ntiid.lower() if ntiid else ''
+
+		sm = component.getSiteManager()
+		searcher = sm.queryUtility(IContentSearcher,
+								   name=ntiid)
+		result = searcher
+		if searcher is not None:
+			result = sm.unregisterUtility(searcher,
+										  provided=IContentSearcher,
+										  name=ntiid)
+
+		logger.info("Unregistered content '%s' from index manager? %s", ntiid, result)
+		return result
 
 	def get_content_searcher(self, query):
 		name = query.indexid.lower() if query.indexid else ''
-		searcher = component.queryUtility(search_interfaces.IContentSearcher, name=name)
+		searcher = component.queryUtility(IContentSearcher, name=name)
 		return searcher
 
 	def content_search(self, query, store=None):
-		query = search_interfaces.ISearchQuery(query)
+		query = ISearchQuery(query)
 		results = search_results.get_or_create_search_results(query, store)
 		searcher = self.get_content_searcher(query)
 		if searcher is not None:
@@ -135,7 +160,7 @@ class IndexManager(object):
 		return results
 
 	def content_suggest_and_search(self, query, store=None):
-		query = search_interfaces.ISearchQuery(query)
+		query = ISearchQuery(query)
 		results = search_results.get_or_create_suggest_and_search_results(query, store)
 		searcher = self.get_content_searcher(query)
 		if searcher is not None:
@@ -144,7 +169,7 @@ class IndexManager(object):
 		return results
 
 	def content_suggest(self, query, store=None, *args, **kwargs):
-		query = search_interfaces.ISearchQuery(query)
+		query = ISearchQuery(query)
 		results = search_results.get_or_create_suggest_results(query, store)
 		searcher = self.get_content_searcher(query)
 		if searcher is not None:
@@ -153,26 +178,26 @@ class IndexManager(object):
 		return results
 
 	def user_data_search(self, query, store=None, *args, **kwargs):
-		query = search_interfaces.ISearchQuery(query)
+		query = ISearchQuery(query)
 		entity = self.get_entity(query.username)
-		controller = search_interfaces.IEntityIndexController(entity, None)
+		controller = IEntityIndexController(entity, None)
 		results = controller.search(query, store=store) if controller is not None \
 				  else search_results.empty_search_results(query)
 		return results
 
 	def user_data_suggest_and_search(self, query, store=None, *args, **kwargs):
-		query = search_interfaces.ISearchQuery(query)
+		query = ISearchQuery(query)
 		entity = self.get_entity(query.username)
-		controller = search_interfaces.IEntityIndexController(entity, None)
+		controller = IEntityIndexController(entity, None)
 		results = controller.suggest_and_search(query, store=store) \
 				  if controller is not None \
 				  else search_results.empty_suggest_and_search_results(query)
 		return results
 
 	def user_data_suggest(self, query, store=None, *args, **kwargs):
-		query = search_interfaces.ISearchQuery(query)
+		query = ISearchQuery(query)
 		entity = self.get_entity(query.username)
-		controller = search_interfaces.IEntityIndexController(entity, None)
+		controller = IEntityIndexController(entity, None)
 		results = controller.suggest(query, store=store) if controller is not None \
 				  else search_results.empty_suggest_results(query)
 		return results
@@ -180,27 +205,27 @@ class IndexManager(object):
 	def index_user_content(self, target, data, *args, **kwargs):
 		if data is not None:
 			target = self.get_entity(target)
-			controller = search_interfaces.IEntityIndexController(target, None)
+			controller = IEntityIndexController(target, None)
 			if controller is not None:
 				controller.index_content(data)
 
 	def update_user_content(self, target, data, *args, **kwargs):
 		if data is not None:
 			target = self.get_entity(target)
-			controller = search_interfaces.IEntityIndexController(target, None)
+			controller = IEntityIndexController(target, None)
 			if controller is not None:
 				controller.update_content(data)
 
 	def delete_user_content(self, target, data, *args, **kwargs):
 		if data is not None:
 			target = self.get_entity(target)
-			controller = search_interfaces.IEntityIndexController(target, None)
+			controller = IEntityIndexController(target, None)
 			if controller is not None:
 				controller.delete_content(data)
 
 	def unindex(self, target, uid):
 		target = self.get_entity(target)
-		controller = search_interfaces.IEntityIndexController(target, None)
+		controller = IEntityIndexController(target, None)
 		return controller.unindex(uid) if controller is not None else None
 
 	def close(self):
@@ -210,6 +235,6 @@ class IndexManager(object):
 		# called.
 		pass
 
-@interface.implementer(search_interfaces.IIndexManager)
+@interface.implementer(IIndexManager)
 def create_index_manager():
 	return IndexManager(parallel_search=False)
--- src/nti/contentsearch/tests/test_indexmanager.py
+++ src/nti/contentsearch/tests/test_indexmanager.py
@@ -13,39 +13,29 @@ from hamcrest import has_length
 from hamcrest import assert_that
 
 import time
-import uuid
 import shutil
 import tempfile
 import unittest
 from datetime import datetime
 
-from zope import component
-
-from nti.dataserver.users import User
-from nti.dataserver.users import Community
-from nti.dataserver.contenttypes import Note
-from nti.dataserver.users import DynamicFriendsList
-
 from nti.ntiids.ntiids import make_ntiid
 
-from nti.externalization.externalization import toExternalObject
 
 from nti.contentsearch.search_query import QueryObject
-from nti.contentsearch import interfaces as search_interfaces
+
 from nti.contentsearch.indexmanager import create_index_manager
 from nti.contentsearch.whoosh_schemas import create_book_schema
 from nti.contentsearch.whoosh_storage import create_directory_index
 from nti.contentsearch.whoosh_searcher import WhooshContentSearcher
 
-from nti.contentsearch.constants import (ITEMS, HIT_COUNT)
-
-import nti.dataserver.tests.mock_dataserver as mock_dataserver
 from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
 
-from nti.contentsearch.tests import phrases
-from nti.contentsearch.tests import find_test
-from nti.contentsearch.tests import zanpakuto_commands
-from nti.contentsearch.tests import SharedConfiguringTestLayer
+from . import phrases
+from . import find_test
+from . import SharedConfiguringTestLayer
+
+from nti.testing.matchers import is_false
+from nti.testing.matchers import is_true
 
 class IndexManagerTestLayer(SharedConfiguringTestLayer):
 
@@ -53,7 +43,7 @@ class IndexManagerTestLayer(SharedConfiguringTestLayer):
 	def _add_book_data(cls):
 		indexname = 'bleach'
 		cls.now = time.time()
-		cls.book_idx_dir = tempfile.mkdtemp(dir="/tmp")
+		cls.book_idx_dir = tempfile.mkdtemp()
 		_, storage = create_directory_index(indexname, create_book_schema(),
 											cls.book_idx_dir)
 		cls.bim = WhooshContentSearcher(indexname, storage)
@@ -75,40 +65,47 @@ class IndexManagerTestLayer(SharedConfiguringTestLayer):
 
 	@classmethod
 	def testSetUp(cls, test=None):
-		cls.test = test or find_test()
-		cls.test.now = cls.now
-		cls.test.bim = cls.bim
-		cls.test.book_idx_dir = cls.book_idx_dir
+		test = test or find_test()
+
+		test.now = cls.now
+		test.bim = cls.bim
+		test.book_idx_dir = cls.book_idx_dir
+		test.im = create_index_manager()
 
 	@classmethod
 	def tearDown(cls):
 		cls.bim.close()
 		shutil.rmtree(cls.book_idx_dir, True)
 
-@unittest.SkipTest
 class TestIndexManager(unittest.TestCase):
 
 	layer = IndexManagerTestLayer
 
-	def get_index_mananger(self):
-		result = create_index_manager(parallel_search=False)
-		component.provideUtility(result, search_interfaces.IIndexManager)
-		return result
-	
-	def wait_delay(self):
-		pass
+	now = None
+	bim = None
+	book_idx_dir = None
+	im = None
+
 
 	@WithMockDSTrans
-	def test_add_book(self):
-		self.im = self.get_index_mananger()
-		assert_that(self.im.add_book(indexname='unknown', ntiid='unknown',
-									 indexdir='/tmp'),
-					is_(False))
+	def test_register_content(self):
+		assert_that(self.im.register_content(indexname='unknown', ntiid='unknown',
+											 indexdir=self.book_idx_dir),
+					is_false())
+
+		assert_that(self.im.register_content(indexname='bleach', ntiid='bleach', indexdir=self.book_idx_dir),
+					is_true())
+
+		assert_that( self.im.unregister_content('bleach'),
+					 is_true() )
+
+		assert_that( self.im.unregister_content('bleach'),
+					 is_false() )
+
 
 	@WithMockDSTrans
 	def test_search_book(self):
-		self.im = self.get_index_mananger()
-		self.im.add_book(indexname='bleach', ntiid='bleach', indexdir=self.book_idx_dir)
+		self.im.register_content(indexname='bleach', ntiid='bleach', indexdir=self.book_idx_dir)
 
 		q = QueryObject(indexid='bleach', term='omega')
 		hits = self.im.content_search(query=q)
@@ -122,223 +119,11 @@ class TestIndexManager(unittest.TestCase):
 		hits = self.im.content_suggest(query=q)
 		assert_that(hits, has_length(1))
 
-	def _add_notes_to_ds(self, strings=zanpakuto_commands):
-		notes = []
-		conn = mock_dataserver.current_transaction
-
-		username = str(uuid.uuid4()).split('-')[-1] + '@nti.com'
-		usr = User.create_user(mock_dataserver.current_mock_ds, username=username,
-							   password='temp001')
-
-		for x in strings:
-			note = Note()
-			note.body = [unicode(x)]
-			note.creator = usr.username
-			note.containerId = make_ntiid(nttype='bleach', specific='manga')
-			conn.add(note)
-			notes.append(usr.addContainedObject(note))
-		return notes, usr
-
-	def _add_notes_to_index(self, im, notes, user):
-		for note in notes:
-			im.index_user_content(data=note, target=user)
-		return notes
-
-	def _add_notes_and_index(self, strings=zanpakuto_commands):
-		self.im = self.get_index_mananger()
-		notes, usr = self._add_notes_to_ds(strings)
-		self._add_notes_to_index(self.im, notes, usr)
-		return notes, usr
-
-	@WithMockDSTrans
-	def test_unified_search(self):
-		_, usr = self._add_notes_and_index(('omega radicals', 'the queen of coffee'))
-		self.im.add_book(indexname='bleach', ntiid='bleach', indexdir=self.book_idx_dir)
-		self.wait_delay()
-
-		q = QueryObject(term='omega', indexid='bleach', username=usr.username)
-		hits = self.im.search(q)
-		assert_that(hits, has_length(2))
-
-		q.term = 'coffee'
-		hits = self.im.search(q)
-		assert_that(hits, has_length(1))
-
-	@WithMockDSTrans
-	def test_unified_search_ngrams(self):
-		_, usr = self._add_notes_and_index(('omega radicals', 'the queen of coffee'))
-		self.im.add_book(indexname='bleach', indexdir=self.book_idx_dir)
-		self.wait_delay()
-
-		q = QueryObject(term='omeg', indexid='bleach', username=usr.username)
-		hits = self.im.suggest(q)
-		assert_that(hits, has_length(1))
-
-		hits = self.im.suggest_and_search(q)
-		assert_that(hits, has_length(2))
 
 	@WithMockDSTrans
 	def test_unified_search_suggest(self):
-		_, usr = self._add_notes_and_index(('omega radicals', 'the queen of coffee'))
-		self.im.add_book(indexname='bleach', indexdir=self.book_idx_dir)
-		self.wait_delay()
+		self.im.register_content(indexname='bleach', ntiid='bleach', indexdir=self.book_idx_dir)
 
-		q = QueryObject(term='omeg', indexid='bleach', username=usr.username)
+		q = QueryObject(term='omeg', indexid='bleach')
 		hits = self.im.suggest(q)
-		assert_that(hits, has_length(1))
-
-	@WithMockDSTrans
-	def test_unified_search_suggest_and_search(self):
-		_, usr = self._add_notes_and_index(('omega radicals', 'the queen of coffee'))
-		self.im.add_book(indexname='bleach', indexdir=self.book_idx_dir)
-		self.wait_delay()
-
-		q = QueryObject(term='omeg', indexid='bleach', username=usr.username)
-		hits = self.im.suggest_and_search(q)
-		assert_that(hits, has_length(2))
-
-	@WithMockDSTrans
-	def test_add_notes(self):
-		self._add_notes_and_index()
-
-	@WithMockDSTrans
-	def test_search_notes(self):
-		_, usr = self._add_notes_and_index()
-		self.wait_delay()
-
-		q = QueryObject(term='not_to_be_found', username=usr.username,
-						searchOn=('note',))
-		hits = self.im.user_data_search(query=q)
-		assert_that(hits, has_length(0))
-
-		q = QueryObject(term='rage', username=usr.username, searchOn=('note',))
-		hits = self.im.user_data_search(query=q)
-		assert_that(hits, has_length(1))
-
-	@WithMockDSTrans
-	def test_search_notes_suggest(self):
-		_, usr = self._add_notes_and_index()
-		self.wait_delay()
-
-		q = QueryObject(term='flow', username=usr.username, searchOn=('note',))
-		hits = self.im.user_data_suggest(q)
-		assert_that(hits, has_length(1))
-
-	@WithMockDSTrans
-	def test_search_notes_suggest_and_search(self):
-		_, usr = self._add_notes_and_index()
-		self.wait_delay()
-
-		q = QueryObject(term='creat', username=usr.username, searchOn=('note',))
-		hits = self.im.user_data_suggest_and_search(query=q)
-		assert_that(hits, has_length(1))
-
-	@WithMockDSTrans
-	def test_update_delete_note(self):
-		notes, user = self._add_notes_and_index()
-		self.wait_delay()
-
-		note = notes[0]
-		note.body = [u'Shoot To Death']
-		self.im.update_user_content(user, data=note)
-		q = QueryObject(term='death', username=user.username, searchOn=('note',))
-		hits = self.im.user_data_search(query=q)
-		assert_that(hits, has_length(2))
-
-		note = notes[1]
-		self.im.delete_user_content(user, data=note)
-		q = QueryObject(term='deviate', username=user.username, searchOn=('note',))
-		hits = self.im.user_data_search(query=q)
-		assert_that(hits, has_length(0))
-
-	@WithMockDSTrans
-	def test_note_share_comm(self):
-		ds = mock_dataserver.current_mock_ds
-		user_1 = User.create_user(ds, username='nti-1.com', password='temp001')
-		user_2 = User.create_user(ds, username='nti-2.com', password='temp001')
-
-		c = Community.create_community(ds, username='Bankai')
-		for u in (user_1, user_2):
-			u.record_dynamic_membership(c)
-			u.follow(c)
-
-		note = Note()
-		note.body = [unicode('Hitsugaya and Madarame performing Jinzen')]
-		note.creator = 'nti.com'
-		note.containerId = make_ntiid(nttype='bleach', specific='manga')
-		note.addSharingTarget(c)
-		note = user_2.addContainedObject(note)
-
-		self.im = self.get_index_mananger()
-		self.im.index_user_content(data=note, target=user_2)
-		self.im.index_user_content(data=note, target=c)
-		self.wait_delay()
-
-		q = QueryObject(term='jinzen', username=user_1.username)
-		hits = self.im.user_data_search(query=q)
-		assert_that(hits, has_length(1))
-
-	@WithMockDSTrans
-	def test_same_content_two_comm(self):
-		ds = mock_dataserver.current_mock_ds
-		user = User.create_user(ds, username='nti.com', password='temp001')
-
-		note = Note()
-		note.body = [unicode('Only a few atain both')]
-		note.creator = 'nti.com'
-		note.containerId = make_ntiid(nttype='bleach', specific='manga')
-
-		comms = []
-		for name in ('Bankai', 'Shikai'):
-			c = Community.create_community(ds, username=name)
-			user.record_dynamic_membership(c)
-			user.follow(c)
-			comms.append(c)
-			note.addSharingTarget(c)
-
-		note = user.addContainedObject(note)
-
-		self.im = self.get_index_mananger()
-		for c in comms:
-			self.im.index_user_content(data=note, target=c)
-		self.wait_delay()
-
-		q = QueryObject(term='atain', username=user.username)
-		hits = self.im.user_data_search(query=q)
-		assert_that(hits, has_length(1))
-
-		hits = toExternalObject(hits)
-		assert_that(hits, has_entry(HIT_COUNT, 1))
-		assert_that(hits, has_entry(ITEMS, has_length(1)))
-
-	@WithMockDSTrans
-	def test_note_share_dfl(self):
-		ds = mock_dataserver.current_mock_ds
-		ichigo = User.create_user(ds, username='ichigo@nti.com', password='temp001')
-		aizen = User.create_user(ds, username='aizen@nti.com', password='temp001')
-		gin = User.create_user(ds, username='gin@nti.com', password='temp001')
-
-		bleach = DynamicFriendsList(username='bleach')
-		bleach.creator = ichigo  # Creator must be set
-		ichigo.addContainedObject(bleach)
-		bleach.addFriend(aizen)
-		bleach.addFriend(gin)
-
-		note = Note()
-		note.body = [u'Getsuga Tensho']
-		note.creator = 'nti.com'
-		note.containerId = make_ntiid(nttype='bleach', specific='manga')
-		note.addSharingTarget(bleach)
-		note = ichigo.addContainedObject(note)
-
-		self.im = self.get_index_mananger()
-		for c in (ichigo, bleach):
-			self.im.index_user_content(data=note, target=c)
-
-		q = QueryObject(term='getsuga', username=gin.username)
-		hits = self.im.user_data_search(query=q)
-		assert_that(hits, has_length(1))
-
-		q = QueryObject(term='tensho', username=aizen.username)
-		hits = self.im.user_data_search(query=q)
 		assert_that(hits, has_length(1))
