a751519dd723e9b6eaa51724adb0a13f4d933e03
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-07-09 17:00:48 +0000

    set props and fix call
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@42845 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/courseinfo/checkcourseinfo.py
+++ src/nti/contentrendering/courseinfo/checkcourseinfo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-.. $Id: contentsizesetter.py 37781 2014-04-24 02:33:15Z carlos.sanchez $
+.. $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
