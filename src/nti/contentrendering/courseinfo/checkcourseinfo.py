29f1c6e37fb06d539b48be8d420929963a434d30
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-07-09 16:58:41 +0000

    set props, fix call
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@42843 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/courseinfo/checkcourseinfo.py
+++ src/nti/contentrendering/courseinfo/checkcourseinfo.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*
-
+# -*- coding: utf-8 -*-
+"""
+.. $Id: contentsizesetter.py 37781 2014-04-24 02:33:15Z carlos.sanchez $
+"""
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 import os
+
 from zope import interface
 
 from .. import interfaces
 interface.moduleProvides(interfaces.IRenderedBookValidator)
 
-from nti.contentrendering.courseinfo import courseinfochecker
-
+from .courseinfochecker import CourseInfoValidation
 
 def check(book):
 	contentPath = os.path.dirname(book.toc.root_topic.filename )
 	course_info_file = os.path.join(contentPath, 'course_info.json')
-	course_info = courseinfochecker.CourseInfoValidation()
-	error_check, error_msg, unmatched_fields = course_info.check_course_info(course_info_file)
-	if error_check == True and error_msg[0:7] != 'warning':
+	
+	course_info = CourseInfoValidation()
+	error_check, error_msg, unmatched_fields = \
+		 course_info.check_course_info(course_info_file)
+
+	if error_check and error_msg[0:7] != 'warning':
 		logger.info(error_msg)
 		logger.info(unmatched_fields)
-	elif error_check == True and error_msg[0:7] == 'warning':
+	elif error_check and error_msg[0:7] == 'warning':
 		logger.info(error_msg)
 		logger.info(unmatched_fields)
 	else: 
