96c5f65f88ca7056eb161422c197a6fbf9c72b1b
Jason Madden <jason.madden@nextthought.com>
2014-07-16 15:19:46 +0000

    Highlights have a fill color (default value: what the webapp currently uses). Share the code that canvas uses for reading/writing colors.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@43286 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/contenttypes/highlight.py
+++ src/nti/dataserver/contenttypes/highlight.py
@@ -20,14 +20,18 @@ UserContentRoot = UserContentRoot # BWC top-level import
 from .selectedrange import SelectedRange # BWC top-level import
 from nti.schema.fieldproperty import createDirectFieldProperties
 
+from .color import createColorProperty
+from .color import updateColorFromExternalValue
+
 @interface.implementer(nti_interfaces.IHighlight)
 class Highlight(SelectedRange): #, _HighlightBWC):
 	"""
 	Implementation of a highlight.
 	"""
 
-	createDirectFieldProperties(nti_interfaces.IHighlight) # style
-
+	createDirectFieldProperties(nti_interfaces.IHighlight,
+								omit=('fillColor', 'fillOpacity', 'fillRGBAOpacity')) # style
+	createColorProperty('fill', r=0.882, g=0.956, b=0.996)
 
 	def __init__( self ):
 		super(Highlight,self).__init__()
@@ -37,3 +41,8 @@ from .selectedrange import SelectedRangeInternalObjectIO
 @component.adapter(nti_interfaces.IHighlight)
 class HighlightInternalObjectIO(SelectedRangeInternalObjectIO):
 	_ext_primitive_out_ivars_ = { 'style' } | SelectedRangeInternalObjectIO._ext_primitive_out_ivars_
+
+
+	def updateFromExternalObject( self, ext_parsed, *args, **kwargs ):
+		updateColorFromExternalValue(self.context, 'fill', ext_parsed)
+		SelectedRangeInternalObjectIO.updateFromExternalObject(self, ext_parsed, *args, **kwargs)
