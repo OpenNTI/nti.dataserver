191fe70e9d2efab2e7df4f8e88200fb34e2b3fce
Jason Madden <jason.madden@nextthought.com>
2014-07-21 15:16:36 +0000

    Migrate the legacy course enrollments to the new system. I ran this against my (oldish) copy of the alpha database in about 2 minutes. NOTE: This does not take care of the migration for existing open-enrolls in the courses that will become sectioned.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@43720 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/install.py
+++ src/nti/dataserver/generations/install.py
@@ -9,7 +9,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 48
+generation = 49
 
 # Allow going forward/backward for testing
 import os
