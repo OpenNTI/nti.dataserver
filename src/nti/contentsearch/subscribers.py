985382f2057ff8f2536d76504b42302367685beb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-07-31 22:06:07 +0000

    clean unused
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@44917 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/subscribers.py
+++ src/nti/contentsearch/subscribers.py
@@ -8,16 +8,11 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from zope import component
 from zope import interface
 
-from pyramid.events import ApplicationCreated
+from .interfaces import ISearchHitPredicate
 
-from nti.dataserver import interfaces as nti_interfaces
-
-from . import interfaces as search_interfaces
-
-@interface.implementer(search_interfaces.ISearchHitPredicate)
+@interface.implementer(ISearchHitPredicate)
 class _DefaultSearchHitPredicate(object):
 
 	__slots__ = ()
