e0b66d15f5c15bb6f4e4a25c2c6305feabd64487
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-08-22 03:57:53 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@47791 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_create_user.py
+++ src/nti/dataserver/utils/nti_create_user.py
@@ -13,59 +13,52 @@ relstorage_patch_all_except_gevent_on_import.patch()
 
 logger = __import__('logging').getLogger(__name__)
 
+import os
 import sys
 import pprint
 import argparse
 
 from zope import interface
 from zope import component
+from zope.component import hooks
 
-from nti.dataserver import users
-from nti.dataserver import shards as nti_shards
+from nti.dataserver.users import User
+from nti.dataserver.users import Entity
+from nti.dataserver.users import Community
+from nti.dataserver.interfaces import IUser
+from nti.dataserver.interfaces import INewUserPlacer
 from nti.dataserver.utils import run_with_dataserver
-from nti.dataserver import interfaces as nti_interfaces
+from nti.dataserver.shards import AbstractShardPlacer
+from nti.dataserver.interfaces import ICoppaUserWithoutAgreement
 
 from nti.externalization.externalization import to_external_object
 
-_type_map = { 'user': users.User.create_user,
-			  'community': users.Community.create_community }
+from nti.site.site import get_site_for_site_names
+
+_type_map = { 'user': User.create_user,
+			  'community': Community.create_community }
 
 def _create_user(factory, username, password, realname, communities=(), options=None):
 	__traceback_info__ = locals().items()
 
 	if options.shard:
 		# Provide the unnamed, default utility to do this
-		class FixedShardPlacer(nti_shards.AbstractShardPlacer):
+		class FixedShardPlacer(AbstractShardPlacer):
 			def placeNewUser( self, user, user_directory, *args ):
 				self.place_user_in_shard_named(user, user_directory, options.shard)
-
-		component.provideUtility(FixedShardPlacer(),
-								 provides=nti_interfaces.INewUserPlacer)
+		component.provideUtility(FixedShardPlacer(), provides=INewUserPlacer)
 	elif options.site:
-		# The easiest way to make this happen is to do what the test cases
-		# do, and mock out a pyramid setup with a current request.
-		# (TODO: pyramid has some support for running things as scripts, we should
-		# probably look into that; it should fix the hack for mutating the request?)
-		from pyramid.testing import setUp as psetUp
-		#from pyramid.testing import tearDown as ptearDown
-		from pyramid.testing import DummyRequest
-
-		request = DummyRequest()
-		config = psetUp(registry=component.getGlobalSiteManager(),
-						request=request,
-						hook_zca=False)
-		config.setup_registry()
-		request.headers['origin'] = \
-					'http://' + options.site \
-					if not options.site.startswith('http') else options.site
-		# zope_site_tween tweaks some things on the request that we need to as well
-		request.possible_site_names = \
-			(options.site if not options.site.startswith('http') else options.site[7:],)
-
+		cur_site = hooks.getSite()
+		new_site = get_site_for_site_names( (options.site,), site=cur_site )
+		if new_site is cur_site:
+			print("Unknown site name", options.site)
+			sys.exit(2)
+		hooks.setSite(new_site)
+	
 	user = factory.im_self.get_entity(username)
 	if user:
 		print("Not overwriting existing entity", repr(user), file=sys.stderr)
-		sys.exit( 2 )
+		sys.exit(2)
 
 	alias = options.alias
 	if alias and not isinstance(alias, unicode):
@@ -79,7 +72,7 @@ def _create_user(factory, username, password, realname, communities=(), options=
 	if password:
 		args['password'] = password
 	ext_value = {}
-	# TODO: We could almost pass the options argument itself. It's dict-like
+	
 	if options.email:
 		ext_value['email'] = unicode(options.email)
 	if realname:
@@ -90,18 +83,18 @@ def _create_user(factory, username, password, realname, communities=(), options=
 		ext_value['birthdate'] = unicode(options.birthdate)
 
 	args['external_value'] = ext_value
-	user = factory( **args )
-	if nti_interfaces.IUser.providedBy( user ):
+	user = factory(**args)
+	if IUser.providedBy(user):
 		for com_name in communities or ():
-			community = users.Entity.get_entity( com_name, default='' )
+			community = Entity.get_entity(com_name, default='')
 			if community:
 				user.record_dynamic_membership(community)
-				user.follow( community )
+				user.follow(community)
 
 		if 	options.coppa and \
-			not nti_interfaces.ICoppaUserWithoutAgreement.providedBy(user):
+			not ICoppaUserWithoutAgreement.providedBy(user):
 			logger.info("Applying coppa to %s", user)
-			interface.alsoProvides( user, nti_interfaces.ICoppaUserWithoutAgreement )
+			interface.alsoProvides(user, ICoppaUserWithoutAgreement )
 
 	if options.verbose:
 		pprint.pprint(to_external_object(user))
@@ -112,7 +105,6 @@ def create_user(args=None):
 	arg_parser = argparse.ArgumentParser(description="Create a user-type object")
 	arg_parser.add_argument('username', help="The username to create")
 	arg_parser.add_argument('password', nargs='?')
-	arg_parser.add_argument('--env_dir', help="Dataserver environment root directory")
 	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
 							dest='verbose')
 	arg_parser.add_argument('-t', '--type',
@@ -136,7 +128,8 @@ def create_user(args=None):
 							 dest='communities',
 							 nargs="+",
 							 default=(),
-							 help="The names of communities to add the user to. Slightly incompatible with --site")
+							 help="The names of communities to add the user to. "
+							 	  "Slightly incompatible with --site")
 	arg_parser.add_argument('--coppa',
 							 dest='coppa',
 							 action='store_true',
@@ -147,35 +140,32 @@ def create_user(args=None):
 							 help="The contact email address of the user")
 
 	site_group = arg_parser.add_mutually_exclusive_group()
+
 	site_group.add_argument('-s', '--shard',
 							 dest='shard',
-							 help="The name of the shard to put the user in. Overrides any automatic policy.")
+							 help="The name of the shard to put the user in."
+							 	  "Overrides any automatic policy.")
 
 	site_group.add_argument('--site',
 							 dest='site',
-							 help="Create the user as if done by a request in this application SITE. " +
-							 	  "Use this when site policy should be invoked to set interfaces, establish communities, etc")
+							 help="Application SITE.")
 
 	args = arg_parser.parse_args(args=args)
 
-	import os
-	
-	env_dir = args.env_dir
-	if not env_dir:
-		env_dir = os.getenv( 'DATASERVER_DIR' )
+	env_dir = os.getenv('DATASERVER_DIR')
 	if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
-		raise ValueError( "Invalid dataserver environment root directory", env_dir )
+		print("Invalid dataserver environment root directory", env_dir)
+		sys.exit(2)
 	
 	username = args.username
 	password = args.password
 
-	conf_packages = () if not args.site else ('nti.appserver',)
-
 	run_with_dataserver(environment_dir=env_dir,
-						 xmlconfig_packages=conf_packages,
+						 xmlconfig_packages=('nti.appserver',),
 						 verbose=args.verbose,
 						 function=lambda: _create_user(_type_map[args.type], username,
-													   password, args.name, args.communities, args))
+													   password, args.name, 
+													   args.communities, args))
 
 def main(args=None):
 	create_user(args)
