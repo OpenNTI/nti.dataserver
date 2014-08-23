5c1689e230a3b44637b526e6ea620ddc7e7ff985
Jason Madden <jason.madden@nextthought.com>
2014-08-23 20:38:10 +0000

    First part of generalizing the way forum objects are created based on the constraints declared in the interfaces, part of paving the way for more custom permissioning.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@47869 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/forums/views/create_views.py
+++ src/nti/app/forums/views/create_views.py
@@ -28,21 +28,12 @@ frm_ext = frm_ext
 from nti.dataserver.contenttypes.forums import interfaces as frm_interfaces
 
 
-from nti.dataserver.contenttypes.forums.forum import CommunityForum
-from nti.dataserver.contenttypes.forums.forum import ACLCommunityForum
-from nti.dataserver.contenttypes.forums.topic import PersonalBlogEntry
-from nti.dataserver.contenttypes.forums.post import PersonalBlogComment
-from nti.dataserver.contenttypes.forums.post import GeneralForumComment
-from nti.dataserver.contenttypes.forums.post import PersonalBlogEntryPost
-from nti.dataserver.contenttypes.forums.post import CommunityHeadlinePost
-from nti.dataserver.contenttypes.forums.topic import CommunityHeadlineTopic
-
-
 from .. import VIEW_CONTENTS
 
 
 from .view_mixins import _AbstractForumPostView
 from .view_mixins import AbstractBoardPostView
+from .view_mixins import _AbstractTopicPostView
 
 _view_defaults = dict(  route_name='objects.generic.traversal',
 						renderer='rest' )
@@ -65,16 +56,10 @@ _d_view_defaults.update( permission=nauth.ACT_DELETE,
 
 @view_config( name='' )
 @view_config( name=VIEW_CONTENTS )
-@view_defaults( context=frm_interfaces.IPersonalBlog,
+@view_defaults( context=frm_interfaces.IBoard,
 				**_c_view_defaults)
-class PersonalBlogPostView(_AbstractForumPostView):
-	""" Given an incoming IPost, creates a new topic in the blog """
-
-	_constraint = frm_interfaces.IPersonalBlogEntryPost.providedBy
-	@property
-	def _override_content_type(self):
-		return PersonalBlogEntryPost.mimeType
-	_factory = PersonalBlogEntry
+class BoardPostView(AbstractBoardPostView):
+	""" Given an incoming post, create a new forum. """
 
 
 @view_config( name='' )
@@ -82,62 +67,23 @@ class PersonalBlogPostView(_AbstractForumPostView):
 @view_defaults( context=frm_interfaces.ICommunityBoard,
 				**_c_view_defaults)
 class CommunityBoardPostView(AbstractBoardPostView):
-	""" Given an incoming IPost, creates a new forum in the community board """
-
-	_forum_factory = CommunityForum
-
-
-	def _constructor(self, external_value=None):
-		# TODO: cleaner way to handle community forums with ACL
-		# TODO: We should do some validation of the entity names?
-		# By doing this, we potentially allow the user to create something he cannot
-		# subsequently access, violating a tenet by returning this
-		if external_value and 'ACL' in external_value:
-			def f():
-				result = ACLCommunityForum()
-				# at this point the ACE objects should have been created when _read_incoming_post is called
-				result.ACL = [x for x in external_value['ACL'] if frm_interfaces.IForumACE.providedBy(x)]
-				return result
-			self._forum_factory = f
-		return super(CommunityBoardPostView,self)._constructor(external_value)
-
-	def _get_topic_creator( self ):
+	# XXX: We can do better
+	def _get_topic_creator(self):
 		return self.request.context.creator # the community
 
-@view_config( name='' )
-@view_config( name=VIEW_CONTENTS )
-@view_defaults( context=frm_interfaces.ICommunityForum,
-				**_c_view_defaults )
-class CommunityForumPostView(_AbstractForumPostView):
-	""" Given an incoming IPost, creates a new topic in the community forum """
 
-	_constraint = frm_interfaces.ICommunityHeadlinePost.providedBy
-	@property
-	def _override_content_type(self):
-		return CommunityHeadlinePost.mimeType
-	_factory = CommunityHeadlineTopic
-
-
-from .view_mixins import _AbstractTopicPostView
 
 @view_config( name='' )
 @view_config( name=VIEW_CONTENTS )
-@view_defaults( context=frm_interfaces.ICommunityHeadlineTopic,
-				**_c_view_defaults )
-class CommunityHeadlineTopicPostView(_AbstractTopicPostView):
+@view_defaults( context=frm_interfaces.IForum,
+				**_c_view_defaults)
+class ForumPostView(_AbstractForumPostView):
+	""" Given an incoming post, create a new topic. """
 
-	_constraint = frm_interfaces.IGeneralForumComment.providedBy
-	@property
-	def _override_content_type(self):
-		return GeneralForumComment.mimeType
 
 @view_config( name='' )
 @view_config( name=VIEW_CONTENTS )
-@view_defaults( context=frm_interfaces.IPersonalBlogEntry,
-				**_c_view_defaults )
-class PersonalBlogEntryPostView(_AbstractTopicPostView):
-
-	_constraint = frm_interfaces.IPersonalBlogComment.providedBy
-	@property
-	def _override_content_type(self):
-		return PersonalBlogComment.mimeType
+@view_defaults( context=frm_interfaces.ITopic,
+				**_c_view_defaults)
+class TopicPostView(_AbstractTopicPostView):
+	""" Given an incoming post, create a new comment. """
