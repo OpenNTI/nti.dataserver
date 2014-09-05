d8265ae221167de329e9384f4b664b56945db685
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-09-05 02:49:25 +0000

    load all PluginPoint when deleting a user
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@48686 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_remove_user.py
+++ src/nti/dataserver/utils/nti_remove_user.py
@@ -17,14 +17,33 @@ import os
 import sys
 import argparse
 
+import zope.browserpage
+
 from zope.component import hooks
+from zope.container.contained import Contained
+from zope.configuration import xmlconfig, config
+from zope.dottedname import resolve as dottedname
+
+from z3c.autoinclude.zcml import includePluginsDirective
 
 from nti.dataserver import users
 from nti.dataserver.utils import run_with_dataserver
+
 from nti.site.site import get_site_for_site_names
 
 _type_map = { 'user': (users.User.get_user, users.User.delete_user),
 			  'community': (users.Community.get_entity,  users.Community.delete_entity) }
+
+# package loader info
+
+class PluginPoint(Contained):
+
+	def __init__(self, name):
+		self.__name__ = name
+
+PP_APP = PluginPoint('nti.app')
+PP_APP_SITES = PluginPoint('nti.app.sites')
+PP_APP_PRODUCTS = PluginPoint('nti.app.products')
 
 def _delete_user(factory, username, site=None):
 	__traceback_info__ = locals().items()
@@ -34,9 +53,7 @@ def _delete_user(factory, username, site=None):
 		new_site = get_site_for_site_names( (site,), site=cur_site )
 		if new_site is cur_site:
 			raise ValueError("Unknown site name", site)
-
 		hooks.setSite(new_site)
-
 
 	getter, deleter = factory
 	entity = getter(username)
@@ -46,9 +63,39 @@ def _delete_user(factory, username, site=None):
 
 	return deleter(username)
 
+def _create_context(env_dir, devmode=False):
+	etc = os.getenv('DATASERVER_ETC_DIR') or os.path.join(env_dir, 'etc')
+	etc = os.path.expanduser(etc)
+
+	context = config.ConfigurationMachine()
+	xmlconfig.registerCommonDirectives(context)
+
+	if devmode:
+		context.provideFeature("devmode")
+		
+	slugs = os.path.join(etc, 'package-includes')
+	if os.path.exists(slugs) and os.path.isdir(slugs):
+		package = dottedname.resolve('nti.dataserver')
+		context = xmlconfig.file('configure.zcml', package=package, context=context)
+		xmlconfig.include(context, files=os.path.join(slugs, '*.zcml'),
+						  package='nti.appserver')
+
+	# Include zope.browserpage.meta.zcm for tales:expressiontype
+	# before including the products
+	xmlconfig.include(context, file="meta.zcml", package=zope.browserpage)
+
+	# include plugins
+	includePluginsDirective(context, PP_APP)
+	includePluginsDirective(context, PP_APP_SITES)
+	includePluginsDirective(context, PP_APP_PRODUCTS)
+	
+	return context
+
 def main():
 	arg_parser = argparse.ArgumentParser( description="Delete a user-type object" )
 	arg_parser.add_argument('username', help="The username to delete")
+	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
+							dest='verbose')
 	arg_parser.add_argument('-t', '--type',
 							dest='type',
 							choices=_type_map,
@@ -57,23 +104,21 @@ def main():
 	arg_parser.add_argument('--site',
 							dest='site',
 							help="Delete the user as if done by a request in this application SITE.")
-	arg_parser.add_argument('--env_dir', help="Dataserver environment root directory")
 	args = arg_parser.parse_args()
 
-	site = args.site
-
-	env_dir = args.env_dir
-	if not env_dir:
-		env_dir = os.getenv( 'DATASERVER_DIR' )
+	env_dir = os.getenv('DATASERVER_DIR')
 	if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
-		raise ValueError( "Invalid dataserver environment root directory", env_dir )
+		raise IOError("Invalid dataserver environment root directory")
 
+	site = args.site
 	username = args.username
-
+	context = _create_context(env_dir)
+	conf_packages = ('nti.appserver',)
+	
 	run_with_dataserver( environment_dir=env_dir,
-						 # MUST be sure to get the site configurations
-						 # loaded.
-						 xmlconfig_packages=('nti.appserver',),
+						 xmlconfig_packages=conf_packages,
+						 verbose=args.verbose,
+						 context=context,
 						 function=lambda: _delete_user(_type_map[args.type], username, site))
 	sys.exit( 0 )
 
