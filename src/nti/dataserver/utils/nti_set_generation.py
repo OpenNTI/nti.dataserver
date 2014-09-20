3bf1d255e0f82de92e042ca0d213e644d22fc85b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-09-20 22:37:06 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@49736 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_set_generation.py
+++ src/nti/dataserver/utils/nti_set_generation.py
@@ -11,26 +11,26 @@ relstorage_patch_all_except_gevent_on_import.patch()
 
 logger = __import__('logging').getLogger(__name__)
 
+import os
 import argparse
 
 from zope import component
 
-from nti.dataserver import interfaces as nti_interfaces
+from nti.dataserver.interfaces import IDataserver
 
 from nti.dataserver.utils import run_with_dataserver
 
 def _set_generation(args):
-	ds = component.getUtility(nti_interfaces.IDataserver)
+	ds = component.getUtility(IDataserver)
 	conn = ds.root._p_jar
 	root = conn.root()
 	old_value = root['zope.generations'].get( args.id )
 	if old_value is None:
-		raise ValueError( 'Invalid key, location does not exist (%s)', args.id )
-
+		raise ValueError('Invalid key, location does not exist (%s)', args.id)
 	root['zope.generations'][args.id] = args.generation
 
 def main():
-	arg_parser = argparse.ArgumentParser( description="Create a class with a section" )
+	arg_parser = argparse.ArgumentParser( description="Set generation" )
 	arg_parser.add_argument('generation', help="The generation number to set", type=int)
 	arg_parser.add_argument('id',
 							nargs='?',
@@ -39,15 +39,14 @@ def main():
 	arg_parser.add_argument('--env_dir', help="Dataserver environment root directory")
 	args = arg_parser.parse_args()
 
-	import os
-
 	env_dir = args.env_dir
 	if not env_dir:
 		env_dir = os.getenv( 'DATASERVER_DIR' )
 	if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
 		raise ValueError( "Invalid dataserver environment root directory", env_dir )
 
-	run_with_dataserver( environment_dir=env_dir, function=lambda: _set_generation(args) )
+	run_with_dataserver(environment_dir=env_dir, 
+						function=lambda: _set_generation(args) )
 
 if __name__ == '__main__':
 	main()
