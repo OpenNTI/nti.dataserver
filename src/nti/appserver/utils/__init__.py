f0805412dab76c777f90a742b3c1da93639fddbb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-10-03 01:11:56 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@50405 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/__init__.py
+++ src/nti/appserver/utils/__init__.py
@@ -1,21 +1,20 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Appserver utils views
-
 .. $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+import zope.i18nmessageid
+MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
 
 import simplejson
 import collections
 
 from nti.utils.maps import CaseInsensitiveDict
-
-import zope.i18nmessageid
-MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
 
 class _JsonBodyView(object):
 
