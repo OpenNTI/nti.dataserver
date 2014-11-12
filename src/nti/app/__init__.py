7465afbfe0cc548555df2195335a0576da54285e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-12 16:45:06 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@53500 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/__init__.py
+++ src/nti/app/__init__.py
@@ -1,2 +1,9 @@
-__import__('pkg_resources').declare_namespace(__name__)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
 
+logger = __import__('logging').getLogger(__name__)
