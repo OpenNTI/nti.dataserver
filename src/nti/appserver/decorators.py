a5835b4098f4d0a7c4f27d10c5e2581dbbc45706
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-17 17:44:12 +0000

    add site name in pong response; for JSG
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@53918 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/appserver/decorators.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+from zope.component.hooks import getSite
+
+from nti.externalization.singleton import SingletonDecorator
+from nti.externalization.interfaces import IExternalObjectDecorator
+
+from .interfaces import ILogonPong
+
+@component.adapter(ILogonPong)
+@interface.implementer(IExternalObjectDecorator)
+class _SiteNameAdder(object):
+
+	__metaclass__ = SingletonDecorator
+
+	def decorateExternalObject(self, context, mapping):
+		site = getSite()
+		mapping['Site'] = site.__name__ if site is not None else None
