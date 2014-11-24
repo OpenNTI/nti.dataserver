a8f96daf3c59e0a21dd06a9195ef0e704d8ce539
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-24 15:15:47 +0000

    enable migration
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@54477 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/evolve52.py
+++ src/nti/dataserver/generations/evolve52.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-schema generation installation.
+Generation 52 evolver
 
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
