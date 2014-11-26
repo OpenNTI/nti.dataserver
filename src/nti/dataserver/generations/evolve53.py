f5f3fbd9219655f63ae6ecda5127b4655b130fd3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-26 01:13:56 +0000

    ignore broken entities
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@54679 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/evolve53.py
+++ src/nti/dataserver/generations/evolve53.py
@@ -17,6 +17,9 @@ from zope import component
 from zope.component.hooks import site
 from zope.component.hooks import setHooks
 
+from ZODB.POSException import POSError
+
+from nti.dataserver.users import User
 from nti.dataserver.interfaces import IUser
 
 BROKEN = ('nti.salesforce.users.SalesforceTokenInfo',
@@ -33,16 +36,20 @@ def do_evolve(context):
 		assert	component.getSiteManager() == ds_folder.getSiteManager(), \
 				"Hooks not installed?"
 		users = ds_folder['users']
-		for user in users.values():
-			if not IUser.providedBy(user):
-				continue
-			
+		for username in users.keys():		
 			try:
+				user = User.get_entity(username)
+				if user is None or not IUser.providedBy(user):
+					continue
+			
 				annotations = user.__annotations__
 				for name in BROKEN:
 					annotations.pop(name, None)
-			except (AttributeError):
+			except AttributeError:
 				pass
+			except POSError:
+				logger.exception("Ignoring broken entity object")
+			
 		logger.info('Dataserver evolution %s done', generation)
 
 def evolve(context):
