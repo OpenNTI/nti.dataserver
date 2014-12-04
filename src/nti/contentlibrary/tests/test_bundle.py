95282465c1207e7cdaed1aff6589fd30f2257bfb
Josh Zuech <josh.zuech@nextthought.com>
2014-12-04 20:30:39 +0000

    Add test for bundle package validation
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@55059 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary/tests/test_bundle.py
+++ src/nti/contentlibrary/tests/test_bundle.py
@@ -14,12 +14,11 @@ logger = __import__('logging').getLogger(__name__)
 #disable: accessing protected members, too many methods
 #pylint: disable=W0212,R0904
 
-import unittest
 from hamcrest import assert_that
-from hamcrest import is_
-from hamcrest import has_key
 from hamcrest import greater_than
 from hamcrest import has_property
+from hamcrest import raises
+from hamcrest import calling
 
 from nti.testing.time import time_monotonically_increases
 import os
@@ -79,3 +78,29 @@ class TestBundle(ContentlibraryLayerTest):
 		# adding them back
 		sync_bundle_from_json_key(key, bundle, self.global_library, _meta=meta)
 		assert_that( bundle, has_property('lastModified', greater_than(lm)))
+
+	@time_monotonically_increases
+	def test_missing_package(self):
+		bucket = filesystem.FilesystemBucket()
+		bucket.absolute_path = os.path.join( os.path.dirname(__file__),
+										  'sites', 'localsite',
+										  'ContentPackageBundles', 'ABundle')
+		bucket.name = 'ABundle'
+		key = bucket.getChildNamed('bundle_meta_info.json')
+
+		bundle = PersistentContentPackageBundle()
+		bundle.lastModified = -1
+		bundle.createdTime = -1
+
+		meta = _ContentBundleMetaInfo(key, self.global_library)
+		meta.lastModified = -1
+
+		sync_bundle_from_json_key(key, bundle, self.global_library, _meta=meta)
+
+		# Empty our library
+		self.global_library._contentPackages = ()
+		self.global_library._content_packages_by_ntiid = {}
+		# We will raise if the package is missing
+		assert_that( calling( sync_bundle_from_json_key ) \
+						.with_args(key, bundle, self.global_library, _meta=meta),
+					raises( ValueError ) )
