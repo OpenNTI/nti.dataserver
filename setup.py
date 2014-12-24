da22d26b95cd71d8867cb7af2457d0da55db8f4e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-12-24 21:05:06 +0000

    bump zope.mimetype; fixes https://github.com/zopefoundation/zope.mimetype/pull/1
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@56042 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -392,7 +392,7 @@ setup(
 		'zope.lifecycleevent >= 4.0.3',	 # Object Added/Removed/etc events
 		'zope.login >= 2.0.0a1',  # Providers of Zope3 authentication.ILoginPassword for Zope3 publisher interfaces; pulled in by zope.file[test] and indirectly zope.app.component[test]
 		'zope.location >= 4.0.2',
-		'zope.mimetype == 1.3.1',  # freeze on 1.3.1 pending 2.0.0a2, https://github.com/zopefoundation/zope.mimetype/pull/1
+		'zope.mimetype >= 1.3.1',
 		'zope.minmax >= 2.0.0',
 		'zope.pagetemplate >= 4.0.4',
 		'zope.password >= 4.0.2',  # encrypted password management
