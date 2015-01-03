4de6b0e72fb9898e2179b5bb2ff1aea0cc3c1828
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-03 21:10:26 +0000

    minor fixes to script
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@56404 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/content_extractors.py
+++ src/nti/contentrendering/content_extractors.py
@@ -16,43 +16,48 @@ import argparse
 from zope import component
 
 from .render_document import parse_tex
+
 from .interfaces import IRenderedBookExtractor
+
 from .utils import NoConcurrentPhantomRenderedBook
 
-def transform(book, savetoc=False, outpath=None):
+DEFAULT_LOG_FORMAT = '[%(asctime)-15s] [%(name)s] %(levelname)s: %(message)s'
+
+def transform(book, save_toc=False, outpath=None):
 	for name, extractor in sorted(component.getUtilitiesFor(IRenderedBookExtractor)):
 		logger.info("Extracting %s/%s", name, extractor)
-		extractor.transform(book, savetoc=savetoc, outpath=outpath)
+		extractor.transform(book, savetoc=save_toc, outpath=outpath)
 
 def main():
 	arg_parser = argparse.ArgumentParser(description="Run content extractor")
 	arg_parser.add_argument('contentpath', help="Path to tex content file")
 	arg_parser.add_argument('-t', '--toc', help="Save objects in toc",
-							action='store_true', dest='savetoc')
+							action='store_true', dest='save_toc')
 	arg_parser.add_argument('-o', '--out', help="Output directoty", dest='outpath')
 	arg_parser.add_argument('-v', '--verbose', help="Be verbose",
 							action='store_true', dest='verbose')
 	args = arg_parser.parse_args()
 
 	# process arguments
-	contentpath = os.path.expanduser(args.contentpath)
-	if not os.path.exists(contentpath) or os.path.isdir(contentpath):
+	contentpath = os.path.expanduser(args.contentpath) if args.contentpath else None
+	if not contentpath or not os.path.exists(contentpath) or os.path.isdir(contentpath):
 		raise IOError("Invalid content file")
 	
-	outpath = os.path.expanduser(args.outpath)
+	outpath = os.path.expanduser(args.outpath) if args.outpath else None
 	if outpath and not os.path.exists(outpath):
 		raise IOError("Invalid output directory")
 
 	verbose = args.verbose
-	savetoc = args.savetoc
+	save_toc = args.save_toc
 	if verbose:
-		ei = '[%(asctime)-15s] [%(name)s] %(levelname)s: %(message)s'
-		logging.basicConfig(level=logging.DEBUG, format=ei)
-
+		logging.basicConfig(level=logging.DEBUG, format=DEFAULT_LOG_FORMAT)
+	else:
+		logging.basicConfig(level=logging.INFO, format=DEFAULT_LOG_FORMAT)
+	
 	# do extraction
 	document = parse_tex(contentpath, perform_transforms=False)
 	book = NoConcurrentPhantomRenderedBook(document, contentpath)
-	transform(book, savetoc=savetoc, outpath=outpath)
+	transform(book, save_toc=save_toc, outpath=outpath)
 
 if __name__ == '__main__':
 	main()
