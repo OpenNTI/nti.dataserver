8cf9662503f3dda4714b943566ad32414fc8e78b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-11 22:02:23 +0000

    add helper admin view to resolve an intid
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@57210 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/intids/views.py
+++ src/nti/app/intids/views.py
@@ -3,6 +3,7 @@
 """
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
@@ -16,6 +17,7 @@ from zope.mimetype.interfaces import IContentTypeAware
 from ZODB.POSException import POSError
 
 from pyramid.view import view_config
+from pyramid import httpexceptions as hexc
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
@@ -27,11 +29,36 @@ from nti.dataserver.interfaces import IDeletedObjectPlaceholder
 from nti.externalization.interfaces import LocatedExternalDict
 
 @view_config(route_name='objects.generic.traversal',
+			 renderer='rest',
+			 name='intid_resolver',
+			 request_method='GET',
+			 context=IDataserverFolder,
+			 permission=nauth.ACT_NTI_ADMIN)
+class IntIdResolverView(AbstractAuthenticatedView):
+
+	def __call__(self):
+		request = self.request
+		uid = request.subpath[0] if request.subpath else ''
+		if uid is None:
+			raise hexc.HTTPUnprocessableEntity("Must specify a intid")
+		
+		try:
+			uid = int(uid)
+		except (ValueError, TypeError, AssertionError):
+			raise hexc.HTTPUnprocessableEntity("Must specify a valid intid")
+		
+		intids = component.getUtility(zope.intid.IIntIds)
+		result = intids.queryObject(uid)
+		if result is None:
+			raise hexc.HTTPNotFound()
+		return result
+
+@view_config(route_name='objects.generic.traversal',
 			 name='unregister_missing_objects',
 			 renderer='rest',
 			 request_method='POST',
 			 context=IDataserverFolder,
-			 permission=nauth.ACT_MODERATE)
+			 permission=nauth.ACT_NTI_ADMIN)
 class UnregisterMissingObjectsView(AbstractAuthenticatedView, 
 						 		   ModeledContentUploadRequestUtilsMixin):
 	
