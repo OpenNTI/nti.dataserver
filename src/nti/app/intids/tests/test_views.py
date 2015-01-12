46846977badb35ab7c9736e254dd1601f24909b0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-12 05:32:16 +0000

    test view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@57364 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/intids/tests/test_views.py
+++ src/nti/app/intids/tests/test_views.py
@@ -13,6 +13,12 @@ from hamcrest import has_length
 from hamcrest import assert_that
 from hamcrest import greater_than
 
+import zope.intid
+
+from zope import component
+
+from nti.dataserver.tests import mock_dataserver
+
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 from nti.app.testing.application_webtest import ApplicationLayerTest
 
@@ -27,3 +33,15 @@ class TestViews(ApplicationLayerTest):
 		assert_that(res.json_body, has_entry('TotalBroken', is_(0)))
 		assert_that(res.json_body, has_entry('TotalMissing', is_(0)))
 		assert_that(res.json_body, has_entry('Total', is_(greater_than(0))))
+		
+	@WithSharedApplicationMockDS(users=True, testapp=True)
+	def test_intid_resolver(self):
+		
+		with mock_dataserver.mock_db_trans(self.ds):
+			user = self._get_user()
+			intids = component.getUtility(zope.intid.IIntIds)
+			uid = intids.getId(user)
+		path = '/dataserver2/@@intid_resolver/%s' % uid
+		res = self.testapp.get(path, status=200)
+		assert_that(res.json_body, has_entry('Class', 'User'))
+
