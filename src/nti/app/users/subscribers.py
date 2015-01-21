c7905f3d9c377461e33ae7843145e4e74b36954c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-21 21:43:36 +0000

    @nextthought.com are special accounts
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@58072 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/users/subscribers.py
+++ src/nti/app/users/subscribers.py
@@ -3,6 +3,7 @@
 """
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
@@ -22,6 +23,5 @@ def new_user_is_not_blacklisted(user, event):
 	Verify that this new user does not exist in our blacklist of former users.
 	"""
 	user_blacklist = component.getUtility( IUserBlacklistedStorage )
-
 	if user_blacklist.is_user_blacklisted( user ):
 		raise BlacklistedUsernameError( user.username )
