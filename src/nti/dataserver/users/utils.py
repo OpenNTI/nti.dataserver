846ebd3aa7be34f3f14f20fdb79fef390b58d7b6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-22 19:31:13 +0000

    add utility
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@58122 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/users/utils.py
+++ src/nti/dataserver/users/utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-$Id$
+.. $Id$
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
@@ -9,3 +9,32 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import component
+from zope.catalog.interfaces import ICatalog
+
+from .index import IX_EMAIL
+from .index import IX_TOPICS
+from .index import CATALOG_NAME
+from .index import IX_EMAIL_VERIFIED
+
+def verifed_email_ids(email):
+    catalog = component.getUtility(ICatalog, name=CATALOG_NAME)
+    email = email.lower() # normalize
+      
+    # all ids w/ this email
+    email_idx = catalog[IX_EMAIL]
+    intids_emails = catalog.family.IF.Set(email_idx._fwd_index.get(email) or ())
+    if not intids_emails:
+        return catalog.family.IF.Set()
+    
+    # all verified emails 
+    verifed_idx = catalog[IX_TOPICS][IX_EMAIL_VERIFIED]    
+    intids_verified = catalog.family.IF.Set(verifed_idx.getIds())
+    
+    # intersect
+    result = catalog.family.IF.intersection(intids_emails, intids_verified)
+    return result
+
+def is_email_verfied(email):
+    result = verifed_email_ids(email)
+    return bool(result)
