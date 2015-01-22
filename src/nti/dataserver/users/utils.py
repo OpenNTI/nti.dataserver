d4f92f0862d66cef3e5a8ffa13c6d4136d04afb3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-22 21:04:19 +0000

    add a function to force email verification profile and index
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@58135 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/users/utils.py
+++ src/nti/dataserver/users/utils.py
@@ -9,6 +9,8 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import zope.intid
+
 from zope import component
 from zope.catalog.interfaces import ICatalog
 
@@ -17,10 +19,12 @@ from .index import IX_TOPICS
 from .index import CATALOG_NAME
 from .index import IX_EMAIL_VERIFIED
 
+from .interfaces import IUserProfile
+
 def verified_email_ids(email):
-    catalog = component.getUtility(ICatalog, name=CATALOG_NAME)
     email = email.lower() # normalize
-      
+    catalog = component.getUtility(ICatalog, name=CATALOG_NAME)
+   
     # all ids w/ this email
     email_idx = catalog[IX_EMAIL]
     intids_emails = catalog.family.IF.Set(email_idx._fwd_index.get(email) or ())
@@ -35,6 +39,18 @@ def verified_email_ids(email):
     result = catalog.family.IF.intersection(intids_emails, intids_verified)
     return result
 
+def force_email_verification(user, profile_iface=IUserProfile):
+    intids = component.getUtility(zope.intid.IIntIds)
+    uid = intids.queryId(user)
+    profile = profile_iface(user, None)   
+    if profile is not None and uid is not None:   
+        profile.email_verified = True
+        catalog = component.getUtility(ICatalog, name=CATALOG_NAME)
+        verifed_idx = catalog[IX_TOPICS][IX_EMAIL_VERIFIED]
+        verifed_idx.index_doc(uid, user)
+        return True
+    return False
+    
 def is_email_verified(email):
     result = verified_email_ids(email)
     return bool(result)
