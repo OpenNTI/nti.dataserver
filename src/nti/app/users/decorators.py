2d5a548c69620d8f33a0b6b693a2d8705218e7a9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-23 05:36:32 +0000

    add link and test it
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@58200 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/users/decorators.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+from zope import component
+
+from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
+
+from nti.dataserver.links import Link
+from nti.dataserver.interfaces import IUser
+from nti.dataserver.users.interfaces import IUserProfile
+
+from nti.externalization.interfaces import StandardExternalFields
+from nti.externalization.interfaces import IExternalMappingDecorator
+
+from . import REQUEST_EMAIL_VERFICATION_VIEW
+
+LINKS = StandardExternalFields.LINKS
+
+@component.adapter(IUser)
+@interface.implementer(IExternalMappingDecorator)
+class _UserEmailVerificationLinkDecorator(AbstractAuthenticatedRequestAwareDecorator):
+
+	def _predicate(self, context, result):
+		profile = IUserProfile(context, None)
+		result = self._is_authenticated and profile and not profile.email_verified
+		return result
+
+	def _do_decorate_external(self, context, result):
+		_links = result.setdefault(LINKS, [])
+		link = Link(context, rel="RequestEmailVerification",
+					elements=(REQUEST_EMAIL_VERFICATION_VIEW,))
+		_links.append(link)
