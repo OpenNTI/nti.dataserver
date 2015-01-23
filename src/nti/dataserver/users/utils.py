a208aa73a0a789f870e058f9c62ecf42586078ce
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-23 20:17:55 +0000

    refactor out for better usability
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@58239 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/users/utils.py
+++ src/nti/dataserver/users/utils.py
@@ -22,35 +22,40 @@ from .index import IX_EMAIL_VERIFIED
 from .interfaces import IUserProfile
 
 def verified_email_ids(email):
-    email = email.lower() # normalize
-    catalog = component.getUtility(ICatalog, name=CATALOG_NAME)
-   
-    # all ids w/ this email
-    email_idx = catalog[IX_EMAIL]
-    intids_emails = catalog.family.IF.Set(email_idx._fwd_index.get(email) or ())
-    if not intids_emails:
-        return catalog.family.IF.Set()
-    
-    # all verified emails 
-    verifed_idx = catalog[IX_TOPICS][IX_EMAIL_VERIFIED]    
-    intids_verified = catalog.family.IF.Set(verifed_idx.getIds())
-    
-    # intersect
-    result = catalog.family.IF.intersection(intids_emails, intids_verified)
-    return result
+	email = email.lower() # normalize
+	catalog = component.getUtility(ICatalog, name=CATALOG_NAME)
 
+	# all ids w/ this email
+	email_idx = catalog[IX_EMAIL]
+	intids_emails = catalog.family.IF.Set(email_idx._fwd_index.get(email) or ())
+	if not intids_emails:
+		return catalog.family.IF.Set()
+	
+	# all verified emails 
+	verifed_idx = catalog[IX_TOPICS][IX_EMAIL_VERIFIED]	
+	intids_verified = catalog.family.IF.Set(verifed_idx.getIds())
+	
+	# intersect
+	result = catalog.family.IF.intersection(intids_emails, intids_verified)
+	return result
+
+def reindex_email_verification(user):
+	intids = component.getUtility(zope.intid.IIntIds)
+	uid = intids.queryId(user)
+	if uid is not None:
+		catalog = component.getUtility(ICatalog, name=CATALOG_NAME)
+		verifed_idx = catalog[IX_TOPICS][IX_EMAIL_VERIFIED]
+		verifed_idx.index_doc(uid, user)
+		return True
+	return False
+	
 def force_email_verification(user, profile_iface=IUserProfile):
-    intids = component.getUtility(zope.intid.IIntIds)
-    uid = intids.queryId(user)
-    profile = profile_iface(user, None)   
-    if profile is not None and uid is not None:   
-        profile.email_verified = True
-        catalog = component.getUtility(ICatalog, name=CATALOG_NAME)
-        verifed_idx = catalog[IX_TOPICS][IX_EMAIL_VERIFIED]
-        verifed_idx.index_doc(uid, user)
-        return True
-    return False
-    
+	profile = profile_iface(user, None)   
+	if profile is not None:   
+		profile.email_verified = True
+		return reindex_email_verification(user)
+	return False
+	
 def is_email_verified(email):
-    result = verified_email_ids(email)
-    return bool(result)
+	result = verified_email_ids(email)
+	return bool(result)
