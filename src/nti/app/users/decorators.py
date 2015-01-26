2a03df2603a23219fab473f5da9c37bb5d04e2c4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-26 16:52:08 +0000

    add link to verify email w. token
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@58303 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/users/decorators.py
+++ src/nti/app/users/decorators.py
@@ -14,14 +14,18 @@ from zope import component
 
 from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
 
-from nti.dataserver.links import Link
 from nti.dataserver.interfaces import IUser
+from nti.dataserver.interfaces import IDataserverFolder
+
+from nti.dataserver.links import Link
+from nti.dataserver.traversal import find_interface
 from nti.dataserver.users.interfaces import IUserProfile
 
 from nti.externalization.interfaces import StandardExternalFields
 from nti.externalization.interfaces import IExternalMappingDecorator
 
-from . import REQUEST_EMAIL_VERFICATION_VIEW
+from .import REQUEST_EMAIL_VERFICATION_VIEW
+from .import VERIFY_USER_EMAIL_WITH_TOKEN_VIEW
 
 LINKS = StandardExternalFields.LINKS
 
@@ -38,4 +42,9 @@ class _UserEmailVerificationLinkDecorator(AbstractAuthenticatedRequestAwareDecor
 		_links = result.setdefault(LINKS, [])
 		link = Link(context, rel="RequestEmailVerification",
 					elements=(REQUEST_EMAIL_VERFICATION_VIEW,))
+		_links.append(link)
+		
+		ds2 = find_interface(context, IDataserverFolder)
+		link = Link(ds2, rel="VerifyEmailWithToken", method='POST',
+					elements=(VERIFY_USER_EMAIL_WITH_TOKEN_VIEW,))
 		_links.append(link)
