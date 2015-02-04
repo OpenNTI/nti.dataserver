b5b5b3ffa3a6c534b8880b3ef685a13504e27438
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-04 22:24:53 +0000

    clean up a bit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@58887 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/rating_views.py
+++ src/nti/appserver/rating_views.py
@@ -3,8 +3,9 @@
 """
 Views relating to rating objects.
 
-$Id$
+.. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
@@ -18,22 +19,23 @@ from zope import interface
 from zope import component
 
 from pyramid.view import view_config
+from pyramid.interfaces import IRequest
 from pyramid import httpexceptions as hexc
-import pyramid.interfaces
 
 from nti.app.renderers.caching import uncached_in_response
 from nti.app.renderers.decorators import AbstractTwoStateViewLinkDecorator
 
-from nti.externalization import interfaces as ext_interfaces
+from nti.common.maps import CaseInsensitiveDict
+
+from nti.externalization.interfaces import IExternalMappingDecorator
+
+from nti.dataserver import authorization as nauth
 
 from nti.dataserver import rating as ranking
-from nti.dataserver import authorization as nauth
-from nti.dataserver import interfaces as nti_interfaces
+from nti.dataserver.interfaces import IRatable
 
-from nti.utils.maps import CaseInsensitiveDict
-
-@interface.implementer(ext_interfaces.IExternalMappingDecorator)
-@component.adapter(nti_interfaces.IRatable,pyramid.interfaces.IRequest)
+@interface.implementer(IExternalMappingDecorator)
+@component.adapter(IRatable, IRequest)
 class RatingLinkDecorator(AbstractTwoStateViewLinkDecorator):
 	false_view = 'rate'
 	true_view = 'unrate'
@@ -41,7 +43,7 @@ class RatingLinkDecorator(AbstractTwoStateViewLinkDecorator):
 
 @view_config( route_name='objects.generic.traversal',
 			  renderer='rest',
-			  context=nti_interfaces.IRatable,
+			  context=IRatable,
 			  permission=nauth.ACT_READ, # anyone that can see the object
 			  request_method='POST',
 			  name='rate')
@@ -63,7 +65,7 @@ def _RateView(request):
 
 @view_config( route_name='objects.generic.traversal',
 			  renderer='rest',
-			  context=nti_interfaces.IRatable,
+			  context=IRatable,
 			  permission=nauth.ACT_READ,
 			  request_method='DELETE',
 			  name='unrate')
