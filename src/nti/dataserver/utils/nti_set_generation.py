77c3bb35ffaf85d652a245f2bee017fc4e4cebcb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-05 20:32:18 +0000

    set plugin points
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@58956 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_set_generation.py
+++ src/nti/dataserver/utils/nti_set_generation.py
@@ -3,6 +3,7 @@
 """
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
@@ -14,12 +15,53 @@ logger = __import__('logging').getLogger(__name__)
 import os
 import argparse
 
+import zope.browserpage
+
 from zope import component
+from zope.container.contained import Contained
+from zope.configuration import xmlconfig, config
+from zope.dottedname import resolve as dottedname
+
+from z3c.autoinclude.zcml import includePluginsDirective
 
 from nti.dataserver.interfaces import IDataserver
 
 from nti.dataserver.utils import run_with_dataserver
 
+class PluginPoint(Contained):
+
+	def __init__(self, name):
+		self.__name__ = name
+
+PP_APP = PluginPoint('nti.app')
+PP_APP_SITES = PluginPoint('nti.app.sites')
+PP_APP_PRODUCTS = PluginPoint('nti.app.products')
+			
+def _create_context(env_dir):
+	etc = os.getenv('DATASERVER_ETC_DIR') or os.path.join(env_dir, 'etc')
+	etc = os.path.expanduser(etc)
+
+	context = config.ConfigurationMachine()
+	xmlconfig.registerCommonDirectives(context)
+		
+	slugs = os.path.join(etc, 'package-includes')
+	if os.path.exists(slugs) and os.path.isdir(slugs):
+		package = dottedname.resolve('nti.dataserver')
+		context = xmlconfig.file('configure.zcml', package=package, context=context)
+		xmlconfig.include(context, files=os.path.join(slugs, '*.zcml'),
+						  package='nti.appserver')
+
+	# Include zope.browserpage.meta.zcm for tales:expressiontype
+	# before including the products
+	xmlconfig.include(context, file="meta.zcml", package=zope.browserpage)
+
+	# include plugins
+	includePluginsDirective(context, PP_APP)
+	includePluginsDirective(context, PP_APP_SITES)
+	includePluginsDirective(context, PP_APP_PRODUCTS)
+	
+	return context
+				
 def _set_generation(args):
 	ds = component.getUtility(IDataserver)
 	conn = ds.root._p_jar
@@ -31,22 +73,27 @@ def _set_generation(args):
 
 def main():
 	arg_parser = argparse.ArgumentParser( description="Set generation" )
+	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
+							dest='verbose')
 	arg_parser.add_argument('generation', help="The generation number to set", type=int)
 	arg_parser.add_argument('id',
 							nargs='?',
 							help="The name of the component to set",
 							default='nti.dataserver')
-	arg_parser.add_argument('--env_dir', help="Dataserver environment root directory")
 	args = arg_parser.parse_args()
 
-	env_dir = args.env_dir
-	if not env_dir:
-		env_dir = os.getenv( 'DATASERVER_DIR' )
+	env_dir = os.getenv( 'DATASERVER_DIR' )
 	if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
-		raise ValueError( "Invalid dataserver environment root directory", env_dir )
+		raise IOError("Invalid dataserver environment root directory")
 
-	run_with_dataserver(environment_dir=env_dir, 
-						function=lambda: _set_generation(args) )
+	context = _create_context(env_dir)
+	conf_packages = ('nti.appserver',)
+	
+	run_with_dataserver( environment_dir=env_dir,
+						 xmlconfig_packages=conf_packages,
+						 verbose=args.verbose,
+						 context=context,
+						 function=lambda: _set_generation(args) )
 
 if __name__ == '__main__':
 	main()
