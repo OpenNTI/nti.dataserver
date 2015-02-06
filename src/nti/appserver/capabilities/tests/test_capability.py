c63f539960483840eddc568811b21894e0cc4e12
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-06 06:05:54 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@59015 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/capabilities/tests/test_capability.py
+++ src/nti/appserver/capabilities/tests/test_capability.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-#disable: accessing protected members, too many methods
-#pylint: disable=W0212,R0904
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
 import unittest
 from hamcrest import assert_that
@@ -13,8 +13,8 @@ from hamcrest import assert_that
 from nti.testing.matchers import verifiably_provides
 from nti.testing.matchers import validly_provides
 
-from ..capability import Capability
-from ..interfaces import ICapability
+from nti.appserver.capabilities.capability import Capability
+from nti.appserver.capabilities.interfaces import ICapability
 
 class TestCapability(unittest.TestCase):
 
