42099744a8a13692a00d46de8c64a191c967f98b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-13 00:50:51 +0000

    add some base classes
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@59379 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/dataserver/users/suggested_contacts.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from functools import total_ordering
+
+from zope import interface
+from zope.container.contained import Contained
+
+from nti.externalization.representation import WithRepr
+
+from nti.schema.schema import EqHash
+from nti.schema.field import SchemaConfigured
+from nti.schema.fieldproperty import createDirectFieldProperties
+
+from .interfaces import ISuggestedContact
+from .interfaces import ISuggestedContactsContext
+from .interfaces import ISuggestedContactsProvider
+from .interfaces import ISuggestedContactRankingPolicy
+
+@total_ordering
+@WithRepr
+@EqHash("username", "rank")
+@interface.implementer(ISuggestedContact)
+class SuggestedContact(SchemaConfigured, Contained):
+	
+	createDirectFieldProperties(ISuggestedContact)
+
+	def __lt__(self, other):
+		try:
+			return (self.rank, self.username) < (other.rank, other.username)
+		except AttributeError:
+			return NotImplemented
+
+	def __gt__(self, other):
+		try:
+			return (self.rank, self.username) > (other.rank, other.username)
+		except AttributeError:
+			return NotImplemented
+
+@interface.implementer(ISuggestedContactRankingPolicy)
+class DefaultSuggestedContactRankingPolicy(object):
+	
+	@classmethod
+	def sort(cls, contacts):
+		contacts = contacts or ()
+		return sorted(contacts, reverse=True)
+
+@interface.implementer(ISuggestedContactsContext)
+class DefaultSuggestedContactsContext(object):
+	priority = 1
+
+@interface.implementer(ISuggestedContactsProvider)
+class DefaultSuggestedContactsProvider(SchemaConfigured, Contained):
+	createDirectFieldProperties(ISuggestedContactsProvider)
+	
+	@property
+	def priority(self):
+		result = getattr(self.context, 'priority', None) or 1
+		return result
