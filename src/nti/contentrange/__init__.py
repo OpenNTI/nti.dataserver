2f0c23afedbbceae1a0edab82132c81d39f18244
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-13 05:42:41 +0000

    use EqHash decorator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@59420 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrange/__init__.py
+++ src/nti/contentrange/__init__.py
@@ -8,6 +8,7 @@ content, and to migrate ranges of content from one version to another.
 
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
