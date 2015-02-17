99cf7396ea15e9ae8ad140fba6f3d22e41afc72b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-17 23:44:29 +0000

    use base script
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@59667 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_set_generation.py
+++ src/nti/dataserver/utils/nti_set_generation.py
@@ -7,60 +7,17 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-from nti.monkey import relstorage_patch_all_except_gevent_on_import
-relstorage_patch_all_except_gevent_on_import.patch()
-
 logger = __import__('logging').getLogger(__name__)
 
 import os
 import argparse
 
-import zope.browserpage
-
 from zope import component
-from zope.container.contained import Contained
-from zope.configuration import xmlconfig, config
-from zope.dottedname import resolve as dottedname
-
-from z3c.autoinclude.zcml import includePluginsDirective
 
 from nti.dataserver.interfaces import IDataserver
 
 from nti.dataserver.utils import run_with_dataserver
-
-class PluginPoint(Contained):
-
-	def __init__(self, name):
-		self.__name__ = name
-
-PP_APP = PluginPoint('nti.app')
-PP_APP_SITES = PluginPoint('nti.app.sites')
-PP_APP_PRODUCTS = PluginPoint('nti.app.products')
-			
-def _create_context(env_dir):
-	etc = os.getenv('DATASERVER_ETC_DIR') or os.path.join(env_dir, 'etc')
-	etc = os.path.expanduser(etc)
-
-	context = config.ConfigurationMachine()
-	xmlconfig.registerCommonDirectives(context)
-		
-	slugs = os.path.join(etc, 'package-includes')
-	if os.path.exists(slugs) and os.path.isdir(slugs):
-		package = dottedname.resolve('nti.dataserver')
-		context = xmlconfig.file('configure.zcml', package=package, context=context)
-		xmlconfig.include(context, files=os.path.join(slugs, '*.zcml'),
-						  package='nti.appserver')
-
-	# Include zope.browserpage.meta.zcm for tales:expressiontype
-	# before including the products
-	xmlconfig.include(context, file="meta.zcml", package=zope.browserpage)
-
-	# include plugins
-	includePluginsDirective(context, PP_APP)
-	includePluginsDirective(context, PP_APP_SITES)
-	includePluginsDirective(context, PP_APP_PRODUCTS)
-	
-	return context
+from nti.dataserver.utils.base_script import create_context
 				
 def _set_generation(args):
 	ds = component.getUtility(IDataserver)
@@ -86,7 +43,7 @@ def main():
 	if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
 		raise IOError("Invalid dataserver environment root directory")
 
-	context = _create_context(env_dir)
+	context = create_context(env_dir)
 	conf_packages = ('nti.appserver',)
 	
 	run_with_dataserver( environment_dir=env_dir,
@@ -97,4 +54,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
