ca71fdff652a1b797e6f7211d57a353d3a2f278e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-23 23:59:02 +0000

    use common interface
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@60055 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentindexing/whoosh/interfaces.py
+++ src/nti/contentindexing/whoosh/interfaces.py
@@ -7,6 +7,17 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
+from zope import interface
+
+from nti.schema.field import Object
+from nti.schema.field import ValidTextLine
+
+from ..interfaces import IBookIndexer
+from ..interfaces import IContentIndexer
+from ..interfaces import INTICardIndexer
+from ..interfaces import IAudioTranscriptIndexer
+from ..interfaces import IVideoTranscriptIndexer
+
 from ..interfaces import IBookSchemaCreator
 from ..interfaces import INTICardSchemaCreator
 from ..interfaces import IAudioTranscriptSchemaCreator
@@ -22,4 +33,39 @@ class IWhooshAudioTranscriptSchemaCreator(IAudioTranscriptSchemaCreator):
 	pass
 
 class IWhooshVideoTranscriptSchemaCreator(IVideoTranscriptSchemaCreator):
+	pass
+
+class IWhooshIndexSpec(interface.Interface):
+	content = Object(interface.Interface, required=True, title="Content object")
+	indexname = ValidTextLine(title="Index name", required=False)
+	indexdir = ValidTextLine(title="Output directory", required=False)
+
+class IWhooshContentIndexer(IContentIndexer):
+
+	def index(content, indexdir=None, indexname=None, optimize=False):
+		"""
+		Index the specified content 
+		
+		:param content: Content object
+		:param indexdir: Output directory
+		:param indexname: Index name
+		:param optimize: Optimize index flag
+		"""
+
+class IWhooshBookIndexer(IWhooshContentIndexer, IBookIndexer):
+	pass
+
+class IWhooshMediaTranscriptIndexer(IWhooshContentIndexer):
+	pass
+
+class IWhooshAudioTranscriptIndexer(IWhooshMediaTranscriptIndexer,
+									IAudioTranscriptIndexer):
+	pass
+
+class IWhooshVideoTranscriptIndexer(IWhooshMediaTranscriptIndexer,
+									IVideoTranscriptIndexer):
+	pass
+
+
+class IWhooshNTICardIndexer(IWhooshContentIndexer, INTICardIndexer):
 	pass
