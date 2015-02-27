e37105a17da3a56d1568d49b33ff995a9417607a
Josh Zuech <josh.zuech@nextthought.com>
2015-02-27 16:27:40 +0000

    Add test for first time logon link
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@60453 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/test_logon.py
+++ src/nti/appserver/tests/test_logon.py
@@ -17,8 +17,8 @@ from hamcrest import has_item
 from hamcrest import same_instance, greater_than_or_equal_to, greater_than
 from hamcrest import contains_string
 from hamcrest import contains
-
 from hamcrest.library import has_property
+
 from nti.testing.matchers import provides
 from nose.tools import assert_raises
 import zope.testing.loghandler
@@ -28,9 +28,11 @@ from zope import interface
 from zope.component import eventtesting
 
 from nti.appserver import logon
-from nti.appserver.logon import (ping, handshake,password_logon, google_login, openid_login, ROUTE_OPENID_RESPONSE, _update_users_content_roles, _checksum, _openidcallback)
+from nti.appserver.logon import (ping, handshake,password_logon,
+								google_login, openid_login,
+								ROUTE_OPENID_RESPONSE, _update_users_content_roles,
+								_checksum, _openidcallback)
 from nti.appserver.link_providers import flag_link_provider as user_link_provider
-
 
 from nti.app.testing.application_webtest import ApplicationLayerTest
 from nti.appserver.tests.test_application import WithSharedApplicationMockDS
@@ -417,7 +419,8 @@ class TestLogonViews(ApplicationLayerTest):
 
 	@WithMockDSTrans
 	def test_password_logon_success(self):
-		user = users.User.create_user( self.ds, username='jason.madden@nextthought.com', password='temp001' )
+		with mock_dataserver.mock_db_trans( self.ds ):
+			user = users.User.create_user( self.ds, username='jason.madden@nextthought.com', password='temp001' )
 
 		class Policy(object):
 			interface.implements( pyramid.interfaces.IAuthenticationPolicy )
@@ -425,13 +428,28 @@ class TestLogonViews(ApplicationLayerTest):
 				return [("Policy", who)]
 			def authenticated_userid( self, request ):
 				return 'jason.madden@nextthought.com'
+			def effective_principals(self, request):
+				return [self.authenticated_userid(request)]
+
 		get_current_request().registry.registerUtility( Policy() )
-		result = password_logon( get_current_request() )
+		with mock_dataserver.mock_db_trans( self.ds ):
+			result = password_logon( get_current_request() )
 		assert_that( result, is_( hexc.HTTPNoContent ) )
 		assert_that( result.headers, has_entry( "Policy", 'jason.madden@nextthought.com' ) )
-		# The event fired
-		assert_that( user.lastLoginTime,
-					 is_( greater_than( 0 ) ) )
+
+		with mock_dataserver.mock_db_trans( self.ds ):
+			# The event fired
+			assert_that( user.lastLoginTime,
+						is_( greater_than( 0 ) ) )
+			# Verify we have our first_time_logon flag (that is then deleted)
+			assert_that( user_link_provider.has_link( user, 'first_time_logon' ),
+						is_( True ) )
+			testapp = TestApp( self.app )
+			testapp.delete( '/dataserver2/users/jason.madden@nextthought.com/@@NamedLinks/first_time_logon',
+							extra_environ=self._make_extra_environ( 'jason.madden@nextthought.com' ),
+							status=204 )
+			assert_that( user_link_provider.has_link( user, 'first_time_logon' ),
+						is_( False ) )
 
 		# Or a redirect
 		get_current_request().params['success'] = '/the/url/to/go/to'
