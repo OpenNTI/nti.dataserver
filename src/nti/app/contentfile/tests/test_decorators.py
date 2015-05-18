bead9d95b09d89d547841c1ce4bb0ddc49245572
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-18 20:27:33 +0000

    head scratcher.. naming the module contenttypes would cause nose2 tests to stop running correctly.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@65537 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentfile/tests/test_decorators.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import all_of
+from hamcrest import has_key
+from hamcrest import not_none
+from hamcrest import has_entry
+from hamcrest import assert_that
+
+from nti.externalization.internalization import find_factory_for
+from nti.externalization.internalization import update_from_external_object
+
+from nti.externalization.tests import externalizes
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+GIF_DATAURL = b'data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw=='
+
+class TestDecorators(ApplicationLayerTest):
+
+	def test_content_file(self):
+		ext_obj = {
+			'MimeType': 'application/vnd.nextthought.contentfile',
+			'value': GIF_DATAURL,
+			'filename': r'/Users/ichigo/file.gif',
+			'name':'ichigo'
+		}
+
+		assert_that(find_factory_for(ext_obj), is_(not_none()))
+
+		internal = find_factory_for(ext_obj)()
+		update_from_external_object(internal, ext_obj, require_updater=True)
+
+		assert_that(internal, externalizes(all_of(has_key('FileMimeType'),
+												  has_key('filename'),
+												  has_key('name'),
+												  has_entry('url', none()),
+												  has_key('CreatedTime'),
+												  has_key('Last Modified'))))
