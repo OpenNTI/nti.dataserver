280790bcbd1d16a1a7d3aae003b09da80845a2fb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-19 18:42:16 +0000

    rexport
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@65605 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/types/note.py
+++ src/nti/app/types/note.py
@@ -8,3 +8,23 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+from pyramid.view import view_config
+
+from nti.appserver.ugd_edit_views import UGDPostView
+
+from nti.dataserver.interfaces import INote
+
+from nti.dataserver import authorization as nauth
+
+from ..contentfile import ContentFileUploadMixin
+
+_view_defaults = dict(  route_name='objects.generic.traversal',
+                        renderer='rest' )
+_c_view_defaults = _view_defaults.copy()
+_c_view_defaults.update( permission=nauth.ACT_CREATE,
+                         request_method='POST' )
+
+@view_config( context=INote, **_c_view_defaults)
+class NotePostView(ContentFileUploadMixin, UGDPostView):
+    pass
