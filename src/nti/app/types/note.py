e1ff1a0045e13de039786af237c6a1c992141adf
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-20 19:42:56 +0000

    add PUT test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@65699 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/types/note.py
+++ src/nti/app/types/note.py
@@ -14,34 +14,16 @@ from functools import partial
 from zope import component
 from zope import interface
 
-from pyramid.view import view_config
 from pyramid.interfaces import IRequest
 from pyramid.interfaces import IExceptionResponse
 
-from nti.appserver.ugd_edit_views import UGDPutView
 from nti.appserver.interfaces import INewObjectTransformer
 
 from nti.dataserver.interfaces import INote
-from nti.dataserver.interfaces import IContentFile
-
-from nti.dataserver import authorization as nauth
 
 from ..contentfile import validate_sources
+from ..contentfile import get_content_files
 from ..contentfile import read_multipart_sources
-
-_view_defaults = dict(route_name='objects.generic.traversal', renderer='rest')
-_c_view_defaults = _view_defaults.copy()
-_c_view_defaults.update(permission=nauth.ACT_CREATE,
-						request_method='POST')
-
-def get_content_files(context):
-	result = []
-	for data in context.body or ():
-		if IContentFile.providedBy(data):
-			result.append(data)
-			if data.__parent__ is None:
-				data.__parent__ = context
-	return result
 
 @component.adapter(IRequest, INote)
 @interface.implementer(INewObjectTransformer)
@@ -54,17 +36,7 @@ def _submission_transformer_factory(request, context):
 def _submission_transformer(request, context):
 	sources = get_content_files(context)
 	if sources and request and request.POST:
-		read_multipart_sources(request, sources)
+		read_multipart_sources(request, sources.values())
 	if sources:
-		validate_sources(context, sources)
+		validate_sources(context, sources.values())
 	return context
-
-_view_defaults = dict(  route_name='objects.generic.traversal',
-                        renderer='rest' )
-_u_view_defaults = _view_defaults.copy()
-_u_view_defaults.update( permission=nauth.ACT_UPDATE,
-                         request_method='PUT' )
-
-@view_config( context=INote, **_u_view_defaults)
-class NotePutView(UGDPutView):
-	pass
--- src/nti/app/types/tests/test_note.py
+++ src/nti/app/types/tests/test_note.py
@@ -14,9 +14,11 @@ from hamcrest import none
 from hamcrest import is_not
 from hamcrest import has_item
 from hamcrest import has_entry
+from hamcrest import has_length
 from hamcrest import assert_that
 from hamcrest import has_entries
 from hamcrest import has_property
+from hamcrest import greater_than_or_equal_to
 
 from nti.dataserver.contenttypes.file import ContentFile
 
@@ -75,4 +77,48 @@ class TestNote(ApplicationLayerTest):
 		res = testapp.get( durl,
 						   extra_environ=self._make_extra_environ(),
 						   status=200 )
+		assert_that(res, has_property('body', has_length(greater_than_or_equal_to(60))))
+		
+	@WithSharedApplicationMockDS
+	def test_update_note(self):
+		with mock_dataserver.mock_db_trans(self.ds):
+			self._create_user()
+
+		ext_file = {
+			'MimeType': 'application/vnd.nextthought.contentfile',
+			'value': GIF_DATAURL,
+			'filename': r'/Users/ichigo/file.gif',
+			'name':'ichigo'
+		}
+		ext_obj = { "Class": "Note",
+					"ContainerId": "tag_nti_foo",
+					"MimeType": "application/vnd.nextthought.note",
+					"applicableRange": {"Class": "ContentRangeDescription", 
+										"MimeType": "application/vnd.nextthought.contentrange.contentrangedescription"},
+					"body": ['ichigo', ext_file],
+					"title": "bleach"}
+
+		data = to_json_representation(ext_obj)
+		testapp = TestApp(self.app)
+		path = b'/dataserver2/users/sjohnson@nextthought.com/Objects/'
+		res = testapp.post(	urllib.quote(path), data,
+					 		extra_environ=self._make_extra_environ(update_request=True),
+							headers={u"Content-Type": b"application/json" },
+					 		status=201)
+		path = res.json_body['href']
+		durl = res.json_body['body'][1]['download_url']
+		
+		# udpate
+		ext_obj = dict(res.json_body)
+		ext_obj['body'][0] = 'Azien'
+		data = to_json_representation(ext_obj)
+		res = testapp.put(path, data,
+						  extra_environ=self._make_extra_environ(),
+						  headers={u"Content-Type": b"application/json" },
+						  status=200)
+
+		res = testapp.get( durl,
+						   extra_environ=self._make_extra_environ(),
+						   status=200 )
+		assert_that(res, has_property('body', has_length(greater_than_or_equal_to(60))))
 		
