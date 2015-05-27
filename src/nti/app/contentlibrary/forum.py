044f135f487a70ab30c0abd531585a0e4f45d0a5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-27 22:14:49 +0000

    make sure we add the object to the connection when caching the NTIID
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@66109 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/forum.py
+++ src/nti/app/contentlibrary/forum.py
@@ -13,13 +13,17 @@ logger = __import__('logging').getLogger(__name__)
 
 from nti.dataserver.contenttypes.forums import MessageFactory as _
 
+from functools import partial
+
 from zope import schema
 from zope import interface
 from zope import component
 
 from zope.cachedescriptors.property import cachedIn
 
-### Board
+# Board
+
+from nti.contentlibrary.interfaces import IContentPackageBundle
 
 from nti.dataserver.interfaces import system_user
 from nti.dataserver.contenttypes.forums.board import GeneralBoard
@@ -31,6 +35,9 @@ from nti.ntiids.ntiids import TYPE_OID
 
 from .interfaces import IContentBoard
 
+def _safe_to_external_ntiid_oid():
+	return partial(to_external_ntiid_oid, add_to_connection=True)
+
 @interface.implementer(IContentBoard)
 class ContentBoard(GeneralBoard):
 	mime_type = mimeType = 'application/vnd.nextthought.forums.contentboard'
@@ -40,7 +47,7 @@ class ContentBoard(GeneralBoard):
 	# by OID. We are also IUseOIDForNTIID so our children
 	# inherit this.
 	NTIID_TYPE = _ntiid_type = TYPE_OID
-	NTIID = cachedIn('_v_ntiid')(to_external_ntiid_oid)
+	NTIID = cachedIn('_v_ntiid')(_safe_to_external_ntiid_oid())
 
 	# Who owns this? Who created it?
 	# Right now, we're saying "system" did it...
@@ -65,11 +72,11 @@ class ContentBoard(GeneralBoard):
 @interface.implementer(IContentBoard)
 def ContentBoardAdapter(context):
 	board = AnnotatableBoardAdapter(context, ContentBoard, IContentBoard)
-	if board.creator is None:
+	if board.creator is None or IContentPackageBundle.providedBy(context):
 		board.creator = system_user
 	return board
 
-### Forum
+# Forum
 
 from nti.dataserver.contenttypes.forums.forum import GeneralForum
 
@@ -85,7 +92,7 @@ class ContentForum(GeneralForum):
 		# See above about the sharing stuff
 		return True
 
-### Topic
+# Topic
 
 from nti.dataserver import users
 from nti.dataserver.interfaces import IDefaultPublished
@@ -105,8 +112,8 @@ class ContentHeadlineTopic(GeneralHeadlineTopic):
 		# make it visible to the world
 		# XXX NOTE: This will change as I continue to flesh out
 		# the permissioning of the content bundles themselves
-		#auth = IPrincipal( AUTHENTICATED_GROUP_NAME )
-		#interface.alsoProvides(auth, IEntity)
+		# auth = IPrincipal( AUTHENTICATED_GROUP_NAME )
+		# interface.alsoProvides(auth, IEntity)
 		return (users.Entity.get_entity('Everyone'),)
 
 	@property
@@ -117,13 +124,13 @@ class ContentHeadlineTopic(GeneralHeadlineTopic):
 		return result
 
 	def isSharedWith(self, wants):
-		res = super(ContentHeadlineTopic,self).isSharedWith(wants)
+		res = super(ContentHeadlineTopic, self).isSharedWith(wants)
 		if not res:
 			# again, implicitly with everyone
 			res = IDefaultPublished.providedBy(self)
 		return res
 
-### Posts
+# Posts
 
 from nti.dataserver.contenttypes.forums.post import GeneralHeadlinePost
 from nti.dataserver.contenttypes.forums.post import GeneralForumComment
@@ -144,7 +151,7 @@ class ContentCommentPost(GeneralForumComment):
 		# See above about the sharing stuff
 		return True
 
-### ACLs
+# ACLs
 
 from nti.dataserver.contenttypes.forums.acl import _CommunityForumACLProvider
 from nti.dataserver.contenttypes.forums.acl import _CommunityBoardACLProvider
@@ -163,10 +170,10 @@ class _ContentBoardACLProvider(_CommunityBoardACLProvider):
 	"""
 
 	def _extend_acl_after_creator_and_sharing(self, acl):
-		acl.append( ace_allowing( AUTHENTICATED_GROUP_NAME, ACT_READ, ContentBoard ))
+		acl.append(ace_allowing(AUTHENTICATED_GROUP_NAME, ACT_READ, ContentBoard))
 
-		#acl.append( ace_allowing( prin, ACT_CREATE, ContentBoard ))
-		super(_ContentBoardACLProvider,self)._extend_acl_after_creator_and_sharing(acl)
+		# acl.append( ace_allowing( prin, ACT_CREATE, ContentBoard ))
+		super(_ContentBoardACLProvider, self)._extend_acl_after_creator_and_sharing(acl)
 
 @component.adapter(IContentForum)
 class _ContentForumACLProvider(_CommunityForumACLProvider):
@@ -177,7 +184,7 @@ class _ContentForumACLProvider(_CommunityForumACLProvider):
 	def _get_sharing_target_names(self):
 		return ('Everyone', AUTHENTICATED_GROUP_NAME)
 
-### Forum decorators
+# Forum decorators
 
 from nti.externalization.singleton import SingletonDecorator
 from nti.externalization.interfaces import StandardExternalFields
@@ -190,17 +197,17 @@ LINKS = StandardExternalFields.LINKS
 
 @interface.implementer(IExternalMappingDecorator)
 class ContentBoardLinkDecorator(object):
-	#### XXX Very similar to the decorators for Community and PersonalBlog;
+	# XXX Very similar to the decorators for Community and PersonalBlog;
 	# can we unify these?
 	__metaclass__ = SingletonDecorator
 
-	def decorateExternalMapping( self, context, mapping ):
+	def decorateExternalMapping(self, context, mapping):
 		# TODO: This may be slow, if the forum doesn't persistently
 		# exist and we keep creating it and throwing it away (due to
 		# not commiting on GET)
-		board = IContentBoard( context, None )
-		if board is not None: # Not checking security. If the community is visible to you, the forum is too
-			the_links = mapping.setdefault( LINKS, [] )
-			link = Link( board, rel=board.__name__ )
+		board = IContentBoard(context, None)
+		if board is not None:  # Not checking security. If the community is visible to you, the forum is too
+			the_links = mapping.setdefault(LINKS, [])
+			link = Link(board, rel=board.__name__)
 			# link_belongs_to_user( link, context )
-			the_links.append( link )
+			the_links.append(link)
