3cadddcbebf0fdcf14585ff172b72c2623181736
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-03 01:23:16 +0000

    add zope.security
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@66432 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/pyramid_auth.py
+++ src/nti/appserver/pyramid_auth.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-$Id$
+.. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__ )
+logger = __import__('logging').getLogger(__name__)
 
-from zope import interface
 from zope import component
-
-from nti.dataserver import interfaces as nti_interfaces
-from nti.dataserver import authentication as nti_authentication
-from nti.dataserver import authorization as nti_authorization
-
-
-from .interfaces import IUserViewTokenCreator
+from zope import interface
 
 from nti.app.authentication.interfaces import IIdentifiedUserTokenAuthenticator
+
+from nti.dataserver.interfaces import IUser
+from nti.dataserver.interfaces import IGroupMember
+from nti.dataserver import authorization as nti_authorization
+from nti.dataserver import authentication as nti_authentication
+
+from .interfaces import IUserViewTokenCreator
 
 @interface.implementer(IUserViewTokenCreator)
 class _UserViewTokenCreator(object):
@@ -26,26 +27,29 @@ class _UserViewTokenCreator(object):
 	def __init__(self, secret):
 		self.secret = secret
 
-	def getTokenForUserId( self, userid ):
+	def getTokenForUserId(self, userid):
 		"""
 		Given a logon for a user, return a token that can be
 		used to identify the user in the future. If the user
 		does not exist or cannot get a token, return None.
 		"""
-		return component.getAdapter(self.secret,IIdentifiedUserTokenAuthenticator).getTokenForUserId(userid)
+		authenticator = component.getAdapter(self.secret, IIdentifiedUserTokenAuthenticator)
+		return authenticator.getTokenForUserId(userid)
 
 ONE_DAY = 24 * 60 * 60
 ONE_WEEK = 7 * ONE_DAY
 ONE_MONTH = 30 * ONE_DAY
 
+from nti.app.authentication.who_views import ForbiddenView
 from nti.app.authentication.who_policy import AuthenticationPolicy
 from nti.app.authentication.who_apifactory import create_who_apifactory
-from nti.app.authentication.who_views import ForbiddenView
 
-def configure_authentication_policy( pyramid_config,
+DEFAULT_COKIE_SECRET = '$Id$'
+
+def configure_authentication_policy(pyramid_config,
 									 secure_cookies=True,
-									 cookie_secret='$Id$',
-									 cookie_timeout=ONE_WEEK ):
+									 cookie_secret=DEFAULT_COKIE_SECRET,
+									 cookie_timeout=ONE_WEEK):
 	"""
 	Create and configure the authentication policy and the things that go with it.
 
@@ -66,26 +70,27 @@ def configure_authentication_policy( pyramid_config,
 								  cookie_timeout=cookie_timeout,
 								  api_factory=api_factory)
 	# And make it capable of impersonation
-	policy = nti_authentication.DelegatingImpersonatedAuthenticationPolicy( policy )
+	policy = nti_authentication.DelegatingImpersonatedAuthenticationPolicy(policy)
 
-
-	pyramid_config.set_authentication_policy( policy )
-	pyramid_config.add_forbidden_view( ForbiddenView() )
+	pyramid_config.set_authentication_policy(policy)
+	pyramid_config.add_forbidden_view(ForbiddenView())
 
 	user_token_creator = _UserViewTokenCreator(cookie_secret)
 	for view_name in token_allowed_views:
-		pyramid_config.registry.registerUtility( user_token_creator,
+		pyramid_config.registry.registerUtility(user_token_creator,
 												 IUserViewTokenCreator,
-												 name=view_name )
+												 name=view_name)
 	pyramid_config.registry.registerUtility(api_factory)
 
-
-@interface.implementer(nti_interfaces.IGroupMember)
-@component.adapter(nti_interfaces.IUser)
+@component.adapter(IUser)
+@interface.implementer(IGroupMember)
 class NextthoughtDotComAdmin(object):
 	"""
 	Somewhat hackish way to grant the admin role to any account in @nextthought.com
 	"""
 
-	def __init__( self, context ):
-		self.groups = (nti_authorization.ROLE_ADMIN,) if context.username.endswith( '@nextthought.com' ) else ()
+	def __init__(self, context):
+		if context.username.endswith('@nextthought.com'):
+			self.groups = (nti_authorization.ROLE_ADMIN,)
+		else:
+			self.groups = ()
