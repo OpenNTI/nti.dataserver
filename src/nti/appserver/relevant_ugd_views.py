99e2d3203524b3967be7acf58c2239f3d880c1db
Josh Zuech <josh.zuech@nextthought.com>
2015-06-03 15:44:10 +0000

    Dedupe relevant ugd view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@66447 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/relevant_ugd_views.py
+++ src/nti/appserver/relevant_ugd_views.py
@@ -10,6 +10,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from itertools import chain
 
 from zope import interface
 from zope import component
@@ -27,36 +28,15 @@ from nti.contentlibrary import interfaces as lib_interfaces
 from nti.contentlibrary.indexed_data.interfaces import IVideoIndexedDataContainer
 
 from nti.externalization import interfaces as ext_interfaces
-from nti.externalization.interfaces import LocatedExternalDict
-from nti.externalization.interfaces import LocatedExternalList
 
 from nti.mimetype.mimetype import nti_mimetype_with_class
+
+from .ugd_query_views import lists_and_dicts_to_ext_collection
 
 LAST_MODIFIED = ext_interfaces.StandardExternalFields.LAST_MODIFIED
 
 union_operator = query_views.Operator.union
 intersection_operator = query_views.Operator.intersection
-
-def _flatten_list_and_dicts(result, lists_and_dicts, predicate=None):
-	lastMod = getattr(result, 'lastModified', 0)
-	for list_or_dict in lists_and_dicts:
-		if list_or_dict is None:
-			continue
-		try:
-			lastMod = max(lastMod, list_or_dict.lastModified)
-		except AttributeError:
-			pass
-		try:
-			to_iter = list_or_dict.itervalues()
-			lastMod = max(lastMod, list_or_dict.get('Last Modified', 0))
-		except (AttributeError, TypeError):
-			to_iter = list_or_dict
-
-		for item in to_iter:
-			if item is not None and (predicate is None or predicate(item)):
-				result.append(item)  # add to list
-				lastMod = max(lastMod, getattr(item, 'lastModified', 0))
-	result.lastModified = lastMod
 
 class _RelevantUGDView(query_views._UGDView):
 
@@ -93,68 +73,64 @@ class _RelevantUGDView(query_views._UGDView):
 
 	def getObjectsForId(self, user, ntiid):
 		try:
-			objects = super(_RelevantUGDView, self).getObjectsForId(user, ntiid)
+			results = super(_RelevantUGDView, self).getObjectsForId(user, ntiid)
 		except hexc.HTTPNotFound:
-			objects = ({}, {})
+			results = ({}, {})
+		return results
 
-		result = LocatedExternalList()
-		predicate = self._make_complete_predicate()
-		_flatten_list_and_dicts(result, objects, predicate)
-		return result
-
-	def _get_items(self, ntiid, result=None):
+	def _get_items(self, ntiid):
 		# query objects
 		view = query_views._UGDView(self.request)
 		view._DEFAULT_BATCH_SIZE = view._DEFAULT_BATCH_START = None
 		try:
-			all_objects = view.getObjectsForId(self.user, ntiid)
+			results = view.getObjectsForId(self.user, ntiid)
 		except _hexc.HTTPNotFound:
-			return ()
-
-		result = LocatedExternalList() if result is None else result
-		predicate = self._make_complete_predicate()
-		_flatten_list_and_dicts(result, all_objects, predicate)
-		return result
+			results = ()
+		return results
 
 	def _get_library_path(self, ntiid):
 		library = component.getUtility(lib_interfaces.IContentPackageLibrary)
 		paths = library.pathToNTIID(ntiid) if library else None
 		return paths[-1] if paths else None
 
-	def _scan_quizzes(self, ntiid, result=None):
-		result = LocatedExternalList() if result is None else result
+	def _scan_quizzes(self, ntiid):
 		library = component.getUtility(lib_interfaces.IContentPackageLibrary)
 		# quizzes are often subcontainers, so we look at the parent
 		# and its children
 		ntiids = set()
+		results = []
 		for unit in library.childrenOfNTIID(ntiid) + [self._get_library_path(ntiid)]:
 			for asm_item in IQAssessmentItemContainer(unit, ()):
 				q_ntiid = getattr(asm_item, 'ntiid', None)
 				if q_ntiid and q_ntiid not in ntiids:
 					ntiids.add(q_ntiid)
-					self._get_items(q_ntiid, result)
-		return result
+					items = self._get_items(q_ntiid)
+					if items:
+						results.extend( items )
+		return results
 
-	def _scan_videos(self, ntiid, result=None):
-		result = LocatedExternalList() if result is None else result
+	def _scan_videos(self, ntiid):
 		unit = self._get_library_path(ntiid)
+		results = []
 		if unit is not None:
 			for video_data in IVideoIndexedDataContainer(unit).get_data_items():
 				video_id = video_data['ntiid']
-				self._get_items(video_id, result)
-		return result
+				items = self._get_items(video_id)
+				if items:
+					results.extend( items )
+		return results
 
 	def __call__(self):
-		# gather data
+		# Gather data
 		items = self.getObjectsForId(self.user, self.ntiid)
-		self._scan_quizzes(self.ntiid, items)
-		self._scan_videos(self.ntiid, items)
+		quiz_items = self._scan_quizzes(self.ntiid)
+		video_items = self._scan_videos(self.ntiid)
 
-		# return
-		result = LocatedExternalDict()
-		result['Items'] = items
-		result['Total'] = len(items)
+		predicate = self._make_complete_predicate()
+		all_items = chain( items, quiz_items, video_items )
+		# De-dupe; we could batch here if needed.
+		result = lists_and_dicts_to_ext_collection( all_items, predicate )
+		result['Total'] = len( result.get( 'Items', () ) )
 		result.mimeType = nti_mimetype_with_class(None)
-		result.lastModified = result[LAST_MODIFIED] = items.lastModified
 		interface.alsoProvides(result, IUGDExternalCollection)
 		return result
