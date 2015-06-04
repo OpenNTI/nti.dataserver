f9a69c602b0c652336595e4f393c5a5065b1117c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-04 03:30:57 +0000

    export Everyone from its owner module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@66502 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/users/__init__.py
+++ src/nti/dataserver/users/__init__.py
@@ -12,17 +12,21 @@ logger = __import__('logging').getLogger(__name__)
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
 
+from ..activitystream_change import Change
+
 from .users import User, FacebookUser, OpenIdUser
 
-from .users import Community, Everyone, Device
-from .users import Entity, Principal, _Password
+from .communities import Community, Everyone
+
+from .entity import Entity
+
+from .users import Device, Principal, _Password
 from .users import _FriendsListMap, _DevicesMap, _TranscriptsMap
 
 from .users import onChange
 from .users import user_devicefeedback
 from .users import get_shared_dataserver as _get_shared_dataserver
 
-from nti.dataserver.activitystream_change import Change
-
 # BWC re-exports
-from .friends_lists import FriendsList, DynamicFriendsList, _FriendsListUsernameIterable
+from .friends_lists import _FriendsListUsernameIterable
+from .friends_lists import FriendsList, DynamicFriendsList
