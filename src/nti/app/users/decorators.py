2b13eac1ad1130ffc5307971404eeea120ef7219
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-04 23:18:26 +0000

    add community link decorator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@66587 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/users/decorators.py
+++ src/nti/app/users/decorators.py
@@ -15,9 +15,11 @@ from zope import component
 from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
 
 from nti.dataserver.interfaces import IUser
+from nti.dataserver.interfaces import ICommunity
 from nti.dataserver.interfaces import IDataserverFolder
 
 from nti.dataserver.users.interfaces import IUserProfile
+from nti.dataserver.users.interfaces import IHiddenMembership
 
 from nti.externalization.interfaces import StandardExternalFields
 from nti.externalization.interfaces import IExternalMappingDecorator
@@ -51,4 +53,32 @@ class _UserEmailVerificationLinkDecorator(AbstractAuthenticatedRequestAwareDecor
 		ds2 = find_interface(context, IDataserverFolder)
 		link = Link(ds2, rel="VerifyEmailWithToken", method='POST',
 					elements=('@@' + VERIFY_USER_EMAIL_WITH_TOKEN_VIEW,))
+		_links.append(link)
+
+@component.adapter(ICommunity)
+@interface.implementer(IExternalMappingDecorator)
+class _CommunityLinkDecorator(AbstractAuthenticatedRequestAwareDecorator):
+
+	def _predicate(self, context, result):
+		result = bool(self._is_authenticated)
+		return result
+
+	def _do_decorate_external(self, context, result):
+		_links = result.setdefault(LINKS, [])
+		in_community = self.remoteUser in context
+		if context.joinable:
+			if not in_community:
+				link = Link(context, rel="join")
+			else:
+				link = Link(context, rel="leave")
+			_links.append(link)
+		
+		if context.public or in_community:
+			link = Link(context, rel="members")
+			_links.append(link)
+
+		if self.remoteUser in IHiddenMembership(context):
+			link = Link(context, rel="unhide")
+		else:
+			link = Link(context, rel="hide")
 		_links.append(link)
