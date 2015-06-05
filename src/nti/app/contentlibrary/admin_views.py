933eca338032aa260bd1ef7777efcb229fc03fdb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-05 20:17:24 +0000

    use a simple redis lock for library sync
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@66683 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/admin_views.py
+++ src/nti/app/contentlibrary/admin_views.py
@@ -11,9 +11,10 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-
 import time
 from six import string_types
+
+from zope import component
 
 from zope.security.management import endInteraction
 from zope.security.management import restoreInteraction
@@ -24,9 +25,12 @@ from nti.app.base.abstract_views import AbstractAuthenticatedView
 from nti.app.externalization.internalization import read_body_as_external_object
 from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
 
+from nti.common.property import Lazy
 from nti.common.maps import CaseInsensitiveDict
 
+from nti.dataserver.interfaces import IRedisClient
 from nti.dataserver.interfaces import IDataserverFolder
+
 from nti.dataserver.authorization import ACT_NTI_ADMIN
 
 from nti.externalization.interfaces import LocatedExternalDict
@@ -36,7 +40,7 @@ from .synchronize import synchronize
 
 ITEMS = StandardExternalFields.ITEMS
 
-@view_config( route_name='objects.generic.traversal',
+@view_config(route_name='objects.generic.traversal',
 			  renderer='rest',
 			  context=IDataserverFolder,
 			  permission=ACT_NTI_ADMIN,
@@ -56,14 +60,20 @@ class _SyncAllLibrariesView(AbstractAuthenticatedView,
 		from there.
 	"""
 
-	#: Because we'll be doing a lot of filesystem IO, which may not
-	#: be well cooperatively tasked (gevent), we would like to give
-	#: the opportunity for other greenlets to run by sleeping inbetween
-	#: syncing each library. However, for some reason, under unittests,
-	#: this leads to very odd and unexpected test failures
-	#: (specifically in nti.app.products.courseware) so we allow
-	#: disabling it.
+	# : Because we'll be doing a lot of filesystem IO, which may not
+	# : be well cooperatively tasked (gevent), we would like to give
+	# : the opportunity for other greenlets to run by sleeping inbetween
+	# : syncing each library. However, for some reason, under unittests,
+	# : this leads to very odd and unexpected test failures
+	# : (specifically in nti.app.products.courseware) so we allow
+	# : disabling it.
 	_SLEEP = True
+
+	# : The amount of time for which we will hold the lock during
+	# : sync
+	lock_timeout = 60 * 30  # 30 minutes
+
+	lock_name = '/var/libraries/Lock/sync'
 
 	def readInput(self, value=None):
 		result = CaseInsensitiveDict()
@@ -74,31 +84,44 @@ class _SyncAllLibrariesView(AbstractAuthenticatedView,
 				values = self.request.params
 			result.update(values)
 		return result
-	
-	def __call__(self):
+
+	@Lazy
+	def redis(self):
+		return component.getUtility(IRedisClient)
+
+	@Lazy
+	def lock(self):
+		return self.redis.lock(self.lock_name, self.lock_timeout)
+
+	def _do_call(self):
 		values = self.readInput()
 		site = values.get('site')
 		packages = values.get('packages') or values.get('package') or ()
 		packages = set(packages.split()) if isinstance(packages, string_types) else packages
-		
-		## Unfortunately, zope.dublincore includes a global subscriber registration
-		## (zope.dublincore.creatorannotator.CreatorAnnotator)
-		## that will update the `creators` property of IZopeDublinCore to include
-		## the current principal when any ObjectCreated /or/ ObjectModified event
-		## is fired, if there is a current interaction. Normally we want this,
-		## but here we care specifically about getting the dublincore metadata
-		## we specifically defined in the libraries, and not the requesting principal.
-		## Our simple-minded approach is to simply void the interaction during this process
-		## (which works so long as zope.securitypolicy doesn't get involved...)
-		## This is somewhat difficult to test the side-effects of, sadly.
+
+		# Unfortunately, zope.dublincore includes a global subscriber registration
+		# (zope.dublincore.creatorannotator.CreatorAnnotator)
+		# that will update the `creators` property of IZopeDublinCore to include
+		# the current principal when any ObjectCreated /or/ ObjectModified event
+		# is fired, if there is a current interaction. Normally we want this,
+		# but here we care specifically about getting the dublincore metadata
+		# we specifically defined in the libraries, and not the requesting principal.
+		# Our simple-minded approach is to simply void the interaction during this process
+		# (which works so long as zope.securitypolicy doesn't get involved...)
+		# This is somewhat difficult to test the side-effects of, sadly.
 		now = time.time()
 		endInteraction()
 		try:
 			result = LocatedExternalDict()
-			result[ITEMS] = synchronize(sleep=self._SLEEP, 
+			result['Started'] = now
+			result[ITEMS] = synchronize(sleep=self._SLEEP,
 										site=site,
 										packages=packages)
 			result['Elapsed'] = time.time() - now
 			return result
 		finally:
 			restoreInteraction()
+
+	def __call__(self):
+		with self.lock:
+			return self._do_call()
