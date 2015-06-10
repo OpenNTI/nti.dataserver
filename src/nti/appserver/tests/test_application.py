2b57e82c90fa09667a00ca80f29e8d4b31b4e704
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-10 01:02:14 +0000

    fix test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@66875 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/test_application.py
+++ src/nti/appserver/tests/test_application.py
@@ -1114,13 +1114,13 @@ class TestApplication(ApplicationLayerTest):
 			username = user.username
 			user_ext_id = to_external_ntiid_oid( user )
 
-		res = testapp.get('/dataserver2/users/' + username + '/@@avatar', extra_environ=self._make_extra_environ(),
-						  status=302)
-		assert_that( res.location, is_('https://secure.gravatar.com/avatar/31f94302764fc0b184fd0c2e96e4084f?s=128&d=identicon') )
+		testapp.get('/dataserver2/users/' + username + '/@@avatar', extra_environ=self._make_extra_environ(),
+					status=404)
+		# assert_that(res.location,
+		#			  is_('https://secure.gravatar.com/avatar/31f94302764fc0b184fd0c2e96e4084f?s=128&d=identicon') )
 
 		res = self._edit_user_ext_field( 'avatarURL', data, username, user_ext_id )
 		assert_that( res.json_body, has_entry( 'avatarURL', starts_with( '/dataserver2/' ) ) )
-
 
 		res = testapp.get( res.json_body['avatarURL'], extra_environ=self._make_extra_environ() )
 		assert_that( res.content_type, is_( 'image/gif' ) )
