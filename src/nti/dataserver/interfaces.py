f3f7826cfe67e234afd72bfa8d6e60b452f69bec
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-11 18:15:50 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@67007 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/interfaces.py
+++ src/nti/dataserver/interfaces.py
@@ -9,9 +9,7 @@ Dataserver interfaces
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-import zope.i18nmessageid
-MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
-_ = MessageFactory
+from . import MessageFactory as _
 
 import six
 import sys
