65e693f4493ad28185e0de25ed732d63207ae0f7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-11 15:26:18 +0000

    add registry method helper
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@66987 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary/indexed_data/__init__.py
+++ src/nti/contentlibrary/indexed_data/__init__.py
@@ -19,34 +19,46 @@ import time
 
 from zope import component
 
+from zope.annotation.interfaces import IAnnotations
+
+from ..interfaces import IContentPackageLibrary
+from ..interfaces import IGlobalContentPackageLibrary
+
 from .interfaces import TAG_NAMESPACE_FILE
 from .interfaces import IContainedObjectCatalog
 
-from zope.annotation.interfaces import IAnnotations
-
 CATALOG_INDEX_NAME = '++etc++contentlibrary.container_index'
 
-_KEY =  'nti.contentlibrary.indexed_data.LastModified'
+BASE_KEY = 'nti.contentlibrary.indexed_data.LastModified'
 
 def get_catalog():
-	result = component.queryUtility( IContainedObjectCatalog, name=CATALOG_INDEX_NAME )
+	result = component.queryUtility(IContainedObjectCatalog, name=CATALOG_INDEX_NAME)
 	return result
 get_index = get_catalog
 
-def _get_key( index_iface ):
-	key = _KEY
-	namespace = index_iface.queryTaggedValue( TAG_NAMESPACE_FILE,'' )
+def _get_key(index_iface):
+	key = BASE_KEY
+	namespace = index_iface.queryTaggedValue(TAG_NAMESPACE_FILE, '')
 	if namespace:
 		key = key + '_' + namespace
 	return key
 
-def get_index_last_modified( index_iface, unit ):
-	key = _get_key( index_iface )
-	annotes = IAnnotations( unit )
-	return annotes.get( key )
+def get_index_last_modified(index_iface, unit):
+	key = _get_key(index_iface)
+	annotes = IAnnotations(unit)
+	return annotes.get(key)
 
-def set_index_last_modified( index_iface, unit, now=None ):
+def set_index_last_modified(index_iface, unit, now=None):
 	now = time.time() if now is None else now
-	key = _get_key( index_iface )
-	annotes = IAnnotations( unit )
+	key = _get_key(index_iface)
+	annotes = IAnnotations(unit)
 	annotes[key] = now
+
+def registry(registry=None):
+	if registry is None:
+		library = component.queryUtility(IContentPackageLibrary)
+		if IGlobalContentPackageLibrary.providedBy(library):
+			registry = component.getGlobalSiteManager()
+		else:
+			registry = component.getSiteManager()
+	return registry
