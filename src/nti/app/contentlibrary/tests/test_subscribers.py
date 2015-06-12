0ded9d52427787bca61a2203c70b6352f5cdf959
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-12 16:03:18 +0000

    move tests to proper place
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@67080 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/tests/test_subscribers.py
+++ src/nti/app/contentlibrary/tests/test_subscribers.py
@@ -6,37 +6,60 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-import os
-import fudge
-
 from hamcrest import has_length
 from hamcrest import assert_that
 from hamcrest import contains_inanyorder
+
+import os
+import fudge
 
 from zope import component
 
 from zope.component.persistentregistry import PersistentComponents as Components
 
+from zope.intid import IIntIds
+
 from persistent import Persistent
 
 from nti.app.contentlibrary.subscribers import _clear_when_removed
+from nti.app.contentlibrary.subscribers import _remove_from_registry
+from nti.app.contentlibrary.subscribers import _load_and_register_json
+from nti.app.contentlibrary.subscribers import _load_and_register_slidedeck_json
 
 from nti.contentlibrary.indexed_data import get_catalog
-
 from nti.contentlibrary.interfaces import IContentPackageLibrary
-
+from nti.contenttypes.presentation import ALL_PRESENTATION_ASSETS_INTERFACES
 from nti.contentlibrary.filesystem import EnumerateOnceFilesystemLibrary as FileLibrary
 
 from nti.contenttypes.presentation.interfaces import INTIVideo
+from nti.contenttypes.presentation.interfaces import INTISlide
 from nti.contenttypes.presentation.interfaces import INTITimeline
 from nti.contenttypes.presentation.interfaces import INTISlideDeck
+from nti.contenttypes.presentation.interfaces import INTISlideVideo
 from nti.contenttypes.presentation.interfaces import INTIRelatedWorkRef
+
+from nti.contenttypes.presentation.utils import create_object_from_external
+from nti.contenttypes.presentation.utils import create_timelime_from_external
+from nti.contenttypes.presentation.utils import create_ntivideo_from_external
+from nti.contenttypes.presentation.utils import create_relatedwork_from_external
 
 from nti.contentlibrary.tests import ContentlibraryLayerTest
 
 import nti.dataserver.tests.mock_dataserver as mock_dataserver
 from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
 
+def iface_of_thing(item):
+	for iface in ALL_PRESENTATION_ASSETS_INTERFACES:
+		if iface.providedBy(item):
+			return iface
+	return None
+
+def _index_items(item_iface, namespace, *registered):
+	catalog = get_catalog()
+	intids = component.queryUtility(IIntIds)
+	for item in registered:
+		catalog.index(item, intids=intids, namespace=namespace)
+		
 class PersistentComponents(Components, Persistent):
 	pass
 
@@ -129,3 +152,61 @@ class TestSubscribers(ContentlibraryLayerTest):
 		self.library.syncContentPackages()
 		results = catalog.search_objects(container_ntiids=(unit_ntiid,))
 		assert_that(results, has_length(0))
+
+	def _test_feed(self, source, iface, count, object_creator=create_object_from_external):
+		path = os.path.join(os.path.dirname(__file__), source)
+		with open(path, "r") as fp:
+			source = fp.read()
+			
+		registry = PersistentComponents()
+		mock_dataserver.current_transaction.add(registry)
+
+		result = _load_and_register_json(iface, source, registry=registry,
+										 external_object_creator=object_creator)
+		assert_that(result, has_length(count))
+		assert_that(list(registry.registeredUtilities()), has_length(count))
+
+		_index_items(iface, 'xxx', *result)
+		
+		result = _remove_from_registry(namespace='xxx', provided=iface, registry=registry)
+		assert_that(result, has_length(count))
+
+	@WithMockDSTrans
+	def test_video_index(self):
+		self._test_feed('video_index.json', INTIVideo, 94,
+						create_ntivideo_from_external)
+
+	@WithMockDSTrans
+	def test_timeline_index(self):
+		self._test_feed('timeline_index.json', INTITimeline, 11,
+						create_timelime_from_external)
+
+	@WithMockDSTrans
+	def test_related_content_index(self):
+		self._test_feed('related_content_index.json', INTIRelatedWorkRef, 372,
+						create_relatedwork_from_external)
+	
+	@WithMockDSTrans
+	def test_slidedeck_index(self):
+		path = os.path.join(os.path.dirname(__file__), 'slidedeck_index.json')
+		with open(path, "r") as fp:
+			source = fp.read()
+			
+		registry = PersistentComponents()
+		mock_dataserver.current_transaction.add(registry)
+		
+		result = _load_and_register_slidedeck_json(source, registry=registry)
+		assert_that(result, has_length(742))
+		
+		for item in result:
+			iface = iface_of_thing(item)
+			_index_items(iface, 'xxx', item)
+		
+		result = _remove_from_registry(namespace='xxx', provided=INTISlideDeck, registry=registry)
+		assert_that(result, has_length(57))
+		
+		result = _remove_from_registry(namespace='xxx', provided=INTISlideVideo, registry=registry)
+		assert_that(result, has_length(57))
+		
+		result = _remove_from_registry(namespace='xxx', provided=INTISlide, registry=registry)
+		assert_that(result, has_length(628))
