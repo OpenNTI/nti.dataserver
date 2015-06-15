e45db6c4d4bf55d1d020711710e52d35100e2b66
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-15 15:20:54 +0000

    fix alpha errors w/ regard to video index container
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@67191 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/relevant_ugd_views.py
+++ src/nti/appserver/relevant_ugd_views.py
@@ -3,8 +3,9 @@
 """
 Views for relevant UGD
 
-$Id$
+.. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
@@ -12,35 +13,37 @@ logger = __import__('logging').getLogger(__name__)
 
 from itertools import chain
 
-from zope import interface
 from zope import component
+from zope import interface
 
 from pyramid import httpexceptions as _hexc
 
 from nti.app.renderers.interfaces import IUGDExternalCollection
 
 from nti.appserver import httpexceptions as hexc
-from nti.appserver import ugd_query_views as query_views
+from nti.appserver.ugd_query_views import Operator
+from nti.appserver.ugd_query_views import _UGDView
+from nti.appserver.ugd_query_views import _combine_predicate
 
 from nti.assessment.interfaces import IQAssessmentItemContainer
 
-from nti.contentlibrary import interfaces as lib_interfaces
+from nti.contentlibrary.interfaces import IContentPackageLibrary
 from nti.contentlibrary.indexed_data.interfaces import IVideoIndexedDataContainer
 
-from nti.externalization import interfaces as ext_interfaces
+from nti.externalization.interfaces import StandardExternalFields
 
 from nti.mimetype.mimetype import nti_mimetype_with_class
 
 from .ugd_query_views import lists_and_dicts_to_ext_collection
 
-LAST_MODIFIED = ext_interfaces.StandardExternalFields.LAST_MODIFIED
+LAST_MODIFIED = StandardExternalFields.LAST_MODIFIED
 
-union_operator = query_views.Operator.union
-intersection_operator = query_views.Operator.intersection
+union_operator = Operator.union
+intersection_operator = Operator.intersection
 
-class _RelevantUGDView(query_views._UGDView):
+class _RelevantUGDView(_UGDView):
 
-	def _make_accept_predicate( self ):
+	def _make_accept_predicate(self):
 		accept_types = ('application/vnd.nextthought.redaction',)
 		return self._MIME_FILTER_FACTORY(accept_types)
 
@@ -55,9 +58,7 @@ class _RelevantUGDView(query_views._UGDView):
 			the_filter = self.FILTER_NAMES[filter_name]
 			if isinstance(the_filter, tuple):
 				the_filter = the_filter[0](self.request)
-			predicate = query_views._combine_predicate(the_filter,
-													   predicate,
-													   operator=operator)
+			predicate = _combine_predicate(the_filter, predicate, operator=operator)
 
 		# things shared w/ me and are top level
 		top_level = self.FILTER_NAMES[top_level_filter]
@@ -65,10 +66,7 @@ class _RelevantUGDView(query_views._UGDView):
 			x_sharedWith = getattr(x, 'sharedWith', ())
 			if self.user.username in x_sharedWith and top_level(x):
 				return True
-		predicate = query_views._combine_predicate(filter_shared_with,
-												   predicate,
-												   operator=operator)
-
+		predicate = _combine_predicate(filter_shared_with, predicate, operator=operator)
 		return predicate
 
 	def getObjectsForId(self, user, ntiid):
@@ -80,7 +78,7 @@ class _RelevantUGDView(query_views._UGDView):
 
 	def _get_items(self, ntiid):
 		# query objects
-		view = query_views._UGDView(self.request)
+		view = _UGDView(self.request)
 		view._DEFAULT_BATCH_SIZE = view._DEFAULT_BATCH_START = None
 		try:
 			results = view.getObjectsForId(self.user, ntiid)
@@ -89,14 +87,13 @@ class _RelevantUGDView(query_views._UGDView):
 		return results
 
 	def _get_library_path(self, ntiid):
-		library = component.getUtility(lib_interfaces.IContentPackageLibrary)
+		library = component.getUtility(IContentPackageLibrary)
 		paths = library.pathToNTIID(ntiid) if library else None
 		return paths[-1] if paths else None
 
 	def _scan_quizzes(self, ntiid):
-		library = component.getUtility(lib_interfaces.IContentPackageLibrary)
-		# quizzes are often subcontainers, so we look at the parent
-		# and its children
+		library = component.getUtility(IContentPackageLibrary)
+		# quizzes are often subcontainers, so we look at the parent and its children
 		ntiids = set()
 		results = []
 		for unit in library.childrenOfNTIID(ntiid) + [self._get_library_path(ntiid)]:
@@ -106,18 +103,18 @@ class _RelevantUGDView(query_views._UGDView):
 					ntiids.add(q_ntiid)
 					items = self._get_items(q_ntiid)
 					if items:
-						results.extend( items )
+						results.extend(items)
 		return results
 
 	def _scan_videos(self, ntiid):
 		unit = self._get_library_path(ntiid)
 		results = []
 		if unit is not None:
-			for video_data in IVideoIndexedDataContainer(unit).get_data_items():
+			for video_data in IVideoIndexedDataContainer(unit).values():
 				video_id = video_data['ntiid']
 				items = self._get_items(video_id)
 				if items:
-					results.extend( items )
+					results.extend(items)
 		return results
 
 	def __call__(self):
@@ -127,10 +124,10 @@ class _RelevantUGDView(query_views._UGDView):
 		video_items = self._scan_videos(self.ntiid)
 
 		predicate = self._make_complete_predicate()
-		all_items = chain( items, quiz_items, video_items )
+		all_items = chain(items, quiz_items, video_items)
 		# De-dupe; we could batch here if needed.
-		result = lists_and_dicts_to_ext_collection( all_items, predicate )
-		result['Total'] = len( result.get( 'Items', () ) )
+		result = lists_and_dicts_to_ext_collection(all_items, predicate)
+		result['Total'] = len(result.get('Items', ()))
 		result.mimeType = nti_mimetype_with_class(None)
 		interface.alsoProvides(result, IUGDExternalCollection)
 		return result
