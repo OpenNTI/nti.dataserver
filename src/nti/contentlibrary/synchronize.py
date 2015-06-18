4193deecfea8bf5d1880db4c5c70d2ff29d1f6bc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-18 00:42:44 +0000

    whoops :p
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@67440 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary/synchronize.py
+++ src/nti/contentlibrary/synchronize.py
@@ -5,7 +5,6 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
-from _ast import Add
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -74,4 +73,4 @@ class SynchronizationResults(SchemaConfigured, Contained):
 		self.Items = [] if self.Items is None else self.Items
 		item.__parent__ = self
 		self.Items.append(item)
-	append = Add
+	append = add
