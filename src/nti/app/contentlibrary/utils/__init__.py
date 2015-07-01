0efa596e67746b39cb434a95d481e88e30077cfa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-01 17:34:22 +0000

    module could not be read b/c of pkg
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@68081 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/utils/__init__.py
+++ src/nti/app/contentlibrary/utils/__init__.py
@@ -8,3 +8,30 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+
+from nti.contentlibrary.indexed_data import get_catalog
+
+from nti.contentlibrary.interfaces import IContentUnit
+from nti.contentlibrary.interfaces import IContentPackageLibrary
+
+from nti.ntiids.ntiids import find_object_with_ntiid
+
+def get_content_units(ntiids=()):
+	result = []
+	library = component.queryUtility(IContentPackageLibrary)
+	if library is not None:
+		for ntiid in ntiids or ():
+			context = find_object_with_ntiid(ntiid)
+			if not IContentUnit.providedBy(context):
+				context = IContentUnit(context, None)
+			if context is not None:
+				result.append(context)
+		return result
+
+def get_item_content_units(item, sort=False):
+	catalog = get_catalog()
+	entries = catalog.get_containers(item)
+	result = get_content_units(entries) if entries else ()
+	return result
