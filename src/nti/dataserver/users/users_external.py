0e250f9eab2aed13778dad7346150f0ccce744c5
Josh Zuech <josh.zuech@nextthought.com>
2015-07-03 03:49:33 +0000

    Return new profile fields to other users
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@68215 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/users/users_external.py
+++ src/nti/dataserver/users/users_external.py
@@ -263,7 +263,9 @@ class _UserSummaryExternalObject(_EntitySummaryExternalObject):
 	# Even in summary (i.e. to other people), we want to publish all these fields
 	# because it looks better
 	public_summary_profile_fields = ('affiliation', 'home_page', 'description',
-									 'location', 'role', 'about')
+									 'location', 'role', 'about', 'twitter',
+									 'facebook', 'googlePlus', 'linkedin',
+									 'education', 'positions' )
 
 	# These could probably be put as tags on the interface fields, but the number of
 	# profile interfaces in use makes that a chore. At the moment, this is the simpler option
