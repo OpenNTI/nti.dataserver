65c32b8a9c27a823b77e51fdfa552924989c0257
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-13 11:59:20 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@68842 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/decorators.py
+++ src/nti/app/contentlibrary/decorators.py
@@ -124,7 +124,7 @@ class _UGDLibraryPathLinkDecorator(object):
 	def decorateExternalMapping(self, context, result):
 		container_id = context.containerId
 		container = find_object_with_ntiid(container_id)
-		# TODO This what we want, or just use containerId?
+		# TODO: This what we want, or just use containerId?
 		external_ntiid = to_external_ntiid_oid(container) if container else None
 		if external_ntiid is not None:
 			path = '/dataserver2/%s' % LIBRARY_PATH_GET_VIEW
