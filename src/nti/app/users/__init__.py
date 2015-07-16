5c5e77a3abf0ec2e22038cee191718701f8b8eac
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-16 18:27:17 +0000

    refactor; and view to find 'ghost' containers
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@69042 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/users/__init__.py
+++ src/nti/app/users/__init__.py
@@ -14,6 +14,11 @@ MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
 
 import six
 
+from zope import component
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IShardLayout
+
 REL_MY_MEMBERSHIP = 'my_membership'
 SUGGESTED_CONTACTS = 'SuggestedContacts'
 VERIFY_USER_EMAIL_VIEW = "verify_user_email"
@@ -24,3 +29,21 @@ VERIFY_USER_EMAIL_WITH_TOKEN_VIEW = "verify_user_email_with_token"
 def is_true(value):
 	value = value if isinstance(value, six.string_types) else str(value)
 	return value.lower() in ('1', 'y', 'yes', 't', 'true')
+
+def _make_min_max_btree_range(search_term):
+	min_inclusive = search_term  # start here
+	max_exclusive = search_term[0:-1] + unichr(ord(search_term[-1]) + 1)
+	return min_inclusive, max_exclusive
+
+def username_search(search_term):
+	min_inclusive, max_exclusive = _make_min_max_btree_range(search_term)
+	dataserver = component.getUtility(IDataserver)
+	_users = IShardLayout(dataserver).users_folder
+	usernames = list(_users.iterkeys(min_inclusive, max_exclusive, excludemax=True))
+	return usernames
+
+def all_usernames():
+	dataserver = component.getUtility(IDataserver)
+	users_folder = IShardLayout(dataserver).users_folder
+	usernames = users_folder.keys()
+	return usernames
--- src/nti/app/users/export_views.py
+++ src/nti/app/users/export_views.py
@@ -12,10 +12,8 @@ logger = __import__('logging').getLogger(__name__)
 import zope.intid
 
 from zope import component
-from zope.catalog.interfaces import ICatalog
 
-from ZODB.interfaces import IBroken
-from ZODB.POSException import POSError
+from zope.catalog.interfaces import ICatalog
 
 from pyramid.view import view_config
 from pyramid import httpexceptions as hexc
@@ -28,8 +26,6 @@ from nti.chatserver.interfaces import IUserTranscriptStorage
 from nti.common.maps import CaseInsensitiveDict
 
 from nti.dataserver.interfaces import IUser
-from nti.dataserver.interfaces import IDataserver
-from nti.dataserver.interfaces import IShardLayout
 from nti.dataserver.interfaces import IDataserverFolder
 from nti.dataserver.interfaces import IDeletedObjectPlaceholder
 
@@ -47,12 +43,16 @@ from nti.externalization.interfaces import StandardExternalFields
 
 from nti.ntiids.ntiids import find_object_with_ntiid
 
+from nti.zodb import isBroken
+
 from . import is_true
+from . import all_usernames
+from . import username_search
 
 ITEMS = StandardExternalFields.ITEMS
 
 transcript_mime_type = u'application/vnd.nextthought.transcript'
-messageinfo_mime_type =  u'application/vnd.nextthought.messageinfo'
+messageinfo_mime_type = u'application/vnd.nextthought.messageinfo'
 
 def parse_mime_types(value):
 	mime_types = set(value.split(',')) if value else ()
@@ -63,26 +63,14 @@ def parse_mime_types(value):
 		mime_types.discard(u'')
 	return tuple(mime_types) if mime_types else ()
 
-def _make_min_max_btree_range(search_term):
-	min_inclusive = search_term  # start here
-	max_exclusive = search_term[0:-1] + unichr(ord(search_term[-1]) + 1)
-	return min_inclusive, max_exclusive
-
-def username_search(search_term):
-	min_inclusive, max_exclusive = _make_min_max_btree_range(search_term)
-	dataserver = component.getUtility(IDataserver)
-	_users = IShardLayout(dataserver).users_folder
-	usernames = list(_users.iterkeys(min_inclusive, max_exclusive, excludemax=True))
-	return usernames
-
 def get_user_objects(user, mime_types=()):
 	intids = component.getUtility(zope.intid.IIntIds)
 	catalog = component.getUtility(ICatalog, METADATA_CATALOG_NAME)
-	
+
 	result_ids = None
 	created_ids = None
 	mime_types_intids = None
-	
+
 	username = user.username
 	process_transcripts = False
 
@@ -90,52 +78,52 @@ def get_user_objects(user, mime_types=()):
 		mime_types = set(mime_types)
 		process_transcripts = \
 				bool(transcript_mime_type in mime_types or
-				     messageinfo_mime_type in mime_types)
+					 messageinfo_mime_type in mime_types)
 		if process_transcripts:
 			mime_types.discard(transcript_mime_type)
 			mime_types.discard(messageinfo_mime_type)
-	
+
 		if mime_types:
 			mime_types_intids = catalog[IX_MIMETYPE].apply({'any_of': tuple(mime_types)})
 		else:
-			created_ids = () # mark so we don't query the catalog
+			created_ids = ()  # mark so we don't query the catalog
 	else:
 		process_transcripts = True
-		
+
 	if created_ids is None:
 		created_ids = catalog[IX_CREATOR].apply({'any_of': (username,)})
-		
+
 	if mime_types_intids is None:
 		result_ids = created_ids
 	elif created_ids:
 		result_ids = catalog.family.IF.intersection(created_ids, mime_types_intids)
-			
+
 	for uid in result_ids or ():
 		try:
 			obj = intids.queryObject(uid)
 			if	obj is None or \
+				isBroken(obj, uid) or \
 				IUser.providedBy(obj) or \
-				IBroken.providedBy(obj) or \
 				IDeletedObjectPlaceholder.providedBy(obj):
 				continue
 			if process_transcripts and IMessageInfo.providedBy(obj):
 				continue
 			yield obj
-		except (TypeError, POSError) as e:
+		except TypeError as e:
 			logger.debug("Error processing object %s(%s); %s", type(obj), uid, e)
 
 	if process_transcripts:
 		storage = IUserTranscriptStorage(user)
 		for transcript in storage.transcripts:
 			yield transcript
-		
+
 @view_config(route_name='objects.generic.traversal',
 			 name='export_user_objects',
 			 request_method='GET',
 			 context=IDataserverFolder,
 			 permission=nauth.ACT_NTI_ADMIN)
 class ExportUserObjectsView(AbstractAuthenticatedView):
-	
+
 	def __call__(self):
 		request = self.request
 		values = CaseInsensitiveDict(**request.params)
@@ -147,10 +135,10 @@ class ExportUserObjectsView(AbstractAuthenticatedView):
 			usernames = usernames.split(",")
 		else:
 			usernames = ()
-	
+
 		mime_types = values.get('mime_types') or values.get('mimeTypes') or u''
 		mime_types = parse_mime_types(mime_types)
-	
+
 		total = 0
 		result = LocatedExternalDict()
 		items = result[ITEMS] = {}
@@ -171,7 +159,7 @@ class ExportUserObjectsView(AbstractAuthenticatedView):
 			 context=IDataserverFolder,
 			 permission=nauth.ACT_NTI_ADMIN)
 class ExportObjectsSharedWithView(AbstractAuthenticatedView):
-	
+
 	def __call__(self):
 		request = self.request
 		params = CaseInsensitiveDict(**request.params)
@@ -182,34 +170,34 @@ class ExportObjectsSharedWithView(AbstractAuthenticatedView):
 
 		intids = component.getUtility(zope.intid.IIntIds)
 		catalog = component.getUtility(ICatalog, METADATA_CATALOG_NAME)
-		
+
 		sharedWith_ids = catalog[IX_SHAREDWITH].apply({'any_of': (username,)})
-		
+
 		mime_types = params.get('mime_types') or params.get('mimeTypes') or u''
 		mime_types = parse_mime_types(mime_types)
 		if mime_types:
 			mime_types_intids = catalog[IX_MIMETYPE].apply({'any_of': mime_types})
 		else:
 			mime_types_intids = None
-		
+
 		result_ids = None
 		if mime_types_intids is None:
 			result_ids = sharedWith_ids
 		else:
 			result_ids = catalog.family.IF.intersection(sharedWith_ids,
 														mime_types_intids)
-			
+
 		result = LocatedExternalDict()
 		items = result[ITEMS] = []
 		for uid in result_ids:
 			try:
 				obj = intids.queryObject(uid)
 				if	obj is not None and \
-					not IBroken.providedBy(obj) and \
+					not isBroken(obj, uid) and \
 					not IUser.providedBy(obj) and \
 					not IDeletedObjectPlaceholder.providedBy(obj):
 					items.append(obj)
-			except (TypeError, POSError) as e:
+			except TypeError as e:
 				logger.debug("Error processing object %s(%s); %s", type(obj), uid, e)
 		result['Total'] = len(items)
 		return result
@@ -227,7 +215,7 @@ class ObjectResolverView(AbstractAuthenticatedView):
 		ntiid = request.subpath[0] if request.subpath else ''
 		if not ntiid:
 			raise hexc.HTTPUnprocessableEntity("Must specify a ntiid")
-		
+
 		result = find_object_with_ntiid(ntiid)
 		if result is None:
 			raise hexc.HTTPNotFound()
@@ -240,7 +228,7 @@ class ObjectResolverView(AbstractAuthenticatedView):
 			 context=IDataserverFolder,
 			 permission=nauth.ACT_NTI_ADMIN)
 class ExportUsersView(AbstractAuthenticatedView):
-	
+
 	def __call__(self):
 		request = self.request
 		values = CaseInsensitiveDict(**request.params)
@@ -252,10 +240,8 @@ class ExportUsersView(AbstractAuthenticatedView):
 		elif usernames:
 			usernames = usernames.split(',')
 		else:
-			dataserver = component.getUtility(IDataserver)
-			users_folder = IShardLayout(dataserver).users_folder
-			usernames = users_folder.keys()
-	
+			usernames = all_usernames()
+
 		result = LocatedExternalDict()
 		items = result[ITEMS] = {}
 		for username in usernames:
