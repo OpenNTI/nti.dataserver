a49e98f181b3514f6062ac59a3f7789d6727833d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-19 18:20:21 +0000

    Capture ValueError is iterable is empty
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@69206 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/authentication/tests/test_who_basicauth.py
+++ src/nti/app/authentication/tests/test_who_basicauth.py
@@ -1,40 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-
-
-.. $Id$
-"""
 
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
-#disable: accessing protected members, too many methods
-#pylint: disable=W0212,R0904
+from hamcrest import is_
+from hamcrest import equal_to
+from hamcrest import has_key
+from hamcrest import assert_that
+from hamcrest import is_not as does_not
 
 import unittest
-from hamcrest import assert_that
-from hamcrest import is_
-from hamcrest import has_key
-from hamcrest import is_not as does_not
+
+from repoze.who.interfaces import IChallenger
+
+from nti.app.authentication.who_basicauth import ApplicationBasicAuthPlugin
 
 from nti.testing.matchers import validly_provides
 
-from ..who_basicauth import ApplicationBasicAuthPlugin
-from repoze.who.interfaces import IChallenger
-
 class TestBasicAuth(unittest.TestCase):
+
 	def test_non_challenging_challenge(self):
 
 		challenger = ApplicationBasicAuthPlugin('nti')
-		assert_that( challenger, validly_provides(IChallenger) )
+		assert_that(challenger, validly_provides(IChallenger))
 
 		# Challenging produces as 401, but without a WWW-Authenticate header
-		unauth = challenger.challenge( {}, '401', {}, [] )
-		assert_that( unauth.headers, does_not( has_key( 'WWW-Authenticate' ) ) )
-		assert_that( unauth.headers, has_key( 'Content-Type' ) )
+		unauth = challenger.challenge({}, '401', {}, [])
+		assert_that(unauth.headers, does_not(has_key('WWW-Authenticate')))
+		assert_that(unauth.headers, has_key('Content-Type'))
 
 		# forgetting adds no headers
-		assert_that( challenger.forget( {}, {} ), is_( () ) )
+		result = challenger.forget({}, {})
+		assert_that(result, is_(equal_to(())))
