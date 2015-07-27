b09716838992712ddc5b9f331e3aaaaac6950d53
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-27 16:09:28 +0000

    minor opt for searching
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@69573 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentsearch/views.py
+++ src/nti/app/contentsearch/views.py
@@ -11,6 +11,7 @@ logger = __import__('logging').getLogger(__name__)
 
 import time
 
+from zope import component
 from zope import interface
 
 from zope.event import notify
@@ -29,6 +30,7 @@ from nti.app.externalization.internalization import update_object_from_external_
 
 from nti.app.renderers.interfaces import IUncacheableInResponse
 
+from nti.common.property import Lazy
 from nti.common.property import CachedProperty
 
 from nti.contentsearch.interfaces import IIndexManager
@@ -37,6 +39,7 @@ from nti.contentsearch.interfaces import SearchCompletedEvent
 from nti.contentsearch.search_utils import create_queryobject
 
 from nti.dataserver.users import Entity
+from nti.dataserver.interfaces import IMemcacheClient
 
 from nti.externalization.internalization import find_factory_for
 from nti.externalization.interfaces import StandardExternalFields
@@ -105,12 +108,11 @@ class BaseSearchView(BaseView, BatchingUtilsMixin):
 		result = self.indexmanager.search(query=query, store=store)
 		return result
 
-	def search(self, *queries):
+	def search(self, query):
 		result = None
 		now = time.time()
 		# execute search
-		for query in queries: # TODO: Handle multiple queries
-			result = self._do_search(query=query, store=result)
+		result = self._do_search(query=query, store=result)
 		
 		# page if required	
 		batch_size, batch_start = self._get_batch_size_start()
@@ -138,6 +140,67 @@ class BaseSearchView(BaseView, BatchingUtilsMixin):
 
 class SearchView(BaseSearchView):
 	name = 'Search'
+
+	max_cache_time = 60
+	max_cache_size = 100
+	use_memcache = True
+	
+	DATA_BASE_KEY = "/search/%s/results/data"
+	QUERY_BASE_KEY = "/search/%s/results/query"
+	
+	@Lazy
+	def memcache(self):
+		client = component.queryUtility(IMemcacheClient) if self.use_memcache else None
+		return client
+
+	@Lazy
+	def _data_key(self):
+		return self.DATA_BASE_KEY % self.remoteUser.username
+	
+	@Lazy
+	def _query_key(self):
+		return self.QUERY_BASE_KEY % self.remoteUser.username
+
+	def _check_memcache(self, query):
+		result = None
+		client = self.memcache
+		if client is None:
+			return result
+		try:
+			data = client.get(self._query_key)
+			from IPython.core.debugger import Tracer; Tracer()()
+			if data:
+				stored_query, stamp = data
+				if 	stored_query.digest() == query.digest() and \
+					time.time() - stamp < self.max_cache_time:
+					result = client.get(self._data_key)
+				else:
+					client.delete(self._data_key)
+					client.delete(self._query_key)
+		except Exception as e:
+			logger.error("Cannot get search result from Memcache. %s", e)
+			result = None
+		return result
+
+	def _store_memcache(self, query, results):
+		client = self.memcache
+		if client is None:
+			return
+		try:
+			data = (query, time.time())
+			client.set(self._query_key, data)
+			client.set(self._data_key, results)
+		except Exception as e:
+			logger.error("Cannot store results in Memcache. %s", e)
+
+	def _do_search(self, query, store=None):
+		result = self._check_memcache(query)
+		if result is None:
+			result = self.indexmanager.search(query=query, store=store)
+			if len(result) >= self.max_cache_size:
+				self._store_memcache(query, result)
+		return result
+
 Search = SearchView  # BWC
 
 class UserDataSearchView(BaseSearchView):
