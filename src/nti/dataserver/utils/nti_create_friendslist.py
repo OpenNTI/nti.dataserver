fb882eb5c50d932806b105596db53e9baaaf96d4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-08-05 15:06:31 +0000

    remove unused option
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@69994 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/nti_create_friendslist.py
+++ src/nti/dataserver/utils/nti_create_friendslist.py
@@ -9,6 +9,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import os
 import sys
 import pprint
 import argparse
@@ -29,7 +30,6 @@ from .base_script import set_site
 def create_friends_list(owner, username, realname=None, members=(), 
 						dynamic=False, locked=False):
 
-	# set factory
 	factory = DynamicFriendsList if dynamic else FriendsList
 	result = factory(username=unicode(username))
 	result.creator = owner
@@ -55,8 +55,7 @@ def _process_args(args):
 		sys.exit( 2 )
 
 	site = args.site
-	if site:
-		set_site(site)
+	set_site(site)
 
 	result = create_friends_list(owner, args.username, args.name, args.members,
 								 args.dynamic, args.locked)
@@ -69,33 +68,37 @@ def main():
 	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
 							dest='verbose')
 	arg_parser.add_argument('owner', help="The username of the owner")
+	
 	arg_parser.add_argument('username', help="The username of the new DFL")
-	arg_parser.add_argument('--env_dir', help="Dataserver environment root directory")
+	
 	arg_parser.add_argument('-n', '--name',
 							dest='name',
 							help="The display name of the list")
+	
 	arg_parser.add_argument('--dynamic',
 							help="Create a Dynamic FriendsList",
 							action='store_true',
 							dest='dynamic')
+	
 	arg_parser.add_argument('--locked',
 							help="Lock the DFL. Only valid used with --dynamic",
 							action='store_true',
 							dest='locked')
+	
 	arg_parser.add_argument('--site',
 							dest='site',
 							help="Request SITE.")
+	
 	arg_parser.add_argument('-m', '--members',
 							dest='members',
 							nargs="+",
 							help="The usernames of the entities to add")
 	args = arg_parser.parse_args()
 
-	import os
-	
-	env_dir = args.env_dir
+	env_dir = os.getenv('DATASERVER_DIR')
 	if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
-		raise ValueError("Invalid dataserver environment root directory", env_dir)
+		print("Invalid dataserver environment root directory", env_dir)
+		sys.exit(2)
 	
 	run_with_dataserver( environment_dir=env_dir,
 						 verbose=args.verbose,
