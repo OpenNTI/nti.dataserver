0d40d0d0087fa4dd18d0a578af07a2d32ade1bbc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-08-16 19:45:59 +0000

    clean up;white space
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@70669 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/_hacks.py
+++ src/nti/appserver/_hacks.py
@@ -15,13 +15,16 @@ from nti.appserver import httpexceptions as hexc
 from nti.appserver.ugd_edit_views import UGDPutView
 
 from nti.dataserver import authorization as nauth
-from nti.dataserver import interfaces as nti_interfaces
 
-@view_config( route_name='objects.generic.traversal',
-			  renderer='rest',
-			  context=nti_interfaces.IFriendsList,
-			  name='++fields++friends',
-			  permission=nauth.ACT_UPDATE, request_method='PUT' )
+from nti.dataserver.interfaces import IFriendsList
+from nti.dataserver.interfaces import IDataserverFolder
+
+@view_config(route_name='objects.generic.traversal',
+			 renderer='rest',
+			 context=IFriendsList,
+			 name='++fields++friends',
+			 permission=nauth.ACT_UPDATE, 
+			 request_method='PUT')
 class _FriendsListsFriendsFieldUpdateView(UGDPutView):
 	"""
 	This is a temporary fast hack to enable updating friends list objects
@@ -34,18 +37,18 @@ class _FriendsListsFriendsFieldUpdateView(UGDPutView):
 	# TODO: Can this go away now?
 	inputClass = list
 
-	def _get_object_to_update( self ):
+	def _get_object_to_update(self):
 		return self.request.context
 
-	def _transformInput( self, externalValue ):
+	def _transformInput(self, externalValue):
 		return {"friends": externalValue}
 
-@view_config( route_name='objects.generic.traversal',
-			  context=nti_interfaces.IDataserverFolder,
-			  permission=nauth.ACT_READ, # anyone logged in...
-			  request_method='GET',
-			  request_param='image_url',
-			  name="echo_image_url")
+@view_config(route_name='objects.generic.traversal',
+			 context=IDataserverFolder,
+			 permission=nauth.ACT_READ,  # anyone logged in...
+			 request_method='GET',
+			 request_param='image_url',
+			 name="echo_image_url")
 def echo_image_url(request):
 	"""
 	Temporary hack to circumvent cross-domain problems
@@ -66,22 +69,23 @@ def echo_image_url(request):
 	# external traffic and we'll pay data transfer charges.
 
 	try:
-		image_response = requests.get( request.params['image_url'], stream=True ) # stream=False: Download whole body
+		# stream=False: Download whole body
+		image_response = requests.get(request.params['image_url'], stream=True)
 		image_response.raise_for_status()
 	except requests.exceptions.HTTPError as e:
 		request.response.status_code = e.response.status_code
 		request.response.body = e.args[0]
 		return request.response
 	except requests.exceptions.RequestException as e:
-		return hexc.HTTPBadRequest( *e.args )
+		return hexc.HTTPBadRequest(*e.args)
 
 	if not image_response.headers.get('content-type', '').lower().startswith('image/'):
 		return hexc.HTTPNotFound()
 
-	for header_name_to_copy in (b'Content-Type', b'ETag', b'Last-Modified','Content-Length'):
-		header_value = image_response.headers.get( header_name_to_copy )
+	for header_name_to_copy in (b'Content-Type', b'ETag', b'Last-Modified', 'Content-Length'):
+		header_value = image_response.headers.get(header_name_to_copy)
 		if header_value:
 			request.response.headers[header_name_to_copy] = header_value
 
-	request.response.app_iter = image_response.iter_content( chunk_size=256 )
+	request.response.app_iter = image_response.iter_content(chunk_size=256)
 	return request.response
--- src/nti/appserver/_stream_event_listeners.py
+++ src/nti/appserver/_stream_event_listeners.py
@@ -13,7 +13,9 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 from zope import interface
+
 from zope.event import notify
+
 from zope.publisher.interfaces.browser import IBrowserRequest
 
 from ZODB import loglevels
@@ -36,16 +38,16 @@ from nti.dataserver.users.interfaces import IUserProfile
 from ._email_utils import queue_simple_html_text_email
 from .interfaces import IChangePresentationDetails
 
-@component.adapter( IUser, IStreamChangeEvent )
-def user_change_broadcaster( user, change ):
+@component.adapter(IUser, IStreamChangeEvent)
+def user_change_broadcaster(user, change):
 	"""
 	Notifies the chat server of data change events so they can be
 	put on the user's socket.
 	"""
 	if change.send_change_notice:
-		logger.log( loglevels.TRACE, 'Broadcasting incoming change to %s chg: %s', 
+		logger.log(	loglevels.TRACE, 'Broadcasting incoming change to %s chg: %s',
 					user.username, change.type)
-		notify( DataChangedUserNotificationEvent( (user.username,), change ) )
+		notify(DataChangedUserNotificationEvent((user.username,), change))
 
 class ITemporaryChangeEmailMarker(interface.Interface):
 	"Temporary marker interface, assigned using configuration to a user who wants to get change emails."
@@ -57,8 +59,8 @@ class TemporaryChangeEmailMarker(object):
 class NewNoteBodyTable(table.SequenceTable):
 	pass
 
-@component.adapter( IUser, IStreamChangeEvent )
-def user_change_new_note_emailer( user, change ):
+@component.adapter(IUser, IStreamChangeEvent)
+def user_change_new_note_emailer(user, change):
 	"""
 	For incoming shared objects that are notes being freshly created,
 	attempts to email them
@@ -72,23 +74,23 @@ def user_change_new_note_emailer( user, change ):
 	# above objects that we want to send notices about
 	# (IModeledContent works for notes and posts, but not
 	# topics themselves).
-	#if not nti_interfaces.IModeledContent.providedBy( change_object ):
-	#	return
+	# if not nti_interfaces.IModeledContent.providedBy( change_object ):
+	# 	return
 	# So we drive this off whether we can get a
 	# presentation
-	change_presentation_details = component.queryMultiAdapter( (change_object, change),
-															   IChangePresentationDetails )
+	change_presentation_details = component.queryMultiAdapter((change_object, change),
+															   IChangePresentationDetails)
 	if change_presentation_details is None:
 		return
 
-	profile = IUserProfile( user )
-	email = getattr( profile, 'email', None )
-	opt_in = getattr( profile, 'opt_in_email_communication', True )
+	profile = IUserProfile(user)
+	email = getattr(profile, 'email', None)
+	opt_in = getattr(profile, 'opt_in_email_communication', True)
 
 	if not email or not opt_in:
-		logger.log( loglevels.TRACE,
+		logger.log(	loglevels.TRACE,
 					"User %s has no email (%s) or hasn't opted in (%s), no way to send notices",
-					user, email, opt_in )
+					user, email, opt_in)
 		return
 
 	# Ok, now here's the tricky part. Until we have preferences or something about who/how to send
@@ -99,22 +101,22 @@ def user_change_new_note_emailer( user, change ):
 	# sites
 	# TODO: Convert this to z3c.baseregistry?
 	request = get_current_request()
-	names = [user.username] + list(get_possible_site_names( request=request, include_default=True ))
+	names = [user.username] + list(get_possible_site_names(request=request, include_default=True))
 	send = False
 	for name in names:
-		if component.queryUtility( ITemporaryChangeEmailMarker, name=name ):
+		if component.queryUtility(ITemporaryChangeEmailMarker, name=name):
 			send = True
-			logger.debug( "Sending change email to %s due to configuration at %r", user, name )
+			logger.debug("Sending change email to %s due to configuration at %r", user, name)
 			break
 	if send:
 		base_template = 'note_created_email'
 
-		the_table = NewNoteBodyTable( [change_object],
-									  IBrowserRequest( request ) if request else None )
+		the_table = NewNoteBodyTable([change_object],
+									  IBrowserRequest(request) if request else None)
 
 		the_table.update()
 
-		note_text = [component.queryAdapter( x, IPlainTextContentFragment, name='text', default='' ) 
+		note_text = [component.queryAdapter(x, IPlainTextContentFragment, name='text', default='')
 					 for x in change_object.body]
 
 		queue_simple_html_text_email(
@@ -125,6 +127,6 @@ def user_change_new_note_emailer( user, change ):
 						   'change': change,
 						   'user': user,
 						   'note_table': the_table,
-						   'note_text': '\n'.join( note_text ),
+						   'note_text': '\n'.join(note_text),
 						   'profile': profile},
-			request=request	)
+			request=request)
