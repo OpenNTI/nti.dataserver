4c4a334437c95dc0cd508bd8a1068197e049b895
Josh Zuech <josh.zuech@nextthought.com>
2015-08-16 21:05:13 +0000

    Ignore DynamicMemberships when updating user object
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@70672 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/application.py
+++ src/nti/appserver/application.py
@@ -369,7 +369,7 @@ def _modifying_ugd_views(pyramid_config):
 							permission=nauth.ACT_UPDATE, request_method='PUT')
 
 	# And the user itself can be put to
-	pyramid_config.add_view(route_name='objects.generic.traversal', view='nti.appserver.ugd_edit_views.UGDPutView',
+	pyramid_config.add_view(route_name='objects.generic.traversal', view='nti.app.users.user_views.UserUpdateView',
 							renderer='rest', context=nti_interfaces.IUser,
 							permission=nauth.ACT_UPDATE, request_method='PUT')
 
