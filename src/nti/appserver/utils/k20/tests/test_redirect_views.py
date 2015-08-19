1fdc8d7e24f24402c2c5ce84d4ee037113140a56
Josh Zuech <josh.zuech@nextthought.com>
2015-08-19 16:22:33 +0000

    Use opaquek20 token that can retrieve user
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@70979 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/utils/k20/tests/test_redirect_views.py
+++ src/nti/appserver/utils/k20/tests/test_redirect_views.py
@@ -7,6 +7,9 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
+from hamcrest import assert_that
+
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 from nti.app.testing.application_webtest import ApplicationLayerTest
 
@@ -18,10 +21,16 @@ class TestK20Redirect(ApplicationLayerTest):
 	@WithSharedApplicationMockDS(users=True,testapp=True,default_authenticate=True)
 	def test_link(self):
 
+		# Redirect link works
 		link_val = str( 'http://k20.blah.course/Calculus' )
 		params = { K20_LINK_PARAM_NAME : link_val }
 
 		link_url = '/dataserver2/' + K20_VIEW_NAME
-		self.testapp.get( link_url,
+		res = self.testapp.get( link_url,
 							params=params,
 							status=302 )
+
+		# Now get user from token
+		user_id = res.location.split( 'token=' )[1]
+		user = self.testapp.get( '/dataserver2/Objects/%s' % user_id )
+		assert_that( user.json_body.get( 'Username' ), is_( 'sjohnson@nextthought.com'))
