6b12d2a4cdee768c053dc3e2ee39db1babbca7e9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-08-29 02:38:05 +0000

    bump zc.zodbdgc; remove monkey patches per JAM input
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@71985 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -34,8 +34,6 @@ entry_points = {
 		"nti_pserve = nti.appserver.nti_pserve:main",
 		"nti_runzeo = nti.monkey.nti_runzeo:main",
 		"nti_zodbconvert = nti.monkey.nti_zodbconvert:main",
-		"nti_multi-zodb-gc = nti.monkey.nti_multi_zodb_gc:main",
-		"nti_multi-zodb-check-refs = nti.monkey.nti_multi_zodb_check_refs:main",
 		"nti_qp = nti.mailer.queue:run_console",
 		# XXX: NOTE: The following technique is NOT reliable and fails
 		# under buildout or any other scenario that results in this package
