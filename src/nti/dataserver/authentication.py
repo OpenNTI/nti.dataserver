1bc1bfe518cc6093c9b04fea7d9f6e8038743707
Josh Zuech <josh.zuech@nextthought.com>
2015-09-28 15:54:15 +0000

    Add community username principals to effective principals; fixes tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@73717 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/authentication.py
+++ src/nti/dataserver/authentication.py
@@ -38,6 +38,13 @@ def _dynamic_memberships_that_participate_in_security(user, as_principals=True):
 			(ICommunity.providedBy(community) and \
 			 not IUnscopedGlobalCommunity.providedBy(community)):
 			yield IPrincipal(community) if as_principals else community
+			# This is a bit of a hack. Now that we store our principals in a
+			# set. We need hashes to collide when either an id or an NTIID
+			# match. So we return our community principal and the principal
+			# for our community username. This might only apply to test
+			# .nti-acl files, which I don't think are being used anywhere.
+			if as_principals:
+				yield IPrincipal(community.username)
 	# This mimics the sharing target's xxx_intids_of_memberships_and_self
 	# which is used as an ACL optimization
 
