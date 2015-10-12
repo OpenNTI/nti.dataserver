05e662cd3892cf9a5b8db5c532e7cbc00cd06f2c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-12 17:43:21 +0000

    use a registered utility to find an entity in an invitation
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@74555 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/invitations/invitation.py
+++ src/nti/app/invitations/invitation.py
@@ -9,47 +9,17 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import interface
+
 from nti.dataserver.users import Entity
-from nti.dataserver.interfaces import ICommunity
-from nti.dataserver.interfaces import IFriendsList
-from nti.dataserver.interfaces import SYSTEM_USER_NAME
+from nti.dataserver.interfaces import IEntity
 
-from nti.invitations.invitation import ZcmlInvitation
+from nti.invitations.interfaces import IInvitationEntityFinder
 
-class JoinEntitiesInvitation(ZcmlInvitation):
-    """
-    Simple first pass at a pre-configured invitation to join existing
-    entities. Intended to be configured with ZCML and not stored persistently.
-    """
-
-    creator = SYSTEM_USER_NAME
-
-    def __init__(self, code, entities):
-        super(JoinEntitiesInvitation, self).__init__()
-        self.code = code
-        self.entities = entities
-
-    def _iter_entities(self):
-        for entity_name in self.entities:
-            entity = Entity.get_entity(entity_name)
-            if entity is None:
-                logger.warn("Unable to accept invitation to join non-existent entity %s",
-                             entity_name)
-                continue
-            yield entity
-
-    def accept(self, user):
-        for entity in self._iter_entities():
-            if ICommunity.providedBy(entity):
-                logger.info("Accepting invitation to join community %s", entity)
-                user.record_dynamic_membership(entity)
-                user.follow(entity)
-            elif IFriendsList.providedBy(entity):
-                logger.info("Accepting invitation to join DFL %s", entity)
-                entity.addFriend(user)
-            else:
-                logger.warn("Don't know how to accept invitation to join entity %s",
-                            entity)
-        super(JoinEntitiesInvitation, self).accept(user)
-
-JoinCommunityInvitation = JoinEntitiesInvitation
+@interface.implementer(IInvitationEntityFinder)
+class InvitationEntityFinder(object):
+    
+    def find(self, entity):
+        if entity is not None and not IEntity.providedBy(entity):
+            return Entity.get_entity(str(entity))
+        return entity
