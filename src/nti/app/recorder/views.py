9c73b5a7dfd7f063fd95ec2129a4f285eef64b42
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-15 20:57:12 +0000

    Start adding helper views for recordables
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@74825 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/recorder/views.py
+++ src/nti/app/recorder/views.py
@@ -8,3 +8,47 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.coremetadata.interfaces import IRecordable
+
+from nti.recorder import get_transactions
+from nti.recorder import remove_transaction_history
+
+from nti.dataserver.authorization import ACT_NTI_ADMIN
+
+from nti.externalization.interfaces import LocatedExternalDict
+from nti.externalization.interfaces import StandardExternalFields
+
+ITEMS = StandardExternalFields.ITEMS
+
+@view_config(permission=ACT_NTI_ADMIN)
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   request_method='POST',
+			   context=IRecordable,
+			   name='RemoveTransactionHistory')
+class RemoveTransactionHistoryView(AbstractAuthenticatedView):
+
+	def __call__(self):
+		remove_transaction_history(self.context)
+		return hexc.HTTPNoContent()
+
+@view_config(permission=ACT_NTI_ADMIN)
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   request_method='POST',
+			   context=IRecordable,
+			   name='GetTransactionHistory')
+class GetTransactionHistoryView(AbstractAuthenticatedView):
+
+	def __call__(self):
+		result = LocatedExternalDict()
+		result[ITEMS] = get_transactions(self.context)
+		return result
