f2196071b463fa61f50244b2a63a758976a73bb7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-20 21:00:29 +0000

    add [untested] script
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@75133 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/subscribers.py
+++ src/nti/app/contentlibrary/subscribers.py
@@ -257,7 +257,8 @@ def _index_items(content_package, index, item_iface, removed, catalog, registry)
 	return result
 
 def _update_index_when_content_changes(content_package, index_filename,
-									   item_iface, object_creator, catalog=None):
+									   item_iface, object_creator, catalog=None,
+									   force=False):
 	catalog = get_library_catalog() if catalog is None else catalog
 	sibling_key = content_package.does_sibling_entry_exist(index_filename)
 	if not sibling_key:
@@ -268,7 +269,7 @@ def _update_index_when_content_changes(content_package, index_filename,
 		sk_lastModified = sibling_key.lastModified
 		last_mod_namespace = _get_file_last_mod_namespace(content_package, index_filename)
 		last_modified = catalog.get_last_modified(last_mod_namespace)
-		if last_modified and last_modified >= sk_lastModified:
+		if not force and last_modified and last_modified >= sk_lastModified:
 			logger.info("No change to %s since %s, ignoring",
 						sibling_key,
 						sk_lastModified)
@@ -350,10 +351,13 @@ def _clear_assets(content_package):
 			container.clear()
 	recur(content_package)
 
-def _update_indices_when_content_changes(content_package, event):
+def update_indices_when_content_changes(content_package, force=False):
 	_clear_assets(content_package)
 	for name, item_iface, func in INDICES:
-		_update_index_when_content_changes(content_package, name, item_iface, func)
+		_update_index_when_content_changes(content_package, name, item_iface, func, force=force)
+
+def _update_indices_when_content_changes(content_package, event):
+	update_indices_when_content_changes(content_package)
 
 def _clear_when_removed(content_package):
 	"""
