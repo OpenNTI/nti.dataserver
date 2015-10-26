b2fa7b98128b62412d4bb0d4892fffde189b1ee7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-26 16:54:23 +0000

    add decorator for recordable objects
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@75445 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/recorder/decorators.py
+++ src/nti/app/recorder/decorators.py
@@ -14,6 +14,12 @@ from zope import interface
 
 from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
 
+from nti.appserver.pyramid_authorization import has_permission
+
+from nti.coremetadata.interfaces import IRecordable
+
+from nti.dataserver.authorization import ACT_UPDATE
+
 from nti.externalization.externalization import to_external_object
 from nti.externalization.interfaces import IExternalMappingDecorator
 
@@ -27,3 +33,14 @@ class _TransactionRecordDecorator(AbstractAuthenticatedRequestAwareDecorator):
 		target = context.__parent__
 		if target is not None:
 			result['Target'] = to_external_object(target)
+
+@component.adapter(IRecordable)
+@interface.implementer(IExternalMappingDecorator)
+class _RecordableDecorator(AbstractAuthenticatedRequestAwareDecorator):
+
+	def _predicate(self, context, result):
+		return 	bool(self.authenticated_userid) and \
+				has_permission(ACT_UPDATE, context, self.request)
+
+	def _do_decorate_external(self, context, result):
+		result['locked'] = context.locked
