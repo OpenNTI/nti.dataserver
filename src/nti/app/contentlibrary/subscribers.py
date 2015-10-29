73e65c79362aac6006be139122862db122a375cc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-29 18:35:33 +0000

    minor opt
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@75738 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/subscribers.py
+++ src/nti/app/contentlibrary/subscribers.py
@@ -160,8 +160,7 @@ def _load_and_register_slidedeck_json(jtext, registry=None, connection=None,
 
 def _can_be_removed(registered, force=False):
 	result = registered is not None and \
-			 (	force or
-			 	(not IRecordable.providedBy(registered) or not registered.locked) )
+			 (force or not IRecordable.providedBy(registered) or not registered.locked)
 	return result
 can_be_removed = _can_be_removed
 
