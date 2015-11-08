03c2a2d9fc95abcb4b2e8dc3c15533b143303931
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-11-08 18:05:25 +0000

    site index does not make sense on metadata catalog
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@76550 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/install.py
+++ src/nti/dataserver/generations/install.py
@@ -10,7 +10,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 71
+generation = 72
 
 # Allow going forward/backward for testing
 import os
