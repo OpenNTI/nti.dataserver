a0b9ee3dcaac329857ef42585a11ed44c5d6f02a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-01-06 01:01:18 +0000

    add test; set props; move externalization to app
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@79932 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/extractors/views.py
+++ src/nti/app/extractors/views.py
@@ -11,10 +11,12 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import gevent
+
 from zope import interface
 
+from pyramid.view import view_config
 from pyramid import httpexceptions as hexc
-from pyramid.view import view_config, view_defaults
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
@@ -25,16 +27,12 @@ from nti.contentprocessing.metadata_extractors import get_metadata_from_http_url
 from nti.dataserver import authorization as nauth
 from nti.dataserver.interfaces import IDataserverFolder
 
-import gevent
-
-
 @view_config(route_name='objects.generic.traversal',
 			 renderer='rest',
+			 request_method='GET',
 			 context=IDataserverFolder,
 			 name='URLMetaDataExtractor',
-			 #Global content admins only for now, can open up to authed users
-			 permission=nauth.ACT_CONTENT_EDIT,
-			 request_method='GET')
+			 permission=nauth.ACT_CONTENT_EDIT)
 class _URLMetaDataExtractor(AbstractAuthenticatedView):
 
 	def __call__(self):
@@ -44,10 +42,10 @@ class _URLMetaDataExtractor(AbstractAuthenticatedView):
 
 		result = None
 
-		#We connect to an external service so guard ourselves with a timeout
+		# We connect to an external service so guard ourselves with a timeout
 		with gevent.Timeout(3, hexc.HTTPGatewayTimeout):
 			try:
-				#Make sure people can't query the filesystem through the api
+				# Make sure people can't query the filesystem through the api
 				result = get_metadata_from_http_url(url)
 			except ValueError:
 				raise hexc.HTTPUnprocessableEntity('Invalid URL')
