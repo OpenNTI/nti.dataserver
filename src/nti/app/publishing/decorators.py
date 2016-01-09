0a1209379169ba7ef096f2c6ab89c53e6d22139e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-01-09 20:31:56 +0000

    check for acl_decoration in request
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@80356 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/publishing/decorators.py
+++ src/nti/app/publishing/decorators.py
@@ -37,11 +37,16 @@ from nti.links.links import Link
 
 LINKS = StandardExternalFields.LINKS
 
-def _expose_links( context, request ):
-	return 	getattr(context, '_p_jar', None) \
- 		and has_permission(ACT_CONTENT_EDIT, context, request)
+def _acl_decoration(request):
+	result = getattr(request, 'acl_decoration', True)
+	return result
+	
+def _expose_links(context, request):
+	return 		_acl_decoration(request) \
+			and	getattr(context, '_p_jar', None) \
+			and has_permission(ACT_CONTENT_EDIT, context, request)
 
-def _get_publish_state( obj ):
+def _get_publish_state(obj):
 	return 'DefaultPublished' if obj.is_published() else None
 
 @interface.implementer(IExternalMappingDecorator)
@@ -65,16 +70,15 @@ class PublishLinkDecorator(AbstractTwoStateViewLinkDecorator):
 
 	def _do_decorate_external_link(self, context, mapping, extra_elements=()):
 		# ICalendarPublishables have their own publish link decorator.
-		if 		_expose_links( context, self.request ) \
-			and not ICalendarPublishable.providedBy( context ):
+		if 		_expose_links(context, self.request) \
+			and not ICalendarPublishable.providedBy(context):
 			super(PublishLinkDecorator, self)._do_decorate_external_link(context, mapping)
 
 	def _do_decorate_external(self, context, mapping):
 		super(PublishLinkDecorator, self)._do_decorate_external(context, mapping)
 		# Everyone gets the status
 		if 'PublicationState' not in mapping:
-			mapping['PublicationState'] = _get_publish_state( context )
-
+			mapping['PublicationState'] = _get_publish_state(context)
 
 @interface.implementer(IExternalMappingDecorator)
 class CalendarPublishStateDecorator(object):
@@ -88,7 +92,7 @@ class CalendarPublishStateDecorator(object):
 
 	def decorateExternalMapping(self, context, result):
 		request = get_current_request()
-		if _expose_links( context, request ):
+		if _expose_links(context, request):
 			_links = result.setdefault(LINKS, [])
 			for rel in (VIEW_PUBLISH, VIEW_UNPUBLISH):
 				el = '@@%s' % rel
@@ -97,6 +101,6 @@ class CalendarPublishStateDecorator(object):
 				link.__name__ = ''
 				link.__parent__ = context
 				_links.append(link)
-		result['PublicationState'] = _get_publish_state( context )
+		result['PublicationState'] = _get_publish_state(context)
 		result['publishBeginning'] = context.publishBeginning
 		result['publishEnding'] = context.publishEnding
