e148a55b6864b106c901874ab29d8ee027570018
Josh Zuech <josh.zuech@nextthought.com>
2016-01-25 17:22:46 +0000

    Revert
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@81665 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/tests/test_application_hacks.py
+++ src/nti/appserver/tests/test_application_hacks.py
@@ -7,7 +7,9 @@ from __future__ import print_function
 from hamcrest import assert_that
 from hamcrest import is_
 
-from .test_application import TestApp
+from nti.appserver.tests.test_application import TestApp
+
+from nti.dataserver.tests import mock_dataserver
 
 from nti.app.testing.application_webtest import ApplicationLayerTest
 from nti.app.testing.decorators import WithSharedApplicationMockDS
@@ -21,17 +23,23 @@ class TestApplicationLiveImageUrl(ApplicationLayerTest):
 
 	@WithSharedApplicationMockDS
 	def test_echo_bad_param(self):
+		with mock_dataserver.mock_db_trans( self.ds ):
+			self._create_user()
+
 		testapp = TestApp( self.app )
 
 		path = '/dataserver2/@@echo_image_url?image_url=foo'
 		environ = self._make_extra_environ()
 		testapp.get( path,
-					extra_environ=environ,
-					status=400)
+					 extra_environ=environ,
+				   	 status=400)
 
 
 	@WithSharedApplicationMockDS
 	def test_echo_correct_image(self):
+		with mock_dataserver.mock_db_trans( self.ds ):
+			self._create_user()
+
 		testapp = TestApp( self.app )
 
 		path = '/dataserver2/@@echo_image_url?image_url=http://www.python.org/images/python-logo.gif'
@@ -44,6 +52,9 @@ class TestApplicationLiveImageUrl(ApplicationLayerTest):
 
 	@WithSharedApplicationMockDS
 	def test_echo_404(self):
+		with mock_dataserver.mock_db_trans( self.ds ):
+			self._create_user()
+
 		testapp = TestApp( self.app )
 
 		path = '/dataserver2/@@echo_image_url?image_url=http://www.python.org/images/python-logo.gif.missing'
@@ -54,6 +65,9 @@ class TestApplicationLiveImageUrl(ApplicationLayerTest):
 
 	@WithSharedApplicationMockDS
 	def test_echo_wrong_target_type(self):
+		with mock_dataserver.mock_db_trans( self.ds ):
+			self._create_user()
+
 		testapp = TestApp( self.app )
 
 		path = '/dataserver2/@@echo_image_url?image_url=http://www.python.org/index.html'
