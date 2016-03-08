401972dce0da03ef1660814a2b78557d8b38ef95
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-03-08 23:31:03 +0000

    prepare work for zcml course invitiations
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@84265 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/invitations/invitation.py
+++ src/nti/app/invitations/invitation.py
@@ -9,13 +9,24 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import component
 from zope import interface
+
+from zope.intid.interfaces import IIntIds
 
 from nti.dataserver.interfaces import IEntity
 
 from nti.dataserver.users import Entity
 
+from nti.dataserver.interfaces import IDynamicSharingTargetFriendsList
+
+from nti.externalization import integer_strings
+
 from nti.invitations.interfaces import IInvitationEntityFinder
+
+from nti.invitations.invitation import JoinEntitiesInvitation
+
+from nti.invitations.utility import ZcmlInvitations
 
 @interface.implementer(IInvitationEntityFinder)
 class InvitationEntityFinder(object):
@@ -24,3 +35,42 @@ class InvitationEntityFinder(object):
         if entity is not None and not IEntity.providedBy(entity):
             return Entity.get_entity(str(entity))
         return entity
+
+#: To work better with the ZcmlInvitations, and until we need
+#: configured persistent invitations (e.g., user-editable)
+#: we synthesize the default invitation. It's tied directly to the
+#: intid of the actual object we will be joining. Note that as soon
+#: as we go persistent, these codes will probably be invalidated.
+#: We only generate invitations to IDynamicSharingTargetFriendsList objects, even
+#: though in theory this could work for Communities and regular FL too; this is
+#: alright because the accept view and the link provider are also tied to the
+#: IDynamicSharingTargetFriendsList
+
+class _DefaultJoinEntityInvitation(JoinEntitiesInvitation):
+
+    def _iter_entities(self):
+        yield self.entities
+
+class _TrivialDefaultInvitations(ZcmlInvitations):
+
+    def _getDefaultInvitationCode(self, dfl):
+        iid = component.getUtility(IIntIds).getId(dfl)
+        return integer_strings.to_external_string(iid)
+
+    def _getObjectByCode(self, code):
+        iid = integer_strings.from_external_string(code)
+        result = component.getUtility(IIntIds).getObject(iid)
+        return result
+
+    def getInvitationByCode(self, code):
+        __traceback_info__ = code,
+        invite = super(_TrivialDefaultInvitations, self).getInvitationByCode(code)
+        if invite is None:
+            try:
+                dfl = self._getObjectByCode(code)
+                if IDynamicSharingTargetFriendsList.providedBy(dfl):
+                    invite = _DefaultJoinEntityInvitation(code, dfl)
+                    invite.creator = dfl.creator
+            except (KeyError, ValueError, AttributeError):
+                return None
+        return invite
--- src/nti/app/invitations/views.py
+++ src/nti/app/invitations/views.py
@@ -14,8 +14,6 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
-from zope.intid.interfaces import IIntIds
-
 from pyramid import httpexceptions as hexc
 
 from pyramid.interfaces import IRequest
@@ -35,16 +33,12 @@ from nti.dataserver import authorization as nauth
 from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import IDynamicSharingTargetFriendsList
 
-from nti.externalization import integer_strings
 from nti.externalization.interfaces import LocatedExternalDict
 from nti.externalization.interfaces import IExternalMappingDecorator
 
 from nti.invitations.interfaces import IInvitations
 from nti.invitations.interfaces import InvitationValidationError
 
-from nti.invitations.invitation import JoinEntitiesInvitation
-
-from nti.invitations.utility import ZcmlInvitations
 from nti.invitations.utility import accept_invitations
 
 #: The link relationship type to which an authenticated
@@ -90,45 +84,6 @@ def accept_invitations_view(request):
 		handle_possible_validation_error(request, e)
 
 	return hexc.HTTPNoContent()
-
-#: To work better with the ZcmlInvitations, and until we need
-#: configured persistent invitations (e.g., user-editable)
-#: we synthesize the default invitation. It's tied directly to the
-#: intid of the actual object we will be joining. Note that as soon
-#: as we go persistent, these codes will probably be invalidated.
-#: We only generate invitations to IDynamicSharingTargetFriendsList objects, even
-#: though in theory this could work for Communities and regular FL too; this is
-#: alright because the accept view and the link provider are also tied to the
-#: IDynamicSharingTargetFriendsList
-
-class _DefaultJoinEntityInvitation(JoinEntitiesInvitation):
-
-	def _iter_entities(self):
-		yield self.entities
-
-class _TrivialDefaultInvitations(ZcmlInvitations):
-
-	def _getDefaultInvitationCode(self, dfl):
-		iid = component.getUtility(IIntIds).getId(dfl)
-		return integer_strings.to_external_string(iid)
-
-	def _getObjectByCode(self, code):
-		iid = integer_strings.from_external_string(code)
-		result = component.getUtility(IIntIds).getObject(iid)
-		return result
-
-	def getInvitationByCode(self, code):
-		__traceback_info__ = code,
-		invite = super(_TrivialDefaultInvitations, self).getInvitationByCode(code)
-		if invite is None:
-			try:
-				dfl = self._getObjectByCode(code)
-				if IDynamicSharingTargetFriendsList.providedBy(dfl):
-					invite = _DefaultJoinEntityInvitation(code, dfl)
-					invite.creator = dfl.creator
-			except (KeyError, ValueError, AttributeError):
-				return None
-		return invite
 
 @view_config(route_name='objects.generic.traversal',
 			 renderer='rest',
