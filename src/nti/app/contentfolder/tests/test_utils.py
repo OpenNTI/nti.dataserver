9e43b3473be0c0f6518b3bf490d31086ff3b47fa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-03-29 18:54:13 +0000

    add utils unit test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@85480 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentfolder/tests/test_utils.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import is_not
+from hamcrest import assert_that
+does_not = is_not
+
+import fudge
+import unittest
+
+from zope import interface
+
+from plone.namedfile.interfaces import INamed
+
+from nti.app.contentfolder.utils import get_file_from_cf_io_url
+
+class TestUtils(unittest.TestCase):
+
+	@fudge.patch('nti.app.contentfolder.utils.get_object')
+	def test_get_file_from_cf_io_url(self, mock_fon):
+		class Foo(object):
+			pass
+		foo = Foo()
+		interface.alsoProvides(foo, INamed)
+		mock_fon.is_callable().with_args().returns(foo)
+
+		n = get_file_from_cf_io_url('/dataserver2/Objects/tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323')
+		assert_that(n, is_(none()))
+
+		n = get_file_from_cf_io_url('/dataserver2/cf.io/xrPH9')
+		assert_that(n, is_(foo))
+
+		n = get_file_from_cf_io_url('/dataserver2/cf.io/xrPH9/sample.dat')
+		assert_that(n, is_(foo))
--- src/nti/app/contentfolder/utils.py
+++ src/nti/app/contentfolder/utils.py
@@ -9,6 +9,8 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import re
+from urllib import unquote
 from urlparse import urljoin
 
 from zope import component
@@ -17,11 +19,16 @@ from zope.intid.interfaces import IIntIds
 
 from pyramid.threadlocal import get_current_request
 
+from plone.namedfile.interfaces import INamed as IPloneNamed
+
 from nti.app.contentfolder import CFIO
 
 from nti.contentfile.interfaces import IContentBaseFile
 
 from nti.externalization.integer_strings import to_external_string
+from nti.externalization.integer_strings import from_external_string
+
+pattern = re.compile('(.+)/%s/(.+)(\/.*)?' % CFIO, re.UNICODE | re.IGNORECASE)
 
 def get_ds2(request=None):
 	request = request if request else get_current_request()
@@ -30,6 +37,9 @@ def get_ds2(request=None):
 	except AttributeError:  # in unit test we may see this
 		result = None
 	return result or "dataserver2"
+
+def is_cf_io_href(link):
+	return bool(pattern.match(unquote(link)))
 
 def get_cf_io_href(context, request=None):
 	context = IContentBaseFile(context, None)
@@ -46,4 +56,26 @@ def get_cf_io_url(context, request=None):
 	request = request if request else get_current_request()
 	href = get_cf_io_href(context, request=request)
 	result = urljoin(request.host_url, href) if href else href
+	return result
+
+def get_object(uid, intids=None):
+	intids = component.getUtility(IIntIds) if intids is None else intids
+	result = intids.queryObject(uid)
+	return result
+
+def get_file_from_cf_io_url(link, intids=None):
+	result = None
+	try:
+		link = unquote(link)
+		if is_cf_io_href(link):
+			match = pattern.match(link)
+			uid = match.groups()[1]
+			if '/' in uid:
+				uid = uid.split('/')[0]
+			uid = from_external_string(uid)
+			result = get_object(uid, intids=intids)
+			if not IPloneNamed.providedBy(result):
+				result = None
+	except Exception:
+		logger.error("Error while getting file from %s", link)
 	return result
