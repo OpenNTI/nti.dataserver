94eea30fa5c263327f91570c61ae77c8b763a8be
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-15 20:58:15 +0000

    Plone formatting
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@86848 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/workspaces/providers.py
+++ src/nti/appserver/workspaces/providers.py
@@ -9,26 +9,27 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from zope import interface
 from zope import component
+from zope import interface
+
 from zope.location.location import Location
 from zope.location.interfaces import ILocation
+
+from nti.appserver._util import link_belongs_to_user
+
+from nti.appserver.workspaces.interfaces import IUserWorkspaceLinkProvider
 
 from nti.dataserver.interfaces import IUser
 
 from nti.links.links import Link
 
-from .._util import link_belongs_to_user
-
-from .interfaces import IUserWorkspaceLinkProvider
-
 @component.adapter(IUser)
 @interface.implementer(IUserWorkspaceLinkProvider)
 class _SearchLinksProvider(object):
-	
+
 	def __init__(self, user):
 		self.user = user
-	
+
 	def links(self, workspace):
 		# Note that we are providing a complete link with a target
 		# that is a string and also the name of the link. This is
@@ -38,23 +39,23 @@ class _SearchLinksProvider(object):
 		search_parent = Location()
 		search_parent.__name__ = 'Search'
 		search_parent.__parent__ = workspace
-		result = [Link( 'RecursiveUserGeneratedData', rel='UGDSearch' ),
-				  Link( 'UnifiedSearch', rel='UnifiedSearch' ) ]
+		result = [Link('RecursiveUserGeneratedData', rel='UGDSearch'),
+				  Link('UnifiedSearch', rel='UnifiedSearch') ]
 		for lnk in result:
 			lnk.__name__ = lnk.target
 			lnk.__parent__ = search_parent
-			interface.alsoProvides( lnk, ILocation )
+			interface.alsoProvides(lnk, ILocation)
 		return result
 
 @component.adapter(IUser)
 @interface.implementer(IUserWorkspaceLinkProvider)
 class _ResolveMeLinkProvider(object):
-	
+
 	def __init__(self, user):
 		self.user = user
-	
+
 	def links(self, workspace):
 		user = self.user
-		resolve_me_link = Link( user, rel="ResolveSelf", method='GET' )
-		link_belongs_to_user( resolve_me_link, user )
+		resolve_me_link = Link(user, rel="ResolveSelf", method='GET')
+		link_belongs_to_user(resolve_me_link, user)
 		return [resolve_me_link]
