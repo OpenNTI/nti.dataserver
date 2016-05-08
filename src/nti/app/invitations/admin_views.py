94ab60a2452abdc670ee0e5d35a194ef22fb6ac9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-05-08 02:36:16 +0000

    add delete_expired_invitations view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@88202 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/invitations/admin_views.py
+++ src/nti/app/invitations/admin_views.py
@@ -11,12 +11,19 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import six
+
 from pyramid.view import view_config
 from pyramid.view import view_defaults
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
+from nti.app.externalization.internalization import read_body_as_external_object
+from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
+
 from nti.app.invitations import REL_EXPIRED_INVITATIONS
+
+from nti.common.maps import CaseInsensitiveDict
 
 from nti.dataserver import authorization as nauth
 
@@ -26,6 +33,7 @@ from nti.externalization.interfaces import LocatedExternalDict
 from nti.externalization.interfaces import StandardExternalFields
 
 from nti.invitations.utils import get_expired_invitations
+from nti.invitations.utils import delete_expired_invitations
 
 ITEMS = StandardExternalFields.ITEMS
 
@@ -41,8 +49,42 @@ class GetExpiredInvitationsView(AbstractAuthenticatedView):
 		"""
 		Implementation of :const:`REL_EXPIRED_INVITATIONS`.
 		"""
+		values = CaseInsensitiveDict(self.request.params)
+		usernames = values.get('username') or values.get('usernames')
+		if isinstance(usernames, six.string_types):
+			usernames = usernames.split(",")
+		usernames = None if not usernames else set(usernames)
 		result = LocatedExternalDict()
-		result[ITEMS] = get_expired_invitations()
+		result[ITEMS] = get_expired_invitations(usernames)
+		result.__name__ = self.request.view_name
+		result.__parent__ = self.request.context
+		return result
+
+@view_config(name='DeleteExpiredInvitations')
+@view_config(name='delete_expired_invitations')
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   permission=nauth.ACT_READ,
+			   request_method='POST',
+			   context=IDataserverFolder)
+class DeleteExpiredInvitationsView(AbstractAuthenticatedView,
+								   ModeledContentUploadRequestUtilsMixin):
+
+	def readInput(self, value=None):
+		if self.request.body:
+			result = CaseInsensitiveDict(read_body_as_external_object(self.request))
+		else:
+			result = CaseInsensitiveDict()
+		return result
+
+	def __call__(self):
+		values = self.readInput()
+		usernames = values.get('username') or values.get('usernames')
+		if isinstance(usernames, six.string_types):
+			usernames = usernames.split(",")
+		usernames = None if not usernames else set(usernames)
+		result = LocatedExternalDict()
+		result[ITEMS] = delete_expired_invitations(usernames)
 		result.__name__ = self.request.view_name
 		result.__parent__ = self.request.context
 		return result
