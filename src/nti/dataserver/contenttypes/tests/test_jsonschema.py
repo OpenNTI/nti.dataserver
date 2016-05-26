94b9f92304009a84902f6f0ac38d7c87e78b293f
Josh Zuech <josh.zuech@nextthought.com>
2016-05-26 15:57:04 +0000

    Fix test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@89336 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/contenttypes/tests/test_jsonschema.py
+++ src/nti/dataserver/contenttypes/tests/test_jsonschema.py
@@ -11,7 +11,7 @@ from hamcrest import has_entry
 from hamcrest import has_length
 from hamcrest import assert_that
 
-from nti.coremetadata.jsonschema import make_schema
+from nti.coremetadata.utils import make_schema
 
 from nti.dataserver.interfaces import INote
 
@@ -24,24 +24,24 @@ class TestJsonSchema(DataserverLayerTest):
 		assert_that(result, has_entry('Fields', has_length(10)))
 		fields = result['Fields']
 		assert_that(fields, has_length(10))
-		
+
 		assert_that(fields, has_entry('title', has_entry('type', 'string')))
 
 		assert_that(fields, has_entry('inReplyTo', has_entry('type', 'string')))
 		assert_that(fields, has_entry('inReplyTo', has_entry('base_type', 'string')))
 
-		assert_that(fields, has_entry('applicableRange', has_entry('type', 'Object')))		
-		assert_that(fields, has_entry('applicableRange', 
+		assert_that(fields, has_entry('applicableRange', has_entry('type', 'Object')))
+		assert_that(fields, has_entry('applicableRange',
 									  has_entry('base_type', 'application/vnd.nextthought.contentrange.contentrangedescription')))
-		
+
 
 		assert_that(fields, has_entry('presentationProperties', has_entry('type', 'Dict')))
 		assert_that(fields, has_entry('presentationProperties', has_entry('base_type', 'string')))
-		
+
 		assert_that(fields, has_entry('body', has_entry('base_type', [u'string', u'namedfile', u'media', u'canvas'])))
-		
+
 		for name in ('body', 'sharedWith', 'tags'):
 			assert_that(fields, has_entry(name, has_entry('type', 'List')))
-			
+
 		for name in ('sharedWith', 'tags'):
 			assert_that(fields, has_entry(name, has_entry('base_type', 'string')))
