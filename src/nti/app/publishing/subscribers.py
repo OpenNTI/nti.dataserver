bca057f0a6bc531b75f46b949079622db7c51c16
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-07-14 16:01:00 +0000

    remove nti.externalization dep from nti.coremetadata
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@92286 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/publishing/subscribers.py
+++ src/nti/app/publishing/subscribers.py
@@ -15,8 +15,12 @@ from nti.app.publishing import TRX_TYPE_PUBLISH
 from nti.app.publishing import TRX_TYPE_UNPUBLISH
 
 from nti.coremetadata.interfaces import IPublishable
+from nti.coremetadata.interfaces import ICalendarPublishable
 from nti.coremetadata.interfaces import IObjectPublishedEvent
 from nti.coremetadata.interfaces import IObjectUnpublishedEvent
+from nti.coremetadata.interfaces import ICalendarPublishableModifiedEvent
+
+from nti.externalization.internalization import notifyModified
 
 from nti.recorder.utils import record_transaction
 
@@ -27,3 +31,8 @@ def _record_published(obj, event):
 @component.adapter(IPublishable, IObjectUnpublishedEvent)
 def _record_unpublished(obj, event):
 	record_transaction(obj, type_=TRX_TYPE_UNPUBLISH)
+
+@component.adapter(ICalendarPublishable, ICalendarPublishableModifiedEvent)
+def _on_calendar_publishable_modified(obj, event):
+	notifyModified(obj, {'publishBeginning': event.publishBeginning,
+						 'publishEnding': event.publishBeginning })
