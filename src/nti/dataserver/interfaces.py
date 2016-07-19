f41bb27b19dadb5e2a2e42c1d62b14dbf1b5e2cd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-07-19 18:53:20 +0000

    Move interface
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@92602 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/interfaces.py
+++ src/nti/dataserver/interfaces.py
@@ -244,25 +244,13 @@ ILinkExternalHrefOnly = ILinkExternalHrefOnly
 from nti.dataserver_core.interfaces import IContainer
 from nti.dataserver_core.interfaces import IContainerNamesContainer
 from nti.dataserver_core.interfaces import IZContainerNamesContainer
+from nti.dataserver_core.interfaces import IHomogeneousTypeContainer
 
 IContainer = IContainer
 IContainerNamesContainer = IContainerNamesContainer
 IZContainerNamesContainer = IZContainerNamesContainer
+IHomogeneousTypeContainer = IHomogeneousTypeContainer
 
-class IHomogeneousTypeContainer(IContainer):
-	"""
-	Things that only want to contain items of a certain type.
-	In some cases, an object of this type would be specified
-	in an interface as a :class:`zope.schema.List` with a single
-	`value_type`.
-	"""
-
-	contained_type = interface.Attribute(
-		"""
-		The type of objects in the container. May be an Interface type
-		or a class type. There should be a ZCA factory to create instances
-		of this type associated as tagged data on the type at :data:IHTC_NEW_FACTORY
-		""")
 
 IHTC_NEW_FACTORY = 'nti.dataserver.interfaces.IHTCNewFactory'
 
