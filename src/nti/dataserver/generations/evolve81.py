ef0ce63912f3425303154b7a3768e3992711c6f0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-07-25 22:39:07 +0000

    no need for evol
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@93074 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/evolve81.py
+++ src/nti/dataserver/generations/evolve81.py
@@ -9,19 +9,16 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 81
+generation = 82
 
 from zope import component
 
+from zope.component.hooks import site
 from zope.component.hooks import setHooks
-from zope.component.hooks import site as current_site
 
-from BTrees.OIBTree import OIBTree
-from BTrees.OOBTree import OOBTree
+from zope.intid.interfaces import IIntIds
 
-from nti.site.hostpolicy import get_all_host_sites
-
-from nti.site.interfaces import IHostPolicySiteManager
+from nti.contentlibrary.indexed_data.catalog import install_library_catalog
 
 def do_evolve(context):
 	setHooks()
@@ -29,30 +26,19 @@ def do_evolve(context):
 	root = conn.root()
 	ds_folder = root['nti.dataserver']
 
-	with current_site(ds_folder):
+	with site(ds_folder):
 		assert  component.getSiteManager() == ds_folder.getSiteManager(), \
 				"Hooks not installed?"
 
-		for site in get_all_host_sites():
-			manager = site.getSiteManager()
-			if not IHostPolicySiteManager.providedBy(manager):
-				continue	
-			if 		hasattr(manager, "_utility_registrations")	\
-				and not isinstance(manager._utility_registrations, OOBTree):					
-				manager._utility_registrations = OOBTree(manager._utility_registrations)
-				manager._adapter_registrations = OOBTree(manager._adapter_registrations)
-			
-			for name in ('adapters', 'utilities'):
-				registry = getattr(manager, name, None)
-				if 		hasattr(registry, "_provided")	\
-					and not isinstance(registry._provided, OIBTree):
-					registry._provided = OIBTree(registry._provided)
-				
+		lsm = ds_folder.getSiteManager()
+		intids = lsm.getUtility(IIntIds)
+
+		install_library_catalog(ds_folder, intids)
 		logger.info('Dataserver evolution %s done.', generation)
 
 def evolve(context):
 	"""
-	Evolve to gen 81 to update persistent local site mangager internals
+	Evolve to gen 81 by installing the new library asset catalog.
 	"""
+	# do_evolve(context)  XXX DON'T INSTALL YET
 	pass
-	
