933023cae351c58796ca7bc73101498e02a27d2a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-08-08 15:40:42 +0000

    use site funcs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@93721 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/scripts/nti_remove_package_inaccessible_assets.py
+++ src/nti/app/contentlibrary/scripts/nti_remove_package_inaccessible_assets.py
@@ -16,25 +16,30 @@ import argparse
 
 from zope import component
 
+from zope.component.hooks import site as current_site
+
 from nti.app.contentlibrary.utils.common import remove_package_inaccessible_assets
 
 from nti.contentlibrary.interfaces import IContentPackageLibrary
 
 from nti.dataserver.utils import run_with_dataserver
-from nti.dataserver.utils.base_script import set_site
 from nti.dataserver.utils.base_script import create_context
+
+from nti.site.hostpolicy import get_host_site
 
 def _process_args(args):
 	# sync global library
 	library = component.getUtility(IContentPackageLibrary)
 	library.syncContentPackages()
+	if args.verbose:
+		print()
 	for site in args.sites or ():
-		set_site(site)
-		result = remove_package_inaccessible_assets()
-		if args.verbose:
-			print()
-			pprint.pprint(result)
-			print()
+		site = get_host_site(site)
+		with current_site(site):
+			result = remove_package_inaccessible_assets()
+			if args.verbose:
+				pprint.pprint(result)
+				print()
 
 def main():
 	arg_parser = argparse.ArgumentParser(description="Remove package inaccessible assets")
