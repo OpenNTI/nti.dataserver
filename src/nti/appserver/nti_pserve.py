f0be9cff7d7323a2f9ad498ead17e7e94b0a9f1f
Chris Utz <chris.utz@nextthought.com>
2016-09-10 01:32:47 +0000

    Work around an issue with calling subprocess in a gunicorn worker.  Thanks JM.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@96571 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/nti_pserve.py
+++ src/nti/appserver/nti_pserve.py
@@ -15,6 +15,26 @@ of these locks while a previous greenlet already has it.
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
+# Before doing any monkey patching, patch gevent.signal.signal
+# to work around an issue where using subprocess in a gunicorn worker
+# is broken because of the way that gevent and gunicorn fight over
+# SIGCHLD.  [Gevent github issue to come]
+import gevent.signal
+from gevent.signal import signal as gevent_signal
+from gevent.signal import getsignal as gevent_getsignal
+
+import signal as _signal
+
+def _patched_signal(signalnum, handler):
+	if signalnum == _signal.SIGCHLD and handler in (_signal.SIG_DFL, _signal.SIG_IGN):
+		if gevent_getsignal(_signal.SIGCHLD) not in (_signal.SIG_DFL, _signal.SIG_IGN):
+			# They tried to ignore/default action this signal (it's ignored by default)
+			# and previous code was there, so make sure the previous code doesn't run
+			return gevent_signal(signalnum, lambda *args: None)
+		return
+	return gevent_signal(signalnum, handler)
+gevent.signal.signal = _patched_signal
+
 # Note that we must not import *anything* before the patch
 from nti.monkey import patch_gevent_on_import
 patch_gevent_on_import.patch()
