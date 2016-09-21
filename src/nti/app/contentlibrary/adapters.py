1b28157f5a2b51a74fee25c8ac6828ddb8e81ebf
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-09-21 15:10:02 +0000

    move catalog adpaters to its own module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@97184 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/adapters.py
+++ src/nti/app/contentlibrary/adapters.py
@@ -24,8 +24,6 @@ from persistent.mapping import PersistentMapping
 
 from BTrees.OOBTree import OOBTree
 
-from pyramid.location import lineage
-
 from nti.appserver.context_providers import get_top_level_contexts
 
 from nti.appserver.interfaces import IJoinableContextProvider
@@ -42,27 +40,7 @@ from nti.contentlibrary.interfaces import IContentPackageBundleLibrary
 
 from nti.contentlibrary.indexed_data import get_library_catalog
 
-from nti.contentlibrary.indexed_data.interfaces import INTIIDAdapter
-from nti.contentlibrary.indexed_data.interfaces import INamespaceAdapter
-from nti.contentlibrary.indexed_data.interfaces import ISlideDeckAdapter
-from nti.contentlibrary.indexed_data.interfaces import IContainersAdapter
-from nti.contentlibrary.indexed_data.interfaces import IContainedTypeAdapter
-
-from nti.contenttypes.courses.interfaces import ICourseInstance
-from nti.contenttypes.courses.interfaces import ICourseOutlineNode
-from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
-
-from nti.contenttypes.presentation import iface_of_asset
-
-from nti.contenttypes.presentation.interfaces import INTISlide, INTISlideDeck
-from nti.contenttypes.presentation.interfaces import INTISlideVideo
-from nti.contenttypes.presentation.interfaces import IPresentationAsset
-from nti.contenttypes.presentation.interfaces import ICoursePresentationAsset
-from nti.contenttypes.presentation.interfaces import IPackagePresentationAsset
 from nti.contenttypes.presentation.interfaces import IPresentationAssetContainer
-
-from nti.contenttypes.courses.utils import get_course_subinstances
-from nti.contenttypes.courses.utils import get_courses_for_packages
 
 from nti.dataserver.contenttypes.forums.interfaces import IPost
 from nti.dataserver.contenttypes.forums.interfaces import ITopic
@@ -75,147 +53,7 @@ from nti.dublincore.time_mixins import PersistentCreatedAndModifiedTimeObject
 
 from nti.ntiids.ntiids import find_object_with_ntiid
 
-from nti.site.interfaces import IHostPolicyFolder
-
 from nti.traversal.traversal import find_interface
-
-# Type
-
-class _Type(object):
-
-	__slots__ = (b'type',)
-
-	def __init__(self, type_):
-		self.type = type_
-
-@component.adapter(IPresentationAsset)
-@interface.implementer(IContainedTypeAdapter)
-def _asset_to_contained_type(context):
-	provided = iface_of_asset(context)
-	return _Type(provided.__name__)
-
-# Namespace
-
-class _Namespace(object):
-
-	__slots__ = (b'namespace',)
-
-	def __init__(self, namespace):
-		self.namespace = namespace
-
-@interface.implementer(INamespaceAdapter)
-@component.adapter(ICoursePresentationAsset)
-def _course_asset_to_namespace(context):
-	node = find_interface(context, ICourseOutlineNode, strict=False)
-	source = getattr(node, 'src', None)
-	if source:
-		return _Namespace(source)
-	return None
-
-def _package_lineage_to_namespace(context):
-	package = find_interface(context, IContentPackage, strict=False)
-	if package is not None:
-		return _Namespace(package.ntiid)
-	return None
-
-@interface.implementer(INamespaceAdapter)
-@component.adapter(IPackagePresentationAsset)
-def _package_asset_to_namespace(context):
-	return _package_lineage_to_namespace(context)
-
-# Namespace
-
-class _NTIID(object):
-
-	__slots__ = (b'ntiid',)
-
-	def __init__(self, ntiid):
-		self.ntiid = ntiid
-
-@interface.implementer(INTIIDAdapter)
-@component.adapter(IPresentationAsset)
-def _asset_to_ntiid(context):
-	return _NTIID(context.ntiid)
-
-# Containers
-
-class _Containers(object):
-
-	__slots__ = (b'containers',)
-
-	def __init__(self, containers):
-		self.containers = containers
-
-def _package_lineage_to_containers(context):
-	result = set()
-	for location in lineage(context):
-		if IContentUnit.providedBy(location):
-			result.add(location.ntiid)
-		if IContentPackage.providedBy(location):
-			break
-	result.discard(None)
-	return _Containers(tuple(result))
-
-@interface.implementer(IContainersAdapter)
-@component.adapter(IPackagePresentationAsset)
-def _pacakge_asset_to_containers(context):
-	result = _package_lineage_to_containers(context)
-	package = find_interface(context, IContentPackage, strict=False)
-	folder = find_interface(context, IHostPolicyFolder, strict=False)
-	if package is not None and folder is not None:
-		containers = set(result.containers)
-		courses = get_courses_for_packages(folder.__name__, package.ntiid)
-		for course in courses:
-			entry = ICourseCatalogEntry(course)
-			containers.add(entry.ntiid)
-		result = _Containers(tuple(containers))
-
-	# check for slides and slidevideos
-	if		(INTISlide.providedBy(context) or INTISlideVideo.providedBy(context)) \
-		and context.__parent__ is not None \
-		and context.__parent__.ntiid:
-		containers = set(result.containers)
-		containers.add(context.__parent__.ntiid)
-		result = _Containers(tuple(containers))
-	return result
-
-@interface.implementer(IContainersAdapter)
-@component.adapter(ICoursePresentationAsset)
-def _course_asset_to_containers(context):
-	course = None
-	result = set()
-	for location in lineage(context):
-		if IPresentationAsset.providedBy(location):
-			result.add(location.ntiid)
-		if ICourseInstance.providedBy(location):
-			course = location
-			entry = ICourseCatalogEntry(course, None)
-			result.add(getattr(entry, 'ntiid', None))
-			break
-
-	# include NTIIDs of sections
-	for instance in get_course_subinstances(course):
-		entry = ICourseCatalogEntry(instance, None)
-		result.add(getattr(entry, 'ntiid', None))
-
-	result.discard(None)
-	result.discard(context.ntiid)
-	return _Containers(tuple(result))
-
-# Containers
-
-class _SlideDeck(object):
-
-	__slots__ = (b'videos',)
-
-	def __init__(self, videos):
-		self.videos = videos
-
-@component.adapter(INTISlideDeck)
-@interface.implementer(ISlideDeckAdapter)
-def _slideck_data(context):
-	videos = {v.video_ntiid for v in context.Videos or ()}
-	return _SlideDeck(videos)
 
 # Bundles
 
