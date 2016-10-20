03057f401eae0483596fc5ae70a698656001d336
Bobby Hagen <bobby.hagen@nextthought.com>
2016-10-20 06:56:11 +0000

    OUB-70: pull common event and handler logic down into dataserver
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@99060 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/saml/events.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: views.py 94273 2016-08-15 18:55:08Z carlos.sanchez $
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.app.saml.interfaces import ISAMLUserCreatedEvent
+
+from nti.dataserver.interfaces import UserEvent
+
+from nti.dataserver.saml.interfaces import ISAMLIDPUserInfoBindings
+from nti.dataserver.saml.interfaces import ISAMLProviderUserInfo
+
+
+
+@interface.implementer(ISAMLUserCreatedEvent)
+class SAMLUserCreatedEvent(UserEvent):
+	def __init__(self, idp_id, user, user_info, request):
+		super(SAMLUserCreatedEvent, self).__init__(user)
+		self.idp_id = idp_id
+		self.user_info = user_info
+		self.request = request
+
+@component.adapter(ISAMLUserCreatedEvent)
+def _user_created(event):
+	attach_idp_user_info(event)
+
+def attach_idp_user_info(event):
+	idp_user_info_container = ISAMLIDPUserInfoBindings(event.user)
+	if event.idp_id not in idp_user_info_container:
+		# change to DEBUG
+		logger.info('Attaching user\'s IdP info: %s', str(event.user_info))
+		idp_user_info = component.queryAdapter(event.user_info, ISAMLProviderUserInfo, event.idp_id)
+		if idp_user_info is not None:
+			idp_user_info_container[event.idp_id] = idp_user_info
+		else:
+			logger.warn('Failed to adapt ISAMLUserAssertionInfo("%s") to ISAMLProviderUserInfo for user "%s", event "%s"',
+						str(event.user_info),
+						str(event.user.username),
+						str(event))
+	else:
+		logger.info('Skipping attachment of user''s IdP info: %s', str(event.user_info))
