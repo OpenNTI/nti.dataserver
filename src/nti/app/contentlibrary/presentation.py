2732a00d64ff67f3a21f635dd54b00390a1436e9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-08 23:34:16 +0000

    include all assets
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@100393 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/presentation.py
+++ src/nti/app/contentlibrary/presentation.py
@@ -18,16 +18,14 @@ from nti.intid.interfaces import IntIdAddedEvent
 from nti.contentlibrary.indexed_data import get_library_catalog
 
 from nti.contenttypes.presentation.interfaces import INTICourseOverviewGroup
-from nti.contenttypes.presentation.interfaces import IPackagePresentationAsset
 
 @component.adapter(INTICourseOverviewGroup, IntIdAddedEvent)
 def _on_course_overview_registered(group, event):
 	parent = group.__parent__
 	catalog = get_library_catalog()
 	extended = (group.ntiid, getattr(parent, 'ntiid', None))
-	for item in group:
-		if IPackagePresentationAsset.providedBy(item):
-			catalog.update_containers(item, extended)
+	for item in group or ():
+		catalog.update_containers(item, extended)
 
 @component.adapter(INTICourseOverviewGroup, IObjectModifiedEvent)
 def _on_course_overview_modified(group, event):
