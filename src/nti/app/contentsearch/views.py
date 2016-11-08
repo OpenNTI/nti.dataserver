77f79c702e131086abc8a0e0727f9ba48568bd23
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-08 16:51:11 +0000

    use ISearcher interface
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/branches/20161104@100320 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentsearch/views.py
+++ src/nti/app/contentsearch/views.py
@@ -11,7 +11,6 @@ logger = __import__('logging').getLogger(__name__)
 
 import time
 
-from zope import component
 from zope import interface
 
 from zope.event import notify
@@ -25,25 +24,18 @@ from pyramid import httpexceptions as hexc
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
 from nti.app.externalization.view_mixins import BatchingUtilsMixin
-from nti.app.externalization.internalization import read_body_as_external_object
-from nti.app.externalization.internalization import update_object_from_external_object
 
 from nti.app.renderers.interfaces import IUncacheableInResponse
 
-from nti.contentsearch.interfaces import IIndexManager
+from nti.contentsearch.interfaces import ISearcher
 from nti.contentsearch.interfaces import ISearchResults
 from nti.contentsearch.interfaces import SearchCompletedEvent
+
 from nti.contentsearch.search_utils import create_queryobject
 
 from nti.dataserver.users import Entity
-from nti.dataserver.interfaces import IMemcacheClient
 
-from nti.externalization.internalization import find_factory_for
 from nti.externalization.interfaces import StandardExternalFields
-from nti.externalization.externalization import to_external_object
-
-from nti.property.property import Lazy
-from nti.property.property import CachedProperty
 
 ITEMS = StandardExternalFields.ITEMS
 
@@ -67,14 +59,8 @@ class BaseView(AbstractAuthenticatedView):
 	def query(self):
 		return self.construct_queryobject(self.request)
 
-	@CachedProperty
-	def indexmanager(self):
-		return self.request.registry.getUtility(IIndexManager)
-
 	def locate(self, obj, parent):
-		# TODO: (Instead of modification info, we should be using etags here, anyway).
 		locate(obj, parent, self.name)
-		# TODO: Make cachable?
 		interface.alsoProvides(obj, IUncacheableInResponse)
 		return obj
 
@@ -106,9 +92,9 @@ class BaseSearchView(BaseView, BatchingUtilsMixin):
 			response = (new_results, results, batch_hits)
 		return response
 
-	def _do_search(self, query, store=None):
-		result = self.indexmanager.search(query=query, store=store)
-		return result
+	def _do_search(self, query):
+		searcher = ISearcher(self.remoteUser)
+		return searcher.search(query=query)
 
 	def search(self, query):
 		result = None
@@ -116,83 +102,33 @@ class BaseSearchView(BaseView, BatchingUtilsMixin):
 		# execute search
 		result = self._do_search(query=query, store=result)
 
-		# page if required
-		batch_size, batch_start = self._get_batch_size_start()
-		result, original, batch = self._batch_results(result, batch_size, batch_start)
+# 		# page if required
+# 		batch_size, batch_start = self._get_batch_size_start()
+# 		result, original, batch = self._batch_results(result, batch_size, batch_start)
 		elapsed = time.time() - now
-
+# 
 		# notify
 		entity = Entity.get_entity(query.username)
-		notify(SearchCompletedEvent(entity, original, elapsed))
-
-		# externalize to add links
-		result = to_external_object(result)
-		if batch is not None:
-			total_pages = len(original) // batch_size + 1
-			result['BatchPage'] = batch_start // batch_size + 1
-			result['ItemCount'] = len(result.get(ITEMS, ()))
-			prev_batch_start, next_batch_start = self._batch_start_tuple(batch_start,
-																		 batch_size)
-			# check last page
-			if total_pages == result['BatchPage']:
-				next_batch_start = None
-			self._create_batch_links(self.request, result,
-									 next_batch_start, prev_batch_start)
+		notify(SearchCompletedEvent(entity, result, elapsed))
+# 
+# 		# externalize to add links
+# 		result = to_external_object(result)
+# 		if batch is not None:
+# 			total_pages = len(original) // batch_size + 1
+# 			result['BatchPage'] = batch_start // batch_size + 1
+# 			result['ItemCount'] = len(result.get(ITEMS, ()))
+# 			prev_batch_start, next_batch_start = self._batch_start_tuple(batch_start,
+# 																		 batch_size)
+# 			# check last page
+# 			if total_pages == result['BatchPage']:
+# 				next_batch_start = None
+# 			self._create_batch_links(self.request, result,
+# 									 next_batch_start, prev_batch_start)
 		return result
 
 class SearchView(BaseSearchView):
+
 	name = 'Search'
-
-	max_cache_time = 60
-	max_cache_size = 50
-	use_memcache = True
-
-	DATA_BASE_KEY = "/search/%s/results/data"
-	QUERY_BASE_KEY = "/search/%s/results/query"
-
-	@Lazy
-	def memcache(self):
-		client = component.queryUtility(IMemcacheClient) if self.use_memcache else None
-		return client
-
-	@Lazy
-	def _data_key(self):
-		return self.DATA_BASE_KEY % self.remoteUser.username
-
-	@Lazy
-	def _query_key(self):
-		return self.QUERY_BASE_KEY % self.remoteUser.username
-
-	def _check_memcache(self, query):
-		result = None
-		client = self.memcache
-		if client is None:
-			return result
-		try:
-			data = client.get(self._query_key)
-			if data:
-				stored_query, stamp = data
-				if 	stored_query.digest() == query.digest() and \
-					time.time() - stamp < self.max_cache_time:
-					result = client.get(self._data_key)
-				else:
-					client.delete(self._data_key)
-					client.delete(self._query_key)
-		except Exception as e:
-			logger.error("Cannot get search result from Memcache. %s", e)
-			result = None
-		return result
-
-	def _store_memcache(self, query, results):
-		client = self.memcache
-		if client is None:
-			return
-		try:
-			data = (query, time.time())
-			client.set(self._query_key, data)
-			client.set(self._data_key, results)
-		except Exception as e:
-			logger.error("Cannot store results in Memcache. %s", e)
 
 	def _do_search(self, query, store=None):
 		result = self._check_memcache(query)
@@ -225,25 +161,3 @@ class SuggestView(BaseView):
 		notify(SearchCompletedEvent(entity, result, elapsed))
 		return result
 Suggest = SuggestView  # BWC
-
-class HighlightSearchHitsView(AbstractAuthenticatedView):
-
-	def readInput(self):
-		return read_body_as_external_object(self.request)
-
-	def createResults(self):
-		externalValue = self.readInput()
-		result = find_factory_for(externalValue)
-		result = result() if result else None
-		if not ISearchResults.providedBy(result):
-			raise hexc.HTTPBadRequest(detail="invalid type")
-		update_object_from_external_object(result, externalValue)
-		return result
-
-	def __call__(self):
-		result = self.createResults()
-		result.Query.applyHighlights = True
-		for hit in result.Hits:  # make sure we null out Fragments field
-			hit.Fragments = None
-		return result
-HighlightResults = HighlightSearchHitsView  # BWC
