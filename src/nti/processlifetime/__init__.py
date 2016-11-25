8f1dbeed90440f765ee720406be5a245ddcc10cd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-25 22:02:11 +0000

    add an interface module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@101370 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/processlifetime/__init__.py
+++ src/nti/processlifetime/__init__.py
@@ -11,8 +11,6 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from zope import interface
-
 # Export these things
 from zope.processlifetime import DatabaseOpened
 from zope.processlifetime import IDatabaseOpened
@@ -21,78 +19,17 @@ from zope.processlifetime import IProcessStarting
 from zope.processlifetime import DatabaseOpenedWithRoot
 from zope.processlifetime import IDatabaseOpenedWithRoot 
 
-# Assign to them to keep pylint from complaining
-
-DatabaseOpened = DatabaseOpened
-IDatabaseOpened = IDatabaseOpened
-
-DatabaseOpenedWithRoot = DatabaseOpenedWithRoot
-IDatabaseOpenedWithRoot = IDatabaseOpenedWithRoot
-
-class IAfterDatabaseOpenedEvent(interface.Interface):
-	"""
-	In the startup sequence, this should be notified after the
-	:class:`IDatabaseOpened` event has been notified for each
-	database, but before the root is available. This allows the
-	application a chance to do some intermediate processing.
-	"""
-	database = interface.Attribute("The main database.")
-
-@interface.implementer(IAfterDatabaseOpenedEvent)
-class AfterDatabaseOpenedEvent(object):
-
-	def __init__(self, database):
-		self.database = database
-
-class IProcessWillFork(interface.Interface):
-	"""
-	An event that *may* be fired (on best effort basis) in a parent process
-	before a call to :func:`os.fork`
-	"""
-
-@interface.implementer(IProcessWillFork)
-class ProcessWillFork(object):
-	pass
-
-class IProcessDidFork(interface.Interface):
-	"""
-	An event that *may* be fired (on best effort basis) in the *child* process
-	following a call to :func:`os.fork`. The timing (how soon after the fork)
-	of this event is not defined.
-	"""
-
-@interface.implementer(IProcessDidFork)
-class ProcessDidFork(object):
-	pass
-
-class IApplicationTransactionOpenedEvent(interface.Interface):
-	"""
-	An event fired during application startup, after :class:`IDatabaseOpenedWithRoot`
-	has been fired (and so :mod:`zope.generations` installs and evolutions
-	have been done), within the scope of a transaction manager, and within
-	the scope of the application's main :class:`zope.component.interfaces.ISite`.
-
-	This event is intended to allow additional (database) setup that cannot
-	be handled with simple generations, or that need to take place every time
-	the application starts. If the process is going to fork, this will be fired
-	before the fork, in the main process.
-
-	The transaction will be committed after this event has fired.
-
-	.. note:: Unlike the database events, which repeat during process
-		lifetime (such as after a fork, where the database must be re-opened)
-		this event is only fired once during a particular application lifetime.
-	"""
-
-@interface.implementer(IApplicationTransactionOpenedEvent)
-class ApplicationTransactionOpenedEvent(object):
-	pass
-
-class IApplicationProcessStarting(IProcessStarting):
-	xml_conf_machine = interface.Attribute("The main database.")
-
-@interface.implementer(IApplicationProcessStarting)
-class ApplicationProcessStarting(ProcessStarting):
+from .interfaces import AfterDatabaseOpenedEvent
+from .interfaces import IAfterDatabaseOpenedEvent
 	
-	def __init__(self, xml_conf_machine=None):
-		self.xml_conf_machine = xml_conf_machine
+from .interfaces import ProcessWillFork
+from .interfaces import IProcessWillFork
+
+from .interfaces import ProcessDidFork
+from .interfaces import IProcessDidFork
+
+from .interfaces import ApplicationTransactionOpenedEvent
+from .interfaces import IApplicationTransactionOpenedEvent
+
+from .interfaces import ApplicationProcessStarting
+from .interfaces import IApplicationProcessStarting
