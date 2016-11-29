37caa63d8239b16c99454b87f57634c79ecd675c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-29 22:18:16 +0000

    clean up dead code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@101590 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentsearch/views.py
+++ src/nti/app/contentsearch/views.py
@@ -17,8 +17,6 @@ from zope.event import notify
 
 from zope.location import locate
 
-from z3c.batching.batch import Batch
-
 from pyramid import httpexceptions as hexc
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
@@ -28,15 +26,12 @@ from nti.app.externalization.view_mixins import BatchingUtilsMixin
 from nti.app.renderers.interfaces import IUncacheableInResponse
 
 from nti.contentsearch.interfaces import ISearcher 
-from nti.contentsearch.interfaces import ISearchResults
-from nti.contentsearch.interfaces import ISuggestResults 
 from nti.contentsearch.interfaces import SearchCompletedEvent
 
 from nti.contentsearch.search_utils import create_queryobject
 
 from nti.dataserver.users import Entity
 
-from nti.externalization.interfaces import LocatedExternalList
 from nti.externalization.interfaces import StandardExternalFields
 
 ITEMS = StandardExternalFields.ITEMS
@@ -80,52 +75,16 @@ class BaseView(AbstractAuthenticatedView):
 
 class BaseSearchView(BaseView, BatchingUtilsMixin):
 
-	def _batch_results(self, results, batch_size, batch_start):
-		if 		batch_size is None \
-			or 	batch_start is None \
-			or	not ISearchResults.providedBy(results):
-			response = (results, results, None)
-		elif batch_start >= len(results):
-			response = ((), results, None)
-		else:
-			new_results = results.clone(hits=False)
-			batch_hits = Batch(results.Hits, batch_start, batch_size)
-			new_results.Hits = batch_hits  # Set hits
-			response = (new_results, results, batch_hits)
-		return response
-
 	def _do_search(self, query):
 		searcher = ISearcher(self.remoteUser)
-		result = searcher.search(query=query)
-		return (result,) if ISearchResults.providedBy(result) else result
+		return searcher.search(query=query)
 
 	def search(self, query):
 		now = time.time()
-		# execute search
 		result = self._do_search(query=query)
-
-# 		# page if required
-# 		batch_size, batch_start = self._get_batch_size_start()
-# 		result, original, batch = self._batch_results(result, batch_size, batch_start)
 		elapsed = time.time() - now
-# 
-		# notify
 		entity = Entity.get_entity(query.username)
-#		notify(SearchCompletedEvent(entity, result, elapsed))
-# 
-# 		# externalize to add links
-# 		result = to_external_object(result)
-# 		if batch is not None:
-# 			total_pages = len(original) // batch_size + 1
-# 			result['BatchPage'] = batch_start // batch_size + 1
-# 			result['ItemCount'] = len(result.get(ITEMS, ()))
-# 			prev_batch_start, next_batch_start = self._batch_start_tuple(batch_start,
-# 																		 batch_size)
-# 			# check last page
-# 			if total_pages == result['BatchPage']:
-# 				next_batch_start = None
-# 			self._create_batch_links(self.request, result,
-# 									 next_batch_start, prev_batch_start)
+		notify(SearchCompletedEvent(entity, result, elapsed))
 		return result
 
 class SearchView(BaseSearchView):
@@ -142,14 +101,11 @@ class SuggestView(BaseView):
 
 	def _do_search(self, query):
 		searcher = ISearcher(self.remoteUser)
-		result = searcher.suggest(query=query)
-		return (result,) if ISuggestResults.providedBy(result) else result
+		return searcher.suggest(query=query)
 
-	def search(self, *queries):
+	def search(self, query):
 		now = time.time()
-		result = LocatedExternalList()
-		for query in queries:
-			result.extend(self._do_search(query=query))
+		result = self._do_search(query)
 		elapsed = time.time() - now
 		entity = Entity.get_entity(query.username)
 		notify(SearchCompletedEvent(entity, result, elapsed))
