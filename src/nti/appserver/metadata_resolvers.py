6dd7cae371e705c09b49e546ff1c993596579a51
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-02 20:21:24 +0000

    same in branch
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/branches/20161128@101886 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/metadata_resolvers.py
+++ src/nti/appserver/metadata_resolvers.py
@@ -12,17 +12,10 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from zope import component
 from zope import interface
-
-from nti.contentlibrary.interfaces import IContentUnit
-from nti.contentlibrary.interfaces import IContentUnitHrefMapper
 
 from nti.contentprocessing.interfaces import IContentMetadata
 from nti.contentprocessing.interfaces import IContentMetadataURLHandler
-
-from nti.contentprocessing.metadata_extractors import ImageMetadata
-from nti.contentprocessing.metadata_extractors import ContentMetadata
 
 from nti.ntiids.ntiids import find_object_with_ntiid
 
@@ -37,27 +30,3 @@ class TagURLHandler(object):
 		obj = find_object_with_ntiid(url)
 		if obj is not None:
 			return IContentMetadata(obj, None)
-
-@interface.implementer(IContentMetadata)
-@component.adapter(IContentUnit)
-def ContentMetadataFromContentUnit(content_unit):
-	# TODO: Is this the right level at which to externalize the hrefs?
-	result = ContentMetadata(title=content_unit.title,
-							 description=content_unit.description,
-							 contentLocation=IContentUnitHrefMapper(content_unit).href,
-							 mimeType='text/html')
-	result.__name__ = '@@metadata'
-	result.__parent__ = content_unit  # for ACL
-
-	def _attach_image(key):
-		image = ImageMetadata(url=IContentUnitHrefMapper(key).href)
-		image.__parent__ = result
-		if not result.images:
-			result.images = []
-		result.images.append(image)
-
-	for name in ('icon', 'thumbnail'):
-		key = getattr(content_unit, name)
-		if key:
-			_attach_image(key)
-	return result
