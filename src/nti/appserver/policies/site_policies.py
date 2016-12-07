bf55ab3a82037cb0024babbba6dde824975469bb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-07 20:12:38 +0000

    only include override if nti.app.contentlibrary is installed
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@102177 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/policies/site_policies.py
+++ src/nti/appserver/policies/site_policies.py
@@ -61,9 +61,6 @@ from nti.common.string import has_emoji_chars
 from nti.contentfragments import censor
 from nti.contentfragments.interfaces import ICensoredContentPolicy
 
-from nti.contentlibrary.interfaces import IS3Key
-from nti.contentlibrary.interfaces import IAbsoluteContentUnitHrefMapper
-
 from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import ICoppaUser
 from nti.dataserver.interfaces import INewUserPlacer
@@ -76,6 +73,7 @@ from nti.dataserver.shards import AbstractShardPlacer
 
 from nti.dataserver.users import Entity
 from nti.dataserver.users import Community
+
 from nti.dataserver.users.interfaces import IUserProfile
 from nti.dataserver.users.interfaces import IFriendlyNamed
 from nti.dataserver.users.interfaces import IEmailAddressable
@@ -215,35 +213,6 @@ def queryMultiAdapterInSite(objects, target, request=None, site_names=None, defa
 		if result is not _marker:
 			return result
 	return default
-
-@component.adapter(IS3Key)
-@interface.implementer(IAbsoluteContentUnitHrefMapper)
-class RequestAwareS3KeyHrefMapper(object):
-	"""
-	Produces HTTP URLs for keys in buckets.	Takes steps to work with CORS
-	and other distribution strategies.
-
-	Use this mapper when the bucket name is a DNS name, and the bucket name
-	also has a DNS CNAME set up for it, and the application accessing the content
-	was served from the same CNAME origin (or doesn't care about cross-origin concerns).
-	"""
-	href = None
-
-	def __init__(self, key):
-		# TODO: The following may not be the case?
-		# We have to force HTTP here, because using https (or protocol relative)
-		# falls down for the browser: the certs on the CNAME we redirect to, *.s3.aws.amazon.com
-		# don't match for bucket.name host
-		sites = get_possible_site_names()
-		if sites:
-			# In the CORS case, we may be coming from an origin, to the dataserver
-			# and serving content which ought to come back from the origin CDN. We cannot use
-			# the request.host (Host) header, because that would name the dataserver, which
-			# might not be the content origin. The preferred sites send back the
-			# origin first
-			self.href = 'http://' + sites[0] + '/' + key.key
-		else:
-			self.href = 'http://' + key.bucket.name + '/' + key.key
 
 @interface.implementer(INewUserPlacer)
 class RequestAwareUserPlacer(AbstractShardPlacer):
