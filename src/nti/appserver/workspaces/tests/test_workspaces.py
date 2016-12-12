2a50924de70318a92a7074c5e846760561286894
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-12 16:17:42 +0000

    remove test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@102385 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/workspaces/tests/test_workspaces.py
+++ src/nti/appserver/workspaces/tests/test_workspaces.py
@@ -21,11 +21,7 @@ from hamcrest import contains_inanyorder
 from hamcrest import greater_than_or_equal_to
 does_not = is_not
 
-import os
-import shutil
-import tempfile
 import transaction
-import pyramid.interfaces
 
 from persistent import Persistent
 
@@ -41,8 +37,6 @@ from zope.schema import interfaces as sch_interfaces
 
 from zc import intid as zc_intid
 
-from nti.appserver import pyramid_authorization
-
 from nti.appserver.policies.interfaces import ISitePolicyUserEventListener
 
 from nti.appserver.workspaces import Service
@@ -53,15 +47,11 @@ from nti.appserver.workspaces import ContainerEnumerationWorkspace as CEW
 from nti.appserver.workspaces import HomogeneousTypedContainerCollection as HTCW
 from nti.appserver.workspaces import _UserPagesCollection as UserPagesCollection
 
-from nti.appserver.workspaces.interfaces import IWorkspace
 from nti.appserver.workspaces.interfaces import ICollection
-
-from nti.contentlibrary.filesystem import DynamicFilesystemLibrary as DynamicLibrary
 
 from nti.dataserver import  users
 from nti.dataserver import interfaces as nti_interfaces
 
-from nti.externalization import interfaces as ext_interfaces
 from nti.externalization.externalization import toExternalObject
 
 from nti.ntiids import ntiids
@@ -75,8 +65,6 @@ from nti.dataserver.tests import mock_dataserver
 # registrations to generate links.
 # TODO: Break this dep.
 from nti.app.testing.application_webtest import ApplicationLayerTest
-
-from nti.app.testing.layers import NewRequestLayerTest
 
 from nti.dataserver.tests.mock_dataserver import DataserverLayerTest
 
@@ -354,100 +342,6 @@ class TestUserService(ApplicationLayerTest):
 		vocab = component.getUtility( sch_interfaces.IVocabularyFactory, "Creatable External Object Types" )( user )
 		terms = [x.token for x in vocab]
 		assert_that( 'application/vnd.nextthought.canvasurlshape', is_not( is_in( terms ) ) )
-
-class TestLibraryCollectionDetailExternalizer(NewRequestLayerTest):
-
-	def setUp(self):
-		super(TestLibraryCollectionDetailExternalizer,self).setUp()
-
-		self.__policy = component.queryUtility( pyramid.interfaces.IAuthenticationPolicy )
-		self.__acl_policy = component.queryUtility( pyramid.interfaces.IAuthorizationPolicy )
-
-		self.temp_dir = tempfile.mkdtemp()
-		self.entry_dir =  os.path.join( self.temp_dir, 'TheEntry' )
-		os.mkdir( self.entry_dir )
-		with open( os.path.join( self.entry_dir, 'eclipse-toc.xml' ), 'w' ) as f:
-			f.write( """<?xml version="1.0"?>
-			<toc NTIRelativeScrollHeight="58" href="index.html"
-			icon="icons/Faa%20Aviation%20Maintenance%20Technician%20Knowledge%20Test%20Guide-Icon.png"
-			label="FAA Aviation Maintenance Technician Knowledge Test" ntiid="tag:nextthought.com,2011-10:foo-bar-baz" thumbnail="./thumbnails/index.png">
-			<topic label="C1" href="faa-index.html" ntiid="tag:nextthought.com,2011-10:foo-bar-baz.child"/>
-			</toc>""")
-		self.library = DynamicLibrary( self.temp_dir )
-		self.library.syncContentPackages()
-
-		class Policy(object):
-			interface.implements( pyramid.interfaces.IAuthenticationPolicy )
-			def authenticated_userid( self, request ):
-				return 'jason.madden@nextthought.com'
-			def effective_principals( self, request ):
-				result = [nti_interfaces.IPrincipal(x) for x in [	self.authenticated_userid(request),
-																	nti_interfaces.AUTHENTICATED_GROUP_NAME,
-																	nti_interfaces.EVERYONE_GROUP_NAME]]
-				return frozenset( result )
-
-		self.policy = Policy()
-		component.provideUtility( self.policy )
-		self.acl_policy = pyramid_authorization.ZopeACLAuthorizationPolicy()
-		component.provideUtility( self.acl_policy )
-
-		self.library_workspace = component.getMultiAdapter( (self.library, self.request), IWorkspace )
-		self.library_collection = self.library_workspace.collections[0]
-
-	def tearDown(self):
-		shutil.rmtree( self.temp_dir )
-		component.getGlobalSiteManager().unregisterUtility( self.policy )
-		component.getGlobalSiteManager().unregisterUtility( self.acl_policy )
-		if self.__policy:
-			component.provideUtility(self.__policy)
-		if self.__acl_policy:
-			component.provideUtility(self.__acl_policy)
-
-		super(TestLibraryCollectionDetailExternalizer,self).tearDown()
-
-	def test_no_acl_file(self):
-		external = ext_interfaces.IExternalObject( self.library_collection ).toExternalObject()
-		assert_that( external, has_entry( 'titles', has_length( 1 ) ) )
-
-	def test_malformed_acl_file_denies_all(self):
-		with open( os.path.join( self.entry_dir, '.nti_acl' ), 'w' ) as f:
-			f.write( "This file is invalid" )
-		external = ext_interfaces.IExternalObject( self.library_collection ).toExternalObject()
-		assert_that( external, has_entry( 'titles', has_length( 0 ) ) )
-
-	def test_specific_acl_file_forbids(self):
-		acl_file = os.path.join( self.entry_dir, '.nti_acl' )
-		with open( acl_file, 'w' ) as f:
-			f.write( "Allow:User:[nti.actions.create]\n" )
-			f.write( 'Deny:system.Everyone:All\n' )
-
-		external = toExternalObject( self.library_collection )
-		assert_that( external, has_entry( 'titles', has_length( 0 ) ) )
-
-	def test_specific_acl_to_user(self):
-		acl_file = os.path.join( self.entry_dir, '.nti_acl' )
-
-		# Now, grant it to a user
-		with open( acl_file, 'w' ) as f:
-			f.write( "Allow:jason.madden@nextthought.com:[zope.View]\n" )
-
-		external = toExternalObject( self.library_collection )
-		assert_that( external, has_entry( 'titles', has_length( 1 ) ) )
-
-	def test_specific_acl_to_user_chapter(self):
-		acl_file = os.path.join( self.entry_dir, '.nti_acl' )
-
-		# Back to the original entry on the ACL, denying it to everyone
-		with open( acl_file, 'w' ) as f:
-			f.write( "Allow:User:[nti.actions.create]\n" )
-			f.write( 'Deny:system.Everyone:All\n' )
-
-		# But the first chapter is allowed to the user:
-		with open( acl_file + '.1', 'w' ) as f:
-			f.write( "Allow:jason.madden@nextthought.com:[zope.View]\n" )
-
-		external = toExternalObject( self.library_collection )
-		assert_that( external, has_entry( 'titles', has_length( 1 ) ) )
 
 class TestFriendsListContainerCollection(DataserverLayerTest, tests.TestBaseMixin):
 
