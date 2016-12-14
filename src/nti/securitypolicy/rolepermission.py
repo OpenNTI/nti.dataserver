9a48c31308312042228ed567d645a57339975ffa
Chris Utz <chris.utz@nextthought.com>
2016-12-14 06:09:52 +0000

    add a basic, non-persistent IRolePermissionMap that can be used to grant a static set of permissions by role.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@102536 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/securitypolicy/rolepermission.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+
+from zope.securitypolicy.interfaces import Allow
+from zope.securitypolicy.interfaces import IRolePermissionMap
+from zope.securitypolicy.interfaces import Unset
+
+from zope.securitypolicy.securitymap import SecurityMap
+
+
+@interface.implementer(IRolePermissionMap)
+class PermissionGrantingRoleMap(SecurityMap):
+	"""
+	A basic role permission map that grants a set
+	of permissions to roles.
+	"""
+
+	def __init__(self, grants):
+		super(PermissionGrantingRoleMap, self).__init__()
+
+		for role in grants:
+			for perm in grants.get(role, ()):
+				self.addCell(perm, role, Allow)
+
+
+	getRolesForPermission = SecurityMap.getRow
+	getPermissionsForRole = SecurityMap.getCol
+	getRolesAndPermissions = SecurityMap.getAllCells
+
+	def getSetting(self, permission_id, role_id, default=Unset):
+		return self.queryCell(permission_id, role_id, default)
