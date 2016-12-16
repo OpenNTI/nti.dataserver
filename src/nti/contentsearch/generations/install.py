42bddcca6cf1ea6a88ec8598ffb29a793004db21
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-16 15:30:39 +0000

    one more generation
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@102750 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/generations/install.py
+++ src/nti/contentsearch/generations/install.py
@@ -9,7 +9,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 31
+generation = 32
 
 from zope.generations.generations import SchemaManager
 
