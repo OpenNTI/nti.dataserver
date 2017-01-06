b166e47fcb076e00219143f09fdc1968e574f9ad
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-06 16:30:03 +0000

    correct pop field
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@103797 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/search_utils.py
+++ src/nti/contentsearch/search_utils.py
@@ -157,7 +157,7 @@ def create_queryobject(username, params, clazz=QueryObject):
                 packages.append(root_ntiid)
     args['packages'] = sorted(set(args['packages']))  # predictable order
 
-    fields = args.pop('fields') or args.pop('searchFields')
+    fields = args.pop('fields', None) or args.pop('searchFields', None)
     if fields:
         fields = fields.split() if isinstance(
             fields, six.string_types) else fields
