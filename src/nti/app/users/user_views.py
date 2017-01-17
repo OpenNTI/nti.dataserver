dcb80f8de4a618d3a41597d74ea80b3746f940c3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-17 16:53:50 +0000

    pylint;pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@104589 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/users/user_views.py
+++ src/nti/app/users/user_views.py
@@ -13,7 +13,6 @@ logger = __import__('logging').getLogger(__name__)
 
 import six
 import time
-import datetime
 
 from pyramid import httpexceptions as hexc
 
@@ -53,196 +52,203 @@ from nti.links import render_link
 
 ITEMS = StandardExternalFields.ITEMS
 
+
 def _image_view(context, request, func):
-	"""
-	Redirects to the location of the actual image.
-	"""
-	# Use a 302 response to tell clients where to go,
-	# and let them cache it for awhile (a 303 is completely
-	# uncachable). We expect that this method will not be
-	# hit by the actual user himself, only his friends, so
-	# he won't notice a stale response.
+    """
+    Redirects to the location of the actual image.
+    """
+    # Use a 302 response to tell clients where to go,
+    # and let them cache it for awhile (a 303 is completely
+    # uncachable). We expect that this method will not be
+    # hit by the actual user himself, only his friends, so
+    # he won't notice a stale response.
 
-	# We use a private method to do this because we rely
-	# on implementation details about the user and his data.
+    # We use a private method to do this because we rely
+    # on implementation details about the user and his data.
 
-	url_or_link = func(context)
-	if url_or_link is None:
-		raise hexc.HTTPNotFound()
+    url_or_link = func(context)
+    if url_or_link is None:
+        raise hexc.HTTPNotFound()
 
-	if not isinstance(url_or_link, six.string_types):
-		# In this case, we have a file we're hosting.
-		# What happens when the user changes or removes that file?
-		# we're sending direct OID links, does it still work? Or will it 404?
-		url_or_link = render_link(url_or_link)
+    if not isinstance(url_or_link, six.string_types):
+        # In this case, we have a file we're hosting.
+        # What happens when the user changes or removes that file?
+        # we're sending direct OID links, does it still work? Or will it 404?
+        url_or_link = render_link(url_or_link)
 
-	result = hexc.HTTPFound(url_or_link)
-	# Let it be cached for a bit. gravatar uses 5 minutes
-	result.cache_control.max_age = 300
-	result.expires = time.time() + 300
-	return result
+    result = hexc.HTTPFound(url_or_link)
+    # Let it be cached for a bit. gravatar uses 5 minutes
+    result.cache_control.max_age = 300
+    result.expires = time.time() + 300
+    return result
+
 
 @view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 context=IUser,
-			 request_method='GET',
-			 name='avatar')
+             renderer='rest',
+             context=IUser,
+             request_method='GET',
+             name='avatar')
 def avatar_view(context, request):
-	result = _image_view(context, request, _avatar_url)
-	return result
+    result = _image_view(context, request, _avatar_url)
+    return result
+
 
 @view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 context=IEntity,
-			 request_method='GET',
-			 name='background')
+             renderer='rest',
+             context=IEntity,
+             request_method='GET',
+             name='background')
 def background_view(context, request):
-	result = _image_view(context, request, _background_url)
-	return result
+    result = _image_view(context, request, _background_url)
+    return result
+
 
 @view_config(route_name='objects.generic.traversal',
-			 name='memberships',
-			 request_method='GET',
-			 context=IUser)
+             name='memberships',
+             request_method='GET',
+             context=IUser)
 class UserMembershipsView(AbstractAuthenticatedView, BatchingUtilsMixin):
 
-	_DEFAULT_BATCH_SIZE = 50
-	_DEFAULT_BATCH_START = 0
+    _DEFAULT_BATCH_SIZE = 50
+    _DEFAULT_BATCH_START = 0
 
-	def _batch_params(self):
-		self.batch_size, self.batch_start = self._get_batch_size_start()
-		self.limit = self.batch_start + self.batch_size + 2
-		self.batch_after = None
-		self.batch_before = None
+    def _batch_params(self):
+        self.batch_size, self.batch_start = self._get_batch_size_start()
+        self.limit = self.batch_start + self.batch_size + 2
+        self.batch_after = None
+        self.batch_before = None
 
-	def __call__(self):
-		if self.remoteUser is None:
-			raise hexc.HTTPForbidden()
+    def __call__(self):
+        if self.remoteUser is None:
+            raise hexc.HTTPForbidden()
 
-		self._batch_params()
-		context = self.request.context
-		memberships = set(context.dynamic_memberships)
-		memberships.update(set(context.friendsLists.values()))
+        self._batch_params()
+        context = self.request.context
+        memberships = set(context.dynamic_memberships)
+        memberships.update(set(context.friendsLists.values()))
 
-		everyone = Entity.get_entity(u'Everyone')
-		def _selector(x):
-			result = None
-			if x == everyone:  # always
-				result = None
-			elif 	ICommunity.providedBy(x) \
-				and not IDisallowMembershipOperations.providedBy(x) \
-				and (x.public or self.remoteUser in x):
-				result = toExternalObject(x, name='summary')
-			elif 	IDynamicSharingTargetFriendsList.providedBy(x) \
-				and (self.remoteUser in x or self.remoteUser == x.creator):
-				result = toExternalObject(x)
-			return result
+        everyone = Entity.get_entity(u'Everyone')
 
-		result = LocatedExternalDict()
-		self._batch_items_iterable(result, memberships,
-								   number_items_needed=self.limit,
-								   batch_size=self.batch_size,
-								   batch_start=self.batch_start,
-								   selector=_selector)
-		return result
+        def _selector(x):
+            result = None
+            if x == everyone:  # always
+                result = None
+            elif 	ICommunity.providedBy(x) \
+                and not IDisallowMembershipOperations.providedBy(x) \
+                and (x.public or self.remoteUser in x):
+                result = toExternalObject(x, name='summary')
+            elif 	IDynamicSharingTargetFriendsList.providedBy(x) \
+                    and (self.remoteUser in x or self.remoteUser == x.creator):
+                result = toExternalObject(x)
+            return result
+
+        result = LocatedExternalDict()
+        self._batch_items_iterable(result, memberships,
+                                   number_items_needed=self.limit,
+                                   batch_size=self.batch_size,
+                                   batch_start=self.batch_start,
+                                   selector=_selector)
+        return result
+
 
 class UserUpdateView(UGDPutView):
-	"""
-	A concrete class to update user objects. Currently, we just exclude
-	`DynamicMemberships` from the inbound user object.  We don't care
-	about it and the internalization factory tries to create a None username DFL.
-	"""
+    """
+    A concrete class to update user objects. Currently, we just exclude
+    `DynamicMemberships` from the inbound user object.  We don't care
+    about it and the internalization factory tries to create a None username DFL.
+    """
 
-	def readInput(self, value=None):
-		value = super(UserUpdateView, self).readInput(value=value)
-		value.pop('DynamicMemberships', None)
-		self.validateInput(value)
-		return value
+    def readInput(self, value=None):
+        value = super(UserUpdateView, self).readInput(value=value)
+        value.pop('DynamicMemberships', None)
+        self.validateInput(value)
+        return value
 
-	@staticmethod
-	def is_valid_year(year):
-		if year is None:
-			return False
-		elif isinstance(year, six.string_types):
-			try:
-				year = int(year)
-			except (ValueError):
-				return False
-		if year < 1900:
-			return False
-		return True
+    @staticmethod
+    def is_valid_year(year):
+        if year is None:
+            return False
+        elif isinstance(year, six.string_types):
+            try:
+                year = int(year)
+            except (ValueError):
+                return False
+        if year < 1900:
+            return False
+        return True
 
-	def validateInput(self, source):
-		# Assume input is valid until shown otherwise
-		# Validate that startYear < endYear for education,
-		# and that they are in an appropriate range
-		for education in source.get('education') or ():
-			start_year = education.get('startYear', None)
-			end_year = education.get('endYear', None)
-			if start_year and not self.is_valid_year(start_year):
-				raise_json_error(
-						self.request,
-						hexc.HTTPUnprocessableEntity,
-						{
-							u'message': _('Invalid education start year.'),
-							u'code': 'InvalidStartYear',
-						},
-						None)
-			if end_year and not self.is_valid_year(end_year):
-				raise_json_error(
-						self.request,
-						hexc.HTTPUnprocessableEntity,
-						{
-							u'message': _('Invalid education end year.'),
-							u'code': 'InvalidEndYear',
-						},
-						None)
-			if start_year and end_year and not start_year <= end_year:
-				raise_json_error(
-						self.request,
-						hexc.HTTPUnprocessableEntity,
-						{
-							u'message': _('Invalid education year range.'),
-							u'code': 'InvalidYearRange',
-						},
-						None)
+    def validateInput(self, source):
+        # Assume input is valid until shown otherwise
+        # Validate that startYear < endYear for education,
+        # and that they are in an appropriate range
+        for education in source.get('education') or ():
+            start_year = education.get('startYear', None)
+            end_year = education.get('endYear', None)
+            if start_year and not self.is_valid_year(start_year):
+                raise_json_error(
+                    self.request,
+                    hexc.HTTPUnprocessableEntity,
+                    {
+                        u'message': _('Invalid education start year.'),
+                        u'code': 'InvalidStartYear',
+                    },
+                    None)
+            if end_year and not self.is_valid_year(end_year):
+                raise_json_error(
+                    self.request,
+                    hexc.HTTPUnprocessableEntity,
+                    {
+                        u'message': _('Invalid education end year.'),
+                        u'code': 'InvalidEndYear',
+                    },
+                    None)
+            if start_year and end_year and not start_year <= end_year:
+                raise_json_error(
+                    self.request,
+                    hexc.HTTPUnprocessableEntity,
+                    {
+                        u'message': _('Invalid education year range.'),
+                        u'code': 'InvalidYearRange',
+                    },
+                    None)
 
-		# Same thing for professional experience
-		for position in source.get('positions') or ():
-			start_year = position.get('startYear', None)
-			end_year = position.get('endYear', None)
-			if start_year and not self.is_valid_year(start_year):
-				raise_json_error(
-						self.request,
-						hexc.HTTPUnprocessableEntity,
-						{
-							u'message': _('Invalid position start year.'),
-							u'code': 'InvalidStartYear',
-						},
-						None)
-			if end_year and not self.is_valid_year(end_year):
-				raise_json_error(
-						self.request,
-						hexc.HTTPUnprocessableEntity,
-						{
-							u'message': _('Invalid position end year.'),
-							u'code': 'InvalidEndYear',
-						},
-						None)
-			if start_year and end_year and not start_year <= end_year:
-				raise_json_error(
-						self.request,
-						hexc.HTTPUnprocessableEntity,
-						{
-							u'message': _('Invalid position year range.'),
-							u'code': 'InvalidYearRange',
-						},
-						None)
-		return True
+        # Same thing for professional experience
+        for position in source.get('positions') or ():
+            start_year = position.get('startYear', None)
+            end_year = position.get('endYear', None)
+            if start_year and not self.is_valid_year(start_year):
+                raise_json_error(
+                    self.request,
+                    hexc.HTTPUnprocessableEntity,
+                    {
+                        u'message': _('Invalid position start year.'),
+                        u'code': 'InvalidStartYear',
+                    },
+                    None)
+            if end_year and not self.is_valid_year(end_year):
+                raise_json_error(
+                    self.request,
+                    hexc.HTTPUnprocessableEntity,
+                    {
+                        u'message': _('Invalid position end year.'),
+                        u'code': 'InvalidEndYear',
+                    },
+                    None)
+            if start_year and end_year and not start_year <= end_year:
+                raise_json_error(
+                    self.request,
+                    hexc.HTTPUnprocessableEntity,
+                    {
+                        u'message': _('Invalid position year range.'),
+                        u'code': 'InvalidYearRange',
+                    },
+                    None)
+        return True
+
 
 @view_config(context=IUsersFolder,
-			 request_method='GET')
+             request_method='GET')
 class UsersGetView(GenericGetView):
 
-	def __call__(self):
-		raise hexc.HTTPForbidden()
+    def __call__(self):
+        raise hexc.HTTPForbidden()
