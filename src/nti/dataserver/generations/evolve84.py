a7e71753a0e3740faa018b6f55fde6fd91bfa286
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-20 21:56:38 +0000

    remove invalid catalogs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@104938 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/evolve84.py
+++ src/nti/dataserver/generations/evolve84.py
@@ -18,6 +18,8 @@ from zope.component.hooks import setHooks
 
 from zope.catalog.interfaces import ICatalog
 
+from zope.intid.interfaces import IIntIds
+
 from zope.index.topic import TopicIndex
 
 from zope.index.topic.interfaces import ITopicFilteredSet
@@ -33,52 +35,61 @@ from nti.zope_catalog.interfaces import IKeywordIndex
 
 
 def do_evolve(context):
-    setHooks()
-    conn = context.connection
-    root = conn.root()
-    ds_folder = root['nti.dataserver']
+	setHooks()
+	conn = context.connection
+	root = conn.root()
+	ds_folder = root['nti.dataserver']
 
-    with site(ds_folder):
-        assert  component.getSiteManager() == ds_folder.getSiteManager(), \
-            "Hooks not installed?"
+	with site(ds_folder):
+		assert  component.getSiteManager() == ds_folder.getSiteManager(), \
+				"Hooks not installed?"
 
-        queue = metadata_queue()
-        if queue is None:
-            return
+		queue = metadata_queue()
+		if queue is None:
+			return
 
-        rest_ids = set()
-        creator_ids = set()
-        creator_index = dataserver_metadata_catalog()[IX_CREATOR]
-        for _, catalog in component.getUtilitiesFor(ICatalog):
-            if not hasattr(catalog, "values"):
-                continue
-            for index in catalog.values():
-                if index is creator_index:
-                    s = creator_ids
-                else:
-                    s = rest_ids
+		lsm = ds_folder.getSiteManager()
+		intids = lsm.getUtility(IIntIds)
+		
+		rest_ids = set()
+		creator_ids = set()
+		creator_index = dataserver_metadata_catalog()[IX_CREATOR]
+		for name, catalog in list(component.getUtilitiesFor(ICatalog)):
+			if not hasattr(catalog, "values"): # invalid catalog
+				obj = lsm.queryUtility(provided=ICatalog, name=name)
+				if intids.queryId(obj) is not None:
+					intids.unregister(obj)
+				lsm.unregisterUtility(obj, provided=ICatalog, name=name)
+				obj.__parent__ = None
+				continue
 
-                if IIndexValues.providedBy(index):
-                    s.update(index.ids())
-                elif IKeywordIndex.providedBy(index):
-                    s.update(index.ids())
-                elif isinstance(index, TopicIndex):
-                    for filter_index in index._filters.values():
-                        if ITopicFilteredSet.providedBy(filter_index):
-                            s.update(filter_index.getIds())
+			for index in catalog.values():
+				if index is creator_index:
+					s = creator_ids
+				else:
+					s = rest_ids
 
-        # index difference
-        for uid in rest_ids.difference(creator_ids):
-            try:
-                queue.add(uid)
-            except TypeError:
-                pass
+				if IIndexValues.providedBy(index):
+					s.update(index.ids())
+				elif IKeywordIndex.providedBy(index):
+					s.update(index.ids())
+				elif isinstance(index, TopicIndex):
+					for filter_index in index._filters.values():
+						if ITopicFilteredSet.providedBy(filter_index):
+							s.update(filter_index.getIds())
 
-        logger.info('Dataserver evolution %s done.', generation)
+		# index difference
+		for uid in rest_ids.difference(creator_ids):
+			try:
+				queue.add(uid)
+			except TypeError:
+				pass
+
+		logger.info('Dataserver evolution %s done.', generation)
 
 
 def evolve(context):
-    """
-    Evolve to gen 84 by reindexing the missing creators
-    """
-    do_evolve(context)
+	"""
+	Evolve to gen 84 by reindexing the missing creators
+	"""
+	do_evolve(context)
