a45567de54a83a78f76e096f90e90cf935505ac9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-22 02:43:26 +0000

    prepare for pyramid 1.8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@104959 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/nti_pserve.py
+++ src/nti/appserver/nti_pserve.py
@@ -25,14 +25,17 @@ from gevent.signal import getsignal as gevent_getsignal
 
 import signal as _signal
 
+
 def _patched_signal(signalnum, handler):
-	if signalnum == _signal.SIGCHLD and handler in (_signal.SIG_DFL, _signal.SIG_IGN):
-		if gevent_getsignal(_signal.SIGCHLD) not in (_signal.SIG_DFL, _signal.SIG_IGN):
-			# They tried to ignore/default action this signal (it's ignored by default)
-			# and previous code was there, so make sure the previous code doesn't run
-			return gevent_signal(signalnum, lambda *args: None)
-		return
-	return gevent_signal(signalnum, handler)
+    if        signalnum == _signal.SIGCHLD \
+        and handler in (_signal.SIG_DFL, _signal.SIG_IGN):
+        if gevent_getsignal(_signal.SIGCHLD) not in (_signal.SIG_DFL, _signal.SIG_IGN):
+            # They tried to ignore/default action this signal (it's ignored by default)
+            # and previous code was there, so make sure the previous code
+            # doesn't run
+            return gevent_signal(signalnum, lambda *args: None)
+        return
+    return gevent_signal(signalnum, handler)
 gevent.signal.signal = _patched_signal
 
 # Note that we must not import *anything* before the patch
@@ -53,16 +56,18 @@ patch_random_seed_on_import.patch()
 import sys
 from pkg_resources import load_entry_point, get_distribution
 
-def main():
-	# We used to monkey patch some things in 1.3. We no longer
-	# do now that we expect to be on 1.5. Check this.
-	pyramid_dist = get_distribution('pyramid')
-	if pyramid_dist and pyramid_dist.has_version():
-		assert pyramid_dist.version.startswith('1.7')
 
-	sys.exit(
-		load_entry_point('pyramid', 'console_scripts', 'pserve')()
-	)
+def main():
+    # We used to monkey patch some things in 1.3. We no longer
+    # do now that we expect to be on 1.5. Check this.
+    pyramid_dist = get_distribution('pyramid')
+    if pyramid_dist and pyramid_dist.has_version():
+        assert pyramid_dist.version.startswith('1.7') \
+            or pyramid_dist.version.startswith('1.8')
+
+    sys.exit(
+        load_entry_point('pyramid', 'console_scripts', 'pserve')()
+    )
 
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())
