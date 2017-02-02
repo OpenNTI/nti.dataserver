a74d950a0c227e4b60e0ab577a56658e0bda496f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-02 22:07:15 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@105723 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/intids/tests/test_views.py
+++ src/nti/app/intids/tests/test_views.py
@@ -31,29 +31,30 @@ import nti.dataserver.tests.mock_dataserver as mock_dataserver
 INTID = StandardExternalFields.INTID
 NTIID = StandardExternalFields.NTIID
 
+
 class TestViews(ApplicationLayerTest):
 
-	@WithSharedApplicationMockDS(users=True, testapp=True)
-	def test_intid_resolver(self):
-		with mock_dataserver.mock_db_trans(self.ds):
-			user = self._get_user()
-			intids = component.getUtility(IIntIds)
-			uid = intids.getId(user)
-		path = '/dataserver2/@@IntIdResolver/%s' % uid
-		res = self.testapp.get(path, status=200)
-		assert_that(res.json_body, has_entry('Class', 'User'))
-		
-	@WithSharedApplicationMockDS(users=True, testapp=True)
-	def test_register_with_intid(self):
-		with mock_dataserver.mock_db_trans(self.ds):
-			data = PersistentMapping()
-			mock_dataserver.current_transaction.add(data)
-			intids = component.getUtility(IIntIds)
-			uid = intids.queryId(data)
-			assert_that(uid, is_(none()))
-			oid = to_external_ntiid_oid(data)
-		data = {'ntiid': oid}
-		res = self.testapp.post_json('/dataserver2/@@RegisterWithIntId',
-							   		 data, status=200)
-		assert_that(res.json_body, has_entry(NTIID, is_(oid)))
-		assert_that(res.json_body, has_entry(INTID, is_not(none())))
+    @WithSharedApplicationMockDS(users=True, testapp=True)
+    def test_intid_resolver(self):
+        with mock_dataserver.mock_db_trans(self.ds):
+            user = self._get_user()
+            intids = component.getUtility(IIntIds)
+            uid = intids.getId(user)
+        path = '/dataserver2/@@IntIdResolver/%s' % uid
+        res = self.testapp.get(path, status=200)
+        assert_that(res.json_body, has_entry('Class', 'User'))
+
+    @WithSharedApplicationMockDS(users=True, testapp=True)
+    def test_register_with_intid(self):
+        with mock_dataserver.mock_db_trans(self.ds):
+            data = PersistentMapping()
+            mock_dataserver.current_transaction.add(data)
+            intids = component.getUtility(IIntIds)
+            uid = intids.queryId(data)
+            assert_that(uid, is_(none()))
+            oid = to_external_ntiid_oid(data)
+        data = {'ntiid': oid}
+        res = self.testapp.post_json('/dataserver2/@@RegisterWithIntId',
+                                     data, status=200)
+        assert_that(res.json_body, has_entry(NTIID, is_(oid)))
+        assert_that(res.json_body, has_entry(INTID, is_not(none())))
