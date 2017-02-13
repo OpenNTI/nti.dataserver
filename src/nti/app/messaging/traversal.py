dfba3763eb91ebecd52fe0ff62a813e5bc5f1f80
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-13 23:05:46 +0000

    lift more code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@106436 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/messaging/traversal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+
+from zope.location.interfaces import LocationError
+
+from zope.traversing.interfaces import IPathAdapter
+
+from nti.messaging.interfaces import IMailbox
+
+
+@interface.implementer(IPathAdapter)
+def _mailbox_path_adapter(user, request):
+    mailbox = IMailbox(user, None)
+    if not mailbox:
+        raise LocationError(user.username)
+    return mailbox
