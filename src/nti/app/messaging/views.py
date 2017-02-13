213cbe4ccafb437c9dade3d8e7ff70ef2e7b8f0b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-13 23:53:39 +0000

    add view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@106444 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/messaging/views.py
+++ src/nti/app/messaging/views.py
@@ -13,8 +13,11 @@ from pyramid.view import view_config
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
+from nti.app.messaging.interfaces import IConversationProvider
+
 from nti.dataserver import authorization as nauth
 
+from nti.externalization.interfaces import LocatedExternalDict
 from nti.externalization.interfaces import StandardExternalFields
 
 from nti.messaging.interfaces import IMailbox
@@ -33,3 +36,23 @@ class MailboxGetView(AbstractAuthenticatedView):
 
     def __call__(self):
         return self.context
+
+
+@view_config(route_name='objects.generic.traversal',
+             renderer='rest',
+             permission=nauth.ACT_READ,
+             request_method='GET',
+             name="conversations",
+             context=IMailbox)
+class MailboxConversationsGetView(AbstractAuthenticatedView):
+
+    def __call__(self):
+        provider = IConversationProvider(self.context)
+        conversations = provider.conversations(self.remoteUser)
+        result = LocatedExternalDict()
+        result.__name__ = self.request.view_name
+        result.__parent__ = self.request.context
+        result.lastModified = self.context.lastModified
+        result[ITEMS] = conversations
+        result[TOTAL] = result[ITEM_COUNT] = len(conversations)
+        return result
