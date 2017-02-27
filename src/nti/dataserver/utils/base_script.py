4e44e9f1794e4b33cded3e17dbe2a18f95e95951
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-27 20:09:17 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@107568 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/utils/base_script.py
+++ src/nti/dataserver/utils/base_script.py
@@ -29,55 +29,65 @@ from z3c.autoinclude.zcml import includePluginsDirective
 
 from nti.site.site import get_site_for_site_names
 
+
 class PluginPoint(Contained):
 
-	def __init__(self, name):
-		self.__name__ = name
+    def __init__(self, name):
+        self.__name__ = name
 
 PP_APP = PluginPoint('nti.app')
 PP_APP_SITES = PluginPoint('nti.app.sites')
 PP_APP_PRODUCTS = PluginPoint('nti.app.products')
 
-def create_context(env_dir=None, with_library=False, context=None, plugins=True, 
-				   slugs=True, slugs_files=("*.zcml",)):
-	etc = os.getenv('DATASERVER_ETC_DIR') or os.path.join(env_dir, 'etc')
-	etc = os.path.expanduser(etc)
 
-	context = context or config.ConfigurationMachine()
-	xmlconfig.registerCommonDirectives(context)
+def create_context(env_dir=None, with_library=False, context=None, plugins=True,
+                   slugs=True, slugs_files=("*.zcml",)):
+    etc = os.getenv('DATASERVER_ETC_DIR') or os.path.join(env_dir, 'etc')
+    etc = os.path.expanduser(etc)
 
-	slugs_dir = os.path.join(etc, 'package-includes')
-	if slugs and os.path.exists(slugs_dir) and os.path.isdir(slugs_dir):
-		package = dottedname.resolve('nti.dataserver')
-		context = xmlconfig.file('configure.zcml', package=package, context=context)
-		for name in slugs_files or ():
-			xmlconfig.include(context, files=os.path.join(slugs_dir, name),
-						  	 package='nti.appserver')
-	if with_library:
-		library_zcml = os.path.join(etc, 'library.zcml')
-		if not os.path.exists(library_zcml):
-			logger.warn("Could not locate library zcml file %s", library_zcml)
-		else:
-			xmlconfig.include(context, file=library_zcml, package='nti.appserver')
+    context = context or config.ConfigurationMachine()
+    xmlconfig.registerCommonDirectives(context)
 
-	# Include zope.browserpage.meta.zcm for tales:expressiontype
-	# before including the products
-	xmlconfig.include(context, file="meta.zcml", package=zope.browserpage)
+    slugs_dir = os.path.join(etc, 'package-includes')
+    if slugs and os.path.exists(slugs_dir) and os.path.isdir(slugs_dir):
+        package = dottedname.resolve('nti.dataserver')
+        context = xmlconfig.file('configure.zcml',
+                                 package=package,
+                                 context=context)
+        for name in slugs_files or ():
+            xmlconfig.include(context, 
+                              files=os.path.join(slugs_dir, name),
+                              package='nti.appserver')
+    if with_library:
+        library_zcml = os.path.join(etc, 'library.zcml')
+        if not os.path.exists(library_zcml):
+            logger.warn("Could not locate library zcml file %s", library_zcml)
+        else:
+            xmlconfig.include(context,
+                              file=library_zcml,
+                              package='nti.appserver')
 
-	# include plugins
-	if plugins:
-		includePluginsDirective(context, PP_APP)
-		includePluginsDirective(context, PP_APP_SITES)
-		includePluginsDirective(context, PP_APP_PRODUCTS)
+    # Include zope.browserpage.meta.zcm for tales:expressiontype
+    # before including the products
+    xmlconfig.include(context,
+                      file="meta.zcml", 
+                      package=zope.browserpage)
 
-	return context
+    # include plugins
+    if plugins:
+        includePluginsDirective(context, PP_APP)
+        includePluginsDirective(context, PP_APP_SITES)
+        includePluginsDirective(context, PP_APP_PRODUCTS)
+
+    return context
+
 
 def set_site(site):
-	if site:
-		cur_site = hooks.getSite()
-		new_site = get_site_for_site_names((site,), site=cur_site)
-		if new_site is cur_site:
-			raise ValueError("Unknown site name", site)
-		hooks.setSite(new_site)
-		return new_site
+    if site:
+        cur_site = hooks.getSite()
+        new_site = get_site_for_site_names((site,), site=cur_site)
+        if new_site is cur_site:
+            raise ValueError("Unknown site name", site)
+        hooks.setSite(new_site)
+        return new_site
 setSite = set_site
