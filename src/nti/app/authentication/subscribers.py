862891b5696988bb08e26cd77efbffceb7323d4a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-13 21:04:33 +0000

    pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@108768 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/authentication/subscribers.py
+++ src/nti/app/authentication/subscribers.py
@@ -15,60 +15,67 @@ import binascii
 
 from zope import component
 
+from zope.authentication.loginpassword import LoginPassword
+
 from zope.lifecycleevent import IObjectCreatedEvent
 
 from pyramid.interfaces import IRequest
 
 from pyramid.request import Request
 
+
 @component.adapter(IRequest, IObjectCreatedEvent)
 def _decode_username_request_event(request, event):
-	"""
-	Decodes %40 in a Basic Auth username into an @, and canonizes the
-	incoming username to lower case. Modifies the request if
-	necessary.
+    """
+    Decodes %40 in a Basic Auth username into an @, and canonizes the
+    incoming username to lower case. Modifies the request if
+    necessary.
 
-	Our usernames may be in email/domain syntax. This sometimes
-	confuses browsers who expect to use an @ to separate user and
-	password, so clients often workaround this by percent-encoding the
-	username. Reverse that step here. This should be an outer layer
-	before authkit gets to do anything.
+    Our usernames may be in email/domain syntax. This sometimes
+    confuses browsers who expect to use an @ to separate user and
+    password, so clients often workaround this by percent-encoding the
+    username. Reverse that step here. This should be an outer layer
+    before authkit gets to do anything.
 
-	:return: Tuple (user,pass).
-	"""
-	try:
-		return _decode_username_request(request)
-	except AttributeError:
-		# The dummy request doesn't have all the same header attributes
-		# as a real request, so turn it into a real request. This works
-		# because all the state is in the environment
-		return _decode_username_request(Request(request.environ))
+    :return: Tuple (user,pass).
+    """
+    try:
+        return _decode_username_request(request)
+    except AttributeError:
+        # The dummy request doesn't have all the same header attributes
+        # as a real request, so turn it into a real request. This works
+        # because all the state is in the environment
+        return _decode_username_request(Request(request.environ))
+
 
 def _decode_username_request(request):
-	authmeth, auth = request.authorization or ('', '')
-	if authmeth.lower() != b'basic':
-		return (None, None)
+    authmeth, auth = request.authorization or ('', '')
+    if authmeth.lower() != b'basic':
+        return (None, None)
 
-	# Remember here we're working with byte headers
-	try:
-		username, password = auth.strip().decode('base64').split(b':', 1)
-	except (ValueError, binascii.Error):  # pragma: no cover
-		return (None, None)
+    # Remember here we're working with byte headers
+    try:
+        username, password = auth.strip().decode('base64').split(b':', 1)
+    except (ValueError, binascii.Error):  # pragma: no cover
+        return (None, None)
 
-	# we only get here with two strings, although either could be empty
-	canonical_username = username.lower().replace(b'%40', b'@').strip() if username else username
-	if canonical_username != username:
-		username = canonical_username
-		auth = (username + b':' + password).encode('base64').strip()
-		request.authorization = (authmeth, auth)
-		request.remote_user = username
+    # we only get here with two strings, although either could be empty
+    if username:
+        canonical_username = username.lower().replace(b'%40', b'@').strip()
+    else:
+        canonical_username = username
+    if canonical_username != username:
+        username = canonical_username
+        auth = (username + b':' + password).encode('base64').strip()
+        request.authorization = (authmeth, auth)
+        request.remote_user = username
 
-	return (username, password)
+    return (username, password)
 
-from zope.authentication.loginpassword import LoginPassword
 
 @component.adapter(IRequest)
 class BasicAuthLoginPassword(LoginPassword):
 
-	def __init__(self, request):
-		super(BasicAuthLoginPassword, self).__init__(*_decode_username_request(request))
+    def __init__(self, request):
+        username = _decode_username_request(request)
+        super(BasicAuthLoginPassword, self).__init__(*username)
