06af32c6613736ee4bce7e7e4385fe4377c6446c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-13 23:31:22 +0000

    add acl provider
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@108792 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/recorder/acl.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.dataserver.authorization_acl import ace_allowing
+from nti.dataserver.authorization_acl import acl_from_aces
+
+from nti.dataserver.interfaces import ALL_PERMISSIONS
+
+from nti.dataserver.interfaces import IACLProvider
+
+from nti.recorder.interfaces import ITransactionRecord
+
+from nti.property.property import Lazy
+
+
+@component.adapter(ITransactionRecord)
+@interface.implementer(IACLProvider)
+class TransactionRecordACLProvider(object):
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def __parent__(self):
+        return self.context.__parent__
+
+    @Lazy
+    def __acl__(self):
+        creator = self.context.creator or self.context.principal
+        result = acl_from_aces(ace_allowing(creator, ALL_PERMISSIONS, type(self)))
+        return result
