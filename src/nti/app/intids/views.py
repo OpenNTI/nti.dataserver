8b26658fa1f1583958ec923ca9fa8328ca0fe37b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-19 22:42:06 +0000

    remove unused view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@109151 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/intids/views.py
+++ src/nti/app/intids/views.py
@@ -9,13 +9,9 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from requests.structures import CaseInsensitiveDict
-
 from zope import component
 
 from zope.intid.interfaces import IIntIds
-
-from persistent.interfaces import IPersistent
 
 from pyramid import httpexceptions as hexc
 
@@ -24,17 +20,10 @@ from pyramid.view import view_defaults
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
-from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
-
 from nti.dataserver import authorization as nauth
 from nti.dataserver.interfaces import IDataserverFolder
 
-from nti.externalization.interfaces import LocatedExternalDict
 from nti.externalization.interfaces import StandardExternalFields
-
-from nti.intid.common import addIntId
-
-from nti.ntiids.ntiids import find_object_with_ntiid
 
 INTID = StandardExternalFields.INTID
 NTIID = StandardExternalFields.NTIID
@@ -63,38 +52,4 @@ class IntIdResolverView(AbstractAuthenticatedView):
         result = intids.queryObject(uid)
         if result is None:
             raise hexc.HTTPNotFound()
-        return result
-
-
-@view_config(name='RegisterWithIntId')
-@view_defaults(route_name='objects.generic.traversal',
-               renderer='rest',
-               request_method='POST',
-               context=IDataserverFolder,
-               permission=nauth.ACT_NTI_ADMIN)
-class RegisterWithIntIdView(AbstractAuthenticatedView,
-                            ModeledContentUploadRequestUtilsMixin):
-
-    def readInput(self, value=None):
-        result = super(RegisterWithIntIdView, self).readInput(value=value)
-        return CaseInsensitiveDict(result)
-
-    def __call__(self):
-        values = self.readInput()
-        ntiid = values.get(NTIID)
-        if ntiid is None:
-            raise hexc.HTTPUnprocessableEntity("Must specify an NTIID")
-        obj = find_object_with_ntiid(ntiid)
-        if obj is None:
-            raise hexc.HTTPNotFound()
-        if not IPersistent.providedBy(obj):
-            raise hexc.HTTPUnprocessableEntity("Must be a persistent object")
-        intids = component.getUtility(IIntIds)
-        uid = intids.queryId(obj)
-        if uid is None:
-            addIntId(obj)
-            uid = intids.queryId(obj)
-        result = LocatedExternalDict()
-        result[INTID] = uid
-        result[NTIID] = ntiid
         return result
