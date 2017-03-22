ab4bd5853f07c4b9f57cad46e71b0ab3e0e97806
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-22 19:52:54 +0000

    pylint;pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@109378 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/liking_views.py
+++ src/nti/appserver/liking_views.py
@@ -14,106 +14,114 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
-from pyramid.view import view_config
 from pyramid.interfaces import IRequest
 
+from pyramid.view import view_config
+
 from nti.app.renderers.caching import uncached_in_response
+
 from nti.app.renderers.decorators import AbstractTwoStateViewLinkDecorator
 
-from nti.dataserver import liking
-
 from nti.dataserver import authorization as nauth
+
+from nti.dataserver import liking
 
 from nti.dataserver.interfaces import ILikeable
 from nti.dataserver.interfaces import IFavoritable
 
 from nti.externalization.interfaces import IExternalMappingDecorator
 
-@interface.implementer(IExternalMappingDecorator)
+
 @component.adapter(ILikeable, IRequest)
+@interface.implementer(IExternalMappingDecorator)
 class LikeLinkDecorator(AbstractTwoStateViewLinkDecorator):
-	"""
-	Adds the appropriate like or unlike link.
-	"""
-	false_view = 'like'
-	true_view = 'unlike'
-	link_predicate = staticmethod(liking.likes_object)
+    """
+    Adds the appropriate like or unlike link.
+    """
+    false_view = 'like'
+    true_view = 'unlike'
+    link_predicate = staticmethod(liking.likes_object)
+
 
 @view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 context=ILikeable,
-			 permission=nauth.ACT_READ,  # anyone that can see the object
-			 request_method='POST',
-			 name='like')
+             renderer='rest',
+             context=ILikeable,
+             permission=nauth.ACT_READ,  # anyone that can see the object
+             request_method='POST',
+             name='like')
 def _LikeView(request):
-	"""
-	Given an :class:`nti_interfaces.ILikeable`, make the
-	current user like the object, and return it.
+    """
+    Given an :class:`ILikeable`, make the
+    current user like the object, and return it.
 
-	Registered as a named view, so invoked via the @@like syntax.
+    Registered as a named view, so invoked via the @@like syntax.
 
-	"""
-	liking.like_object(request.context, request.authenticated_userid)
-	return uncached_in_response(request.context)
+    """
+    liking.like_object(request.context, request.authenticated_userid)
+    return uncached_in_response(request.context)
+
 
 @view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 context=ILikeable,
-			 permission=nauth.ACT_READ,  # anyone that can see the object
-			 request_method='POST',
-			 name='unlike')
+             renderer='rest',
+             context=ILikeable,
+             permission=nauth.ACT_READ,  # anyone that can see the object
+             request_method='POST',
+             name='unlike')
 def _UnlikeView(request):
-	"""
-	Given an :class:`nti_interfaces.ILikeable`, make the
-	current user no longer like the object, and return it.
+    """
+    Given an :class:`ILikeable`, make the
+    current user no longer like the object, and return it.
 
-	Registered as a named view, so invoked via the @@unlike syntax.
+    Registered as a named view, so invoked via the @@unlike syntax.
 
-	"""
-	liking.unlike_object(request.context, request.authenticated_userid)
-	return uncached_in_response(request.context)
+    """
+    liking.unlike_object(request.context, request.authenticated_userid)
+    return uncached_in_response(request.context)
+
 
 @interface.implementer(IExternalMappingDecorator)
 @component.adapter(IFavoritable, IRequest)
 class FavoriteLinkDecorator(AbstractTwoStateViewLinkDecorator):
-	"""
-	Adds the appropriate favorite or unfavorite link.
-	"""
+    """
+    Adds the appropriate favorite or unfavorite link.
+    """
 
-	false_view = 'favorite'
-	true_view = 'unfavorite'
-	link_predicate = staticmethod(liking.favorites_object)
+    false_view = 'favorite'
+    true_view = 'unfavorite'
+    link_predicate = staticmethod(liking.favorites_object)
+
 
 @view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 context=IFavoritable,
-			 permission=nauth.ACT_READ,  # anyone that can see the object
-			 request_method='POST',
-			 name='favorite')
+             renderer='rest',
+             context=IFavoritable,
+             permission=nauth.ACT_READ,  # anyone that can see the object
+             request_method='POST',
+             name='favorite')
 def _FavoriteView(request):
-	"""
-	Given an :class:`nti_interfaces.IFavoritable`, make the
-	current user favorite the object, and return it.
+    """
+    Given an :class:`IFavoritable`, make the
+    current user favorite the object, and return it.
 
-	Registered as a named view, so invoked via the @@favorite syntax.
+    Registered as a named view, so invoked via the @@favorite syntax.
 
-	"""
-	liking.favorite_object(request.context, request.authenticated_userid)
-	return uncached_in_response(request.context)
+    """
+    liking.favorite_object(request.context, request.authenticated_userid)
+    return uncached_in_response(request.context)
+
 
 @view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 context=IFavoritable,
-			 permission=nauth.ACT_READ,  # anyone that can see the object
-			 request_method='POST',
-			 name='unfavorite')
+             renderer='rest',
+             context=IFavoritable,
+             permission=nauth.ACT_READ,  # anyone that can see the object
+             request_method='POST',
+             name='unfavorite')
 def _UnfavoriteView(request):
-	"""
-	Given an :class:`nti_interfaces.IFavoritable`, make the
-	current user no longer favorite the object, and return it.
+    """
+    Given an :class:`IFavoritable`, make the
+    current user no longer favorite the object, and return it.
 
-	Registered as a named view, so invoked via the @@unlike syntax.
+    Registered as a named view, so invoked via the @@unlike syntax.
 
-	"""
-	liking.unfavorite_object(request.context, request.authenticated_userid)
-	return uncached_in_response(request.context)
+    """
+    liking.unfavorite_object(request.context, request.authenticated_userid)
+    return uncached_in_response(request.context)
