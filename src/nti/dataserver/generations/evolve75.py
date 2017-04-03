bfe8a0eee8aee0e707f2929474e9b8b4e4f6c9db
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-03 16:29:48 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@110061 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/evolve75.py
+++ src/nti/dataserver/generations/evolve75.py
@@ -29,49 +29,52 @@ from nti.metadata import metadata_queue
 
 from nti.site.hostpolicy import run_job_in_all_host_sites
 
+
 def _index_assets(queue, intids):
-	
-	def _index_item(item):
-		doc_id = intids.queryId(item)
-		if doc_id is not None:
-			try:
-				queue.add(doc_id)
-				return True
-			except TypeError:
-				pass
-		return False
 
-	for _, item in component.getUtilitiesFor(IPresentationAsset):
-		_index_item(item)
+    def _index_item(item):
+        doc_id = intids.queryId(item)
+        if doc_id is not None:
+            try:
+                queue.add(doc_id)
+                return True
+            except TypeError:
+                pass
+        return False
 
-	for _, item in component.getUtilitiesFor(IQAssessment):
-		_index_item(item)
-	
-	for _, item in component.getUtilitiesFor(IQInquiry):
-		_index_item(item)
+    for _, item in component.getUtilitiesFor(IPresentationAsset):
+        _index_item(item)
+
+    for _, item in component.getUtilitiesFor(IQAssessment):
+        _index_item(item)
+
+    for _, item in component.getUtilitiesFor(IQInquiry):
+        _index_item(item)
+
 
 def do_evolve(context, generation=generation):
-	setHooks()
-	conn = context.connection
-	root = conn.root()
-	dataserver_folder = root['nti.dataserver']
+    setHooks()
+    conn = context.connection
+    root = conn.root()
+    dataserver_folder = root['nti.dataserver']
 
-	with site(dataserver_folder):
-		assert	component.getSiteManager() == dataserver_folder.getSiteManager(), \
-				"Hooks not installed?"
+    with site(dataserver_folder):
+        assert component.getSiteManager() == dataserver_folder.getSiteManager(), \
+               "Hooks not installed?"
 
-		lsm = dataserver_folder.getSiteManager()
-		intids = lsm.getUtility(IIntIds)
-		queue = metadata_queue()
-		if queue is None:
-			return
+        lsm = dataserver_folder.getSiteManager()
+        intids = lsm.getUtility(IIntIds)
+        queue = metadata_queue()
+        if queue is None:
+            return
 
-		run_job_in_all_host_sites(partial(_index_assets, queue, intids))
+        run_job_in_all_host_sites(partial(_index_assets, queue, intids))
 
-		logger.info('Dataserver evolution %s done.', generation)
+        logger.info('Dataserver evolution %s done.', generation)
+
 
 def evolve(context):
-	"""
-	Evolve to 75 by indexing assessments, inquiries and assets
-	"""
-	do_evolve(context, generation)
+    """
+    Evolve to 75 by indexing assessments, inquiries and assets
+    """
+    do_evolve(context, generation)
