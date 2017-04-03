982ddfea20c2a6fd8afe4256d292f77b227d1fb5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-03 16:33:10 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@110063 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/users/index.py
+++ src/nti/dataserver/users/index.py
@@ -57,143 +57,160 @@ IX_PASSWORD_RECOVERY_EMAIL_HASH = 'password_recovery_email_hash'
 IX_EMAIL_VERIFIED = 'email_verified'
 IX_OPT_IN_EMAIL_COMMUNICATION = 'opt_in_email_communication'
 
+
 class ValidatingMimeType(object):
 
-	__slots__ = (b'mimeType',)
+    __slots__ = (b'mimeType',)
 
-	def __init__(self, obj, default=None):
-		try:
-			if IEntity.providedBy(obj):
-				self.mimeType = 	getattr(obj, 'mimeType', None) \
-								or	getattr(obj, 'mime_type', None) 
-		except (AttributeError, TypeError):
-			pass
+    def __init__(self, obj, default=None):
+        try:
+            if IEntity.providedBy(obj):
+                self.mimeType = getattr(obj, 'mimeType', None) \
+                             or getattr(obj, 'mime_type', None)
+        except (AttributeError, TypeError):
+            pass
 
-	def __reduce__(self):
-		raise TypeError()
+    def __reduce__(self):
+        raise TypeError()
+
 
 class MimeTypeIndex(ValueIndex):
-	default_field_name = IX_MIMETYPE
-	default_interface = ValidatingMimeType
+    default_field_name = IX_MIMETYPE
+    default_interface = ValidatingMimeType
+
 
 class AliasIndex(CaseInsensitiveFieldIndex):
-	default_field_name = IX_ALIAS
-	default_interface = IFriendlyNamed
+    default_field_name = IX_ALIAS
+    default_interface = IFriendlyNamed
+
 
 class RealnameIndex(CaseInsensitiveFieldIndex):
-	default_field_name = IX_REALNAME
-	default_interface = IFriendlyNamed
+    default_field_name = IX_REALNAME
+    default_interface = IFriendlyNamed
+
 
 class RealnamePartsIndex(CaseInsensitiveKeywordIndex):
-	default_interface = IFriendlyNamed
-	default_field_name = 'get_searchable_realname_parts'
+    default_interface = IFriendlyNamed
+    default_field_name = 'get_searchable_realname_parts'
 
-	def __init__(self, *args, **kwargs):
-		super(RealnamePartsIndex, self).__init__(*args, **kwargs)
-		self.field_callable = True
+    def __init__(self, *args, **kwargs):
+        super(RealnamePartsIndex, self).__init__(*args, **kwargs)
+        self.field_callable = True
+
 
 class EmailIndex(CaseInsensitiveFieldIndex):
-	default_field_name = IX_EMAIL
-	default_interface = IUserProfile
+    default_field_name = IX_EMAIL
+    default_interface = IUserProfile
+
 
 class ContactEmailIndex(CaseInsensitiveFieldIndex):
-	default_field_name = IX_CONTACT_EMAIL
-	default_interface = IUserProfile
+    default_field_name = IX_CONTACT_EMAIL
+    default_interface = IUserProfile
+
 
 class PasswordRecoveryEmailHashIndex(FieldIndex):
-	default_field_name = IX_PASSWORD_RECOVERY_EMAIL_HASH
-	default_interface = IRestrictedUserProfile
+    default_field_name = IX_PASSWORD_RECOVERY_EMAIL_HASH
+    default_interface = IRestrictedUserProfile
+
 
 class ContactEmailRecoveryHashIndex(FieldIndex):
-	default_field_name = IX_CONTACT_EMAIL_RECOVERY_HASH
-	default_interface = IContactEmailRecovery
+    default_field_name = IX_CONTACT_EMAIL_RECOVERY_HASH
+    default_interface = IContactEmailRecovery
+
 
 # XXX Note that FilteredSetBase uses a BTrees Set by default,
 # NOT a TreeSet. So updating them when large is quite expensive.
 # You can override clear() to use a TreeSet.
+
 # TODO: Investigate migrating these two indexes to use a TreeSet,
 # they have a size equal to the number of users and will conflict
 # if many users are added at once.
 
+
 class OptInEmailCommunicationFilteredSet(FilteredSetBase):
 
-	EXPR = 'IUserProfile(context).opt_in_email_communication'
+    EXPR = 'IUserProfile(context).opt_in_email_communication'
 
-	def __init__(self, iden, family=None):
-		super(OptInEmailCommunicationFilteredSet, self).__init__(iden, self.EXPR, family=family)
+    def __init__(self, iden, family=None):
+        super(OptInEmailCommunicationFilteredSet, self).__init__(iden, self.EXPR, family=family)
 
-	def index_doc(self, docid, context):
-		try:
-			index = IUserProfile(context).opt_in_email_communication
-		except (TypeError, AttributeError):
-			# Could not adapt, not in profile
-			index = False
+    def index_doc(self, docid, context):
+        try:
+            index = IUserProfile(context).opt_in_email_communication
+        except (TypeError, AttributeError):
+            # Could not adapt, not in profile
+            index = False
 
-		if index:
-			self._ids.insert(docid)
-		else:
-			# The normal PythonFilteredSet seems to have a bug and never unindexes?
-			self.unindex_doc(docid)
+        if index:
+            self._ids.insert(docid)
+        else:
+            # The normal PythonFilteredSet seems to have a bug and never
+            # unindexes?
+            self.unindex_doc(docid)
+
 
 class EmailVerifiedFilteredSet(FilteredSetBase):
 
-	EXPR = 'IUserProfile(context).email_verified'
+    EXPR = 'IUserProfile(context).email_verified'
 
-	def __init__(self, iden, family=None):
-		super(EmailVerifiedFilteredSet, self).__init__(iden, self.EXPR, family=family)
+    def __init__(self, iden, family=None):
+        super(EmailVerifiedFilteredSet, self).__init__(iden, self.EXPR, family=family)
 
-	def index_doc(self, docid, context):
-		try:
-			index = IUserProfile(context).email_verified
-		except (TypeError, AttributeError):
-			# Could not adapt, not in profile
-			index = False
+    def index_doc(self, docid, context):
+        try:
+            index = IUserProfile(context).email_verified
+        except (TypeError, AttributeError):
+            # Could not adapt, not in profile
+            index = False
 
-		if index:
-			self._ids.insert(docid)
-		else:
-			# The normal PythonFilteredSet seems to have a bug and never unindexes?
-			self.unindex_doc(docid)
+        if index:
+            self._ids.insert(docid)
+        else:
+            # The normal PythonFilteredSet seems to have a bug and never
+            # unindexes?
+            self.unindex_doc(docid)
+
 
 def install_entity_catalog(site_manager_container, intids=None):
-	lsm = site_manager_container.getSiteManager()
-	catalog = lsm.queryUtility(ICatalog, name=CATALOG_NAME)
-	if catalog is not None:
-		return catalog
+    lsm = site_manager_container.getSiteManager()
+    catalog = lsm.queryUtility(ICatalog, name=CATALOG_NAME)
+    if catalog is not None:
+        return catalog
 
-	intids = lsm.getUtility(IIntIds) if intids is None else intids
-	catalog = Catalog(family=intids.family)
-	catalog.__name__ = CATALOG_NAME
-	catalog.__parent__ = site_manager_container
-	intids.register(catalog)
-	lsm.registerUtility(catalog, provided=ICatalog, name=CATALOG_NAME)
+    intids = lsm.getUtility(IIntIds) if intids is None else intids
+    catalog = Catalog(family=intids.family)
+    catalog.__name__ = CATALOG_NAME
+    catalog.__parent__ = site_manager_container
+    intids.register(catalog)
+    lsm.registerUtility(catalog, provided=ICatalog, name=CATALOG_NAME)
 
-	for name, clazz in ((IX_ALIAS, AliasIndex),
-						(IX_EMAIL, EmailIndex),
-						(IX_MIMETYPE, MimeTypeIndex),
-						(IX_REALNAME, RealnameIndex),
-						(IX_CONTACT_EMAIL, ContactEmailIndex),
-						(IX_REALNAME_PARTS, RealnamePartsIndex),
-						(IX_CONTACT_EMAIL_RECOVERY_HASH, ContactEmailRecoveryHashIndex),
-						(IX_PASSWORD_RECOVERY_EMAIL_HASH, PasswordRecoveryEmailHashIndex)):
-		index = clazz(family=intids.family)
-		intids.register(index)
-		locate(index, catalog, name)
-		catalog[name] = index
+    for name, clazz in ((IX_ALIAS, AliasIndex),
+                        (IX_EMAIL, EmailIndex),
+                        (IX_MIMETYPE, MimeTypeIndex),
+                        (IX_REALNAME, RealnameIndex),
+                        (IX_CONTACT_EMAIL, ContactEmailIndex),
+                        (IX_REALNAME_PARTS, RealnamePartsIndex),
+                        (IX_CONTACT_EMAIL_RECOVERY_HASH, ContactEmailRecoveryHashIndex),
+                        (IX_PASSWORD_RECOVERY_EMAIL_HASH, PasswordRecoveryEmailHashIndex)):
+        index = clazz(family=intids.family)
+        intids.register(index)
+        locate(index, catalog, name)
+        catalog[name] = index
 
-	opt_in_comm_set = OptInEmailCommunicationFilteredSet(IX_OPT_IN_EMAIL_COMMUNICATION,
-														 family=intids.family)
+    opt_in_comm_set = OptInEmailCommunicationFilteredSet(IX_OPT_IN_EMAIL_COMMUNICATION,
+                                                         family=intids.family)
 
-	email_verified_set = EmailVerifiedFilteredSet(IX_EMAIL_VERIFIED, 
-												  family=intids.family)
+    email_verified_set = EmailVerifiedFilteredSet(IX_EMAIL_VERIFIED,
+                                                  family=intids.family)
 
-	topics_index = TopicIndex(family=intids.family)
-	topics_index.addFilter(opt_in_comm_set)
-	topics_index.addFilter(email_verified_set)
-	intids.register(topics_index)
+    topics_index = TopicIndex(family=intids.family)
+    topics_index.addFilter(opt_in_comm_set)
+    topics_index.addFilter(email_verified_set)
+    intids.register(topics_index)
 
-	topics_index.__name__ = IX_TOPICS
-	topics_index.__parent__ = catalog
-	catalog[IX_TOPICS] = topics_index
-	return catalog
+    topics_index.__name__ = IX_TOPICS
+    topics_index.__parent__ = catalog
+    catalog[IX_TOPICS] = topics_index
+    return catalog
+
 install_user_catalog = install_entity_catalog
