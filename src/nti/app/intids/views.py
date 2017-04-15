f3cc48acd0d55e8230e438256a5ea7404bb60c6d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-15 20:02:19 +0000

    trap value errors
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@110758 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/intids/views.py
+++ src/nti/app/intids/views.py
@@ -9,6 +9,8 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import sys
+
 from zope import component
 
 from zope.intid.interfaces import IIntIds
@@ -67,7 +69,19 @@ class IntIdResolverView(AbstractAuthenticatedView):
                 None)
 
         intids = component.getUtility(IIntIds)
-        result = intids.queryObject(uid)
+        try:
+            result = intids.queryObject(uid)
+        except Exception as e:
+            exc_info = sys.exc_info()
+            raise_json_error(
+                self.request,
+                hexc.HTTPUnprocessableEntity,
+                {
+                    u'message': str(e),
+                    u'code': e.__class__.__name__,
+                },
+                exc_info[2])
+
         if result is None:
             raise_json_error(
                 self.request,
