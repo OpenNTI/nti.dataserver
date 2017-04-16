a1f994927eecbcc15416c45f98671eb65486d273
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-16 02:48:29 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@110776 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/application_server.py
+++ src/nti/appserver/application_server.py
@@ -15,19 +15,22 @@ import gevent.pywsgi
 
 import geventwebsocket.handler
 
-class WebSocketServer(gevent.pywsgi.WSGIServer): # In gevent 1.0, gevent.wsgi is an alias for WSGIServer
-	"""
-	HTTP server that can handle websockets.
-	"""
 
-	handler_class = geventwebsocket.handler.WebSocketHandler
+# In gevent 1.0, gevent.wsgi is an alias for WSGIServer
+class WebSocketServer(gevent.pywsgi.WSGIServer):
+    """
+    HTTP server that can handle websockets.
+    """
 
-	def __init__( self, *args, **kwargs ):
-		"""
-		:raises ValueError: If a ``handler_class`` keyword argument is supplied
-			that specifies a non-:class:`geventwebsocket.handler.WebSocketHandler` subclass.
-			That type of handler is required for websockets to work.
-		"""
-		super(WebSocketServer,self).__init__(*args, **kwargs)
-		if not issubclass( self.handler_class, geventwebsocket.handler.WebSocketHandler ):
-			raise ValueError( "Unable to run with a handler that is not a type of %s", WebSocketServer.handler_class )
+    handler_class = geventwebsocket.handler.WebSocketHandler
+
+    def __init__(self, *args, **kwargs):
+        """
+        :raises ValueError: If a ``handler_class`` keyword argument is supplied
+                that specifies a non-:class:`geventwebsocket.handler.WebSocketHandler` subclass.
+                That type of handler is required for websockets to work.
+        """
+        super(WebSocketServer, self).__init__(*args, **kwargs)
+        if not issubclass(self.handler_class, geventwebsocket.handler.WebSocketHandler):
+            raise ValueError("Unable to run with a handler that is not a type of %s", 
+							 WebSocketServer.handler_class)
