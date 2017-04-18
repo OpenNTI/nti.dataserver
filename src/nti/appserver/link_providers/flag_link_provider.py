89bb00467c8137b56d9a744163ead5a35b4c767b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-18 00:46:26 +0000

    add constant
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@110865 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/link_providers/flag_link_provider.py
+++ src/nti/appserver/link_providers/flag_link_provider.py
@@ -46,64 +46,67 @@ from nti.common import sets
 from nti.dataserver.interfaces import IUser
 
 # We store links in an OOTreeSet annotation on the User object
-_LINK_ANNOTATION_KEY = 'nti.appserver.user_link_provider' + '.LinkAnnotation'  # hardcoded name for BWC
+_PKG_ANNOTATION_KEY = 'nti.appserver.user_link_provider'
+_LINK_ANNOTATION_KEY = _PKG_ANNOTATION_KEY + '.LinkAnnotation'  # hardcoded name for BWC
+
 
 def add_link(user, link_name):
-	"""
-	Add the given link name to the user.
+    """
+    Add the given link name to the user.
 
-	:param user: An annotatable user.
-	:param unicode link_name: The link name.
-	"""
-	the_set = IAnnotations(user).get(_LINK_ANNOTATION_KEY)
-	if the_set is None:
-		the_set = OOTreeSet()
-		IAnnotations(user)[_LINK_ANNOTATION_KEY] = the_set
-	if link_name not in the_set:
-		notify(FlagLinkAddedEvent(user, link_name))
-	the_set.add(link_name)
+    :param user: An annotatable user.
+    :param unicode link_name: The link name.
+    """
+    the_set = IAnnotations(user).get(_LINK_ANNOTATION_KEY)
+    if the_set is None:
+        the_set = OOTreeSet()
+        IAnnotations(user)[_LINK_ANNOTATION_KEY] = the_set
+    if link_name not in the_set:
+        notify(FlagLinkAddedEvent(user, link_name))
+    the_set.add(link_name)
+
 
 def has_link(user, link_name):
-	"""
-	Primarily for testing, answer whether the user is known to
-	have the given link.
+    """
+    Primarily for testing, answer whether the user is known to
+    have the given link.
 
-	:param user: An annotatable user.
-	:param unicode link_name: The link name.
-	"""
+    :param user: An annotatable user.
+    :param unicode link_name: The link name.
+    """
 
-	the_set = IAnnotations(user).get(_LINK_ANNOTATION_KEY, ())
-	return link_name in the_set
+    the_set = IAnnotations(user).get(_LINK_ANNOTATION_KEY, ())
+    return link_name in the_set
+
 
 def delete_link(user, link_name):
-	"""
-	Ensure the given user does not have a link with the
-	given name.
+    """
+    Ensure the given user does not have a link with the
+    given name.
 
-	:param user: An annotatable user.
-	:param unicode link_name: The link name.
-	:return: A truthy value that will be true if the link
-		existed and was discarded or false of the link didn't exist.
-	"""
+    :param user: An annotatable user.
+    :param unicode link_name: The link name.
+    :return: A truthy value that will be true if the link
+            existed and was discarded or false of the link didn't exist.
+    """
 
-	the_set = IAnnotations(user).get(_LINK_ANNOTATION_KEY)
-	if the_set is None:
-		return
-
-	result = sets.discard_p(the_set, link_name)
-	if result:
-		notify(FlagLinkRemovedEvent(user, link_name))
-	return result
-
+    the_set = IAnnotations(user).get(_LINK_ANNOTATION_KEY)
+    if the_set is None:
+        return
+    result = sets.discard_p(the_set, link_name)
+    if result:
+        notify(FlagLinkRemovedEvent(user, link_name))
+    return result
 _delete_link = delete_link
+
 
 @component.adapter(IUser, IRequest)
 @interface.implementer(IDeletableLinkProvider)
 class FlagLinkProvider(LinkProvider):
 
-	def get_links(self):
-		the_set = IAnnotations(self.user).get(_LINK_ANNOTATION_KEY, ())
-		return [self._make_link_with_rel(link_name) for link_name in the_set]
+    def get_links(self):
+        the_set = IAnnotations(self.user).get(_LINK_ANNOTATION_KEY, ())
+        return [self._make_link_with_rel(link_name) for link_name in the_set]
 
-	def delete_link(self, link_name):
-		return _delete_link(self.user, link_name)
+    def delete_link(self, link_name):
+        return _delete_link(self.user, link_name)
