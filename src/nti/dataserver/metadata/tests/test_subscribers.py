994bd7bfe1b27deb4d87eeba684affe19d6872fe
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-31 20:10:56 +0000

    add tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@113902 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/dataserver/metadata/tests/test_subscribers.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import is_not
+from hamcrest import has_length
+from hamcrest import assert_that
+
+from nti.contentfragments.interfaces import IPlainTextContentFragment
+
+from nti.dataserver.contenttypes import Note
+
+from nti.dataserver.metadata.index import IX_CREATOR
+from nti.dataserver.metadata.index import get_metadata_catalog
+
+from nti.dataserver.metadata.utils import delete_entity_metadata
+
+from nti.dataserver.users import User
+
+from nti.ntiids.ntiids import make_ntiid
+
+from nti.dataserver.tests import mock_dataserver
+
+from nti.dataserver.tests.mock_dataserver import DataserverLayerTest
+
+
+class TestSubscribers(DataserverLayerTest):
+
+    def _create_user(self, username=u'nt@nti.com', password=u'temp001'):
+        ds = mock_dataserver.current_mock_ds
+        usr = User.create_user(ds, username=username, password=password)
+        return usr
+
+    def _create_note(self, msg, owner, title=None, inReplyTo=None):
+        note = Note()
+        note.body = [msg]
+        note.creator = owner
+        note.inReplyTo = inReplyTo
+        note.title = IPlainTextContentFragment(title) if title else None
+        note.containerId = make_ntiid(nttype=u'bleach', specific=u'manga')
+        return note
+
+    def _create_notes(self, user, notes):
+        connection = mock_dataserver.current_transaction
+        for x in range(notes):
+            title = u"title %s" % x
+            message = u"body %s" % x
+            note = self._create_note(message, user, title=title)
+            connection.add(note)
+            user.addContainedObject(note)
+
+    @mock_dataserver.WithMockDSTrans
+    def test_create_delete_notes(self):
+        notes = 30
+        username = u'ichigo@bleach.org'
+        user = self._create_user(username)
+        self._create_notes(user, notes)
+
+        catalog = get_metadata_catalog()
+        assert_that(catalog, is_not(none()))
+        query = {
+            IX_CREATOR: {'any_of': (username,)}
+        }
+        results = catalog.searchResults(**query)
+        assert_that(results, has_length(notes))
+
+        deleted = delete_entity_metadata(catalog, username)
+        assert_that(deleted, is_(notes))
+
+        results = catalog.searchResults(**query)
+        assert_that(results, has_length(0))
