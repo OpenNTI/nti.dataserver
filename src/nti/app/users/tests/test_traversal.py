a182c21fe6943517edccd999b22c9e279cb38ae8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-27 23:47:43 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@116073 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/users/tests/test_traversal.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+
+from zope import component
+
+from zope.traversing.interfaces import ITraversable
+
+from pyramid.testing import DummyRequest
+
+from nti.dataserver.interfaces import UNAUTHENTICATED_PRINCIPAL_NAME
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IShardLayout
+from nti.dataserver.interfaces import IAnonymousUser
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.dataserver.tests import mock_dataserver
+
+
+class TestTraversal(ApplicationLayerTest):
+
+    @WithSharedApplicationMockDS(users=False, testapp=False)
+    def test_anonymous_user(self):
+        with mock_dataserver.mock_db_trans(self.ds):
+            dataserver = component.getUtility(IDataserver)
+            users_folder = IShardLayout(dataserver).users_folder
+            traversable = ITraversable(users_folder)
+            obj = traversable.traverse(UNAUTHENTICATED_PRINCIPAL_NAME, '')
+            assert_that(IAnonymousUser.providedBy(obj), is_(True))
+
+            traversable = ITraversable(users_folder, DummyRequest())
+            obj = traversable.traverse(UNAUTHENTICATED_PRINCIPAL_NAME, '')
+            assert_that(IAnonymousUser.providedBy(obj), is_(True))
