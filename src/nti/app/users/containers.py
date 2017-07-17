63ee7da54b4a6a02813cb723a55c808d105b6741
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-17 19:40:02 +0000

    add IUserContainersQuerier subs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@117626 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/users/containers.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.appserver.interfaces import IUserContainersQuerier
+
+from nti.dataserver.interfaces import IUser
+
+from nti.ntiids.ntiids import ROOT
+
+
+@component.adapter(IUser)
+@interface.implementer(IUserContainersQuerier)
+class _UserContainersQuerier(object):
+
+    def __init__(self, user=None):
+        self.user = user
+
+    def query(self, user, ntiid, include_stream, stream_only):
+        containers = set()
+        user = self.user if user is None else user
+        if ntiid == ROOT:
+            containers.update(user.iterntiids(include_stream=include_stream,
+                                              stream_only=stream_only))
+        # We always include the unnamed root (which holds things like CIRCLED)
+        # NOTE: This is only in the stream. Normally we cannot store contained
+        # objects with an empty container key, so this takes internal magic
+        containers.add('')  # root
+        return containers
+    __call__ = query
