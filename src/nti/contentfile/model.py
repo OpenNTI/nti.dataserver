31aa6368a30dac6886d818ec14a3a8a84858c827
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-18 16:59:39 +0000

    add mixins module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@117715 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentfile/model.py
+++ src/nti/contentfile/model.py
@@ -9,16 +9,11 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from io import BytesIO
-
 from zope import interface
-
-from zope.cachedescriptors.property import Lazy
-from zope.cachedescriptors.property import CachedProperty
 
 from zope.deprecation import deprecated
 
-from BTrees.OOBTree import OOTreeSet
+from nti.contentfile.mixins import BaseContentMixin
 
 from nti.contentfile.interfaces import IContentFile
 from nti.contentfile.interfaces import IContentImage
@@ -32,123 +27,6 @@ from nti.namedfile.file import NamedBlobImage
 
 from nti.namedfile.interfaces import IInternalFileRef
 
-from nti.property.property import alias
-
-from nti.wref.interfaces import IWeakRef
-
-
-class BaseContentMixin(object):
-
-    tags = ()
-    creator = None
-
-    path = None  # XXX BWC
-
-    __parent__ = None
-    __name__ = alias('name')
-
-    def __init__(self, *args, **kwargs):
-        super(BaseContentMixin, self).__init__(*args, **kwargs)
-
-    # associations
-
-    def _lazy_create_ootreeset_for_wref(self):
-        self._p_changed = True
-        result = OOTreeSet()
-        if self._p_jar:
-            self._p_jar.add(result)
-        return result
-
-    def discard(self, container, value):
-        try:
-            container.discard(value)
-        except AttributeError:
-            try:
-                container.remove(value)
-            except (KeyError, ValueError):
-                pass
-
-    def _remove_from_named_lazy_set_of_wrefs(self, name, context):
-        self._p_activate()
-        if name in self.__dict__:
-            jar = getattr(self, '_p_jar', None)
-            container = getattr(self, name)
-            if jar is not None:
-                jar.readCurrent(self)
-                container._p_activate()
-                jar.readCurrent(container)
-            wref = IWeakRef(context, None)
-            if wref is not None:
-                __traceback_info__ = context, wref
-                self.discard(container, wref)
-
-    @Lazy
-    def _associations(self):
-        return self._lazy_create_ootreeset_for_wref()
-
-    def add_association(self, context):
-        wref = IWeakRef(context, None)
-        if wref is not None:
-            self._associations.add(wref)
-            return True
-        return False
-
-    def remove_association(self, context):
-        self._remove_from_named_lazy_set_of_wrefs('_associations', context)
-
-    def associations(self):
-        for wref in self._associations:
-            try:
-                obj = wref()
-                if obj is not None:
-                    yield obj
-            except Exception:
-                logger.exception("Error while getting associatied object")
-
-    def has_associations(self):
-        return  bool(   '_associations' in self.__dict__
-                     and self._associations)
-
-    def count_associations(self):
-        result = 0
-        if '_associations' in self.__dict__:
-            result = len(list(self.associations()))
-        return result
-
-    def validate_associations(self):
-        if not '_associations' in self.__dict__:
-            return
-        for wref in list(self._associations):
-            try:
-                obj = wref()
-                if obj is None:
-                    self.discard(self._associations, wref)
-            except Exception:
-                logger.exception("Error while getting associatied object")
-
-    def clear_associations(self):
-        if '_associations' in self.__dict__:
-            self._associations.clear()
-
-    # IFileReader
-
-    @CachedProperty('data')
-    def _v_fp(self):
-        return BytesIO(self.data)
-
-    def read(self, size=-1):
-        return self._v_fp.read(size) if size != -1 else self.data
-
-    def seek(self, offset, whence=0):
-        return self._v_fp.seek(offset, whence)
-
-    def tell(self):
-        return self._v_fp.tell()
-
-    # compatible methods
-
-    def readContents(self):
-        return self.data
 BaseMixin = BaseContentMixin  # BWC
 
 
