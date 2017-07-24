c5955202c7dd1347c35c1f37bbc5e50cdc81e9b1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-24 16:40:33 +0000

    remove adapter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@118024 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentfile/adapters.py
+++ src/nti/app/contentfile/adapters.py
@@ -9,8 +9,6 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import six
-
 from zope import component
 from zope import interface
 
@@ -25,7 +23,6 @@ from nti.contentfile import CONTENT_BLOB_IMAGE_MIMETYPE
 
 from nti.contentfile.interfaces import IS3File
 from nti.contentfile.interfaces import IS3Image
-from nti.contentfile.interfaces import IS3FileIO
 from nti.contentfile.interfaces import IContentImage
 from nti.contentfile.interfaces import IContentBaseFile
 from nti.contentfile.interfaces import IContentBlobFile
@@ -38,8 +35,6 @@ from nti.contentfolder.adapters import Associations
 from nti.contentfolder.adapters import name_adapter
 from nti.contentfolder.adapters import path_adapter
 from nti.contentfolder.adapters import filename_adapter
-
-from nti.contentfolder.boto_s3 import BotoS3Mixin
 
 from nti.contentfolder.interfaces import INameAdapter
 from nti.contentfolder.interfaces import IPathAdapter
@@ -107,36 +102,3 @@ def _contentfile_associations_adapter(context):
         ids = {intid.queryId(x) for x in context.associations()}
         ids.discard(None)
         return Associations(tuple(ids)) if ids else None
-
-
-@component.adapter(IS3File)
-@interface.implementer(IS3FileIO)
-class S3FileIO(BotoS3Mixin):
-
-    def __init__(self, context):
-        self.context = context
-        
-    def key(self):
-        return self.get_key(self.context)
-
-    def exists(self, debug=True):
-        return self.exists_key(self.key(), debug)
-
-    def contents(self, encoding=None, debug=True):
-        return self.get_contents(self.key(), encoding, debug)
-
-    def size(self, debug=True):
-        return self.size_key(self.key(), debug)
-
-    def save(self, debug=True):
-        self.save_key(self.key(), self.context.data, debug)
-
-    def remove(self, debug=True):
-        self.remove_key(self.key(), debug)
-
-    def rename(self, target, debug=True):
-        if IS3File.providedBy(target):
-            newKey = IS3FileIO(target).key()
-        elif not isinstance(target, six.string_types):
-            newKey = str(target)
-        self.rename_key(self.key(), newKey, debug)
