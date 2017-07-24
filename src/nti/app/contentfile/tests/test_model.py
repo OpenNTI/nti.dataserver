01d742c0be7cb11e35beb239e03cc739f918c094
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-24 03:06:47 +0000

    test persistance
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@118004 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentfile/tests/test_model.py
+++ src/nti/app/contentfile/tests/test_model.py
@@ -8,8 +8,12 @@ __docformat__ = "restructuredtext en"
 # pylint: disable=W0212,R0904
 
 from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import has_key
 from hamcrest import has_length
 from hamcrest import assert_that
+from hamcrest import has_property
+does_not = is_not
 
 from nti.externalization.internalization import find_factory_for
 from nti.externalization.internalization import update_from_external_object
@@ -23,13 +27,13 @@ from nti.dataserver.tests import mock_dataserver
 GIF_DATAURL = b'data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw=='
 
 
-class TestAdapters(ApplicationLayerTest):
+class TestModel(ApplicationLayerTest):
 
     ext_obj = {
         'MimeType': 'application/vnd.nextthought.contentblobimage',
         'value': GIF_DATAURL,
         'filename': u'image.gif',
-        'name': u'image2.gif'
+        'name': u'IMGAGE_2.gif'
     }
 
     @WithSharedApplicationMockDS(users=False, testapp=False)
@@ -67,3 +71,27 @@ class TestAdapters(ApplicationLayerTest):
             assert_that(master.has_associations(), is_(False))
             assert_that(set(master.associations()), has_length(0))
             
+    @WithSharedApplicationMockDS(users=False, testapp=False)
+    def test_persitance(self):
+        with mock_dataserver.mock_db_trans(self.ds):
+            obj = find_factory_for(self.ext_obj)()
+            update_from_external_object(obj,
+                                        self.ext_obj,
+                                        require_updater=True)
+            assert_that(obj, has_property('name', is_('IMGAGE_2.gif')))
+            assert_that(obj, has_property('__name__', is_('IMGAGE_2.gif')))
+            assert_that(obj.__dict__, does_not(has_key('__name__')))
+            self.ds.root['image'] = obj
+
+        with mock_dataserver.mock_db_trans(self.ds):
+            image = self.ds.root['image']
+            assert_that(image.__dict__, does_not(has_key('__name__')))
+            image.__name__ = u'image.gif'
+            assert_that(image.__dict__, does_not(has_key('__name__')))
+            assert_that(image, has_property('name', is_('image.gif')))
+        
+        with mock_dataserver.mock_db_trans(self.ds):
+            image = self.ds.root['image']
+            assert_that(image, has_property('__name__', is_('image.gif')))
+            assert_that(image, has_property('name', is_('image.gif')))
+            assert_that(image.__dict__, does_not(has_key('__name__')))
