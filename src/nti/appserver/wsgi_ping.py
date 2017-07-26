f7c6f4c24749be61a1d7f67b1394c70d168aae16
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-26 17:02:00 +0000

    use native strings
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@118174 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/wsgi_ping.py
+++ src/nti/appserver/wsgi_ping.py
@@ -6,7 +6,7 @@ Simple ping paste filter that does nothing but return a 200.
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -21,15 +21,15 @@ class PingHandler(object):
         self.captured = app
 
     def __call__(self, environ, start_response):
-        if environ['PATH_INFO'] == b'/_ops/ping':
-            start_response(b'200 OK', [(b'Content-Type', b'text/plain')])
-            result = (b"",)
+        if environ['PATH_INFO'] == '/_ops/ping':
+            start_response('200 OK', [('Content-Type', 'text/plain')])
+            result = ("",)
         else:
             result = self.captured(environ, start_response)
         return result
 
 
-def ping_handler_factory(app, global_conf=None):
+def ping_handler_factory(app, unused_global_conf=None):
     """
     Paste factory for :class:`PingHandler`
     """
