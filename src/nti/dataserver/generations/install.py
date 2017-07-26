9cd997bb701905baf2c5127ea7fde9e0a864dab8
Josh Zuech <josh.zuech@nextthought.com>
2017-07-26 19:51:45 +0000

    Enable migration to install external identifiers catalog
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@118185 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/install.py
+++ src/nti/dataserver/generations/install.py
@@ -10,7 +10,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 86
+generation = 87
 
 # Allow going forward/backward for testing
 import os
