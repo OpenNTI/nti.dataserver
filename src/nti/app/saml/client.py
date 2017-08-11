5078dd50e795e7b18b201364a41f934be9030d4f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-11 22:21:46 +0000

    remove unicode literals;pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@119320 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/saml/client.py
+++ src/nti/app/saml/client.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -34,16 +34,18 @@ from pyramid import httpexceptions as hexc
 
 from pyramid.threadlocal import get_current_request
 
+from nti.app.saml import make_location as _make_location
+
 from nti.app.saml.interfaces import ISAMLClient
 from nti.app.saml.interfaces import ISAMLNameId
 from nti.app.saml.interfaces import ISAMLIDPInfo
 from nti.app.saml.interfaces import ISAMLACSLinkProvider
 
-from nti.base._compat import unicode_
+from nti.appserver.interfaces import IApplicationSettings
+
+from nti.base._compat import text_
 
 from nti.schema.fieldproperty import createFieldProperties
-
-from . import make_location as _make_location
 
 RELAY_STATE = u'RelayState'
 SAML_RESPONSE = u'SAMLResponse'
@@ -51,18 +53,14 @@ ERROR_STATE_PARAM = u'_nti_error'
 SUCCESS_STATE_PARAM = u'_nti_success'
 
 
-def _get_signer_secret(default_secret='not-very-secure-secret'):
-    try:
-        from nti.appserver.interfaces import IApplicationSettings
-        settings = component.queryUtility(IApplicationSettings) or {}
-        # XXX: Reusing the cookie secret, we should probably have our own
-        secret_key = settings.get('cookie_secret', default_secret)
-        return secret_key
-    except ImportError:
-        return default_secret
+def _get_signer_secret(default_secret=u'not-very-secure-secret'):
+    settings = component.queryUtility(IApplicationSettings) or {}
+    # XXX: Reusing the cookie secret, we should probably have our own
+    secret_key = settings.get('cookie_secret', default_secret)
+    return secret_key or default_secret
 
 
-def _make_signer(secret, salt='nti-saml-relay-state'):
+def _make_signer(secret, salt=u'nti-saml-relay-state'):
     return JSONWebSignatureSerializer(secret, salt=salt)
 
 
@@ -71,13 +69,14 @@ class _SAMLNameId(object):
     createFieldProperties(ISAMLNameId)
 
     __external_class_name__ = 'SAMLNameId'
-    mimeType = mime_type = u'application/vnd.nextthought.saml.samlnameid'
+
+    mimeType = mime_type = 'application/vnd.nextthought.saml.samlnameid'
 
     def __init__(self, name_id):
-        self.nameid = unicode_(name_id.text)
+        self.nameid = text_(name_id.text)
         self.name_format = name_id.format
-        self.name_qualifier = unicode_(name_id.name_qualifier)
-        self.sp_name_qualifier = unicode_(name_id.sp_name_qualifier)
+        self.name_qualifier = text_(name_id.name_qualifier)
+        self.sp_name_qualifier = text_(name_id.sp_name_qualifier)
 
 
 @interface.implementer(ISAMLClient)
@@ -98,19 +97,12 @@ class BasicSAMLClient(object):
 
     saml_client = None
 
-    def __init__(self,
-                 config,
-                 saml_client,
-                 wayf,
-                 cache,
-                 idp_query_param=""):
-
+    def __init__(self, config, saml_client, wayf, cache, idp_query_param=u""):
         self.wayf = wayf
-        self.saml_client = saml_client
-        self.conf = config
         self.cache = cache
+        self.conf = config
+        self.saml_client = saml_client
         self.idp_query_param = idp_query_param
-
         try:
             self.metadata = self.conf.metadata
         except KeyError:
@@ -139,7 +131,8 @@ class BasicSAMLClient(object):
         signer = _make_signer(_get_signer_secret())
         return signer.dumps(state)
 
-    def response_for_logging_out(self, session_auth_id, success, error, entity_id=None):
+    def response_for_logging_out(self, session_auth_id, success,
+                                 error, entity_id=None):
         if not entity_id:
             entity_id = self._pick_idp()
 
@@ -149,16 +142,22 @@ class BasicSAMLClient(object):
         _binding = BINDING_HTTP_REDIRECT
         _cli = self.saml_client
 
+        if error is not None:
+            logger.warn(error)
+
         logger.info("Generating SAML logout request for IDP %s", entity_id)
         srvs = _cli.metadata.single_logout_service(entity_id,
                                                    _binding,
                                                    'idpsso')
         dest = destinations(srvs)[0]
 
-        return hexc.HTTPSeeOther(_make_location(dest,
-                                                {'TargetResource': success,
-                                                 'InErrorResource': success,
-                                                 'SpSessionAuthn': session_auth_id}))
+        location = _make_location(dest,
+                                  {
+                                      'TargetResource': success,
+                                      'InErrorResource': success,
+                                      'SpSessionAuthn': session_auth_id
+                                  })
+        return hexc.HTTPSeeOther(location)
 
     def response_for_logging_in(self, success, error, state={}, passive=False,
                                 force_authn=False, entity_id=None, acs_link=None):
@@ -203,28 +202,28 @@ class BasicSAMLClient(object):
                                                   ISAMLACSLinkProvider)
                 acs_link = provider.acs_link(request) if provider else None
 
-            extra_args = {'force_authn': force,
-                          'is_passive': is_passive,
-                          'assertion_consumer_service_url': acs_link}
+            extra_args = {
+                'force_authn': force,
+                'is_passive': is_passive,
+                'assertion_consumer_service_url': acs_link
+            }
 
             if _cli.authn_requests_signed:
                 _sid = saml2.s_utils.sid()
-                req_id, msg_str = _cli.create_authn_request(
-                    dest,
-                    vorg="",
-                    message_id=_sid,
-                    extensions=extensions,
-                    sign=_cli.authn_requests_signed,
-                    **extra_args)
+                req_id, msg_str = \
+                    _cli.create_authn_request(dest, vorg=u"",
+                                              message_id=_sid,
+                                              extensions=extensions,
+                                              sign=_cli.authn_requests_signed,
+                                              **extra_args)
                 _sid = req_id
             else:
-                req_id, req = _cli.create_authn_request(
-                    dest,
-                    vorg="",
-                    sign=False,
-                    extensions=extensions,
-                    **extra_args)
-                msg_str = "%s" % req
+                req_id, req = \
+                    _cli.create_authn_request(dest, vorg=u"",
+                                              sign=False,
+                                              extensions=extensions,
+                                              **extra_args)
+                msg_str = u"%s" % req
                 _sid = req_id
 
             state = self._create_relay_state(state=state,
@@ -257,12 +256,13 @@ class BasicSAMLClient(object):
     def process_saml_acs_request(self, request):
         for param in (SAML_RESPONSE, RELAY_STATE):
             if param not in request.params:
-                logger.warn('Unexpected saml response. No param named %s', param)
+                logger.warn('Unexpected saml response. No param named %s', 
+                            param)
                 raise hexc.HTTPBadRequest()
 
         # parse out our relay state first so we have it
-        state, success, error = self._extract_relay_state(
-            request.params.get(RELAY_STATE, None))
+        state, success, error = \
+            self._extract_relay_state(request.params.get(RELAY_STATE, None))
 
         # this will throw if the response is invalid, such as invalid authentication
         # but we need to capture and provide our state, success, and error
