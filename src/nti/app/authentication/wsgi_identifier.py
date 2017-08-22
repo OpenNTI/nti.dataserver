e29000a766926b4e7d276d55f287163afd90a4d7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-22 15:14:54 +0000

    use native strings
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@120233 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/authentication/wsgi_identifier.py
+++ src/nti/app/authentication/wsgi_identifier.py
@@ -10,7 +10,7 @@ allowed to log on to this site (assuming a secure cookie secret).
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -19,50 +19,55 @@ from zope import component
 
 from repoze.who.interfaces import IAPIFactory
 
-from .interfaces import ILogonWhitelist
+from nti.app.authentication.interfaces import ILogonWhitelist
+
 
 class IdentifyHandler(object):
-	"""
-	Handles the ``/_ops/identify`` url exactly.
+    """
+    Handles the ``/_ops/identify`` url exactly.
 
-	We will either return a plain 200 response, or a 403
-	(never actually a 401, we don't want to prompt a login dialog anywhere).
-	"""
-	def __init__( self, app ):
-		self.captured = app
+    We will either return a plain 200 response, or a 403
+    (never actually a 401, we don't want to prompt a login dialog anywhere).
+    """
 
-	def __call__( self, environ, start_response ):
-		if environ['PATH_INFO'] != b'/_ops/identify':
-			return self.captured( environ, start_response )
+    def __init__(self, app):
+        self.captured = app
 
-		# Get the (global) API factory...
-		factory = component.getUtility(IAPIFactory)
-		# ...create an API...
-		api = factory(environ)
-		# ...get the cookie plugin...
-		auth_tkt = api.name_registry[factory.default_identifier_name]
-		# ...check the identity...
-		identity = auth_tkt.identify(environ)
-		if identity is not None:
-			# ...yay, got a valid cookie for this site. check the logon whitelist in case
-			# it has changed. NOTE: this does not protect against the user
-			# having been deleted...
-			whitelist = component.getUtility(ILogonWhitelist)
-			username = identity['repoze.who.plugins.auth_tkt.userid']
-			if username not in whitelist:
-				identity = None
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'] != '/_ops/identify':
+            return self.captured(environ, start_response)
 
-		if identity is None:
-			# they failed. either not a valid cookie, or not in the whitelist
-			status = b'403 Forbidden'
-		else:
-			status = b'200 OK'
+        # Get the (global) API factory...
+        factory = component.getUtility(IAPIFactory)
+        # ...create an API...
+        api = factory(environ)
+        # ...get the cookie plugin...
+        auth_tkt = api.name_registry[factory.default_identifier_name]
+        # ...check the identity...
+        identity = auth_tkt.identify(environ)
+        if identity is not None:
+            # ...yay, got a valid cookie for this site. check the logon whitelist in case
+            # it has changed. NOTE: this does not protect against the user
+            # having been deleted...
+            whitelist = component.getUtility(ILogonWhitelist)
+            username = identity['repoze.who.plugins.auth_tkt.userid']
+            if username not in whitelist:
+                identity = None
 
-		start_response( status, [(b'Content-Type', b'text/plain')] )
-		result = (b"",)
+        if identity is None:
+            # they failed. either not a valid cookie, or not in the whitelist
+            status = '403 Forbidden'
+        else:
+            status = '200 OK'
 
-		return result
+        start_response(status, [('Content-Type', 'text/plain')])
+        result = ("",)
 
-def identify_handler_factory( app, global_conf=None ):
-	"Paste factory for :class:`IdentifyHandler`"
-	return IdentifyHandler( app )
+        return result
+
+
+def identify_handler_factory(app, unused_global_conf=None):
+    """
+    Paste factory for :class:`IdentifyHandler`
+    """
+    return IdentifyHandler(app)
