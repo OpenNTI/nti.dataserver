57cfa86bdb5ff7bbf6cfc2f0a8477dc90d64cafa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-22 17:04:29 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@120246 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/authentication/tests/test_zcml.py
+++ src/nti/app/authentication/tests/test_zcml.py
@@ -18,6 +18,7 @@ from zope import component
 from zope.component.hooks import site
 
 from nti.app.authentication.interfaces import ILogonWhitelist
+from nti.app.authentication.interfaces import ISiteLogonWhitelist
 
 from nti.appserver.policies.sites import BASECOPPA as MATHCOUNTS
 
@@ -25,7 +26,8 @@ from nti.site.transient import TrivialSite as _TrivialSite
 
 import nti.testing.base
 
-ZCML_STRING = u"""
+
+WHITE_LIST_ZCML_STRING = u"""
 <configure xmlns="http://namespaces.zope.org/zope"
     xmlns:zcml="http://namespaces.zope.org/zcml"
     xmlns:logon="http://nextthought.com/ntp/logon"
@@ -46,13 +48,37 @@ ZCML_STRING = u"""
             entities="a b c" />
     </registerIn>
 </configure>
-        """
+"""
+
+SITE_LIST_ZCML_STRING = u"""
+<configure xmlns="http://namespaces.zope.org/zope"
+    xmlns:zcml="http://namespaces.zope.org/zcml"
+    xmlns:logon="http://nextthought.com/ntp/logon"
+    i18n_domain='nti.dataserver'>
+
+    <include package="zope.component" />
+    <include package="zope.annotation" />
+    <include package="z3c.baseregistry" file="meta.zcml" />
+    <include package="." file="meta.zcml" />
+    
+    <utility
+        component="nti.appserver.policies.sites.BASECOPPA"
+        provides="zope.component.interfaces.IComponents"
+        name="mathcounts.nextthought.com" />
+    
+    <registerIn registry="nti.appserver.policies.sites.BASECOPPA">
+        <logon:sitelist
+            sites="mathcounts.nextthought.com
+                   mathcounts-alpha.nextthought.com" />
+    </registerIn>
+</configure>
+"""
 
 
 class TestZcml(nti.testing.base.ConfiguringTestBase):
 
-    def test_site_registrations(self):
-        self.configure_string(ZCML_STRING)
+    def test_white_list_registrations(self):
+        self.configure_string(WHITE_LIST_ZCML_STRING)
         assert_that(MATHCOUNTS.__bases__, is_((component.globalSiteManager,)))
         with site(_TrivialSite(MATHCOUNTS)):
             whitelist = component.getUtility(ILogonWhitelist)
@@ -60,3 +86,12 @@ class TestZcml(nti.testing.base.ConfiguringTestBase):
             assert_that(whitelist, has_item('b'))
             assert_that(whitelist, has_item('c'))
             assert_that(whitelist, does_not(has_item('d')))
+            
+    def test_site_list_registrations(self):
+        self.configure_string(SITE_LIST_ZCML_STRING)
+        assert_that(MATHCOUNTS.__bases__, is_((component.globalSiteManager,)))
+        with site(_TrivialSite(MATHCOUNTS)):
+            sites = component.getUtility(ISiteLogonWhitelist)
+            assert_that(sites, has_item('mathcounts.nextthought.com'))
+            assert_that(sites, has_item('mathcounts-alpha.nextthought.com'))
+            assert_that(sites, does_not(has_item('mathcounts-tests.nextthought.com')))
