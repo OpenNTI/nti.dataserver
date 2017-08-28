13a05efad9ca4e7b94043ebb8ef8c37878d6bdb4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-28 02:39:13 +0000

    enable evol 90 to change the storage mapping of the digest email class
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@120601 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/install.py
+++ src/nti/dataserver/generations/install.py
@@ -10,7 +10,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 89
+generation = 90
 
 # Allow going forward/backward for testing
 import os
