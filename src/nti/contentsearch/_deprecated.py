b574f1faccfc24278b8f9021189d150c22fa2009
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-08 21:22:45 +0000

    pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@121384 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentsearch/_deprecated.py
+++ src/nti/contentsearch/_deprecated.py
@@ -49,151 +49,151 @@ class NTICardContent(object):
 
 
 deprecated('create_index_manager', 'No longer used')
-def create_index_manager(*args, **kwargs):
+def create_index_manager(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_sharedWith', 'No longer used')
-def get_sharedWith(*args, **kwargs):
+def get_sharedWith(*unused_args, **unused_kwargs):
     return ()
 
 
 deprecated('get_object_ngrams', 'No longer used')
-def get_object_ngrams(*args, **kwargs):
+def get_object_ngrams(*unused_args, **unused_kwargs):
     return ()
 
 
 deprecated('get_object_content', 'No longer used')
-def get_object_content(*args, **kwargs):
+def get_object_content(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_content', 'No longer used')
-def get_content(*args, **kwargs):
+def get_content(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_oid', 'No longer used')
-def get_oid(*args, **kwargs):
+def get_oid(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_uid', 'No longer used')
-def get_uid(*args, **kwargs):
+def get_uid(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('query_uid', 'No longer used')
-def query_uid(*args, **kwargs):
+def query_uid(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_object', 'No longer used')
-def get_object(*args, **kwargs):
+def get_object(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('query_object', 'No longer used')
-def query_object(*args, **kwargs):
+def query_object(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_type', 'No longer used')
-def get_type(*args, **kwargs):
+def get_type(*unused_args, **unused_kwargs):
     return None
 
 deprecated('get_mimetype', 'No longer used')
-def get_mimetype(*args, **kwargs):
+def get_mimetype(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_containerId', 'No longer used')
-def get_containerId(*args, **kwargs):
+def get_containerId(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_ntiid', 'No longer used')
-def get_ntiid(*args, **kwargs):
+def get_ntiid(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_creator', 'No longer used')
-def get_creator(*args, **kwargs):
+def get_creator(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_title', 'No longer used')
-def get_title(*args, **kwargs):
+def get_title(*unused_args, **unused_kwargs):
     return None
 
 deprecated('get_ngrams', 'No longer used')
-def get_ngrams(*args, **kwargs):
+def get_ngrams(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_ngrams', 'No longer used')
-def get_title_and_ngrams(*args, **kwargs):
+def get_title_and_ngrams(*unused_args, **unused_kwargs):
     return ()
 
 
 deprecated('get_last_modified', 'No longer used')
-def get_last_modified(*args, **kwargs):
+def get_last_modified(*unused_args, **unused_kwargs):
     return 0
 
 deprecated('get_created_time', 'No longer used')
-def get_created_time(*args, **kwargs):
+def get_created_time(*unused_args, **unused_kwargs):
     return 0
 
 
 deprecated('get_tags', 'No longer used')
-def get_tags(*args, **kwargs):
+def get_tags(*unused_args, **unused_kwargs):
     return ()
 
 
 deprecated('get_keywords', 'No longer used')
-def get_keywords(*args, **kwargs):
+def get_keywords(*unused_args, **unused_kwargs):
     return ()
 
 
 deprecated('get_acl', 'No longer used')
-def get_acl(*args, **kwargs):
+def get_acl(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_references', 'No longer used')
-def get_references(*args, **kwargs):
+def get_references(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_channel', 'No longer used')
-def get_channel(*args, **kwargs):
+def get_channel(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_recipients', 'No longer used')
-def get_recipients(*args, **kwargs):
+def get_recipients(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_replacement_content', 'No longer used')
-def get_replacement_content(*args, **kwargs):
+def get_replacement_content(*unused_args, **unused_kwargs):
     return None
 get_replacementContent = get_replacement_content
 
 
 deprecated('get_redaction_explanation', 'No longer used')
-def get_redaction_explanation(*args, **kwargs):
+def get_redaction_explanation(*unused_args, **unused_kwargs):
     return None
 get_redactionExplanation = get_redaction_explanation
 
 
 deprecated('get_replacement_content_and_ngrams', 'No longer used')
-def get_replacement_content_and_ngrams(*args, **kwargs):
+def get_replacement_content_and_ngrams(*unused_args, **unused_kwargs):
     return None
 
 
 deprecated('get_redaction_explanation_and_ngrams', 'No longer used')
-def get_redaction_explanation_and_ngrams(*args, **kwargs):
+def get_redaction_explanation_and_ngrams(*unused_args, **unused_kwargs):
     return None
 
 get_note_title = get_title
@@ -205,7 +205,7 @@ get_post_title_and_ngrams = get_title_and_ngrams
 
 
 deprecated('get_content_and_ngrams', 'No longer used')
-def get_content_and_ngrams(*args, **kwargs):
+def get_content_and_ngrams(*unused_args, **unused_kwargs):
     return None
 
 
