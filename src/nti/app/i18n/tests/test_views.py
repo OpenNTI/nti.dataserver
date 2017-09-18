9111aa55290a5783c59f50bf7267b3c6c64baba4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-18 03:35:51 +0000

    pep8;pylint; remove unicode literals
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@121797 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/i18n/tests/test_views.py
+++ src/nti/app/i18n/tests/test_views.py
@@ -1,74 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
 
-
-.. $Id$
-"""
-
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
-#disable: accessing protected members, too many methods
-#pylint: disable=W0212,R0904
+from hamcrest import raises
+from hamcrest import calling
+from hamcrest import assert_that
+from hamcrest import has_property
+
+import fudge
 
 from zope import interface
 
-from hamcrest import is_
-from nti.app.testing.application_webtest import ApplicationLayerTest
-from hamcrest import assert_that
-from hamcrest import has_property
-from hamcrest import calling
-from hamcrest import raises
 from zope.event import notify
+from pyramid.events import ContextFound
 
-
-from nti.dataserver.interfaces import IUser
-import fudge
+from pyramid.httpexceptions import HTTPNotFound
 
 from pyramid.request import Request
-from pyramid.events import ContextFound
-def adjust(request):
-	notify(ContextFound(request))
 
 from nti.app.i18n.views import StringsLocalizer
 
-from pyramid.httpexceptions import HTTPNotFound
+from nti.dataserver.interfaces import IUser
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+
+def adjust(request):
+    notify(ContextFound(request))
 
 
 class TestApplicationViews(ApplicationLayerTest):
 
-	request = None
-	view = None
+    request = None
+    view = None
 
-	def setUp(self):
-		self.request = Request.blank('/')
-		self.request.environ[b'HTTP_ACCEPT_LANGUAGE'] = b'ru'
-		self.view = StringsLocalizer(self.request)
-		self.view._DOMAIN = 'nti.dataserver'
+    def setUp(self):
+        self.request = Request.blank('/')
+        self.request.environ['HTTP_ACCEPT_LANGUAGE'] = 'ru'
+        self.view = StringsLocalizer(self.request)
+        self.view._DOMAIN = 'nti.dataserver'
 
-	def test_no_domain_found(self):
-		self.view._DOMAIN = 'this domain should never exist'
-		assert_that( calling(self.view),
-					 raises(HTTPNotFound))
+    def test_no_domain_found(self):
+        self.view._DOMAIN = 'this domain should never exist'
+        assert_that(calling(self.view),
+                    raises(HTTPNotFound))
 
-	@fudge.patch('nti.app.i18n.subscribers.get_remote_user',
-				 'nti.app.i18n.adapters.get_remote_user')
-	def test_adjust_remote_user_default(self, fake_get1, fake_get2):
-		@interface.implementer(IUser)
-		class User(object):
-			pass
+    @fudge.patch('nti.app.i18n.subscribers.get_remote_user',
+                 'nti.app.i18n.adapters.get_remote_user')
+    def test_adjust_remote_user_default(self, fake_get1, fake_get2):
+        @interface.implementer(IUser)
+        class User(object):
+            pass
 
-		fake_get1.is_callable().returns(User())
-		fake_get2.is_callable().returns(User())
+        fake_get1.is_callable().returns(User())
+        fake_get2.is_callable().returns(User())
 
+        adjust(self.request)
+        # The accept header rules
+        res = self.view()
 
-		adjust(self.request)
-		# The accept header rules
-		res = self.view()
-
-		assert_that( res,
-					 has_property( 'location',
-								   'http://localhost/app/resources/locales/ru/LC_MESSAGES/nti.dataserver.js'))
+        assert_that(res,
+                    has_property('location',
+                                 'http://localhost/app/resources/locales/ru/LC_MESSAGES/nti.dataserver.js'))
