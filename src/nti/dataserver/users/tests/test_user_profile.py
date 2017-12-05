2c642d6e81eafde6e36abc8c5e77ca495f424f1c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-12-05 01:50:54 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@124548 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/users/tests/test_user_profile.py
+++ src/nti/dataserver/users/tests/test_user_profile.py
@@ -5,6 +5,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+# pylint: disable=protected-access,too-many-public-methods
+
 from hamcrest import is_
 from hamcrest import none
 from hamcrest import is_not
