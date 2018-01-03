da6dfea69201387f197461db88707c9dfb36f593
Josh Zuech <josh.zuech@nextthought.com>
2018-01-03 16:21:32 +0000

    Add paging to catalog collections
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@125217 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/workspaces/views.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Views relating to working with invitations.
+
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from pyramid.view import view_config
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.externalization.view_mixins import BatchingUtilsMixin
+
+from nti.appserver.workspaces.interfaces import ICatalogCollection
+
+from nti.dataserver import authorization as nauth
+
+from nti.externalization.externalization import to_external_object
+
+from nti.externalization.interfaces import StandardExternalFields
+
+
+ITEMS = StandardExternalFields.ITEMS
+TOTAL = StandardExternalFields.TOTAL
+
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@view_config(route_name='objects.generic.traversal',
+             renderer='rest',
+             context=ICatalogCollection,
+             permission=nauth.ACT_READ,
+             request_method='GET')
+class CatalogCollectionView(AbstractAuthenticatedView,
+                            BatchingUtilsMixin):
+    """
+    A generic :class:`ICatalogCollection` view that supports paging on the
+    collection.
+    """
+
+    #: To maintain BWC; disable paging by default.
+    _DEFAULT_BATCH_SIZE = None
+    _DEFAULT_BATCH_START = None
+
+    def __call__(self):
+        result = to_external_object(self.context)
+        self._batch_items_iterable(result, result[ITEMS])
+        result[TOTAL] = len(result[ITEMS])
+        return result
+
+
