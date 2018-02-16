b4130b7af5be27600cecbd4623b28e7619e1249c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-16 23:45:09 +0000

    remove pyramid check
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@126474 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/nti_pserve.py
+++ src/nti/appserver/nti_pserve.py
@@ -18,17 +18,10 @@ patch_nti_pserve_on_import.patch()
 logger = __import__('logging').getLogger(__name__)
 
 import sys
-from pkg_resources import load_entry_point, get_distribution
+from pkg_resources import load_entry_point
 
 
 def main():
-    # We used to monkey patch some things in 1.3. We no longer
-    # do now that we expect to be on 1.5. Check this.
-    pyramid_dist = get_distribution('pyramid')
-    if pyramid_dist and pyramid_dist.has_version():
-        assert pyramid_dist.version.startswith('1.8') \
-            or pyramid_dist.version.startswith('1.9')
-
     sys.exit(
         load_entry_point('pyramid', 'console_scripts', 'pserve')()
     )
