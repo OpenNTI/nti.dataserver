1f9cc76ca46fb479ef6afcea0f9b4bbd90672643
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-04-30 20:44:07 +0000

    pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@128686 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/appserver/rating_views.py
+++ src/nti/appserver/rating_views.py
@@ -6,10 +6,15 @@ Views relating to rating objects.
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-logger = __import__('logging').getLogger(__name__)
+from pyramid import httpexceptions as hexc
+
+from pyramid.interfaces import IRequest
+
+from pyramid.view import view_config
 
 from requests.structures import CaseInsensitiveDict
 
@@ -17,12 +22,6 @@ import simplejson
 
 from zope import interface
 from zope import component
-
-from pyramid import httpexceptions as hexc
-
-from pyramid.interfaces import IRequest
-
-from pyramid.view import view_config
 
 from nti.app.renderers.caching import uncached_in_response
 
@@ -41,6 +40,8 @@ from nti.dataserver import authorization as nauth
 from nti.dataserver import rating as ranking
 
 from nti.dataserver.interfaces import IRatable
+
+logger = __import__('logging').getLogger(__name__)
 
 
 @interface.implementer(IExternalMappingDecorator)
@@ -62,8 +63,8 @@ def _RateView(request):
     values = simplejson.loads(data) if request.body else {}
     values = CaseInsensitiveDict(**values)
     rating = values.get('rate', None) \
-         or values.get('rating', None) \
-         or values.get('ranking', None)
+          or values.get('rating', None) \
+          or values.get('ranking', None)
     if rating is None:
         raise_json_error(request,
                          hexc.HTTPUnprocessableEntity,
