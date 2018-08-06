20e2783b547f1714a18782fb23b6eb67d105cb89
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-06 15:54:29 +0000

    Initial pass for a view to get the users by site
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@130382 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/users/tests/test_list_views.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# pylint: disable=protected-access,too-many-public-methods,arguments-differ
+
+from hamcrest import is_
+from hamcrest import has_entry
+from hamcrest import has_length
+from hamcrest import assert_that
+
+import fudge
+
+from zope import lifecycleevent
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.app.users.utils import set_user_creation_site
+
+from nti.dataserver.tests import mock_dataserver
+
+
+class TestListViews(ApplicationLayerTest):
+
+    @WithSharedApplicationMockDS(users=True, testapp=True, default_authenticate=True)
+    @fudge.patch('nti.app.users.views.list_views.get_component_hierarchy_names')
+    def test_list_users(self, mock_sites):
+        mock_sites.is_callable().returns(("bleach.org",))
+
+        with mock_dataserver.mock_db_trans(self.ds):
+            user = self._create_user(username=u'steve@nt.com',
+                                     external_value={'email': u"steve@nt.com",
+                                                     'realname': u'steve johnson',
+                                                     'alias': u'citadel'})
+            set_user_creation_site(user, "nt.com")
+            lifecycleevent.modified(user)
+
+            user = self._create_user(username=u'rukia@bleach.com',
+                                     external_value={'email': u'rukia@bleach.com',
+                                                     'realname': u'rukia kuchiki',
+                                                     'alias': u'sode no shirayuki'})
+            set_user_creation_site(user, "bleach.org")
+            lifecycleevent.modified(user)
+
+            user = self._create_user(username=u'ichigo@bleach.com',
+                                     external_value={'email': u'ichigo@bleach.com',
+                                                     'realname': u'ichigo kurosaki',
+                                                     'alias': u'zangetsu'})
+            set_user_creation_site(user, "bleach.org")
+            lifecycleevent.modified(user)
+
+            user = self._create_user(username=u'aizen@bleach.com',
+                                     external_value={'email': u'aizen@bleach.com',
+                                                     'realname': u'aizen sosuke',
+                                                     'alias': u'kyoka suigetsu'})
+            set_user_creation_site(user, "bleach.org")
+            lifecycleevent.modified(user)
+
+        url = '/dataserver2/@@site_users'
+        self.testapp.get(url,
+                         extra_environ=self._make_extra_environ(username='steve@nt.com'),
+                         status=401)
+
+        params = {"site": 'othersite.com'}
+        self.testapp.get(url, params, status=422)
+
+        params = {"site": 'bleach.org'}
+        res = self.testapp.get(url, params, status=200)
+        assert_that(res.json_body, has_entry('Total', is_(3)))
+        assert_that(res.json_body, has_entry('Items', has_length(3)))
---
+++ src/nti/app/users/views/list_views.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from requests.structures import CaseInsensitiveDict
+
+from zope.component.hooks import getSite
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.externalization.error import raise_json_error
+
+from nti.app.externalization.view_mixins import BatchingUtilsMixin
+
+from nti.app.users import MessageFactory as _
+
+from nti.dataserver import authorization as nauth
+
+from nti.dataserver.authorization import is_admin_or_site_admin
+
+from nti.dataserver.interfaces import IDataserverFolder
+
+from nti.dataserver.users.utils import get_users_by_site
+
+from nti.externalization.externalization import to_external_object
+
+from nti.externalization.interfaces import LocatedExternalDict
+from nti.externalization.interfaces import StandardExternalFields
+
+from nti.site.site import get_component_hierarchy_names
+
+TOTAL = StandardExternalFields.TOTAL
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@view_config(name='site_users')
+@view_defaults(route_name='objects.generic.traversal',
+               request_method='GET',
+               context=IDataserverFolder,
+               permission=nauth.ACT_READ)
+class SiteUsersView(AbstractAuthenticatedView,
+                    BatchingUtilsMixin):
+
+    _DEFAULT_BATCH_SIZE = 30
+    _DEFAULT_BATCH_START = 0
+
+    def check_access(self):
+        if not is_admin_or_site_admin(self.remoteUser):
+            raise hexc.HTTPForbidden()
+
+    def _get_externalizer(self, user):
+        result = 'summary'
+        if user == self.remoteUser:
+            result = 'personal-summary'
+        return result
+
+    def __call__(self):
+        self.check_access()
+        values = CaseInsensitiveDict(**self.request.params)
+        site = values.get('site') or getSite().__name__
+        if site not in get_component_hierarchy_names():
+            raise_json_error(self.request,
+                             hexc.HTTPUnprocessableEntity,
+                             {
+                                 'message': _(u'Invalid site.'),
+                             },
+                             None)
+
+        def _selector(x):
+            return to_external_object(x, name=self._get_externalizer(x))
+
+        items = get_users_by_site(site)
+        result = LocatedExternalDict()
+        self._batch_items_iterable(result, items, selector=_selector)
+        result[TOTAL] = len(items)
+        return result
