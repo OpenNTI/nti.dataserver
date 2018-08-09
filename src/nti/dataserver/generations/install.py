bed93b9a769e5c15853c09d87c08e154fdf85338
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-09 15:03:39 +0000

    Enable generation 96 to add the "displayname" index
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk@130486 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/generations/install.py
+++ src/nti/dataserver/generations/install.py
@@ -9,7 +9,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-generation = 95
+generation = 96
 
 # Allow going forward/backward for testing
 import os
