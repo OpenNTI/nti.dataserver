407268776509f2207e8376fda4676d23f21da6d4
Josh Zuech <josh.zuech@nextthought.com>
2018-08-23 16:03:17 -0500

    Return request path to describe tx

--- src/nti/appserver/tweens/transaction_tween.py
+++ src/nti/appserver/tweens/transaction_tween.py
@@ -123,7 +123,7 @@ class _transaction_tween(TransactionLoop):
 				request.environ.get('nti.early_teardown_happened')  # see zope_site_tween
 
 	def describe_transaction(self, request):
-		return None # can return None or request.path_info
+		return request.path_info
 
 	def run_handler(self, request):
 		try:
