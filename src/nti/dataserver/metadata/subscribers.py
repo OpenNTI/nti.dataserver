3c214619fb6761d30710e7fa1b2d7bcbf25e1e5a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-09-08 17:16:39 -0500

    add logging

--- src/nti/dataserver/metadata/subscribers.py
+++ src/nti/dataserver/metadata/subscribers.py
@@ -23,12 +23,13 @@ logger = __import__('logging').getLogger(__name__)
 
 
 @component.adapter(IEntity, IObjectRemovedEvent)
-def _on_entity_removed(entity, _):
+def _on_entity_removed(entity, unused_event=None):
     username = entity.username
     logger.info("Removing metadata data for user %s", username)
     delete_entity_metadata(get_metadata_catalog(), username)
 
 
 @component.adapter(IEntity, IObjectRemovedEvent)
-def _clear_replies_to_creator_when_creator_removed(entity, _):
+def _clear_replies_to_creator_when_creator_removed(entity, unused_event=None):
+    logger.info("Clearing replies to for user %s", entity)
     clear_replies_to_creator(get_metadata_catalog(), entity.username)
