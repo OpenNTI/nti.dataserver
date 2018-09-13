e7d4fa9b866e13be6b629f93ad0d8f09233340d3
Josh Zuech <josh.zuech@nextthought.com>
2018-09-12 20:49:13 -0500

    User server pool attribute to monitor pool size

--- src/nti/appserver/application_server.py
+++ src/nti/appserver/application_server.py
@@ -16,34 +16,6 @@ import gevent.pywsgi
 import geventwebsocket.handler
 
 
-class RequestCounter(object):
-
-    def __init__(self, server):
-        self.count = 0
-        self.current_count = 0
-        self.server = server
-
-    @property
-    def pool_size(self):
-        return self.server.worker.worker_connections
-
-    def increment(self):
-        self.count += 1
-        self.current_count += 1
-
-    def decrement(self):
-        self.current_count -= 1
-
-    def __repr__(self):
-        return str(self.current_count)
-
-    def __enter__(self):
-        self.increment()
-
-    def __exit__(self, *args):
-        self.decrement()
-
-
 # In gevent 1.0, gevent.wsgi is an alias for WSGIServer
 class WebSocketServer(gevent.pywsgi.WSGIServer):
     """
@@ -62,11 +34,3 @@ class WebSocketServer(gevent.pywsgi.WSGIServer):
         if not issubclass(self.handler_class, geventwebsocket.handler.WebSocketHandler):
             raise ValueError("Unable to run with a handler that is not a type of %s",
                              WebSocketServer.handler_class)
-        # Stash a request worker in the environ that will give us status on
-        # how our connection pool is being used (see nti_gunicorn.py).
-        self.request_counter = RequestCounter(self)
-        self.environ['nti_request_counter'] = self.request_counter
-
-    def handle(self, *args, **kwargs):
-        with self.request_counter:
-            return super(WebSocketServer, self).handle(*args, **kwargs)
--- src/nti/appserver/nti_gunicorn.py
+++ src/nti/appserver/nti_gunicorn.py
@@ -59,9 +59,10 @@ def _glogging_atoms(self, resp, req, environ, request_time):
     client_version = environ.get('HTTP_X_NTI_CLIENT_VERSION', '-')
     atoms['C'] = "%s@%s" % (client_app_id, client_version)
 
-    request_counter = environ.get('nti_request_counter', None)
-    atoms['R'] = "(%s/%s)" % (getattr(request_counter, 'current_count', ''),
-                               getattr(request_counter, 'pool_size', ''))
+    connection_pool = environ['nti_connection_pool']
+    used_count = connection_pool.size - connection_pool.free_count()
+    atoms['R'] = "(%s/%s)" % (used_count,
+                              connection_pool.size)
     return atoms
 glogging.Logger.atoms = _glogging_atoms
 
@@ -388,6 +389,10 @@ class _ServerFactory(object):
         # We want to log with the appropriate logger, which
         # has monitoring info attached to it
         app_server.log = log
+
+        # Stash the worker connection pool in the environ to see
+        # how our connection pool is being used.
+        app_server.environ['nti_connection_pool'] = app_server.pool
 
         # Now, for logging to actually work, we need to replace
         # the handler class with one that sets up the required values in the
