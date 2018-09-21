48d1c98fafab5d7ddaef8af987981312479bf54d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-09-21 16:45:27 -0500

    pep8; pylint;

--- src/nti/appserver/policies/object_io.py
+++ src/nti/appserver/policies/object_io.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
+from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
-from __future__ import division
 
 from nti.appserver.policies.interfaces import ICommunitySitePolicyUserEventListener
 
 from nti.externalization.datastructures import InterfaceObjectIO
 
-__docformat__ = "restructuredtext en"
-
 logger = __import__('logging').getLogger(__name__)
 
-class SitePolicyUserEventListenerObjectIO(InterfaceObjectIO):
 
+class SitePolicyUserEventListenerObjectIO(InterfaceObjectIO):
     _ext_iface_upper_bound = ICommunitySitePolicyUserEventListener
