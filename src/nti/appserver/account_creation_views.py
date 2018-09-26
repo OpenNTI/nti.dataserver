0fa8be1f7085276574a0e714599e9a6654a56274
Alex Yang <alex.yang@nextthought.com>
2018-09-26 15:47:14 -0500

    comment

--- src/nti/appserver/account_creation_views.py
+++ src/nti/appserver/account_creation_views.py
@@ -546,7 +546,7 @@ class _AccountProfileSchemafier(JsonSchemafier):
         profile_schema = find_most_derived_interface(profile, profile_iface,
                                                      interface.providedBy(profile))
         if readonly_override is None:
-            # Fall back to None if the profile_schema is an IUIReadOnlyProfileSchema.
+            # Fall back to None if the profile_schema is not an IUIReadOnlyProfileSchema.
             readonly_override = profile_schema.isOrExtends(IUIReadOnlyProfileSchema) or None
         super(_AccountProfileSchemafier, self).__init__(profile_schema,
                                                         readonly_override=readonly_override)
