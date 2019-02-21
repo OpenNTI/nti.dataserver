1c5b27047d52434c0328da00a574473f8dde3fcf
Sheldon Smith <sheldon.smith@nextthought.com>
2019-02-21 17:49:09 -0600

    Use get_host_site instead of get_site_for_site_names in job decorator

--- src/nti/dataserver/job/decorators.py
+++ src/nti/dataserver/job/decorators.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from nti.site.site import get_site_for_site_names
+from nti.site import get_host_site
 
 from zope.component.hooks import site as current_site
 
@@ -20,8 +20,7 @@ def RunJobInSite(method):
     @wraps(method)
     def run_job_in_site(self, *args, **kwargs):
         site_name = self.job_kwargs.get('site_name')
-        site = get_site_for_site_names((site_name,))
-        assert site
+        site = get_host_site(site_name)
         with current_site(site):
             return method(self, *args, **kwargs)
     return run_job_in_site
