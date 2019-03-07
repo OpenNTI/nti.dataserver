5c946838c672c33a7e3d486bace159858aa2bea3
Sheldon Smith <sheldon.smith@nextthought.com>
2019-03-07 10:50:07 -0600

    Add a POST view for creating scheduled jobs

--- src/nti/app/scheduler/externalization.py
+++ src/nti/app/scheduler/externalization.py
@@ -5,9 +5,19 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+from datetime import datetime
+
+from pyramid.threadlocal import get_current_request
+
+from zope import component
+
+from nti.appserver.interfaces import IDisplayableTimeProvider
+
 from nti.asynchronous.interfaces import IBaseQueue
 
 from nti.asynchronous.scheduled.interfaces import IScheduledJob
+
+from nti.dataserver.users import User
 
 from nti.externalization.datastructures import InterfaceObjectIO
 
@@ -16,6 +26,15 @@ from nti.externalization.interfaces import LocatedExternalList
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+
+def adjust_timestamp(timestamp, request=None):
+    request = request if request else get_current_request()
+    remote_user = User.get_user(request.remote_user)
+    timezone_util = component.queryMultiAdapter((remote_user, request),
+                                                IDisplayableTimeProvider)
+    date = datetime.utcfromtimestamp(timestamp)
+    return timezone_util.adjust_date(date)
 
 
 class BaseQueueInterfaceObjectIO(InterfaceObjectIO):
@@ -28,22 +47,21 @@ class BaseQueueInterfaceObjectIO(InterfaceObjectIO):
         result['jobs'] = LocatedExternalList(ext_self.all())
         return result
 
-    def updateFromExternalObject(self, parsed, *unused_args, **unused_kwargs):
-        # TODO
-        return super(BaseQueueInterfaceObjectIO, self).updateFromExternalObject(parsed,
-                                                                                *unused_args,
-                                                                                **unused_kwargs)
-
 
 class ScheduledJobInterfaceObjectIO(InterfaceObjectIO):
 
     _ext_iface_upper_bound = IScheduledJob
-    _excluded_out_ivars_ = ('callable',)
+    _excluded_out_ivars_ = frozenset(('callable',
+                                      'timestamp'))
     
     def toExternalObject(self, mergeFrom=None, **kwargs):
         result = super(ScheduledJobInterfaceObjectIO, self).toExternalObject(mergeFrom, **kwargs)
         ext_self = self._ext_replacement()
-        result['callable'] = str(ext_self.callable)  # This could be a function in some cases so we keep it simple
+        result['callable'] = str(ext_self.callable)  # This could be a function so we keep it simple
+        from IPython.terminal.debugger import set_trace;set_trace()
+
+        # Fix up timestamp to be more friendly
+        result['execution_time'] = adjust_timestamp(ext_self.timestamp)
         return result
 
     def updateFromExternalObject(self, parsed, *unused_args, **unused_kwargs):
--- src/nti/app/scheduler/views.py
+++ src/nti/app/scheduler/views.py
@@ -5,6 +5,57 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+
+from zope.dottedname import resolve as dottedname
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
+
+from nti.asynchronous.interfaces import IBaseQueue
+
+from nti.asynchronous.scheduled.job import create_scheduled_job
+from nti.asynchronous.scheduled.job import ScheduledJob
+
+from nti.dataserver import authorization as nauth
+
+from nti.externalization.interfaces import StandardExternalFields
+
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+
+@view_config(route_name='objects.generic.traversal',
+             context=IBaseQueue,
+             request_method='POST',
+             permission=nauth.ACT_NTI_ADMIN,
+             renderer='rest')
+class ScheduledQueuePostView(AbstractAuthenticatedView,
+                             ModeledContentUploadRequestUtilsMixin):
+
+    def createContentObject(self, user, datatype, externalValue, creator):
+        clazz = externalValue.get(StandardExternalFields.CLASS)
+        if clazz != ScheduledJob.__name__:
+            raise hexc.HTTPUnprocessableEntity(u'Class type must be "ScheduledJob" (%s provided)' % clazz)
+        execution_time = externalValue.get('timestamp')  # TODO may do some checking for datetime etc to make friendlier
+        job_args = externalValue.get('job_args')
+        job_kwargs = externalValue.get('job_kwargs')
+        job_id = externalValue.get('job_id')
+        call = externalValue.get('callable')
+        call = dottedname.resolve(call)
+        job = create_scheduled_job(call,
+                                   execution_time,
+                                   jargs=job_args,
+                                   jkwargs=job_kwargs,
+                                   jobid=job_id)
+        # TODO if externalValue.get('periodic'): interface.alsoProvides(job, IPeriodicJob) -- make it reschedule automatically
+        return job
+
+    def __call__(self, *args, **kwargs):
+        job = self.readCreateUpdateContentObject(self.remoteUser)
+        self.context.put(job)
+        return job
