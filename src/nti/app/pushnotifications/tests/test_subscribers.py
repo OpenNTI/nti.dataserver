8ae38420b714da8fe9e3b15a1390dad72429012b
Josh Zuech <josh.zuech@nextthought.com>
2019-04-22 13:34:15 -0500

    Fix test

--- src/nti/app/pushnotifications/tests/test_subscribers.py
+++ src/nti/app/pushnotifications/tests/test_subscribers.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
@@ -10,15 +11,9 @@ __docformat__ = "restructuredtext en"
 import fudge
 
 from hamcrest import is_
-from hamcrest import none
-from hamcrest import has_entries
 from hamcrest import has_length
-from hamcrest import is_not
-from hamcrest import not_none
+from hamcrest import has_entries
 from hamcrest import assert_that
-from hamcrest import same_instance
-
-from zope import component
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 from nti.app.testing.application_webtest import ApplicationLayerTest
@@ -92,7 +87,7 @@ class TestSubscribers(ApplicationLayerTest):
 			assert_that(_mockMailer._calls, has_length(0))
 
 			# Same creator with the creator of inReplyTo
-			comment2 = self._add_comment(user3, topic, inReplyTo=comment1)
+			self._add_comment(user3, topic, inReplyTo=comment1)
 			assert_that(_mockMailer._calls, has_length(0))
 
 			mock_is_subscribed.is_callable().returns(False)
@@ -100,16 +95,16 @@ class TestSubscribers(ApplicationLayerTest):
 			assert_that(_mockMailer._calls, has_length(0))
 
 			mock_is_subscribed.is_callable().returns(None)
-			comment4 = self._add_comment(user4, topic, inReplyTo=comment1)
+			self._add_comment(user4, topic, inReplyTo=comment1)
 			assert_that(_mockMailer._calls, has_length(0))
 
 			# Success
 			mock_is_subscribed.is_callable().returns(True)
-			comment5 = self._add_comment(user4, topic, inReplyTo=comment1)
+			self._add_comment(user4, topic, inReplyTo=comment1)
 			assert_that(_mockMailer._calls, has_length(1))
 			assert_that(_mockMailer._calls[0][0], is_('nti.app.pushnotifications:templates/digest_email'))
 			assert_that(_mockMailer._calls[0][1], is_('Your NextThought Updates'))
-			assert_that(_mockMailer._calls[0][2], is_(['comment_user1@example.com']))
+			assert_that(_mockMailer._calls[0][2], is_([user3]))
 			assert_that(_mockMailer._calls[0][3], is_(None))
 			assert_that(_mockMailer._calls[0][4], is_('.mak'))
 			assert_that(_mockMailer._calls[0][5], has_entries({ 'total_found': 1,
@@ -118,9 +113,9 @@ class TestSubscribers(ApplicationLayerTest):
 																'notable_text': 'A user has replied to one of your comments.',
 																'email_to': 'comment_user1@example.com (comment_user1)'}))
 
-			comment6 = self._add_comment(user3, topic, inReplyTo=comment3)
+			self._add_comment(user3, topic, inReplyTo=comment3)
 			assert_that(_mockMailer._calls, has_length(2))
-			assert_that(_mockMailer._calls[1][2], is_(['comment_user2@example.com']))
+			assert_that(_mockMailer._calls[1][2], is_([user4]))
 			assert_that(_mockMailer._calls[1][5], has_entries({ 'total_found': 1,
 																'display_name': 'user4',
 																'site_name': 'Example',
@@ -134,5 +129,5 @@ class TestSubscribers(ApplicationLayerTest):
 			assert_that(_mockMailer._calls, has_length(0))
 
 			# No email found for the top comment
-			comment12 = self._add_comment(user4, topic, inReplyTo=comment11)
+			self._add_comment(user4, topic, inReplyTo=comment11)
 			assert_that(_mockMailer._calls, has_length(0))
