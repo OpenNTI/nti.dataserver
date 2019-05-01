54d3d5654cc1e8a86922fb66f1230e4a2e26aadc
Josh Zuech <josh.zuech@nextthought.com>
2019-05-01 16:36:14 -0500

    Add various options useful when removing sites

--- src/nti/dataserver/utils/nti_site_ops.py
+++ src/nti/dataserver/utils/nti_site_ops.py
@@ -19,6 +19,12 @@ from zope.component.hooks import site as current_site
 
 from zope.traversing.interfaces import IEtcNamespace
 
+from nti.dataserver.interfaces import ISiteAdminManagerUtility
+
+from nti.dataserver.users.entity import Entity
+
+from nti.dataserver.users.utils import get_entities_by_site
+
 from nti.dataserver.utils import run_with_dataserver
 
 from nti.dataserver.utils.base_script import create_context
@@ -38,7 +44,7 @@ def list_sites():
             print("\t", k, v)
 
 
-def remove_sites(names=(), verbose=True, library=True):
+def remove_sites(names=(), remove_site_entities=True, remove_child_sites=True, remove_only_child_sites=False, excluded_sites=(), verbose=True, library=True):
     if library:
         try:
             from nti.contentlibrary.interfaces import IContentPackageLibrary
@@ -47,10 +53,37 @@ def remove_sites(names=(), verbose=True, library=True):
         except ImportError:  # pragma: no cover
             pass
     sites_folder = component.getUtility(IEtcNamespace, name='hostsites')
-    for name in names or ():
+
+    # Starter set
+    sites_to_remove = set(names)
+    if remove_child_sites:
+        # Add in child sites
+        site_admin_utility = component.getUtility(ISiteAdminManagerUtility)
+        for site_name in list(sites_to_remove):
+            child_site_names = site_admin_utility.get_descendant_site_names(site_name)
+            sites_to_remove.update(child_site_names)
+            if remove_only_child_sites:
+                # Remove parent site if we only remove child sites
+                sites_to_remove.remove(site_name)
+    for name in sites_to_remove or ():
+        if name in excluded_sites:
+            if verbose:
+                print('[%s] Skipping site' % name)
+            continue
+        if remove_site_entities:
+            for entity in get_entities_by_site(name):
+                try:
+                    Entity.delete_entity(entity.username)
+                except KeyError:
+                    # Propbably user contained friends lists
+                    pass
+                else:
+                    if verbose:
+                        print('[%s] Entity removed (%s)' % (name, entity.username))
+        # FIXME: Do we need to remove associated registered components?
         del sites_folder[name]
         if verbose:
-            print('Site ' + name + ' removed')
+            print('[%s] Site removed' % name)
 
 
 def info_site(name):
@@ -75,6 +108,7 @@ def info_site(name):
 def main():
     arg_parser = argparse.ArgumentParser(description="Site operations")
     arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
+                            default=True,
                             dest='verbose')
 
     site_group = arg_parser.add_mutually_exclusive_group()
@@ -92,6 +126,31 @@ def main():
     site_group.add_argument('--info',
                             dest='info',
                             help="print site info")
+
+    # Exclude args
+
+    arg_parser.add_argument('--remove_child_sites',
+                            dest='remove_child_sites',
+                            action='store_true',
+                            default=True,
+                            help="remove child sites (default True)")
+
+    arg_parser.add_argument('--remove_site_entities',
+                            dest='remove_site_entities',
+                            action='store_true',
+                            default=True,
+                            help="remove site entities (default True)")
+
+    arg_parser.add_argument('--remove_only_child_sites',
+                            dest='remove_only_child_sites',
+                            action='store_true',
+                            default=False,
+                            help="remove only child sites of given site (default False)")
+
+    arg_parser.add_argument('--excluded_sites',
+                            dest='excluded_sites',
+                            nargs="+",
+                            help="sites to be excluded during remove")
 
     env_dir = os.getenv('DATASERVER_DIR')
     args = arg_parser.parse_args()
@@ -111,7 +170,12 @@ def main():
                             context=context,
                             minimal_ds=True,
                             verbose=args.verbose,
-                            function=lambda: remove_sites(args.remove, args.verbose))
+                            function=lambda: remove_sites(args.remove,
+                                                          args.remove_site_entities,
+                                                          args.remove_child_sites,
+                                                          args.remove_only_child_sites,
+                                                          args.excluded_sites,
+                                                          args.verbose))
     elif args.info:
         context = create_context(env_dir, with_library=True)
         conf_packages = (conf_package,)
