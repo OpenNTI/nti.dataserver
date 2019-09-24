99f29c505330124dcc346dcb93fec611c83fe05e
Josh Zuech <josh.zuech@nextthought.com>
2019-09-24 08:56:53 -0500

    Validate an image url can be passed in

--- src/nti/app/users/tests/test_community_views.py
+++ src/nti/app/users/tests/test_community_views.py
@@ -725,6 +725,7 @@ class TestCommunityViews(ApplicationLayerTest):
         gif_image_data = 'data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw=='
         png_image_data = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACXBIWXMAAAsTAAALEwEAmpwYAAACbmlUWHRYTUw6Y29tLmFkb2JlLnhtcAAAAAAAPHg6eG1wbWV0YSB4bWxuczp4PSJhZG9iZTpuczptZXRhLyIgeDp4bXB0az0iWE1QIENvcmUgNS4xLjIiPgogICA8cmRmOlJERiB4bWxuczpyZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiPgogICAgICA8cmRmOkRlc2NyaXB0aW9uIHJkZjphYm91dD0iIgogICAgICAgICAgICB4bWxuczp4bXA9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC8iPgogICAgICAgICA8eG1wOkNyZWF0b3JUb29sPkFjb3JuIHZlcnNpb24gMi42LjU8L3htcDpDcmVhdG9yVG9vbD4KICAgICAgPC9yZGY6RGVzY3JpcHRpb24+CiAgICAgIDxyZGY6RGVzY3JpcHRpb24gcmRmOmFib3V0PSIiCiAgICAgICAgICAgIHhtbG5zOnRpZmY9Imh0dHA6Ly9ucy5hZG9iZS5jb20vdGlmZi8xLjAvIj4KICAgICAgICAgPHRpZmY6Q29tcHJlc3Npb24+NTwvdGlmZjpDb21wcmVzc2lvbj4KICAgICAgICAgPHRpZmY6WVJlc29sdXRpb24+NzI8L3RpZmY6WVJlc29sdXRpb24+CiAgICAgICAgIDx0aWZmOlhSZXNvbHV0aW9uPjcyPC90aWZmOlhSZXNvbHV0aW9uPgogICAgICA8L3JkZjpEZXNjcmlwdGlvbj4KICAgPC9yZGY6UkRGPgo8L3g6eG1wbWV0YT4KO/MupgAAAA1JREFUCB1j+P//PwMACPwC/uYM/6sAAAAASUVORK5CYII='
         svg_image_data = "data:image/svg+xml;base64,CgkJPCEtLSBudGktc29saWQtY29sb3IgLS0+CgkJPD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KCQk8c3ZnIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KCQkgPHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgZmlsbD0icmdiKDUyLCAyOCwgMjAxKSIvPgoJCTwvc3ZnPgoJ"
+        image_url = u'https://s3.amazonaws.com/content.nextthought.com/images/ifsta/communityassets/community_avatar.png'
 
         with mock_dataserver.mock_db_trans(self.ds):
             comm = Community.create_community(username='community_image_test')
@@ -759,6 +760,15 @@ class TestCommunityViews(ApplicationLayerTest):
         res = self.testapp.put_json(comm_href, {'avatarURL': gif_image_data})
         res = res.json_body
         assert_that(res, has_entries('avatarURL', not_none(),
+                                     'backgroundURL', none(),
+                                     'blurredAvatarURL', none()))
+
+        # Reset and pass in url
+        self.testapp.put_json(comm_href, {'avatarURL': png_image_data})
+
+        res = self.testapp.put_json(comm_href, {'avatarURL': image_url})
+        res = res.json_body
+        assert_that(res, has_entries('avatarURL', is_(image_url),
                                      'backgroundURL', none(),
                                      'blurredAvatarURL', none()))
 
