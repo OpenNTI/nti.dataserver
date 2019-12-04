865d502d504862e54cb9d0436c3b51fbbbabca86
cutz <christopher.m.utz@gmail.com>
2019-12-04 16:45:45 -0600

    use a one way hash for the opaque identifier

--- src/nti/appserver/logon.py
+++ src/nti/appserver/logon.py
@@ -34,6 +34,8 @@ from __future__ import absolute_import
 import logging
 logger = logging.getLogger(__name__)
 
+import hashlib
+
 from six.moves import urllib_parse
 
 import simplejson as json
@@ -70,10 +72,10 @@ from pyramid.view import view_config
 import requests
 from requests.exceptions import RequestException
 
-from nti.app.authentication import get_remote_user
-
 from nti.app.renderers.interfaces import IResponseCacheController
 from nti.app.renderers.interfaces import IPrivateUncacheableInResponse
+
+from nti.app.users.utils import get_user_creation_sitename
 
 from nti.appserver import MessageFactory as _
 
@@ -132,8 +134,6 @@ from nti.externalization.datastructures import InterfaceObjectIO
 from nti.externalization.interfaces import IExternalObject
 from nti.externalization.interfaces import StandardExternalFields
 from nti.externalization.interfaces import ObjectModifiedFromExternalEvent
-
-from nti.externalization.oids import to_external_oid
 
 from nti.identifiers.interfaces import IUserExternalIdentityContainer
 
@@ -431,7 +431,22 @@ def ping(request):
     result = _Pong(links)
     if username:
         result.AuthenticatedUsername = username
-        result.AuthenticatedUserId = to_external_oid(get_remote_user())
+
+        # As of 12/4/2019 we are only using this for google analytics.
+        # We want something unique and stable that ids the user but that isn't
+        # PII and isn't easy to reverse. We use a one way hash of an identifier
+        # based off the username and user creation site if it exists.
+        #
+        # TODO: Really we add the site because users are only specific to a db.
+        # We collect analytics across all environements and we don't want collisions.
+        # The current site isn't exactly right especially in a child site scenario like
+        # ifsta. It also probably isn't accurate if people jump between vanity urls.
+        # so we use the user creation site. This still isn't guarenteed to be unique, but
+        # in practice, in our current and known future setups it will be
+
+        scope = get_user_creation_sitename(username)
+        uid = username + u'@' + scope if scope else username
+        result.AuthenticatedUserId = hashlib.md5(uid).hexdigest()
     return result
 
 
