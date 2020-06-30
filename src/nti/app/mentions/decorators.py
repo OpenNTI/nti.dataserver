f492042b0f1fd137f0ee3e5a35c9149d54d32d68
Bobby Hagen <bobby.hagen@nextthought.com>
2020-06-30 01:30:55 -0500

    NTI-9340: Decorate IsNewlyMentioned on streamed mentionables that added a mention for the user.

--- src/nti/app/mentions/decorators.py
+++ src/nti/app/mentions/decorators.py
@@ -6,20 +6,24 @@
 
 from __future__ import print_function, absolute_import, division
 
-from nti.app.base.abstract_views import make_sharing_security_check_for_object
-from nti.dataserver.users import User
-from nti.externalization.externalization import toExternalObject
 from pyramid.interfaces import IRequest
 
 from zope import interface
 from zope import component
+
+from nti.app.base.abstract_views import make_sharing_security_check_for_object
 
 from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
 
 from nti.coremetadata.interfaces import IMentionable
 
 from nti.externalization.interfaces import IExternalObjectDecorator
-from zope.cachedescriptors.property import Lazy
+
+from nti.dataserver.interfaces import IStreamChangeEvent
+
+from nti.dataserver.users import User
+
+from nti.externalization.externalization import toExternalObject
 
 logger = __import__('logging').getLogger(__name__)
 
@@ -36,6 +40,25 @@ class _IsMentionedDecorator(AbstractAuthenticatedRequestAwareDecorator):
 
     def _do_decorate_external(self, context, result):
         result['isMentioned'] = True
+
+
+@interface.implementer(IExternalObjectDecorator)
+@component.adapter(IStreamChangeEvent, IRequest)
+class _NewlyMentionedDecorator(AbstractAuthenticatedRequestAwareDecorator):
+
+    def _is_newly_mentioned(self, change):
+        mentions_info = getattr(change, 'mentions_info', None)
+
+        if mentions_info is None:
+            return False
+
+        return self.remoteUser in mentions_info.new_effective_mentions
+
+    def _predicate(self, context, result):
+        return self._is_authenticated and self._is_newly_mentioned(context)
+
+    def _do_decorate_external(self, context, result):
+        result['IsNewlyMentioned'] = True
 
 
 @interface.implementer(IExternalObjectDecorator)
