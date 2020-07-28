eed67e95e4a974640b95e0e0dffa6276a5c3d40a
Josh Zuech <josh.zuech@nextthought.com>
2020-07-28 01:14:08 -0500

    Get int

--- src/nti/appserver/nti_gunicorn.py
+++ src/nti/appserver/nti_gunicorn.py
@@ -352,7 +352,7 @@ class _ServerFactory(object):
         max_accept = os.environ.get('gevent_max_accept', None)
         if max_accept is not None:
             logger.info("Setting max_accept to %s", max_accept)
-            app_server.max_accept = max_accept
+            app_server.max_accept = int(max_accept)
         elif self.worker.cfg.workers > 1:
             logger.info("Setting max_accept to 1")
             app_server.max_accept = 1
