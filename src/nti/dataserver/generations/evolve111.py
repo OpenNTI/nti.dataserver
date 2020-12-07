30c618954c5bc931ca2ee121baebedb4695d0321
Bobby Hagen <bobby.hagen@nextthought.com>
2020-12-07 16:01:09 -0600

    NTI-9924: Make new IAuthentication provided by ds folder traversable.

--- src/nti/dataserver/generations/evolve111.py
+++ src/nti/dataserver/generations/evolve111.py
@@ -8,18 +8,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-from zope.authentication.interfaces import IAuthentication
+from .install import install_zope_authentication
 
 logger = __import__('logging').getLogger(__name__)
 
 generation = 111
-
-
-def install_zope_authentication(dataserver_folder):
-    from nti.app.authentication import _DSAuthentication
-
-    lsm = dataserver_folder.getSiteManager()
-    lsm.registerUtility(_DSAuthentication(), provided=IAuthentication)
 
 
 def do_evolve(context, generation=generation):  # pylint: disable=redefined-outer-name
