8dbcd7bc58ab35070cb028b8fbe51d1152c7bcf8
Josh Zuech <josh.zuech@nextthought.com>
2021-05-17 09:21:38 -0500

    Migrate to use the validating username class in username idx

---
+++ src/nti/dataserver/generations/evolve115.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import component
+from zope import interface
+
+from zope.component.hooks import site as current_site
+
+from nti.coremetadata.interfaces import IX_USERNAME
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+from nti.dataserver.users.index import ValidatingUsername
+from nti.dataserver.users.index import get_entity_catalog
+
+generation = 115
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+    root = None
+
+    def get_by_oid(self, oid, ignore_creator=False):
+        resolver = component.queryUtility(IOIDResolver)
+        if resolver is None:
+            logger.warning(
+                "Using dataserver without a proper ISiteManager."
+            )
+        else:
+            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+        return None
+
+
+def do_evolve(context, generation=generation): # pylint: disable=redefined-outer-name
+    conn = context.connection
+    ds_folder = conn.root()['nti.dataserver']
+
+    mock_ds = MockDataserver()
+    mock_ds.root = ds_folder
+    component.provideUtility(mock_ds, IDataserver)
+
+    with current_site(ds_folder):
+        assert component.getSiteManager() == ds_folder.getSiteManager(), \
+                "Hooks not installed?"
+        entity_catalog = get_entity_catalog()
+        idx = entity_catalog[IX_USERNAME]
+        idx.default_interface = ValidatingUsername
+
+    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+    logger.info('Evolution %s done.', generation)
+
+
+def evolve(context):
+    """
+    Evolve to generation 115 by changing the `default_interface` for these
+    username indexes.
+    """
+    do_evolve(context, generation)
