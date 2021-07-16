606b9547b2cbeb8eb7740605089750966b6c9f11
Josh Zuech <josh.zuech@nextthought.com>
2021-07-16 15:30:13 -0500

    Add ISeatLimit interface

---
+++ src/nti/dataserver/seat_limit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Support for flagging modeled content.
+
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import interface
+
+from nti.dataserver.interfaces import ISeatLimit
+
+from nti.schema.fieldproperty import createDirectFieldProperties
+
+from nti.schema.schema import SchemaConfigured
+
+logger = __import__('logging').getLogger(__name__)
+
+@interface.implementer(ISeatLimit)
+class AbstractSeatLimit(SchemaConfigured):
+
+    createDirectFieldProperties(ISeatLimit)
